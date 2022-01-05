8accb815e1c2134e08c129753c530c460b93503c
Kyle Carberry <kyle@coder.com>
2022-01-05 11:18:29 -0600

    chore: Add peer package for networking (#6)
    This package was pulled straight from github.com/coder/m. Nothing has been changed.
    
    It will be used for networking clients<->workspaces, and coderd<->provisionerd.

--- go.mod
+++ go.mod
@@ -3,10 +3,16 @@ module github.com/coder/coder
 go 1.17
 
 require (
+	cdr.dev/slog v1.4.1
 	github.com/golang-migrate/migrate/v4 v4.15.1
 	github.com/lib/pq v1.10.4
 	github.com/ory/dockertest/v3 v3.8.1
+	github.com/pion/datachannel v1.5.2
+	github.com/pion/logging v0.2.2
+	github.com/pion/transport v0.13.0
+	github.com/pion/webrtc/v3 v3.1.13
 	github.com/stretchr/testify v1.7.0
+	go.uber.org/atomic v1.7.0
 	go.uber.org/goleak v1.1.12
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 )
@@ -15,33 +21,56 @@ require (
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/Microsoft/go-winio v0.5.1 // indirect
 	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/alecthomas/chroma v0.9.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
 	github.com/containerd/continuity v0.1.0 // indirect
+	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dhui/dktest v0.3.8 // indirect
+	github.com/dlclark/regexp2 v1.4.0 // indirect
 	github.com/docker/cli v20.10.11+incompatible // indirect
 	github.com/docker/docker v20.10.12+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
+	github.com/fatih/color v1.12.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/opencontainers/runc v1.0.2 // indirect
+	github.com/pion/dtls/v2 v2.0.13 // indirect
+	github.com/pion/ice/v2 v2.1.18 // indirect
+	github.com/pion/interceptor v0.1.4 // indirect
+	github.com/pion/mdns v0.0.5 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/rtcp v1.2.9 // indirect
+	github.com/pion/rtp v1.7.4 // indirect
+	github.com/pion/sctp v1.8.2 // indirect
+	github.com/pion/sdp/v3 v3.0.4 // indirect
+	github.com/pion/srtp/v2 v2.0.5 // indirect
+	github.com/pion/stun v0.3.5 // indirect
+	github.com/pion/turn/v2 v2.0.6 // indirect
+	github.com/pion/udp v0.1.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	golang.org/x/net v0.0.0-20211013171255-e13a2654a71e // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	golang.org/x/net v0.0.0-20211215060638-4ddde0e984e9 // indirect
 	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ peer/conn_test.go
@@ -0,0 +1,294 @@
+package peer_test
+
+import (
+	"context"
+	"io"
+	"net"
+	"net/http"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/pion/logging"
+	"github.com/pion/transport/vnet"
+	"github.com/pion/webrtc/v3"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
+
+	"cdr.dev/slog"
+	"cdr.dev/slog/sloggers/slogtest"
+	"github.com/coder/coder/peer"
+)
+
+const (
+	disconnectedTimeout = time.Millisecond * 200
+	failedTimeout       = disconnectedTimeout * 5
+	keepAliveInterval   = time.Millisecond * 2
+)
+
+var (
+	// There's a global race in the vnet library allocation code.
+	// This mutex locks around the creation of the vnet.
+	vnetMutex = sync.Mutex{}
+)
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
+
+func TestConn(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Ping", func(t *testing.T) {
+		t.Parallel()
+		client, server, _ := createPair(t)
+		_, err := client.Ping()
+		require.NoError(t, err)
+		_, err = server.Ping()
+		require.NoError(t, err)
+	})
+
+	t.Run("PingNetworkOffline", func(t *testing.T) {
+		t.Parallel()
+		_, server, wan := createPair(t)
+		_, err := server.Ping()
+		require.NoError(t, err)
+		err = wan.Stop()
+		require.NoError(t, err)
+		_, err = server.Ping()
+		require.ErrorIs(t, err, peer.ErrFailed)
+	})
+
+	t.Run("PingReconnect", func(t *testing.T) {
+		t.Parallel()
+		_, server, wan := createPair(t)
+		_, err := server.Ping()
+		require.NoError(t, err)
+		// Create a channel that closes on disconnect.
+		ch, err := server.Dial(context.Background(), "wow", nil)
+		assert.NoError(t, err)
+		err = wan.Stop()
+		require.NoError(t, err)
+		// Once the connection is marked as disconnected, this
+		// channel will be closed.
+		_, err = ch.Read(make([]byte, 4))
+		assert.ErrorIs(t, err, peer.ErrClosed)
+		err = wan.Start()
+		require.NoError(t, err)
+		_, err = server.Ping()
+		require.NoError(t, err)
+	})
+
+	t.Run("Accept", func(t *testing.T) {
+		t.Parallel()
+		client, server, _ := createPair(t)
+		cch, err := client.Dial(context.Background(), "hello", &peer.ChannelOpts{})
+		require.NoError(t, err)
+
+		sch, err := server.Accept(context.Background())
+		require.NoError(t, err)
+
+		_ = cch.Close()
+		_, err = sch.Read(make([]byte, 4))
+		require.ErrorIs(t, err, peer.ErrClosed)
+	})
+
+	t.Run("AcceptNetworkOffline", func(t *testing.T) {
+		t.Parallel()
+		client, server, wan := createPair(t)
+		cch, err := client.Dial(context.Background(), "hello", &peer.ChannelOpts{})
+		require.NoError(t, err)
+		sch, err := server.Accept(context.Background())
+		require.NoError(t, err)
+
+		err = wan.Stop()
+		require.NoError(t, err)
+		_ = cch.Close()
+		_, err = sch.Read(make([]byte, 4))
+		require.ErrorIs(t, err, peer.ErrClosed)
+	})
+
+	t.Run("Buffering", func(t *testing.T) {
+		t.Parallel()
+		client, server, _ := createPair(t)
+		cch, err := client.Dial(context.Background(), "hello", &peer.ChannelOpts{})
+		require.NoError(t, err)
+		sch, err := server.Accept(context.Background())
+		require.NoError(t, err)
+		go func() {
+			for i := 0; i < 1024; i++ {
+				_, err := cch.Write(make([]byte, 4096))
+				require.NoError(t, err)
+			}
+			_ = cch.Close()
+		}()
+		for {
+			_, err = sch.Read(make([]byte, 4096))
+			if err != nil {
+				require.ErrorIs(t, err, peer.ErrClosed)
+				break
+			}
+		}
+	})
+
+	t.Run("NetConn", func(t *testing.T) {
+		t.Parallel()
+		client, server, _ := createPair(t)
+		srv, err := net.Listen("tcp", "127.0.0.1:0")
+		require.NoError(t, err)
+		defer srv.Close()
+		go func() {
+			sch, err := server.Accept(context.Background())
+			require.NoError(t, err)
+			nc2 := sch.NetConn()
+			nc1, err := net.Dial("tcp", srv.Addr().String())
+			require.NoError(t, err)
+			go func() {
+				_, _ = io.Copy(nc1, nc2)
+			}()
+			_, _ = io.Copy(nc2, nc1)
+		}()
+		go func() {
+			s := http.Server{
+				Handler: http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+					rw.WriteHeader(200)
+				}),
+			}
+			defer s.Close()
+			_ = s.Serve(srv)
+		}()
+
+		dt := http.DefaultTransport.(*http.Transport).Clone()
+		var cch *peer.Channel
+		dt.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+			cch, err = client.Dial(context.Background(), "hello", &peer.ChannelOpts{})
+			if err != nil {
+				return nil, err
+			}
+			return cch.NetConn(), nil
+		}
+		c := http.Client{
+			Transport: dt,
+		}
+		req, err := http.NewRequestWithContext(context.Background(), "GET", "http://localhost/", nil)
+		require.NoError(t, err)
+		resp, err := c.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+		require.Equal(t, resp.StatusCode, 200)
+		// Triggers any connections to close.
+		// This test below ensures the DataChannel actually closes.
+		dt.CloseIdleConnections()
+		err = cch.Close()
+		require.ErrorIs(t, err, peer.ErrClosed)
+	})
+
+	t.Run("CloseBeforeNegotiate", func(t *testing.T) {
+		t.Parallel()
+		client, server, _ := createPair(t)
+		err := client.Close()
+		require.NoError(t, err)
+		err = server.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("PingConcurrent", func(t *testing.T) {
+		t.Parallel()
+		client, server, _ := createPair(t)
+		var wg sync.WaitGroup
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			_, err := client.Ping()
+			require.NoError(t, err)
+		}()
+		go func() {
+			defer wg.Done()
+			_, err := server.Ping()
+			require.NoError(t, err)
+		}()
+		wg.Wait()
+	})
+}
+
+func createPair(t *testing.T) (client *peer.Conn, server *peer.Conn, wan *vnet.Router) {
+	lf := logging.NewDefaultLoggerFactory()
+	lf.DefaultLogLevel = logging.LogLevelDisabled
+	vnetMutex.Lock()
+	defer vnetMutex.Unlock()
+	wan, err := vnet.NewRouter(&vnet.RouterConfig{
+		CIDR:          "1.2.3.0/24",
+		LoggerFactory: lf,
+	})
+	require.NoError(t, err)
+	c1Net := vnet.NewNet(&vnet.NetConfig{
+		StaticIPs: []string{"1.2.3.4"},
+	})
+	err = wan.AddNet(c1Net)
+	require.NoError(t, err)
+	c2Net := vnet.NewNet(&vnet.NetConfig{
+		StaticIPs: []string{"1.2.3.5"},
+	})
+	err = wan.AddNet(c2Net)
+	require.NoError(t, err)
+
+	c1SettingEngine := webrtc.SettingEngine{}
+	c1SettingEngine.SetVNet(c1Net)
+	c1SettingEngine.SetPrflxAcceptanceMinWait(0)
+	c1SettingEngine.SetICETimeouts(disconnectedTimeout, failedTimeout, keepAliveInterval)
+	c1, err := peer.Client([]webrtc.ICEServer{}, &peer.ConnOpts{
+		SettingEngine: c1SettingEngine,
+		Logger:        slogtest.Make(t, nil).Named("client").Leveled(slog.LevelDebug),
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		c1.Close()
+	})
+	c2SettingEngine := webrtc.SettingEngine{}
+	c2SettingEngine.SetVNet(c2Net)
+	c2SettingEngine.SetPrflxAcceptanceMinWait(0)
+	c2SettingEngine.SetICETimeouts(disconnectedTimeout, failedTimeout, keepAliveInterval)
+	c2, err := peer.Server([]webrtc.ICEServer{}, &peer.ConnOpts{
+		SettingEngine: c2SettingEngine,
+		Logger:        slogtest.Make(t, nil).Named("server").Leveled(slog.LevelDebug),
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		c2.Close()
+	})
+
+	err = wan.Start()
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		_ = wan.Stop()
+	})
+
+	go func() {
+		for {
+			select {
+			case c := <-c2.LocalCandidate():
+				_ = c1.AddRemoteCandidate(c)
+			case c := <-c2.LocalSessionDescription():
+				c1.SetRemoteSessionDescription(c)
+			case <-c2.Closed():
+				return
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case c := <-c1.LocalCandidate():
+				_ = c2.AddRemoteCandidate(c)
+			case c := <-c1.LocalSessionDescription():
+				c2.SetRemoteSessionDescription(c)
+			case <-c1.Closed():
+				return
+			}
+		}
+	}()
+
+	return c1, c2, wan
+}
