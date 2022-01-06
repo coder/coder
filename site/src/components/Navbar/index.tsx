fdf34ede49b7eb658b8b1763313df59d6f74474f
Bryan Phelps <bryan@coder.com>
2022-01-06 21:13:25 +0000

    Add Coder logo

--- site/src/components/Navbar/index.tsx
+++ site/src/components/Navbar/index.tsx
@@ -1,9 +1,18 @@
 import React from "react"
 import { fade, makeStyles } from "@material-ui/core/styles"
+import Button from "@material-ui/core/Button"
+import { Link, useLocation } from "react-router-dom"
+import { Logo } from "./../Icons/Logo"
 
 export const Navbar: React.FC = () => {
   const styles = useStyles()
-  return <div className={styles.root} />
+  return <div className={styles.root}>
+    <Link to="/">
+      <Button className={styles.logo} variant="text">
+        <Logo fill="white" opacity={1} />
+      </Button>
+    </Link>
+  </div>
 }
 
 const useStyles = makeStyles((theme) => ({
@@ -17,5 +26,16 @@ const useStyles = makeStyles((theme) => ({
       borderTop: `1px solid ${theme.palette.divider}`,
     },
     borderBottom: `1px solid #383838`,
+  },
+  logo: {
+    height: "56px",
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(2),
+    margin: "0 auto",
+    borderRadius: 0,
+    "& svg": {
+      display: "block",
+      width: 125,
+    },
   },
 }))
