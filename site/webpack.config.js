8a5ca3b645205d3cdd21a075ebd899ec399c0c62
Bryan Phelps <bryan@coder.com>
2022-01-10 17:24:30 +0000

    Fix formatting

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = {
-  entry: './site/src/index.tsx',
+  entry: "./site/src/index.tsx",
   mode: "development",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-};
+}
