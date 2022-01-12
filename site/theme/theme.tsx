8ef457f867e049cd8516a7877765436ec6e9d9ac
Bryan Phelps <bryan@coder.com>
2022-01-12 22:02:30 +0000

    Fix stray typo

--- site/theme/theme.tsx
+++ site/theme/theme.tsx
@@ -2,7 +2,7 @@ import { createMuiTheme } from "@material-ui/core/styles"
 import { borderRadius } from "./constants"
 import { CustomPalette, darkPalette, lightPalette } from "./palettes"
 import { typography } from "./typography"
-g
+
 const makeTheme = (palette: CustomPalette) => {
   // Grab defaults to re-use in overrides
   const { breakpoints } = createMuiTheme()
