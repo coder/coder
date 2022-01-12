a1ea349de3861df3aa6de170673c391a88b15701
Presley <presley@coder.com>
2022-01-12 03:45:28 +0000

    Lint

--- provisionersdk/proto/xstate_models.ts
+++ provisionersdk/proto/xstate_models.ts
@@ -1,7 +1,7 @@
-import { createMachine } from 'xstate';
+import { createMachine } from "xstate"
 
-/** If using VSCode, install the XState VSCode extension to get a 
- * "Open Inspector" link above each machine in your code that will 
+/** If using VSCode, install the XState VSCode extension to get a
+ * "Open Inspector" link above each machine in your code that will
  * visualize the machine. Otherwise, you can paste code into stately.ai/viz.
  */
 
@@ -9,108 +9,106 @@ interface WorkspaceContext {
   errorMessage?: string
 }
 
-type WorkspaceEvent =
-  | { type: "START" }
-  | { type: "STOP" }
-  | { type: "REBUILD" }
+type WorkspaceEvent = { type: "START" } | { type: "STOP" } | { type: "REBUILD" }
 
-export const workspaceModel = createMachine<WorkspaceContext, WorkspaceEvent>(
-  {
-    id: "workspaceV2Model",
-    initial: "off",
-    states: {
-      off: { 
-        on: {
-          START: "starting"
-        }
-      },
-      starting: {
-        invoke: {
-          src: "buildWorkspace",
-          onDone: "running",
-          onError: "error",
-        },
-      },
-      running: {
-        on: {
-          STOP: "stopping",
-          REBUILD: "rebuilding"
-        }
-      },
-      stopping: {
-        invoke: {
-          src: "stopWorkspace",
-          onDone: "off",
-          onError: "error"
-        }
-      },
-      rebuilding: {
-        invoke: {
-          src: "stopAndStartWorkspace",
-          onDone: "running",
-          onError: "error"
-        }
-      },
-      error: {
-        entry: "saveErrorMessage",
+export const workspaceModel = createMachine<WorkspaceContext, WorkspaceEvent>({
+  id: "workspaceV2Model",
+  initial: "off",
+  states: {
+    off: {
+      on: {
+        START: "starting",
       },
     },
+    starting: {
+      invoke: {
+        src: "buildWorkspace",
+        onDone: "running",
+        onError: "error",
+      },
+    },
+    running: {
+      on: {
+        STOP: "stopping",
+        REBUILD: "rebuilding",
+      },
+    },
+    stopping: {
+      invoke: {
+        src: "stopWorkspace",
+        onDone: "off",
+        onError: "error",
+      },
+    },
+    rebuilding: {
+      invoke: {
+        src: "stopAndStartWorkspace",
+        onDone: "running",
+        onError: "error",
+      },
+    },
+    error: {
+      entry: "saveErrorMessage",
+    },
   },
-)
+})
 
 export const provisionerModel = createMachine({
-  id: 'provisionerModel',
-  initial: 'starting',
+  id: "provisionerModel",
+  initial: "starting",
   context: {
     automator: undefined,
-    supportedProjects: []
+    supportedProjects: [],
   },
   states: {
     starting: {
       on: {
-        PROVISION: 'provisioning',
-        PARSE: 'parsing'
-      }
+        PROVISION: "provisioning",
+        PARSE: "parsing",
+      },
     },
     provisioning: {
       invoke: {
         src: "provision",
-        onDone: 'off',
-        onError: 'off'
-      }
+        onDone: "off",
+        onError: "off",
+      },
     },
     parsing: {
       invoke: {
-        src: 'parse',
-        onDone: 'off',
-        onError: 'off'
-      }
+        src: "parse",
+        onDone: "off",
+        onError: "off",
+      },
     },
     off: {
-      type: 'final'
-    }
-  }
+      type: "final",
+    },
+  },
 })
 
-export const provisionerDaemonModel = createMachine({
-  id: 'provisionerd',
-  initial: 'polling',
-  states: {
-    polling: {
-      on: { 
-        JOB_HAS_PROVISIONER_REGISTERED: 'executing',
-        NO_JOB_READY: 'polling'
-      }
+export const provisionerDaemonModel = createMachine(
+  {
+    id: "provisionerd",
+    initial: "polling",
+    states: {
+      polling: {
+        on: {
+          JOB_HAS_PROVISIONER_REGISTERED: "executing",
+          NO_JOB_READY: "polling",
+        },
+      },
+      executing: {
+        invoke: {
+          id: "callProvisionerWithParameters",
+          src: "provisionerModel",
+          onDone: { target: "polling", actions: ["returnState", "parseProjectCode"] },
+          onError: { target: "polling", actions: ["returnState", "markJobFailed"] },
+        },
+      },
     },
-    executing: {
-      invoke: {
-        id: 'callProvisionerWithParameters',
-        src: 'provisionerModel',
-        onDone: {target: 'polling', actions: ['returnState', 'parseProjectCode']},
-        onError: {target: 'polling', actions: ['returnState', 'markJobFailed']}
-      }
-    }
-  }
-}, {
-  services: { provisionerModel }
-})
+  },
+  {
+    services: { provisionerModel },
+  },
+)
