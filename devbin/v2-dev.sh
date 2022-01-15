b516310a3737fdaddf33b164a3c8972a626b94d0
Bryan Phelps <bryan@coder.com>
2022-01-15 01:14:04 +0000

    Update v2-dev script

--- devbin/v2-dev.sh
+++ devbin/v2-dev.sh
@@ -5,9 +5,7 @@ set -euo pipefail
 PROJECT_ROOT="$(git rev-parse --show-toplevel)"
 cd "$(PROJECT_ROOT)"
 
-# Do initial build
-make build
-
+# Do initial build - a dev build for coderd that doesn't require front-end assets
+make dev/go/coderd
 
 (trap 'kill 0' SIGINT; ./build/main & yarn dev)
-
