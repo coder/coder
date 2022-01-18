d9c6aebf3154a57b1520b47c09dee11071b2fcc4
Bryan Phelps <bryan@coder.com>
2022-01-18 15:57:16 +0000

    Default CODERV2_HOST to local server on :3000

--- site/scripts/dev.ts
+++ site/scripts/dev.ts
@@ -5,11 +5,17 @@ import next from "next"
 const port = process.env.PORT || 8080
 const dev = process.env.NODE_ENV !== "production"
 
-if (!process.env.CODERV2_HOST) {
-  throw new Error("CODERV2_HOST must be set")
-} else if (!/^http(s)?:\/\//.test(process.env.CODERV2_HOST)) {
-  throw new Error("CODERV2_HOST must be http(s)")
+let coderV2Host = "http://127.0.0.1:3000"
+
+if (process.env.CODERV2_HOST) {
+  if (!/^http(s)?:\/\//.test(process.env.CODERV2_HOST)) {
+    throw new Error("CODERV2_HOST must be http(s)")
+  } else {
+    coderV2Host = process.env.CODERV2_HOST
+  }
 }
+
+console.log(`Using CODERV2_HOST: ${coderV2Host}`)
 
 const app = next({ dev, dir: "./site" })
 const handle = app.getRequestHandler()
@@ -21,7 +27,7 @@ app
     server.use(
       "/api",
       createProxyMiddleware("/api", {
-        target: process.env.CODERV2_HOST,
+        target: coderV2Host,
         ws: true,
         secure: false,
         changeOrigin: true,
