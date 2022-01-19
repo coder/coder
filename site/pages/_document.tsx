97b23832e80a689657d0c3b1f976e89fc409b111
Bryan Phelps <bryan@coder.com>
2022-01-19 05:47:26 +0000

    Add missed favicons

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -18,9 +18,9 @@ class MyDocument extends Document {
           <meta name="application-name" content="Coder" />
           <meta property="og:type" content="website" />
           <meta property="csp-nonce" content="{{ .CSP.Nonce }}" />
-          <link crossOrigin="use-credentials" rel="mask-icon" href="/favicon.svg" color="#000000" />
-          <link rel="alternate icon" type="image/png" href="/favicon.png" />
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link crossOrigin="use-credentials" rel="mask-icon" href="/static/favicon.svg" color="#000000" />
+          <link rel="alternate icon" type="image/png" href="/static/favicon.png" />
+          <link rel="icon" type="image/svg+xml" href="/static/favicon.svg" />
         </Head>
         <body>
           <Main />
