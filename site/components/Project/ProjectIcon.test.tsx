61cd37f1b6711716ed0023c932feab6d4e3e8aae
Bryan Phelps <bryan@coder.com>
2022-01-20 05:55:36 +0000

    Add smoke test for ProjectIcon

---
+++ site/components/Project/ProjectIcon.test.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { screen } from "@testing-library/react"
+
+import { render } from "../../test_helpers"
+import { ProjectIcon } from "./ProjectIcon"
+
+describe("ProjectIcon", () => {
+  it("renders content", async () => {
+    // When
+    render(<ProjectIcon title="Test Title" onClick={() => { return }} />)
+
+    // Then
+    await screen.findByText("Test Title", { exact: false })
+  })
+})
--- site/util/firstOrOnly.ts
+++ site/util/firstOrOnly.ts
@@ -1,3 +1,8 @@
+/**
+ * `firstOrOnly` handles disambiguation of a value that is either a single item or array.
+ *
+ * If an array is passed in, the first item will be returned.
+ */
 export const firstOrOnly = <T>(itemOrItems: T | T[]) => {
   if (Array.isArray(itemOrItems)) {
     return itemOrItems[0]
