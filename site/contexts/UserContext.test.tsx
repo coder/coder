708c509aa5f8f978c657b722f70fcbe6e95d6fad
Bryan Phelps <bryan@coder.com>
2022-01-21 19:01:14 +0000

    Clean up test

--- site/contexts/UserContext.test.tsx
+++ site/contexts/UserContext.test.tsx
@@ -7,25 +7,35 @@ import { render, screen, waitFor } from "@testing-library/react"
 import { User, UserProvider, useUser } from "./UserContext"
 
 namespace Helpers {
+  // Helper component that renders out the state of the `useUser` hook.
+  // It just renders simple text in the 'error', 'me', and 'loading' states,
+  // so that the test can get a peak at the state of the hook.
   const TestComponent: React.FC<{ redirectOnFailure: boolean }> = ({ redirectOnFailure }) => {
     const { me, error } = useUser(redirectOnFailure)
 
-    if (!!error) {
+    if (error) {
       return <div>{`Error: ${error.toString()}`}</div>
     }
-    if (!!me) {
+    if (me) {
       return <div>{`Me: ${me.toString()}`}</div>
     }
 
     return <div>Loading</div>
   }
 
-  export const renderUserContext = (simulatedRequest: () => Promise<User>, redirectOnFailure: boolean) => {
+  // Helper to render a userContext, and all the scaffolding needed
+  // (an SWRConfig as well as a UserPRovider)
+  export const renderUserContext = (
+    simulatedRequest: () => Promise<User>,
+    redirectOnFailure: boolean,
+  ): React.ReactElement => {
     return (
+      // Set up an SWRConfig that works for testing - we'll simulate a request,
+      // and set up the cache to reset every test.
       <SWRConfig
         value={{
           fetcher: simulatedRequest,
-          // Reset cache for every test
+          // Reset cache for every test. Without this, requests will be cached between test cases.
           provider: () => new Map(),
         }}
       >
