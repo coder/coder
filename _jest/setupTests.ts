50537a5745230e8abb22a00b88f5e75897f48bc8
Bryan Phelps <bryan@coder.com>
2022-01-21 02:26:27 +0000

    Fix duplicated line

--- _jest/setupTests.ts
+++ _jest/setupTests.ts
@@ -2,6 +2,6 @@
  * Global setup for our Jest tests
  */
 
-// Set up 'next-router-mock' to with our tests:
-// jest.mock('next/router', () => require('next-router-mock'))
+// Set up 'next-router-mock' to with our front-end tests:
+// https://github.com/scottrippey/next-router-mock#quick-start
 jest.mock("next/router", () => require("next-router-mock"))
