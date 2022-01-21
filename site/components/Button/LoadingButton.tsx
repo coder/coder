5ab009f9aba38c3301ae33f1796972d141417cc2
Bryan Phelps <bryan@coder.com>
2022-01-21 01:18:34 +0000

    Formatting

--- site/components/Button/LoadingButton.tsx
+++ site/components/Button/LoadingButton.tsx
@@ -1,4 +1,3 @@
-
 import Button, { ButtonProps } from "@material-ui/core/Button"
 import CircularProgress from "@material-ui/core/CircularProgress"
 import { makeStyles } from "@material-ui/core/styles"
@@ -11,18 +10,13 @@ export interface LoadingButtonProps extends ButtonProps {
 
 /**
  * LoadingButton is a small wrapper around Material-UI's button to show a loading spinner
- * 
+ *
  * In Material-UI 5+ - this is built-in, but since we're on an earlier version,
  * we have to roll our own.
  */
-export const LoadingButton: React.FC<LoadingButtonProps> = ({
-  loading = false,
-  children,
-  ...rest
-}) => {
+export const LoadingButton: React.FC<LoadingButtonProps> = ({ loading = false, children, ...rest }) => {
   const styles = useStyles()
   const hidden = loading ? { opacity: 0 } : undefined
-
 
   return (
     <Button {...rest} disabled={rest.disabled || loading}>
@@ -36,18 +30,16 @@ export const LoadingButton: React.FC<LoadingButtonProps> = ({
   )
 }
 
-const useStyles = makeStyles(
-  (theme) => ({
-    loader: {
-      position: "absolute",
-      top: "50%",
-      left: "50%",
-      transform: "translate(-50%, -50%)",
-      height: 18,
-      width: 18,
-    },
-    spinner: {
-      color: theme.palette.text.disabled,
-    },
-  })
-)
+const useStyles = makeStyles((theme) => ({
+  loader: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    height: 18,
+    width: 18,
+  },
+  spinner: {
+    color: theme.palette.text.disabled,
+  },
+}))
--- site/components/Form/FormTextField.tsx
+++ site/components/Form/FormTextField.tsx
@@ -26,31 +26,31 @@ export interface FormFieldProps<T> {
  */
 export interface FormTextFieldProps<T>
   extends Pick<
-  TextFieldProps,
-  | "autoComplete"
-  | "autoFocus"
-  | "children"
-  | "className"
-  | "disabled"
-  | "fullWidth"
-  | "helperText"
-  | "id"
-  | "InputLabelProps"
-  | "InputProps"
-  | "inputProps"
-  | "label"
-  | "margin"
-  | "multiline"
-  | "onChange"
-  | "placeholder"
-  | "required"
-  | "rows"
-  | "select"
-  | "SelectProps"
-  | "style"
-  | "type"
-  >,
-  FormFieldProps<T> {
+      TextFieldProps,
+      | "autoComplete"
+      | "autoFocus"
+      | "children"
+      | "className"
+      | "disabled"
+      | "fullWidth"
+      | "helperText"
+      | "id"
+      | "InputLabelProps"
+      | "InputProps"
+      | "inputProps"
+      | "label"
+      | "margin"
+      | "multiline"
+      | "onChange"
+      | "placeholder"
+      | "required"
+      | "rows"
+      | "select"
+      | "SelectProps"
+      | "style"
+      | "type"
+    >,
+    FormFieldProps<T> {
   /**
    * eventTransform is an optional transformer on the event data before it is
    * processed by formik.
