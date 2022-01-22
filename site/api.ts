cb6ad184d7bdd8fa4227e38941eada3978c7ef9e
Bryan Phelps <bryan@coder.com>
2022-01-22 01:43:22 +0000

    API handler for logout

--- site/api.ts
+++ site/api.ts
@@ -14,6 +14,18 @@ export const login = async (email: string, password: string): Promise<LoginRespo
     }),
   })
 
+  return await readOrThrowResponse(response)
+}
+
+export const logout = async (): Promise<void> => {
+  const response = await fetch("/api/v2/logout", {
+    method: "POST",
+  })
+
+  return await readOrThrowResponse(response)
+}
+
+const readOrThrowResponse = async (response: Response): Promise<any> => {
   const body = await response.json()
   if (!response.ok) {
     throw new Error(body.message)
