47f70a99ee237f0bbbac97672b4f30989a35e695
Kyle Carberry <kyle@coder.com>
2022-01-22 21:59:44 +0000

    Fix tests

--- codersdk/users_test.go
+++ codersdk/users_test.go
@@ -2,18 +2,16 @@ package codersdk_test
 
 import (
 	"context"
-	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/coder/coder/coderd"
 	"github.com/coder/coder/coderd/coderdtest"
-	"github.com/coder/coder/codersdk"
 )
 
 func TestUsers(t *testing.T) {
-	t.Run("MultipleInitial", func(t *testing.T) {
+	t.Run("CreateInitial", func(t *testing.T) {
 		server := coderdtest.New(t)
 		_, err := server.Client.CreateInitialUser(context.Background(), coderd.CreateInitialUserRequest{
 			Email:        "wowie@coder.com",
@@ -21,14 +19,12 @@ func TestUsers(t *testing.T) {
 			Username:     "tester",
 			Password:     "moo",
 		})
-		var cerr *codersdk.Error
-		require.ErrorAs(t, err, &cerr)
-		require.Equal(t, cerr.StatusCode(), http.StatusConflict)
-		require.Greater(t, len(cerr.Error()), 0)
+		require.NoError(t, err)
 	})
 
 	t.Run("Get", func(t *testing.T) {
 		server := coderdtest.New(t)
+		_ = server.RandomInitialUser(t)
 		_, err := server.Client.User(context.Background(), "")
 		require.NoError(t, err)
 	})
