07313e9be15941f6263bf22cd99ee01e6914d54c
Bryan Phelps <bryan@coder.com>
2022-01-26 06:01:28 +0000

    Clean-up

--- site/forms/CreateWorkspaceForm.test.tsx
+++ site/forms/CreateWorkspaceForm.test.tsx
@@ -1,23 +1,16 @@
 import { render, screen } from "@testing-library/react"
 import React from "react"
 import { CreateWorkspaceForm } from "./CreateWorkspaceForm"
-import { MockProvisioner, MockOrganization, MockProject, MockWorkspace } from "./../test_helpers"
+import { MockProject, MockWorkspace } from "./../test_helpers"
 
 describe("CreateProjectForm", () => {
   it("renders", async () => {
     // Given
-    const project = MockProject
     const onSubmit = () => Promise.resolve(MockWorkspace)
     const onCancel = () => Promise.resolve()
 
     // When
-    render(
-      <CreateWorkspaceForm
-        project={project}
-        onSubmit={onSubmit}
-        onCancel={onCancel}
-      />,
-    )
+    render(<CreateWorkspaceForm project={MockProject} onSubmit={onSubmit} onCancel={onCancel} />)
 
     // Then
     // Simple smoke test to verify form renders
