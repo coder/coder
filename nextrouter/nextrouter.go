fadc0b671e3607f9f8d55a0c2ca27cdbe9f7c439
Bryan Phelps <bryan@coder.com>
2022-02-05 01:20:09 +0000

    Factor router out

--- nextrouter/nextrouter.go
+++ nextrouter/nextrouter.go
@@ -23,21 +23,28 @@ func serve(fileSystem fs.FS, filePath string) http.HandlerFunc {
 	}
 }
 
-// Handler returns an HTTP handler for serving a next-based static site
-func Handler(fileSystem fs.FS) (http.Handler, error) {
-	rtr := chi.NewRouter()
-
-	files, err := fs.ReadDir(fileSystem, ".")
+func buildRouter(parentFileSystem fs.FS, path string) (chi.Router, error) {
+	fileSystem, err := fs.Sub(parentFileSystem, path)
 	if err != nil {
 		return nil, err
 	}
-
+	files, err := fs.ReadDir(fileSystem, ".")
+	rtr := chi.NewRouter()
 	rtr.Route("/", func(r chi.Router) {
 		for _, file := range files {
 			name := file.Name()
 			rtr.Get("/"+name, serve(fileSystem, name))
 		}
 	})
-
 	return rtr, nil
+}
+
+// Handler returns an HTTP handler for serving a next-based static site
+func Handler(fileSystem fs.FS) (http.Handler, error) {
+
+	router, err := buildRouter(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
+	return router, nil
 }
