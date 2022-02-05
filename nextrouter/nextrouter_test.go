33897e568fc6db40c7a1a5ff9d88b08cf443192a
Bryan Phelps <bryan@coder.com>
2022-02-05 00:41:52 +0000

    Integrate test server too

--- nextrouter/nextrouter_test.go
+++ nextrouter/nextrouter_test.go
@@ -1,7 +1,10 @@
 package nextrouter_test
 
 import (
+	"context"
 	"io/fs"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/psanford/memfs"
@@ -14,8 +17,17 @@ func TestConn(t *testing.T) {
 	t.Run("Smoke test", func(t *testing.T) {
 		t.Parallel()
 
+		server := httptest.NewServer(nil)
+
+		req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, server.URL, nil)
+		require.NoError(t, err)
+		res, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+		defer res.Body.Close()
+		require.Equal(t, res.StatusCode, 404)
+
 		rootFS := memfs.New()
-		err := rootFS.MkdirAll("test/a/b", 0777)
+		err = rootFS.MkdirAll("test/a/b", 0777)
 		require.NoError(t, err)
 
 		rootFS.WriteFile("test/a/b/c.txt", []byte("test123"), 0755)
