a5061480b38e2ba8260fe5bf5049c035b16add3d
Bryan Phelps <bryan@coder.com>
2022-02-05 02:24:54 +0000

    Remove now-unnecessary smoke test

--- nextrouter/nextrouter_test.go
+++ nextrouter/nextrouter_test.go
@@ -157,29 +157,4 @@ func TestNextRouter(t *testing.T) {
 		defer res.Body.Close()
 		require.Equal(t, res.StatusCode, 404)
 	})
-
-	t.Run("Smoke test", func(t *testing.T) {
-		t.Parallel()
-
-		server := httptest.NewServer(nil)
-
-		req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, server.URL, nil)
-		require.NoError(t, err)
-		res, err := http.DefaultClient.Do(req)
-		require.NoError(t, err)
-		defer res.Body.Close()
-		require.Equal(t, res.StatusCode, 404)
-
-		rootFS := memfs.New()
-		err = rootFS.MkdirAll("test/a/b", 0777)
-		require.NoError(t, err)
-
-		rootFS.WriteFile("test/a/b/c.txt", []byte("test123"), 0755)
-		content, err := fs.ReadFile(rootFS, "test/a/b/c.txt")
-		require.NoError(t, err)
-
-		require.Equal(t, string(content), "test123")
-
-		//require.Equal(t, 1, 2)
-	})
 }
