57143dafaa7af326e97d327fc84a63504d19a78e
Bryan Phelps <bryan@coder.com>
2022-02-09 17:45:13 +0000

    Initial scaffolding for workspaces page

---
+++ site/pages/workspaces/[user]/[workspace].tsx
@@ -0,0 +1,43 @@
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import { useRouter } from "next/router"
+import { Navbar } from "../../components/Navbar"
+import { Footer } from "../../components/Page"
+import { useUser } from "../../contexts/UserContext"
+
+//import { Workspace } from "../../components/Workspace"
+//import { MockWorkspace } from "../../test_helpers"
+
+const WorkspacesPage: React.FC = () => {
+  const styles = useStyles()
+  const router = useRouter()
+  const { me, signOut } = useUser(true)
+
+  const { user, workspace } = router.query
+
+  return (
+    <div className={styles.root}>
+      <Navbar user={me} onSignOut={signOut} />
+
+      <div className={styles.inner}>
+        <div>Hello, world</div>
+      </div>
+
+      <Footer />
+    </div>
+  )
+}
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  inner: {
+    maxWidth: "1380px",
+    margin: "1em auto",
+    width: "100%",
+  },
+}))
+
+export default WorkspacesPage
