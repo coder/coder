0e323ce2dbd61094635f618fae15449d93155132
Bryan Phelps <bryan@coder.com>
2022-02-17 00:20:02 +0000

    Add storybook for CliAuthToken

---
+++ site/components/SignIn/CliAuthToken.stories.tsx
@@ -0,0 +1,16 @@
+import { Story } from "@storybook/react"
+import React from "react"
+import { CliAuthToken, CliAuthTokenProps } from "./CliAuthToken"
+
+export default {
+  title: "SignIn/CliAuthToken",
+  component: CliAuthToken,
+  argTypes: {
+    sessionToken: { control: "text", defaultValue: "some-session-token" },
+  },
+}
+
+const Template: Story<CliAuthTokenProps> = (args) => <CliAuthToken {...args} />
+
+export const Example = Template.bind({})
+Example.args = {}
