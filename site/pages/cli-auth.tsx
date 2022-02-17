6607a5ad55adf9d6c622ef5afb8046e7851409c7
Bryan Phelps <bryan@coder.com>
2022-02-17 00:15:45 +0000

    Factor out CliAuth token component

--- site/pages/cli-auth.tsx
+++ site/pages/cli-auth.tsx
@@ -1,9 +1,7 @@
-import Paper from "@material-ui/core/Paper"
-import Typography from "@material-ui/core/Typography"
 import { makeStyles } from "@material-ui/core/styles"
 import React, { useEffect, useState } from "react"
 import { getApiKey } from "../api"
-import { CodeExample } from "../components/CodeExample"
+import { CliAuthToken } from "../components/SignIn"
 
 import { FullScreenLoader } from "../components/Loader/FullScreenLoader"
 import { useUser } from "../contexts/UserContext"
@@ -28,28 +26,18 @@ const CliAuthenticationPage: React.FC = () => {
 
   return (
     <div className={styles.root}>
-      <Paper className={styles.container}>
-        <Typography className={styles.title}>Session Token</Typography>
-        <CodeExample code={apiKey} />
-      </Paper>
+      <CliAuthToken sessionToken={apiKey} />
     </div>
   )
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: "100vh",
-    height: "100vw",
+    width: "100vw",
+    height: "100vh",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-  },
-  title: {
-    marginBottom: theme.spacing(2),
-  },
-  container: {
-    maxWidth: "680px",
-    padding: theme.spacing(2),
   },
 }))
 
