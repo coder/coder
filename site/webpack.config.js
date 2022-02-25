69010f8c38e89c3946b75a76d9e25356843ab4dc
Bryan Phelps <bryan@coder.com>
2022-02-25 03:42:19 +0000

    Hook up workspaces route

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,13 +1,34 @@
 const path = require("path")
 
 module.exports = {
+  devServer: {
+    allowedHosts: 'all',
+    client: {
+      overlay: true,
+      progress: false,
+    },
+    devMiddleware: {
+      publicPath: '/',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    historyApiFallback: {
+      index: 'index.html'
+    },
+    hot: true,
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+    static: ["./static"]
+  },
   entry: "./index.tsx",
   mode: "development",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: ["ts-loader"],
         exclude: [
           /node_modules/,
         ],
@@ -21,4 +42,5 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "out"),
   },
+  target: "web"
 }
