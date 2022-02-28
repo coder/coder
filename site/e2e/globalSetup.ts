5eb93147750c22783234121563a9adc72be52a8d
Bryan Phelps <bryan@coder.com>
2022-02-28 23:20:24 +0000

    Use coderd directly, create initial user in setup script

---
+++ site/e2e/globalSetup.ts
@@ -0,0 +1,26 @@
+import { FullConfig, request } from '@playwright/test';
+
+async function globalSetup(config: FullConfig) {
+  const { baseURL } = config.projects[0].use;
+  console.log(" -- Starting global setup: " + baseURL)
+
+  // Create a context that will issue http requests.
+  const context = await request.newContext({
+    baseURL,
+  });
+
+  // Create initial user
+  await context.post(
+    "/api/v2/user", {
+    data: {
+      email: "admin@coder.com",
+      username: "admin",
+      password: "password",
+      organization: "acme-corp"
+    }
+  });
+
+  console.log("-- Created initial user.")
+}
+
+export default globalSetup;
--- site/e2e/playwright.config.ts
+++ site/e2e/playwright.config.ts
@@ -3,21 +3,21 @@ import { PlaywrightTestConfig } from "@playwright/test"
 
 const config: PlaywrightTestConfig = {
   testDir: "tests",
+  globalSetup: require.resolve('./globalSetup'),
 
   use: {
+    baseURL: "http://localhost:3000",
     video: "retain-on-failure",
   },
 
   // `webServer` tells Playwright to launch a test server - more details here:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
-    command: path.join(__dirname, "../../develop.sh"),
-    port: 8080,
+    // Run the 'coderd' binary directly
+    command: path.join(__dirname, "../../bin/coderd"),
+    port: 3000,
     timeout: 120 * 10000,
     reuseExistingServer: false,
-    env: {
-      HUMAN_LOG: "1",
-    },
   },
 }
 
