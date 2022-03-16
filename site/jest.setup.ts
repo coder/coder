ce7008437900ee3bc066079e4f5a1ba3d5a9e052
Bryan Phelps <bryan@coder.com>
2022-03-16 21:44:41 +0000

    Add test cases; make sure green

--- site/jest.setup.ts
+++ site/jest.setup.ts
@@ -5,18 +5,18 @@
 // For now, I limited this to just 'error' - but failing on warnings
 // would be a nice next step! We may need to filter out some noise
 // from material-ui though.
-const CONSOLE_FAIL_TYPES = ['error', /* 'warn' */]
+const CONSOLE_FAIL_TYPES = ["error" /* 'warn' */]
 
 // Throw errors when a `console.error` or `console.warn` happens
 // by overriding the functions
 CONSOLE_FAIL_TYPES.forEach((logType: string) => {
+  // Suppressing the no-explicit-any to override certain console functions for testing
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const consoleAsAny = global.console as any
   consoleAsAny[logType] = (message: string): void => {
-    throw new Error(
-      `Failing due to console.${logType} while running test!\n\n${message}`,
-    )
+    throw new Error(`Failing due to console.${logType} while running test!\n\n${message}`)
   }
 })
 
 // This is needed because we are compiling under `--isolatedModules`
-export { }
+export {}
