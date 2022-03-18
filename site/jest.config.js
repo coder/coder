b58e8c30e3cfd4129c130b2216e8f559cee0758c
G r e y <grey@coder.com>
2022-03-18 01:14:48 +0000

    chore: improve sb, tsc configs
    Summary:
    
    This commit is a bit of a shotgun fix for various project settings.
    Realistically, they could've been separate commits, but this is
    convenience for just getting things into a green state to unblock
    further work.
    
    Details:
    
    - Use our version of TS in vscode plugins
    - organize vscode/settings.json
    - fix tsconfig.test and tsconfig.prod (removes errors in test files)
    - only use prod tsconfig in webpack
    - point .eslintrc to both test and prod configs
    - cleanup storybook

--- site/jest.config.js
+++ site/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       globals: {
         "ts-jest": {
-          tsconfig: "tsconfig.test.json",
+          tsconfig: "./tsconfig.test.json",
         },
       },
       coverageReporters: ["text", "lcov"],
