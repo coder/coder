577f489e41ca372b61eb0ac4c3927318f9040ecd
Presley <presley@coder.com>
2022-03-30 23:30:02 +0000

    chore: update develop script

--- develop.sh
+++ develop.sh
@@ -2,24 +2,8 @@
 
 set -euo pipefail
 
-EMAIL="${EMAIL:-admin@coder.com}"
-USERNAME="${USERNAME:-admin}"
-ORGANIZATION="${ORGANIZATION:-ACME-Corp}"
-PASSWORD="${PASSWORD:-password}"
 PROJECT_ROOT="$(git rev-parse --show-toplevel)"
 cd "${PROJECT_ROOT}"
-
-# Helper to create an initial user
-function create_initial_user() {
-  # TODO: We need to wait for `coderd` to spin up -
-  # need to replace with a deterministic strategy
-  sleep 5s
-
-  curl -X POST \
-    -d "{\"email\": \"$EMAIL\", \"username\": \"$USERNAME\", \"organization\": \"$ORGANIZATION\", \"password\": \"$PASSWORD\"}" \
-    -H 'Content-Type:application/json' \
-    http://localhost:3000/api/v2/users/first
-}
 
 # Run yarn install, to make sure node_modules are ready to go
 "$PROJECT_ROOT/scripts/yarn_install.sh"
@@ -31,5 +15,5 @@ function create_initial_user() {
   trap 'kill 0' SIGINT
   create_initial_user &
   CODERV2_HOST=http://127.0.0.1:3000 yarn --cwd=./site dev &
-  go run cmd/coder/main.go start
+  go run cmd/coder/main.go start --dev --tunnel=false
 )
