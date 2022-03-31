65ef4e3fb4c9455c1522c9035df6c784b9ce9fda
Steven Masley <stevenmasley@coder.com>
2022-03-31 16:02:47 -0500

    Use an interface for the object

--- coderd/authz/object.go
+++ coderd/authz/object.go
@@ -1,12 +1,43 @@
 package authz
 
-// Object is the resource being accessed
-type Object struct {
-	ObjectID   string `json:"object_id"`
-	OwnerID    string `json:"owner_id"`
-	OrgOwnerID string `json:"org_owner_id"`
+type Object interface {
+	ID() string
+	ResourceType() ResourceType
+}
+
+type UserObject interface {
+	Object
+	OwnerID() string
+}
+
+type OrgObject interface {
+	Object
+	OrgOwnerID() string
+}
+
+// ZObject is the resource being accessed
+type ZObject struct {
+	ObjectID string `json:"object_id"`
+	Owner    string `json:"owner_id"`
+	OrgOwner string `json:"org_owner_id"`
 
 	// ObjectType is "workspace", "project", "devurl", etc
 	ObjectType ResourceType `json:"object_type"`
 	// TODO: SharedUsers?
+}
+
+func (z ZObject) ID() string {
+	return z.ObjectID
+}
+
+func (z ZObject) ResourceType() ResourceType {
+	return z.ObjectType
+}
+
+func (z ZObject) OwnerID() string {
+	return z.Owner
+}
+
+func (z ZObject) OrgOwnerID() string {
+	return z.OrgOwner
 }
