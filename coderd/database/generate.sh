7e32318d7dfeed3e80695e0bacc28873f68dcce1
Colin Adler <colin@coder.com>
2022-03-31 16:50:58 -0500

    comment generate.sh

--- coderd/database/generate.sh
+++ coderd/database/generate.sh
@@ -8,19 +8,26 @@ sqlc generate
 
 first=true
 for fi in queries/*.sql.go; do
+    # Find the last line from the imports section and add 1.
     cut=$(grep -n ')' "$fi" | head -n 1 | cut -d: -f1)
     cut=$((cut + 1))
 
+    # Copy the header from the first file only, ignoring the source comment.
     if $first; then
         head -n 4 < "$fi" | grep -v "source" > queries.sql.go
         first=false
     fi
 
+    # Append the file past the imports section into queries.sql.go.
     tail -n "+$cut" < "$fi" >> queries.sql.go
 done
 
+# Remove temporary go files.
 rm -f queries/*.go
 
+# Ensure correct imports exist.
 goimports -w queries.sql.go
+
+# Fix struct/interface names.
 gofmt -w -r 'Querier -> querier' -- *.go
 gofmt -w -r 'Queries -> sqlQuerier' -- *.go
