50a9b26055c9ace125e1cc6a675a291fad2bc1ce
Cian Johnston <public@cianjohnston.ie>
2022-04-01 21:17:12 +0100

    feat: add crontab package for supporting autostart/stop. This is basically a small wrapper around robfig/cron/v3.
    Fixes #817.

---
+++ coderd/crontab/crontab.go
@@ -0,0 +1,64 @@
+package crontab
+
+import (
+	"time"
+
+	"github.com/robfig/cron/v3"
+	"golang.org/x/xerrors"
+)
+
+const parserFormat = cron.Minute | cron.Hour | cron.Dow
+
+var defaultParser = cron.NewParser(parserFormat)
+
+// WeeklySchedule represents a weekly cron schedule serializable to and from a string.
+//
+// Example Usage:
+//  local_sched, _ := cron.Parse("59 23 *")
+//  fmt.Println(sched.Next(time.Now().Format(time.RFC3339)))
+//  // Output: 2022-04-04T23:59:00Z
+//  us_sched, _ := cron.Parse("TZ=US/Central 30 9 1-5")
+//  fmt.Println(sched.Next(time.Now()).Format(time.RFC3339))
+//  // Output: 2022-04-04T14:30:00Z
+type WeeklySchedule interface {
+	String() string
+	Next(time.Time) time.Time
+}
+
+// cronSchedule is a thin wrapper for cron.SpecSchedule that implements Stringer.
+type cronSchedule struct {
+	sched *cron.SpecSchedule
+	// XXX: there isn't any nice way for robfig/cron to serialize
+	spec string
+}
+
+var _ WeeklySchedule = (*cronSchedule)(nil)
+
+// String serializes the schedule to its original human-friendly format.
+func (s cronSchedule) String() string {
+	return s.spec
+}
+
+// Next returns the next time in the schedule relative to t.
+func (s cronSchedule) Next(t time.Time) time.Time {
+	return s.sched.Next(t)
+}
+
+func Parse(spec string) (WeeklySchedule, error) {
+	s, err := defaultParser.Parse(spec)
+	if err != nil {
+		return nil, xerrors.Errorf("parse schedule: %w", err)
+	}
+
+	schedule, ok := s.(*cron.SpecSchedule)
+	if !ok {
+		return nil, xerrors.Errorf("expected cron.SpecSchedule but got %T", s)
+	}
+
+	cs := &cronSchedule{
+		sched: schedule,
+		spec:  spec,
+	}
+	return cs, nil
+
+}
---
+++ coderd/crontab/crontab_test.go
@@ -0,0 +1,65 @@
+package crontab
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Parse(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		name          string
+		spec          string
+		at            time.Time
+		expectedNext  time.Time
+		expectedError string
+	}{
+		{
+			name:          "with timezone",
+			spec:          "TZ=US/Central 30 9 1-5",
+			at:            time.Date(2022, 4, 1, 14, 29, 0, 0, time.UTC),
+			expectedNext:  time.Date(2022, 4, 1, 14, 30, 0, 0, time.UTC),
+			expectedError: "",
+		},
+		{
+			name:          "without timezone",
+			spec:          "30 9 1-5",
+			at:            time.Date(2022, 4, 1, 9, 29, 0, 0, time.Local),
+			expectedNext:  time.Date(2022, 4, 1, 9, 30, 0, 0, time.Local),
+			expectedError: "",
+		},
+		{
+			name:          "invalid schedule",
+			spec:          "asdfasdfasdfsd",
+			at:            time.Time{},
+			expectedNext:  time.Time{},
+			expectedError: "parse schedule: expected exactly 3 fields, found 1: [asdfasdfasdfsd]",
+		},
+		{
+			name:          "invalid location",
+			spec:          "TZ=Fictional/Country 30 9 1-5",
+			at:            time.Time{},
+			expectedNext:  time.Time{},
+			expectedError: "parse schedule: provided bad location Fictional/Country: unknown time zone Fictional/Country",
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			actual, err := Parse(tc.spec)
+			if tc.expectedError == "" {
+				nextTime := actual.Next(tc.at)
+				require.NoError(t, err)
+				require.Equal(t, tc.expectedNext, nextTime)
+				require.Equal(t, tc.spec, actual.String())
+			} else {
+				require.EqualError(t, err, tc.expectedError)
+				require.Nil(t, actual)
+			}
+		})
+	}
+}
