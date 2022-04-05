6e29dd2b3f8bc8755c854df47b2350764fd60ed5
Bruno <bruno@coder.com>
2022-04-05 14:31:46 +0000

    fix: add Section.Action example

--- site/src/components/Section/SectionView.stories.tsx
+++ site/src/components/Section/SectionView.stories.tsx
@@ -1,3 +1,4 @@
+import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
 import { Story } from "@storybook/react"
 import React from "react"
@@ -20,9 +21,17 @@ Example.args = {
   title: "User Settings",
   description: "Add your personal info",
   children: (
-    <form style={{ display: "grid", gridAutoFlow: "row", gap: 12 }}>
-      <TextField label="Name" variant="filled" fullWidth />
-      <TextField label="Email" variant="filled" fullWidth />
-    </form>
+    <>
+      <form style={{ display: "grid", gridAutoFlow: "row", gap: 12 }}>
+        <TextField label="Name" variant="filled" fullWidth />
+        <TextField label="Email" variant="filled" fullWidth />
+      </form>
+
+      <Section.Action>
+        <Button variant="contained" color="primary">
+          Submit
+        </Button>
+      </Section.Action>
+    </>
   ),
 }
