70a782cd292be4d4e41aa5c65f889e18f8247074
Bruno <bruno@coder.com>
2022-04-06 19:08:53 +0000

    Fix AppRouter

--- site/src/AppRouter.tsx
+++ site/src/AppRouter.tsx
@@ -6,19 +6,13 @@ import { NotFoundPage } from "./pages/404"
 import { CliAuthenticationPage } from "./pages/cli-auth"
 import { HealthzPage } from "./pages/healthz"
 import { SignInPage } from "./pages/login"
-<<<<<<< Updated upstream
-import { ProjectsPage } from "./pages/projects"
-import { ProjectPage } from "./pages/projects/[organization]/[project]"
-import { CreateWorkspacePage } from "./pages/projects/[organization]/[project]/create"
-=======
 import { PreferencesAccountPage } from "./pages/preferences/account"
 import { PreferencesLinkedAccountsPage } from "./pages/preferences/linked-accounts"
 import { PreferencesSecurityPage } from "./pages/preferences/security"
 import { PreferencesSSHKeysPage } from "./pages/preferences/ssh-keys"
-import { TemplatesPage } from "./pages/templates"
-import { TemplatePage } from "./pages/templates/[organization]/[template]"
-import { CreateWorkspacePage } from "./pages/templates/[organization]/[template]/create"
->>>>>>> Stashed changes
+import { ProjectsPage } from "./pages/projects"
+import { ProjectPage } from "./pages/projects/[organization]/[project]"
+import { CreateWorkspacePage } from "./pages/projects/[organization]/[project]/create"
 import { WorkspacePage } from "./pages/workspaces/[workspace]"
 
 export const AppRouter: React.FC = () => (
@@ -77,8 +71,6 @@ export const AppRouter: React.FC = () => (
         />
       </Route>
 
-<<<<<<< Updated upstream
-=======
       <Route path="preferences">
         <Route index element={<Navigate to="account" />} />
         <Route
@@ -115,7 +107,6 @@ export const AppRouter: React.FC = () => (
         />
       </Route>
 
->>>>>>> Stashed changes
       {/* Using path="*"" means "match anything", so this route
         acts like a catch-all for URLs that we don't have explicit
         routes for. */}
