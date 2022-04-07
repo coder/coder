bc84caf6f8aa6292df1f44320f8a147e71d753ca
Presley <presley@coder.com>
2022-04-07 21:36:23 +0000

    Storybook and organize text stories

---
+++ site/src/components/Typography/Typography.stories.tsx
@@ -0,0 +1,28 @@
+import { Story } from "@storybook/react"
+import React from "react"
+import { Typography, TypographyProps } from "."
+
+export default {
+  title: "Text/Typography",
+  component: Typography,
+}
+
+const Template: Story<TypographyProps> = (args: TypographyProps) => (
+  <>
+    <Typography {...args}>
+      Colorless green ideas sleep furiously
+    </Typography>
+    <Typography {...args}>
+      More people have been to France than I have
+    </Typography>
+  </>
+)
+
+export const Short = Template.bind({})
+Short.args = {
+  short: true
+}
+export const Tall = Template.bind({})
+Tall.args = {
+  short: false
+}
