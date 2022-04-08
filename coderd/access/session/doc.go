efcee3dde5c9b71196ab5d9227a20beb05f85332
Dean Sheather <dean@deansheather.com>
2022-04-08 20:57:35 +0000

    fixup! chore: write RequireAuthentication middleware

--- coderd/access/session/doc.go
+++ coderd/access/session/doc.go
@@ -1,4 +1,7 @@
 // Package session provides session authentication via middleware for the Coder
 // HTTP API. It also exposes the Actor type, which is the intermediary layer
 // between identity and RBAC authorization.
+//
+// The Actor types exposed by this package are consumed by the authz packages to
+// determine if a request is authorized to perform an API action.
 package session
