300c6d08249cddcd0ff2411a03e434d7ab9fedf7
Bruno Quaresma <bruno@coder.com>
2022-04-13 15:34:06 -0300

    feat: add global notification component (#996)
    * feat: add global notification component
    
    * fix: update yarn.lock
    
    * fix: pin @testing-library/react-hooks
    
    * fix: update yarn.lock
    
    * refactor: remove displayError

---
+++ site/src/components/Snackbar/index.test.ts
@@ -0,0 +1,79 @@
+import { displaySuccess, isNotificationTextPrefixed, MsgType, NotificationMsg } from "./index"
+
+describe("Snackbar", () => {
+  describe("isNotificationTextPrefixed", () => {
+    // Regression test for case found in #10436
+    it("does not crash on null values", () => {
+      // Given
+      const msg = null
+
+      // When
+      const isTextPrefixed = isNotificationTextPrefixed(msg)
+
+      // Then
+      expect(isTextPrefixed).toBe(false)
+    })
+  })
+
+  describe("displaySuccess", () => {
+    const originalWindowDispatchEvent = window.dispatchEvent
+    let dispatchEventMock: jest.Mock
+
+    // Helper function to extract the notification event
+    // that was sent to `dispatchEvent`. This lets us validate
+    // the contents of the notification event are what we expect.
+    const extractNotificationEvent = (dispatchEventMock: jest.Mock): NotificationMsg => {
+      // The jest mock API isn't typesafe - but we know in our usage that
+      // this will always be a `NotificationMsg`.
+
+      // calls[0] is the first call made to the mock (this is reset in `beforeEach`)
+      // calls[0][0] is the first argument of the first call
+      // calls[0][0].detail is the 'detail' argument passed to the `CustomEvent` -
+      // this is the `NotificationMsg` object that gets sent to `dispatchEvent`
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      return dispatchEventMock.mock.calls[0][0].detail as NotificationMsg
+    }
+
+    beforeEach(() => {
+      dispatchEventMock = jest.fn()
+      window.dispatchEvent = dispatchEventMock
+    })
+
+    afterEach(() => {
+      window.dispatchEvent = originalWindowDispatchEvent
+    })
+
+    it("can be called with only a title", () => {
+      // Given
+      const expected: NotificationMsg = {
+        msgType: MsgType.Success,
+        msg: "Test",
+        additionalMsgs: undefined,
+      }
+
+      // When
+      displaySuccess("Test")
+
+      // Then
+      expect(dispatchEventMock).toBeCalledTimes(1)
+      expect(extractNotificationEvent(dispatchEventMock)).toStrictEqual(expected)
+    })
+
+    it("can be called with a title and additional message", () => {
+      // Given
+      const expected: NotificationMsg = {
+        msgType: MsgType.Success,
+        msg: "Test",
+        additionalMsgs: ["additional message"],
+      }
+
+      // When
+      displaySuccess("Test", "additional message")
+
+      // Then
+      expect(dispatchEventMock).toBeCalledTimes(1)
+      expect(extractNotificationEvent(dispatchEventMock)).toStrictEqual(expected)
+    })
+  })
+})
