8dbd90476e78af4247af8a918d212815fc6d975a
G r e y <grey@coder.com>
2022-04-13 12:58:31 -0400

    Apply suggestions from code review

--- site/src/test_helpers/handlers.ts
+++ site/src/test_helpers/handlers.ts
@@ -44,10 +44,10 @@ export const handlers = [
   rest.get("/api/v2/workspaces/:workspaceId", async (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(M.MockWorkspace))
   }),
-  rest.get("/api/v2/workspaces/:workspaceId/autostart", async (req, res, ctx) => {
+  rest.put("/api/v2/workspaces/:workspaceId/autostart", async (req, res, ctx) => {
     return res(ctx.status(200))
   }),
-  rest.get("/api/v2/workspaces/:workspaceId/autostop", async (req, res, ctx) => {
+  rest.put("/api/v2/workspaces/:workspaceId/autostop", async (req, res, ctx) => {
     return res(ctx.status(200))
   }),
 ]
