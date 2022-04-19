c35be02a7e568c1469c1469d7228ab88750e84cc
G r e y <grey@coder.com>
2022-04-19 12:48:50 -0400

    feat: web console logo (#1085)

--- site/src/Main.tsx
+++ site/src/Main.tsx
@@ -19,6 +19,12 @@ if (process.env.NODE_ENV === "development" && process.env.INSPECT_XSTATE === "tr
 // In the future, we'll likely bring in more bootstrapping logic -
 // like: https://github.com/coder/m/blob/50898bd4803df7639bd181e484c74ac5d84da474/product/coder/site/pages/_app.tsx#L32
 const main = () => {
+  console.info(`    ▄█▀    ▀█▄
+     ▄▄ ▀▀▀  █▌   ██▀▀█▄          ▐█
+ ▄▄██▀▀█▄▄▄  ██  ██      █▀▀█ ▐█▀▀██ ▄█▀▀█ █▀▀
+█▌   ▄▌   ▐█ █▌  ▀█▄▄▄█▌ █  █ ▐█  ██ ██▀▀  █
+     ██████▀▄█    ▀▀▀▀   ▀▀▀▀  ▀▀▀▀▀  ▀▀▀▀ ▀
+`)
   const element = document.getElementById("root")
   ReactDOM.render(<App />, element)
 }
