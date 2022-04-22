db013f864c125913b18f89dbc6bfbb1d5b6f3b64
Presley <presley@coder.com>
2022-04-22 16:26:04 +0000

    Add button and route

---
+++ site/src/pages/UsersPage/CreateUserPage/CreateUserPage.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const CreateUserPage = () => {
+  return <div>"Coming soon!"</div>
+}
--- site/src/pages/UsersPage/UsersPage.tsx
+++ site/src/pages/UsersPage/UsersPage.tsx
@@ -1,5 +1,6 @@
 import { useActor } from "@xstate/react"
 import React, { useContext } from "react"
+import { useNavigate } from "react-router"
 import { ErrorSummary } from "../../components/ErrorSummary/ErrorSummary"
 import { XServiceContext } from "../../xServices/StateContext"
 import { UsersPageView } from "./UsersPageView"
@@ -8,10 +9,11 @@ export const UsersPage: React.FC = () => {
   const xServices = useContext(XServiceContext)
   const [usersState] = useActor(xServices.usersXService)
   const { users, pager, getUsersError } = usersState.context
+  const navigate = useNavigate()
 
   if (usersState.matches("error")) {
     return <ErrorSummary error={getUsersError} />
   } else {
-    return <UsersPageView users={users} pager={pager} />
+    return <UsersPageView users={users} pager={pager} openUserCreationDialog={() => {navigate("/users/create")}} />
   }
 }
