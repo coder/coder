021e4cd957573e9387969d413eba9d79484f03c8
Bruno Quaresma <bruno@coder.com>
2022-04-29 13:59:14 -0500

    refactor: Load users from the API (#1218)

--- site/src/pages/UsersPage/UsersPage.test.tsx
+++ site/src/pages/UsersPage/UsersPage.test.tsx
@@ -1,15 +1,9 @@
 import { screen } from "@testing-library/react"
 import React from "react"
-import { MockPager, render } from "../../testHelpers"
+import { render } from "../../testHelpers"
 import { UsersPage } from "./UsersPage"
-import { Language } from "./UsersPageView"
 
 describe("Users Page", () => {
-  it("has a header with the total number of users", async () => {
-    render(<UsersPage />)
-    const total = await screen.findByText(/\d+ total/)
-    expect(total.innerHTML).toEqual(Language.pageSubtitle(MockPager))
-  })
   it("shows users", async () => {
     render(<UsersPage />)
     const users = await screen.findAllByText(/.*@coder.com/)
