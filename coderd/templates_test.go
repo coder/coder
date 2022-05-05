be8ba6c257283a060e414d8053f7df03f300ab51
Mathias Fredriksson <mathias@coder.com>
2022-05-05 12:26:15 +0000

    Reinit tt due to parallel test

--- coderd/templates_test.go
+++ coderd/templates_test.go
@@ -224,6 +224,7 @@ func TestPaginatedTemplateVersions(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			got, err := client.TemplateVersionsByTemplate(tt.args.ctx, codersdk.TemplateVersionsByTemplateRequest{
