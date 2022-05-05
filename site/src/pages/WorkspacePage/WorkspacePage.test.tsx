cfac4c2de0f5a8c535c17bc570798f802e78fc5f
Presley <presley@coder.com>
2022-05-05 04:32:59 +0000

    Fix tests

--- site/src/pages/WorkspacePage/WorkspacePage.test.tsx
+++ site/src/pages/WorkspacePage/WorkspacePage.test.tsx
@@ -1,14 +1,11 @@
 import React from "react"
 import { screen } from "@testing-library/react"
-import { MockTemplate, MockWorkspace, render, history } from "../../testHelpers"
+import { MockTemplate, MockWorkspace, render, history, renderWithAuth } from "../../testHelpers"
 import { WorkspacePage } from "./WorkspacePage"
 
 describe("Workspace Page", () => {
-  beforeEach(() => {
-    history.replace(`/workspace/${MockWorkspace.id}`)
-  })
-  it.only("shows a workspace", async () => {
-    render(<WorkspacePage />)
+  it("shows a workspace", async () => {
+    renderWithAuth(<WorkspacePage />, { route: `/workspaces/${MockWorkspace.id}`, path: "/workspaces/:workspace"})
     const workspaceName = await screen.findByText(MockWorkspace.name)
     const templateName = await screen.findByText(MockTemplate.name)
     expect(workspaceName).toBeDefined()
