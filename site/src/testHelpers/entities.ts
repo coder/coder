10bb649c42beaba054f3273ae4093b31f5c15a70
Asher <ash@coder.com>
2022-05-11 12:44:59 -0500

    Use generated types in test helper entities

--- site/src/testHelpers/entities.ts
+++ site/src/testHelpers/entities.ts
@@ -1,44 +1,37 @@
-import {
-  BuildInfoResponse,
-  Organization,
-  Provisioner,
-  Template,
-  UserAgent,
-  UserResponse,
-  Workspace,
-  WorkspaceAgent,
-  WorkspaceAutostartRequest,
-  WorkspaceResource,
-} from "../api/types"
-import { AuthMethods, Role } from "../api/typesGenerated"
+import * as Types from "../api/types"
+import * as TypesGen from "../api/typesGenerated"
 
-export const MockSessionToken = { session_token: "my-session-token" }
+export const MockSessionToken: TypesGen.LoginWithPasswordResponse = {
+  session_token: "my-session-token",
+}
 
-export const MockAPIKey = { key: "my-api-key" }
+export const MockAPIKey: TypesGen.GenerateAPIKeyResponse = {
+  key: "my-api-key",
+}
 
-export const MockBuildInfo: BuildInfoResponse = {
+export const MockBuildInfo: TypesGen.BuildInfoResponse = {
   external_url: "file:///mock-url",
   version: "v99.999.9999+c9cdf14",
 }
 
-export const MockAdminRole: Role = {
+export const MockAdminRole: TypesGen.Role = {
   name: "admin",
   display_name: "Admin",
 }
 
-export const MockMemberRole: Role = {
+export const MockMemberRole: TypesGen.Role = {
   name: "member",
   display_name: "Member",
 }
 
-export const MockAuditorRole: Role = {
+export const MockAuditorRole: TypesGen.Role = {
   name: "auditor",
   display_name: "Auditor",
 }
 
 export const MockSiteRoles = [MockAdminRole, MockAuditorRole, MockMemberRole]
 
-export const MockUser: UserResponse = {
+export const MockUser: TypesGen.User = {
   id: "test-user",
   username: "TestUser",
   email: "test@coder.com",
@@ -48,7 +41,7 @@ export const MockUser: UserResponse = {
   roles: [MockAdminRole, MockMemberRole],
 }
 
-export const MockUser2: UserResponse = {
+export const MockUser2: TypesGen.User = {
   id: "test-user-2",
   username: "TestUser2",
   email: "test2@coder.com",
@@ -58,19 +51,27 @@ export const MockUser2: UserResponse = {
   roles: [MockMemberRole],
 }
 
-export const MockOrganization: Organization = {
+export const MockOrganization: TypesGen.Organization = {
   id: "test-org",
   name: "Test Organization",
   created_at: "",
   updated_at: "",
 }
 
-export const MockProvisioner: Provisioner = {
+export const MockProvisioner: TypesGen.ProvisionerDaemon = {
+  created_at: "",
   id: "test-provisioner",
   name: "Test Provisioner",
+  provisioners: [],
 }
 
-export const MockTemplate: Template = {
+export const MockProvisionerJob: TypesGen.ProvisionerJob = {
+  created_at: "",
+  id: "test-provisioner-job",
+  status: "succeeded",
+}
+
+export const MockTemplate: TypesGen.Template = {
   id: "test-template",
   created_at: "",
   updated_at: "",
@@ -78,60 +79,86 @@ export const MockTemplate: Template = {
   name: "Test Template",
   provisioner: MockProvisioner.id,
   active_version_id: "",
+  workspace_owner_count: 1,
 }
 
-export const MockWorkspaceAutostartDisabled: WorkspaceAutostartRequest = {
+export const MockWorkspaceAutostartDisabled: TypesGen.UpdateWorkspaceAutostartRequest = {
   schedule: "",
 }
 
-export const MockWorkspaceAutostartEnabled: WorkspaceAutostartRequest = {
+export const MockWorkspaceAutostartEnabled: TypesGen.UpdateWorkspaceAutostartRequest = {
   // Runs at 9:30am Monday through Friday using Canada/Eastern
   // (America/Toronto) time
   schedule: "CRON_TZ=Canada/Eastern 30 9 * * 1-5",
 }
 
-export const MockWorkspaceAutostopDisabled: WorkspaceAutostartRequest = {
+export const MockWorkspaceAutostopDisabled: TypesGen.UpdateWorkspaceAutostartRequest = {
   schedule: "",
 }
 
-export const MockWorkspaceAutostopEnabled: WorkspaceAutostartRequest = {
+export const MockWorkspaceAutostopEnabled: TypesGen.UpdateWorkspaceAutostartRequest = {
   // Runs at 9:30pm Monday through Friday using America/Toronto
   schedule: "CRON_TZ=America/Toronto 30 21 * * 1-5",
 }
 
-export const MockWorkspace: Workspace = {
+export const MockWorkspaceBuild: TypesGen.WorkspaceBuild = {
+  after_id: "",
+  before_id: "",
+  created_at: "",
+  id: "test-workspace-build",
+  initiator_id: "",
+  job: MockProvisionerJob,
+  name: "a-workspace-build",
+  template_version_id: "",
+  transition: "start",
+  updated_at: "",
+  workspace_id: "test-workspace",
+}
+
+export const MockWorkspace: TypesGen.Workspace = {
   id: "test-workspace",
   name: "Test-Workspace",
   created_at: "",
   updated_at: "",
   template_id: MockTemplate.id,
+  template_name: MockTemplate.name,
+  outdated: false,
   owner_id: MockUser.id,
   autostart_schedule: MockWorkspaceAutostartEnabled.schedule,
   autostop_schedule: MockWorkspaceAutostopEnabled.schedule,
-  latest_build: {
-    id: "test-workspace-build",
-  },
+  latest_build: MockWorkspaceBuild,
 }
 
-export const MockWorkspaceAgent: WorkspaceAgent = {
+export const MockWorkspaceAgent: TypesGen.WorkspaceAgent = {
+  architecture: "amd64",
+  created_at: "",
+  environment_variables: {},
   id: "test-workspace-agent",
   name: "a-workspace-agent",
   operating_system: "linux",
+  resource_id: "",
+  status: "connected",
+  updated_at: "",
 }
 
-export const MockWorkspaceResource: WorkspaceResource = {
-  id: "test-workspace-resource",
+export const MockWorkspaceResource: TypesGen.WorkspaceResource = {
   agents: [MockWorkspaceAgent],
+  created_at: "",
+  id: "test-workspace-resource",
+  job_id: "",
+  name: "a-workspace-resource",
+  type: "google_compute_disk",
+  workspace_transition: "start",
 }
 
-export const MockUserAgent: UserAgent = {
+export const MockUserAgent: Types.UserAgent = {
   browser: "Chrome 99.0.4844",
   device: "Other",
   ip_address: "11.22.33.44",
   os: "Windows 10",
 }
 
-export const MockAuthMethods: AuthMethods = {
+export const MockAuthMethods: TypesGen.AuthMethods = {
   password: true,
   github: false,
 }
