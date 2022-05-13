f7aca885b06a94bec67f27917daf96fa86aed168
Colin Adler <colin@coder.com>
2022-05-13 17:05:58 -0500

    add test for deleting multiple templates

--- cli/templatedelete_test.go
+++ cli/templatedelete_test.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/coder/coder/cli/clitest"
 	"github.com/coder/coder/coderd/coderdtest"
+	"github.com/coder/coder/codersdk"
 	"github.com/coder/coder/pty/ptytest"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/require"
 )
 
@@ -28,9 +28,36 @@ func TestTemplateDelete(t *testing.T) {
 		clitest.SetupConfig(t, client, root)
 		require.NoError(t, cmd.Execute())
 
-		template, err := client.Template(context.Background(), template.ID)
-		spew.Dump(template, err)
+		_, err := client.Template(context.Background(), template.ID)
 		require.Error(t, err, "template should not exist")
+	})
+
+	t.Run("Multiple", func(t *testing.T) {
+		t.Parallel()
+
+		client := coderdtest.New(t, nil)
+		user := coderdtest.CreateFirstUser(t, client)
+		_ = coderdtest.NewProvisionerDaemon(t, client)
+		version := coderdtest.CreateTemplateVersion(t, client, user.OrganizationID, nil)
+		_ = coderdtest.AwaitTemplateVersionJob(t, client, version.ID)
+		templates := []codersdk.Template{
+			coderdtest.CreateTemplate(t, client, user.OrganizationID, version.ID),
+			coderdtest.CreateTemplate(t, client, user.OrganizationID, version.ID),
+			coderdtest.CreateTemplate(t, client, user.OrganizationID, version.ID),
+		}
+		templateNames := []string{}
+		for _, template := range templates {
+			templateNames = append(templateNames, template.Name)
+		}
+
+		cmd, root := clitest.New(t, append([]string{"templates", "delete"}, templateNames...)...)
+		clitest.SetupConfig(t, client, root)
+		require.NoError(t, cmd.Execute())
+
+		for _, template := range templates {
+			_, err := client.Template(context.Background(), template.ID)
+			require.Error(t, err, "template should not exist")
+		}
 	})
 
 	t.Run("Selector", func(t *testing.T) {
