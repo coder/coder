abde75b5906e2235b93ea4c405794308661659d7
kylecarbs <kyle@carberry.com>
2022-05-13 22:32:07 +0000

    fix: Remove CSS overrides for material-ui
    This hopefully removes some debt, and doesn't mess up
    styling too much!

--- site/src/theme/theme.ts
+++ site/src/theme/theme.ts
@@ -1,15 +1,10 @@
 import { createMuiTheme } from "@material-ui/core/styles"
-import { Overrides } from "@material-ui/core/styles/overrides"
 import { borderRadius } from "./constants"
-import { getOverrides } from "./overrides"
 import { CustomPalette, darkPalette, lightPalette } from "./palettes"
 import { props } from "./props"
 import { typography } from "./typography"
 
 const makeTheme = (palette: CustomPalette) => {
-  // Grab defaults to re-use in overrides
-  const { breakpoints } = createMuiTheme()
-
   return createMuiTheme({
     palette,
     typography,
@@ -17,7 +12,6 @@ const makeTheme = (palette: CustomPalette) => {
       borderRadius,
     },
     props,
-    overrides: getOverrides(palette, breakpoints) as Overrides,
   })
 }
 
