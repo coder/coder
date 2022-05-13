b3bdc36ae3ade14efd0d37715c99cd984357e12a
Bruno <bruno@coder.com>
2022-05-13 16:57:59 +0000

    Add workspaces table

--- site/src/pages/TemplatesPages/OrganizationPage/TemplatePage/TemplatePage.tsx
+++ site/src/pages/TemplatesPages/OrganizationPage/TemplatePage/TemplatePage.tsx
@@ -1,33 +1,17 @@
-import Box from "@material-ui/core/Box"
-import Button from "@material-ui/core/Button"
-import Table from "@material-ui/core/Table"
-import TableBody from "@material-ui/core/TableBody"
-import TableCell from "@material-ui/core/TableCell"
-import TableHead from "@material-ui/core/TableHead"
-import TableRow from "@material-ui/core/TableRow"
 import React from "react"
-import { Link, useNavigate, useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import useSWR from "swr"
 import * as TypesGen from "../../../../api/typesGenerated"
-import { EmptyState } from "../../../../components/EmptyState/EmptyState"
 import { ErrorSummary } from "../../../../components/ErrorSummary/ErrorSummary"
 import { Header } from "../../../../components/Header/Header"
 import { Margins } from "../../../../components/Margins/Margins"
 import { Stack } from "../../../../components/Stack/Stack"
-import { TableHeaderRow } from "../../../../components/TableHeaders/TableHeaders"
-import { TableLoader } from "../../../../components/TableLoader/TableLoader"
-import { TableTitle } from "../../../../components/TableTitle/TableTitle"
+import { WorkspacesTable } from "../../../../components/WorkspacesTable/WorkspacesTable"
 import { unsafeSWRArgument } from "../../../../util"
 import { firstOrItem } from "../../../../util/array"
 
 export const Language = {
-  tableTitle: "Workspaces",
-  nameLabel: "Name",
-  emptyMessage: "No workspaces have been created yet",
-  emptyDescription: "Create a workspace to get started",
-  totalLabel: "total",
-  ctaAction: "Create workspace",
-  subtitlePosfix: "workspaces",
+  subtitle: "workspaces",
 }
 
 export const TemplatePage: React.FC = () => {
@@ -44,13 +28,11 @@ export const TemplatePage: React.FC = () => {
 
   // This just grabs all workspaces... and then later filters them to match the
   // current template.
-
   const { data: workspaces, error: workspacesError } = useSWR<TypesGen.Workspace[], Error>(
     () => `/api/v2/organizations/${unsafeSWRArgument(organizationInfo).id}/workspaces`,
   )
 
   const hasError = organizationError || templateError || workspacesError
-  const isLoading = !templateInfo || !workspaces
 
   const createWorkspace = () => {
     navigate(`/templates/${organizationName}/${templateName}/create`)
@@ -68,7 +50,7 @@ export const TemplatePage: React.FC = () => {
       <Header
         title={firstOrItem(templateName, "")}
         description={firstOrItem(organizationName, "")}
-        subTitle={perTemplateWorkspaces ? `${perTemplateWorkspaces.length} ${Language.subtitlePosfix}` : ""}
+        subTitle={perTemplateWorkspaces ? `${perTemplateWorkspaces.length} ${Language.subtitle}` : ""}
         action={{
           text: "Create Workspace",
           onClick: createWorkspace,
@@ -80,43 +62,7 @@ export const TemplatePage: React.FC = () => {
         {templateError && <ErrorSummary error={templateError} />}
         {workspacesError && <ErrorSummary error={workspacesError} />}
         {!hasError && (
-          <Table>
-            <TableHead>
-              <TableTitle title={Language.tableTitle} />
-              <TableHeaderRow>
-                <TableCell size="small">{Language.nameLabel}</TableCell>
-              </TableHeaderRow>
-            </TableHead>
-            <TableBody>
-              {isLoading && <TableLoader />}
-              {workspaces &&
-                workspaces.map((w) => (
-                  <TableRow key={w.id}>
-                    <TableCell>
-                      <Link to={`/workspaces/${w.id}`}>{w.name}</Link>
-                    </TableCell>
-                  </TableRow>
-                ))}
-
-              {workspaces && workspaces.length === 0 && (
-                <TableRow>
-                  <TableCell colSpan={999}>
-                    <Box p={4}>
-                      <EmptyState
-                        message={Language.emptyMessage}
-                        description={Language.emptyDescription}
-                        cta={
-                          <Button variant="contained" color="primary" onClick={createWorkspace}>
-                            {Language.ctaAction}
-                          </Button>
-                        }
-                      />
-                    </Box>
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
+          <WorkspacesTable templateInfo={templateInfo} workspaces={workspaces} onCreateWorkspace={createWorkspace} />
         )}
       </Margins>
     </Stack>
