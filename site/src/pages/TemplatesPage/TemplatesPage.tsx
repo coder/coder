a72ba4072796b637ac8869641d8bb4c60f45ca6a
kylecarbs <kyle@carberry.com>
2022-05-16 22:48:09 +0000

    feat: Add template page

---
+++ site/src/pages/TemplatesPage/TemplatesPage.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { TemplatesPageView } from "./TemplatesPageView"
+
+const TemplatesPage: React.FC = () => {
+    return (
+        <TemplatesPageView />
+    )
+}
+
+export default TemplatesPage
---
+++ site/src/pages/TemplatesPage/TemplatesPageView.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const TemplatesPageView: React.FC = () => {
+  return <div>testing</div>
+}
