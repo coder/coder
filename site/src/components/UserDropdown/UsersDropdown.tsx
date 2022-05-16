d3a90220b8939bb21b6f93545a5fef5589b2149a
Bruno <bruno@coder.com>
2022-05-16 18:31:27 +0000

    Rename menu item to be settings

--- site/src/components/UserDropdown/UsersDropdown.tsx
+++ site/src/components/UserDropdown/UsersDropdown.tsx
@@ -16,7 +16,7 @@ import { UserAvatar } from "../UserAvatar/UserAvatar"
 import { UserProfileCard } from "../UserProfileCard/UserProfileCard"
 
 export const Language = {
-  accountLabel: "Account",
+  settingsLabel: "Settings",
   docsLabel: "Documentation",
   signOutLabel: "Sign Out",
 }
@@ -73,7 +73,7 @@ export const UserDropdown: React.FC<UserDropdownProps> = ({ user, onSignOut }: U
               <ListItemIcon className={styles.icon}>
                 <AccountIcon />
               </ListItemIcon>
-              <ListItemText primary={Language.accountLabel} />
+              <ListItemText primary={Language.settingsLabel} />
             </MenuItem>
           </Link>
 
