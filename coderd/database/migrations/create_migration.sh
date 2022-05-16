f007aeee1f8fa1d8d10342c2ad592f16d71bfe69
Colin Adler <colin@coder.com>
2022-05-16 12:35:00 -0500

    chore: standardize migration names in create_migration.sh (#1480)

--- coderd/database/migrations/create_migration.sh
+++ coderd/database/migrations/create_migration.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+# Usage:
+# ./create_migration name of migration
+# ./create_migration "name of migration"
+# ./create_migration name_of_migration
+
 set -euo pipefail
 
 cd "$(dirname "$0")"
 
-if [ -z "$1" ]; then
-    echo "First argument is the migration name!"
-    exit 1
-fi
+# if migration name is an empty string exit
+[[ -z "${*}" ]] && (echo "Must provide a migration name" && exit 1)
 
-migrate create -ext sql -dir . -seq "$1"
+# " " && "-" -> "_"
+title="$(echo "${@}" | tr "[:upper:]" "[:lower:]" | sed -E -e "s/( |-)/_/g")"
+
+migrate create -ext sql -dir . -seq "$title"
 
 echo "Run \"make gen\" to generate models."
