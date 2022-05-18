de0d26af6c5d75a92e00fdf5a04e82f103c29ad0
Steven Masley <stevenmasley@coder.com>
2022-05-18 16:01:01 -0500

    Merge rules files into 1

--- gorules/rules.go
+++ gorules/rules.go
@@ -20,3 +20,12 @@ func xerrors(m dsl.Matcher) {
 		Suggest("xerrors.New($msg)").
 		Report("Use xerrors to provide additional stacktrace information!")
 }
+
+// databaseImport enforces not importing any database types into /codersdk.
+//nolint:unused,deadcode,varnamelen
+func databaseImport(m dsl.Matcher) {
+	m.Import("github.com/coder/coder/coderd/database")
+	m.Match("database.$_").
+		Report("Do not import any database types into codersdk").
+		Where(m.File().PkgPath.Matches("github.com/coder/coder/codersdk"))
+}
