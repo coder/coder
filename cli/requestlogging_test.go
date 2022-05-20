bfa8e083a2f42d4a60edd2112a81b265e45c90bc
David Wahler <david@coder.com>
2022-05-20 00:19:05 +0000

    add more thorough tests

--- cli/requestlogging_test.go
+++ cli/requestlogging_test.go
@@ -1,12 +1,14 @@
 package cli_test
 
 import (
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/coder/coder/cli/clitest"
 	"github.com/coder/coder/coderd/coderdtest"
+	"github.com/coder/coder/codersdk"
 	"github.com/coder/coder/pty/ptytest"
 )
 
@@ -15,12 +17,7 @@ func TestRequestLogging(t *testing.T) {
 	t.Run("List", func(t *testing.T) {
 		t.Parallel()
 		client := coderdtest.New(t, &coderdtest.Options{IncludeProvisionerD: true})
-		user := coderdtest.CreateFirstUser(t, client)
-		version := coderdtest.CreateTemplateVersion(t, client, user.OrganizationID, nil)
-		coderdtest.AwaitTemplateVersionJob(t, client, version.ID)
-		template := coderdtest.CreateTemplate(t, client, user.OrganizationID, version.ID)
-		workspace := coderdtest.CreateWorkspace(t, client, user.OrganizationID, template.ID)
-		coderdtest.AwaitWorkspaceBuildJob(t, client, workspace.LatestBuild.ID)
+		_ = coderdtest.CreateFirstUser(t, client)
 		cmd, root := clitest.New(t, "ls", "--log-requests")
 		clitest.SetupConfig(t, client, root)
 		doneChan := make(chan struct{})
@@ -33,8 +30,44 @@ func TestRequestLogging(t *testing.T) {
 			require.NoError(t, err)
 		}()
 		pty.ExpectMatch("GET " + client.URL.String() + "/api/v2/workspaces 200")
-		pty.ExpectMatch(workspace.Name)
-		pty.ExpectMatch("Running")
+		<-doneChan
+	})
+
+	t.Run("NoAuthentication", func(t *testing.T) {
+		t.Parallel()
+		client := coderdtest.New(t, &coderdtest.Options{IncludeProvisionerD: true})
+		cmd, root := clitest.New(t, "ls", "--log-requests")
+		clitest.SetupConfig(t, client, root)
+		doneChan := make(chan struct{})
+		pty := ptytest.New(t)
+		cmd.SetIn(pty.Input())
+		cmd.SetOut(pty.Output())
+		go func() {
+			defer close(doneChan)
+			err := cmd.Execute()
+			require.Error(t, err)
+		}()
+		pty.ExpectMatch("GET " + client.URL.String() + "/api/v2/workspaces 401")
+		<-doneChan
+	})
+
+	t.Run("InvalidUrl", func(t *testing.T) {
+		t.Parallel()
+		parsedURL, err := url.Parse("invalidprotocol://foobar")
+		require.NoError(t, err)
+		client := codersdk.New(parsedURL)
+		cmd, root := clitest.New(t, "ls", "--log-requests")
+		clitest.SetupConfig(t, client, root)
+		doneChan := make(chan struct{})
+		pty := ptytest.New(t)
+		cmd.SetIn(pty.Input())
+		cmd.SetOut(pty.Output())
+		go func() {
+			defer close(doneChan)
+			err := cmd.Execute()
+			require.Error(t, err)
+		}()
+		pty.ExpectMatch("GET " + client.URL.String() + "/api/v2/workspaces (err)")
 		<-doneChan
 	})
 }
