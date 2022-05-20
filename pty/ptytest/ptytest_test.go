bf3a378139c7a43955a82f7cfa321e1d8f6f272a
Mathias Fredriksson <mafredri@gmail.com>
2022-05-20 19:11:39 +0300

    bug: Add test that breaks ptytest on Intel Mac

--- pty/ptytest/ptytest_test.go
+++ pty/ptytest/ptytest_test.go
@@ -1,7 +1,10 @@
 package ptytest_test
 
 import (
+	"os/exec"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/coder/coder/pty/ptytest"
 )
@@ -14,5 +17,14 @@ func TestPtytest(t *testing.T) {
 		pty.Output().Write([]byte("write"))
 		pty.ExpectMatch("write")
 		pty.WriteLine("read")
+	})
+	// nolint:paralleltest
+	t.Run("Do not hang on Intel macOS", func(t *testing.T) {
+		cmd := exec.Command("sh", "-c", "for i in $(seq 1 1000); do echo $i; done")
+		pty := ptytest.New(t)
+		cmd.Stdin = pty.Input()
+		cmd.Stdout = pty.Output()
+		err := cmd.Run()
+		require.NoError(t, err)
 	})
 }
