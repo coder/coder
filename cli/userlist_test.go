9e47df29375649033b732c3e8373d475f492db8c
Steven Masley <stevenmasley@coder.com>
2022-05-20 11:06:13 -0500

    Add get user unit test

--- cli/userlist_test.go
+++ cli/userlist_test.go
@@ -1,12 +1,14 @@
 package cli_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/coder/coder/cli/clitest"
 	"github.com/coder/coder/coderd/coderdtest"
+	"github.com/coder/coder/codersdk"
 	"github.com/coder/coder/pty/ptytest"
 )
 
@@ -26,5 +28,28 @@ func TestUserList(t *testing.T) {
 		require.NoError(t, err)
 	}()
 	pty.ExpectMatch("coder.com")
+	<-doneChan
+}
+
+func TestUserMe(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	client := coderdtest.New(t, nil)
+	admin := coderdtest.CreateFirstUser(t, client)
+	other := coderdtest.CreateAnotherUser(t, client, admin.OrganizationID)
+	otherUser, err := other.User(ctx, codersdk.Me)
+	require.NoError(t, err, "fetch other user")
+	cmd, root := clitest.New(t, "users", "get", otherUser.Username)
+	clitest.SetupConfig(t, client, root)
+	doneChan := make(chan struct{})
+	pty := ptytest.New(t)
+	cmd.SetIn(pty.Input())
+	cmd.SetOut(pty.Output())
+	go func() {
+		defer close(doneChan)
+		err := cmd.Execute()
+		require.NoError(t, err)
+	}()
+	pty.ExpectMatch(otherUser.Email)
 	<-doneChan
 }
