638acb6f24d3a5a3b3a237b72bac97725247e735
Steven Masley <stevenmasley@coder.com>
2022-05-20 11:30:21 -0500

    Add test using cobra cmd

--- pty/ptytest/ptytest_test.go
+++ pty/ptytest/ptytest_test.go
@@ -1,9 +1,11 @@
 package ptytest_test
 
 import (
+	"fmt"
 	"os/exec"
 	"testing"
 
+	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
 
 	"github.com/coder/coder/pty/ptytest"
@@ -18,6 +20,7 @@ func TestPtytest(t *testing.T) {
 		pty.ExpectMatch("write")
 		pty.WriteLine("read")
 	})
+
 	// nolint:paralleltest
 	t.Run("Do not hang on Intel macOS", func(t *testing.T) {
 		cmd := exec.Command("sh", "-c", "echo hi, I will cause a hang")
@@ -25,6 +28,25 @@ func TestPtytest(t *testing.T) {
 		cmd.Stdin = pty.Input()
 		cmd.Stdout = pty.Output()
 		err := cmd.Run()
+		require.NoError(t, err)
+	})
+
+	// nolint:paralleltest
+	t.Run("CobraCommandWorksLinux", func(t *testing.T) {
+		// Example with cobra command instead of exec. More abstractions, but
+		// for some reason works on linux.
+		cmd := cobra.Command{
+			Use: "test",
+			RunE: func(cmd *cobra.Command, args []string) error {
+				fmt.Println("Hello world")
+				return nil
+			},
+		}
+
+		pty := ptytest.New(t)
+		cmd.SetIn(pty.Input())
+		cmd.SetOut(pty.Output())
+		err := cmd.Execute()
 		require.NoError(t, err)
 	})
 }
