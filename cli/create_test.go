71e94e31f15451acece3ed3c0b300a93958b5c2d
Abhineet Jain <abhineet@coder.com>
2022-05-20 03:52:57 +0000

    Uncomment and fix unit test

--- cli/create_test.go
+++ cli/create_test.go
@@ -252,48 +252,48 @@ func TestCreate(t *testing.T) {
 		}
 		<-doneChan
 	})
-	// t.Run("WithParameterFileNotContainingTheValue", func(t *testing.T) {
-	// 	t.Parallel()
-	// 	client := coderdtest.New(t, nil)
-	// 	user := coderdtest.CreateFirstUser(t, client)
-	// 	coderdtest.NewProvisionerDaemon(t, client)
-	// 	version := coderdtest.CreateTemplateVersion(t, client, user.OrganizationID, &echo.Responses{
-	// 		Parse: []*proto.Parse_Response{{
-	// 			Type: &proto.Parse_Response_Complete{
-	// 				Complete: &proto.Parse_Complete{
-	// 					ParameterSchemas: []*proto.ParameterSchema{{
-	// 						AllowOverrideSource: true,
-	// 						Name:                "region",
-	// 						Description:         "description",
-	// 						DefaultSource: &proto.ParameterSource{
-	// 							Scheme: proto.ParameterSource_DATA,
-	// 							Value:  "something",
-	// 						},
-	// 						DefaultDestination: &proto.ParameterDestination{
-	// 							Scheme: proto.ParameterDestination_PROVISIONER_VARIABLE,
-	// 						},
-	// 					}},
-	// 				},
-	// 			},
-	// 		}},
-	// 		Provision:       echo.ProvisionComplete,
-	// 		ProvisionDryRun: echo.ProvisionComplete,
-	// 	})
-	// 	coderdtest.AwaitTemplateVersionJob(t, client, version.ID)
-	// 	template := coderdtest.CreateTemplate(t, client, user.OrganizationID, version.ID)
-	// 	parameterFile, _ := os.CreateTemp(t.TempDir(), "testParameterFile*.yaml")
-	// 	_, _ = parameterFile.WriteString("zone: \"bananas\"")
-	// 	cmd, root := clitest.New(t, "create", "my-workspace", "--template", template.Name, "--parameter-file", parameterFile.Name())
-	// 	clitest.SetupConfig(t, client, root)
-	// 	doneChan := make(chan struct{})
-	// 	pty := ptytest.New(t)
-	// 	cmd.SetIn(pty.Input())
-	// 	cmd.SetOut(pty.Output())
-	// 	go func() {
-	// 		defer close(doneChan)
-	// 		err := cmd.Execute()
-	// 		require.EqualError(t, err, "Parameter value absent in parameter file for \"region\"!")
-	// 	}()
-	// 	<-doneChan
-	// })
+	t.Run("WithParameterFileNotContainingTheValue", func(t *testing.T) {
+		t.Parallel()
+		client := coderdtest.New(t, &coderdtest.Options{IncludeProvisionerD: true})
+		user := coderdtest.CreateFirstUser(t, client)
+
+		version := coderdtest.CreateTemplateVersion(t, client, user.OrganizationID, &echo.Responses{
+			Parse: []*proto.Parse_Response{{
+				Type: &proto.Parse_Response_Complete{
+					Complete: &proto.Parse_Complete{
+						ParameterSchemas: []*proto.ParameterSchema{{
+							AllowOverrideSource: true,
+							Name:                "region",
+							Description:         "description",
+							DefaultSource: &proto.ParameterSource{
+								Scheme: proto.ParameterSource_DATA,
+								Value:  "something",
+							},
+							DefaultDestination: &proto.ParameterDestination{
+								Scheme: proto.ParameterDestination_PROVISIONER_VARIABLE,
+							},
+						}},
+					},
+				},
+			}},
+			Provision:       echo.ProvisionComplete,
+			ProvisionDryRun: echo.ProvisionComplete,
+		})
+		coderdtest.AwaitTemplateVersionJob(t, client, version.ID)
+		template := coderdtest.CreateTemplate(t, client, user.OrganizationID, version.ID)
+		parameterFile, _ := os.CreateTemp(t.TempDir(), "testParameterFile*.yaml")
+		_, _ = parameterFile.WriteString("zone: \"bananas\"")
+		cmd, root := clitest.New(t, "create", "my-workspace", "--template", template.Name, "--parameter-file", parameterFile.Name())
+		clitest.SetupConfig(t, client, root)
+		doneChan := make(chan struct{})
+		pty := ptytest.New(t)
+		cmd.SetIn(pty.Input())
+		cmd.SetOut(pty.Output())
+		go func() {
+			defer close(doneChan)
+			err := cmd.Execute()
+			require.EqualError(t, err, "Parameter value absent in parameter file for \"region\"!")
+		}()
+		<-doneChan
+	})
 }
