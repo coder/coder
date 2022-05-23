de6b5ed561659f0a194d8f28bad0437e4660acfe
Cian Johnston <cian@coder.com>
2022-05-23 16:15:46 +0100

    chore: clitest: fix TestCli

--- cli/clitest/clitest_test.go
+++ cli/clitest/clitest_test.go
@@ -1,11 +1,8 @@
 package clitest_test
 
 import (
-	"context"
 	"testing"
-	"time"
 
-	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 
 	"github.com/coder/coder/cli/clitest"
@@ -19,8 +16,6 @@ func TestMain(m *testing.M) {
 
 func TestCli(t *testing.T) {
 	t.Parallel()
-	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelFunc()
 	clitest.CreateTemplateVersionSource(t, nil)
 	client := coderdtest.New(t, nil)
 	cmd, config := clitest.New(t)
@@ -28,11 +23,8 @@ func TestCli(t *testing.T) {
 	pty := ptytest.New(t)
 	cmd.SetIn(pty.Input())
 	cmd.SetOut(pty.Output())
-	errC := make(chan error)
 	go func() {
-		errC <- cmd.ExecuteContext(ctx)
+		_ = cmd.Execute()
 	}()
 	pty.ExpectMatch("coder")
-	cancelFunc()
-	require.NoError(t, <-errC)
 }
