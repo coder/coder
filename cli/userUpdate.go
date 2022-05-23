61312d489a69f6f523ccf87732936a1e100632c6
Steven Masley <stevenmasley@coder.com>
2022-05-23 08:59:41 -0500

    Bring back user arg for regen-ssh

--- cli/userUpdate.go
+++ cli/userUpdate.go
@@ -3,20 +3,19 @@ package cli
 import (
 	"fmt"
 
-	"github.com/coder/coder/codersdk"
+	"github.com/spf13/cobra"
 
 	"github.com/coder/coder/cli/cliui"
-
-	"github.com/spf13/cobra"
 )
 
 func userResetGitSSH() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "regen-ssh",
-		Short: "Generates a new ssh key for the logged-in user. The old ssh key will be deleted.",
-		Long: "Generates a new ssh key for the logged-in user. The old ssh key will be deleted. " +
+		Use:   "regen-ssh <username|user_id|'me'>",
+		Short: "Generates a new ssh key for the user. The old ssh key will be deleted.",
+		Long: "Generates a new ssh key for the user. The old ssh key will be deleted. " +
 			"The command outputs public key of the new ssh key.",
-		Example: "coder users regen-ssh",
+		Args:    cobra.ExactArgs(1),
+		Example: "coder users regen-ssh me",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			_, err := cliui.Prompt(cmd, cliui.PromptOptions{
 				Text:      "Confirm regenerate a new sshkey for your workspaces?",
@@ -32,7 +31,7 @@ func userResetGitSSH() *cobra.Command {
 				return err
 			}
 
-			gitSSH, err := client.RegenerateGitSSHKey(ctx, codersdk.Me)
+			gitSSH, err := client.RegenerateGitSSHKey(ctx, args[0])
 			if err != nil {
 				return err
 			}
