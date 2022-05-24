23c950a9abc87752c38069dbccf43da991e68f39
Garrett Delfosse <garrett@coder.com>
2022-05-24 22:51:29 +0000

    try removing parallel

--- cli/dotfiles_test.go
+++ cli/dotfiles_test.go
@@ -11,43 +11,48 @@ import (
 	"github.com/coder/coder/cli/clitest"
 )
 
+// nolint:paralleltest
 func TestDotfiles(t *testing.T) {
 	t.Run("MissingArg", func(t *testing.T) {
-		t.Parallel()
 		cmd, _ := clitest.New(t, "dotfiles")
 		err := cmd.Execute()
 		assert.Error(t, err)
 	})
 	t.Run("NoInstallScript", func(t *testing.T) {
-		t.Parallel()
 		_, root := clitest.New(t)
 		testRepo := filepath.Join(string(root), "test-repo")
+
 		err := os.MkdirAll(testRepo, 0750)
 		assert.NoError(t, err)
+
 		c := exec.Command("git", "init")
 		c.Dir = testRepo
 		err = c.Run()
 		assert.NoError(t, err)
+
 		// nolint:gosec
 		err = os.WriteFile(filepath.Join(testRepo, ".bashrc"), []byte("wow"), 0750)
 		assert.NoError(t, err)
+
 		c = exec.Command("git", "add", ".bashrc")
 		c.Dir = testRepo
 		err = c.Run()
 		assert.NoError(t, err)
+
 		c = exec.Command("git", "commit", "-m", `"add .bashrc"`)
 		c.Dir = testRepo
 		err = c.Run()
 		assert.NoError(t, err)
+
 		cmd, _ := clitest.New(t, "dotfiles", "--global-config", string(root), "--home-dir", string(root), "-y", testRepo)
 		err = cmd.Execute()
 		assert.NoError(t, err)
+
 		b, err := os.ReadFile(filepath.Join(string(root), ".bashrc"))
 		assert.NoError(t, err)
 		assert.Equal(t, string(b), "wow")
 	})
 	t.Run("InstallScript", func(t *testing.T) {
-		t.Parallel()
 		_, root := clitest.New(t)
 		testRepo := filepath.Join(string(root), "test-repo")
 		err := os.MkdirAll(testRepo, 0750)
