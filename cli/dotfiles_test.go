2769e0ac66dc282869cf80c28d490a532cc2b62d
Garrett Delfosse <garrett@coder.com>
2022-05-24 22:36:02 +0000

    Add tests

--- cli/dotfiles_test.go
+++ cli/dotfiles_test.go
@@ -1,6 +1,9 @@
 package cli_test
 
 import (
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 
 	"github.com/coder/coder/cli/clitest"
@@ -13,5 +16,59 @@ func TestDotfiles(t *testing.T) {
 		cmd, _ := clitest.New(t, "dotfiles")
 		err := cmd.Execute()
 		assert.Error(t, err)
+	})
+	t.Run("NoInstallScript", func(t *testing.T) {
+		t.Parallel()
+		_, root := clitest.New(t)
+		testRepo := filepath.Join(string(root), "test-repo")
+		err := os.MkdirAll(testRepo, 0750)
+		assert.NoError(t, err)
+		c := exec.Command("git", "init")
+		c.Dir = testRepo
+		err = c.Run()
+		assert.NoError(t, err)
+		err = os.WriteFile(filepath.Join(testRepo, ".bashrc"), []byte("wow"), 0750)
+		assert.NoError(t, err)
+		c = exec.Command("git", "add", ".bashrc")
+		c.Dir = testRepo
+		err = c.Run()
+		assert.NoError(t, err)
+		c = exec.Command("git", "commit", "-m", `"add .bashrc"`)
+		c.Dir = testRepo
+		err = c.Run()
+		assert.NoError(t, err)
+		cmd, _ := clitest.New(t, "dotfiles", "--global-config", string(root), "--home-dir", string(root), "-y", testRepo)
+		err = cmd.Execute()
+		assert.NoError(t, err)
+		b, err := os.ReadFile(filepath.Join(string(root), ".bashrc"))
+		assert.NoError(t, err)
+		assert.Equal(t, string(b), "wow")
+	})
+	t.Run("InstallScript", func(t *testing.T) {
+		t.Parallel()
+		_, root := clitest.New(t)
+		testRepo := filepath.Join(string(root), "test-repo")
+		err := os.MkdirAll(testRepo, 0750)
+		assert.NoError(t, err)
+		c := exec.Command("git", "init")
+		c.Dir = testRepo
+		err = c.Run()
+		assert.NoError(t, err)
+		err = os.WriteFile(filepath.Join(testRepo, "install.sh"), []byte("#!/bin/bash\necho wow > "+filepath.Join(string(root), ".bashrc")), 0750)
+		assert.NoError(t, err)
+		c = exec.Command("git", "add", "install.sh")
+		c.Dir = testRepo
+		err = c.Run()
+		assert.NoError(t, err)
+		c = exec.Command("git", "commit", "-m", `"add install.sh"`)
+		c.Dir = testRepo
+		err = c.Run()
+		assert.NoError(t, err)
+		cmd, _ := clitest.New(t, "dotfiles", "--global-config", string(root), "--home-dir", string(root), "-y", testRepo)
+		err = cmd.Execute()
+		assert.NoError(t, err)
+		b, err := os.ReadFile(filepath.Join(string(root), ".bashrc"))
+		assert.NoError(t, err)
+		assert.Equal(t, string(b), "wow\n")
 	})
 }
