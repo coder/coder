cf08efb6f4760d7f91ad2656716a8e425d1e7d5e
Bruno <bruno@coder.com>
2022-05-24 18:51:52 +0000

    rfeat: Add resource type and restyle terminal link

--- site/src/components/TerminalLink/TerminalLink.tsx
+++ site/src/components/TerminalLink/TerminalLink.tsx
@@ -1,15 +1,17 @@
 import Link from "@material-ui/core/Link"
+import ComputerIcon from "@material-ui/icons/Computer"
 import React from "react"
 import * as TypesGen from "../../api/typesGenerated"
 
 export const Language = {
-  linkText: "Open in terminal",
+  linkText: "Open terminal",
 }
 
 export interface TerminalLinkProps {
   agentName?: TypesGen.WorkspaceAgent["name"]
   userName?: TypesGen.User["username"]
   workspaceName: TypesGen.Workspace["name"]
+  className?: string
 }
 
 /**
@@ -19,9 +21,14 @@ export interface TerminalLinkProps {
  * If no user name is provided "me" is used however it makes the link not
  * shareable.
  */
-export const TerminalLink: React.FC<TerminalLinkProps> = ({ agentName, userName = "me", workspaceName }) => {
+export const TerminalLink: React.FC<TerminalLinkProps> = ({ agentName, userName = "me", workspaceName, className }) => {
   return (
-    <Link href={`/${userName}/${workspaceName}${agentName ? `.${agentName}` : ""}/terminal`} target="_blank">
+    <Link
+      href={`/${userName}/${workspaceName}${agentName ? `.${agentName}` : ""}/terminal`}
+      className={className}
+      target="_blank"
+    >
+      <ComputerIcon />
       {Language.linkText}
     </Link>
   )
