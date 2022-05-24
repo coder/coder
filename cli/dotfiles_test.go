43c8b4ff6a2fd8a299c1370b60085980bb89497b
Garrett Delfosse <garrett@coder.com>
2022-05-24 22:58:10 +0000

    formatting

--- cli/dotfiles_test.go
+++ cli/dotfiles_test.go
@@ -57,24 +57,30 @@ func TestDotfiles(t *testing.T) {
 		testRepo := filepath.Join(string(root), "test-repo")
 		err := os.MkdirAll(testRepo, 0750)
 		assert.NoError(t, err)
+
 		c := exec.Command("git", "init")
 		c.Dir = testRepo
 		err = c.Run()
 		assert.NoError(t, err)
+
 		// nolint:gosec
 		err = os.WriteFile(filepath.Join(testRepo, "install.sh"), []byte("#!/bin/bash\necho wow > "+filepath.Join(string(root), ".bashrc")), 0750)
 		assert.NoError(t, err)
+
 		c = exec.Command("git", "add", "install.sh")
 		c.Dir = testRepo
 		err = c.Run()
 		assert.NoError(t, err)
+
 		c = exec.Command("git", "commit", "-m", `"add install.sh"`)
 		c.Dir = testRepo
 		err = c.Run()
 		assert.NoError(t, err)
+
 		cmd, _ := clitest.New(t, "dotfiles", "--global-config", string(root), "--home-dir", string(root), "-y", testRepo)
 		err = cmd.Execute()
 		assert.NoError(t, err)
+
 		b, err := os.ReadFile(filepath.Join(string(root), ".bashrc"))
 		assert.NoError(t, err)
 		assert.Equal(t, string(b), "wow\n")
