6dfc469e6c33cc3a8233a54d695eb11c233c0167
Garrett <garrett@coder.com>
2022-05-25 20:45:53 +0000

    switch to require

--- cli/dotfiles_test.go
+++ cli/dotfiles_test.go
@@ -7,7 +7,7 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/coder/coder/cli/clitest"
 	"github.com/coder/coder/cli/config"
@@ -19,7 +19,7 @@ func TestDotfiles(t *testing.T) {
 	t.Run("MissingArg", func(t *testing.T) {
 		cmd, _ := clitest.New(t, "dotfiles")
 		err := cmd.Execute()
-		assert.Error(t, err)
+		require.Error(t, err)
 	})
 	t.Run("NoInstallScript", func(t *testing.T) {
 		_, root := clitest.New(t)
@@ -27,25 +27,25 @@ func TestDotfiles(t *testing.T) {
 
 		// nolint:gosec
 		err := os.WriteFile(filepath.Join(testRepo, ".bashrc"), []byte("wow"), 0750)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		c := exec.Command("git", "add", ".bashrc")
 		c.Dir = testRepo
 		err = c.Run()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		c = exec.Command("git", "commit", "-m", `"add .bashrc"`)
 		c.Dir = testRepo
 		out, err := c.CombinedOutput()
-		assert.NoError(t, err, string(out))
+		require.NoError(t, err, string(out))
 
 		cmd, _ := clitest.New(t, "dotfiles", "--global-config", string(root), "--symlink-dir", string(root), "-y", testRepo)
 		err = cmd.Execute()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		b, err := os.ReadFile(filepath.Join(string(root), ".bashrc"))
-		assert.NoError(t, err)
-		assert.Equal(t, string(b), "wow")
+		require.NoError(t, err)
+		require.Equal(t, string(b), "wow")
 	})
 	t.Run("InstallScript", func(t *testing.T) {
 		_, root := clitest.New(t)
@@ -53,25 +53,25 @@ func TestDotfiles(t *testing.T) {
 
 		// nolint:gosec
 		err := os.WriteFile(filepath.Join(testRepo, "install.sh"), []byte("#!/bin/bash\necho wow > "+filepath.Join(string(root), ".bashrc")), 0750)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		c := exec.Command("git", "add", "install.sh")
 		c.Dir = testRepo
 		err = c.Run()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		c = exec.Command("git", "commit", "-m", `"add install.sh"`)
 		c.Dir = testRepo
 		err = c.Run()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		cmd, _ := clitest.New(t, "dotfiles", "--global-config", string(root), "--symlink-dir", string(root), "-y", testRepo)
 		err = cmd.Execute()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		b, err := os.ReadFile(filepath.Join(string(root), ".bashrc"))
-		assert.NoError(t, err)
-		assert.Equal(t, string(b), "wow\n")
+		require.NoError(t, err)
+		require.Equal(t, string(b), "wow\n")
 	})
 	t.Run("SymlinkBackup", func(t *testing.T) {
 		_, root := clitest.New(t)
@@ -79,59 +79,59 @@ func TestDotfiles(t *testing.T) {
 
 		// nolint:gosec
 		err := os.WriteFile(filepath.Join(testRepo, ".bashrc"), []byte("wow"), 0750)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		// add a conflicting file at destination
 		// nolint:gosec
 		err = os.WriteFile(filepath.Join(string(root), ".bashrc"), []byte("backup"), 0750)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		c := exec.Command("git", "add", ".bashrc")
 		c.Dir = testRepo
 		err = c.Run()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		c = exec.Command("git", "commit", "-m", `"add .bashrc"`)
 		c.Dir = testRepo
 		out, err := c.CombinedOutput()
-		assert.NoError(t, err, string(out))
+		require.NoError(t, err, string(out))
 
 		cmd, _ := clitest.New(t, "dotfiles", "--global-config", string(root), "--symlink-dir", string(root), "-y", testRepo)
 		err = cmd.Execute()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		b, err := os.ReadFile(filepath.Join(string(root), ".bashrc"))
-		assert.NoError(t, err)
-		assert.Equal(t, string(b), "wow")
+		require.NoError(t, err)
+		require.Equal(t, string(b), "wow")
 
 		// check for backup file
 		b, err = os.ReadFile(filepath.Join(string(root), ".bashrc.bak"))
-		assert.NoError(t, err)
-		assert.Equal(t, string(b), "backup")
+		require.NoError(t, err)
+		require.Equal(t, string(b), "backup")
 	})
 }
 
 func testGitRepo(t *testing.T, root config.Root) string {
 	r, err := cryptorand.String(8)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	dir := filepath.Join(string(root), fmt.Sprintf("test-repo-%s", r))
 	err = os.MkdirAll(dir, 0750)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	c := exec.Command("git", "init")
 	c.Dir = dir
 	err = c.Run()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	c = exec.Command("git", "config", "user.email", "ci@coder.com")
 	c.Dir = dir
 	err = c.Run()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	c = exec.Command("git", "config", "user.name", "C I")
 	c.Dir = dir
 	err = c.Run()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	return dir
 }
