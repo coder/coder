f3e53b4fde813b554f7db94620835fc0b61d82d7
Bruno <bruno@coder.com>
2022-05-25 13:21:49 +0000

    Add terminal link style

--- site/src/components/TerminalLink/TerminalLink.tsx
+++ site/src/components/TerminalLink/TerminalLink.tsx
@@ -1,7 +1,9 @@
 import Link from "@material-ui/core/Link"
+import { makeStyles } from "@material-ui/core/styles"
 import ComputerIcon from "@material-ui/icons/Computer"
 import React from "react"
 import * as TypesGen from "../../api/typesGenerated"
+import { combineClasses } from "../../util/combineClasses"
 
 export const Language = {
   linkText: "Open terminal",
@@ -22,14 +24,33 @@ export interface TerminalLinkProps {
  * shareable.
  */
 export const TerminalLink: React.FC<TerminalLinkProps> = ({ agentName, userName = "me", workspaceName, className }) => {
+  const styles = useStyles()
+
   return (
     <Link
       href={`/${userName}/${workspaceName}${agentName ? `.${agentName}` : ""}/terminal`}
-      className={className}
+      className={combineClasses([styles.link, className])}
       target="_blank"
     >
-      <ComputerIcon />
+      <ComputerIcon className={styles.icon} />
       {Language.linkText}
     </Link>
   )
 }
+
+// Replicating these from accessLink style from Resources component until we
+// define if we want these styles coming from the parent or having a
+// ResourceLink component for that
+const useStyles = makeStyles((theme) => ({
+  link: {
+    color: theme.palette.text.secondary,
+    display: "flex",
+    alignItems: "center",
+  },
+
+  icon: {
+    width: 16,
+    height: 16,
+    marginRight: theme.spacing(1.5),
+  },
+}))
