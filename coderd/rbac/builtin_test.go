9129ef64c6f8fda68b83614e07e0d604df45b4bd
Steven Masley <stevenmasley@coder.com>
2022-05-26 10:19:45 -0500

    Import order

--- coderd/rbac/builtin_test.go
+++ coderd/rbac/builtin_test.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/google/uuid"
-
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/coder/coder/coderd/rbac"
