6afbb7426772dbe3ba62c6b57756bcacc7e598f0
G r e y <grey@coder.com>
2022-05-26 05:47:10 +0000

    improve error message

--- site/src/components/WorkspaceStats/WorkspaceScheduleForm.tsx
+++ site/src/components/WorkspaceStats/WorkspaceScheduleForm.tsx
@@ -27,7 +27,7 @@ dayjs.extend(timezone)
 
 export const Language = {
   errorNoDayOfWeek: "Must set at least one day of week",
-  errorTime: "Invalid time",
+  errorTime: "Time must be in HH:mm format (24 hours)",
   errorTimezone: "Invalid timezone",
   daysOfWeekLabel: "Days of Week",
   daySundayLabel: "Sunday",
