3e3247d2e21eed7c4114b4b83cf151b096e12bce
Garrett <garrett@coder.com>
2022-05-26 02:07:35 +0000

    Add test for PATH

--- agent/agent.go
+++ agent/agent.go
@@ -363,7 +363,7 @@ func (a *agent) createCommand(ctx context.Context, rawCommand string, env []stri
 	// If using backslashes, it's unable to find the executable.
 	executablePath = strings.ReplaceAll(executablePath, "\\", "/")
 	cmd.Env = append(cmd.Env, fmt.Sprintf(`GIT_SSH_COMMAND=%s gitssh --`, executablePath))
-	cmd.Env = append(cmd.Env, fmt.Sprintf(`PATH=%s%s%s`, os.Getenv("PATH"), os.PathListSeparator, executablePath))
+	cmd.Env = append(cmd.Env, fmt.Sprintf(`PATH=%s%b%s`, os.Getenv("PATH"), os.PathListSeparator, executablePath))
 	// These prevent the user from having to specify _anything_ to successfully commit.
 	// Both author and committer must be set!
 	cmd.Env = append(cmd.Env, fmt.Sprintf(`GIT_AUTHOR_EMAIL=%s`, metadata.OwnerEmail))
