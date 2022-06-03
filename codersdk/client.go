6b08a08b30720014d773f9f007f3100b64469768
Steven Masley <stevenmasley@coder.com>
2022-06-03 09:36:47 -0500

    Add internal error to the cli

--- codersdk/client.go
+++ codersdk/client.go
@@ -202,6 +202,9 @@ func (e *Error) Error() string {
 	if e.Helper != "" {
 		_, _ = fmt.Fprintf(&builder, ": %s", e.Helper)
 	}
+	if e.Internal != "" {
+		_, _ = fmt.Fprintf(&builder, "\n\tError: %s", e.Internal)
+	}
 	for _, err := range e.Errors {
 		_, _ = fmt.Fprintf(&builder, "\n\t%s: %s", err.Field, err.Detail)
 	}
