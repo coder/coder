8b81c35c31a297231041b69d9265935e6490d1a3
kylecarbs <kyle@carberry.com>
2022-06-03 04:07:39 +0000

    chore: Refactor site to improve testing
    It was difficult to develop this package due to the
    embed build tag being mandatory on the tests. The logic
    to test doesn't require any embedded files.

---
+++ site/site_embed.go
@@ -0,0 +1,24 @@
+//go:build embed
+// +build embed
+
+package site
+
+import (
+	"embed"
+	"io/fs"
+)
+
+//go:embed out
+//go:embed out/bin/*
+var site embed.FS
+
+func FS() fs.FS {
+	// the out directory is where webpack builds are created. It is in the same
+	// directory as this file (package site).
+	out, err := fs.Sub(site, "out")
+	if err != nil {
+		// This can't happen... Go would throw a compilation error.
+		panic(err)
+	}
+	return out
+}
