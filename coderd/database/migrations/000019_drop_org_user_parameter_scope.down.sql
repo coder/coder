da754568779747df99c519d840e5b427e5ce4989
Spike Curtis <spike@coder.com>
2022-06-03 11:31:22 -0700

    Fix down migration
    Signed-off-by: Spike Curtis <spike@coder.com>

--- coderd/database/migrations/000019_drop_org_user_parameter_scope.down.sql
+++ coderd/database/migrations/000019_drop_org_user_parameter_scope.down.sql
@@ -1,2 +1,10 @@
-ALTER TYPE parameter_scope ADD VALUE 'organization';
-ALTER TYPE parameter_scope ADD VALUE 'user';
+CREATE TYPE old_parameter_scope AS ENUM (
+	'organization',
+	'template',
+	'import_job',
+	'user',
+	'workspace'
+	);
+ALTER TABLE parameter_values ALTER COLUMN scope TYPE old_parameter_scope USING (scope::text::old_parameter_scope);
+DROP TYPE parameter_scope;
+ALTER TYPE old_parameter_scope RENAME TO parameter_scope;
