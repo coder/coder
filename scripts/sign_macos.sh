be842a0c3c6ce7fdff8bb2ea5e7902ca40efa77e
deansheather <dean@deansheather.com>
2022-06-06 15:30:24 +0000

    chore: split release workflow so the majority happens on Linux

--- scripts/sign_macos.sh
+++ scripts/sign_macos.sh
@@ -5,19 +5,17 @@ set -euo pipefail
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 PROJECT_ROOT=$(cd "$SCRIPT_DIR" && git rev-parse --show-toplevel)
 
-(
-	cd "${PROJECT_ROOT}"
+cd "${PROJECT_ROOT}"
 
-	codesign -s "$AC_APPLICATION_IDENTITY" -f -v --timestamp --options runtime "$1"
+codesign -s "$AC_APPLICATION_IDENTITY" -f -v --timestamp --options runtime "$1"
 
-	config=$(mktemp -d)/gon.json
-	jq -r --null-input --arg path "$(pwd)/$1" '{
-		"notarize": [
-			{
-				"path": $path,
-				"bundle_id": "com.coder.cli"
-			}
-		]
-	}' >"$config"
-	gon "$config"
-)
+config=$(mktemp -d)/gon.json
+jq -r --null-input --arg path "$(pwd)/$1" '{
+	"notarize": [
+		{
+			"path": $path,
+			"bundle_id": "com.coder.cli"
+		}
+	]
+}' >"$config"
+gon "$config"
