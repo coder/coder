c4e15cd04cee500ab3b65fcdcfa6c339404f849d
Bruno <bruno@coder.com>
2022-06-06 17:29:57 +0000

    Add header in the logs page

--- site/src/pages/WorkspaceBuildPage/WorkspaceBuildPage.tsx
+++ site/src/pages/WorkspaceBuildPage/WorkspaceBuildPage.tsx
@@ -1,21 +1,10 @@
-import { makeStyles } from "@material-ui/core/styles"
-import Typography from "@material-ui/core/Typography"
 import { useMachine } from "@xstate/react"
 import { FC } from "react"
 import { Helmet } from "react-helmet"
 import { useParams } from "react-router-dom"
-import { ProvisionerJobLog } from "../../api/typesGenerated"
-import { Loader } from "../../components/Loader/Loader"
-import { Margins } from "../../components/Margins/Margins"
-import { Stack } from "../../components/Stack/Stack"
-import { WorkspaceBuildLogs } from "../../components/WorkspaceBuildLogs/WorkspaceBuildLogs"
-import { WorkspaceBuildStats } from "../../components/WorkspaceBuildStats/WorkspaceBuildStats"
 import { pageTitle } from "../../util/page"
 import { workspaceBuildMachine } from "../../xServices/workspaceBuild/workspaceBuildXService"
-
-const sortLogsByCreatedAt = (logs: ProvisionerJobLog[]) => {
-  return [...logs].sort((a, b) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime())
-}
+import { WorkspaceBuildPageView } from "./WorkspaceBuildPageView"
 
 const useBuildId = () => {
   const { buildId } = useParams()
@@ -32,29 +21,14 @@ export const WorkspaceBuildPage: FC = () => {
   const [buildState] = useMachine(workspaceBuildMachine, { context: { buildId } })
   const { logs, build } = buildState.context
   const isWaitingForLogs = !buildState.matches("logs.loaded")
-  const styles = useStyles()
 
   return (
-    <Margins>
+    <>
       <Helmet>
         <title>{build ? pageTitle(`Build #${build.build_number} · ${build.workspace_name}`) : ""}</title>
       </Helmet>
-      <Stack>
-        <Typography variant="h4" className={styles.title}>
-          Logs
-        </Typography>
 
-        {build && <WorkspaceBuildStats build={build} />}
-        {!logs && <Loader />}
-        {logs && <WorkspaceBuildLogs logs={sortLogsByCreatedAt(logs)} isWaitingForLogs={isWaitingForLogs} />}
-      </Stack>
-    </Margins>
+      <WorkspaceBuildPageView logs={logs} build={build} isWaitingForLogs={isWaitingForLogs} />
+    </>
   )
 }
-
-const useStyles = makeStyles((theme) => ({
-  title: {
-    paddingTop: theme.spacing(5),
-    paddingBottom: theme.spacing(2),
-  },
-}))
---
+++ site/src/pages/WorkspaceBuildPage/WorkspaceBuildPageView.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react"
+import { ProvisionerJobLog, WorkspaceBuild } from "../../api/typesGenerated"
+import { Loader } from "../../components/Loader/Loader"
+import { Margins } from "../../components/Margins/Margins"
+import { PageHeader, PageHeaderTitle } from "../../components/PageHeader/PageHeader"
+import { Stack } from "../../components/Stack/Stack"
+import { WorkspaceBuildLogs } from "../../components/WorkspaceBuildLogs/WorkspaceBuildLogs"
+import { WorkspaceBuildStats } from "../../components/WorkspaceBuildStats/WorkspaceBuildStats"
+
+const sortLogsByCreatedAt = (logs: ProvisionerJobLog[]) => {
+  return [...logs].sort((a, b) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime())
+}
+
+export interface WorkspaceBuildPageViewProps {
+  logs: ProvisionerJobLog[] | undefined
+  build: WorkspaceBuild | undefined
+  isWaitingForLogs: boolean
+}
+
+export const WorkspaceBuildPageView: FC<WorkspaceBuildPageViewProps> = ({ logs, build, isWaitingForLogs }) => {
+  return (
+    <Margins>
+      <PageHeader>
+        <PageHeaderTitle>Logs</PageHeaderTitle>
+      </PageHeader>
+
+      <Stack>
+        {build && <WorkspaceBuildStats build={build} />}
+        {!logs && <Loader />}
+        {logs && <WorkspaceBuildLogs logs={sortLogsByCreatedAt(logs)} isWaitingForLogs={isWaitingForLogs} />}
+      </Stack>
+    </Margins>
+  )
+}
