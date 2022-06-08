806fb03ecc7e4dd98881a1ecc5ffdf1ead82d3e1
kylecarbs <kyle@carberry.com>
2022-06-08 21:19:44 +0000

    fix: Improve Terraform agent<->resource association testing
    There were a few bugs with multiple agent association this fixes,
    and it adds thorough tests using real Terraform state and plans.

---
+++ provisioner/terraform/testdata/instance-id/with-instance-id.tf
@@ -0,0 +1,25 @@
+terraform {
+	required_providers {
+		coder = {
+			source = "coder/coder"
+			version = "0.4.2"
+		}
+	}
+}
+
+resource "coder_agent" "dev" {
+    os = "linux"
+    arch = "amd64"
+    auth = "google-instance-identity"
+}
+
+resource "null_resource" "dev" {
+    depends_on = [
+      coder_agent.dev
+    ]
+}
+
+resource "coder_agent_instance" "dev" {
+    agent_id = coder_agent.dev.id
+    instance_id = "example"
+}
