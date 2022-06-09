fff59ef6ad4e1d9026b228e8d2164004a5c7ff60
Kyle Carberry <kyle@coder.com>
2022-06-09 08:34:24 -0500

    fix: Add tests for instance and app association (#2198)
    This was regressed in #2187. There was bad testing around this
    before, and this should prevent a similiar situation from happening
    again!

---
+++ provisioner/terraform/testdata/multiple-apps/multiple-apps.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = "0.4.2"
+    }
+  }
+}
+
+resource "coder_agent" "dev1" {
+  os   = "linux"
+  arch = "amd64"
+}
+
+resource "coder_app" "app1" {
+  agent_id = coder_agent.dev1.id
+}
+
+resource "coder_app" "app2" {
+  agent_id = coder_agent.dev1.id
+}
+
+resource "null_resource" "dev" {
+  depends_on = [
+    coder_agent.dev1
+  ]
+}
