518495a6c5a30800c05761a0519347b127c2d8a6
Kira Pilot <kira@coder.com>
2022-06-09 11:43:49 -0400

    feat: show deleted workspace after delete action (#2208)
    * added deleted workspace banner
    
    * x state pass
    
    * added include_deleted param
    
    * clean up x state
    
    * added teests
    
    * cleaning up unneeded xstate service

---
+++ site/src/components/WorkspaceDeletedBanner/WorkspaceDeletedBanner.tsx
@@ -0,0 +1,50 @@
+import Button from "@material-ui/core/Button"
+import { makeStyles } from "@material-ui/core/styles"
+import Alert from "@material-ui/lab/Alert"
+import AlertTitle from "@material-ui/lab/AlertTitle"
+import { FC } from "react"
+import * as TypesGen from "../../api/typesGenerated"
+import { isWorkspaceDeleted } from "../../util/workspace"
+
+const Language = {
+  bannerTitle: "This workspace has been deleted and cannot be edited.",
+  createWorkspaceCta: "Create new workspace",
+}
+
+export interface WorkspaceDeletedBannerProps {
+  workspace: TypesGen.Workspace
+  handleClick: () => void
+}
+
+export const WorkspaceDeletedBanner: FC<WorkspaceDeletedBannerProps> = ({ workspace, handleClick }) => {
+  const styles = useStyles()
+
+  if (!isWorkspaceDeleted(workspace)) {
+    return null
+  }
+
+  return (
+    <Alert
+      className={styles.root}
+      action={
+        <Button color="inherit" onClick={handleClick} size="small">
+          {Language.createWorkspaceCta}
+        </Button>
+      }
+      severity="warning"
+    >
+      <AlertTitle>{Language.bannerTitle}</AlertTitle>
+    </Alert>
+  )
+}
+
+export const useStyles = makeStyles(() => {
+  return {
+    root: {
+      alignItems: "center",
+      "& .MuiAlertTitle-root": {
+        marginBottom: "0px",
+      },
+    },
+  }
+})
