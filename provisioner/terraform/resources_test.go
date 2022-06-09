coder/coder

commit 14701498c9d5faba51864cfc056f5af397a67c88

fix: Improve Terraform agent<->resource association testing (#2187)

provisioner/terraform/resources.go
@@ -1,0 +1,188 @@
+package terraform
+
+import (
+	"strings"
+
+	"github.com/awalterschulze/gographviz"
+	tfjson "github.com/hashicorp/terraform-json"
+	"github.com/mitchellh/mapstructure"
+	"golang.org/x/xerrors"
+
+	"github.com/coder/coder/provisionersdk/proto"
+)
+
+// ConvertResources consumes Terraform state and a GraphViz representation produced by
+// `terraform graph` to produce resources consumable by Coder.
+func ConvertResources(module *tfjson.StateModule, rawGraph string) ([]*proto.Resource, error) {
+	parsedGraph, err := gographviz.ParseString(rawGraph)
+	if err != nil {
+		return nil, xerrors.Errorf("parse graph: %w", err)
+	}
+	graph, err := gographviz.NewAnalysedGraph(parsedGraph)
+	if err != nil {
+		return nil, xerrors.Errorf("analyze graph: %w", err)
+	}
+	resourceDependencies := map[string][]string{}
+	for _, node := range graph.Nodes.Nodes {
+		label, exists := node.Attrs["label"]
+		if !exists {
+			continue
+		}
+		label = strings.Trim(label, `"`)
+		resourceDependencies[label] = findDependenciesWithLabels(graph, node.Name)
+	}
+
+	resources := make([]*proto.Resource, 0)
+	agents := map[string]*proto.Agent{}
+
+	tfResources := make([]*tfjson.StateResource, 0)
+	var appendResources func(mod *tfjson.StateModule)
+	appendResources = func(mod *tfjson.StateModule) {
+		for _, module := range mod.ChildModules {
+			appendResources(module)
+		}
+		tfResources = append(tfResources, mod.Resources...)
+	}
+	appendResources(module)
+
+	type agentAttributes struct {
+		Auth            string            `mapstructure:"auth"`
+		OperatingSystem string            `mapstructure:"os"`
+		Architecture    string            `mapstructure:"arch"`
+		Directory       string            `mapstructure:"dir"`
+		ID              string            `mapstructure:"id"`
+		Token           string            `mapstructure:"token"`
+		Env             map[string]string `mapstructure:"env"`
+		StartupScript   string            `mapstructure:"startup_script"`
+	}
+
+	// Store all agents inside the maps!
+	for _, resource := range tfResources {
+		if resource.Type != "coder_agent" {
+			continue
+		}
+		var attrs agentAttributes
+		err = mapstructure.Decode(resource.AttributeValues, &attrs)
+		if err != nil {
+			return nil, xerrors.Errorf("decode agent attributes: %w", err)
+		}
+		agent := &proto.Agent{
+			Name:            resource.Name,
+			Id:              attrs.ID,
+			Env:             attrs.Env,
+			StartupScript:   attrs.StartupScript,
+			OperatingSystem: attrs.OperatingSystem,
+			Architecture:    attrs.Architecture,
+			Directory:       attrs.Directory,
+		}
+		switch attrs.Auth {
+		case "token":
+			agent.Auth = &proto.Agent_Token{
+				Token: attrs.Token,
+			}
+		default:
+			agent.Auth = &proto.Agent_InstanceId{}
+		}
+
+		agents[convertAddressToLabel(resource.Address)] = agent
+	}
+
+	// Manually associate agents with instance IDs.
+	for _, resource := range tfResources {
+		if resource.Type != "coder_agent_instance" {
+			continue
+		}
+		agentIDRaw, valid := resource.AttributeValues["agent_id"]
+		if !valid {
+			continue
+		}
+		agentID, valid := agentIDRaw.(string)
+		if !valid {
+			continue
+		}
+		instanceIDRaw, valid := resource.AttributeValues["instance_id"]
+		if !valid {
+			continue
+		}
+		instanceID, valid := instanceIDRaw.(string)
+		if !valid {
+			continue
+		}
+
+		for _, agent := range agents {
+			if agent.Id != agentID {
+				continue
+			}
+			agent.Auth = &proto.Agent_InstanceId{
+				InstanceId: instanceID,
+			}
+			break
+		}
+	}
+
+	for _, resource := range tfResources {
+		if resource.Mode == tfjson.DataResourceMode {
+			continue
+		}
+		if resource.Type == "coder_agent" || resource.Type == "coder_agent_instance" || resource.Type == "coder_app" {
+			continue
+		}
+		resources = append(resources, &proto.Resource{
+			Name:   resource.Name,
+			Type:   resource.Type,
+			Agents: findAgents(resourceDependencies, agents, convertAddressToLabel(resource.Address)),
+		})
+	}
+
+	return resources, nil
+}
+
+// convertAddressToLabel returns the Terraform address without the count
+// specifier. eg. "module.ec2_dev.ec2_instance.dev[0]" becomes "module.ec2_dev.ec2_instance.dev"
+func convertAddressToLabel(address string) string {
+	return strings.Split(address, "[")[0]
+}
+
+// findAgents recursively searches through resource dependencies
+// to find associated agents. Nested is required for indirect
+// dependency matching.
+func findAgents(resourceDependencies map[string][]string, agents map[string]*proto.Agent, resourceLabel string) []*proto.Agent {
+	resourceNode, exists := resourceDependencies[resourceLabel]
+	if !exists {
+		return []*proto.Agent{}
+	}
+	// Associate resources that depend on an agent.
+	resourceAgents := make([]*proto.Agent, 0)
+	for _, dep := range resourceNode {
+		var has bool
+		agent, has := agents[dep]
+		if !has {
+			resourceAgents = append(resourceAgents, findAgents(resourceDependencies, agents, dep)...)
+			continue
+		}
+		// An agent must be deleted after being assigned so it isn't referenced twice.
+		delete(agents, dep)
+		resourceAgents = append(resourceAgents, agent)
+	}
+	return resourceAgents
+}
+
+// findDependenciesWithLabels recursively finds nodes with labels (resource and data nodes)
+// to build a dependency tree.
+func findDependenciesWithLabels(graph *gographviz.Graph, nodeName string) []string {
+	dependencies := make([]string, 0)
+	for destination := range graph.Edges.SrcToDsts[nodeName] {
+		dependencyNode, exists := graph.Nodes.Lookup[destination]
+		if !exists {
+			continue
+		}
+		label, exists := dependencyNode.Attrs["label"]
+		if !exists {
+			dependencies = append(dependencies, findDependenciesWithLabels(graph, dependencyNode.Name)...)
+			continue
+		}
+		label = strings.Trim(label, `"`)
+		dependencies = append(dependencies, label)
+	}
+	return dependencies
+}

provisioner/terraform/resources_test.go
@@ -1,0 +1,142 @@
+package terraform_test
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"runtime"
+	"sort"
+	"testing"
+
+	tfjson "github.com/hashicorp/terraform-json"
+	"github.com/stretchr/testify/require"
+
+	"github.com/coder/coder/provisioner/terraform"
+	"github.com/coder/coder/provisionersdk/proto"
+)
+
+func TestConvertResources(t *testing.T) {
+	t.Parallel()
+	// nolint:dogsled
+	_, filename, _, _ := runtime.Caller(0)
+	// nolint:paralleltest
+	for folderName, expected := range map[string][]*proto.Resource{
+		"chaining-resources": {{
+			Name: "first",
+			Type: "null_resource",
+			Agents: []*proto.Agent{{
+				Name:            "dev1",
+				OperatingSystem: "linux",
+				Architecture:    "amd64",
+				Auth:            &proto.Agent_Token{},
+			}},
+		}, {
+			Name: "second",
+			Type: "null_resource",
+		}},
+		"instance-id": {{
+			Name: "dev",
+			Type: "null_resource",
+			Agents: []*proto.Agent{{
+				Name:            "dev",
+				OperatingSystem: "linux",
+				Architecture:    "amd64",
+				Auth:            &proto.Agent_InstanceId{},
+			}},
+		}},
+		"calling-module": {{
+			Name: "example",
+			Type: "null_resource",
+			Agents: []*proto.Agent{{
+				Name:            "dev",
+				OperatingSystem: "linux",
+				Architecture:    "amd64",
+				Auth:            &proto.Agent_Token{},
+			}},
+		}},
+		"multiple-agents": {{
+			Name: "dev",
+			Type: "null_resource",
+			Agents: []*proto.Agent{{
+				Name:            "dev1",
+				OperatingSystem: "linux",
+				Architecture:    "amd64",
+				Auth:            &proto.Agent_Token{},
+			}, {
+				Name:            "dev2",
+				OperatingSystem: "darwin",
+				Architecture:    "amd64",
+				Auth:            &proto.Agent_Token{},
+			}, {
+				Name:            "dev3",
+				OperatingSystem: "windows",
+				Architecture:    "arm64",
+				Auth:            &proto.Agent_Token{},
+			}},
+		}},
+	} {
+		folderName := folderName
+		expected := expected
+		t.Run(folderName, func(t *testing.T) {
+			t.Parallel()
+			dir := filepath.Join(filepath.Dir(filename), "testdata", folderName)
+			t.Run("Plan", func(t *testing.T) {
+				t.Parallel()
+
+				tfPlanRaw, err := os.ReadFile(filepath.Join(dir, folderName+".tfplan.json"))
+				require.NoError(t, err)
+				var tfPlan tfjson.Plan
+				err = json.Unmarshal(tfPlanRaw, &tfPlan)
+				require.NoError(t, err)
+				tfPlanGraph, err := os.ReadFile(filepath.Join(dir, folderName+".tfplan.dot"))
+				require.NoError(t, err)
+
+				resources, err := terraform.ConvertResources(tfPlan.PlannedValues.RootModule, string(tfPlanGraph))
+				require.NoError(t, err)
+				for _, resource := range resources {
+					sort.Slice(resource.Agents, func(i, j int) bool {
+						return resource.Agents[i].Name < resource.Agents[j].Name
+					})
+				}
+				resourcesWant, err := json.Marshal(expected)
+				require.NoError(t, err)
+				resourcesGot, err := json.Marshal(resources)
+				require.NoError(t, err)
+				require.Equal(t, string(resourcesWant), string(resourcesGot))
+			})
+			t.Run("Provision", func(t *testing.T) {
+				t.Parallel()
+				tfStateRaw, err := os.ReadFile(filepath.Join(dir, folderName+".tfstate.json"))
+				require.NoError(t, err)
+				var tfState tfjson.State
+				err = json.Unmarshal(tfStateRaw, &tfState)
+				require.NoError(t, err)
+				tfStateGraph, err := os.ReadFile(filepath.Join(dir, folderName+".tfstate.dot"))
+				require.NoError(t, err)
+
+				resources, err := terraform.ConvertResources(tfState.Values.RootModule, string(tfStateGraph))
+				require.NoError(t, err)
+				for _, resource := range resources {
+					sort.Slice(resource.Agents, func(i, j int) bool {
+						return resource.Agents[i].Name < resource.Agents[j].Name
+					})
+					for _, agent := range resource.Agents {
+						agent.Id = ""
+						if agent.GetToken() != "" {
+							agent.Auth = &proto.Agent_Token{}
+						}
+						if agent.GetInstanceId() != "" {
+							agent.Auth = &proto.Agent_InstanceId{}
+						}
+					}
+				}
+				resourcesWant, err := json.Marshal(expected)
+				require.NoError(t, err)
+				resourcesGot, err := json.Marshal(resources)
+				require.NoError(t, err)
+
+				require.Equal(t, string(resourcesWant), string(resourcesGot))
+			})
+		})
+	}
+}

.gitattributes
@@ -3,3 +3,7 @@ coderd/database/dump.sql linguist-generated=true
 peerbroker/proto/*.go linguist-generated=true
 provisionerd/proto/*.go linguist-generated=true
 provisionersdk/proto/*.go linguist-generated=true
+*.tfplan.json linguist-generated=true
+*.tfstate.json linguist-generated=true
+*.tfstate.dot linguist-generated=true
+*.tfplan.dot linguist-generated=true

commit bc84d9f09d8a12dacae54e94477509bcf9702970

fix: Add tests for instance and app association
This was regressed in #2187. There was bad testing around this
before, and this should prevent a similiar situation from happening
again!

provisioner/terraform/resources.go
@@ -120,6 +120,42 @@ func ConvertResources(module *tfjson.StateModule, rawGraph string) ([]*proto.Res
 		}
 	}
 
+	type appAttributes struct {
+		AgentID      string `mapstructure:"agent_id"`
+		Name         string `mapstructure:"name"`
+		Icon         string `mapstructure:"icon"`
+		URL          string `mapstructure:"url"`
+		Command      string `mapstructure:"command"`
+		RelativePath bool   `mapstructure:"relative_path"`
+	}
+	// Associate Apps with agents.
+	for _, resource := range tfResources {
+		if resource.Type != "coder_app" {
+			continue
+		}
+		var attrs appAttributes
+		err = mapstructure.Decode(resource.AttributeValues, &attrs)
+		if err != nil {
+			return nil, xerrors.Errorf("decode app attributes: %w", err)
+		}
+		if attrs.Name == "" {
+			// Default to the resource name if none is set!
+			attrs.Name = resource.Name
+		}
+		for _, agent := range agents {
+			if agent.Id != attrs.AgentID {
+				continue
+			}
+			agent.Apps = append(agent.Apps, &proto.App{
+				Name:         attrs.Name,
+				Command:      attrs.Command,
+				Url:          attrs.URL,
+				Icon:         attrs.Icon,
+				RelativePath: attrs.RelativePath,
+			})
+		}
+	}
+
 	for _, resource := range tfResources {
 		if resource.Mode == tfjson.DataResourceMode {
 			continue
@@ -127,10 +163,43 @@ func ConvertResources(module *tfjson.StateModule, rawGraph string) ([]*proto.Res
 		if resource.Type == "coder_agent" || resource.Type == "coder_agent_instance" || resource.Type == "coder_app" {
 			continue
 		}
+		agents := findAgents(resourceDependencies, agents, convertAddressToLabel(resource.Address))
+		for _, agent := range agents {
+			// Didn't use instance identity.
+			if agent.GetToken() != "" {
+				continue
+			}
+
+			// These resource types are for automatically associating an instance ID
+			// with an agent for authentication.
+			key, isValid := map[string]string{
+				"google_compute_instance":         "instance_id",
+				"aws_instance":                    "id",
+				"azurerm_linux_virtual_machine":   "id",
+				"azurerm_windows_virtual_machine": "id",
+			}[resource.Type]
+			if !isValid {
+				// The resource type doesn't support
+				// automatically setting the instance ID.
+				continue
+			}
+			instanceIDRaw, valid := resource.AttributeValues[key]
+			if !valid {
+				continue
+			}
+			instanceID, valid := instanceIDRaw.(string)
+			if !valid {
+				continue
+			}
+			agent.Auth = &proto.Agent_InstanceId{
+				InstanceId: instanceID,
+			}
+		}
+
 		resources = append(resources, &proto.Resource{
 			Name:   resource.Name,
 			Type:   resource.Type,
-			Agents: findAgents(resourceDependencies, agents, convertAddressToLabel(resource.Address)),
+			Agents: agents,
 		})
 	}
 

provisioner/terraform/resources_test.go
@@ -11,6 +11,7 @@ import (
 	tfjson "github.com/hashicorp/terraform-json"
 	"github.com/stretchr/testify/require"
 
+	"github.com/coder/coder/cryptorand"
 	"github.com/coder/coder/provisioner/terraform"
 	"github.com/coder/coder/provisionersdk/proto"
 )
@@ -72,6 +73,21 @@ func TestConvertResources(t *testing.T) {
 				OperatingSystem: "windows",
 				Architecture:    "arm64",
 				Auth:            &proto.Agent_Token{},
+			}},
+		}},
+		"multiple-apps": {{
+			Name: "dev",
+			Type: "null_resource",
+			Agents: []*proto.Agent{{
+				Name:            "dev1",
+				OperatingSystem: "linux",
+				Architecture:    "amd64",
+				Apps: []*proto.App{{
+					Name: "app1",
+				}, {
+					Name: "app2",
+				}},
+				Auth: &proto.Agent_Token{},
 			}},
 		}},
 	} {
@@ -137,6 +153,72 @@ func TestConvertResources(t *testing.T) {
 
 				require.Equal(t, string(resourcesWant), string(resourcesGot))
 			})
+		})
+	}
+}
+
+func TestInstanceIDAssociation(t *testing.T) {
+	t.Parallel()
+	type tc struct {
+		Auth          string
+		ResourceType  string
+		InstanceIDKey string
+	}
+	for _, tc := range []tc{{
+		Auth:          "google-instance-identity",
+		ResourceType:  "google_compute_instance",
+		InstanceIDKey: "instance_id",
+	}, {
+		Auth:          "aws-instance-identity",
+		ResourceType:  "aws_instance",
+		InstanceIDKey: "id",
+	}, {
+		Auth:          "azure-instance-identity",
+		ResourceType:  "azurerm_linux_virtual_machine",
+		InstanceIDKey: "id",
+	}, {
+		Auth:          "azure-instance-identity",
+		ResourceType:  "azurerm_windows_virtual_machine",
+		InstanceIDKey: "id",
+	}} {
+		tc := tc
+		t.Run(tc.ResourceType, func(t *testing.T) {
+			t.Parallel()
+			instanceID, err := cryptorand.String(12)
+			require.NoError(t, err)
+			resources, err := terraform.ConvertResources(&tfjson.StateModule{
+				Resources: []*tfjson.StateResource{{
+					Address: "coder_agent.dev",
+					Type:    "coder_agent",
+					Name:    "dev",
+					AttributeValues: map[string]interface{}{
+						"arch": "amd64",
+						"auth": tc.Auth,
+					},
+				}, {
+					Address:   tc.ResourceType + ".dev",
+					Type:      tc.ResourceType,
+					Name:      "dev",
+					DependsOn: []string{"coder_agent.dev"},
+					AttributeValues: map[string]interface{}{
+						tc.InstanceIDKey: instanceID,
+					},
+				}},
+				// This is manually created to join the edges.
+			}, `digraph {
+	compound = "true"
+	newrank = "true"
+	subgraph "root" {
+		"[root] coder_agent.dev" [label = "coder_agent.dev", shape = "box"]
+		"[root] `+tc.ResourceType+`.dev" [label = "`+tc.ResourceType+`.dev", shape = "box"]
+		"[root] `+tc.ResourceType+`.dev" -> "[root] coder_agent.dev"
+	}
+}
+`)
+			require.NoError(t, err)
+			require.Len(t, resources, 1)
+			require.Len(t, resources[0].Agents, 1)
+			require.Equal(t, resources[0].Agents[0].GetInstanceId(), instanceID)
 		})
 	}
 }

provisioner/terraform/testdata/multiple-apps/multiple-apps.tf
@@ -1,0 +1,27 @@
+terraform {
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = "0.4.2"
+    }
+  }
+}
+
+resource "coder_agent" "dev1" {
+  os   = "linux"
+  arch = "amd64"
+}
+
+resource "coder_app" "app1" {
+  agent_id = coder_agent.dev1.id
+}
+
+resource "coder_app" "app2" {
+  agent_id = coder_agent.dev1.id
+}
+
+resource "null_resource" "dev" {
+  depends_on = [
+    coder_agent.dev1
+  ]
+}

provisioner/terraform/testdata/multiple-apps/multiple-apps.tfplan.dot
@@ -1,0 +1,23 @@
+digraph {
+	compound = "true"
+	newrank = "true"
+	subgraph "root" {
+		"[root] coder_agent.dev1 (expand)" [label = "coder_agent.dev1", shape = "box"]
+		"[root] coder_app.app1 (expand)" [label = "coder_app.app1", shape = "box"]
+		"[root] coder_app.app2 (expand)" [label = "coder_app.app2", shape = "box"]
+		"[root] null_resource.dev (expand)" [label = "null_resource.dev", shape = "box"]
+		"[root] provider[\"registry.terraform.io/coder/coder\"]" [label = "provider[\"registry.terraform.io/coder/coder\"]", shape = "diamond"]
+		"[root] provider[\"registry.terraform.io/hashicorp/null\"]" [label = "provider[\"registry.terraform.io/hashicorp/null\"]", shape = "diamond"]
+		"[root] coder_agent.dev1 (expand)" -> "[root] provider[\"registry.terraform.io/coder/coder\"]"
+		"[root] coder_app.app1 (expand)" -> "[root] coder_agent.dev1 (expand)"
+		"[root] coder_app.app2 (expand)" -> "[root] coder_agent.dev1 (expand)"
+		"[root] null_resource.dev (expand)" -> "[root] coder_agent.dev1 (expand)"
+		"[root] null_resource.dev (expand)" -> "[root] provider[\"registry.terraform.io/hashicorp/null\"]"
+		"[root] provider[\"registry.terraform.io/coder/coder\"] (close)" -> "[root] coder_app.app1 (expand)"
+		"[root] provider[\"registry.terraform.io/coder/coder\"] (close)" -> "[root] coder_app.app2 (expand)"
+		"[root] provider[\"registry.terraform.io/hashicorp/null\"] (close)" -> "[root] null_resource.dev (expand)"
+		"[root] root" -> "[root] provider[\"registry.terraform.io/coder/coder\"] (close)"
+		"[root] root" -> "[root] provider[\"registry.terraform.io/hashicorp/null\"] (close)"
+	}
+}
+

provisioner/terraform/testdata/multiple-apps/multiple-apps.tfstate.dot
@@ -1,0 +1,23 @@
+digraph {
+	compound = "true"
+	newrank = "true"
+	subgraph "root" {
+		"[root] coder_agent.dev1 (expand)" [label = "coder_agent.dev1", shape = "box"]
+		"[root] coder_app.app1 (expand)" [label = "coder_app.app1", shape = "box"]
+		"[root] coder_app.app2 (expand)" [label = "coder_app.app2", shape = "box"]
+		"[root] null_resource.dev (expand)" [label = "null_resource.dev", shape = "box"]
+		"[root] provider[\"registry.terraform.io/coder/coder\"]" [label = "provider[\"registry.terraform.io/coder/coder\"]", shape = "diamond"]
+		"[root] provider[\"registry.terraform.io/hashicorp/null\"]" [label = "provider[\"registry.terraform.io/hashicorp/null\"]", shape = "diamond"]
+		"[root] coder_agent.dev1 (expand)" -> "[root] provider[\"registry.terraform.io/coder/coder\"]"
+		"[root] coder_app.app1 (expand)" -> "[root] coder_agent.dev1 (expand)"
+		"[root] coder_app.app2 (expand)" -> "[root] coder_agent.dev1 (expand)"
+		"[root] null_resource.dev (expand)" -> "[root] coder_agent.dev1 (expand)"
+		"[root] null_resource.dev (expand)" -> "[root] provider[\"registry.terraform.io/hashicorp/null\"]"
+		"[root] provider[\"registry.terraform.io/coder/coder\"] (close)" -> "[root] coder_app.app1 (expand)"
+		"[root] provider[\"registry.terraform.io/coder/coder\"] (close)" -> "[root] coder_app.app2 (expand)"
+		"[root] provider[\"registry.terraform.io/hashicorp/null\"] (close)" -> "[root] null_resource.dev (expand)"
+		"[root] root" -> "[root] provider[\"registry.terraform.io/coder/coder\"] (close)"
+		"[root] root" -> "[root] provider[\"registry.terraform.io/hashicorp/null\"] (close)"
+	}
+}
+