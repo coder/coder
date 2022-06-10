8bfe1f7dfdc2e8697d11245c9e2dc60e4a8b23f5
Steven Masley <stevenmasley@coder.com>
2022-06-10 12:55:16 -0500

    fix test assert

--- coderd/httpapi/httpapi.go
+++ coderd/httpapi/httpapi.go
@@ -80,7 +80,7 @@ type Error struct {
 // to prevent leaking existence of resources.
 func ResourceNotFound(rw http.ResponseWriter) {
 	Write(rw, http.StatusNotFound, Response{
-		Message: fmt.Sprintf("Resource not found"),
+		Message: "Resource not found",
 	})
 }
 
