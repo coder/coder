fe20e253f0866c47ad208586fd2dc775d0139d6d
Kira Pilot <kira@coder.com>
2022-06-13 21:19:05 +0000

    feat: add tooltips to templates page
    resolves #2242

--- site/src/components/Tooltips/HelpTooltip/HelpTooltip.tsx
+++ site/src/components/Tooltips/HelpTooltip/HelpTooltip.tsx
@@ -4,7 +4,7 @@ import { makeStyles } from "@material-ui/core/styles"
 import HelpIcon from "@material-ui/icons/HelpOutline"
 import OpenInNewIcon from "@material-ui/icons/OpenInNew"
 import { useState } from "react"
-import { Stack } from "../Stack/Stack"
+import { Stack } from "../../Stack/Stack"
 
 type Size = "small" | "medium"
 export interface HelpTooltipProps {
