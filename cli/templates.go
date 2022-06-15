15b4dc85e4fdeaed2a5ba3cfd6aafb4039a83f2b
Steven Masley <stevenmasley@coder.com>
2022-06-15 11:20:00 -0500

    feat: Implement parameters list
    - Allow more columns on template list

--- cli/templates.go
+++ cli/templates.go
@@ -1,9 +1,14 @@
 package cli
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
 
 	"github.com/coder/coder/cli/cliui"
+	"github.com/coder/coder/codersdk"
 )
 
 func templates() *cobra.Command {
@@ -17,7 +22,7 @@ func templates() *cobra.Command {
     ` + cliui.Styles.Code.Render("$ coder templates create") + `
 
   - Make changes to your template, and plan the changes
- 
+
     ` + cliui.Styles.Code.Render("$ coder templates plan <name>") + `
 
   - Update the template. Your developers can update their workspaces
@@ -36,4 +41,37 @@ func templates() *cobra.Command {
 	)
 
 	return cmd
+}
+
+// displayTemplates will return a table displaying all templates passed in.
+// filterColumns must be a subset of the template fields and will determine which
+// columns to display
+func displayTemplates(filterColumns []string, templates ...codersdk.Template) string {
+	tableWriter := cliui.Table()
+	header := table.Row{
+		"Name", "Created At", "Last Updated", "Organization ID", "Provisioner",
+		"Active Version ID", "Used By", "Max TTL", "Min Autostart"}
+	tableWriter.AppendHeader(header)
+	tableWriter.SetColumnConfigs(cliui.FilterTableColumns(header, filterColumns))
+	tableWriter.SortBy([]table.SortBy{{
+		Name: "name",
+	}})
+	for _, template := range templates {
+		suffix := ""
+		if template.WorkspaceOwnerCount != 1 {
+			suffix = "s"
+		}
+		tableWriter.AppendRow(table.Row{
+			template.Name,
+			template.CreatedAt.Format("January 2, 2006"),
+			template.UpdatedAt.Format("January 2, 2006"),
+			template.OrganizationID.String(),
+			template.Provisioner,
+			template.ActiveVersionID.String(),
+			cliui.Styles.Fuschia.Render(fmt.Sprintf("%d developer%s", template.WorkspaceOwnerCount, suffix)),
+			fmt.Sprintf("%s", time.Duration(template.MaxTTLMillis)*time.Millisecond),
+			fmt.Sprintf("%s", time.Duration(template.MinAutostartIntervalMillis)*time.Millisecond),
+		})
+	}
+	return tableWriter.Render()
 }
