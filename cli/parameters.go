1f8c14c5f252f870578e90b5c66434631feca7ae
Steven Masley <stevenmasley@coder.com>
2022-06-15 11:31:30 -0500

    Add comment

--- cli/parameters.go
+++ cli/parameters.go
@@ -13,6 +13,11 @@ func parameters() *cobra.Command {
 		Short:   "List parameters for a given scope",
 		Example: "coder parameters list workspace my-workspace",
 		Use:     "parameters",
+		// Currently hidden as this shows parameter values, not parameter
+		// schemes. Until we have a good way to distinguish the two, it's better
+		// not to add confusion or lock ourselves into a certain api.
+		// This cmd is still valuable debugging tool for devs to avoid
+		// constructing curl requests.
 		Hidden:  true,
 		Aliases: []string{"params"},
 	}
