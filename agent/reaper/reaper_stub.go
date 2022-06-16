41e0165f3f6707a8f979412f8bf8c18d208f4b58
sreya <jon@coder.com>
2022-06-16 22:52:04 +0000

    make windows-specific stubs

---
+++ agent/reaper/reaper_stub.go
@@ -0,0 +1,19 @@
+//go:build windows
+
+package reaper
+
+import "github.com/hashicorp/go-reap"
+
+// IsChild returns true if we're the forked process.
+func IsChild() bool {
+	return false
+}
+
+// IsInitProcess returns true if the current process's PID is 1.
+func IsInitProcess() bool {
+	return false
+}
+
+func ForkReap(pids reap.PidCh) error {
+	return nil
+}
--- agent/reaper/reaper_test.go
+++ agent/reaper/reaper_test.go
@@ -1,8 +1,9 @@
+//go:build !windows
+
 package reaper_test
 
 import (
 	"os/exec"
-	"runtime"
 	"testing"
 	"time"
 
@@ -14,10 +15,6 @@ import (
 
 func TestReap(t *testing.T) {
 	t.Parallel()
-
-	if runtime.GOOS != "linux" {
-		t.Skipf("Skipping non-Linux OS %q", runtime.GOOS)
-	}
 
 	// Because we're forkexecing these tests will try to run twice...
 	if reaper.IsChild() {
