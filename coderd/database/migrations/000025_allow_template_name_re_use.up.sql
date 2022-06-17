1fa648909e55a98ce547bab26a6e60f04b19d927
Mathias Fredriksson <mafredri@gmail.com>
2022-06-17 20:43:53 +0300

    fix: Consolidate idx_templates_name_lower and templates_organization_id_name_idx

--- coderd/database/migrations/000025_allow_template_name_re_use.up.sql
+++ coderd/database/migrations/000025_allow_template_name_re_use.up.sql
@@ -1,4 +1,5 @@
 DROP INDEX idx_templates_name_lower;
-CREATE UNIQUE INDEX idx_templates_name_lower ON templates USING btree (lower(name)) WHERE deleted = false;
+DROP INDEX templates_organization_id_name_idx;
+CREATE UNIQUE INDEX templates_organization_id_name_idx ON templates (organization_id, lower(name)) WHERE deleted = false;
 
 ALTER TABLE ONLY templates DROP CONSTRAINT templates_organization_id_name_key;
