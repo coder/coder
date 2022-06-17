17ba4c8e88f06206a41b6a616d5702a1e2479313
Mathias Fredriksson <mafredri@gmail.com>
2022-06-17 22:18:07 +0300

    fix: Allow template names to be re-used after deletion (#2454)
    Fixes #2152

--- coderd/database/dump.sql
+++ coderd/database/dump.sql
@@ -400,9 +400,6 @@ ALTER TABLE ONLY template_versions
     ADD CONSTRAINT template_versions_template_id_name_key UNIQUE (template_id, name);
 
 ALTER TABLE ONLY templates
-    ADD CONSTRAINT templates_organization_id_name_key UNIQUE (organization_id, name);
-
-ALTER TABLE ONLY templates
     ADD CONSTRAINT templates_pkey PRIMARY KEY (id);
 
 ALTER TABLE ONLY users
@@ -453,13 +450,11 @@ CREATE UNIQUE INDEX idx_organization_name ON organizations USING btree (name);
 
 CREATE UNIQUE INDEX idx_organization_name_lower ON organizations USING btree (lower(name));
 
-CREATE UNIQUE INDEX idx_templates_name_lower ON templates USING btree (lower((name)::text));
-
 CREATE UNIQUE INDEX idx_users_email ON users USING btree (email);
 
 CREATE UNIQUE INDEX idx_users_username ON users USING btree (username);
 
-CREATE UNIQUE INDEX templates_organization_id_name_idx ON templates USING btree (organization_id, name) WHERE (deleted = false);
+CREATE UNIQUE INDEX templates_organization_id_name_idx ON templates USING btree (organization_id, lower((name)::text)) WHERE (deleted = false);
 
 CREATE UNIQUE INDEX users_username_lower_idx ON users USING btree (lower(username));
 
