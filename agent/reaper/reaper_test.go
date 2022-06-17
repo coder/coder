18973a65c10e5869b8baa1eb49697784076d16a5
Jon Ayers <jon@coder.com>
2022-06-17 11:51:46 -0500

    fix: Add reaper to coder agent (#2441)
    * fix: Add reaper to coder agent
    
    - The coder agent runs as PID 1 in some of our Docker workspaces.
      In such cases it is the responsibility of the init process to
      reap dead processes. Failing to do so can result in an inability
      to create new processes by running out of PIDs.
    
      This PR adds a reaper to our agent that is only spawned if it
      detects that it is PID1.

---
+++ agent/reaper/reaper_test.go
@@ -0,0 +1,66 @@
+//go:build linux
+
+package reaper_test
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/go-reap"
+	"github.com/stretchr/testify/require"
+
+	"github.com/coder/coder/agent/reaper"
+)
+
+func TestReap(t *testing.T) {
+	t.Parallel()
+
+	// Don't run the reaper test in CI. It does weird
+	// things like forkexecing which may have unintended
+	// consequences in CI.
+	if _, ok := os.LookupEnv("CI"); ok {
+		t.Skip("Detected CI, skipping reaper tests")
+	}
+
+	// Because we're forkexecing these tests will try to run twice...
+	if reaper.IsChild() {
+		t.Skip("I'm a child!")
+	}
+
+	// OK checks that's the reaper is successfully reaping
+	// exited processes and passing the PIDs through the shared
+	// channel.
+	t.Run("OK", func(t *testing.T) {
+		pids := make(reap.PidCh, 1)
+		err := reaper.ForkReap(pids)
+		require.NoError(t, err)
+
+		cmd := exec.Command("tail", "-f", "/dev/null")
+		err = cmd.Start()
+		require.NoError(t, err)
+
+		cmd2 := exec.Command("tail", "-f", "/dev/null")
+		err = cmd2.Start()
+		require.NoError(t, err)
+
+		err = cmd.Process.Kill()
+		require.NoError(t, err)
+
+		err = cmd2.Process.Kill()
+		require.NoError(t, err)
+
+		expectedPIDs := []int{cmd.Process.Pid, cmd2.Process.Pid}
+
+		deadline := time.NewTimer(time.Second * 5)
+		for i := 0; i < len(expectedPIDs); i++ {
+			select {
+			case <-deadline.C:
+				t.Fatalf("Timed out waiting for process")
+			case pid := <-pids:
+				require.Contains(t, expectedPIDs, pid)
+			}
+		}
+	})
+}
