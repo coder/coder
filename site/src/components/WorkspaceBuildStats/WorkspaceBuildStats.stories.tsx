8a853a64a59f95d36bde586f9bfc1741aeb50684
Abhineet Jain <AbhineetJain@users.noreply.github.com>
2022-06-20 10:38:57 -0400

    Show build initiator on Workspace Build page (#2446)
    * show build initiator in ui
    
    * update autostop story

--- site/src/components/WorkspaceBuildStats/WorkspaceBuildStats.stories.tsx
+++ site/src/components/WorkspaceBuildStats/WorkspaceBuildStats.stories.tsx
@@ -13,3 +13,20 @@ export const Example = Template.bind({})
 Example.args = {
   build: MockWorkspaceBuild,
 }
+
+export const Autostart = Template.bind({})
+Autostart.args = {
+  build: {
+    ...MockWorkspaceBuild,
+    reason: "autostart",
+  },
+}
+
+export const Autostop = Template.bind({})
+Autostop.args = {
+  build: {
+    ...MockWorkspaceBuild,
+    transition: "stop",
+    reason: "autostop",
+  },
+}
