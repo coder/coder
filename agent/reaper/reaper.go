fa115a36c4534bee55d90b690aa46fdf507904e5
Jon Ayers <jon@coder.com>
2022-06-21 17:35:35 -0500

    Update agent/reaper/reaper.go
    Co-authored-by: Dean Sheather <dean@deansheather.com>

--- agent/reaper/reaper.go
+++ agent/reaper/reaper.go
@@ -5,7 +5,7 @@ import "github.com/hashicorp/go-reap"
 type Option func(o *options)
 
 // WithExecArgs specifies the exec arguments for the fork exec call.
-// By default the same arguments as the parent are used as dictates by
+// By default the same arguments as the parent are used as dictated by
 // os.Args. Since ForkReap calls a fork-exec it is the responsibility of
 // the caller to avoid fork-bombing oneself.
 func WithExecArgs(args ...string) Option {
