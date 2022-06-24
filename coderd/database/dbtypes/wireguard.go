bcb95d46f47bd7b854d42472d0847c2c52f74d07
Colin Adler <colin1adler@gmail.com>
2022-06-24 12:33:44 -0500

    fixup! Merge branch 'main' into colin/wg-db-typed

--- coderd/database/dbtypes/wireguard.go
+++ coderd/database/dbtypes/wireguard.go
@@ -9,6 +9,18 @@ import (
 
 type NodePublic key.NodePublic
 
+func (n NodePublic) String() string {
+	return key.NodePublic(n).String()
+}
+
+func (n NodePublic) MarshalJSON() ([]byte, error) {
+	j, err := key.NodePublic(n).MarshalText()
+	// surround in quotes to make it a JSON string
+	j = append([]byte{'"'}, j...)
+	j = append(j, '"')
+	return j, err
+}
+
 func (n NodePublic) Value() (driver.Value, error) {
 	return key.NodePublic(n).MarshalText()
 }
@@ -25,6 +37,18 @@ func (n *NodePublic) Scan(value interface{}) error {
 }
 
 type DiscoPublic key.DiscoPublic
+
+func (n DiscoPublic) String() string {
+	return key.DiscoPublic(n).String()
+}
+
+func (n DiscoPublic) MarshalJSON() ([]byte, error) {
+	j, err := key.DiscoPublic(n).MarshalText()
+	// surround in quotes to make it a JSON string
+	j = append([]byte{'"'}, j...)
+	j = append(j, '"')
+	return j, err
+}
 
 func (n DiscoPublic) Value() (driver.Value, error) {
 	return key.DiscoPublic(n).MarshalText()
