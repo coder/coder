603553af52e2c4b102923ac2493bf7d2473adffa
Colin Adler <colin1adler@gmail.com>
2022-06-24 14:20:20 -0500

    comments

--- coderd/database/dbtypes/dbtypes.go
+++ coderd/database/dbtypes/dbtypes.go
@@ -7,24 +7,28 @@ import (
 	"tailscale.com/types/key"
 )
 
+// NodePublic is a wrapper around a key.NodePublic which represents the
+// Wireguard public key for an agent..
 type NodePublic key.NodePublic
 
 func (n NodePublic) String() string {
 	return key.NodePublic(n).String()
 }
 
+// This is necessary so NodePublic can be serialized in JSON loggers.
 func (n NodePublic) MarshalJSON() ([]byte, error) {
 	j, err := key.NodePublic(n).MarshalText()
 	// surround in quotes to make it a JSON string
-	j = append([]byte{'"'}, j...)
-	j = append(j, '"')
+	j = append([]byte{'"'}, append(j, '"')...)
 	return j, err
 }
 
+// Value is so NodePublic can be inserted into the database.
 func (n NodePublic) Value() (driver.Value, error) {
 	return key.NodePublic(n).MarshalText()
 }
 
+// Scan is so NodePublic can be read from the database.
 func (n *NodePublic) Scan(value interface{}) error {
 	switch v := value.(type) {
 	case []byte:
@@ -36,24 +40,28 @@ func (n *NodePublic) Scan(value interface{}) error {
 	}
 }
 
+// NodePublic is a wrapper around a key.NodePublic which represents the
+// Tailscale disco key for an agent.
 type DiscoPublic key.DiscoPublic
 
 func (n DiscoPublic) String() string {
 	return key.DiscoPublic(n).String()
 }
 
+// This is necessary so DiscoPublic can be serialized in JSON loggers.
 func (n DiscoPublic) MarshalJSON() ([]byte, error) {
 	j, err := key.DiscoPublic(n).MarshalText()
 	// surround in quotes to make it a JSON string
-	j = append([]byte{'"'}, j...)
-	j = append(j, '"')
+	j = append([]byte{'"'}, append(j, '"')...)
 	return j, err
 }
 
+// Value is so DiscoPublic can be inserted into the database.
 func (n DiscoPublic) Value() (driver.Value, error) {
 	return key.DiscoPublic(n).MarshalText()
 }
 
+// Scan is so DiscoPublic can be read from the database.
 func (n *DiscoPublic) Scan(value interface{}) error {
 	switch v := value.(type) {
 	case []byte:
