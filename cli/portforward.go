eb94a30cecd0a233a307ed6a7b3a43740967c0a8
Ketan Gangatirkar <33678+ketang@users.noreply.github.com>
2022-07-03 19:48:43 +0000

    add CLI help for port-forward command (#2802)

--- cli/portforward.go
+++ cli/portforward.go
@@ -29,6 +29,7 @@ func portForward() *cobra.Command {
 	)
 	cmd := &cobra.Command{
 		Use:     "port-forward <workspace>",
+		Short:   "Forward one or more ports from the local machine to the remote workspace",
 		Aliases: []string{"tunnel"},
 		Args:    cobra.ExactArgs(1),
 		Example: `
