33673153f84f2255a5d9c32d1011cbba78b30a51
ketang <33678+ketang@users.noreply.github.com>
2022-07-10 14:49:19 -0500

    switch to using provisionersdk.Untar, which eliminates need for test

--- cli/templatecheckout.go
+++ cli/templatecheckout.go
@@ -1,53 +1,14 @@
 package cli
 
 import (
-	"archive/tar"
-	"bytes"
-	"fmt"
-	"io"
 	"os"
-	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"golang.org/x/xerrors"
 
 	"github.com/coder/coder/cli/cliui"
+	"github.com/coder/coder/provisionersdk"
 )
-
-func TarBytesToTree(destination string, raw []byte) error {
-	err := os.Mkdir(destination, 0700)
-
-	archiveReader := tar.NewReader(bytes.NewReader(raw))
-	hdr, err := archiveReader.Next()
-	for err != io.EOF {
-		if hdr == nil { //	some blog post indicated this could happen sometimes
-			continue
-		}
-		filename := filepath.FromSlash(fmt.Sprintf("%s/%s", destination, hdr.Name))
-		switch hdr.Typeflag {
-		case tar.TypeDir:
-			err = os.Mkdir(filename, 0700)
-			if err != nil {
-				return xerrors.Errorf("unable to check out template directory: %w", err)
-			}
-		case tar.TypeReg:
-			f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0600)
-			if err != nil {
-				return xerrors.Errorf("unable to create template file: %w", err)
-			}
-
-			_, err = io.Copy(f, archiveReader)
-			if err != nil {
-				f.Close() // is this necessary?
-				return xerrors.Errorf("error writing template file: %w", err)
-			}
-			f.Close()
-		}
-
-		hdr, err = archiveReader.Next()
-	}
-	return nil
-}
 
 func templateCheckout() *cobra.Command {
 	cmd := &cobra.Command{
@@ -75,7 +36,7 @@ func templateCheckout() *cobra.Command {
 				return xerrors.Errorf("template file/directory already exists: %s", destination)
 			}
 
-			return TarBytesToTree(destination, raw)
+			return provisionersdk.Untar(destination, raw)
 		},
 	}
 
