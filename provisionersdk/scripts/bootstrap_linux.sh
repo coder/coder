8a59178e7e076a56e03cd971247991189524806e
Cian Johnston <cian@coder.com>
2022-07-11 12:43:14 +0100

    provisionersdk: extract and embed agent bootstrap scripts (#2886)
    This commit extracts the existing hard-coded agent scripts to their own files and embeds them using go:embed.
    We can more easily use e.g. shellcheck to validate our agent scripts.

---
+++ provisionersdk/scripts/bootstrap_linux.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+set -eux pipefail
+BINARY_DIR=$(mktemp -d -t coder.XXXXXX)
+BINARY_NAME=coder
+BINARY_URL=${ACCESS_URL}bin/coder-linux-${ARCH}
+cd "$BINARY_DIR"
+if command -v curl >/dev/null 2>&1; then
+	curl -fsSL --compressed "${BINARY_URL}" -o "${BINARY_NAME}"
+elif command -v wget >/dev/null 2>&1; then
+	wget -q "${BINARY_URL}" -O "${BINARY_NAME}"
+elif command -v busybox >/dev/null 2>&1; then
+	busybox wget -q "${BINARY_URL}" -O "${BINARY_NAME}"
+else
+	echo "error: no download tool found, please install curl, wget or busybox wget"
+	exit 1
+fi
+chmod +x $BINARY_NAME
+export CODER_AGENT_AUTH="${AUTH_TYPE}"
+export CODER_AGENT_URL="${ACCESS_URL}"
+exec ./$BINARY_NAME agent
