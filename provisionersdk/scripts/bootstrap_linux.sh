ca8aac50965a4d33ffb3cd09f07717d6987775b4
johnstcn <public@cianjohnston.ie>
2022-07-11 12:55:15 +0000

    add sleeps to agent startup script

--- provisionersdk/scripts/bootstrap_linux.sh
+++ provisionersdk/scripts/bootstrap_linux.sh
@@ -6,16 +6,26 @@ BINARY_NAME=coder
 BINARY_URL=${ACCESS_URL}bin/coder-linux-${ARCH}
 cd "$BINARY_DIR"
 if command -v curl >/dev/null 2>&1; then
-	curl -fsSL --compressed "${BINARY_URL}" -o "${BINARY_NAME}"
+	curl -fsSL --compressed "${BINARY_URL}" -o "${BINARY_NAME}" || (
+		echo "error: failed to download coder agent" && sleep 600
+	)
 elif command -v wget >/dev/null 2>&1; then
-	wget -q "${BINARY_URL}" -O "${BINARY_NAME}"
+	wget -q "${BINARY_URL}" -O "${BINARY_NAME}" || (
+		echo "error: failed to download coder agent" && sleep 600
+	)
 elif command -v busybox >/dev/null 2>&1; then
-	busybox wget -q "${BINARY_URL}" -O "${BINARY_NAME}"
+	busybox wget -q "${BINARY_URL}" -O "${BINARY_NAME}" || (
+		echo "error: failed to download coder agent" && sleep 600
+	)
 else
 	echo "error: no download tool found, please install curl, wget or busybox wget"
 	exit 1
 fi
-chmod +x $BINARY_NAME
+chmod +x $BINARY_NAME || (
+	echo "Failed to make $BINARY_NAME executable" && sleep 600
+)
 export CODER_AGENT_AUTH="${AUTH_TYPE}"
 export CODER_AGENT_URL="${ACCESS_URL}"
-exec ./$BINARY_NAME agent
+exec ./$BINARY_NAME agent || (
+	echo "Failed to exec ${BINARY_NAME}" && sleep 600
+)
