49423350420b53e16cf3b46fbc88907438fe9e44
johnstcn <public@cianjohnston.ie>
2022-07-12 11:20:40 +0000

    make scripts loop forever

--- provisionersdk/scripts/bootstrap_linux.sh
+++ provisionersdk/scripts/bootstrap_linux.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 set -eux pipefail
+# Sleep for a good long while before exiting.
+# This is to allow folks to exec into a failed workspace and poke around to
+# troubleshoot.
 waitonexit() {
 	echo '=== Agent script exited with non-zero code. Sleeping 24h to preserve logs...'
 	sleep 86400
@@ -9,43 +12,41 @@ BINARY_DIR=$(mktemp -d -t coder.XXXXXX)
 BINARY_NAME=coder
 BINARY_URL=${ACCESS_URL}bin/coder-linux-${ARCH}
 cd "$BINARY_DIR"
-# In the below invocations, we sleep for 30 seconds before exiting.
-# This is because some providers (e.g. kreuzwerker/docker) will
-# automatically remove a Docker container that exits within 15
-# seconds, making troubleshooting a failed workspace build
-# extremely difficult.
-if command -v curl >/dev/null 2>&1; then
-	curl -fsSL --compressed "${BINARY_URL}" -o "${BINARY_NAME}" || (
+# Attempt to download the coder agent.
+# This could fail for a number of reasons, many of which are likely transient.
+# So just keep trying!
+while true; do
+	# Try a number of different download tools, as we don't know what we'll
+	# have available
+	if command -v curl >/dev/null 2>&1; then
+		curl -fsSL --compressed "${BINARY_URL}" -o "${BINARY_NAME}" && break
 		status=$?
 		echo "error: failed to download coder agent using curl"
-		sleep 30
-		exit $status
-	)
-elif command -v wget >/dev/null 2>&1; then
-	wget -q "${BINARY_URL}" -O "${BINARY_NAME}" || (
+		echo "curl exit code: ${status}"
+	elif command -v wget >/dev/null 2>&1; then
+		wget -q "${BINARY_URL}" -O "${BINARY_NAME}" && break
 		status=$?
+		test "${status}" -eq 0 && break
 		echo "error: failed to download coder agent using wget"
-		sleep 30
-		exit $status
-	)
-elif command -v busybox >/dev/null 2>&1; then
-	busybox wget -q "${BINARY_URL}" -O "${BINARY_NAME}" || (
-		status=$?
+		echo "wget exit code: ${status}"
+	elif command -v busybox >/dev/null 2>&1; then
+		busybox wget -q "${BINARY_URL}" -O "${BINARY_NAME}" && break
+		test "${status}" -eq 0 && break
 		echo "error: failed to download coder agent using busybox wget"
-		sleep 30
-		exit $status
-	)
-else
-	echo "error: no download tool found, please install curl, wget or busybox wget"
+		echo "busybox wget exit code: ${status}"
+	else
+		echo "error: no download tool found, please install curl, wget or busybox wget"
+		exit 127
+	fi
+	echo "trying again in 30 seconds..."
+	sleep 30
+done
+
+if ! chmod +x $BINARY_NAME; then
+	echo "Failed to make $BINARY_NAME executable"
 	exit 1
 fi
-chmod +x $BINARY_NAME || (
-	echo "Failed to make $BINARY_NAME executable" && sleep 30 && exit 1
-)
+
 export CODER_AGENT_AUTH="${AUTH_TYPE}"
 export CODER_AGENT_URL="${ACCESS_URL}"
-exec ./$BINARY_NAME agent || (
-	echo "Failed to exec ${BINARY_NAME}"
-	sleep 30
-	exit 126
-)
+exec ./$BINARY_NAME agent
--- provisionersdk/scripts/bootstrap_windows.ps1
+++ provisionersdk/scripts/bootstrap_windows.ps1
@@ -1,9 +1,23 @@
-# On Windows, VS Code Remote requires a parent process of the
-# executing shell to be named "sshd", otherwise it fails. See:
-# https://github.com/microsoft/vscode-remote-release/issues/5699
-$ProgressPreference = "SilentlyContinue"
-Invoke-WebRequest -Uri ${ACCESS_URL}bin/coder-windows-${ARCH}.exe -OutFile $env:TEMP\sshd.exe
-Set-MpPreference -DisableRealtimeMonitoring $true -ExclusionPath $env:TEMP\sshd.exe
-$env:CODER_AGENT_AUTH = "${AUTH_TYPE}"
-$env:CODER_AGENT_URL = "${ACCESS_URL}"
-Start-Process -FilePath $env:TEMP\sshd.exe -ArgumentList "agent" -PassThru
+while ($true) {
+	try {
+		$ProgressPreference = "SilentlyContinue"
+		# On Windows, VS Code Remote requires a parent process of the
+		# executing shell to be named "sshd", otherwise it fails. See:
+		# https://github.com/microsoft/vscode-remote-release/issues/5699
+		$BINARY_URL="${ACCESS_URL}/bin/coder-windows-${ARCH}.exe"
+		Invoke-WebRequest -Uri "${BINARY_URL}" -OutFile $env:TEMP\sshd.exe
+		Set-MpPreference -DisableRealtimeMonitoring $true -ExclusionPath $env:TEMP\sshd.exe
+		$env:CODER_AGENT_AUTH = "${AUTH_TYPE}"
+		$env:CODER_AGENT_URL = "${ACCESS_URL}"
+		Start-Process -FilePath $env:TEMP\sshd.exe -ArgumentList "agent" -PassThru
+	} catch [System.Net.WebException],[System.IO.IOException] {
+		Write-Error "error: failed to download coder agent from ${ACCESS_URL}"
+		Write-Error $_.ScriptStackTrace
+	} catch {
+		Write-Error "error: unhandled exception fetching and starting coder agent:"
+		Write-Error $_.ScriptStackTrace
+	} finally {
+		Write-Output "trying again in 30 seconds..."
+		Start-Sleep -Seconds 30
+	}
+}
