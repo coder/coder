8c551bd6b53d640c6e3a0725f0b34d0cc8ad5e26
Bruno <bruno@coder.com>
2022-07-12 14:16:35 +0000

    Add SSH button

---
+++ site/src/components/SSHButton/SSHButton.tsx
@@ -0,0 +1,95 @@
+import Button from "@material-ui/core/Button"
+import Popover from "@material-ui/core/Popover"
+import { makeStyles } from "@material-ui/core/styles"
+import CloudIcon from "@material-ui/icons/CloudOutlined"
+import { useRef, useState } from "react"
+import { CodeExample } from "../CodeExample/CodeExample"
+import { Stack } from "../Stack/Stack"
+import { HelpTooltipLink, HelpTooltipLinksGroup, HelpTooltipText } from "../Tooltips/HelpTooltip"
+
+export const SSHButton: React.FC<{ workspaceName: string; agentName: string }> = ({
+  workspaceName,
+  agentName,
+}) => {
+  const anchorRef = useRef<HTMLButtonElement>(null)
+  const [isOpen, setIsOpen] = useState(false)
+  const id = isOpen ? "schedule-popover" : undefined
+  const styles = useStyles()
+
+  const onClose = () => {
+    setIsOpen(false)
+  }
+
+  return (
+    <>
+      <Button
+        startIcon={<CloudIcon />}
+        size="small"
+        ref={anchorRef}
+        onClick={() => {
+          setIsOpen(true)
+        }}
+      >
+        SSH
+      </Button>
+      <Popover
+        classes={{ paper: styles.popoverPaper }}
+        id={id}
+        open={isOpen}
+        anchorEl={anchorRef.current}
+        onClose={onClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <HelpTooltipText>
+          To connect with SSH you have to run the following commands:
+        </HelpTooltipText>
+
+        <Stack spacing={0.5} className={styles.codeExamples}>
+          <div>
+            <HelpTooltipText>
+              <strong className={styles.codeExampleLabel}>Configure ssh</strong>
+            </HelpTooltipText>
+            <CodeExample code="coder config-ssh" />
+          </div>
+
+          <div>
+            <HelpTooltipText>
+              <strong className={styles.codeExampleLabel}>Connect to the agent</strong>
+            </HelpTooltipText>
+            <CodeExample code={`ssh coder.${workspaceName}.${agentName}`} />
+          </div>
+        </Stack>
+
+        <HelpTooltipLinksGroup>
+          <HelpTooltipLink href="#">Install Coder CLI</HelpTooltipLink>
+          <HelpTooltipLink href="#">Configuring VS Code</HelpTooltipLink>
+          <HelpTooltipLink href="#">SSH configuration</HelpTooltipLink>
+        </HelpTooltipLinksGroup>
+      </Popover>
+    </>
+  )
+}
+
+const useStyles = makeStyles((theme) => ({
+  popoverPaper: {
+    padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`,
+    width: theme.spacing(38),
+    color: theme.palette.text.secondary,
+    marginTop: theme.spacing(0.25),
+  },
+
+  codeExamples: {
+    marginTop: theme.spacing(1.5),
+  },
+
+  codeExampleLabel: {
+    fontSize: 12,
+  },
+}))
