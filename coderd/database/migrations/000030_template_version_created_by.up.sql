c98f87b87d1c367ee156e431946914d4180ac5f2
Abhineet Jain <abhineet@coder.com>
2022-07-15 00:58:25 +0000

    wrap migration for a single txn

--- coderd/database/migrations/000030_template_version_created_by.up.sql
+++ coderd/database/migrations/000030_template_version_created_by.up.sql
@@ -1,14 +1,18 @@
+BEGIN;
+
 ALTER TABLE ONLY template_versions ADD COLUMN IF NOT EXISTS created_by uuid REFERENCES users (id) ON DELETE RESTRICT;
 
 UPDATE
     template_versions
 SET
-    created_by = (
-        SELECT created_by FROM templates
+    template_versions.created_by = (
+        SELECT templates.created_by FROM templates
         WHERE template_versions.template_id = templates.id
         LIMIT 1
     )
 WHERE
-    created_by IS NULL;
+    template_versions.created_by IS NULL;
 
 ALTER TABLE ONLY template_versions ALTER COLUMN created_by SET NOT NULL;
+
+COMMIT;
