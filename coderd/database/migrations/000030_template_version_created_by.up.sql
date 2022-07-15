08b6b5d07f30172049f62ec497d81530913220db
Abhineet Jain <abhineet@coder.com>
2022-07-15 15:55:15 +0000

    remove scoping from migration

--- coderd/database/migrations/000030_template_version_created_by.up.sql
+++ coderd/database/migrations/000030_template_version_created_by.up.sql
@@ -5,13 +5,13 @@ ALTER TABLE ONLY template_versions ADD COLUMN IF NOT EXISTS created_by uuid REFE
 UPDATE
     template_versions
 SET
-    template_versions.created_by = (
-        SELECT templates.created_by FROM templates
+    created_by = (
+        SELECT created_by FROM templates
         WHERE template_versions.template_id = templates.id
         LIMIT 1
     )
 WHERE
-    template_versions.created_by IS NULL;
+    created_by IS NULL;
 
 ALTER TABLE ONLY template_versions ALTER COLUMN created_by SET NOT NULL;
 
