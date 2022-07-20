b9d8afcb67eff6948574256a0199f8e9bdda7c7a
Jon Ayers <jon@coder.com>
2022-07-20 00:03:30 +0000

    add some more tests

--- cli/root_test.go
+++ cli/root_test.go
@@ -31,25 +31,87 @@ func TestRoot(t *testing.T) {
 		t.Run("Verbose", func(t *testing.T) {
 			t.Parallel()
 
-			cmd, _ := clitest.New(t, "--verbose")
+			// Test that the verbose error is masked without verbose flag.
+			t.Run("NoVerboseAPIError", func(t *testing.T) {
+				t.Parallel()
 
-			cmd.RunE = func(cmd *cobra.Command, args []string) error {
-				var err error = &codersdk.Error{
-					Response: codersdk.Response{
-						Message: "This is a message.",
-					},
-					Helper: "Try this instead.",
+				cmd, _ := clitest.New(t)
+
+				cmd.RunE = func(cmd *cobra.Command, args []string) error {
+					var err error = &codersdk.Error{
+						Response: codersdk.Response{
+							Message: "This is a message.",
+						},
+						Helper: "Try this instead.",
+					}
+
+					err = xerrors.Errorf("wrap me: %w", err)
+
+					return err
 				}
 
-				err = xerrors.Errorf("wrap me: %w", err)
+				cmd, err := cmd.ExecuteC()
+				errStr := cli.FormatCobraError(err, cmd)
+				require.Contains(t, errStr, "This is a message. Try this instead.")
+				require.NotContains(t, errStr, err.Error())
+			})
 
-				return err
-			}
+			// Assert that a regular error is not masked when verbose is not
+			// specified.
+			t.Run("NoVerboseRegularError", func(t *testing.T) {
+				t.Parallel()
 
-			cmd, err := cmd.ExecuteC()
-			errStr := cli.FormatCobraError(err, cmd)
-			require.Contains(t, errStr, "This is a message. Try this instead.")
-			require.Contains(t, errStr, err.Error())
+				cmd, _ := clitest.New(t)
+
+				cmd.RunE = func(cmd *cobra.Command, args []string) error {
+					return xerrors.Errorf("this is a non-codersdk error: %w", xerrors.Errorf("a wrapped error"))
+				}
+
+				cmd, err := cmd.ExecuteC()
+				errStr := cli.FormatCobraError(err, cmd)
+				require.Contains(t, errStr, err.Error())
+			})
+
+			// Test that both the friendly error and the verbose error are
+			// displayed when verbose is passed.
+			t.Run("APIError", func(t *testing.T) {
+				t.Parallel()
+
+				cmd, _ := clitest.New(t, "--verbose")
+
+				cmd.RunE = func(cmd *cobra.Command, args []string) error {
+					var err error = &codersdk.Error{
+						Response: codersdk.Response{
+							Message: "This is a message.",
+						},
+						Helper: "Try this instead.",
+					}
+
+					err = xerrors.Errorf("wrap me: %w", err)
+
+					return err
+				}
+
+				cmd, err := cmd.ExecuteC()
+				errStr := cli.FormatCobraError(err, cmd)
+				require.Contains(t, errStr, "This is a message. Try this instead.")
+				require.Contains(t, errStr, err.Error())
+			})
+
+			// Assert that a regular error is not masked when verbose specified.
+			t.Run("RegularError", func(t *testing.T) {
+				t.Parallel()
+
+				cmd, _ := clitest.New(t, "--verbose")
+
+				cmd.RunE = func(cmd *cobra.Command, args []string) error {
+					return xerrors.Errorf("this is a non-codersdk error: %w", xerrors.Errorf("a wrapped error"))
+				}
+
+				cmd, err := cmd.ExecuteC()
+				errStr := cli.FormatCobraError(err, cmd)
+				require.Contains(t, errStr, err.Error())
+			})
 		})
 	})
 
