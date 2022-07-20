081832f09cc464913f3433a4e3053e8fe5dd82a1
Kira Pilot <kira.pilot23@gmail.com>
2022-07-20 20:45:57 +0000

    added unit test for convertProvisionerJob

--- coderd/provisionerjobs_internal_test.go
+++ coderd/provisionerjobs_internal_test.go
@@ -3,6 +3,7 @@ package coderd
 import (
 	"context"
 	"crypto/sha256"
+	"database/sql"
 	"encoding/json"
 	"net/http/httptest"
 	"net/url"
@@ -144,6 +145,111 @@ func TestProvisionerJobLogs_Unit(t *testing.T) {
 		}
 		assert.False(t, fPubsub.closed)
 	})
+}
+
+func TestConvertProvisionerJob_Unit(t *testing.T) {
+	t.Parallel()
+	validNullTimeMock := sql.NullTime{
+		Time:  database.Now(),
+		Valid: true,
+	}
+	invalidNullTimeMock := sql.NullTime{
+		Valid: false,
+	}
+	errorMock := sql.NullString{
+		String: "error",
+		Valid:  true,
+	}
+	testCases := []struct {
+		name     string
+		input    database.ProvisionerJob
+		expected codersdk.ProvisionerJob
+	}{
+		{
+			name:  "empty",
+			input: database.ProvisionerJob{},
+			expected: codersdk.ProvisionerJob{
+				Status: codersdk.ProvisionerJobPending,
+			},
+		},
+		{
+			name: "cancellation pending",
+			input: database.ProvisionerJob{
+				CanceledAt:  validNullTimeMock,
+				CompletedAt: invalidNullTimeMock,
+			},
+			expected: codersdk.ProvisionerJob{
+				Status: codersdk.ProvisionerJobCanceling,
+			},
+		},
+		{
+			name: "cancellation failed",
+			input: database.ProvisionerJob{
+				CanceledAt:  validNullTimeMock,
+				CompletedAt: validNullTimeMock,
+				Error:       errorMock,
+			},
+			expected: codersdk.ProvisionerJob{
+				CompletedAt: &validNullTimeMock.Time,
+				Status:      codersdk.ProvisionerJobFailed,
+				Error:       errorMock.String,
+			},
+		},
+		{
+			name: "cancellation succeeded",
+			input: database.ProvisionerJob{
+				CanceledAt:  validNullTimeMock,
+				CompletedAt: validNullTimeMock,
+			},
+			expected: codersdk.ProvisionerJob{
+				CompletedAt: &validNullTimeMock.Time,
+				Status:      codersdk.ProvisionerJobCanceled,
+			},
+		},
+		{
+			name: "job pending",
+			input: database.ProvisionerJob{
+				StartedAt: invalidNullTimeMock,
+			},
+			expected: codersdk.ProvisionerJob{
+				Status: codersdk.ProvisionerJobPending,
+			},
+		},
+		{
+			name: "job failed",
+			input: database.ProvisionerJob{
+				CompletedAt: validNullTimeMock,
+				StartedAt:   validNullTimeMock,
+				Error:       errorMock,
+			},
+			expected: codersdk.ProvisionerJob{
+				CompletedAt: &validNullTimeMock.Time,
+				StartedAt:   &validNullTimeMock.Time,
+				Error:       errorMock.String,
+				Status:      codersdk.ProvisionerJobFailed,
+			},
+		},
+		{
+			name: "job succeeded",
+			input: database.ProvisionerJob{
+				CompletedAt: validNullTimeMock,
+				StartedAt:   validNullTimeMock,
+			},
+			expected: codersdk.ProvisionerJob{
+				CompletedAt: &validNullTimeMock.Time,
+				StartedAt:   &validNullTimeMock.Time,
+				Status:      codersdk.ProvisionerJobSucceeded,
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			actual := convertProvisionerJob(testCase.input)
+			assert.Equal(t, testCase.expected, actual)
+		})
+	}
 }
 
 type fakePubSub struct {
