d7ec407a7c80ab05532ccfc669a8faa965dbba69
Mathias Fredriksson <mafredri@gmail.com>
2022-07-25 16:56:20 +0300

    fix: Improve coder list and show CLI help (#3167)

--- cli/show.go
+++ cli/show.go
@@ -10,7 +10,7 @@ import (
 func show() *cobra.Command {
 	return &cobra.Command{
 		Annotations: workspaceCommand,
-		Use:         "show",
+		Use:         "show <workspace>",
 		Short:       "Show details of a workspace's resources and agents",
 		Args:        cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
