0128ca6bd1fc5ca3afd3adbd3a2edccf97352b4b
Abhineet Jain <AbhineetJain@users.noreply.github.com>
2022-07-26 15:39:45 -0400

    fix: manage backend authXService errors (#3190)

--- site/src/pages/UserSettingsPage/AccountPage/AccountPage.tsx
+++ site/src/pages/UserSettingsPage/AccountPage/AccountPage.tsx
@@ -1,25 +1,17 @@
 import { useActor } from "@xstate/react"
 import React, { useContext } from "react"
-import { isApiError, mapApiErrorToFieldErrors } from "../../../api/errors"
 import { Section } from "../../../components/Section/Section"
 import { AccountForm } from "../../../components/SettingsAccountForm/SettingsAccountForm"
 import { XServiceContext } from "../../../xServices/StateContext"
 
 export const Language = {
   title: "Account",
-  unknownError: "Oops, an unknown error occurred.",
 }
 
 export const AccountPage: React.FC = () => {
   const xServices = useContext(XServiceContext)
   const [authState, authSend] = useActor(xServices.authXService)
   const { me, updateProfileError } = authState.context
-  const hasError = !!updateProfileError
-  const formErrors =
-    hasError && isApiError(updateProfileError)
-      ? mapApiErrorToFieldErrors(updateProfileError.response.data)
-      : undefined
-  const hasUnknownError = hasError && !isApiError(updateProfileError)
 
   if (!me) {
     throw new Error("No current user found")
@@ -29,8 +21,7 @@ export const AccountPage: React.FC = () => {
     <Section title={Language.title}>
       <AccountForm
         email={me.email}
-        error={hasUnknownError ? Language.unknownError : undefined}
-        formErrors={formErrors}
+        updateProfileError={updateProfileError}
         isLoading={authState.matches("signedIn.profile.updatingProfile")}
         initialValues={{ username: me.username }}
         onSubmit={(data) => {
