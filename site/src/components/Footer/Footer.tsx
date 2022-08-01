56ee105a2ab474e638ee6de3912ddde9c8c9a2cd
Ben Potter <ben@coder.com>
2022-08-01 10:03:52 -0500

    chore: add Discord link to footer (#3239)

--- site/src/components/Footer/Footer.tsx
+++ site/src/components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import Link from "@material-ui/core/Link"
 import { makeStyles } from "@material-ui/core/styles"
 import AccountTreeIcon from "@material-ui/icons/AccountTree"
 import AssistantIcon from "@material-ui/icons/Assistant"
+import ChatIcon from "@material-ui/icons/Chat"
 import * as TypesGen from "../../api/typesGenerated"
 
 export const Language = {
@@ -10,6 +11,7 @@ export const Language = {
   },
   copyrightText: `Copyright \u00a9 ${new Date().getFullYear()} Coder Technologies, Inc. All rights reserved.`,
   reportBugLink: "Report an issue or share feedback",
+  discordLink: "Join Coder on Discord",
 }
 
 export interface FooterProps {
@@ -20,6 +22,7 @@ export const Footer: React.FC<FooterProps> = ({ buildInfo }) => {
   const styles = useFooterStyles()
 
   const githubUrl = `https://github.com/coder/coder/issues/new?labels=bug,needs+grooming&title=Bug+in+${buildInfo?.version}:&template=external_bug_report.md`
+  const discordUrl = `https://discord.gg/coder`
 
   return (
     <div className={styles.root}>
@@ -37,6 +40,10 @@ export const Footer: React.FC<FooterProps> = ({ buildInfo }) => {
           &nbsp;|&nbsp;
           <Link className={styles.link} variant="caption" target="_blank" href={githubUrl}>
             <AssistantIcon className={styles.icon} /> {Language.reportBugLink}
+          </Link>
+          &nbsp;|&nbsp;
+          <Link className={styles.link} variant="caption" target="_blank" href={discordUrl}>
+            <ChatIcon className={styles.icon} /> {Language.discordLink}
           </Link>
         </div>
       )}
