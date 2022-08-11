b5e0a63e6e4cc27e7bb8d2d94518fb1ce1ab53cb
Bruno Quaresma <youbruno@coder.com>
2022-08-11 16:29:43 +0000

    Fix formatting

--- site/src/xServices/setup/setupXService.ts
+++ site/src/xServices/setup/setupXService.ts
@@ -11,7 +11,7 @@ import * as TypesGen from "api/typesGenerated"
 import { assign, createMachine } from "xstate"
 
 export const Language = {
-  createFirstUserError: Failed to create the user.",
+  createFirstUserError: "Failed to create the user.",
 }
 
 export interface SetupContext {
