4108ece22a96b0153db22565dccb7edeb01ce782
Jon Ayers <jon@coder.com>
2022-08-11 21:16:28 +0000

    begin refactoring PR

--- coderd/database/migrations/000034_linked_user_id.up.sql
+++ coderd/database/migrations/000034_linked_user_id.up.sql
@@ -1,6 +1,15 @@
 BEGIN;
 
-ALTER TABLE users ADD COLUMN login_type login_type NOT NULL DEFAULT 'password'; 
+CREATE TABLE IF NOT EXISTS users (
+	user_id uuid NOT NULL,
+	login_type login_type NOT NULL,
+	linked_id text NOT NULL DEFAULT ''::text NOT NULL,
+    oauth_access_token text DEFAULT ''::text NOT NULL,
+    oauth_refresh_token text DEFAULT ''::text NOT NULL,
+    oauth_id_token text DEFAULT ''::text NOT NULL,
+    oauth_expiry timestamp with time zone DEFAULT '0001-01-01 00:00:00+00'::timestamp with time zone NOT NULL,
+	UNIQUE(user_id, login_type),
+)
 ALTER TABLE users ADD COLUMN linked_id text NOT NULL DEFAULT ''; 
 
 UPDATE 
