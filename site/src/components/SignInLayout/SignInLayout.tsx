7b37e0effbdf45756018de2e35cbdf659b55c73d
Bruno Quaresma <bruno@coder.com>
2022-08-11 12:33:07 -0300

    Apply suggestions from code review
    Co-authored-by: Kira Pilot <kira@coder.com>

--- site/src/components/SignInLayout/SignInLayout.tsx
+++ site/src/components/SignInLayout/SignInLayout.tsx
@@ -1,5 +1,5 @@
 import { makeStyles } from "@material-ui/core/styles"
-import React from "react"
+import { FC } from "react"
 import { Footer } from "../../components/Footer/Footer"
 
 export const useStyles = makeStyles((theme) => ({
@@ -21,7 +21,7 @@ export const useStyles = makeStyles((theme) => ({
   },
 }))
 
-export const SignInLayout: React.FC = ({ children }) => {
+export const SignInLayout: FC = ({ children }) => {
   const styles = useStyles()
 
   return (
