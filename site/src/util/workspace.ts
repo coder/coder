494213fca5154573c095aece6f42f3c18920619d
Cian Johnston <cian@coder.com>
2022-08-18 12:46:28 +0000

    fix dayjs plugin silliness the right way

--- site/src/util/workspace.ts
+++ site/src/util/workspace.ts
@@ -2,11 +2,13 @@ import { Theme } from "@material-ui/core/styles"
 import dayjs from "dayjs"
 import duration from "dayjs/plugin/duration"
 import utc from "dayjs/plugin/utc"
+import minMax from "dayjs/plugin/minMax"
 import { WorkspaceBuildTransition } from "../api/types"
 import * as TypesGen from "../api/typesGenerated"
 
 dayjs.extend(duration)
 dayjs.extend(utc)
+dayjs.extend(minMax)
 
 // all the possible states returned by the API
 export enum WorkspaceStateEnum {
@@ -274,10 +276,10 @@ export const deadlineExtensionMax = dayjs.duration(24, "hours")
 export function maxDeadline(ws: TypesGen.Workspace, tpl: TypesGen.Template): dayjs.Dayjs {
   // note: we count runtime from updated_at as started_at counts from the start of
   // the workspace build process, which can take a while.
-  const startedAtMillis = dayjs(ws.latest_build.updated_at).unix() * 1000
-  const maxTemplateDeadline = startedAtMillis + tpl.max_ttl_ms
-  const maxGlobalDeadline = startedAtMillis + deadlineExtensionMax.asMilliseconds()
-  return dayjs(Math.min(maxTemplateDeadline, maxGlobalDeadline))
+  const startedAt = dayjs(ws.latest_build.updated_at)
+  const maxTemplateDeadline = startedAt.add(dayjs.duration(tpl.max_ttl_ms, "milliseconds"))
+  const maxGlobalDeadline = startedAt.add(deadlineExtensionMax)
+  return dayjs.min(maxTemplateDeadline, maxGlobalDeadline)
 }
 
 export function minDeadline(): dayjs.Dayjs {
