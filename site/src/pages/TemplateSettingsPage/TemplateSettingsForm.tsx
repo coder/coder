ac67ccbaefa478c315c364d279500e6327d67fa7
Kira Pilot <kira.pilot23@gmail.com>
2022-08-24 15:34:58 +0000

    ran prettier

--- site/src/pages/TemplateSettingsPage/TemplateSettingsForm.tsx
+++ site/src/pages/TemplateSettingsPage/TemplateSettingsForm.tsx
@@ -25,9 +25,8 @@ export const Language = {
   selectEmoji: "Select emoji",
   ttlMaxError: "Please enter a limit that is less than or equal to 168 hours (7 days).",
   descriptionMaxError: "Please enter a description that is less than or equal to 128 characters.",
-  ttlHelperText: (
-    ttl: number,
-  ): string => `Workspaces created from this template may not remain running longer than ${ttl}.`,
+  ttlHelperText: (ttl: number): string =>
+    `Workspaces created from this template may not remain running longer than ${ttl}.`,
 }
 
 const MAX_DESCRIPTION_CHAR_LIMIT = 128
