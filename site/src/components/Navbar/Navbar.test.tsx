8b88265d617c8a398e1a348168f82580d1c5bf4d
Presley Pizzo <presley@coder.com>
2022-08-24 20:52:18 +0000

    Format

--- site/src/components/Navbar/Navbar.test.tsx
+++ site/src/components/Navbar/Navbar.test.tsx
@@ -1,33 +1,40 @@
-import { render, MockEntitlementsWithAuditLog, MockMemberPermissions, MockUser } from "testHelpers/renderHelpers"
-import { server } from "testHelpers/server"
 import { screen } from "@testing-library/react"
-import { Navbar } from "./Navbar"
 import { rest } from "msw"
+import {
+  MockEntitlementsWithAuditLog,
+  MockMemberPermissions,
+  MockUser,
+  render,
+} from "testHelpers/renderHelpers"
+import { server } from "testHelpers/server"
+import { Navbar } from "./Navbar"
 
 describe("Navbar", () => {
-  it("shows Audit Log link when permitted and entitled", () => {
+  it("shows Audit Log link when permitted and entitled", async () => {
     server.use(
       rest.get("/api/entitlements", (req, res, ctx) => {
         return res(ctx.status(200), ctx.json(MockEntitlementsWithAuditLog))
       }),
     )
     render(<Navbar />)
-    expect(screen.getByText("Audit Log"))
+    const link = await screen.findByText("Audit Log")
+    expect(link).toBeDefined()
   })
 
   it("does not show Audit Log link when not entitled", () => {
-    server.use()
     render(<Navbar />)
-    expect(screen.queryByText("Audit Log")).not.toBeDefined()
+    const link = screen.getByText("Audit Log")
+    expect(link).not.toBeDefined()
   })
 
   it("does not show Audit Log link when not permitted via role", () => {
-     server.use(
+    server.use(
       rest.post(`/api/v2/users/${MockUser.id}/authorization`, async (req, res, ctx) => {
         return res(ctx.status(200), ctx.json(MockMemberPermissions))
       }),
     )
     render(<Navbar />)
-    expect(screen.queryByText("Audit Log")).not.toBeDefined()
+    const link = screen.getByText("Audit Log")
+    expect(link).not.toBeDefined()
   })
 })
