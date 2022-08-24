f8dcf51ce26356f3e239ca7cd17085e7aa0fe295
Mathias Fredriksson <mafredri@gmail.com>
2022-08-24 15:47:09 +0300

    fix: Add data loss warning

--- cli/rename.go
+++ cli/rename.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 	"golang.org/x/xerrors"
 
+	"github.com/coder/coder/cli/cliui"
 	"github.com/coder/coder/codersdk"
 )
 
@@ -14,7 +15,7 @@ func rename() *cobra.Command {
 		Short:       "Rename a workspace",
 		Args:        cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			client, err := createClient(cmd)
+			client, err := CreateClient(cmd)
 			if err != nil {
 				return err
 			}
@@ -22,6 +23,15 @@ func rename() *cobra.Command {
 			if err != nil {
 				return xerrors.Errorf("get workspace: %w", err)
 			}
+
+			_, err = cliui.Prompt(cmd, cliui.PromptOptions{
+				Text:      "WARNING: A rename can result in loss of home volume if the template references the workspace name. Continue?",
+				IsConfirm: true,
+			})
+			if err != nil {
+				return err
+			}
+
 			err = client.UpdateWorkspace(cmd.Context(), workspace.ID, codersdk.UpdateWorkspaceRequest{
 				Name: args[1],
 			})
