b940f5bfb915c260a64e2ad416abe8c23e6b93ef
Ammar Bandukwala <ammar@ammar.io>
2022-08-25 05:18:38 +0000

    site: correct documentation on gitsshkey

--- site/src/pages/UserSettingsPage/SSHKeysPage/SSHKeysPage.tsx
+++ site/src/pages/UserSettingsPage/SSHKeysPage/SSHKeysPage.tsx
@@ -7,8 +7,14 @@ import { SSHKeysPageView } from "./SSHKeysPageView"
 
 export const Language = {
   title: "SSH keys",
-  description:
-    "Coder automatically inserts a private key into every workspace; you can add the corresponding public key to any services (such as Git) that you need access to from your workspace.",
+  description: (
+    <p>
+      Git in workspaces is authenticated with the following public key. You may add it to Git
+      services (such as GitHub) that you need to access from your workspace. <br />
+      <br />
+      Coder sets <code>$GIT_SSH_COMMAND</code> to perform the override.
+    </p>
+  ),
   regenerateDialogTitle: "Regenerate SSH key?",
   regenerateDialogMessage:
     "You will need to replace the public SSH key on services you use it with, and you'll need to rebuild existing workspaces.",
