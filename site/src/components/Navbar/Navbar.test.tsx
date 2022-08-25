a26786ed727f49f5beb11fe1221d54f9e0db5b0c
Presley Pizzo <presley@coder.com>
2022-08-25 21:50:27 +0000

    Add comments and try shorter timeout

--- site/src/components/Navbar/Navbar.test.tsx
+++ site/src/components/Navbar/Navbar.test.tsx
@@ -15,41 +15,55 @@ import { server } from "testHelpers/server"
  */
 describe("Navbar", () => {
   it("shows Audit Log link when permitted and entitled", async () => {
+    // set entitlements to allow audit log
     server.use(
       rest.get("/api/v2/entitlements", (req, res, ctx) => {
         return res(ctx.status(200), ctx.json(MockEntitlementsWithAuditLog))
       }),
     )
     render(<App />)
-    await waitFor(() => {
-      const link = screen.getByText(Language.users) // TODO change after debugging
-      expect(link).toBeDefined()
-    }, { timeout: 10000 })
+    await waitFor(
+      () => {
+        const link = screen.getByText(Language.audit)
+        expect(link).toBeDefined()
+      },
+      { timeout: 5000 },
+    )
   })
 
   it("does not show Audit Log link when not entitled", async () => {
+    // by default, user is an Admin with permission to see the audit log,
+    // but is unlicensed so not entitled to see the audit log
     render(<App />)
-    await waitFor(() => {
-      const link = screen.queryByText(Language.audit)
-      expect(link).toBe(null)
-    })
+    await waitFor(
+      () => {
+        const link = screen.queryByText(Language.audit)
+        expect(link).toBe(null)
+      },
+      { timeout: 5000 },
+    )
   })
 
   it("does not show Audit Log link when not permitted via role", async () => {
+    // set permissions to Member (can't audit)
     server.use(
       rest.post(`/api/v2/users/${MockUser.id}/authorization`, async (req, res, ctx) => {
         return res(ctx.status(200), ctx.json(MockMemberPermissions))
       }),
     )
+    // set entitlements to allow audit log
     server.use(
       rest.get("/api/v2/entitlements", (req, res, ctx) => {
         return res(ctx.status(200), ctx.json(MockEntitlementsWithAuditLog))
       }),
     )
     render(<App />)
-    await waitFor(() => {
-      const link = screen.queryByText(Language.audit)
-      expect(link).toBe(null)
-    })
+    await waitFor(
+      () => {
+        const link = screen.queryByText(Language.audit)
+        expect(link).toBe(null)
+      },
+      { timeout: 5000 },
+    )
   })
 })
