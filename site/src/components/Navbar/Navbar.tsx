fc237a0ebf2b2b446a777d377314003d6397c330
Presley Pizzo <presley@coder.com>
2022-08-25 18:37:20 +0000

    Upgrade xstate/react to fix crashing tests

--- site/src/components/Navbar/Navbar.tsx
+++ site/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useActor, useSelector } from "@xstate/react"
+import { shallowEqual, useActor, useSelector } from "@xstate/react"
 import { FeatureNames } from "api/types"
 import React, { useContext } from "react"
 import { selectFeatureVisibility } from "xServices/entitlements/entitlementsSelectors"
@@ -9,7 +9,7 @@ export const Navbar: React.FC = () => {
   const xServices = useContext(XServiceContext)
   const [authState, authSend] = useActor(xServices.authXService)
   const { me, permissions } = authState.context
-  const featureVisibility = useSelector(xServices.entitlementsXService, selectFeatureVisibility)
+  const featureVisibility = useSelector(xServices.entitlementsXService, selectFeatureVisibility, shallowEqual)
   const canViewAuditLog = featureVisibility[FeatureNames.AuditLog] && !!permissions?.viewAuditLog
   const onSignOut = () => authSend("SIGN_OUT")
 
