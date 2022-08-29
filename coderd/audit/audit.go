823a58d4936678b856240366537f6d057b3e2309
Colin Adler <colin1adler@gmail.com>
2022-08-29 18:27:49 -0500

    add nop auditor

--- coderd/audit/audit.go
+++ coderd/audit/audit.go
@@ -10,3 +10,15 @@ type Auditor interface {
 	Export(ctx context.Context, alog database.AuditLog) error
 	diff(old, new any) Map
 }
+
+func NewNop() Auditor {
+	return nop{}
+}
+
+type nop struct{}
+
+func (nop) Export(context.Context, database.AuditLog) error {
+	return nil
+}
+
+func (nop) diff(any, any) Map { return Map{} }
