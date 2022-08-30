2d9f24af0ea6e9d91b4ca64e11eaaa822545f9d7
Eric Paulsen <ericpaulsen@coder.com>
2022-08-29 21:23:14 -0500

    update: boot disk version

--- examples/templates/gcp-linux/main.tf
+++ examples/templates/gcp-linux/main.tf
@@ -39,7 +39,7 @@ resource "google_compute_disk" "root" {
   name  = "coder-${lower(data.coder_workspace.me.owner)}-${lower(data.coder_workspace.me.name)}-root"
   type  = "pd-ssd"
   zone  = var.zone
-  image = "debian-cloud/debian-10"
+  image = "debian-cloud/debian-11"
   lifecycle {
     ignore_changes = [image]
   }
