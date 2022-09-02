ff0aa8d742c0afc9ce6a0f6a5c115af85f49ab7e
Colin Adler <colin1adler@gmail.com>
2022-09-02 13:04:29 -0500

    feat: add unique ids to all HTTP requests (#3845)

---
+++ coderd/httpmw/requestid_test.go
@@ -0,0 +1,33 @@
+package httpmw_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/require"
+
+	"github.com/coder/coder/coderd/httpmw"
+)
+
+func TestRequestID(t *testing.T) {
+	t.Parallel()
+
+	rtr := chi.NewRouter()
+	rtr.Use(httpmw.AttachRequestID)
+	rtr.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		rid := httpmw.RequestID(r)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(rid.String()))
+	})
+	r := httptest.NewRequest("GET", "/", nil)
+	rw := httptest.NewRecorder()
+	rtr.ServeHTTP(rw, r)
+
+	res := rw.Result()
+	defer res.Body.Close()
+	require.Equal(t, http.StatusOK, res.StatusCode)
+	require.NotEmpty(t, res.Header.Get("X-Coder-Request-ID"))
+	require.NotEmpty(t, rw.Body.Bytes())
+}
