08443e459d7602613a29bee57a1a6fa4661e85d7
Presley Pizzo <presley@coder.com>
2022-09-02 17:15:05 +0000

    Change hover color of contained buttons

--- site/src/theme/overrides.ts
+++ site/src/theme/overrides.ts
@@ -45,7 +45,7 @@ export const getOverrides = ({ palette, breakpoints }: Theme): Overrides => {
         backgroundColor: colors.gray[17],
         "&:hover": {
           boxShadow: "none",
-          backgroundColor: "#000000",
+          backgroundColor: colors.gray[16]
         },
       },
       sizeSmall: {
