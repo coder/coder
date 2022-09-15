586b3e8d82236f0313fe55a788336b4a43162b88
Kyle Carberry <kyle@carberry.com>
2022-09-15 05:28:34 +0000

    Remove features interface

--- coderd/features/features.go
+++
@@ -1,9 +0,0 @@
-package features
-
-// Service is the interface for interacting with enterprise features.
-type Service interface {
-	// Get returns the implementations for feature interfaces. Parameter `s` must be a pointer to a
-	// struct type containing feature interfaces as fields.  The FeatureService sets all fields to
-	// the correct implementations depending on whether the features are turned on.
-	Get(s any) error
-}
