51bf940bf8f7f8220669dd6fb51933ebc8edc16d
Bruno Quaresma <bruno@coder.com>
2022-09-19 19:30:59 +0000

    Reorder preset filters

--- site/src/pages/AuditPage/AuditPageView.tsx
+++ site/src/pages/AuditPage/AuditPageView.tsx
@@ -25,8 +25,8 @@ const presetFilters = [
   { query: "resource_type:workspace action:create", name: "Created workspaces" },
   { query: "resource_type:template action:create", name: "Added templates" },
   { query: "resource_type:user action:create", name: "Added users" },
-  { query: "resource_type:user action:delete", name: "Deleted users" },
   { query: "resource_type:template action:delete", name: "Deleted templates" },
+  { query: "resource_type:user action:delete", name: "Deleted users" },
 ]
 
 export interface AuditPageViewProps {
