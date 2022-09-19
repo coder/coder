d533a16fccb96523af8b1b795bbddcbfc4e97492
Jon Ayers <jon@coder.com>
2022-09-19 16:07:30 +0000

    undo insert templates query change

--- coderd/database/queries/templates.sql
+++ coderd/database/queries/templates.sql
@@ -68,11 +68,10 @@ INSERT INTO
 		max_ttl,
 		min_autostart_interval,
 		created_by,
-		icon,
-		user_acl
+		icon
 	)
 VALUES
-	($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *;
+	($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *;
 
 -- name: UpdateTemplateActiveVersionByID :exec
 UPDATE
