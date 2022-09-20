6e722869b01a5212f37d752b740bdf9763af2b7b
Jon Ayers <jon@coder.com>
2022-09-20 17:48:27 +0000

    fmt

--- site/src/testHelpers/entities.ts
+++ site/src/testHelpers/entities.ts
@@ -184,7 +184,7 @@ export const MockTemplate: TypesGen.Template = {
   created_by_name: "test_creator",
   icon: "/icon/code.svg",
   user_roles: {},
-  is_private: false
+  is_private: false,
 }
 
 export const MockWorkspaceAutostartDisabled: TypesGen.UpdateWorkspaceAutostartRequest = {
