bad7ffb67718faea82aca7431ddf671c6a0e9410
Joe Previte <jjprevite@gmail.com>
2022-09-20 20:27:52 +0000

    feat: support template query param in index
    This adds support to navigate directly to a template from the index by
    using the `?template=<name>` query  param.

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,6 +1,13 @@
 import { FC } from "react"
-import { Navigate } from "react-router-dom"
+import { Navigate, useSearchParams } from "react-router-dom"
 
 export const IndexPage: FC = () => {
+  const [searchParams, _] = useSearchParams()
+  const template = searchParams.get("template")
+
+  if (template) {
+    return <Navigate to={`/templates/${template}`} replace />
+  }
+
   return <Navigate to="/workspaces" replace />
 }
