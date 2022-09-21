586cc0b0dfa7a6cd0786c8d3bc1cdd6897e07204
Joe Previte <jjprevite@gmail.com>
2022-09-21 18:30:31 +0000

    Revert "feat: support template query param in index"
    This reverts commit bad7ffb67718faea82aca7431ddf671c6a0e9410.
    
    We decided to use the `/template/path` route instead.

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,13 +1,6 @@
 import { FC } from "react"
-import { Navigate, useSearchParams } from "react-router-dom"
+import { Navigate } from "react-router-dom"
 
 export const IndexPage: FC = () => {
-  const [searchParams, _] = useSearchParams()
-  const template = searchParams.get("template")
-
-  if (template) {
-    return <Navigate to={`/templates/${template}`} replace />
-  }
-
   return <Navigate to="/workspaces" replace />
 }
