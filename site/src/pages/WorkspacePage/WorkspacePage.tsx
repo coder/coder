5698b9d70654ad657caf727c7b35ac0bf8fda5c6
Kira Pilot <kira@coder.com>
2022-09-21 14:32:00 -0400

    feat: use sse for workspace page (#4122)
    * added error handling
    
    * workspace machine cleanup
    
    * renaming callback
    
    * general cleanup
    
    * fixed tests
    
    * PR comments

--- site/src/pages/WorkspacePage/WorkspacePage.tsx
+++ site/src/pages/WorkspacePage/WorkspacePage.tsx
@@ -40,9 +40,8 @@ export const WorkspacePage: FC = () => {
     workspace,
     getWorkspaceError,
     template,
-    refreshTemplateError,
-    resources,
-    getResourcesError,
+    refreshTemplateWarning,
+    refreshWorkspaceWarning,
     builds,
     getBuildsError,
     permissions,
@@ -70,7 +69,7 @@ export const WorkspacePage: FC = () => {
     return (
       <div className={styles.error}>
         {Boolean(getWorkspaceError) && <ErrorSummary error={getWorkspaceError} />}
-        {Boolean(refreshTemplateError) && <ErrorSummary error={refreshTemplateError} />}
+        {Boolean(refreshTemplateWarning) && <ErrorSummary error={refreshTemplateWarning} />}
         {Boolean(checkPermissionsError) && <ErrorSummary error={checkPermissionsError} />}
       </div>
     )
@@ -128,11 +127,11 @@ export const WorkspacePage: FC = () => {
           handleDelete={() => workspaceSend("ASK_DELETE")}
           handleUpdate={() => workspaceSend("UPDATE")}
           handleCancel={() => workspaceSend("CANCEL")}
-          resources={resources}
+          resources={workspace.latest_build.resources}
           builds={builds}
           canUpdateWorkspace={canUpdateWorkspace}
           workspaceErrors={{
-            [WorkspaceErrors.GET_RESOURCES_ERROR]: getResourcesError,
+            [WorkspaceErrors.GET_RESOURCES_ERROR]: refreshWorkspaceWarning,
             [WorkspaceErrors.GET_BUILDS_ERROR]: getBuildsError,
             [WorkspaceErrors.BUILD_ERROR]: buildError,
             [WorkspaceErrors.CANCELLATION_ERROR]: cancellationError,
