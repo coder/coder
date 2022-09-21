b5927e866bc37aaba92af8f86f205de6270a29ed
Ben <me@bpmct.net>
2022-09-21 03:11:05 +0000

    whoops

--- site/src/components/Markdown/Markdown.tsx
+++ site/src/components/Markdown/Markdown.tsx
@@ -33,7 +33,6 @@ export const Markdown: FC<{ children: string }> = ({ children }) => {
         // Adding node so the ...props don't have it
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         code: ({ node, inline, className, children, ...props }) => {
-          console.log("proppo", props)
           const match = /language-(\w+)/.exec(className || "")
           return !inline && match ? (
             <SyntaxHighlighter
