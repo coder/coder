0fbd2514799173bb6323feb6a30d4650cd87384c
Garrett <garrett@coder.com>
2022-09-23 16:40:19 +0000

    fix migration number

--- coderd/database/migrations/000051_workspace_app_health.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TYPE workspace_app_health AS ENUM ('disabled', 'initializing', 'healthy', 'unhealthy');
-
-ALTER TABLE ONLY workspace_apps
-	ADD COLUMN IF NOT EXISTS healthcheck_url text NOT NULL DEFAULT '',
-	ADD COLUMN IF NOT EXISTS healthcheck_interval int NOT NULL DEFAULT 0,
-	ADD COLUMN IF NOT EXISTS healthcheck_threshold int NOT NULL DEFAULT 0,
-    ADD COLUMN IF NOT EXISTS health workspace_app_health NOT NULL DEFAULT 'disabled';
