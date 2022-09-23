d304f641820f0d20ad1b3aae1689fdf0b8532476
Garrett <garrett@coder.com>
2022-09-23 17:24:38 +0000

    simplify goroutines

--- agent/apphealth.go
+++ agent/apphealth.go
@@ -56,77 +56,64 @@ func NewWorkspaceAppHealthReporter(logger slog.Logger, workspaceAgentApps Worksp
 				}
 
 				// run a ticker for each app health check.
-				tickers := make(map[string]*time.Ticker)
-				tickerC := make(chan codersdk.WorkspaceApp)
+				var mu sync.RWMutex
+				failures := make(map[string]int, 0)
 				for _, app := range apps {
 					if shouldStartTicker(app) {
 						t := time.NewTicker(time.Duration(app.Healthcheck.Interval) * time.Second)
-						tickers[app.Name] = t
 						go func() {
 							for {
 								select {
 								case <-ctx.Done():
 									return
 								case <-t.C:
-									tickerC <- app
+									// we set the http timeout to the healthcheck interval to prevent getting too backed up.
+									client := &http.Client{
+										Timeout: time.Duration(app.Healthcheck.Interval) * time.Second,
+									}
+									err := func() error {
+										req, err := http.NewRequestWithContext(ctx, http.MethodGet, app.Healthcheck.URL, nil)
+										if err != nil {
+											return err
+										}
+										res, err := client.Do(req)
+										if err != nil {
+											return err
+										}
+										// successful healthcheck is a non-5XX status code
+										res.Body.Close()
+										if res.StatusCode >= http.StatusInternalServerError {
+											return xerrors.Errorf("error status code: %d", res.StatusCode)
+										}
+
+										return nil
+									}()
+									if err != nil {
+										mu.Lock()
+										if failures[app.Name] < int(app.Healthcheck.Threshold) {
+											// increment the failure count and keep status the same.
+											// we will change it when we hit the threshold.
+											failures[app.Name]++
+										} else {
+											// set to unhealthy if we hit the failure threshold.
+											// we stop incrementing at the threshold to prevent the failure value from increasing forever.
+											health[app.Name] = codersdk.WorkspaceAppHealthUnhealthy
+										}
+										mu.Unlock()
+									} else {
+										mu.Lock()
+										// we only need one successful health check to be considered healthy.
+										health[app.Name] = codersdk.WorkspaceAppHealthHealthy
+										failures[app.Name] = 0
+										mu.Unlock()
+									}
+
+									t.Reset(time.Duration(app.Healthcheck.Interval))
 								}
 							}
 						}()
 					}
 				}
-				var mu sync.RWMutex
-				failures := make(map[string]int, 0)
-				go func() {
-					for {
-						select {
-						case <-ctx.Done():
-							return
-						case app := <-tickerC:
-							// we set the http timeout to the healthcheck interval to prevent getting too backed up.
-							client := &http.Client{
-								Timeout: time.Duration(app.Healthcheck.Interval) * time.Second,
-							}
-							err := func() error {
-								req, err := http.NewRequestWithContext(ctx, http.MethodGet, app.Healthcheck.URL, nil)
-								if err != nil {
-									return err
-								}
-								res, err := client.Do(req)
-								if err != nil {
-									return err
-								}
-								// successful healthcheck is a non-5XX status code
-								res.Body.Close()
-								if res.StatusCode >= http.StatusInternalServerError {
-									return xerrors.Errorf("error status code: %d", res.StatusCode)
-								}
-
-								return nil
-							}()
-							if err != nil {
-								mu.Lock()
-								if failures[app.Name] < int(app.Healthcheck.Threshold) {
-									// increment the failure count and keep status the same.
-									// we will change it when we hit the threshold.
-									failures[app.Name]++
-								} else {
-									// set to unhealthy if we hit the failure threshold.
-									// we stop incrementing at the threshold to prevent the failure value from increasing forever.
-									health[app.Name] = codersdk.WorkspaceAppHealthUnhealthy
-								}
-								mu.Unlock()
-							} else {
-								mu.Lock()
-								// we only need one successful health check to be considered healthy.
-								health[app.Name] = codersdk.WorkspaceAppHealthHealthy
-								failures[app.Name] = 0
-								mu.Unlock()
-							}
-
-							tickers[app.Name].Reset(time.Duration(app.Healthcheck.Interval))
-						}
-					}
-				}()
 
 				mu.Lock()
 				lastHealth := copyHealth(health)
