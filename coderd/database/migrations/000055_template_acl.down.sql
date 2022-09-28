200ea81155a4f882d196c748cdd4500389dc6a12
Jon Ayers <jon@coder.com>
2022-09-28 02:16:49 +0000

    fix migration (again)

--- coderd/database/migrations/000055_template_acl.down.sql
+++ coderd/database/migrations/000055_template_acl.down.sql
@@ -1,10 +1,10 @@
 BEGIN;
 
-ALTER TABLE templates DROP COLUMN user_acl;
-ALTER TABLE templates DROP COLUMN group_acl;
-DROP TYPE template_role;
 
-DROP TABLE groups;
 DROP TABLE group_members;
+DROP TABLE groups;
+DROP TYPE template_role;
+ALTER TABLE templates DROP COLUMN group_acl;
+ALTER TABLE templates DROP COLUMN user_acl;
 
 COMMIT;
