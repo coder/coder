4c30857cb25fc08b11946a4ab95a8d524649f36a
Garrett <garrett@coder.com>
2022-09-29 18:47:54 +0000

    fix xstate merge

--- site/src/xServices/createWorkspace/createWorkspaceXService.ts
+++ site/src/xServices/createWorkspace/createWorkspaceXService.ts
@@ -105,7 +105,7 @@ export const createWorkspaceMachine = createMachine(
           id: "checkPermissions",
           onDone: {
             actions: ["assignPermissions"],
-            target: "fillingParams",
+            target: "gettingWorkspaceQuota",
           },
           onError: {
             actions: ["assignCheckPermissionsError"],
