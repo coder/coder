5b13e652de93a5cc00ae70e2c18f5d07bd355c06
Ammar Bandukwala <ammar@ammar.io>
2022-10-01 21:54:05 +0000

    examples: support both localhost and 127.0.0.1 in Docker examples
    And some minor fixes

--- examples/templates/docker/main.tf
+++ examples/templates/docker/main.tf
@@ -83,7 +83,7 @@ resource "docker_container" "workspace" {
     "sh", "-c",
     <<EOT
     trap '[ $? -ne 0 ] && echo === Agent script exited with non-zero code. Sleeping infinitely to preserve logs... && sleep infinity' EXIT
-    ${replace(coder_agent.main.init_script, "localhost", "host.docker.internal")}
+    ${replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")}
     EOT
   ]
   env = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
