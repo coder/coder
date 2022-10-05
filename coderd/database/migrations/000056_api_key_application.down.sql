fd3617b8a2fd062b33ca8fc277061bc951c9d222
Garrett <garrett@coder.com>
2022-10-05 20:59:26 +0000

    fix down migration

--- coderd/database/migrations/000056_api_key_application.down.sql
+++ coderd/database/migrations/000056_api_key_application.down.sql
@@ -1,8 +1,2 @@
-CREATE TYPE old_login_type AS ENUM (
-    'password',
-    'github',
-    'oidc'
-);
-ALTER TABLE api_keys ALTER COLUMN login_type TYPE old_login_type USING (login_type::text::old_login_type);
-DROP TYPE login_type;
-ALTER TYPE old_login_type RENAME TO login_type;
+-- You cannot safely remove values from enums https://www.postgresql.org/docs/current/datatype-enum.html
+-- You cannot create a new type and do a rename because objects depend on this type now.
