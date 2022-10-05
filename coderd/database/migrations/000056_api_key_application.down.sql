cace329c41be0dbebda2cea04437fded9f37f040
Garrett <garrett@coder.com>
2022-10-05 20:33:06 +0000

    fmt

--- coderd/database/migrations/000056_api_key_application.down.sql
+++ coderd/database/migrations/000056_api_key_application.down.sql
@@ -1,7 +1,7 @@
 CREATE TYPE old_login_type AS ENUM (
     'password',
     'github',
-	'oidc'
+    'oidc'
 );
 ALTER TABLE api_keys ALTER COLUMN login_type TYPE old_login_type USING (login_type::text::old_login_type);
 DROP TYPE login_type;
