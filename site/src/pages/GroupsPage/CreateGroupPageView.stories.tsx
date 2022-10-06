20670f1cae6c8182f30a30cfca313e9748fd8650
Bruno Quaresma <bruno@coder.com>
2022-10-06 14:16:18 +0000

    Add CreateGroupPage stories

---
+++ site/src/pages/GroupsPage/CreateGroupPageView.stories.tsx
@@ -0,0 +1,14 @@
+import { Story } from "@storybook/react"
+import { CreateGroupPageView, CreateGroupPageViewProps } from "./CreateGroupPageView"
+
+export default {
+  title: "pages/CreateGroupPageView",
+  component: CreateGroupPageView,
+}
+
+const Template: Story<CreateGroupPageViewProps> = (args: CreateGroupPageViewProps) => (
+  <CreateGroupPageView {...args} />
+)
+
+export const Example = Template.bind({})
+Example.args = {}
