6573805f277af0385d88418cf89d47bcc39b0434
Joe Previte <jjprevite@gmail.com>
2022-10-06 21:03:09 +0000

    fix(site): remove eslint warning in <Markdown />

--- site/src/components/Markdown/Markdown.tsx
+++ site/src/components/Markdown/Markdown.tsx
@@ -30,8 +30,6 @@ export const Markdown: FC<{ children: string }> = ({ children }) => {
           </Link>
         ),
 
-        // Adding node so the ...props don't have it
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         code: ({ node, inline, className, children, ...props }) => {
           const match = /language-(\w+)/.exec(className || "")
           return !inline && match ? (
