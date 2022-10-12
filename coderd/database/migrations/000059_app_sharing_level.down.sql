9f458507436ec6250ceabcc1a50fbda93b34e85e
Dean Sheather <dean@deansheather.com>
2022-10-12 20:03:30 +0000

    chore: add share to all example templates

--- coderd/database/migrations/000059_app_sharing_level.down.sql
+++ coderd/database/migrations/000059_app_sharing_level.down.sql
@@ -1,5 +1,5 @@
--- Drop column share_level from workspace_apps
-ALTER TABLE workspace_apps DROP COLUMN share_level;
+-- Drop column sharing_level from workspace_apps
+ALTER TABLE workspace_apps DROP COLUMN sharing_level;
 
--- Drop type app_share_level
-DROP TYPE app_share_level;
+-- Drop type app_sharing_level
+DROP TYPE app_sharing_level;
