6ddb71f2c85759d07436a60ccf69124281a617e3
Steven Masley <stevenmasley@coder.com>
2022-10-20 00:02:51 -0500

    Do not run tests on windows

--- scripts/apitypings/main_test.go
+++ scripts/apitypings/main_test.go
@@ -1,3 +1,9 @@
+//go:build !windows
+// +build !windows
+
+// Windows tests fail because the \n\r vs \n. It's not worth trying
+// to replace newlines for os tests. If people start using this tool on windows
+// and are seeing problems, then we can add build tags and figure it out.
 package main
 
 import (
