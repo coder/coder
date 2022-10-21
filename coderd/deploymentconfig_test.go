dd2f4ff719ba74b3012e6254ff14e5a4f5a912e2
Garrett <garrett@coder.com>
2022-10-21 02:19:18 +0000

    add back test

--- coderd/deploymentconfig_test.go
+++ coderd/deploymentconfig_test.go
@@ -1,31 +1,46 @@
 package coderd_test
 
-// func TestDeploymentConfig(t *testing.T) {
-// 	t.Parallel()
-// 	hi := "hi"
-// 	ctx, cancel := context.WithTimeout(context.Background(), testutil.WaitLong)
-// 	defer cancel()
-// 	vip := deployment.NewViper()
-// 	cfg := deployment.Config(vip)
-// 	// values should be returned
-// 	cfg.AccessURL.Value = hi
-// 	// values should not be returned
-// 	cfg.OAuth2GithubClientSecret.Value = hi
-// 	cfg.OIDCClientSecret.Value = hi
-// 	cfg.PostgresURL.Value = hi
-// 	cfg.SCIMAuthHeader.Value = hi
+import (
+	"context"
+	"testing"
 
-// 	client := coderdtest.New(t, &coderdtest.Options{
-// 		DeploymentConfig: &cfg,
-// 	})
-// 	_ = coderdtest.CreateFirstUser(t, client)
-// 	scrubbed, err := client.DeploymentConfig(ctx)
-// 	require.NoError(t, err)
-// 	// ensure normal values pass through
-// 	require.EqualValues(t, hi, scrubbed.AccessURL.Value)
-// 	// ensure secrets are removed
-// 	require.Empty(t, scrubbed.OAuth2GithubClientSecret.Value)
-// 	require.Empty(t, scrubbed.OIDCClientSecret.Value)
-// 	require.Empty(t, scrubbed.PostgresURL.Value)
-// 	require.Empty(t, scrubbed.SCIMAuthHeader.Value)
-// }
+	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/require"
+
+	"github.com/coder/coder/cli/deployment"
+	"github.com/coder/coder/coderd/coderdtest"
+	"github.com/coder/coder/testutil"
+)
+
+func TestDeploymentConfig(t *testing.T) {
+	t.Parallel()
+	hi := "hi"
+	ctx, cancel := context.WithTimeout(context.Background(), testutil.WaitLong)
+	defer cancel()
+	vip := deployment.NewViper()
+	fs := pflag.NewFlagSet("test", pflag.ContinueOnError)
+	fs.String("global-config", hi, "usage")
+	cfg, err := deployment.Config(fs, vip, false)
+	require.NoError(t, err)
+	// values should be returned
+	cfg.AccessURL.Value = hi
+	// values should not be returned
+	cfg.OAuth2GithubClientSecret.Value = hi
+	cfg.OIDCClientSecret.Value = hi
+	cfg.PostgresURL.Value = hi
+	cfg.SCIMAuthHeader.Value = hi
+
+	client := coderdtest.New(t, &coderdtest.Options{
+		DeploymentConfig: &cfg,
+	})
+	_ = coderdtest.CreateFirstUser(t, client)
+	scrubbed, err := client.DeploymentConfig(ctx)
+	require.NoError(t, err)
+	// ensure normal values pass through
+	require.EqualValues(t, hi, scrubbed.AccessURL.Value)
+	// ensure secrets are removed
+	require.Empty(t, scrubbed.OAuth2GithubClientSecret.Value)
+	require.Empty(t, scrubbed.OIDCClientSecret.Value)
+	require.Empty(t, scrubbed.PostgresURL.Value)
+	require.Empty(t, scrubbed.SCIMAuthHeader.Value)
+}
