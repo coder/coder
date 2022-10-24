bb494dce6a1d11e69e71a8ebd361f7754fb670c8
Garrett <garrett@coder.com>
2022-10-24 15:40:03 +0000

    feat: support nested structs and arrays in config

--- codersdk/deploymentconfig.go
+++ codersdk/deploymentconfig.go
@@ -12,59 +12,97 @@ import (
 // DeploymentConfig is the central configuration for the coder server.
 // Secret values should specify `json:"-"` to prevent them from being returned by the API.
 type DeploymentConfig struct {
-	AccessURL                     DeploymentConfigField[string]        `json:"access_url"`
-	WildcardAccessURL             DeploymentConfigField[string]        `json:"wildcard_access_url"`
-	Address                       DeploymentConfigField[string]        `json:"address"`
-	AutobuildPollInterval         DeploymentConfigField[time.Duration] `json:"autobuild_poll_interval"`
-	DERPServerEnable              DeploymentConfigField[bool]          `json:"derp_server_enabled"`
-	DERPServerRegionID            DeploymentConfigField[int]           `json:"derp_server_region_id"`
-	DERPServerRegionCode          DeploymentConfigField[string]        `json:"derp_server_region_code"`
-	DERPServerRegionName          DeploymentConfigField[string]        `json:"derp_server_region_name"`
-	DERPServerSTUNAddresses       DeploymentConfigField[[]string]      `json:"derp_server_stun_address"`
-	DERPServerRelayURL            DeploymentConfigField[string]        `json:"derp_server_relay_address"`
-	DERPConfigURL                 DeploymentConfigField[string]        `json:"derp_config_url"`
-	DERPConfigPath                DeploymentConfigField[string]        `json:"derp_config_path"`
-	PrometheusEnable              DeploymentConfigField[bool]          `json:"prometheus_enabled"`
-	PrometheusAddress             DeploymentConfigField[string]        `json:"prometheus_address"`
-	PprofEnable                   DeploymentConfigField[bool]          `json:"pprof_enabled"`
-	PprofAddress                  DeploymentConfigField[string]        `json:"pprof_address"`
-	ProxyTrustedHeaders           DeploymentConfigField[[]string]      `json:"proxy_trusted_headers"`
-	ProxyTrustedOrigins           DeploymentConfigField[[]string]      `json:"proxy_trusted_origins"`
-	CacheDirectory                DeploymentConfigField[string]        `json:"cache_directory"`
-	InMemoryDatabase              DeploymentConfigField[bool]          `json:"in_memory_database"`
-	ProvisionerDaemons            DeploymentConfigField[int]           `json:"provisioner_daemon_count"`
-	PostgresURL                   DeploymentConfigField[string]        `json:"-"`
-	OAuth2GithubClientID          DeploymentConfigField[string]        `json:"oauth2_github_client_id"`
-	OAuth2GithubClientSecret      DeploymentConfigField[string]        `json:"-"`
-	OAuth2GithubAllowedOrgs       DeploymentConfigField[[]string]      `json:"oauth2_github_allowed_orgs"`
-	OAuth2GithubAllowedTeams      DeploymentConfigField[[]string]      `json:"oauth2_github_allowed_teams"`
-	OAuth2GithubAllowSignups      DeploymentConfigField[bool]          `json:"oauth2_github_allow_signups"`
-	OAuth2GithubEnterpriseBaseURL DeploymentConfigField[string]        `json:"oauth2_github_enterprise_base_url"`
-	OIDCAllowSignups              DeploymentConfigField[bool]          `json:"oidc_allow_signups"`
-	OIDCClientID                  DeploymentConfigField[string]        `json:"oidc_client_id"`
-	OIDCClientSecret              DeploymentConfigField[string]        `json:"-"`
-	OIDCEmailDomain               DeploymentConfigField[string]        `json:"oidc_email_domain"`
-	OIDCIssuerURL                 DeploymentConfigField[string]        `json:"oidc_issuer_url"`
-	OIDCScopes                    DeploymentConfigField[[]string]      `json:"oidc_scopes"`
-	TelemetryEnable               DeploymentConfigField[bool]          `json:"telemetry_enable"`
-	TelemetryTrace                DeploymentConfigField[bool]          `json:"telemetry_trace_enable"`
-	TelemetryURL                  DeploymentConfigField[string]        `json:"telemetry_url"`
-	TLSEnable                     DeploymentConfigField[bool]          `json:"tls_enable"`
-	TLSCertFiles                  DeploymentConfigField[[]string]      `json:"tls_cert_files"`
-	TLSClientCAFile               DeploymentConfigField[string]        `json:"tls_client_ca_file"`
-	TLSClientAuth                 DeploymentConfigField[string]        `json:"tls_client_auth"`
-	TLSKeyFiles                   DeploymentConfigField[[]string]      `json:"tls_key_files"`
-	TLSMinVersion                 DeploymentConfigField[string]        `json:"tls_min_version"`
-	TraceEnable                   DeploymentConfigField[bool]          `json:"trace_enable"`
-	SecureAuthCookie              DeploymentConfigField[bool]          `json:"secure_auth_cookie"`
-	SSHKeygenAlgorithm            DeploymentConfigField[string]        `json:"ssh_keygen_algorithm"`
-	AutoImportTemplates           DeploymentConfigField[[]string]      `json:"auto_import_templates"`
-	MetricsCacheRefreshInterval   DeploymentConfigField[time.Duration] `json:"metrics_cache_refresh_interval"`
-	AgentStatRefreshInterval      DeploymentConfigField[time.Duration] `json:"agent_stat_refresh_interval"`
-	AuditLogging                  DeploymentConfigField[bool]          `json:"audit_logging"`
-	BrowserOnly                   DeploymentConfigField[bool]          `json:"browser_only"`
-	SCIMAPIKey                    DeploymentConfigField[string]        `json:"-"`
-	UserWorkspaceQuota            DeploymentConfigField[int]           `json:"user_workspace_quota"`
+	AccessURL                   DeploymentConfigField[string]        `json:"access_url"`
+	WildcardAccessURL           DeploymentConfigField[string]        `json:"wildcard_access_url"`
+	Address                     DeploymentConfigField[string]        `json:"address"`
+	AutobuildPollInterval       DeploymentConfigField[time.Duration] `json:"autobuild_poll_interval"`
+	DERP                        DERP                                 `json:"derp"`
+	Prometheus                  PrometheusConfig                     `json:"prometheus"`
+	Pprof                       PprofConfig                          `json:"pprof"`
+	CacheDirectory              DeploymentConfigField[string]        `json:"cache_directory"`
+	InMemoryDatabase            DeploymentConfigField[bool]          `json:"in_memory_database"`
+	ProvisionerDaemons          DeploymentConfigField[int]           `json:"provisioner_daemon_count"`
+	PostgresURL                 DeploymentConfigField[string]        `json:"pg_connection_url"`
+	OAuth2                      OAuth2Config                         `json:"oauth2"`
+	OIDC                        OIDCConfig                           `json:"oidc"`
+	Telemetry                   TelemetryConfig                      `json:"telemetry"`
+	TLS                         TLSConfig                            `json:"tls"`
+	TraceEnable                 DeploymentConfigField[bool]          `json:"trace_enable"`
+	SecureAuthCookie            DeploymentConfigField[bool]          `json:"secure_auth_cookie"`
+	SSHKeygenAlgorithm          DeploymentConfigField[string]        `json:"ssh_keygen_algorithm"`
+	AutoImportTemplates         DeploymentConfigField[[]string]      `json:"auto_import_templates"`
+	MetricsCacheRefreshInterval DeploymentConfigField[time.Duration] `json:"metrics_cache_refresh_interval"`
+	AgentStatRefreshInterval    DeploymentConfigField[time.Duration] `json:"agent_stat_refresh_interval"`
+	AuditLogging                DeploymentConfigField[bool]          `json:"audit_logging"`
+	BrowserOnly                 DeploymentConfigField[bool]          `json:"browser_only"`
+	SCIMAPIKey                  DeploymentConfigField[string]        `json:"scim_api_key"`
+	UserWorkspaceQuota          DeploymentConfigField[int]           `json:"user_workspace_quota"`
+}
+
+type DERP struct {
+	Server DERPServerConfig `json:"server"`
+	Config DERPConfig       `json:"config"`
+}
+
+type DERPServerConfig struct {
+	Enable        DeploymentConfigField[bool]     `json:"enabled"`
+	RegionID      DeploymentConfigField[int]      `json:"region_id"`
+	RegionCode    DeploymentConfigField[string]   `json:"region_code"`
+	RegionName    DeploymentConfigField[string]   `json:"region_name"`
+	STUNAddresses DeploymentConfigField[[]string] `json:"stun_address"`
+	RelayAddress  DeploymentConfigField[string]   `json:"relay_address"`
+}
+
+type DERPConfig struct {
+	URL  DeploymentConfigField[string] `json:"url"`
+	Path DeploymentConfigField[string] `json:"path"`
+}
+
+type PrometheusConfig struct {
+	Enable  DeploymentConfigField[bool]   `json:"enabled"`
+	Address DeploymentConfigField[string] `json:"address"`
+}
+
+type PprofConfig struct {
+	Enable  DeploymentConfigField[bool]   `json:"enabled"`
+	Address DeploymentConfigField[string] `json:"address"`
+}
+
+type OAuth2Config struct {
+	Github OAuth2GithubConfig `json:"github"`
+}
+
+type OAuth2GithubConfig struct {
+	ClientID             DeploymentConfigField[string]   `json:"client_id"`
+	ClientSecret         DeploymentConfigField[string]   `json:"client_secret"`
+	AllowedOrganizations DeploymentConfigField[[]string] `json:"allowed_organizations"`
+	AllowedTeams         DeploymentConfigField[[]string] `json:"allowed_teams"`
+	AllowSignups         DeploymentConfigField[bool]     `json:"allow_signups"`
+	EnterpriseBaseURL    DeploymentConfigField[string]   `json:"enterprise_base_url"`
+}
+
+type OIDCConfig struct {
+	AllowSignups DeploymentConfigField[bool]     `json:"allow_signups"`
+	ClientID     DeploymentConfigField[string]   `json:"client_id"`
+	ClientSecret DeploymentConfigField[string]   `json:"client_secret"`
+	EmailDomain  DeploymentConfigField[string]   `json:"email_domain"`
+	IssuerURL    DeploymentConfigField[string]   `json:"issuer_url"`
+	Scopes       DeploymentConfigField[[]string] `json:"scopes"`
+}
+
+type TelemetryConfig struct {
+	Enable DeploymentConfigField[bool]   `json:"enabled"`
+	Trace  DeploymentConfigField[bool]   `json:"trace"`
+	URL    DeploymentConfigField[string] `json:"url"`
+}
+
+type TLSConfig struct {
+	Enable       DeploymentConfigField[bool]     `json:"enable"`
+	CertFiles    DeploymentConfigField[[]string] `json:"cert_files"`
+	ClientAuth   DeploymentConfigField[string]   `json:"client_auth"`
+	ClientCAFile DeploymentConfigField[string]   `json:"client_ca_file"`
+	KeyFiles     DeploymentConfigField[[]string] `json:"key_files"`
+	MinVersion   DeploymentConfigField[string]   `json:"min_version"`
 }
 
 type Flaggable interface {
@@ -72,8 +110,7 @@ type Flaggable interface {
 }
 
 type DeploymentConfigField[T Flaggable] struct {
-	Key string `json:"key"`
-	// Name appears in the deployment UI.
+	// Key        string `json:"key"`
 	Name       string `json:"name"`
 	Usage      string `json:"usage"`
 	Flag       string `json:"flag"`
