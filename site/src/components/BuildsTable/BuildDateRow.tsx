19eb0cb6159ada87c4b031974131de14d36f27f5
Bruno Quaresma <bruno@coder.com>
2022-10-24 21:49:32 -0300

    Update site/src/components/BuildsTable/BuildDateRow.tsx
    Co-authored-by: Kira Pilot <kira@coder.com>

--- site/src/components/BuildsTable/BuildDateRow.tsx
+++ site/src/components/BuildsTable/BuildDateRow.tsx
@@ -2,6 +2,7 @@ import { makeStyles } from "@material-ui/core/styles"
 import TableCell from "@material-ui/core/TableCell"
 import TableRow from "@material-ui/core/TableRow"
 import formatRelative from "date-fns/formatRelative"
+import { FC } from "react"
 
 export interface BuildDateRow {
   date: Date
