3c5e292c5aa65ba3a417f1577ec16663e034f41d
Kira Pilot <kira@coder.com>
2022-10-25 15:34:48 -0400

    feat: add workspace build start/stop to audit log (#4744)
    * adding workspace_build resource
    
    * added migration
    
    * fix keyword
    
    * got rid oof diffs for workspace builds
    
    * adding workspace name to string
    
    * renamed migrations
    
    * fixed lint
    
    * pass throough AdditionalFields and fix tests
    
    * no need to pass through each handler
    
    * cleaned up migrations
    
    * generated types; fixed missing cases
    
    * logging error

--- site/src/api/typesGenerated.ts
+++ site/src/api/typesGenerated.ts
@@ -915,7 +915,7 @@ export interface WorkspacesRequest extends Pagination {
 export type APIKeyScope = "all" | "application_connect"
 
 // From codersdk/audit.go
-export type AuditAction = "create" | "delete" | "write"
+export type AuditAction = "create" | "delete" | "start" | "stop" | "write"
 
 // From codersdk/workspacebuilds.go
 export type BuildReason = "autostart" | "autostop" | "initiator"
@@ -975,6 +975,7 @@ export type ResourceType =
   | "template_version"
   | "user"
   | "workspace"
+  | "workspace_build"
 
 // From codersdk/sse.go
 export type ServerSentEventType = "data" | "error" | "ping"
