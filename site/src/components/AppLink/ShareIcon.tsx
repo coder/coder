6c5cfd555154df540aab5a1abaeb346decdcd786
Bruno Quaresma <bruno@coder.com>
2022-10-27 20:03:41 +0000

    Add translation

--- site/src/components/AppLink/ShareIcon.tsx
+++ site/src/components/AppLink/ShareIcon.tsx
@@ -4,21 +4,24 @@ import GroupOutlinedIcon from "@material-ui/icons/GroupOutlined"
 import { FC } from "react"
 import * as TypesGen from "../../api/typesGenerated"
 import Tooltip from "@material-ui/core/Tooltip"
+import { useTranslation } from "react-i18next"
 
 export interface ShareIconProps {
   app: TypesGen.WorkspaceApp
 }
 
 export const ShareIcon: FC<ShareIconProps> = ({ app }) => {
+  const { t } = useTranslation("agent")
+
   let shareIcon = <LockOutlinedIcon />
-  let shareTooltip = "Private, only accessible by you"
+  let shareTooltip = t("shareTooltip.private")
   if (app.sharing_level === "authenticated") {
     shareIcon = <GroupOutlinedIcon />
-    shareTooltip = "Shared with all authenticated users"
+    shareTooltip = t("shareTooltip.authenticated")
   }
   if (app.sharing_level === "public") {
     shareIcon = <PublicOutlinedIcon />
-    shareTooltip = "Shared publicly"
+    shareTooltip = t("shareTooltip.public")
   }
 
   return <Tooltip title={shareTooltip}>{shareIcon}</Tooltip>
