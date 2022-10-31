64ad0f5b00371de6d87a3e892b38de13be4e0b85
Mathias Fredriksson <mafredri@gmail.com>
2022-10-31 18:13:37 +0200

    Fix edge case where update check has never succeeded

--- coderd/updatecheck.go
+++ coderd/updatecheck.go
@@ -1,9 +1,11 @@
 package coderd
 
 import (
+	"database/sql"
 	"net/http"
 
 	"golang.org/x/mod/semver"
+	"golang.org/x/xerrors"
 
 	"github.com/coder/coder/buildinfo"
 	"github.com/coder/coder/coderd/httpapi"
@@ -13,19 +15,28 @@ import (
 func (api *API) updateCheck(rw http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
+	currentVersion := codersdk.UpdateCheckResponse{
+		Current: true,
+		Version: buildinfo.Version(),
+		URL:     buildinfo.ExternalURL(),
+	}
+
 	if api.updateChecker == nil {
 		// If update checking is disabled, echo the current
 		// version.
-		httpapi.Write(ctx, rw, http.StatusOK, codersdk.UpdateCheckResponse{
-			Current: true,
-			Version: buildinfo.Version(),
-			URL:     buildinfo.ExternalURL(),
-		})
+		httpapi.Write(ctx, rw, http.StatusOK, currentVersion)
 		return
 	}
 
 	uc, err := api.updateChecker.Latest(ctx)
 	if err != nil {
+		if xerrors.Is(err, sql.ErrNoRows) {
+			// Update checking is enabled, but has never
+			// succeeded, reproduce behavior as if disabled.
+			httpapi.Write(ctx, rw, http.StatusOK, currentVersion)
+			return
+		}
+
 		httpapi.InternalServerError(rw, err)
 		return
 	}
