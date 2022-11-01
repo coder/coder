a930cf42b409754a5f87d40b72dfc2ba9ac8fcb5
Colin Adler <colin1adler@gmail.com>
2022-11-01 11:35:14 -0500

    fix: templates created_by migration (#4838)

--- coderd/database/migrations/000068_update_template_version_created_by.up.sql
+++ coderd/database/migrations/000068_update_template_version_created_by.up.sql
@@ -1,4 +1,6 @@
 BEGIN;
-	ALTER TABLE template_versions ALTER COLUMN created_by SET NOT NULL;
-	UPDATE template_versions SET created_by = '00000000-0000-0000-0000-000000000000'::uuid WHERE created_by IS NULL;
+
+UPDATE template_versions SET created_by = '00000000-0000-0000-0000-000000000000'::uuid WHERE created_by IS NULL;
+ALTER TABLE template_versions ALTER COLUMN created_by SET NOT NULL;
+
 COMMIT;
