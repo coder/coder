68b0eb14c7cf8461caf329a5b65e2946656a45e4
Mathias Fredriksson <mafredri@gmail.com>
2022-11-04 20:01:34 +0200

    Fix fixture

--- coderd/database/migrations/testdata/fixtures/000024_example.up.sql
+++ coderd/database/migrations/testdata/fixtures/000024_example.up.sql
@@ -1,6 +1,6 @@
 -- Example:
--- This migration is applied after migrations/000024_site_config.up.sql
+-- This fixture is applied after migrations/000024_site_config.up.sql
 -- and inserts a value into site_configs that must not cause issues in
 -- future migrations.
 
-INSERT INTO site_configs(key, value) VALUES ('mytest', 'example');
+INSERT INTO site_configs (key, value) VALUES ('mytest', 'example');
