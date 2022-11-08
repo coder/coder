b2a16d46c692d3ceaeaba61155bdf72227f6147e
Mathias Fredriksson <mafredri@gmail.com>
2022-11-08 18:52:19 +0200

    chore: Unify Docker terraform templates (#4952)
    Changes to terraform templates:
    
    * Remove DNS (this can interfere with users running their own DNS
      servers)
    * Remove `lower()` restriction from hostnames so that it will show the
      name set by the user, as-is (there is no restriction on upper case
      letters in hostnames)
    * Remove superfluous `trap` in entrypoints, this is already handled by
      the init script
    * Switch from `command` to `entrypoint` as the latter can support more
      Docker images out-of-the-box

--- examples/templates/docker/main.tf
+++ examples/templates/docker/main.tf
@@ -80,17 +80,10 @@ resource "docker_container" "workspace" {
   # Uses lower() to avoid Docker restriction on container names.
   name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
   # Hostname makes the shell more user friendly: coder@my-workspace:~$
-  hostname = lower(data.coder_workspace.me.name)
-  dns      = ["1.1.1.1"]
+  hostname = data.coder_workspace.me.name
   # Use the docker gateway if the access URL is 127.0.0.1
-  command = [
-    "sh", "-c",
-    <<EOT
-    trap '[ $? -ne 0 ] && echo === Agent script exited with non-zero code. Sleeping infinitely to preserve logs... && sleep infinity' EXIT
-    ${replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")}
-    EOT
-  ]
-  env = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
+  entrypoint = ["sh", "-c", replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")]
+  env        = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
   host {
     host = "host.docker.internal"
     ip   = "host-gateway"
