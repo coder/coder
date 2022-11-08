86dade1216843db898acb0c665ca761038b45e0f
Presley Pizzo <presley@coder.com>
2022-11-08 20:54:42 +0000

    Unpack workspaces response in load tests

--- loadtest/workspacebuild/run_test.go
+++ loadtest/workspacebuild/run_test.go
@@ -107,12 +107,13 @@ func Test_Runner(t *testing.T) {
 		go func() {
 			var workspace codersdk.Workspace
 			for {
-				workspaces, err := client.Workspaces(ctx, codersdk.WorkspaceFilter{
+				res, err := client.Workspaces(ctx, codersdk.WorkspaceFilter{
 					Owner: codersdk.Me,
 				})
 				if !assert.NoError(t, err) {
 					return
 				}
+				workspaces := res.Workspaces
 
 				if len(workspaces) == 1 {
 					workspace = workspaces[0]
@@ -165,10 +166,11 @@ func Test_Runner(t *testing.T) {
 		require.Contains(t, logsStr, `"agent3" is connected`)
 
 		// Find the workspace.
-		workspaces, err := client.Workspaces(ctx, codersdk.WorkspaceFilter{
+		res, err := client.Workspaces(ctx, codersdk.WorkspaceFilter{
 			Owner: codersdk.Me,
 		})
 		require.NoError(t, err)
+		workspaces := res.Workspaces
 		require.Len(t, workspaces, 1)
 
 		coderdtest.AwaitWorkspaceBuildJob(t, client, workspaces[0].LatestBuild.ID)
