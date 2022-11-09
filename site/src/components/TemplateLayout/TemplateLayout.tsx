7861bdfe9d53840ee5585a84da0a11dff42951eb
Kira Pilot <kira.pilot23@gmail.com>
2022-11-09 16:55:57 +0000

    removed button

--- site/src/components/TemplateLayout/TemplateLayout.tsx
+++ site/src/components/TemplateLayout/TemplateLayout.tsx
@@ -5,10 +5,7 @@ import { makeStyles } from "@material-ui/core/styles"
 import AddCircleOutline from "@material-ui/icons/AddCircleOutline"
 import SettingsOutlined from "@material-ui/icons/SettingsOutlined"
 import { useMachine, useSelector } from "@xstate/react"
-import { ChooseOne, Cond } from "components/Conditionals/ChooseOne"
 import { DeleteDialog } from "components/Dialogs/DeleteDialog/DeleteDialog"
-import { DeleteButton } from "components/DropdownButton/ActionCtas"
-import { DropdownButton } from "components/DropdownButton/DropdownButton"
 import {
   PageHeader,
   PageHeaderSubtitle,
@@ -36,8 +33,8 @@ import {
   TemplateContext,
   templateMachine,
 } from "xServices/template/templateXService"
-import { Margins } from "../../components/Margins/Margins"
-import { Stack } from "../../components/Stack/Stack"
+import { Margins } from "components/Margins/Margins"
+import { Stack } from "components/Stack/Stack"
 import { Permissions } from "xServices/auth/authXService"
 import { Loader } from "components/Loader/Loader"
 
@@ -76,6 +73,35 @@ export const useTemplateLayoutContext = (): TemplateLayoutContextValue => {
   return context
 }
 
+const TemplateSettingsButton: FC<{ templateName: string }> = ({
+  templateName,
+}) => (
+  <Link
+    underline="none"
+    component={RouterLink}
+    to={`/templates/${templateName}/settings`}
+  >
+    <Button variant="outlined" startIcon={<SettingsOutlined />}>
+      {Language.settingsButton}
+    </Button>
+  </Link>
+)
+
+const CreateWorkspaceButton: FC<{
+  templateName: string
+  className?: string
+}> = ({ templateName, className }) => (
+  <Link
+    underline="none"
+    component={RouterLink}
+    to={`/templates/${templateName}/workspace`}
+  >
+    <Button className={className ?? ""} startIcon={<AddCircleOutline />}>
+      {Language.createButton}
+    </Button>
+  </Link>
+)
+
 export const TemplateLayout: FC<PropsWithChildren> = ({ children }) => {
   const styles = useStyles()
   const organizationId = useOrganizationId()
@@ -109,24 +135,18 @@ export const TemplateLayout: FC<PropsWithChildren> = ({ children }) => {
 
   const hasIcon = template && template.icon && template.icon !== ""
 
-  const createWorkspaceButton = (className?: string) => (
-    <Link
-      underline="none"
-      component={RouterLink}
-      to={`/templates/${templateName}/workspace`}
-    >
-      <Button
-        className={className ?? ""}
-        startIcon={<AddCircleOutline />}
-        disabled={isLoading}
-      >
-        {Language.createButton}
-      </Button>
-    </Link>
-  )
+  const generatePageHeaderActions = (): JSX.Element[] => {
+    const pageActions: JSX.Element[] = []
 
-  const handleDeleteTemplate = () => {
-    templateSend("DELETE")
+    if (!isLoading && templatePermissions.canUpdateTemplate) {
+      pageActions.push(<TemplateSettingsButton templateName={templateName} />)
+    }
+
+    if (!isLoading) {
+      pageActions.push(<CreateWorkspaceButton templateName={templateName} />)
+    }
+
+    return pageActions
   }
 
   return (
@@ -134,36 +154,11 @@ export const TemplateLayout: FC<PropsWithChildren> = ({ children }) => {
       <Margins>
         <PageHeader
           actions={
-            isLoading ? undefined : (
-              <ChooseOne>
-                <Cond condition={templatePermissions.canUpdateTemplate}>
-                  <Link
-                    underline="none"
-                    component={RouterLink}
-                    to={`/templates/${template.name}/settings`}
-                  >
-                    <Button variant="outlined" startIcon={<SettingsOutlined />}>
-                      {Language.settingsButton}
-                    </Button>
-                  </Link>
-
-                  <DropdownButton
-                    primaryAction={createWorkspaceButton(styles.actionButton)}
-                    secondaryActions={[
-                      {
-                        action: "delete",
-                        button: (
-                          <DeleteButton handleAction={handleDeleteTemplate} />
-                        ),
-                      },
-                    ]}
-                    canCancel={false}
-                  />
-                </Cond>
-
-                <Cond>{createWorkspaceButton()}</Cond>
-              </ChooseOne>
-            )
+            <>
+              {generatePageHeaderActions().map((action, i) => (
+                <div key={i}>{action}</div>
+              ))}
+            </>
           }
         >
           <Stack direction="row" spacing={3} className={styles.pageTitle}>
@@ -255,10 +250,6 @@ export const TemplateLayout: FC<PropsWithChildren> = ({ children }) => {
 
 export const useStyles = makeStyles((theme) => {
   return {
-    actionButton: {
-      border: "none",
-      borderRadius: `${theme.shape.borderRadius}px 0px 0px ${theme.shape.borderRadius}px`,
-    },
     pageTitle: {
       alignItems: "center",
     },
