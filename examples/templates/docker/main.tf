570a1ffc2b3ff937792a4aeb53e8da0441503de5
Mathias Fredriksson <mafredri@gmail.com>
2022-11-10 12:55:39 +0200

    fix: Improve docker example first user experience (#4972)
    The base ubuntu image lands the user as root, but the terraform tempalte
    expected /home/coder to be used. This change adds a user with the same
    name as the Coder users username and allows them to sudo.

--- examples/templates/docker/main.tf
+++ examples/templates/docker/main.tf
@@ -11,6 +11,10 @@ terraform {
   }
 }
 
+locals {
+  username = data.coder_workspace.me.owner
+}
+
 data "coder_provisioner" "me" {
 }
 
@@ -46,7 +50,7 @@ resource "coder_app" "code-server" {
   agent_id     = coder_agent.main.id
   slug         = "code-server"
   display_name = "code-server"
-  url          = "http://localhost:13337/?folder=/home/coder"
+  url          = "http://localhost:13337/?folder=/home/${local.username}"
   icon         = "/icon/code.svg"
   subdomain    = false
   share        = "owner"
@@ -91,6 +95,9 @@ resource "docker_image" "main" {
   name = "coder-${data.coder_workspace.me.id}"
   build {
     path = "./build"
+    build_arg = {
+      USER = local.username
+    }
   }
   triggers = {
     dir_sha1 = sha1(join("", [for f in fileset(path.module, "build/*") : filesha1(f)]))
@@ -112,7 +119,7 @@ resource "docker_container" "workspace" {
     ip   = "host-gateway"
   }
   volumes {
-    container_path = "/home/coder/"
+    container_path = "/home/${local.username}"
     volume_name    = docker_volume.home_volume.name
     read_only      = false
   }
