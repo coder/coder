d5b5000adb8d1a0ecd418bb92c0e5ea8eee673da
Garrett <garrett@coder.com>
2022-11-14 19:07:07 +0000

    fmt

--- site/src/xServices/users/usersXService.ts
+++ site/src/xServices/users/usersXService.ts
@@ -106,254 +106,260 @@ export const usersMachine =
   /** @xstate-layout N4IgpgJg5mDOIC5QFdZgE6wMoBcCGOYAdLPujgJYB2UACnlNQRQPZUDEA2gAwC6ioAA4tYFSmwEgAnogCM3AJzciAZm4AmAKwAOAOwA2TQt0AWfQBoQAD0QBaWbIWyimzepPqtCpd1n6Avv6WqBjY+IREMDiUNACqaJjsEGzE1ABuLADWxFHxoTz8SCDCouJUkjIIsuoq+kRmmrreJtqertqWNgj2stoKRLq13upOspq+AUEgIZi4BDlg0dRQeYkY6CzoRIIANgQAZpsAtpGLq7AFkiVirOVFXT19A0MKIw7jfpaVDtra9YZNBQtNo6QLBBJheZECgQHZgdhYWJYWgAUQAcgARAD6SJRACVLkVrmVJA8HMYBkCFCptH4Rvp9CYvnI1P1qbJTL1DNx9NoVGDphC5hEYXD2BiUQAZFEAFRROKw+MJQhENwk9zs5N0lJM1Np+npjOZVRpKlUxl5HhMul+3G0ApmkJFsPhAEEAMIygCSADVXXKFUq+FdVSSNd0tTq9XSFAymdI5CZNHVtPpauoGWp9NT+VNHcLUi72HiUYqZYG8VjaK6sFgAOoAeTxGOVxVDt1Jmsc2qauppMbjxrGalUI2TSlkKnGoLzQvChbFsVoGP98txlbxDelWFbxI74Z6FN70YNsaNCaq+kcRDTThUfjPDnUDrnUNF8KXK4D1YA4ijd+26qgGS3ZRv2p6DheD5mvI4ymNwvY1LoL6hAW0JFp+q5YgAYl6kpygSwZEoBdzAV2R5UuBhrxt8Bh1I08h+GYnjpihszzkQADGbD7BQ6BHKsWCoIIYBUKIbDsO6DZorheIALIVliiLIuiWBetJAGlPuZERuo3AmEQfiaCoCipoMnj6UO2iuEQtqNE4SYqCYHJsU6xDcVQvH8YJwmieJHDuq6aLulKinKaiaJqRpREqlpQHWJqekGUZJlmSoFk0XISjqDeuieK81nNJOrloR5XkCQkGJgHCZSSdJskKeuWIStKcqaWqpEJbpHJEIoV5+LIznAkOVL-CY3A8o0SbOSVHFlXxFUYFVNW3JJQUhZKiktbK-4xW2cWdWS6g9X1DhXkNrQjbGRBaI0enqH0g3cJos1QvN3kJK6nGUGkzASVJMlevJiket6fretFhSxR1nbdcoagtBoxgTQomiZVUo1mONk2mGjsivRE72LegX0-X9AXraFTWg76-rqWi7Vhjp9jHfD+naEjugo2jV11LdeUaI940vbOqEcbAvlUBAyyrEkKTQlQGTZCQksQKsjPaVBPguKmg3UgyD0KJ0Xb6MovhDPZNRXgTxAS7AIlSzLCTsOsmzbHsOCHPxKv26JasJBr8UgabvWTtmlu1LIVluDr536wafQ20QEDVYsTsYHLVCpIrWTECnNVgOre17kHXa6JoN2xn4jKoy0Fha6YvVuIoHK6oxIvgmLUL52ncTO67Wy7AcxzJ6nhBF1D+0wweDjl5XV5xrXqbGkl2qaL03BOZOjiDUneDfRQv0xCszvJFnCtK8Q+9k+PAfFyRsM9IyN6DQ4NJ8r0k4jY3z16U4phOF8B3QUXcIjX0PswPuGcB7u2Ht7cBR9C530niXQ6JsDLnTfr8e8tIVAr30mvDeW97wOSTkTVY9BYCwAAO6bAgHiOAiw6qA2Bk1astZGzNixCWMsgc0ERl6P0deKNrSfw8OoFeYdbLqHLrGWkHwFBkJ4gtCheAqG0PQPQxhOA1rBSpoqSs7D6xNmxDw2UfDH4-CEfIRQojaTiMkXyF+7Jf5V0UaLdiUJ0DaOPqo9RdDM7Z0vkQbxaAcB+JoXQixRRKhaH0k3QYNonCowcPGECvQbz6AMA4bsaYTAmCTqExYviEiUMiZol26ANiDw9l7E4RTwmlLUeUiA0TmY5L+AybJWo8no3cMmBJKgkmOHXrvDxbkiDIEEBASBJ8MB4hYHCWAgSL650mdM+YqwFlLLaV1Ho94BhNF+MZLJrg0wr0aGvGo6VJwaHSgU8ZaEpkzJKfMxZcBKnVNgZ7EezzNkJG2XAXZIEDm6COdZWo5dkx4IvBmcYRBTKvDTLydetQHmd08YQdgmEAy4XwkGFBD8Yl2F0M4IYTkHqb1RuMDoXUzBmyaPpPJpyVCssCFMKgLAU7wCKPmcWZBj70EYFQcmIYDqWPyc4XUCErx8ncEORQbMtB6EMMYMwScoivMwGK6e7TJX1CUNkuV6Nqi1H+PZIErRbr2keRxd8OqmZ7MGs5A1Mrej3KHKlXq+UHrv2nDajFEzyEJCEr7MSmtUESpddKo1HqoJo2Srrcax127AL5W9ZRH0lpjwjUSvV0bDWyrjd8A2CLdbPXeBzBCSjPIqM+gfI+ubxUz31TGot8qoI2mcDaK8Mj3DyHyQGkBmLbaq3TugB1msyStsLe6jt3xtY6DjrUBO7jA1oR7lqydpcIwzrdca-BBpDImBuXlIZNItB7wbbM1Y27+H2D3bG+diBPA1AGBNB6wjtDWgNDW8qESNFaLCXeqNUrZ0HthTSCuWhOTjUGnlX9tqvE+PHWUwDIGW0Fv3cWl94wDLPUSY9FJYz10cT+VqwFPLoaOunVhp9fSGQV3OnBVojRzo2ww3qp4ba53o3sCHLeRhUWm0GKmdl-ggA */
   createMachine(
     {
-  tsTypes: {} as import("./usersXService.typegen").Typegen0,
-  schema: {
-    context: {} as UsersContext,
-    events: {} as UsersEvent,
-    services: {} as {
-      getUsers: {
-        data: TypesGen.GetUsersResponse
-      }
-      createUser: {
-        data: TypesGen.User
-      }
-      suspendUser: {
-        data: TypesGen.User
-      }
-      deleteUser: {
-        data: undefined
-      }
-      activateUser: {
-        data: TypesGen.User
-      }
-      updateUserPassword: {
-        data: undefined
-      }
-      updateUserRoles: {
-        data: TypesGen.User
-      }
-    },
-  },
-  predictableActionArguments: true,
-  id: "usersState",
-  on: {
-    UPDATE_FILTER: {
-      target: ".gettingUsers",
-      actions: ["assignFilter", "sendResetPage"],
-      internal: false,
-    },
-  },
-  initial: "startingPagination",
-  states: {
-    startingPagination: {
-      entry: "assignPaginationRef",
-      always: {
-        target: "gettingUsers",
+      tsTypes: {} as import("./usersXService.typegen").Typegen0,
+      schema: {
+        context: {} as UsersContext,
+        events: {} as UsersEvent,
+        services: {} as {
+          getUsers: {
+            data: TypesGen.GetUsersResponse
+          }
+          createUser: {
+            data: TypesGen.User
+          }
+          suspendUser: {
+            data: TypesGen.User
+          }
+          deleteUser: {
+            data: undefined
+          }
+          activateUser: {
+            data: TypesGen.User
+          }
+          updateUserPassword: {
+            data: undefined
+          }
+          updateUserRoles: {
+            data: TypesGen.User
+          }
+        },
       },
-    },
-    gettingUsers: {
-      entry: "clearGetUsersError",
-      invoke: {
-        src: "getUsers",
-        id: "getUsers",
-        onDone: [
-          {
-            target: "idle",
-            actions: "assignUsers",
-          },
-        ],
-        onError: [
-          {
-            target: "idle",
-            actions: [
-              "clearUsers",
-              "assignGetUsersError",
-              "displayGetUsersErrorMessage",
-            ],
-          },
-        ],
-      },
-      tags: "loading",
-    },
-    idle: {
-      entry: "clearSelectedUser",
+      predictableActionArguments: true,
+      id: "usersState",
       on: {
-        SUSPEND_USER: {
-          target: "confirmUserSuspension",
-          actions: "assignUserToSuspend",
-        },
-        DELETE_USER: {
-          target: "confirmUserDeletion",
-          actions: "assignUserToDelete",
-        },
-        ACTIVATE_USER: {
-          target: "confirmUserActivation",
-          actions: "assignUserToActivate",
-        },
-        RESET_USER_PASSWORD: {
-          target: "confirmUserPasswordReset",
-          actions: ["assignUserIdToResetPassword", "generateRandomPassword"],
-        },
-        UPDATE_USER_ROLES: {
-          target: "updatingUserRoles",
-          actions: "assignUserIdToUpdateRoles",
-        },
-        UPDATE_PAGE: {
-          target: "gettingUsers",
-          actions: "updateURL",
-        },
         UPDATE_FILTER: {
-          target: "gettingUsers",
+          target: ".gettingUsers",
           actions: ["assignFilter", "sendResetPage"],
+          internal: false,
         },
       },
-    },
-    confirmUserSuspension: {
-      on: {
-        CONFIRM_USER_SUSPENSION: {
-          target: "suspendingUser",
-        },
-        CANCEL_USER_SUSPENSION: {
-          target: "idle",
-        },
-      },
-    },
-    confirmUserDeletion: {
-      on: {
-        CONFIRM_USER_DELETE: {
-          target: "deletingUser",
-        },
-        CANCEL_USER_DELETE: {
-          target: "idle",
-        },
-      },
-    },
-    confirmUserActivation: {
-      on: {
-        CONFIRM_USER_ACTIVATION: {
-          target: "activatingUser",
-        },
-        CANCEL_USER_ACTIVATION: {
-          target: "idle",
-        },
-      },
-    },
-    suspendingUser: {
-      entry: "clearSuspendUserError",
-      invoke: {
-        src: "suspendUser",
-        id: "suspendUser",
-        onDone: [
-          {
+      initial: "startingPagination",
+      states: {
+        startingPagination: {
+          entry: "assignPaginationRef",
+          always: {
             target: "gettingUsers",
-            actions: "displaySuspendSuccess",
           },
-        ],
-        onError: [
-          {
-            target: "idle",
-            actions: ["assignSuspendUserError", "displaySuspendedErrorMessage"],
-          },
-        ],
-      },
-    },
-    deletingUser: {
-      entry: "clearDeleteUserError",
-      invoke: {
-        src: "deleteUser",
-        id: "deleteUser",
-        onDone: [
-          {
-            target: "gettingUsers",
-            actions: "displayDeleteSuccess",
-          },
-        ],
-        onError: [
-          {
-            target: "idle",
-            actions: ["assignDeleteUserError", "displayDeleteErrorMessage"],
-          },
-        ],
-      },
-    },
-    activatingUser: {
-      entry: "clearActivateUserError",
-      invoke: {
-        src: "activateUser",
-        id: "activateUser",
-        onDone: [
-          {
-            target: "gettingUsers",
-            actions: "displayActivateSuccess",
-          },
-        ],
-        onError: [
-          {
-            target: "idle",
-            actions: [
-              "assignActivateUserError",
-              "displayActivatedErrorMessage",
+        },
+        gettingUsers: {
+          entry: "clearGetUsersError",
+          invoke: {
+            src: "getUsers",
+            id: "getUsers",
+            onDone: [
+              {
+                target: "idle",
+                actions: "assignUsers",
+              },
+            ],
+            onError: [
+              {
+                target: "idle",
+                actions: [
+                  "clearUsers",
+                  "assignGetUsersError",
+                  "displayGetUsersErrorMessage",
+                ],
+              },
             ],
           },
-        ],
-      },
-    },
-    confirmUserPasswordReset: {
-      on: {
-        CONFIRM_USER_PASSWORD_RESET: {
-          target: "resettingUserPassword",
+          tags: "loading",
         },
-        CANCEL_USER_PASSWORD_RESET: {
-          target: "idle",
-        },
-      },
-    },
-    resettingUserPassword: {
-      entry: "clearResetUserPasswordError",
-      invoke: {
-        src: "resetUserPassword",
-        id: "resetUserPassword",
-        onDone: [
-          {
-            target: "idle",
-            actions: "displayResetPasswordSuccess",
+        idle: {
+          entry: "clearSelectedUser",
+          on: {
+            SUSPEND_USER: {
+              target: "confirmUserSuspension",
+              actions: "assignUserToSuspend",
+            },
+            DELETE_USER: {
+              target: "confirmUserDeletion",
+              actions: "assignUserToDelete",
+            },
+            ACTIVATE_USER: {
+              target: "confirmUserActivation",
+              actions: "assignUserToActivate",
+            },
+            RESET_USER_PASSWORD: {
+              target: "confirmUserPasswordReset",
+              actions: [
+                "assignUserIdToResetPassword",
+                "generateRandomPassword",
+              ],
+            },
+            UPDATE_USER_ROLES: {
+              target: "updatingUserRoles",
+              actions: "assignUserIdToUpdateRoles",
+            },
+            UPDATE_PAGE: {
+              target: "gettingUsers",
+              actions: "updateURL",
+            },
+            UPDATE_FILTER: {
+              target: "gettingUsers",
+              actions: ["assignFilter", "sendResetPage"],
+            },
           },
-        ],
-        onError: [
-          {
-            target: "idle",
-            actions: [
-              "assignResetUserPasswordError",
-              "displayResetPasswordErrorMessage",
+        },
+        confirmUserSuspension: {
+          on: {
+            CONFIRM_USER_SUSPENSION: {
+              target: "suspendingUser",
+            },
+            CANCEL_USER_SUSPENSION: {
+              target: "idle",
+            },
+          },
+        },
+        confirmUserDeletion: {
+          on: {
+            CONFIRM_USER_DELETE: {
+              target: "deletingUser",
+            },
+            CANCEL_USER_DELETE: {
+              target: "idle",
+            },
+          },
+        },
+        confirmUserActivation: {
+          on: {
+            CONFIRM_USER_ACTIVATION: {
+              target: "activatingUser",
+            },
+            CANCEL_USER_ACTIVATION: {
+              target: "idle",
+            },
+          },
+        },
+        suspendingUser: {
+          entry: "clearSuspendUserError",
+          invoke: {
+            src: "suspendUser",
+            id: "suspendUser",
+            onDone: [
+              {
+                target: "gettingUsers",
+                actions: "displaySuspendSuccess",
+              },
+            ],
+            onError: [
+              {
+                target: "idle",
+                actions: [
+                  "assignSuspendUserError",
+                  "displaySuspendedErrorMessage",
+                ],
+              },
             ],
           },
-        ],
-      },
-    },
-    updatingUserRoles: {
-      entry: "clearUpdateUserRolesError",
-      invoke: {
-        src: "updateUserRoles",
-        id: "updateUserRoles",
-        onDone: [
-          {
-            target: "idle",
-            actions: "updateUserRolesInTheList",
-          },
-        ],
-        onError: [
-          {
-            target: "idle",
-            actions: [
-              "assignUpdateRolesError",
-              "displayUpdateRolesErrorMessage",
+        },
+        deletingUser: {
+          entry: "clearDeleteUserError",
+          invoke: {
+            src: "deleteUser",
+            id: "deleteUser",
+            onDone: [
+              {
+                target: "gettingUsers",
+                actions: "displayDeleteSuccess",
+              },
+            ],
+            onError: [
+              {
+                target: "idle",
+                actions: ["assignDeleteUserError", "displayDeleteErrorMessage"],
+              },
             ],
           },
-        ],
+        },
+        activatingUser: {
+          entry: "clearActivateUserError",
+          invoke: {
+            src: "activateUser",
+            id: "activateUser",
+            onDone: [
+              {
+                target: "gettingUsers",
+                actions: "displayActivateSuccess",
+              },
+            ],
+            onError: [
+              {
+                target: "idle",
+                actions: [
+                  "assignActivateUserError",
+                  "displayActivatedErrorMessage",
+                ],
+              },
+            ],
+          },
+        },
+        confirmUserPasswordReset: {
+          on: {
+            CONFIRM_USER_PASSWORD_RESET: {
+              target: "resettingUserPassword",
+            },
+            CANCEL_USER_PASSWORD_RESET: {
+              target: "idle",
+            },
+          },
+        },
+        resettingUserPassword: {
+          entry: "clearResetUserPasswordError",
+          invoke: {
+            src: "resetUserPassword",
+            id: "resetUserPassword",
+            onDone: [
+              {
+                target: "idle",
+                actions: "displayResetPasswordSuccess",
+              },
+            ],
+            onError: [
+              {
+                target: "idle",
+                actions: [
+                  "assignResetUserPasswordError",
+                  "displayResetPasswordErrorMessage",
+                ],
+              },
+            ],
+          },
+        },
+        updatingUserRoles: {
+          entry: "clearUpdateUserRolesError",
+          invoke: {
+            src: "updateUserRoles",
+            id: "updateUserRoles",
+            onDone: [
+              {
+                target: "idle",
+                actions: "updateUserRolesInTheList",
+              },
+            ],
+            onError: [
+              {
+                target: "idle",
+                actions: [
+                  "assignUpdateRolesError",
+                  "displayUpdateRolesErrorMessage",
+                ],
+              },
+            ],
+          },
+        },
       },
     },
-  },
-},
     {
       services: {
         // Passing API.getUsers directly does not invoke the function properly
