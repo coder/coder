00342fb7d80ef6c31ed54e50e8fb3643abd01ebb
Presley Pizzo <presley@coder.com>
2022-11-15 20:31:08 +0000

    Move count query to window function

--- coderd/database/queries/auditlogs.sql
+++ coderd/database/queries/auditlogs.sql
@@ -8,7 +8,8 @@ SELECT
     users.created_at AS user_created_at,
     users.status AS user_status,
     users.rbac_roles AS user_roles,
-    users.avatar_url AS user_avatar_url
+    users.avatar_url AS user_avatar_url,
+		COUNT(audit_logs.*) OVER() AS count
 FROM
 	audit_logs
 LEFT JOIN
@@ -68,61 +69,6 @@ LIMIT
     $1
 OFFSET
     $2;
-
--- name: GetAuditLogCount :one
-SELECT
-  COUNT(*) as count
-FROM
-	audit_logs
-WHERE
-    -- Filter resource_type
-	CASE
-		WHEN @resource_type :: text != '' THEN
-			resource_type = @resource_type :: resource_type
-		ELSE true
-	END
-	-- Filter resource_id
-	AND CASE
-		WHEN @resource_id :: uuid != '00000000-0000-0000-0000-000000000000'::uuid THEN
-			resource_id = @resource_id
-		ELSE true
-	END
-	-- Filter by resource_target
-	AND CASE
-		WHEN @resource_target :: text != '' THEN
-			resource_target = @resource_target
-		ELSE true
-	END
-	-- Filter action
-	AND CASE
-		WHEN @action :: text != '' THEN
-			action = @action :: audit_action
-		ELSE true
-	END
-	-- Filter by username
-	AND CASE
-		WHEN @username :: text != '' THEN
-			user_id = (SELECT id from users WHERE users.username = @username )
-		ELSE true
-	END
-	-- Filter by user_email
-	AND CASE
-		WHEN @email :: text != '' THEN
-			user_id = (SELECT id from users WHERE users.email = @email )
-		ELSE true
-	END
-	-- Filter by date_from
-	AND CASE
-		WHEN @date_from :: timestamp with time zone != '0001-01-01 00:00:00' THEN
-			"time" >= @date_from
-		ELSE true
-	END
-	-- Filter by date_to
-	AND CASE
-		WHEN @date_to :: timestamp with time zone != '0001-01-01 00:00:00' THEN
-			"time" <= @date_to
-		ELSE true
-	END;
 
 -- name: InsertAuditLog :one
 INSERT INTO
