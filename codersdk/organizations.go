400413ebb9d5d061403bd15bde5804dcf91380fe
Marcin Tojek <marcin@coder.com>
2022-11-17 11:20:24 +0100

    Adjust API

--- codersdk/organizations.go
+++ codersdk/organizations.go
@@ -72,6 +72,9 @@ type CreateTemplateRequest struct {
 	// DefaultTTLMillis allows optionally specifying the default TTL
 	// for all workspaces created from this template.
 	DefaultTTLMillis *int64 `json:"default_ttl_ms,omitempty"`
+
+	// Allow users to cancel in-progress workspace jobs.
+	AllowUserCancelWorkspaceJobs *bool `json:"allow_user_cancel_workspace_jobs"`
 }
 
 // CreateWorkspaceRequest provides options for creating a new workspace.
