ad38c3b7894ee59664dd0681b61688ab3370120c
Mathias Fredriksson <mafredri@gmail.com>
2022-11-30 22:47:24 +0000

    Add forgotten UpdateCheckBanner.stories.tsx

---
+++ site/src/components/UpdateCheckBanner/UpdateCheckBanner.stories.tsx
@@ -0,0 +1,34 @@
+import { ComponentMeta, Story } from "@storybook/react"
+import { UpdateCheckBanner, UpdateCheckBannerProps } from "./UpdateCheckBanner"
+
+export default {
+  title: "components/UpdateCheckBanner",
+  component: UpdateCheckBanner,
+} as ComponentMeta<typeof UpdateCheckBanner>
+
+const Template: Story<UpdateCheckBannerProps> = (args) => (
+  <UpdateCheckBanner {...args} />
+)
+
+export const UpdateAvailable = Template.bind({})
+UpdateAvailable.args = {
+  updateCheck: {
+    current: false,
+    version: "v0.12.9",
+    url: "https://github.com/coder/coder/releases/tag/v0.12.9",
+  },
+}
+
+export const NoUpdateAvailable = Template.bind({})
+NoUpdateAvailable.args = {
+  updateCheck: {
+    current: true,
+    version: "v0.12.9",
+    url: "https://github.com/coder/coder/releases/tag/v0.12.9",
+  },
+}
+
+export const UpdateCheckError = Template.bind({})
+UpdateCheckError.args = {
+  error: new Error("Coder update check failed."),
+}
