3aceb47b855500fb6f228bfd9b172949697d2d3e
Marcin Tojek <marcin@coder.com>
2022-12-02 17:19:58 +0100

    gosec

--- coderd/client_test.go
+++ coderd/client_test.go
@@ -13,22 +13,33 @@ import (
 // Issue: https://github.com/coder/coder/issues/5249
 // While running tests in parallel, the web server seems to be overloaded and responds with HTTP 502.
 // require.Eventually expects correct HTTP responses.
+
 func doWithRetries(t require.TestingT, client *codersdk.Client, req *http.Request) (*http.Response, error) {
 	var resp *http.Response
 	var err error
 	require.Eventually(t, func() bool {
 		resp, err = client.HTTPClient.Do(req)
-		return resp.StatusCode != http.StatusBadGateway
+		if resp != nil && resp.StatusCode == http.StatusBadGateway {
+			resp.Body.Close()
+			return false
+		}
+		return true
 	}, testutil.WaitLong, testutil.IntervalFast)
 	return resp, err
 }
 
+// context-as-argument: context.Context should be the first parameter of a function (revive)
+// #nosec
 func requestWithRetries(t require.TestingT, client *codersdk.Client, ctx context.Context, method, path string, body interface{}, opts ...codersdk.RequestOption) (*http.Response, error) {
 	var resp *http.Response
 	var err error
 	require.Eventually(t, func() bool {
 		resp, err = client.Request(ctx, method, path, body, opts...)
-		return resp == nil || resp.StatusCode != http.StatusBadGateway
+		if resp != nil && resp.StatusCode == http.StatusBadGateway {
+			resp.Body.Close()
+			return false
+		}
+		return true
 	}, testutil.WaitLong, testutil.IntervalFast)
 	return resp, err
 }
