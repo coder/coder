c8ab9191a79fd4cef8aaa908dc77a0776ede0a32
Marcin Tojek <marcin@coder.com>
2022-12-02 15:43:33 +0100

    WIP

--- coderd/client_test.go
+++ coderd/client_test.go
@@ -1,6 +1,7 @@
 package coderd_test
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/stretchr/testify/require"
@@ -14,6 +15,16 @@ func doWithRetries(t require.TestingT, client *codersdk.Client, req *http.Reques
 	var err error
 	require.Eventually(t, func() bool {
 		resp, err = client.HTTPClient.Do(req)
+		return resp.StatusCode != http.StatusBadGateway
+	}, testutil.WaitShort, testutil.IntervalFast)
+	return resp, err
+}
+
+func requestWithRetries(t require.TestingT, client *codersdk.Client, ctx context.Context, method, path string, body interface{}, opts ...codersdk.RequestOption) (*http.Response, error) {
+	var resp *http.Response
+	var err error
+	require.Eventually(t, func() bool {
+		resp, err = client.Request(ctx, method, path, body, opts...)
 		return resp.StatusCode != http.StatusBadGateway
 	}, testutil.WaitShort, testutil.IntervalFast)
 	return resp, err
