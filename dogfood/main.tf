6f79f9debc906acb13dff330bab14297387dbc96
Joe Previte <jjprevite@gmail.com>
2022-12-07 09:07:42 -0700

    Update dogfood/main.tf

--- dogfood/main.tf
+++ dogfood/main.tf
@@ -44,7 +44,6 @@ resource "coder_agent" "dev" {
     curl -fsSL https://code-server.dev/install.sh | sh
     code-server --auth none --port 13337 &
     sudo service docker start
-    coder dotfiles -y 2>&1 | tee  ~/.personalize.log
     "if [ -n ${var.dotfiles_uri} ]; then coder dotfiles var.dotfiles_uri -y 2>&1 | tee  ~/.personalize.log; fi"
     EOF
 }
