c3b2e67989c0cfa825021fb71e644944d91e7a5d
Bruno Quaresma <bruno@coder.com>
2022-12-08 15:08:25 +0000

    Add styles to the template card

--- site/src/pages/StarterTemplatesPage/StarterTemplatesPageView.tsx
+++ site/src/pages/StarterTemplatesPage/StarterTemplatesPageView.tsx
@@ -1,3 +1,4 @@
+import { makeStyles } from "@material-ui/core/styles"
 import { AlertBanner } from "components/AlertBanner/AlertBanner"
 import { Maybe } from "components/Conditionals/Maybe"
 import { Margins } from "components/Margins/Margins"
@@ -18,6 +19,7 @@ export const StarterTemplatesPageView: FC<StarterTemplatesPageViewProps> = ({
   context,
 }) => {
   const { t } = useTranslation("starterTemplatesPage")
+  const styles = useStyles()
 
   return (
     <Margins>
@@ -30,10 +32,43 @@ export const StarterTemplatesPageView: FC<StarterTemplatesPageViewProps> = ({
         <AlertBanner error={context.error} severity="error" />
       </Maybe>
 
-      {context.starterTemplates &&
-        context.starterTemplates.map((example) => (
-          <div key={example.id}>{example.name}</div>
-        ))}
+      <div className={styles.templates}>
+        {context.starterTemplates &&
+          context.starterTemplates.map((example) => (
+            <div className={styles.template} key={example.id}>
+              <span className={styles.templateName}>{example.name}</span>
+              <span className={styles.templateDescription}>
+                {example.description}
+              </span>
+            </div>
+          ))}
+      </div>
     </Margins>
   )
 }
+
+const useStyles = makeStyles((theme) => ({
+  templates: {
+    display: "grid",
+    gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
+    gap: theme.spacing(2),
+  },
+  template: {
+    padding: theme.spacing(2),
+    border: `1px solid ${theme.palette.divider}`,
+    borderRadius: theme.shape.borderRadius,
+    background: theme.palette.background.paper,
+    display: "flex",
+    flexDirection: "column",
+    gap: theme.spacing(0.5),
+  },
+
+  templateName: {
+    fontSize: theme.spacing(2),
+  },
+
+  templateDescription: {
+    fontSize: theme.spacing(1.75),
+    color: theme.palette.text.secondary,
+  },
+}))
