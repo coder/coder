439ec15feedd0495a0ddbc4727ace51e2d244e31
Bruno Quaresma <bruno@coder.com>
2022-12-12 17:04:14 +0000

    Add services and actions into machine

--- site/src/pages/CreateTemplatePage/CreateTemplatePage.tsx
+++ site/src/pages/CreateTemplatePage/CreateTemplatePage.tsx
@@ -1,14 +1,31 @@
+import { useMachine } from "@xstate/react"
 import { FullPageHorizontalForm } from "components/FullPageForm/FullPageHorizontalForm"
+import { Loader } from "components/Loader/Loader"
+import { useOrganizationId } from "hooks/useOrganizationId"
 import { FC } from "react"
 import { Helmet } from "react-helmet-async"
 import { useTranslation } from "react-i18next"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom"
 import { pageTitle } from "util/page"
+import { createTemplateMachine } from "xServices/createTemplate/createTemplateXService"
 import { CreateTemplateForm } from "./CreateTemplateForm"
 
 const CreateTemplatePage: FC = () => {
   const { t } = useTranslation("createTemplatePage")
   const navigate = useNavigate()
+  const organizationId = useOrganizationId()
+  const [searchParams] = useSearchParams()
+  const [state] = useMachine(createTemplateMachine, {
+    context: {
+      organizationId,
+      exampleId: searchParams.get("exampleId"),
+    },
+    actions: {
+      onCreate: () => {
+        console.log("CREATE!")
+      },
+    },
+  })
 
   const onCancel = () => {
     navigate(-1)
@@ -21,7 +38,10 @@ const CreateTemplatePage: FC = () => {
       </Helmet>
 
       <FullPageHorizontalForm title={t("title")} onCancel={onCancel}>
-        <CreateTemplateForm isSubmitting={false} onCancel={onCancel} />
+        {state.hasTag("loading") && <Loader />}
+        {state.matches("idle") && (
+          <CreateTemplateForm isSubmitting={false} onCancel={onCancel} />
+        )}
       </FullPageHorizontalForm>
     </>
   )
--- site/src/xServices/createTemplate/createTemplateXService.ts
+++ site/src/xServices/createTemplate/createTemplateXService.ts
@@ -1,64 +1,105 @@
-import { TemplateExample } from "api/typesGenerated"
-import { createMachine } from "xstate"
+import { getTemplateExamples } from "api/api"
+import { Template, TemplateExample } from "api/typesGenerated"
+import { MockTemplate } from "testHelpers/entities"
+import { assign, createMachine } from "xstate"
 
 interface CreateTemplateContext {
-  exampleId?: string
+  organizationId: string
+  // It can be null because it is being passed from query string
+  exampleId?: string | null
   error?: unknown
+  starterTemplate?: TemplateExample
 }
 
-export const createTemplateMachine = createMachine({
-  id: "createTemplate",
-  predictableActionArguments: true,
-  schema: {
-    context: {} as CreateTemplateContext,
-    events: {} as { type: "CREATE"; data: any },
-    services: {} as {
+export const createTemplateMachine = createMachine(
+  {
+    id: "createTemplate",
+    predictableActionArguments: true,
+    schema: {
+      context: {} as CreateTemplateContext,
+      events: {} as { type: "CREATE"; data: any },
+      services: {} as {
+        loadingStarterTemplate: {
+          data: TemplateExample
+        }
+        createTemplate: {
+          data: Template
+        }
+      },
+    },
+    tsTypes: {} as import("./createTemplateXService.typegen").Typegen0,
+    initial: "starting",
+    states: {
+      starting: {
+        always: [
+          { target: "loadingStarterTemplate", cond: "isExampleProvided" },
+          { target: "idle" },
+        ],
+        tags: ["loading"],
+      },
       loadingStarterTemplate: {
-        data: TemplateExample
-      }
+        invoke: {
+          src: "loadStarterTemplate",
+          onDone: {
+            target: "idle",
+            actions: ["assignStarterTemplate"],
+          },
+          onError: {
+            target: "idle",
+            actions: ["assignError"],
+          },
+        },
+        tags: ["loading"],
+      },
+      idle: {
+        on: {
+          CREATE: "creating",
+        },
+      },
+      creating: {
+        invoke: {
+          src: "createTemplate",
+          onDone: {
+            target: "created",
+            actions: ["onCreate"],
+          },
+          onError: {
+            target: "idle",
+            actions: ["assignError"],
+          },
+        },
+      },
+      created: {
+        type: "final",
+      },
     },
   },
-  tsTypes: {} as import("./createTemplateXService.typegen").Typegen0,
-  states: {
-    starting: {
-      always: [
-        { target: "loadingStarterTemplate", cond: "isExampleProvided" },
-        { target: "idle" },
-      ],
-    },
-    loadingStarterTemplate: {
-      invoke: {
-        src: "loadStarterTemplate",
-        onDone: {
-          target: "idle",
-          actions: ["assignStarterTemplate"],
-        },
-        onError: {
-          target: "idle",
-          actions: ["assignError"],
-        },
+  {
+    services: {
+      loadStarterTemplate: async ({ organizationId, exampleId }) => {
+        if (!exampleId) {
+          throw new Error(`Example ID is not defined.`)
+        }
+        const examples = await getTemplateExamples(organizationId)
+        const starterTemplate = examples.find(
+          (example) => example.id === exampleId,
+        )
+        if (!starterTemplate) {
+          throw new Error(`Example ${exampleId} not found.`)
+        }
+        return starterTemplate
+      },
+      createTemplate: async () => {
+        console.log("CALL CREATE TEMPLATE!")
+        return MockTemplate
       },
     },
-    idle: {
-      on: {
-        CREATE: "creating",
-      },
+    actions: {
+      assignError: (_, { data }) => assign({ error: data }),
+      assignStarterTemplate: (_, { data }) => assign({ starterTemplate: data }),
     },
-    creating: {
-      invoke: {
-        src: "createTemplate",
-        onDone: {
-          target: "created",
-          actions: ["onCreated"],
-        },
-        onError: {
-          target: "idle",
-          actions: ["assignError"],
-        },
-      },
-    },
-    created: {
-      type: "final",
+    guards: {
+      isExampleProvided: ({ exampleId }) => Boolean(exampleId),
     },
   },
-})
+)
