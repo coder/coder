2853405385e1218b9323c7944c37b0179bd1f32f
Marcin Tojek <marcin@coder.com>
2022-12-15 18:02:00 +0100

    Fix: pushd popd

--- scripts/apidocgen/generate.sh
+++ scripts/apidocgen/generate.sh
@@ -17,35 +17,28 @@ trap cleanup EXIT
 
 log "Use temporary file: ${API_MD_TMP_FILE}"
 
-(
-	cd "$SCRIPT_DIR/../.."
-
-	go run github.com/swaggo/swag/cmd/swag@v1.8.6 init \
+pushd "$SCRIPT_DIR/../.."
+go run github.com/swaggo/swag/cmd/swag@v1.8.6 init \
 		--generalInfo="coderd.go" \
 		--dir="./coderd,./codersdk" \
 		--output="./coderd/apidoc" \
 		--outputTypes="go,json" \
 		--parseDependency=true
-) || exit $?
+popd
 
-(
-	cd "$SCRIPT_DIR"
-	npm ci
+pushd "$SCRIPT_DIR"
+npm ci
 
-	# Make sure that widdershins is installed correctly.
-	npm exec -- widdershins --version
-
-	# Render the Markdown file.
-	npm exec -- widdershins \
-		--user_templates "./markdown-template" \
-		--search false \
-		--omitHeader true \
-		--language_tabs "shell:curl" \
-		--summary "../../coderd/apidoc/swagger.json" \
-		--outfile "${API_MD_TMP_FILE}"
-) || exit $?
-
-(
-	cd "$SCRIPT_DIR"
-	go run postprocess/main.go -in-md-file-single "${API_MD_TMP_FILE}"
-) || exit $?
+# Make sure that widdershins is installed correctly.
+npm exec -- widdershins --version
+# Render the Markdown file.
+npm exec -- widdershins \
+	--user_templates "./markdown-template" \
+	--search false \
+	--omitHeader true \
+	--language_tabs "shell:curl" \
+	--summary "../../coderd/apidoc/swagger.json" \
+	--outfile "${API_MD_TMP_FILE}"
+# Perform the postprocessing
+go run postprocess/main.go -in-md-file-single "${API_MD_TMP_FILE}"
+popd
