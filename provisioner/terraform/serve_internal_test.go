9bb93a3abeb46e7a0fd15aecfcbdc369bcc6d8e8
Geoffrey Huntley <ghuntley@ghuntley.com>
2022-12-22 05:05:50 +0000

    chore(autofix): upgrade-terraform-toolchain
    Automatically generated via https://github.com/coder/autofix

--- provisioner/terraform/serve_internal_test.go
+++ provisioner/terraform/serve_internal_test.go
@@ -27,7 +27,7 @@ func Test_absoluteBinaryPath(t *testing.T) {
 		{
 			name:             "TestCorrectVersion",
 			args:             args{ctx: context.Background()},
-			terraformVersion: "1.3.0",
+			terraformVersion: "1.3.6",
 			expectedErr:      nil,
 		},
 		{
@@ -39,7 +39,7 @@ func Test_absoluteBinaryPath(t *testing.T) {
 		{
 			name:             "TestNewVersion",
 			args:             args{ctx: context.Background()},
-			terraformVersion: "1.3.0",
+			terraformVersion: "1.3.6",
 			expectedErr:      nil,
 		},
 		{
