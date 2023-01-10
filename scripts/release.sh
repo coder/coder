cf2c523d0f9394f430b4a8243e974e9089095a22
Mathias Fredriksson <mafredri@gmail.com>
2023-01-10 20:22:32 +0000

    Fix watch

--- scripts/release.sh
+++ scripts/release.sh
@@ -162,11 +162,13 @@ log 'Waiting for job to become "in_progress"...'
 # Wait at most 3 minutes (3*60)/3 = 60 for the job to start.
 for _ in $(seq 1 60); do
 	mapfile -t run < <(
-		# 3886828508 in_progress
+		# Output:
+		# 3886828508
+		# in_progress
 		gh run list -w release.yaml \
 			--limit 1 \
 			--json status,databaseId \
-			--jq '.[] | (.databaseId | tostring) + " " + .status'
+			--jq '.[] | (.databaseId | tostring), .status'
 	)
 	if [[ ${run[1]} != "in_progress" ]]; then
 		sleep 3
