01652e8afb573d043d1c6d985091940b0a7420c2
Kyle Carberry <kyle@coder.com>
2023-01-10 15:42:33 -0600

    fix: disable pointer events on app icons (#5664)
    Ben accidentally clicked to open this in a new tab
    which seemed kinda janky UX-wise on our part.

--- site/src/components/AppLink/BaseIcon.tsx
+++ site/src/components/AppLink/BaseIcon.tsx
@@ -4,7 +4,13 @@ import ComputerIcon from "@material-ui/icons/Computer"
 
 export const BaseIcon: FC<{ app: WorkspaceApp }> = ({ app }) => {
   return app.icon ? (
-    <img alt={`${app.display_name} Icon`} src={app.icon} />
+    <img
+      alt={`${app.display_name} Icon`}
+      src={app.icon}
+      style={{
+        pointerEvents: "none",
+      }}
+    />
   ) : (
     <ComputerIcon />
   )
