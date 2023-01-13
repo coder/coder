56ad0061a61b4085112d584871ec8bbc0d77c03c
Marcin Tojek <marcin@coder.com>
2023-01-13 12:31:17 +0100

    SQL queries

---
+++ coderd/database/queries/templateversionparameters.sql
@@ -0,0 +1,32 @@
+-- name: InsertTemplateVersionParameter :one
+INSERT INTO
+    template_version_parameters (
+        template_version_id,
+        name,
+        description,
+        type,
+        mutable,
+        default_value,
+        icon,
+        options,
+        validation_regex,
+        validation_min,
+        validation_max
+    )
+VALUES
+    (
+        $1,
+        $2,
+        $3,
+        $4,
+        $5,
+        $6,
+        $7,
+        $8,
+        $9,
+        $10,
+        $11
+    ) RETURNING *;
+
+-- name: GetTemplateVersionParameters :many
+SELECT * FROM template_version_parameters WHERE template_version_id = $1;
