fe1e504ce49d885abcb8daccf965d1d310d92704
Ammar Bandukwala <ammar@ammar.io>
2023-01-13 00:30:55 +0000

    feat(provisionersdk): follow symlinks while archiving

--- provisionersdk/archive.go
+++ provisionersdk/archive.go
@@ -4,6 +4,7 @@ import (
 	"archive/tar"
 	"bytes"
 	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
@@ -31,43 +32,37 @@ func dirHasExt(dir string, ext string) (bool, error) {
 	return false, nil
 }
 
-// Tar archives a Terraform directory.
-func Tar(directory string, limit int64) ([]byte, error) {
-	var buffer bytes.Buffer
-	tarWriter := tar.NewWriter(&buffer)
-	totalSize := int64(0)
 
-	const tfExt = ".tf"
-	hasTf, err := dirHasExt(directory, tfExt)
-	if err != nil {
-		return nil, err
-	}
-	if !hasTf {
-		absPath, err := filepath.Abs(directory)
-		if err != nil {
-			return nil, err
-		}
+type archiver struct {
 
-		// Show absolute path to aid in debugging. E.g. showing "." is
-		// useless.
-		return nil, xerrors.Errorf(
-			"%s is not a valid template since it has no %s files",
-			absPath, tfExt,
-		)
-	}
+}
 
-	err = filepath.Walk(directory, func(file string, fileInfo os.FileInfo, err error) error {
+func (a *archiver) walkFn(path string, fileInfo os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		var link string
 		if fileInfo.Mode()&os.ModeSymlink == os.ModeSymlink {
-			link, err = os.Readlink(file)
+			// Per https://github.com/coder/coder/issues/5677, we want to
+			// follow symlinks.
+			var linkDest string
+			linkDest, err = os.Readlink(file)
 			if err != nil {
 				return err
 			}
+
+			destInfo, err := os.Stat(linkDest)
+			if err != nil {
+				return err
+			}
+			if destInfo.IsDir() {
+				return filepath.Walk(linkDest, func(path string, info fs.FileInfo, err error) error {
+					walkFn(path, info, err)
+				})
+			}
+			return nil
 		}
-		header, err := tar.FileInfoHeader(fileInfo, link)
+
+		header, err := tar.FileInfoHeader(fileInfo, "")
 		if err != nil {
 			return err
 		}
@@ -76,11 +71,10 @@ func Tar(directory string, limit int64) ([]byte, error) {
 			return err
 		}
 		if strings.HasPrefix(rel, ".") || strings.HasPrefix(filepath.Base(rel), ".") {
+			// Don't archive hidden files!
 			if fileInfo.IsDir() && rel != "." {
-				// Don't archive hidden files!
 				return filepath.SkipDir
 			}
-			// Don't archive hidden files!
 			return nil
 		}
 		if strings.Contains(rel, ".tfstate") {
@@ -109,7 +103,35 @@ func Tar(directory string, limit int64) ([]byte, error) {
 			return xerrors.Errorf("Archive too big. Must be <= %d bytes", limit)
 		}
 		return data.Close()
-	})
+	}
+}
+
+// Tar archives a Terraform directory.
+func Tar(directory string, limit int64) ([]byte, error) {
+	var buffer bytes.Buffer
+	tarWriter := tar.NewWriter(&buffer)
+	totalSize := int64(0)
+
+	const tfExt = ".tf"
+	hasTf, err := dirHasExt(directory, tfExt)
+	if err != nil {
+		return nil, err
+	}
+	if !hasTf {
+		absPath, err := filepath.Abs(directory)
+		if err != nil {
+			return nil, err
+		}
+
+		// Show absolute path to aid in debugging. E.g. showing "." is
+		// useless.
+		return nil, xerrors.Errorf(
+			"%s is not a valid template since it has no %s files",
+			absPath, tfExt,
+		)
+	}
+
+	err = filepath.Walk(directory, tarWalkFn())
 	if err != nil {
 		return nil, err
 	}
--- provisionersdk/archive_test.go
+++ provisionersdk/archive_test.go
@@ -12,6 +12,7 @@ import (
 
 func TestTar(t *testing.T) {
 	t.Parallel()
+
 	t.Run("NoTF", func(t *testing.T) {
 		t.Parallel()
 		dir := t.TempDir()
@@ -90,15 +91,58 @@ func TestTar(t *testing.T) {
 
 func TestUntar(t *testing.T) {
 	t.Parallel()
-	dir := t.TempDir()
-	file, err := os.CreateTemp(dir, "*.tf")
-	require.NoError(t, err)
-	_ = file.Close()
-	archive, err := provisionersdk.Tar(dir, 1024)
-	require.NoError(t, err)
-	dir = t.TempDir()
-	err = provisionersdk.Untar(dir, archive)
-	require.NoError(t, err)
-	_, err = os.Stat(filepath.Join(dir, filepath.Base(file.Name())))
-	require.NoError(t, err)
+	t.Run("Normal", func(t *testing.T) {
+		t.Parallel()
+
+		dir := t.TempDir()
+		file, err := os.CreateTemp(dir, "*.tf")
+		require.NoError(t, err)
+		_ = file.Close()
+		archive, err := provisionersdk.Tar(dir, 1024)
+		require.NoError(t, err)
+		dir = t.TempDir()
+		err = provisionersdk.Untar(dir, archive)
+		require.NoError(t, err)
+		_, err = os.Stat(filepath.Join(dir, filepath.Base(file.Name())))
+		require.NoError(t, err)
+	})
+	t.Run("FollowSymlinks", func(t *testing.T) {
+		t.Parallel()
+
+		externalDir := t.TempDir()
+		externalFile, err := os.CreateTemp(externalDir, "")
+		require.NoError(t, err)
+		const externalFileContents = "dogdogdog"
+		externalFile.WriteString(externalFileContents)
+		externalFile.Close()
+
+		dir := t.TempDir()
+
+		file, err := os.CreateTemp(dir, "*.tf")
+		require.NoError(t, err)
+		_ = file.Close()
+
+		err = os.Symlink(
+			externalDir,
+			filepath.Join(dir, "link"),
+		)
+		require.NoError(t, err)
+
+		checkDir := func(dir string) {
+			gotContents, err := os.ReadFile(filepath.Join(dir, "link", filepath.Base(externalFile.Name())))
+			require.NoError(t, err)
+			require.EqualValues(t, externalFileContents, gotContents)
+		}
+
+		checkDir(dir)
+
+		archive, err := provisionersdk.Tar(dir, 1024)
+		require.NoError(t, err)
+
+		extractDir := t.TempDir()
+		err = provisionersdk.Untar(extractDir, archive)
+		require.NoError(t, err)
+
+		checkDir(extractDir)
+	})
 }
