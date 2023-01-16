ac17078f2e79ec331610a9e562d095a1f63625a1
Mathias Fredriksson <mafredri@gmail.com>
2023-01-16 16:33:20 +0000

    Flip the if

--- testutil/temp.go
+++ testutil/temp.go
@@ -9,8 +9,8 @@ import (
 func TempDir(t *testing.T) string {
 	t.Helper()
 	dir := t.TempDir()
-	t.Cleanup(func() {
-		if runtime.GOOS == "windows" {
+	if runtime.GOOS == "windows" {
+		t.Cleanup(func() {
 			tries := 50
 			var err error
 			for i := 0; i < tries; i++ {
@@ -22,7 +22,7 @@ func TempDir(t *testing.T) string {
 			// Note that even though we're giving up here, t.TempDir
 			// cleanup _could_ still succeed (or fail the test).
 			t.Logf("TempDir: delete %q: giving up after %d tries: %v", dir, tries, err)
-		}
-	})
+		})
+	}
 	return dir
 }
