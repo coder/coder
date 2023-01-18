d93a3734cefaa5b0e151fc7634748f6ed9a596c4
Cian Johnston <cian@coder.com>
2023-01-18 11:58:10 +0000

    fix swaggerparser test

--- coderd/coderdtest/swaggerparser.go
+++ coderd/coderdtest/swaggerparser.go
@@ -297,6 +297,7 @@ func assertPathParametersDefined(t *testing.T, comment SwaggerComment) {
 func assertSecurityDefined(t *testing.T, comment SwaggerComment) {
 	if comment.router == "/updatecheck" ||
 		comment.router == "/buildinfo" ||
+		comment.router == "/experiments" ||
 		comment.router == "/" {
 		return // endpoints do not require authorization
 	}
