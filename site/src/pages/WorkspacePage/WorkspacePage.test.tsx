ea4300bd06c47489ba2f914acd783fc29f822dfc
Bruno Quaresma <bruno@coder.com>
2023-01-19 14:11:03 +0000

    Increse timeout

--- site/src/pages/WorkspacePage/WorkspacePage.test.tsx
+++ site/src/pages/WorkspacePage/WorkspacePage.test.tsx
@@ -81,7 +81,6 @@ beforeAll(() => {
 
 beforeEach(() => {
   jest.resetAllMocks()
-  jest.setTimeout(20_000)
 })
 
 afterAll(() => {
@@ -114,7 +113,8 @@ describe("WorkspacePage", () => {
     const confirmButton = await screen.findByRole("button", { name: "Delete" })
     await user.click(confirmButton)
     expect(deleteWorkspaceMock).toBeCalled()
-  })
+    // This test takes long to finish
+  }, 20_000)
 
   it("requests a start job when the user presses Start", async () => {
     server.use(
