60c2c3d62bc8bf5769cb8c995aadf16d6ba66eef
Cian Johnston <cian@coder.com>
2023-01-23 13:04:30 +0000

    wire up AuthzQuerier in coderd

--- coderd/coderd.go
+++ coderd/coderd.go
@@ -40,6 +40,7 @@ import (
 	// Used to serve the Swagger endpoint
 	_ "github.com/coder/coder/coderd/apidoc"
 	"github.com/coder/coder/coderd/audit"
+	"github.com/coder/coder/coderd/authzquery"
 	"github.com/coder/coder/coderd/awsidentity"
 	"github.com/coder/coder/coderd/database"
 	"github.com/coder/coder/coderd/database/dbtype"
@@ -154,6 +155,13 @@ func New(options *Options) *API {
 	if options == nil {
 		options = &Options{}
 	}
+	experiments := initExperiments(options.Logger, options.DeploymentConfig.Experiments.Value, options.DeploymentConfig.Experimental.Value)
+	// TODO: remove this once we promote authz_querier out of experiments.
+	if experiments.Enabled(codersdk.ExperimentAuthzQuerier) {
+		if _, ok := (options.Database).(*authzquery.AuthzQuerier); !ok {
+			options.Database = authzquery.NewAuthzQuerier(options.Database, options.Authorizer)
+		}
+	}
 	if options.AppHostname != "" && options.AppHostnameRegex == nil || options.AppHostname == "" && options.AppHostnameRegex != nil {
 		panic("coderd: both AppHostname and AppHostnameRegex must be set or unset")
 	}
@@ -222,7 +230,7 @@ func New(options *Options) *API {
 		},
 		metricsCache: metricsCache,
 		Auditor:      atomic.Pointer[audit.Auditor]{},
-		Experiments:  initExperiments(options.Logger, options.DeploymentConfig.Experiments.Value, options.DeploymentConfig.Experimental.Value),
+		Experiments:  experiments,
 	}
 	if options.UpdateCheckOptions != nil {
 		api.updateChecker = updatecheck.New(
