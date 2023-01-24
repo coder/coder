ee7da80feca7c528be71ac8960a19a5459dbb2a8
Bruno Quaresma <bruno@coder.com>
2023-01-24 14:49:29 +0000

    Update providers in test

--- site/src/app.tsx
+++ site/src/app.tsx
@@ -2,7 +2,7 @@ import CssBaseline from "@material-ui/core/CssBaseline"
 import ThemeProvider from "@material-ui/styles/ThemeProvider"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { AuthProvider } from "components/AuthProvider/AuthProvider"
-import { FC } from "react"
+import { FC, PropsWithChildren } from "react"
 import { HelmetProvider } from "react-helmet-async"
 import { AppRouter } from "./AppRouter"
 import { ErrorBoundary } from "./components/ErrorBoundary/ErrorBoundary"
@@ -19,7 +19,7 @@ const queryClient = new QueryClient({
   },
 })
 
-export const App: FC = () => {
+export const AppProviders: FC<PropsWithChildren> = ({ children }) => {
   return (
     <HelmetProvider>
       <ThemeProvider theme={dark}>
@@ -27,12 +27,20 @@ export const App: FC = () => {
         <ErrorBoundary>
           <QueryClientProvider client={queryClient}>
             <AuthProvider>
-              <AppRouter />
+              {children}
               <GlobalSnackbar />
             </AuthProvider>
           </QueryClientProvider>
         </ErrorBoundary>
       </ThemeProvider>
     </HelmetProvider>
+  )
+}
+
+export const App: FC = () => {
+  return (
+    <AppProviders>
+      <AppRouter />
+    </AppProviders>
   )
 }
