37be42adfc4d31f6d9abe794dec0c7332b722404
Mark Milligan <mark@coder.com>
2023-01-24 03:11:16 +0000

    chore: remove code-server release limitation and add image push policy

--- examples/templates/kubernetes/main.tf
+++ examples/templates/kubernetes/main.tf
@@ -28,7 +28,7 @@ variable "use_kubeconfig" {
 variable "namespace" {
   type        = string
   sensitive   = true
-  description = "The namespace to create workspaces in (must exist prior to creating workspaces)"
+  description = "The Kubernetes namespace to create workspaces in (must exist prior to creating workspaces)"
 }
 
 variable "home_disk_size" {
@@ -54,17 +54,9 @@ resource "coder_agent" "main" {
   startup_script = <<EOT
     #!/bin/bash
 
-    # home folder can be empty, so copying default bash settings
-    if [ ! -f ~/.profile ]; then
-      cp /etc/skel/.profile $HOME
-    fi
-    if [ ! -f ~/.bashrc ]; then
-      cp /etc/skel/.bashrc $HOME
-    fi
-
     # install and start code-server
-    curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 4.8.3 | tee code-server-install.log
-    code-server --auth none --port 13337 | tee code-server-install.log &
+    curl -fsSL https://code-server.dev/install.sh | sh -s
+    code-server --auth none --port 13337 &
   EOT
 }
 
@@ -143,6 +135,7 @@ resource "kubernetes_pod" "main" {
     container {
       name    = "dev"
       image   = "codercom/enterprise-base:ubuntu"
+      image_pull_policy = "Always"
       command = ["sh", "-c", coder_agent.main.init_script]
       security_context {
         run_as_user = "1000"
