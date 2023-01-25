2e69a8aa04548bb6e8d91eb53bc1a0465d7fc0ba
Cian Johnston <cian@coder.com>
2023-01-25 16:34:12 +0000

    err scoping

--- coderd/authzquery/audit.go
+++ coderd/authzquery/audit.go
@@ -15,8 +15,7 @@ func (q *AuthzQuerier) GetAuditLogsOffset(ctx context.Context, arg database.GetA
 	// To optimize audit logs, we only check the global audit log permission once.
 	// This is because we expect a large unbounded set of audit logs, and applying a SQL
 	// filter would slow down the query for no benefit.
-	err := q.authorizeContext(ctx, rbac.ActionRead, rbac.ResourceAuditLog)
-	if err != nil {
+	if err := q.authorizeContext(ctx, rbac.ActionRead, rbac.ResourceAuditLog); err != nil {
 		return nil, err
 	}
 	return q.database.GetAuditLogsOffset(ctx, arg)
