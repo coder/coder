d451c45098bf7a95b61732eeda90b3ce3aa508f2
Mathias Fredriksson <mafredri@gmail.com>
2023-01-25 10:00:44 +0000

    fix(site): Hide agent lifecycle unless opted in via `delay_login_until_ready`
    Refs: #5835

--- site/src/components/Resources/AgentStatus.tsx
+++ site/src/components/Resources/AgentStatus.tsx
@@ -135,22 +135,28 @@ const StartErrorLifecycle: React.FC<{
 const ConnectedStatus: React.FC<{
   agent: WorkspaceAgent
 }> = ({ agent }) => {
-  return (
-    <ChooseOne>
-      <Cond condition={agent.lifecycle_state === "ready"}>
-        <ReadyLifeCycle />
-      </Cond>
-      <Cond condition={agent.lifecycle_state === "start_timeout"}>
-        <StartTimeoutLifecycle agent={agent} />
-      </Cond>
-      <Cond condition={agent.lifecycle_state === "start_error"}>
-        <StartErrorLifecycle agent={agent} />
-      </Cond>
-      <Cond>
-        <StartingLifecycle />
-      </Cond>
-    </ChooseOne>
-  )
+  // NOTE(mafredri): Keep this behind feature flag for the time-being,
+  // if delay_login_until_ready is true, the user has updated to
+  // terraform-provider-coder v0.6.7 and opted in to the functionality.
+  if (agent.delay_login_until_ready) {
+    return (
+      <ChooseOne>
+        <Cond condition={agent.lifecycle_state === "ready"}>
+          <ReadyLifeCycle />
+        </Cond>
+        <Cond condition={agent.lifecycle_state === "start_timeout"}>
+          <StartTimeoutLifecycle agent={agent} />
+        </Cond>
+        <Cond condition={agent.lifecycle_state === "start_error"}>
+          <StartErrorLifecycle agent={agent} />
+        </Cond>
+        <Cond>
+          <StartingLifecycle />
+        </Cond>
+      </ChooseOne>
+    )
+  }
+  return <ReadyLifeCycle />
 }
 
 const DisconnectedStatus: React.FC = () => {
