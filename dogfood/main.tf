8bbc0f8db63745a5af4bd6c4c126c39e580930c7
Mathias Fredriksson <mafredri@gmail.com>
2023-01-27 20:14:38 +0000

    fixup! Change `delay_login_until_ready` to `login_before_ready`

--- dogfood/main.tf
+++ dogfood/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = "0.6.9"
+      version = "0.6.10"
     }
     docker = {
       source  = "kreuzwerker/docker"
