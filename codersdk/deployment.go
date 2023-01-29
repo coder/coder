ab01da73cacd8892302bf9040db96717ee92d83f
Kyle Carberry <kyle@carberry.com>
2023-01-29 17:21:02 +0000

    Merge `experiments.go` and `features.go` into `deployment.go`

--- codersdk/deployment.go
+++ codersdk/deployment.go
@@ -11,6 +11,97 @@ import (
 	"golang.org/x/xerrors"
 )
 
+type Entitlement string
+
+const (
+	EntitlementEntitled    Entitlement = "entitled"
+	EntitlementGracePeriod Entitlement = "grace_period"
+	EntitlementNotEntitled Entitlement = "not_entitled"
+)
+
+// To add a new feature, modify this set of enums as well as the FeatureNames
+// array below.
+type FeatureName string
+
+const (
+	FeatureUserLimit                  FeatureName = "user_limit"
+	FeatureAuditLog                   FeatureName = "audit_log"
+	FeatureBrowserOnly                FeatureName = "browser_only"
+	FeatureSCIM                       FeatureName = "scim"
+	FeatureTemplateRBAC               FeatureName = "template_rbac"
+	FeatureHighAvailability           FeatureName = "high_availability"
+	FeatureMultipleGitAuth            FeatureName = "multiple_git_auth"
+	FeatureExternalProvisionerDaemons FeatureName = "external_provisioner_daemons"
+	FeatureAppearance                 FeatureName = "appearance"
+)
+
+// FeatureNames must be kept in-sync with the Feature enum above.
+var FeatureNames = []FeatureName{
+	FeatureUserLimit,
+	FeatureAuditLog,
+	FeatureBrowserOnly,
+	FeatureSCIM,
+	FeatureTemplateRBAC,
+	FeatureHighAvailability,
+	FeatureMultipleGitAuth,
+	FeatureExternalProvisionerDaemons,
+	FeatureAppearance,
+}
+
+// Humanize returns the feature name in a human-readable format.
+func (n FeatureName) Humanize() string {
+	switch n {
+	case FeatureTemplateRBAC:
+		return "Template RBAC"
+	case FeatureSCIM:
+		return "SCIM"
+	default:
+		return strings.Title(strings.ReplaceAll(string(n), "_", " "))
+	}
+}
+
+// AlwaysEnable returns if the feature is always enabled if entitled.
+// Warning: We don't know if we need this functionality.
+// This method may disappear at any time.
+func (n FeatureName) AlwaysEnable() bool {
+	return map[FeatureName]bool{
+		FeatureMultipleGitAuth:            true,
+		FeatureExternalProvisionerDaemons: true,
+		FeatureAppearance:                 true,
+	}[n]
+}
+
+type Feature struct {
+	Entitlement Entitlement `json:"entitlement"`
+	Enabled     bool        `json:"enabled"`
+	Limit       *int64      `json:"limit,omitempty"`
+	Actual      *int64      `json:"actual,omitempty"`
+}
+
+type Entitlements struct {
+	Features   map[FeatureName]Feature `json:"features"`
+	Warnings   []string                `json:"warnings"`
+	Errors     []string                `json:"errors"`
+	HasLicense bool                    `json:"has_license"`
+	Trial      bool                    `json:"trial"`
+
+	// DEPRECATED: use Experiments instead.
+	Experimental bool `json:"experimental"`
+}
+
+func (c *Client) Entitlements(ctx context.Context) (Entitlements, error) {
+	res, err := c.Request(ctx, http.MethodGet, "/api/v2/entitlements", nil)
+	if err != nil {
+		return Entitlements{}, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return Entitlements{}, readBodyAsError(res)
+	}
+	var ent Entitlements
+	return ent, json.NewDecoder(res.Body).Decode(&ent)
+}
+
 // DeploymentConfig is the central configuration for the coder server.
 type DeploymentConfig struct {
 	AccessURL                       *DeploymentConfigField[string]          `json:"access_url" typescript:",notnull"`
@@ -313,4 +404,51 @@ func (c *Client) BuildInfo(ctx context.Context) (BuildInfoResponse, error) {
 
 	var buildInfo BuildInfoResponse
 	return buildInfo, json.NewDecoder(res.Body).Decode(&buildInfo)
+}
+
+type Experiment string
+
+const (
+	// ExperimentAuthzQuerier is an internal experiment that enables the ExperimentAuthzQuerier
+	// interface for all RBAC operations. NOT READY FOR PRODUCTION USE.
+	ExperimentAuthzQuerier Experiment = "authz_querier"
+
+	// Add new experiments here!
+	// ExperimentExample Experiment = "example"
+)
+
+var (
+	// ExperimentsAll should include all experiments that are safe for
+	// users to opt-in to via --experimental='*'.
+	// Experiments that are not ready for consumption by all users should
+	// not be included here and will be essentially hidden.
+	ExperimentsAll = Experiments{}
+)
+
+// Experiments is a list of experiments that are enabled for the deployment.
+// Multiple experiments may be enabled at the same time.
+// Experiments are not safe for production use, and are not guaranteed to
+// be backwards compatible. They may be removed or renamed at any time.
+type Experiments []Experiment
+
+func (e Experiments) Enabled(ex Experiment) bool {
+	for _, v := range e {
+		if v == ex {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *Client) Experiments(ctx context.Context) (Experiments, error) {
+	res, err := c.Request(ctx, http.MethodGet, "/api/v2/experiments", nil)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, readBodyAsError(res)
+	}
+	var exp []Experiment
+	return exp, json.NewDecoder(res.Body).Decode(&exp)
 }
