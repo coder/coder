9aa7835d0fb992ec33ca763a8a920b37e05e4944
Steven Masley <stevenmasley@coder.com>
2023-01-31 10:31:13 -0600

    Move experiment init below authz init

--- coderd/coderd.go
+++ coderd/coderd.go
@@ -36,11 +36,11 @@ import (
 
 	"cdr.dev/slog"
 	"github.com/coder/coder/buildinfo"
+	"github.com/coder/coder/coderd/authzquery"
 
 	// Used to serve the Swagger endpoint
 	_ "github.com/coder/coder/coderd/apidoc"
 	"github.com/coder/coder/coderd/audit"
-	"github.com/coder/coder/coderd/authzquery"
 	"github.com/coder/coder/coderd/awsidentity"
 	"github.com/coder/coder/coderd/database"
 	"github.com/coder/coder/coderd/database/dbtype"
@@ -156,12 +156,6 @@ func New(options *Options) *API {
 		options = &Options{}
 	}
 	experiments := initExperiments(options.Logger, options.DeploymentConfig.Experiments.Value, options.DeploymentConfig.Experimental.Value)
-	// TODO: remove this once we promote authz_querier out of experiments.
-	if experiments.Enabled(codersdk.ExperimentAuthzQuerier) {
-		if _, ok := (options.Database).(*authzquery.AuthzQuerier); !ok {
-			options.Database = authzquery.NewAuthzQuerier(options.Database, options.Authorizer)
-		}
-	}
 	if options.AppHostname != "" && options.AppHostnameRegex == nil || options.AppHostname == "" && options.AppHostnameRegex != nil {
 		panic("coderd: both AppHostname and AppHostnameRegex must be set or unset")
 	}
@@ -201,6 +195,12 @@ func New(options *Options) *API {
 	}
 	if options.Auditor == nil {
 		options.Auditor = audit.NewNop()
+	}
+	// TODO: remove this once we promote authz_querier out of experiments.
+	if experiments.Enabled(codersdk.ExperimentAuthzQuerier) {
+		if _, ok := (options.Database).(*authzquery.AuthzQuerier); !ok {
+			options.Database = authzquery.NewAuthzQuerier(options.Database, options.Authorizer)
+		}
 	}
 
 	siteCacheDir := options.CacheDir
