26a5e5d084696d7a166565abe7b55820b68e91d7
Marcin Tojek <marcin@coder.com>
2023-02-01 13:40:47 +0100

    All tests

--- site/src/pages/WorkspaceBuildParametersPage/WorkspaceBuildParametersPage.test.tsx
+++ site/src/pages/WorkspaceBuildParametersPage/WorkspaceBuildParametersPage.test.tsx
@@ -8,7 +8,17 @@ import {
   renderWithAuth,
 } from "testHelpers/renderHelpers"
 import * as API from "api/api"
+import i18next from "i18next"
 import { WorkspaceBuildParametersPage } from "./WorkspaceBuildParametersPage"
+
+const { t } = i18next
+
+const pageTitleText = t("title", { ns: "workspaceBuildParametersPage" })
+const validationNumberNotInRangeText = t("validationNumberNotInRange", {
+  ns: "workspaceBuildParametersPage",
+  min: "1",
+  max: "3",
+})
 
 const renderWorkspaceBuildParametersPage = () => {
   return renderWithAuth(<WorkspaceBuildParametersPage />, {
@@ -23,10 +33,15 @@ describe("WorkspaceBuildParametersPage", () => {
     jest
       .spyOn(API, "getTemplateVersionRichParameters")
       .mockResolvedValueOnce([])
-
+    jest
+      .spyOn(API, "getWorkspaceBuildParameters")
+      .mockResolvedValueOnce([
+        MockWorkspaceBuildParameter1,
+        MockWorkspaceBuildParameter2,
+      ])
     renderWorkspaceBuildParametersPage()
 
-    const element = await screen.findByText("Workspace build parameters")
+    const element = await screen.findByText(pageTitleText)
     expect(element).toBeDefined()
 
     const goBackButton = await screen.findByText("Go back")
@@ -50,7 +65,7 @@ describe("WorkspaceBuildParametersPage", () => {
 
     renderWorkspaceBuildParametersPage()
 
-    const element = await screen.findByText("Workspace build parameters")
+    const element = await screen.findByText(pageTitleText)
     expect(element).toBeDefined()
 
     const firstParameter = await screen.findByLabelText(
@@ -79,7 +94,7 @@ describe("WorkspaceBuildParametersPage", () => {
       ])
     renderWorkspaceBuildParametersPage()
 
-    const element = await screen.findByText("Workspace build parameters")
+    const element = await screen.findByText(pageTitleText)
     expect(element).toBeDefined()
     const secondParameter = await screen.findByText(
       MockTemplateVersionParameter2.description,
@@ -97,7 +112,7 @@ describe("WorkspaceBuildParametersPage", () => {
     fireEvent.submit(secondParameter)
 
     const validationError = await screen.findByText(
-      "Value must be between 1 and 3.",
+      validationNumberNotInRangeText,
     )
     expect(validationError).toBeDefined()
   })
