5a0e5a27efe5ae2386a1173c980ab9bf24bfe79b
Cian Johnston <cian@coder.com>
2023-02-02 17:00:02 +0000

    add existing workspace tests

--- coderd/authzquery/workspace_test.go
+++ coderd/authzquery/workspace_test.go
@@ -3,6 +3,8 @@ package authzquery_test
 import (
 	"testing"
 
+	"github.com/google/uuid"
+
 	"github.com/coder/coder/coderd/database"
 	"github.com/coder/coder/coderd/database/dbgen"
 	"github.com/coder/coder/coderd/rbac"
@@ -11,8 +13,43 @@ import (
 func (s *MethodTestSuite) TestWorkspace() {
 	s.Run("GetWorkspaceByID", func() {
 		s.RunMethodTest(func(t *testing.T, db database.Store) MethodCase {
-			workspace := dbgen.Workspace(t, db, database.Workspace{})
-			return methodCase(inputs(workspace.ID), asserts(workspace, rbac.ActionRead))
+			ws := dbgen.Workspace(t, db, database.Workspace{})
+			return methodCase(inputs(ws.ID), asserts(ws, rbac.ActionRead))
+		})
+	})
+	s.Run("GetWorkspaces", func() {
+		s.RunMethodTest(func(t *testing.T, db database.Store) MethodCase {
+			_ = dbgen.Workspace(t, db, database.Workspace{})
+			_ = dbgen.Workspace(t, db, database.Workspace{})
+			// No asserts here because SQLFilter.
+			return methodCase(inputs(database.GetWorkspacesParams{}), asserts())
+		})
+	})
+	s.Run("GetLatestWorkspaceBuildByWorkspaceID", func() {
+		s.RunMethodTest(func(t *testing.T, db database.Store) MethodCase {
+			ws := dbgen.Workspace(t, db, database.Workspace{})
+			_ = dbgen.WorkspaceBuild(t, db, database.WorkspaceBuild{WorkspaceID: ws.ID})
+			return methodCase(inputs(ws.ID), asserts(ws, rbac.ActionRead))
+		})
+	})
+	s.Run("GetLatestWorkspaceBuildsByWorkspaceIDs", func() {
+		s.RunMethodTest(func(t *testing.T, db database.Store) MethodCase {
+			ws := dbgen.Workspace(t, db, database.Workspace{})
+			return methodCase(
+				inputs([]uuid.UUID{ws.ID}),
+				asserts(ws, rbac.ActionRead))
+		})
+	})
+	s.Run("GetWorkspaceAgentByID", func() {
+		s.RunMethodTest(func(t *testing.T, db database.Store) MethodCase {
+			agt := dbgen.WorkspaceAgent(t, db, database.WorkspaceAgent{})
+			return methodCase(inputs(agt.ID), asserts())
+		})
+	})
+	s.Run("GetWorkspaceAgentByInstanceID", func() {
+		s.RunMethodTest(func(t *testing.T, db database.Store) MethodCase {
+			agt := dbgen.WorkspaceAgent(t, db, database.WorkspaceAgent{})
+			return methodCase(inputs(agt.AuthInstanceID.String), asserts())
 		})
 	})
 }
