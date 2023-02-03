8085abc62e71877d2f6b9bf35b65979efaa0bb3b
Steven Masley <stevenmasley@coder.com>
2023-02-03 12:31:22 -0600

    import order

--- coderd/util/slice/slice_test.go
+++ coderd/util/slice/slice_test.go
@@ -4,9 +4,8 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/coder/coder/coderd/util/slice"
