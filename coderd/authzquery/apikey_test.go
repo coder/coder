92f89ecc9c50ea1a756d060194e7e4d73039ea60
Steven Masley <stevenmasley@coder.com>
2023-02-03 09:57:57 -0600

    fix api key unit tests to expect outputs

--- coderd/authzquery/apikey_test.go
+++ coderd/authzquery/apikey_test.go
@@ -13,13 +13,13 @@ func (suite *MethodTestSuite) TestAPIKey() {
 	suite.Run("DeleteAPIKeyByID", func() {
 		suite.RunMethodTest(func(t *testing.T, db database.Store) MethodCase {
 			key, _ := dbgen.APIKey(t, db, database.APIKey{})
-			return methodCase(values(key.ID), asserts(key, rbac.ActionDelete))
+			return methodCase(values(key.ID), asserts(key, rbac.ActionDelete), values())
 		})
 	})
 	suite.Run("GetAPIKeyByID", func() {
 		suite.RunMethodTest(func(t *testing.T, db database.Store) MethodCase {
 			key, _ := dbgen.APIKey(t, db, database.APIKey{})
-			return methodCase(values(key.ID), asserts(key, rbac.ActionRead))
+			return methodCase(values(key.ID), asserts(key, rbac.ActionRead), values(key))
 		})
 	})
 	suite.Run("GetAPIKeysByLoginType", func() {
@@ -27,7 +27,9 @@ func (suite *MethodTestSuite) TestAPIKey() {
 			a, _ := dbgen.APIKey(t, db, database.APIKey{LoginType: database.LoginTypePassword})
 			b, _ := dbgen.APIKey(t, db, database.APIKey{LoginType: database.LoginTypePassword})
 			_, _ = dbgen.APIKey(t, db, database.APIKey{LoginType: database.LoginTypeGithub})
-			return methodCase(values(database.LoginTypePassword), asserts(a, rbac.ActionRead, b, rbac.ActionRead))
+			return methodCase(values(database.LoginTypePassword),
+				asserts(a, rbac.ActionRead, b, rbac.ActionRead),
+				values([]database.APIKey{a, b}))
 		})
 	})
 	suite.Run("GetAPIKeysLastUsedAfter", func() {
@@ -35,7 +37,9 @@ func (suite *MethodTestSuite) TestAPIKey() {
 			a, _ := dbgen.APIKey(t, db, database.APIKey{LastUsed: time.Now().Add(time.Hour)})
 			b, _ := dbgen.APIKey(t, db, database.APIKey{LastUsed: time.Now().Add(time.Hour)})
 			_, _ = dbgen.APIKey(t, db, database.APIKey{LastUsed: time.Now().Add(-time.Hour)})
-			return methodCase(values(time.Now()), asserts(a, rbac.ActionRead, b, rbac.ActionRead))
+			return methodCase(values(time.Now()),
+				asserts(a, rbac.ActionRead, b, rbac.ActionRead),
+				values([]database.APIKey{a, b}))
 		})
 	})
 	suite.Run("InsertAPIKey", func() {
@@ -45,16 +49,16 @@ func (suite *MethodTestSuite) TestAPIKey() {
 				UserID:    u.ID,
 				LoginType: database.LoginTypePassword,
 				Scope:     database.APIKeyScopeAll,
-			}), asserts(rbac.ResourceAPIKey.WithOwner(u.ID.String()), rbac.ActionCreate))
+			}), asserts(rbac.ResourceAPIKey.WithOwner(u.ID.String()), rbac.ActionCreate),
+				values())
 		})
 	})
 	suite.Run("UpdateAPIKeyByID", func() {
 		suite.RunMethodTest(func(t *testing.T, db database.Store) MethodCase {
 			a, _ := dbgen.APIKey(t, db, database.APIKey{})
 			return methodCase(values(database.UpdateAPIKeyByIDParams{
-				ID:       a.ID,
-				LastUsed: time.Now(),
-			}), asserts(a, rbac.ActionUpdate))
+				ID: a.ID,
+			}), asserts(a, rbac.ActionUpdate), values(a))
 		})
 	})
 }
