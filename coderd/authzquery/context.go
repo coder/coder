ca68db26048d43424ad8ac0fb0527fb50eb79c87
Steven Masley <stevenmasley@coder.com>
2023-02-07 09:19:04 -0600

    Remove duplicate workspace agent scope

--- coderd/authzquery/context.go
+++ coderd/authzquery/context.go
@@ -30,47 +30,6 @@ func WithAuthorizeContext(ctx context.Context, actor rbac.Subject) context.Conte
 	return context.WithValue(ctx, authContextKey{}, actor)
 }
 
-// WithWorkspaceAgentTokenContext returns a context with a workspace agent token
-// authorization subject. A workspace agent authorization subject is the
-// workspace owner's authorization subject + a workspace agent scope.
-//
-// TODO: The arguments and usage of this function are not finalized. It might
-// be a bit awkward to use at present. The arguments are required to build the
-// required authorization context. The arguments should be the owner of the
-// workspace authorization roles.
-func WithWorkspaceAgentTokenContext(ctx context.Context, workspaceID uuid.UUID, actorID uuid.UUID, roles rbac.ExpandableRoles, groups []string) context.Context {
-	// TODO: This workspace ID should be applied in the scope.
-	var _ = workspaceID
-	return context.WithValue(ctx, authContextKey{}, rbac.Subject{
-		ID:    actorID.String(),
-		Roles: roles,
-		Scope: rbac.Scope{
-			Role: rbac.Role{
-				Name:        "workspace-agent-scope",
-				DisplayName: "Workspace Agent Scope",
-				// TODO: More permissions are needed for the agent to work.
-				Site: []rbac.Permission{
-					{
-						ResourceType: rbac.ResourceWorkspace.Type,
-						Action:       rbac.ActionRead,
-					},
-					{
-						ResourceType: rbac.ResourceWorkspace.Type,
-						Action:       rbac.ActionRead,
-					},
-					// TODO: Read the workspace owner user.
-				},
-				Org:  map[string][]rbac.Permission{},
-				User: []rbac.Permission{},
-			},
-			// TODO: We need to whitelist more resources such as the workspace
-			// owner.
-			AllowIDList: []string{workspaceID.String()},
-		},
-		Groups: groups,
-	})
-}
-
 // ActorFromContext returns the authorization subject from the context.
 // All authentication flows should set the authorization subject in the context.
 // If no actor is present, the function returns false.
