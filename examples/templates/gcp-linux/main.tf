ab9cba9396f2c7b2ba9195c317f570aed7d67bd1
Cian Johnston <cian@coder.com>
2023-02-10 13:36:46 +0000

    chore: update template examples and docs to install code-server under /tmp (#6138)
    Updates templates examples and docs to pass the --method standalone argument
    to code-server install script, and installs code-server under /tmp/code-server.
    
    Co-authored-by: Mathias Fredriksson <mafredri@gmail.com>
    
    ---------
    
    Co-authored-by: Mathias Fredriksson <mafredri@gmail.com>

--- examples/templates/gcp-linux/main.tf
+++ examples/templates/gcp-linux/main.tf
@@ -56,8 +56,8 @@ resource "coder_agent" "main" {
     set -e
 
     # install and start code-server
-    curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 4.8.3
-    code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
+    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.8.3
+    /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
   EOT
 }
 
