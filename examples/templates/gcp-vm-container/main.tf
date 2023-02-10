049218e1a9023a04d2ba7d300364ff6a1e5a60c1
Cian Johnston <cian@coder.com>
2023-02-10 12:29:31 +0000

    fixup! chore: update template examples and docs to install code-server under /tmp

--- examples/templates/gcp-vm-container/main.tf
+++ examples/templates/gcp-vm-container/main.tf
@@ -45,10 +45,9 @@ resource "coder_agent" "main" {
   startup_script         = <<-EOT
     set -e
 
-    # Install and start code-server. We install under /tmp as we may not have permission to write outside /home/coder.
-    # The standalone method also requires no sudo or root permissions, as opposed to the autodetect method.
-    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server
-    /tmp/code-server/bin/code-server --auth none --port 13337 &
+    # Install and start code-server.
+    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.8.3
+    /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
   EOT
 }
 
