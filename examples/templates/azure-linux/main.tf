bbdf24686dc51853c27a94eab1029214d263e6a2
Colin Adler <colin1adler@gmail.com>
2023-02-22 14:43:32 -0700

    chore(examples): update provider versions (#6311)

--- examples/templates/azure-linux/main.tf
+++ examples/templates/azure-linux/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = "0.6.10"
+      version = "~> 0.6.12"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
