a090cd4bd961253a63ba8c3b0a7639363ed3e604
Ammar Bandukwala <ammar@ammar.io>
2023-03-06 19:06:56 +0000

    Remove dead function

--- cli/config/file.go
+++ cli/config/file.go
@@ -48,10 +48,6 @@ func (r Root) PostgresPort() File {
 	return File(filepath.Join(r.PostgresPath(), "port"))
 }
 
-func (r Root) DeploymentValuesPath() string {
-	return filepath.Join(string(r), "server.yaml")
-}
-
 // File provides convenience methods for interacting with *os.File.
 type File string
 
