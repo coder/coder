18d31bffdda82918b7a2a9628c04dd432ffed76a
Ammar Bandukwala <ammar@ammar.io>
2023-03-06 22:05:48 +0000

    Fixup middleware

--- cli/clibase/cmd.go
+++ cli/clibase/cmd.go
@@ -25,7 +25,11 @@ type Cmd struct {
 	Long        string
 	Options     *OptionSet
 	Annotations Annotations
-	Handler     Handler
+
+	// Middleware is called before the Handler.
+	// Use Chain() to combine multiple middlewares.
+	Middleware Middleware
+	Handler    Handler
 }
 
 // Name returns the first word in the Use string.
@@ -63,6 +67,8 @@ type Invokation struct {
 	ctx     context.Context
 	Command *Command
 	Args    []string
+	// Environ is the environment variables is os.Environ() form.
+	Environ []string
 	Stdout  io.Writer
 	Stderr  io.Writer
 	Stdin   io.Reader
@@ -82,7 +88,8 @@ func (i *Invokation) Exit(err error) {
 
 	i.err = err
 	// Goexit simulates an os.Exit, but can be captured in tests and
-	// middleware.
+	// middleware. Production callers may switch on err to determine the right
+	// exit code. Perhaps in the future we could add an ExitCoder interface.
 	runtime.Goexit()
 }
 
@@ -90,7 +97,7 @@ func (i *Invokation) Run() error {
 	waitDone := make(chan struct{})
 	go func() {
 		defer close(waitDone)
-		i.Command.Handler.ServeCommand(i)
+		i.Command.Middleware(i.Command.Handler).ServeCommand(i)
 	}()
 	<-waitDone
 	return i.err
@@ -108,15 +115,26 @@ func (i *Invokation) WithContext(ctx context.Context) *Invokation {
 // or nil if there are no more.
 type Middleware func(next Handler) Handler
 
-// Chain returns a Handler that first calls middleware in order.
-func Chain(h Handler, ms ...Middleware) Handler {
-	return HandlerFunc(func(i *Invokation) {
-		if len(ms) == 0 {
-			h.ServeCommand(i)
-			return
+func chain(ms ...Middleware) Middleware {
+	return Middleware(func(next Handler) Handler {
+		if len(ms) > 0 {
+			return chain(ms[1:]...)(ms[0](next))
 		}
-		Chain(ms[0](h), ms[1:]...).ServeCommand(i)
+		return next
 	})
+}
+
+// Chain returns a Handler that first calls middleware in order.
+//
+//nolint:revive
+func Chain(ms ...Middleware) Middleware {
+	// We need to reverse the array to provide top-to-bottom execution
+	// order when defining a command.
+	reversed := make([]Middleware, len(ms))
+	for i := range ms {
+		reversed[len(ms)-1-i] = ms[i]
+	}
+	return chain(reversed...)
 }
 
 func RequireNArgs(want int) Middleware {
--- cli/clibase/command_test.go
+++ cli/clibase/command_test.go
@@ -16,16 +16,16 @@ func TestCommand_ToUpper(t *testing.T) {
 	cmd := &clibase.Command{
 		Use:   "toupper [word]",
 		Short: "Converts a word to upper case",
-		Handler: clibase.Chain(
-			clibase.HandlerFunc(func(i *clibase.Invokation) {
-				_, _ = i.Stdout.Write(
-					[]byte(
-						strings.ToUpper(i.Args[0]),
-					),
-				)
-			}),
+		Middleware: clibase.Chain(
 			clibase.RequireNArgs(1),
 		),
+		Handler: clibase.HandlerFunc(func(i *clibase.Invokation) {
+			_, _ = i.Stdout.Write(
+				[]byte(
+					strings.ToUpper(i.Args[0]),
+				),
+			)
+		}),
 	}
 
 	t.Run("OK", func(t *testing.T) {
@@ -50,4 +50,37 @@ func TestCommand_ToUpper(t *testing.T) {
 		require.Empty(t, io.Stdout.String())
 		require.Error(t, err)
 	})
+}
+
+func TestCommand_MiddlewareOrder(t *testing.T) {
+	t.Parallel()
+
+	mw := func(letter string) clibase.Middleware {
+		return func(next clibase.Handler) clibase.Handler {
+			return clibase.HandlerFunc(func(i *clibase.Invokation) {
+				_, _ = i.Stdout.Write([]byte(letter))
+				next.ServeCommand(i)
+			})
+		}
+	}
+
+	cmd := &clibase.Command{
+		Use:   "toupper [word]",
+		Short: "Converts a word to upper case",
+		Middleware: clibase.Chain(
+			mw("A"),
+			mw("B"),
+			mw("C"),
+		),
+		Handler: clibase.HandlerFunc(func(i *clibase.Invokation) {
+		}),
+	}
+
+	i := &clibase.Invokation{
+		Args:    []string{"hello", "world"},
+		Command: cmd,
+	}
+	io := clibasetest.FakeIO(i)
+	require.NoError(t, i.Run())
+	require.Equal(t, "ABC", io.Stdout.String())
 }
