0813255810a4817d1cdb7f215211da3be30dde4b
Marcin Tojek <marcin@coder.com>
2023-03-06 10:57:03 +0100

    Fix

--- site/src/testHelpers/entities.ts
+++ site/src/testHelpers/entities.ts
@@ -731,6 +731,7 @@ export const MockTemplateVersionParameter1: TypesGen.TemplateVersionParameter =
     mutable: true,
     icon: "/icon/folder.svg",
     options: [],
+    required: true,
   }
 
 export const MockTemplateVersionParameter2: TypesGen.TemplateVersionParameter =
@@ -746,6 +747,7 @@ export const MockTemplateVersionParameter2: TypesGen.TemplateVersionParameter =
     validation_min: 1,
     validation_max: 3,
     validation_monotonic: "increasing",
+    required: true,
   }
 
 export const MockTemplateVersionParameter3: TypesGen.TemplateVersionParameter =
@@ -760,6 +762,7 @@ export const MockTemplateVersionParameter3: TypesGen.TemplateVersionParameter =
     options: [],
     validation_error: "No way!",
     validation_regex: "^[a-z]{3}$",
+    required: true,
   }
 
 export const MockTemplateVersionParameter4: TypesGen.TemplateVersionParameter =
@@ -772,6 +775,7 @@ export const MockTemplateVersionParameter4: TypesGen.TemplateVersionParameter =
     mutable: false,
     icon: "/icon/database.svg",
     options: [],
+    required: true,
   }
 
 export const MockTemplateVersionParameter5: TypesGen.TemplateVersionParameter =
@@ -787,6 +791,7 @@ export const MockTemplateVersionParameter5: TypesGen.TemplateVersionParameter =
     validation_min: 1,
     validation_max: 10,
     validation_monotonic: "decreasing",
+    required: true,
   }
 
 export const MockTemplateVersionVariable1: TypesGen.TemplateVersionVariable = {
