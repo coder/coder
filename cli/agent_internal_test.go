e2a8c11d90a92a609e70362e2e188f8b7f384282
Steven Masley <stevenmasley@coder.com>
2023-03-08 14:50:14 -0600

    Linting

--- cli/agent_internal_test.go
+++ cli/agent_internal_test.go
@@ -8,6 +8,8 @@ import (
 )
 
 func Test_extractPort(t *testing.T) {
+	t.Parallel()
+
 	tests := []struct {
 		name      string
 		urlString string
@@ -46,7 +48,9 @@ func Test_extractPort(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			got, err := extractPort(tt.urlString)
 			if tt.wantErr {
 				require.Error(t, err, fmt.Sprintf("extractPort(%v)", tt.urlString))
