71f2c322025bc7cdf48420efb9e2c1e37af036d3
Ammar Bandukwala <ammar@ammar.io>
2023-03-09 20:37:50 +0000

    Reduce indirection in clibasetest.IO

--- cli/clibase/clibasetest/invokation.go
+++ cli/clibase/clibasetest/invokation.go
@@ -10,22 +10,18 @@ import (
 
 // IO is the standard input, output, and error for a command.
 type IO struct {
-	Stdin  *bytes.Buffer
-	Stdout *bytes.Buffer
-	Stderr *bytes.Buffer
+	Stdin  bytes.Buffer
+	Stdout bytes.Buffer
+	Stderr bytes.Buffer
 }
 
 // FakeIO sets Stdin, Stdout, and Stderr to buffers.
 func FakeIO(i *clibase.Invokation) *IO {
-	b := &IO{
-		Stdin:  bytes.NewBuffer(nil),
-		Stdout: bytes.NewBuffer(nil),
-		Stderr: bytes.NewBuffer(nil),
-	}
-	i.Stdout = b.Stdout
-	i.Stderr = b.Stderr
-	i.Stdin = b.Stdin
-	return b
+	var b IO
+	i.Stdout = &b.Stdout
+	i.Stderr = &b.Stderr
+	i.Stdin = &b.Stdin
+	return &b
 }
 
 type testWriter struct {
