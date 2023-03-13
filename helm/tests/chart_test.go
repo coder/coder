b7a9dee9b35957f68fdceccd613fb3ff03858e00
Cian Johnston <cian@coder.com>
2023-03-13 12:18:21 +0000

    rewrite to just use helm template

--- helm/tests/chart_test.go
+++ helm/tests/chart_test.go
@@ -1,16 +1,20 @@
 package tests // nolint: testpackage
 
 import (
+	"bytes"
 	"flag"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"helm.sh/helm/v3/pkg/chartutil"
+	"golang.org/x/xerrors"
 )
 
-// These tests render the chart with the given test case and compares the output
-// to the corresponding golden file.
+// These tests run `helm template` with the values file specified in each test
+// and compare the output to the contents of the corresponding golden file.
+// All values and golden files are located in the `testdata` directory.
 // To update golden files, run `go test . -update`.
 
 // UpdateGoldenFiles is a flag that can be set to update golden files.
@@ -18,31 +22,26 @@ var UpdateGoldenFiles = flag.Bool("update", false, "Update golden files")
 
 var TestCases = []TestCase{
 	{
-		name: "default_values",
-		fn: func(v *Values) {
-			v.Coder.Image.Tag = "latest"
-		},
+		valuesFile:    "default_values.yaml",
+		goldenFile:    "default_values.golden",
+		expectedError: "",
 	},
 	{
-		name:      "missing_values",
-		fn:        func(v *Values) {},
-		renderErr: `You must specify the coder.image.tag value if you're installing the Helm chart directly from Git.`,
+		valuesFile:    "missing_values.yaml",
+		goldenFile:    "",
+		expectedError: `You must specify the coder.image.tag value if you're installing the Helm chart directly from Git.`,
 	},
 	{
-		name: "tls",
-		fn: func(v *Values) {
-			v.Coder.Image.Tag = "latest"
-			v.Coder.TLS.SecretNames = []string{"coder-tls"}
-		},
+		valuesFile:    "tls.yaml",
+		goldenFile:    "tls.golden",
+		expectedError: "",
 	},
 }
 
 type TestCase struct {
-	name      string                    // Name of the test case. This corresponds to the golden file name.
-	fn        func(*Values)             // Function that mutates the values.
-	opts      *chartutil.ReleaseOptions // Release options to pass to the renderer.
-	caps      *chartutil.Capabilities   // Capabilities to pass to the renderer.
-	renderErr string                    // Expected error from rendering the chart.
+	valuesFile    string // Values file to be passed to `helm template`.
+	goldenFile    string // Expected output from running `helm template`.
+	expectedError string // Expected error from running `helm template`.
 }
 
 func TestRenderChart(t *testing.T) {
@@ -51,26 +50,38 @@ func TestRenderChart(t *testing.T) {
 		t.Skip("Golden files are being updated. Skipping test.")
 	}
 
+	// Ensure that Helm is available in $PATH
+	helmPath := lookupHelm(t)
 	for _, tc := range TestCases {
 		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
+		require.NotEmpty(t, tc.valuesFile, "all test cases must specify a values file")
+		t.Run(tc.valuesFile, func(t *testing.T) {
 			t.Parallel()
 
-			w, err := LoadChart()
-			require.NoError(t, err)
-			require.NoError(t, w.Chart.Validate())
-			manifests, err := renderManifests(w.Chart, w.OriginalValues, tc.fn, tc.opts, tc.caps)
-			if tc.renderErr != "" {
-				require.Error(t, err, "render should have failed")
-				require.Contains(t, err.Error(), tc.renderErr, "render error should match")
-				require.Empty(t, manifests, "manifests should be empty")
+			// Ensure that the values file exists.
+			valuesFilePath := filepath.Join("testdata", tc.valuesFile)
+			if _, err := os.Stat(valuesFilePath); os.IsNotExist(err) {
+				t.Fatalf("values file %q does not exist", valuesFilePath)
+			}
+
+			// Run helm template with the values file.
+			templateOutput, err := runHelmTemplate(t, helmPath, "..", valuesFilePath)
+			if tc.expectedError != "" {
+				require.Error(t, err, "helm template should have failed")
+				require.Contains(t, templateOutput, tc.expectedError, "helm template output should contain expected error")
 			} else {
-				require.NoError(t, err, "render should not have failed")
-				require.NotEmpty(t, manifests, "manifests should not be empty")
-				expected, err := readGoldenFile(tc.name)
-				require.NoError(t, err, "failed to load golden file")
-				actual := dumpManifests(manifests)
-				require.Equal(t, expected, actual)
+				require.NoError(t, err, "helm template should not have failed")
+				require.NotEmpty(t, templateOutput, "helm template output should not be empty")
+				goldenFilePath := filepath.Join("testdata", tc.goldenFile)
+				goldenBytes, err := os.ReadFile(goldenFilePath)
+				require.NoError(t, err, "failed to read golden file %q", goldenFilePath)
+
+				// Remove carriage returns to make tests pass on Windows.
+				goldenBytes = bytes.Replace(goldenBytes, []byte("\r"), []byte(""), -1)
+				expected := string(goldenBytes)
+
+				require.NoError(t, err, "failed to load golden file %q")
+				require.Equal(t, expected, templateOutput)
 			}
 		})
 	}
@@ -81,18 +92,50 @@ func TestUpdateGoldenFiles(t *testing.T) {
 	if !*UpdateGoldenFiles {
 		t.Skip("Run with -update to update golden files")
 	}
+
+	helmPath := lookupHelm(t)
 	for _, tc := range TestCases {
-		w, err := LoadChart()
-		require.NoError(t, err, "failed to load chart")
-		if tc.renderErr != "" {
-			t.Logf("skipping test case %q with render error", tc.name)
+		if tc.expectedError != "" {
+			t.Logf("skipping test case %q with render error", tc.valuesFile)
 			continue
 		}
-		manifests, err := renderManifests(w.Chart, w.OriginalValues, tc.fn, tc.opts, tc.caps)
-		require.NoError(t, err, "failed to render manifests")
-		require.NoError(t, writeGoldenFile(tc.name, manifests), "failed to write golden file")
+
+		valuesPath := filepath.Join("testdata", tc.valuesFile)
+		templateOutput, err := runHelmTemplate(t, helmPath, "..", valuesPath)
+
+		require.NoError(t, err, "failed to run `helm template -f %q`", valuesPath)
+
+		goldenFilePath := filepath.Join("testdata", tc.goldenFile)
+		err = os.WriteFile(goldenFilePath, []byte(templateOutput), 0644) // nolint:gosec
+		require.NoError(t, err, "failed to write golden file %q", goldenFilePath)
 	}
 	t.Log("Golden files updated. Please review the changes and commit them.")
+}
+
+// runHelmTemplate runs helm template on the given chart with the given values and
+// returns the raw output.
+func runHelmTemplate(t testing.TB, helmPath, chartDir, valuesFilePath string) (string, error) {
+	// Ensure that valuesFilePath exists
+	if _, err := os.Stat(valuesFilePath); err != nil {
+		return "", xerrors.Errorf("values file %q does not exist: %w", valuesFilePath, err)
+	}
+
+	cmd := exec.Command(helmPath, "template", chartDir, "-f", valuesFilePath)
+	t.Logf("exec command: %v", cmd.Args)
+	out, err := cmd.CombinedOutput()
+	return string(out), err
+}
+
+// lookupHelm ensures that Helm is available in $PATH and returns the path to the
+// Helm executable.
+func lookupHelm(t testing.TB) string {
+	helmPath, err := exec.LookPath("helm")
+	if err != nil {
+		t.Fatalf("helm not found in $PATH: %v", err)
+		return ""
+	}
+	t.Logf("Using helm at %q", helmPath)
+	return helmPath
 }
 
 func TestMain(m *testing.M) {
