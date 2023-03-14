821664071fcd17f14facc9f4848498f8bb474724
Ammar Bandukwala <ammar@ammar.io>
2023-03-14 16:24:37 +0000

    fix lint

--- testutil/ctx.go
+++ testutil/ctx.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Context(t *testing.T, dur time.Duration) context.Context {
-	ctx, cancel := context.WithTimeout(context.Background(), WaitLong)
+	ctx, cancel := context.WithTimeout(context.Background(), dur)
 	t.Cleanup(cancel)
 	return ctx
 }
