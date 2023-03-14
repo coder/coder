311b20c58da0ec8cf0015baf1ffeaec2b9100725
Muhammad Atif Ali <matifali@live.com>
2023-03-14 18:09:26 +0300

    fix docker migration

--- examples/templates/docker/main.tf
+++ examples/templates/docker/main.tf
@@ -95,7 +95,7 @@ resource "docker_volume" "home_volume" {
 resource "docker_image" "main" {
   name = "coder-${data.coder_workspace.me.id}"
   build {
-    path = "./build"
+    context = "./build"
     build_args = {
       USER = local.username
     }
