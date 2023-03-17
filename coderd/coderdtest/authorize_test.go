c555c5750bcd5a4a3b3e67e1090bc105474d8152
Steven Masley <stevenmasley@coder.com>
2023-03-17 10:14:52 -0500

    Remove AuthorizeAllEndpoints

--- coderd/coderdtest/authorize_test.go
+++ coderd/coderdtest/authorize_test.go
@@ -3,33 +3,13 @@ package coderdtest_test
 import (
 	"context"
 	"math/rand"
-	"os"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/coder/coder/coderd/coderdtest"
 	"github.com/coder/coder/coderd/rbac"
-	"github.com/coder/coder/codersdk"
 )
-
-func TestAuthorizeAllEndpoints(t *testing.T) {
-	if strings.Contains(os.Getenv("CODER_EXPERIMENTS_TEST"), string(codersdk.ExperimentAuthzQuerier)) {
-		t.Skip("Skipping TestAuthorizeAllEndpoints for authz_querier experiment")
-	}
-	t.Parallel()
-	client, _, api := coderdtest.NewWithAPI(t, &coderdtest.Options{
-		// Required for any subdomain-based proxy tests to pass.
-		AppHostname:              "*.test.coder.com",
-		Authorizer:               &coderdtest.RecordingAuthorizer{Wrapped: &coderdtest.FakeAuthorizer{}},
-		IncludeProvisionerDaemon: true,
-	})
-	admin := coderdtest.CreateFirstUser(t, client)
-	a := coderdtest.NewAuthTester(context.Background(), t, client, api, admin)
-	skipRoute, assertRoute := coderdtest.AGPLRoutes(a)
-	a.Test(context.Background(), assertRoute, skipRoute)
-}
 
 func TestAuthzRecorder(t *testing.T) {
 	t.Parallel()
