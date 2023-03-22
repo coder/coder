f5a96733eb00411dac9635aba892c8f53afb0a48
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2023-03-22 16:00:44 +0000

    Remove schedule from general settings

--- site/src/pages/TemplateSettingsPage/TemplateGeneralSettingsPage/TemplateSettingsForm.tsx
+++ site/src/pages/TemplateSettingsPage/TemplateGeneralSettingsPage/TemplateSettingsForm.tsx
@@ -11,7 +11,6 @@ import {
 import * as Yup from "yup"
 import i18next from "i18next"
 import { useTranslation } from "react-i18next"
-import { Maybe } from "components/Conditionals/Maybe"
 import { LazyIconField } from "components/IconField/LazyIconField"
 import {
   FormFields,
@@ -23,28 +22,8 @@ import { Stack } from "components/Stack/Stack"
 import Checkbox from "@material-ui/core/Checkbox"
 import { HelpTooltip, HelpTooltipText } from "components/Tooltips/HelpTooltip"
 import { makeStyles } from "@material-ui/core/styles"
-import Link from "@material-ui/core/Link"
-
-const TTLHelperText = ({
-  ttl,
-  translationName,
-}: {
-  ttl?: number
-  translationName: string
-}) => {
-  const { t } = useTranslation("templateSettingsPage")
-  const count = typeof ttl !== "number" ? 0 : ttl
-  return (
-    // no helper text if ttl is negative - error will show once field is considered touched
-    <Maybe condition={count >= 0}>
-      <span>{t(translationName, { count })}</span>
-    </Maybe>
-  )
-}
 
 const MAX_DESCRIPTION_CHAR_LIMIT = 128
-const MAX_TTL_DAYS = 7
-const MS_HOUR_CONVERSION = 3600000
 
 export const getValidationSchema = (): Yup.AnyObjectSchema =>
   Yup.object({
@@ -58,20 +37,7 @@ export const getValidationSchema = (): Yup.AnyObjectSchema =>
       MAX_DESCRIPTION_CHAR_LIMIT,
       i18next.t("descriptionMaxError", { ns: "templateSettingsPage" }),
     ),
-    default_ttl_ms: Yup.number()
-      .integer()
-      .min(0, i18next.t("defaultTTLMinError", { ns: "templateSettingsPage" }))
-      .max(
-        24 * MAX_TTL_DAYS /* 7 days in hours */,
-        i18next.t("defaultTTLMaxError", { ns: "templateSettingsPage" }),
-      ),
-    max_ttl_ms: Yup.number()
-      .integer()
-      .min(0, i18next.t("maxTTLMinError", { ns: "templateSettingsPage" }))
-      .max(
-        24 * MAX_TTL_DAYS /* 7 days in hours */,
-        i18next.t("maxTTLMaxError", { ns: "templateSettingsPage" }),
-      ),
+
     allow_user_cancel_workspace_jobs: Yup.boolean(),
   })
 
@@ -81,7 +47,6 @@ export interface TemplateSettingsForm {
   onCancel: () => void
   isSubmitting: boolean
   error?: unknown
-  canSetMaxTTL: boolean
   // Helpful to show field errors on Storybook
   initialTouched?: FormikTouched<UpdateTemplateMeta>
 }
@@ -91,11 +56,9 @@ export const TemplateSettingsForm: FC<TemplateSettingsForm> = ({
   onSubmit,
   onCancel,
   error,
-  canSetMaxTTL,
   isSubmitting,
   initialTouched,
 }) => {
-  const { t: commonT } = useTranslation("common")
   const validationSchema = getValidationSchema()
   const form: FormikContextType<UpdateTemplateMeta> =
     useFormik<UpdateTemplateMeta>({
@@ -103,28 +66,12 @@ export const TemplateSettingsForm: FC<TemplateSettingsForm> = ({
         name: template.name,
         display_name: template.display_name,
         description: template.description,
-        // on display, convert from ms => hours
-        default_ttl_ms: template.default_ttl_ms / MS_HOUR_CONVERSION,
-        // the API ignores this value, but to avoid tripping up validation set
-        // it to zero if the user can't set the field.
-        max_ttl_ms: canSetMaxTTL ? template.max_ttl_ms / MS_HOUR_CONVERSION : 0,
         icon: template.icon,
         allow_user_cancel_workspace_jobs:
           template.allow_user_cancel_workspace_jobs,
       },
       validationSchema,
-      onSubmit: (formData) => {
-        // on submit, convert from hours => ms
-        onSubmit({
-          ...formData,
-          default_ttl_ms: formData.default_ttl_ms
-            ? formData.default_ttl_ms * MS_HOUR_CONVERSION
-            : undefined,
-          max_ttl_ms: formData.max_ttl_ms
-            ? formData.max_ttl_ms * MS_HOUR_CONVERSION
-            : undefined,
-        })
-      },
+      onSubmit,
       initialTouched,
     })
   const getFieldHelpers = getFormHelpers<UpdateTemplateMeta>(form, error)
@@ -189,55 +136,6 @@ export const TemplateSettingsForm: FC<TemplateSettingsForm> = ({
       </FormSection>
 
       <FormSection
-        title={t("schedule.title")}
-        description={t("schedule.description")}
-      >
-        <Stack direction="row" className={styles.ttlFields}>
-          <TextField
-            {...getFieldHelpers(
-              "default_ttl_ms",
-              <TTLHelperText
-                translationName="defaultTTLHelperText"
-                ttl={form.values.default_ttl_ms}
-              />,
-            )}
-            disabled={isSubmitting}
-            fullWidth
-            inputProps={{ min: 0, step: 1 }}
-            label={t("defaultTtlLabel")}
-            variant="outlined"
-            type="number"
-          />
-
-          <TextField
-            {...getFieldHelpers(
-              "max_ttl_ms",
-              canSetMaxTTL ? (
-                <TTLHelperText
-                  translationName="maxTTLHelperText"
-                  ttl={form.values.max_ttl_ms}
-                />
-              ) : (
-                <>
-                  {commonT("licenseFieldTextHelper")}{" "}
-                  <Link href="https://coder.com/docs/v2/latest/enterprise">
-                    {commonT("learnMore")}
-                  </Link>
-                  .
-                </>
-              ),
-            )}
-            disabled={isSubmitting || !canSetMaxTTL}
-            fullWidth
-            inputProps={{ min: 0, step: 1 }}
-            label={t("maxTtlLabel")}
-            variant="outlined"
-            type="number"
-          />
-        </Stack>
-      </FormSection>
-
-      <FormSection
         title={t("operations.title")}
         description={t("operations.description")}
       >
@@ -289,9 +187,5 @@ const useStyles = makeStyles((theme) => ({
   optionHelperText: {
     fontSize: theme.spacing(1.5),
     color: theme.palette.text.secondary,
-  },
-
-  ttlFields: {
-    width: "100%",
   },
 }))
