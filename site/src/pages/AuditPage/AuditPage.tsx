7dfad65496755ee5e7966820d0c5df664a1e6b5f
Kira Pilot <kira@coder.com>
2023-03-24 19:00:11 +0000

    fix(UI): redirect if user is not permissioned to see workspace

--- site/src/pages/AuditPage/AuditPage.tsx
+++ site/src/pages/AuditPage/AuditPage.tsx
@@ -11,8 +11,15 @@ import { pageTitle } from "util/page"
 import { auditMachine } from "xServices/audit/auditXService"
 import { PaginationMachineRef } from "xServices/pagination/paginationXService"
 import { AuditPageView } from "./AuditPageView"
+import { RequirePermission } from "components/RequirePermission/RequirePermission"
+import { useReadPagePermissions } from "hooks/useReadPagePermissions"
+import { Loader } from "components/Loader/Loader"
 
 const AuditPage: FC = () => {
+  // we call the below hook to make sure the user has access to view the page
+  const { data: permissions, isLoading: isLoadingPermissions } =
+    useReadPagePermissions("audit_log")
+
   const [searchParams, setSearchParams] = useSearchParams()
   const filter = searchParams.get("filter") ?? ""
   const [auditState, auditSend] = useMachine(auditMachine, {
@@ -28,26 +35,34 @@ const AuditPage: FC = () => {
 
   const { auditLogs, count, apiError } = auditState.context
   const paginationRef = auditState.context.paginationRef as PaginationMachineRef
-  const { audit_log: isAuditLogVisible } = useFeatureVisibility()
+  const { audit_log: isAuditLogEnabled } = useFeatureVisibility()
+
+  if (!permissions || isLoadingPermissions) {
+    return <Loader />
+  }
 
   return (
-    <>
-      <Helmet>
-        <title>{pageTitle("Audit")}</title>
-      </Helmet>
-      <AuditPageView
-        filter={filter}
-        auditLogs={auditLogs}
-        count={count}
-        onFilter={(filter) => {
-          auditSend("FILTER", { filter })
-        }}
-        paginationRef={paginationRef}
-        isNonInitialPage={nonInitialPage(searchParams)}
-        isAuditLogVisible={isAuditLogVisible}
-        error={apiError}
-      />
-    </>
+    <RequirePermission
+      isFeatureVisible={isAuditLogEnabled && permissions.readPagePermissions}
+    >
+      <>
+        <Helmet>
+          <title>{pageTitle("Audit")}</title>
+        </Helmet>
+        <AuditPageView
+          filter={filter}
+          auditLogs={auditLogs}
+          count={count}
+          onFilter={(filter) => {
+            auditSend("FILTER", { filter })
+          }}
+          paginationRef={paginationRef}
+          isNonInitialPage={nonInitialPage(searchParams)}
+          isAuditLogVisible={isAuditLogEnabled}
+          error={apiError}
+        />
+      </>
+    </RequirePermission>
   )
 }
 
--- site/src/pages/WorkspacePage/WorkspacePage.tsx
+++ site/src/pages/WorkspacePage/WorkspacePage.tsx
@@ -9,8 +9,11 @@ import { firstOrItem } from "util/array"
 import { workspaceMachine } from "xServices/workspace/workspaceXService"
 import { WorkspaceReadyPage } from "./WorkspaceReadyPage"
 import { quotaMachine } from "xServices/quotas/quotasXService"
+import { RequirePermission } from "components/RequirePermission/RequirePermission"
+import { useReadPagePermissions } from "hooks/useReadPagePermissions"
 
 export const WorkspacePage: FC = () => {
+  const styles = useStyles()
   const { username: usernameQueryParam, workspace: workspaceQueryParam } =
     useParams()
   const username = firstOrItem(usernameQueryParam, null)
@@ -23,9 +26,13 @@ export const WorkspacePage: FC = () => {
     getTemplateParametersWarning,
     checkPermissionsError,
   } = workspaceState.context
+
+  // we call the below hook to make sure the user has access to view the page
+  const { data: permissions, isLoading: isLoadingPermissions } =
+    useReadPagePermissions("workspace", workspace?.id)
+
   const [quotaState, quotaSend] = useMachine(quotaMachine)
   const { getQuotaError } = quotaState.context
-  const styles = useStyles()
 
   /**
    * Get workspace, template, and organization on mount and whenever workspaceId changes.
@@ -41,47 +48,53 @@ export const WorkspacePage: FC = () => {
     username && quotaSend({ type: "GET_QUOTA", username })
   }, [username, quotaSend])
 
+  if (!permissions || isLoadingPermissions) {
+    return <Loader />
+  }
+
   return (
-    <ChooseOne>
-      <Cond condition={workspaceState.matches("error")}>
-        <div className={styles.error}>
-          {Boolean(getWorkspaceError) && (
-            <AlertBanner severity="error" error={getWorkspaceError} />
-          )}
-          {Boolean(getTemplateWarning) && (
-            <AlertBanner severity="error" error={getTemplateWarning} />
-          )}
-          {Boolean(getTemplateParametersWarning) && (
-            <AlertBanner
-              severity="error"
-              error={getTemplateParametersWarning}
-            />
-          )}
-          {Boolean(checkPermissionsError) && (
-            <AlertBanner severity="error" error={checkPermissionsError} />
-          )}
-          {Boolean(getQuotaError) && (
-            <AlertBanner severity="error" error={getQuotaError} />
-          )}
-        </div>
-      </Cond>
-      <Cond
-        condition={
-          Boolean(workspace) &&
-          workspaceState.matches("ready") &&
-          quotaState.matches("success")
-        }
-      >
-        <WorkspaceReadyPage
-          workspaceState={workspaceState}
-          quotaState={quotaState}
-          workspaceSend={workspaceSend}
-        />
-      </Cond>
-      <Cond>
-        <Loader />
-      </Cond>
-    </ChooseOne>
+    <RequirePermission isFeatureVisible={permissions?.readPagePermissions}>
+      <ChooseOne>
+        <Cond condition={workspaceState.matches("error")}>
+          <div className={styles.error}>
+            {Boolean(getWorkspaceError) && (
+              <AlertBanner severity="error" error={getWorkspaceError} />
+            )}
+            {Boolean(getTemplateWarning) && (
+              <AlertBanner severity="error" error={getTemplateWarning} />
+            )}
+            {Boolean(getTemplateParametersWarning) && (
+              <AlertBanner
+                severity="error"
+                error={getTemplateParametersWarning}
+              />
+            )}
+            {Boolean(checkPermissionsError) && (
+              <AlertBanner severity="error" error={checkPermissionsError} />
+            )}
+            {Boolean(getQuotaError) && (
+              <AlertBanner severity="error" error={getQuotaError} />
+            )}
+          </div>
+        </Cond>
+        <Cond
+          condition={
+            Boolean(workspace) &&
+            workspaceState.matches("ready") &&
+            quotaState.matches("success")
+          }
+        >
+          <WorkspaceReadyPage
+            workspaceState={workspaceState}
+            quotaState={quotaState}
+            workspaceSend={workspaceSend}
+          />
+        </Cond>
+        <Cond>
+          <Loader />
+        </Cond>
+      </ChooseOne>
+    </RequirePermission>
   )
 }
 
