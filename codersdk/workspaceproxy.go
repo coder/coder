f6893812bedca768e184d69a74e8107bad4e29ac
Steven Masley <stevenmasley@coder.com>
2023-03-31 14:20:23 -0500

    Fix unit test

--- codersdk/workspaceproxy.go
+++ codersdk/workspaceproxy.go
@@ -21,16 +21,16 @@ type CreateWorkspaceProxyRequest struct {
 }
 
 type WorkspaceProxy struct {
-	ID             uuid.UUID `db:"id" json:"id"`
-	OrganizationID uuid.UUID `db:"organization_id" json:"organization_id"`
+	ID             uuid.UUID `db:"id" json:"id" format:"uuid"`
+	OrganizationID uuid.UUID `db:"organization_id" json:"organization_id" format:"uuid"`
 	Name           string    `db:"name" json:"name"`
 	Icon           string    `db:"icon" json:"icon"`
 	// Full url including scheme of the proxy api url: https://us.example.com
 	URL string `db:"url" json:"url"`
 	// URL with the wildcard for subdomain based app hosting: https://*.us.example.com
 	WildcardURL string    `db:"wildcard_url" json:"wildcard_url"`
-	CreatedAt   time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt   time.Time `db:"updated_at" json:"updated_at"`
+	CreatedAt   time.Time `db:"created_at" json:"created_at" format:"date-time"`
+	UpdatedAt   time.Time `db:"updated_at" json:"updated_at" format:"date-time"`
 	Deleted     bool      `db:"deleted" json:"deleted"`
 }
 
