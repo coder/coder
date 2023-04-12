7aa731f43aa7042bb89637c495eeee130e212fd8
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2023-04-12 18:43:08 +0000

    Upgrade storybook

--- site/.storybook/preview.js
+++ site/.storybook/preview.js
@@ -1,30 +1,28 @@
 import CssBaseline from "@material-ui/core/CssBaseline"
 import ThemeProvider from "@material-ui/styles/ThemeProvider"
 import { createMemoryHistory } from "history"
-import { addDecorator } from "node_modules/@storybook/react"
 import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom"
 import { dark } from "../src/theme"
 import "../src/theme/globalFonts"
 import "../src/i18n"
 
-addDecorator((story) => (
-  <ThemeProvider theme={dark}>
-    <CssBaseline />
-    {story()}
-  </ThemeProvider>
-))
-
 const history = createMemoryHistory()
 
-const routerDecorator = (Story) => {
-  return (
-    <HistoryRouter history={history}>
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={dark}>
+      <CssBaseline />
       <Story />
-    </HistoryRouter>
-  )
-}
-
-addDecorator(routerDecorator)
+    </ThemeProvider>
+  ),
+  (Story) => {
+    return (
+      <HistoryRouter history={history}>
+        <Story />
+      </HistoryRouter>
+    )
+  },
+]
 
 export const parameters = {
   actions: {
