224fa2f5af64486f89b4fd26139ed8836d45dd9f
Steven Masley <stevenmasley@coder.com>
2023-04-17 10:07:06 -0500

    Add comments to sql columns

--- coderd/database/migrations/000118_workspace_proxy_token.up.sql
+++ coderd/database/migrations/000118_workspace_proxy_token.up.sql
@@ -10,4 +10,13 @@ DELETE FROM workspace_proxies;
 ALTER TABLE workspace_proxies
 	ADD COLUMN token_hashed_secret bytea NOT NULL;
 
+COMMENT ON COLUMN workspace_proxies.token_hashed_secret
+	IS 'Hashed secret is used to authenticate the workspace proxy using a session token.';
+
+COMMENT ON COLUMN workspace_proxies.deleted
+	IS 'Boolean indicator of a deleted workspace proxy. Proxies are soft-deleted.';
+
+COMMENT ON COLUMN workspace_proxies.icon
+	IS 'Expects an emoji character. (/emojis/1f1fa-1f1f8.png)';
+
 COMMIT;
