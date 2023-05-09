101d7969aec1f07e2d62e319c08f4610f2f8f14f
Spike Curtis <spike@coder.com>
2023-05-09 14:44:17 +0400

    Data drive working
    Signed-off-by: Spike Curtis <spike@coder.com>

--- examples/templates/azure-windows/main.tf
+++ examples/templates/azure-windows/main.tf
@@ -43,69 +43,15 @@ data "coder_parameter" "location" {
     name  = "West US 2"
   }
 }
+
 resource "coder_agent" "main" {
   arch               = "amd64"
   auth               = "azure-instance-identity"
   os                 = "windows"
-  dir                = "F:\\Users\\coder"
+  #dir                = "F:\\Users\\coder"
   login_before_ready = false
-  startup_script = <<EOF
-$disk = Get-Disk -Number 2
-if ($disk.PartitionStyle -Eq 'RAW')
-{
-    Write-Host "Initializing data disk"
-    $disk | Initialize-Disk
-} else {
-    Write-Host "data disk already initialized"
 }
 
-$partitions = Get-Partition -DiskNumber $disk.Number | Where-Object Type -Ne 'Reserved'
-if ($partitions.Count -Eq 0) {
-    Write-Host "Creating partition on data disk"
-    New-Partition -DiskNumber $disk.Number -UseMaximumSize -DriveLetter F
-} else {
-    $s = "data disk already has partition of size {0:n1} GiB" -f ($partitions[0].Size / 1073741824)
-    Write-Host $s
-    if ($partitions[0].DriveLetter -Ne "F") {
-        Set-Partition -InputObject $partitions[0] -NewDriveLetter F
-    }
-}
-
-$volume = Get-Volume -DriveLetter F
-if ($volume.FileSystemType -Eq 'Unknown')
-{
-    Write-Host "Formatting data disk"
-    Format-Volume -InputObject $volume -FileSystem NTFS -Confirm:$false
-} else {
-    Write-Host "data disk is already formatted"
-}
-
-$h = "F:\Users\coder"
-if (Test-Path -Path $home) {
-    Write-Host "Home directory exists"
-} else {
-    New-Item -ItemType "directory" -Path $h
-}
-
-$u = Get-LocalUser -Name coder
-$profile = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\ProfileList\{0}" -f $u.SID
-$curHome = Get-ItemProperty -Path $profile -Name "ProfileImagePath"
-if ($curHome -Ne $h) {
-    Write-Host "Updating user profile location"
-    Set-ItemProperty -Path $profile -Name "ProfileImagePath" -Value $h
-}
-
-# Enable RDP
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
-# Enable RDP through Windows Firewall
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-# Disable Network Level Authentication (NLA)
-# Clients will connect via Coder's tunnel
-(Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -ComputerName $env:COMPUTERNAME -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0)
-# choco feature enable -n=allowGlobalConfirmation
-# choco install visualstudio2022community --package-parameters "--add=Microsoft.VisualStudio.Workload.ManagedDesktop;includeRecommended --passive --locale en-US"
-EOF
-}
 locals {
   prefix = "spike"
   admin_username = "coder"
@@ -114,23 +60,8 @@ locals {
   # Must meet Windows password complexity requirements:
   # https://docs.microsoft.com/en-us/windows/security/threat-protection/security-policy-settings/password-must-meet-complexity-requirements#reference
   admin_password = "coderRDP!"
-  # User data is used to stop/start AWS instances. See:
-  # https://github.com/hashicorp/terraform-provider-aws/issues/22
-  user_data_start = <<EOT
-# Install Chocolatey package manager before
-# the agent starts to use via startup_script
-# Reload path so sessions include "choco" and "refreshenv"
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-# Install Git and reload path
-choco install -y git
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-EOT
-  user_data_end = <<EOT
-shutdown /s
-EOT
 }
+
 resource "azurerm_resource_group" "main" {
   name     = "${local.prefix}-${data.coder_workspace.me.name}-resources"
   location = data.coder_parameter.location.value
@@ -214,7 +145,9 @@ resource "azurerm_windows_virtual_machine" "main" {
   resource_group_name   = azurerm_resource_group.main.name
   network_interface_ids = [azurerm_network_interface.main.id]
   size                  = "Standard_DS1_v2"
-  custom_data             = base64encode(coder_agent.main.init_script)
+  custom_data             = base64encode(
+    templatefile("${path.module}/Initialize.ps1.tftpl", {init_script = coder_agent.main.init_script})
+  )
   os_disk {
     name                 = "myOsDisk"
     caching              = "ReadWrite"
@@ -231,7 +164,7 @@ resource "azurerm_windows_virtual_machine" "main" {
     setting = "AutoLogon"
   }
   additional_unattend_content {
-    content = "${file("./FirstLogonCommands.xml")}"
+    content = file("${path.module}/FirstLogonCommands.xml")
     setting = "FirstLogonCommands"
   }
   boot_diagnostics {
