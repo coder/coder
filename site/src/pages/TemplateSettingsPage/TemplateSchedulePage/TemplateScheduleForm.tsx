a42a36a4741568fb21a44c2508ddb4b9c86e22f4
Kira Pilot <kira@coder.com>
2023-05-10 12:57:11 -0700

    feat: add workspace actions cleanup configuration flags to CLI template create and edit (#7453)
    * added cleanup flags on template create
    
    * added cleanup flags on template edit
    
    * fixed tests
    
    * added to tests

--- site/src/pages/TemplateSettingsPage/TemplateSchedulePage/TemplateScheduleForm.tsx
+++ site/src/pages/TemplateSettingsPage/TemplateSchedulePage/TemplateScheduleForm.tsx
@@ -65,7 +65,6 @@ export const getValidationSchema = (): Yup.AnyObjectSchema =>
         i18next.t("maxTTLMaxError", { ns: "templateSettingsPage" }),
       ),
     failure_ttl_ms: Yup.number()
-      .integer()
       .min(0, "Failure cleanup days must not be less than 0.")
       .test(
         "positive-if-enabled",
@@ -80,7 +79,6 @@ export const getValidationSchema = (): Yup.AnyObjectSchema =>
         },
       ),
     inactivity_ttl_ms: Yup.number()
-      .integer()
       .min(0, "Inactivity cleanup days must not be less than 0.")
       .test(
         "positive-if-enabled",
@@ -344,7 +342,7 @@ export const TemplateScheduleForm: FC<TemplateScheduleForm> = ({
                 )}
                 disabled={isSubmitting || !form.values.failure_cleanup_enabled}
                 fullWidth
-                inputProps={{ min: 0, step: 1 }}
+                inputProps={{ min: 0, step: "any" }}
                 label="Time until cleanup (days)"
                 type="number"
                 aria-label="Failure Cleanup"
@@ -378,7 +376,7 @@ export const TemplateScheduleForm: FC<TemplateScheduleForm> = ({
                   isSubmitting || !form.values.inactivity_cleanup_enabled
                 }
                 fullWidth
-                inputProps={{ min: 0, step: 1 }}
+                inputProps={{ min: 0, step: "any" }}
                 label="Time until cleanup (days)"
                 type="number"
                 aria-label="Inactivity Cleanup"
