bc1c10dce8723ff4ad28372ee90f6ecb5b93a505
Steven Masley <stevenmasley@coder.com>
2023-05-10 20:01:56 -0500

    Add performance mock

---
+++ site/src/testHelpers/performanceMock.ts
@@ -0,0 +1,3 @@
+// This is a mock of the performance API. It is used to mock the performance API in tests since the tests
+// do not run in a browser environment. Feel free to add functionality to this if you need.
+window.performance.getEntries = () => []
