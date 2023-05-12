ae2677736518a83e7f8b590dfd387a19f355e23b
Steven Masley <stevenmasley@coder.com>
2023-05-12 09:45:42 -0500

    Fmt

--- site/src/testHelpers/localstorage.ts
+++ site/src/testHelpers/localstorage.ts
@@ -1,22 +1,22 @@
 export const localStorageMock = () => {
-  const store = {} as Record<string, string>;
+  const store = {} as Record<string, string>
 
   return {
-    getItem: (key: string):string =>{
-      return store[key];
+    getItem: (key: string): string => {
+      return store[key]
     },
-    setItem: (key:string, value:string)=> {
-      store[key] = value;
+    setItem: (key: string, value: string) => {
+      store[key] = value
     },
-    clear: ()=> {
+    clear: () => {
       Object.keys(store).forEach((key) => {
-        delete store[key];
-      });
+        delete store[key]
+      })
     },
-    removeItem: (key:string) => {
-      delete store[key];
-    }
+    removeItem: (key: string) => {
+      delete store[key]
+    },
   }
 }
 
-Object.defineProperty(window, "localStorage", { value: localStorageMock() });
+Object.defineProperty(window, "localStorage", { value: localStorageMock() })
