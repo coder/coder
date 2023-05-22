c62d297eecb3df43886ccfa567a1cea16e7741d7
Kira Pilot <kira@coder.com>
2023-05-22 19:53:36 +0000

    fix prettier and lint

--- site/src/components/WorkspaceDeletion/ImpendingDeletionBanner.tsx
+++ site/src/components/WorkspaceDeletion/ImpendingDeletionBanner.tsx
@@ -18,7 +18,7 @@ export const ImpendingDeletionBanner = ({
   workspace?: Workspace
   onDismiss: () => void
   displayImpendingDeletionBanner: boolean
-  count?: Count,
+  count?: Count
 }): JSX.Element | null => {
   const { entitlements, experiments } = useDashboard()
   const allowAdvancedScheduling =
@@ -27,23 +27,25 @@ export const ImpendingDeletionBanner = ({
   // is merged up
   const allowWorkspaceActions = experiments.includes("workspace_actions")
 
-    if (
-      !workspace ||
-          !displayImpendingDeletion(
-            workspace,
-            allowAdvancedScheduling,
-            allowWorkspaceActions,
-          ) ||
-          !displayImpendingDeletionBanner
-    ) {
-      return null
-    }
+  if (
+    !workspace ||
+    !displayImpendingDeletion(
+      workspace,
+      allowAdvancedScheduling,
+      allowWorkspaceActions,
+    ) ||
+    !displayImpendingDeletionBanner
+  ) {
+    return null
+  }
 
   return (
-      <Alert severity="info" onDismiss={onDismiss} dismissible>
-        {count === Count.Singular
-          ? `This workspace has been unused for ${formatDistanceToNow(Date.parse(workspace.last_used_at))} and is scheduled for deletion. To keep it, connect via SSH or the web terminal.`
-          : "You have workspaces that will be deleted soon due to inactivity. To keep these workspaces, connect to them via SSH or the web terminal."}
-      </Alert>
+    <Alert severity="info" onDismiss={onDismiss} dismissible>
+      {count === Count.Singular
+        ? `This workspace has been unused for ${formatDistanceToNow(
+            Date.parse(workspace.last_used_at),
+          )} and is scheduled for deletion. To keep it, connect via SSH or the web terminal.`
+        : "You have workspaces that will be deleted soon due to inactivity. To keep these workspaces, connect to them via SSH or the web terminal."}
+    </Alert>
   )
 }
