d7e8b25fc26d7a7173adb7976f5e7cc9dd492c6f
Steven Masley <stevenmasley@coder.com>
2023-05-23 18:24:44 +0200

    Formatting

--- coderd/metricscache/metricscache.go
+++ coderd/metricscache/metricscache.go
@@ -27,7 +27,8 @@ var (
 	timezoneOffsets = []int{
 		0, // UTC - is listed first intentionally.
 		-12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1,
-		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+	}
 )
 
 // Cache holds the template metrics.
