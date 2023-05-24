0d137b16e3cfe188ca5c2d9751c4fbc467d1b740
Jon Ayers <jon@coder.com>
2023-05-24 03:06:03 +0000

    querying server for version

--- cli/upgrade.go
+++ cli/upgrade.go
@@ -1,44 +1,71 @@
 package cli
 
 import (
-	"bytes"
-	"fmt"
-	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
+	"strings"
+
+	"golang.org/x/mod/semver"
+	"golang.org/x/xerrors"
 
 	"github.com/coder/coder/cli/clibase"
+	"github.com/coder/coder/codersdk"
 )
 
 func (r *RootCmd) upgrade() *clibase.Cmd {
 	cmd := &clibase.Cmd{
-		Use:   "upgrade",
-		Short: "Upgrade the Coder CLI to match the version of a deployment.",
+		Use:     "upgrade",
+		Short:   "Upgrade the Coder CLI to match the version of a deployment.",
+		Options: clibase.OptionSet{},
 		Handler: func(inv *clibase.Invocation) error {
+			var (
+				ctx  = inv.Context()
+				curl = r.clientURL.String()
+			)
+
+			var err error
+			if curl == "" {
+				curl, err = r.createConfig().URL().Read()
+				if err != nil {
+					return xerrors.Errorf("read config url: %w", err)
+				}
+			}
+
+			uri, err := url.Parse(curl)
+			if err != nil {
+				return xerrors.Errorf("parse url: %w", err)
+			}
+
+			client := codersdk.New(uri)
+			serverInfo, err := client.BuildInfo(ctx)
+			if err != nil {
+				return xerrors.Errorf("build info: %w", err)
+			}
 
 			resp, err := http.Get("https://coder.com/install.sh")
 			if err != nil {
-				panic(err)
-			}
-			if resp.StatusCode != http.StatusOK {
-				panic(fmt.Sprintf("code %d", resp.StatusCode))
+				return xerrors.Errorf("fetch install script: %w", err)
 			}
 			defer resp.Body.Close()
 
-			script, err := io.ReadAll(resp.Body)
-			if err != nil {
-				panic(err)
+			if resp.StatusCode != http.StatusOK {
+				return xerrors.Errorf("unexpected error code %d while fetching install script: %w", resp.StatusCode, err)
 			}
-			fmt.Println("script: ", string(script))
 
-			cmd := exec.Command("sh", "-s", "--", "--version", "0.22.2")
-			cmd.Stdin = bytes.NewReader(script)
+			version := semver.Canonical(serverInfo.Version)
+			version = strings.TrimSuffix(version, "-devel")
+			version = strings.TrimPrefix(version, "v")
+
+			// nolint: gosec
+			cmd := exec.Command("sh", "-s", "--", "--version", version)
+			cmd.Stdin = resp.Body
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 			err = cmd.Run()
 			if err != nil {
-				panic(err)
+				return xerrors.Errorf("run install script: %w", err)
 			}
 
 			return nil
