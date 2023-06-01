51bb3c0177d2dcd7920d5fe21dc417060196b11a
Marcin Tojek <marcin@coder.com>
2023-06-01 16:12:33 +0200

    Adjust wording

--- coderd/wsbuilder/wsbuilder.go
+++ coderd/wsbuilder/wsbuilder.go
@@ -485,7 +485,7 @@ func (b *Builder) getParameters() (names, values []string, err error) {
 	}
 	err = b.verifyNoLegacyParameters()
 	if err != nil {
-		return nil, nil, BuildError{http.StatusBadRequest, "Used unsupported legacy parameters", err}
+		return nil, nil, BuildError{http.StatusBadRequest, "Unable to build workspace with unsupported parameters", err}
 	}
 	resolver := codersdk.ParameterResolver{
 		Rich: db2sdk.WorkspaceBuildParameters(lastBuildParameters),
@@ -584,7 +584,7 @@ func (b *Builder) verifyNoLegacyParameters() error {
 	}
 
 	if len(parameterSchemas) > 0 {
-		return xerrors.Errorf("Legacy parameters are not supported anymore. Contact your administrator for assistance.")
+		return xerrors.Errorf("Legacy parameters in use on this version are not supported anymore. Contact your administrator for assistance.")
 	}
 	return nil
 }
