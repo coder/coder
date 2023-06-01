f6857d5a4f363b78e6b85098d45fdb47eafba669
Steven Masley <stevenmasley@coder.com>
2023-06-01 08:28:13 -0400

    Fix comment

--- coderd/database/queries/workspaces.sql
+++ coderd/database/queries/workspaces.sql
@@ -158,7 +158,7 @@ WHERE
 					latest_build.completed_at IS NOT NULL AND
 					latest_build.updated_at - INTERVAL '30 seconds' < NOW() AND
 					latest_build.transition = 'delete'::workspace_transition AND
-					-- If the error field is null, the status is 'failed'
+					-- If the error field is not null, the status is 'failed'
 					latest_build.error IS NULL
 
 				WHEN @status = 'deleting' THEN
