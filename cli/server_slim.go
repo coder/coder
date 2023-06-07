8345a3e422ef616d5e5b37a3e32380957839da2a
Cian Johnston <cian@coder.com>
2023-06-07 09:47:29 +0100

    fixup! fix(cli): speed up CLI over SSH (#7885)

--- cli/server_slim.go
+++ cli/server_slim.go
@@ -30,7 +30,7 @@ func (r *RootCmd) Server(_ func(context.Context, *coderd.Options) (*coderd.API, 
 }
 
 func serverUnsupported(w io.Writer) {
-	_, _ = fmt.Fprintf(w, "You are using a 'slim' build of Coder, which does not support the %s subcommand.\n", cliui.Styles.Code.Render("server"))
+	_, _ = fmt.Fprintf(w, "You are using a 'slim' build of Coder, which does not support the %s subcommand.\n", cliui.DefaultStyles.Code.Render("server"))
 	_, _ = fmt.Fprintln(w, "")
 	_, _ = fmt.Fprintln(w, "Please use a build of Coder from GitHub releases:")
 	_, _ = fmt.Fprintln(w, "  https://github.com/coder/coder/releases")
