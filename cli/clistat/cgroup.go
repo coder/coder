1c8943ef74c4894396e452dc75e04a57f040a77b
Cian Johnston <cian@coder.com>
2023-06-12 17:49:44 +0100

    implement cgroupv1 cpu

--- cli/clistat/cgroup.go
+++ cli/clistat/cgroup.go
@@ -12,8 +12,10 @@ import (
 )
 
 const (
-	cgroupV2CPUMax  = "/sys/fs/cgroup/cpu.max"
-	cgroupV2CPUStat = "/sys/fs/cgroup/cpu.stat"
+	cgroupV1CPUAcctUsage = "/sys/fs/cgroup/cpu,cpuacct/cpuacct.usage"
+	cgroupV1CFSQuotaUs   = "/sys/fs/cgroup/cpu,cpuacct/cpu.cfs_quota_us"
+	cgroupV2CPUMax       = "/sys/fs/cgroup/cpu.max"
+	cgroupV2CPUStat      = "/sys/fs/cgroup/cpu.stat"
 )
 
 // ContainerCPU returns the CPU usage of the container cgroup.
@@ -38,8 +40,8 @@ func (s *Statter) ContainerCPU() (*Result, error) {
 
 	r := &Result{
 		Unit:  "cores",
-		Used:  (used2 - used1).Seconds(),
-		Total: ptr.To(total.Seconds() / s.sampleInterval.Seconds()), // close enough to the truth
+		Used:  (used2 - used1).Seconds() * s.sampleInterval.Seconds(),
+		Total: ptr.To(total.Seconds()), // close enough to the truth
 	}
 	return r, nil
 }
@@ -62,12 +64,12 @@ func (s *Statter) isCGroupV2() bool {
 func (s *Statter) cGroupV2CPU() (used, total time.Duration, err error) {
 	total, err = s.cGroupv2CPUTotal()
 	if err != nil {
-		return 0, 0, xerrors.Errorf("get cgroup v2 CPU cores: %w", err)
+		return 0, 0, xerrors.Errorf("get cgroup v2 cpu total: %w", err)
 	}
 
 	used, err = s.cGroupv2CPUUsed()
 	if err != nil {
-		return 0, 0, xerrors.Errorf("get cgroup v2 CPU used: %w", err)
+		return 0, 0, xerrors.Errorf("get cgroup v2 cpu used: %w", err)
 	}
 
 	return used, total, nil
@@ -77,7 +79,7 @@ func (s *Statter) cGroupv2CPUUsed() (used time.Duration, err error) {
 	var data []byte
 	data, err = afero.ReadFile(s.fs, cgroupV2CPUStat)
 	if err != nil {
-		return 0, xerrors.Errorf("read /sys/fs/cgroup/cpu.stat: %w", err)
+		return 0, xerrors.Errorf("read %s: %w", cgroupV2CPUStat, err)
 	}
 
 	bs := bufio.NewScanner(bytes.NewReader(data))
@@ -89,54 +91,102 @@ func (s *Statter) cGroupv2CPUUsed() (used time.Duration, err error) {
 
 		parts := bytes.Split(line, []byte(" "))
 		if len(parts) != 2 {
-			return 0, xerrors.Errorf("unexpected line in /sys/fs/cgroup/cpu.stat: %s", line)
+			return 0, xerrors.Errorf("unexpected line in %s: %s", cgroupV2CPUStat, line)
 		}
 
 		iused, err := strconv.Atoi(string(parts[1]))
 		if err != nil {
-			return 0, xerrors.Errorf("parse /sys/fs/cgroup/cpu.stat: %w", err)
+			return 0, xerrors.Errorf("parse %s: %w", err, cgroupV2CPUStat)
 		}
 
 		return time.Duration(iused) * time.Microsecond, nil
 	}
 
-	return 0, xerrors.Errorf("did not find expected usage_usec in /sys/fs/cgroup/cpu.stat")
+	return 0, xerrors.Errorf("did not find expected usage_usec in %s", cgroupV2CPUStat)
 }
 
 func (s *Statter) cGroupv2CPUTotal() (total time.Duration, err error) {
 	var data []byte
-	var quotaUs int
-	data, err = afero.ReadFile(s.fs, "/sys/fs/cgroup/cpu.max")
+	var quotaUs int64
+	data, err = afero.ReadFile(s.fs, cgroupV2CPUMax)
 	if err != nil {
-		return 0, xerrors.Errorf("read /sys/fs/cgroup/cpu.max: %w", err)
+		return 0, xerrors.Errorf("read %s: %w", cgroupV2CPUMax, err)
 	}
 
 	lines := bytes.Split(data, []byte("\n"))
 	if len(lines) < 1 {
-		return 0, xerrors.Errorf("unexpected empty /sys/fs/cgroup/cpu.max")
+		return 0, xerrors.Errorf("unexpected empty %s", cgroupV2CPUMax)
 	}
 
-	line := lines[0]
-	parts := bytes.Split(line, []byte(" "))
+	parts := bytes.Split(lines[0], []byte(" "))
 	if len(parts) != 2 {
-		return 0, xerrors.Errorf("unexpected line in /sys/fs/cgroup/cpu.max: %s", line)
+		return 0, xerrors.Errorf("unexpected line in %s: %s", cgroupV2CPUMax, lines[0])
 	}
 
 	if bytes.Equal(parts[0], []byte("max")) {
-		quotaUs = s.nproc * int(time.Second.Microseconds())
+		quotaUs = int64(s.nproc) * time.Second.Microseconds()
 	} else {
-		quotaUs, err = strconv.Atoi(string(parts[0]))
+		quotaUs, err = strconv.ParseInt(string(parts[0]), 10, 64)
 		if err != nil {
-			return 0, xerrors.Errorf("parse /sys/fs/cgroup/cpu.max: %w", err)
+			return 0, xerrors.Errorf("parse %s: %w", cgroupV2CPUMax, err)
 		}
 	}
 
 	return time.Duration(quotaUs) * time.Microsecond, nil
 }
 
-func (*Statter) cGroupV1CPU() (time.Duration, time.Duration, error) {
-	// TODO: implement
-	return 0, 0, nil
+func (s *Statter) cGroupV1CPU() (used, total time.Duration, err error) {
+	total, err = s.cGroupV1CPUTotal()
+	if err != nil {
+		return 0, 0, xerrors.Errorf("get cgroup v1 CPU total: %w", err)
+	}
+
+	used, err = s.cgroupV1CPUUsed()
+	if err != nil {
+		return 0, 0, xerrors.Errorf("get cgruop v1 cpu used: %w", err)
+	}
+
+	return used, total, nil
+}
+
+func (s *Statter) cGroupV1CPUTotal() (time.Duration, error) {
+	var data []byte
+	var err error
+	var quotaUs int64
+
+	data, err = afero.ReadFile(s.fs, cgroupV1CFSQuotaUs)
+	if err != nil {
+		return 0, xerrors.Errorf("read %s: %w", cgroupV1CFSQuotaUs, err)
+	}
+
+	quotaUs, err = strconv.ParseInt(string(bytes.TrimSpace(data)), 10, 64)
+	if err != nil {
+		return 0, xerrors.Errorf("parse %s: %w", cgroupV1CFSQuotaUs, err)
+	}
+
+	if quotaUs < 0 {
+		quotaUs = int64(s.nproc) * time.Second.Microseconds()
+	}
+
+	return time.Duration(quotaUs) * time.Microsecond, nil
+}
+
+func (s *Statter) cgroupV1CPUUsed() (time.Duration, error) {
+	var data []byte
+	var err error
+	var usageUs int64
+
+	data, err = afero.ReadFile(s.fs, cgroupV1CPUAcctUsage)
+	if err != nil {
+		return 0, xerrors.Errorf("read %s: %w", cgroupV1CPUAcctUsage, err)
+	}
+
+	usageUs, err = strconv.ParseInt(string(bytes.TrimSpace(data)), 10, 64)
+	if err != nil {
+		return 0, xerrors.Errorf("parse %s: %w", cgroupV1CPUAcctUsage, err)
+	}
+
+	return time.Duration(usageUs) * time.Microsecond, nil
 }
 
 // ContainerMemory returns the memory usage of the container cgroup.
