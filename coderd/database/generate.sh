794a551176cea16eea6d1b63fa3bc386a214c4a9
Kyle Carberry <kyle@coder.com>
2023-06-13 12:35:12 -0500

    chore: generate dbmetrics automatically when adding new queries (#7999)
    * chore: rename dbgen package files and remove small file
    
    * chore: automatically generate dbmetrics when new queries are added

--- coderd/database/generate.sh
+++ coderd/database/generate.sh
@@ -62,4 +62,7 @@ SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 	# Generate the database fake!
 	go run gen/fake/main.go
 	go run golang.org/x/tools/cmd/goimports@latest -w ./dbfake/dbfake.go
+
+	go run gen/metrics/main.go
+	go run golang.org/x/tools/cmd/goimports@latest -w ./dbmetrics/dbmetrics.go
 )
