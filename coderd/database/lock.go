96cee2b3abcc1512d455f7b3996e889fa72fdfd4
Kyle Carberry <kyle@carberry.com>
2023-06-17 17:39:46 +0000

    Merge enum into dbgen

--- coderd/database/lock.go
+++
@@ -1,8 +0,0 @@
-package database
-
-// Well-known lock IDs for lock functions in the database. These should not
-// change. If locks are deprecated, they should be kept to avoid reusing the
-// same ID.
-const (
-	LockIDDeploymentSetup = iota + 1
-)
