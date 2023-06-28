ed2e564f305c2a9f3d65e11af59158a3b17c95ef
Steven Masley <stevenmasley@coder.com>
2023-06-28 09:15:21 -0400

    Remove messages

--- site/src/pages/UserSettingsPage/WorkspaceProxyPage/WorkspaceProxyRow.tsx
+++ site/src/pages/UserSettingsPage/WorkspaceProxyPage/WorkspaceProxyRow.tsx
@@ -1,4 +1,4 @@
-import { ProxyHealthReport, Region, WorkspaceProxy } from "api/typesGenerated"
+import { Region, WorkspaceProxy } from "api/typesGenerated"
 import { AvatarData } from "components/AvatarData/AvatarData"
 import { Avatar } from "components/Avatar/Avatar"
 import TableCell from "@mui/material/TableCell"
@@ -13,9 +13,6 @@ import {
 import { ProxyLatencyReport } from "contexts/useProxyLatency"
 import { getLatencyColor } from "utils/latency"
 import { makeStyles } from "@mui/styles"
-import { Stack } from "components/Stack/Stack"
-import List from "@mui/material/List"
-import ListItem from "@mui/material/ListItem"
 import { MONOSPACE_FONT_FAMILY } from "theme/constants"
 
 export const ProxyRow: FC<{
@@ -28,16 +25,8 @@ export const ProxyRow: FC<{
   // All users can see healthy/unhealthy, some can see more.
   let statusBadge = <ProxyStatus proxy={proxy} />
   let proxyText
-  let cellClass = ""
   if('status' in proxy) {
     statusBadge = <DetailedProxyStatus proxy= {proxy as WorkspaceProxy} />
-    const report = (proxy as WorkspaceProxy).status?.report
-    // Only show additional information if there are errors or warnings.
-    if(report && (report.errors || report.warnings)) {
-      cellClass = styles.noBottomBorder
-      proxyText=<ProxyStatusText proxyReport={report} />
-    }
-    console.log(report)
   }
 
   return (<>
@@ -45,7 +34,7 @@ export const ProxyRow: FC<{
       key={proxy.name}
       data-testid={`${proxy.name}`}
     >
-      <TableCell className={cellClass}>
+      <TableCell>
         <AvatarData
           title={
             proxy.display_name && proxy.display_name.length > 0
@@ -65,13 +54,12 @@ export const ProxyRow: FC<{
         />
       </TableCell>
 
-      <TableCell className={cellClass} sx={{ fontSize: 14 }}>{proxy.path_app_url}</TableCell>
-      <TableCell className={cellClass} sx={{ fontSize: 14 }}>
+      <TableCell sx={{ fontSize: 14 }}>{proxy.path_app_url}</TableCell>
+      <TableCell sx={{ fontSize: 14 }}>
         {statusBadge}
         {/* <ProxyStatus proxy={proxy} /> */}
       </TableCell>
       <TableCell
-      className={cellClass}
         sx={{
           fontSize: 14,
           textAlign: "right",
@@ -95,34 +83,6 @@ export const ProxyRow: FC<{
     }
     </>
   )
-}
-
-const ProxyStatusText: FC<{
-  proxyReport: ProxyHealthReport
-}> = ({ proxyReport }) => {
-  const styles = useStyles()
-    return (
-      <Stack direction="column">
-        <List>
-          {
-            proxyReport.errors && proxyReport.errors.length > 0 &&
-            proxyReport.errors.map((error, index) => (
-              <ListItem key={index}>
-                <span>Error: </span><span className={styles.proxyStatusText}>{error}</span>
-              </ListItem>
-            ))
-          }
-          {
-            proxyReport.warnings && proxyReport.warnings.length > 0 &&
-            proxyReport.warnings.map((warn, index) => (
-              <ListItem key={index}>
-                <span>Warning: </span>{warn}
-              </ListItem>
-            ))
-          }
-        </List>
-      </Stack>
-    )
 }
 
 const DetailedProxyStatus: FC<{
