af411be28acac62c9ff21b4aa443c6b8c96333a2
Steven Masley <stevenmasley@coder.com>
2023-06-28 16:16:33 -0400

    Include mock of new api route

--- site/src/testHelpers/handlers.ts
+++ site/src/testHelpers/handlers.ts
@@ -134,6 +134,11 @@ export const handlers = [
   rest.post("/api/v2/users", async (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(M.MockUser))
   }),
+  rest.post("/api/v2/users/me/login-type", async (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({
+      login_type: "password",
+    }))
+  }),
   rest.get("/api/v2/users/me/organizations", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json([M.MockOrganization]))
   }),
