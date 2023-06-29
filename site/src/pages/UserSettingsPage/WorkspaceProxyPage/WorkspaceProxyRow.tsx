356554e251acc4dc255d6c1baeba472a57d380b6
Steven Masley <stevenmasley@coder.com>
2023-06-29 08:36:35 -0400

    Remove excess

--- site/src/pages/UserSettingsPage/WorkspaceProxyPage/WorkspaceProxyRow.tsx
+++ site/src/pages/UserSettingsPage/WorkspaceProxyPage/WorkspaceProxyRow.tsx
@@ -12,19 +12,14 @@ import {
 } from "components/DeploySettingsLayout/Badges"
 import { ProxyLatencyReport } from "contexts/useProxyLatency"
 import { getLatencyColor } from "utils/latency"
-import { makeStyles } from "@mui/styles"
-import { MONOSPACE_FONT_FAMILY } from "theme/constants"
 
 export const ProxyRow: FC<{
   latency?: ProxyLatencyReport
   proxy: Region
 }> = ({ proxy, latency }) => {
-  const styles = useStyles()
-
   // If we have a more specific proxy status, use that.
   // All users can see healthy/unhealthy, some can see more.
   let statusBadge = <ProxyStatus proxy={proxy} />
-  let proxyText
   if ("status" in proxy) {
     statusBadge = <DetailedProxyStatus proxy={proxy as WorkspaceProxy} />
   }
@@ -55,7 +50,6 @@ export const ProxyRow: FC<{
         <TableCell sx={{ fontSize: 14 }}>{proxy.path_app_url}</TableCell>
         <TableCell sx={{ fontSize: 14 }}>
           {statusBadge}
-          {/* <ProxyStatus proxy={proxy} /> */}
         </TableCell>
         <TableCell
           sx={{
@@ -70,20 +64,16 @@ export const ProxyRow: FC<{
           {latency ? `${latency.latencyMS.toFixed(0)} ms` : "Not available"}
         </TableCell>
       </TableRow>
-      {proxyText ? (
-        <TableRow className={styles.noBottomBorder}>
-          <TableCell colSpan={4}>{proxyText}</TableCell>
-        </TableRow>
-      ) : null}
     </>
   )
 }
 
+// DetailedProxyStatus allows a more precise status to be displayed.
 const DetailedProxyStatus: FC<{
   proxy: WorkspaceProxy
 }> = ({ proxy }) => {
   if (!proxy.status) {
-    // If the status is not set, go with the less detailed version.
+    // If the status is null/undefined/not provided, just go with the boolean "healthy" value.
     return <ProxyStatus proxy={proxy} />
   }
 
@@ -101,6 +91,7 @@ const DetailedProxyStatus: FC<{
   }
 }
 
+// ProxyStatus will only show "healthy" or "not healthy" status.
 const ProxyStatus: FC<{
   proxy: Region
 }> = ({ proxy }) => {
@@ -111,20 +102,3 @@ const ProxyStatus: FC<{
 
   return icon
 }
-
-const useStyles = makeStyles({
-  noBottomBorder: {
-    borderBottom: "none",
-  },
-  proxyStatusText: {
-    // border: `1px solid ${theme.palette.success.light}`,
-    // backgroundColor: theme.palette.success.dark,
-    // textTransform: "none",
-    // color: "white",
-    fontFamily: MONOSPACE_FONT_FAMILY,
-    textDecoration: "none",
-  },
-  proxyStatusContainer: {
-    gap: "0px",
-  },
-})
