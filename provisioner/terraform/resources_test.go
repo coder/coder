7bf20981b1e0a10bbed152c7401f8411b5dfbf5d
Marcin Tojek <marcin@coder.com>
2023-07-05 12:31:44 +0200

    Test

--- provisioner/terraform/resources_test.go
+++ provisioner/terraform/resources_test.go
@@ -636,6 +636,28 @@ func TestAppSlugValidation(t *testing.T) {
 	require.ErrorContains(t, err, "duplicate app slug")
 }
 
+func TestMetadataResourceDuplicate(t *testing.T) {
+	t.Parallel()
+
+	// nolint:dogsled
+	_, filename, _, _ := runtime.Caller(0)
+
+	// Load the multiple-apps state file and edit it.
+	dir := filepath.Join(filepath.Dir(filename), "testdata", "resource-metadata-duplicate")
+	tfPlanRaw, err := os.ReadFile(filepath.Join(dir, "resource-metadata-duplicate.tfplan.json"))
+	require.NoError(t, err)
+	var tfPlan tfjson.Plan
+	err = json.Unmarshal(tfPlanRaw, &tfPlan)
+	require.NoError(t, err)
+	tfPlanGraph, err := os.ReadFile(filepath.Join(dir, "resource-metadata-duplicate.tfplan.dot"))
+	require.NoError(t, err)
+
+	state, err := terraform.ConvertState([]*tfjson.StateModule{tfPlan.PlannedValues.RootModule}, string(tfPlanGraph))
+	require.Nil(t, state)
+	require.Error(t, err)
+	require.ErrorContains(t, err, "duplicate metadata resource ID: null_resource.about")
+}
+
 func TestParameterValidation(t *testing.T) {
 	t.Parallel()
 
