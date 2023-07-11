5e1a1ce51189141c5a135eb2d70b772c29798b8c
Ben <me@bpmct.net>
2023-07-11 21:03:54 +0000

    docs: custom API use cases

--- docs/api/index.md
+++ docs/api/index.md
@@ -1,4 +1,22 @@
-Get started with Coder API:
+Get started with the Coder API:
+
+## Quickstart
+
+Generate a token on your Coder deployment by visiting:
+
+```sh
+https://coder.example.com/settings/tokens
+```
+
+List your workspaces
+
+```sh
+# CLI
+curl https://coder.example.com/api/v2/workspaces?q=owner:me \
+-H "Coder-Session-Token: <your-token>"
+```
+
+## Sections
 
 <children>
   This page is rendered on https://coder.com/docs/coder-oss/api. Refer to the other documents in the `api/` directory.
--- scripts/apidocgen/postprocess/main.go
+++ scripts/apidocgen/postprocess/main.go
@@ -18,10 +18,28 @@ import (
 const (
 	apiSubdir       = "api"
 	apiIndexFile    = "index.md"
-	apiIndexContent = `Get started with Coder API:
+	apiIndexContent = `Get started with the Coder API:
+
+## Quickstart
+
+Generate a token on your Coder deployment by visiting:
+
+` + "````sh" + `
+https://coder.example.com/settings/tokens
+` + "````" + `
+
+List your workspaces
+
+` + "````sh" + `
+# CLI
+curl https://coder.example.com/api/v2/workspaces?q=owner:me \
+-H "Coder-Session-Token: <your-token>"
+` + "````" + `
+
+## Sections
 
 <children>
-  This page is rendered on https://coder.com/docs/coder-oss/api. Refer to the other documents in the ` + "`" + `api/` + "`" + ` directory.
+  This page is rendered on https://coder.com/docs/coder-oss/api. Refer to the other documents in the ` + "`api/`" + ` directory.
 </children>
 `
 )
