f5a820b2bd0a7789cba58457b92c6660c8e44ef5
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2023-07-17 17:38:33 +0000

    Add ephemeral parameters section

--- site/src/pages/WorkspaceSettingsPage/WorkspaceParametersPage/WorkspaceParametersForm.tsx
+++ site/src/pages/WorkspaceSettingsPage/WorkspaceParametersPage/WorkspaceParametersForm.tsx
@@ -65,36 +65,80 @@ export const WorkspaceParametersForm: FC<{
     form,
     error,
   )
+  const hasEphemeralParameters = mutableParameters.some(
+    (parameter) => parameter.ephemeral,
+  )
+  const hasNonEphemeralParameters = mutableParameters.some(
+    (parameter) => !parameter.ephemeral,
+  )
 
   return (
     <HorizontalForm onSubmit={form.handleSubmit} data-testid="form">
-      {mutableParameters.length > 0 && (
+      {hasNonEphemeralParameters && (
         <FormSection
           title={t("parameters").toString()}
           description={t("parametersDescription").toString()}
         >
           <FormFields>
-            {mutableParameters.map((parameter, index) => (
-              <RichParameterInput
-                {...getFieldHelpers(
-                  "rich_parameter_values[" + index + "].value",
-                )}
-                disabled={isSubmitting}
-                index={index}
-                key={parameter.name}
-                onChange={async (value) => {
-                  await form.setFieldValue("rich_parameter_values." + index, {
-                    name: parameter.name,
-                    value: value,
-                  })
-                }}
-                parameter={parameter}
-                initialValue={workspaceBuildParameterValue(
-                  buildParameters,
-                  parameter,
-                )}
-              />
-            ))}
+            {mutableParameters.map((parameter, index) =>
+              // Since we are adding the values to the form based on the index
+              // we can't filter them to not loose the right index position
+              parameter.ephemeral ? null : (
+                <RichParameterInput
+                  {...getFieldHelpers(
+                    "rich_parameter_values[" + index + "].value",
+                  )}
+                  disabled={isSubmitting}
+                  index={index}
+                  key={parameter.name}
+                  onChange={async (value) => {
+                    await form.setFieldValue("rich_parameter_values." + index, {
+                      name: parameter.name,
+                      value: value,
+                    })
+                  }}
+                  parameter={parameter}
+                  initialValue={workspaceBuildParameterValue(
+                    buildParameters,
+                    parameter,
+                  )}
+                />
+              ),
+            )}
+          </FormFields>
+        </FormSection>
+      )}
+      {hasEphemeralParameters && (
+        <FormSection
+          title="Ephemeral Parameters"
+          description="These parameters only apply for a single workspace start."
+        >
+          <FormFields>
+            {mutableParameters.map((parameter, index) =>
+              // Since we are adding the values to the form based on the index
+              // we can't filter them to not loose the right index position
+              parameter.ephemeral ? (
+                <RichParameterInput
+                  {...getFieldHelpers(
+                    "rich_parameter_values[" + index + "].value",
+                  )}
+                  disabled={isSubmitting}
+                  index={index}
+                  key={parameter.name}
+                  onChange={async (value) => {
+                    await form.setFieldValue("rich_parameter_values." + index, {
+                      name: parameter.name,
+                      value: value,
+                    })
+                  }}
+                  parameter={parameter}
+                  initialValue={workspaceBuildParameterValue(
+                    buildParameters,
+                    parameter,
+                  )}
+                />
+              ) : null,
+            )}
           </FormFields>
         </FormSection>
       )}
