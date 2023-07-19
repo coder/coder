6318c4c09fabfe91106fa69844271e2a785b4fa4
Josh Vawdrey <jvawdrey@canva.com>
2023-07-19 23:43:10 +1000

    feat(site): increase TTL and max TTL validation to 30 days (#8258)
    Co-authored-by: Dean Sheather <dean@deansheather.com>

--- coderd/workspaces.go
+++ coderd/workspaces.go
@@ -32,10 +32,10 @@ import (
 
 var (
 	ttlMin = time.Minute //nolint:revive // min here means 'minimum' not 'minutes'
-	ttlMax = 7 * 24 * time.Hour
+	ttlMax = 30 * 24 * time.Hour
 
 	errTTLMin              = xerrors.New("time until shutdown must be at least one minute")
-	errTTLMax              = xerrors.New("time until shutdown must be less than 7 days")
+	errTTLMax              = xerrors.New("time until shutdown must be less than 30 days")
 	errDeadlineTooSoon     = xerrors.New("new deadline must be at least 30 minutes in the future")
 	errDeadlineBeforeStart = xerrors.New("new deadline must be before workspace start time")
 )
