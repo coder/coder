9ae8f1d4c4a04d50b9e08348c4210d2a6e6d80e8
Marcin Tojek <marcin@coder.com>
2023-07-19 16:14:06 +0200

    WIP

--- cli/portforward.go
+++ cli/portforward.go
@@ -32,7 +32,7 @@ func (r *RootCmd) portForward() *clibase.Cmd {
 	client := new(codersdk.Client)
 	cmd := &clibase.Cmd{
 		Use:     "port-forward <workspace>",
-		Short:   "Forward ports from machine to a workspace",
+		Short:   "Forward ports from a workspace to the local machine",
 		Aliases: []string{"tunnel"},
 		Long: formatExamples(
 			example{
