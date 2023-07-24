dfd2f031a03445fc7f5e9a7c792f11038725996f
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2023-07-24 17:33:45 +0000

    Set basic grid for the panels

--- site/src/pages/TemplatePage/TemplateInsightsPage/TemplateInsightsPage.tsx
+++ site/src/pages/TemplatePage/TemplateInsightsPage/TemplateInsightsPage.tsx
@@ -1,5 +1,42 @@
+import Box from "@mui/material/Box"
+import { styled } from "@mui/material/styles"
+
 const TemplateInsightsPage = () => {
-  return <div>TemplateInsightsPage</div>
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, minmax(0, 1fr))",
+        gridTemplateRows: "440px auto",
+        gap: (theme) => theme.spacing(3),
+      }}
+    >
+      <Panel sx={{ gridColumn: "span 2" }}>
+        <PanelHeader>Active daily users</PanelHeader>
+      </Panel>
+
+      <Panel>
+        <PanelHeader>Latency by user</PanelHeader>
+      </Panel>
+
+      <Panel sx={{ gridColumn: "span 3" }}>
+        <PanelHeader>App&lsquo;s & IDE usage</PanelHeader>
+      </Panel>
+    </Box>
+  )
 }
 
 export default TemplateInsightsPage
+
+const Panel = styled(Box)(({ theme }) => ({
+  borderRadius: theme.shape.borderRadius,
+  border: `1px solid ${theme.palette.divider}`,
+  backgroundColor: theme.palette.background.paper,
+}))
+
+const PanelHeader = styled(Box)(({ theme }) => ({
+  fontSize: 14,
+  fontWeight: 500,
+  padding: theme.spacing(3),
+  lineHeight: 1,
+}))
