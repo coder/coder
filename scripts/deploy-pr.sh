24dc7c27ab3aea8d394c3688f76cb3f2e29e1700
Muhammad Atif Ali <me@matifali.dev>
2023-07-27 14:49:11 +0300

    add dry run flag

--- scripts/deploy-pr.sh
+++ scripts/deploy-pr.sh
@@ -5,7 +5,7 @@
 # if --skip-build is passed, the build step will be skipped and the last build image will be used
 # if --yes or -y is passed, the script will not ask for confirmation before deploying
 
-set -euox pipefail
+set -euo pipefail
 
 # ask for user confirmation before deploying also skip confirmation if --yes or -y is passed
 if [[ "$*" != *--yes* ]] && [[ "$*" != *-y* ]]; then
@@ -22,8 +22,9 @@ prNumber=$(gh pr view --json number | jq -r .number)
 if [[ "$*" == *--skip-build* ]]; then
 	skipBuild=true
 	#check if the image exists
-	foundTag=$(curl -fsSL https://github.com/coder/coder/pkgs/container/coder-preview | grep -o "$prNumber" | head -n 1)
-	if [ -z "${foundTag}" ]; then
+	foundTag=$(curl -fsSL https://github.com/coder/coder/pkgs/container/coder-preview | grep -o "$prNumber" | head -n 1) || true
+	echo "foundTag is: '${foundTag}'"
+	if [[ -z "${foundTag}" ]]; then
 		echo "Image not found"
 		echo "${prNumber} tag not found in ghcr.io/coder/coder-preview"
 		echo "Please remove --skip-build and try again"
@@ -31,6 +32,17 @@ if [[ "$*" == *--skip-build* ]]; then
 	fi
 else
 	skipBuild=false
+fi
+
+## dry run with --dry-run or -n
+
+if [[ "$*" == *--dry-run* ]] || [[ "$*" == *-n* ]]; then
+	echo "dry run"
+	echo "branchName: ${branchName}"
+	echo "prNumber: ${prNumber}"
+	echo "skipBuild: ${skipBuild}"
+	echo "gh workflow run pr-deploy.yaml --ref "${branchName}" -f pr_number="${prNumber}" -f skip_build="${skipBuild}""
+	exit 0
 fi
 
 gh workflow run pr-deploy.yaml --ref "${branchName}" -f pr_number="${prNumber}" -f skip_build="${skipBuild}"
