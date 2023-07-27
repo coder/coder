505fd452821e636009a5b6d58eb13344e6557174
Muhammad Atif Ali <me@matifali.dev>
2023-07-27 09:38:27 +0300

    update

--- scripts/deploy-pr.sh
+++ scripts/deploy-pr.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Usage: ./deploy-pr.sh
 # deploys the current branch to a PR environment and posts login credentials to [#pr-deployments](https://codercom.slack.com/archives/C05DNE982E8) Slack channel
+set -euox pipefail
+
 branchName=$(gh pr view --json headRefName | jq -r .headRefName)
 if [[ "$branchName" == "main" ]]; then
     # get commit sha --short
