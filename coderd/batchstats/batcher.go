554fcf06ef173285cdb9117fed4321201d43cbbd
Cian Johnston <cian@coder.com>
2023-07-31 22:36:31 +0100

    wip

--- coderd/batchstats/batcher.go
+++ coderd/batchstats/batcher.go
@@ -2,6 +2,9 @@ package batchstats
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/google/uuid"
+	"golang.org/x/xerrors"
 	"os"
 	"sync"
 	"time"
@@ -13,9 +16,6 @@ import (
 )
 
 const (
-	// DefaultBatchInterval is the default interval at which the batcher
-	// flushes its buffer.
-	DefaultBatchInterval = 1 * time.Second
 	// DefaultBatchSize is the default size of the batcher's buffer.
 	DefaultBatchSize = 1024
 )
@@ -27,9 +27,12 @@ type Batcher struct {
 	log   slog.Logger
 
 	mu  sync.RWMutex
-	buf []agentsdk.AgentMetric
+	buf []database.InsertWorkspaceAgentStatParams
 
+	// ticker is used to periodically flush the buffer.
 	ticker <-chan time.Time
+	// flushLever is used to signal the flusher to flush the buffer immediately.
+	flushLever chan struct{}
 }
 
 // Option is a functional option for configuring a Batcher.
@@ -45,14 +48,14 @@ func WithStore(store database.Store) Option {
 // WithBatchSize sets the number of stats to store in a batch.
 func WithBatchSize(size int) Option {
 	return func(b *Batcher) {
-		b.buf = make([]agentsdk.AgentMetric, 0, size)
+		b.buf = make([]database.InsertWorkspaceAgentStatParams, 0, size)
 	}
 }
 
-// WithTicker sets the ticker to use for batching stats.
-func WithTicker(ticker <-chan time.Time) Option {
+// WithTicker sets the flush interval.
+func WithTicker(ch <-chan time.Time) Option {
 	return func(b *Batcher) {
-		b.ticker = ticker
+		b.ticker = ch
 	}
 }
 
@@ -64,10 +67,9 @@ func WithLogger(log slog.Logger) Option {
 }
 
 // New creates a new Batcher.
-func New(opts ...Option) (*Batcher, func()) {
+func New(opts ...Option) (*Batcher, error) {
 	b := &Batcher{}
-	closer := func() {}
-	buf := make([]agentsdk.AgentMetric, 0, DefaultBatchSize)
+	buf := make([]database.InsertWorkspaceAgentStatParams, 0, DefaultBatchSize)
 	b.buf = buf
 	b.log = slog.Make(sloghuman.Sink(os.Stderr))
 	for _, opt := range opts {
@@ -75,40 +77,73 @@ func New(opts ...Option) (*Batcher, func()) {
 	}
 
 	if b.store == nil {
-		panic("batcher needs a store")
+		return nil, xerrors.Errorf("no store configured for batcher")
 	}
 
 	if b.ticker == nil {
-		t := time.NewTicker(DefaultBatchInterval)
-		b.ticker = t.C
-		closer = t.Stop
+		return nil, xerrors.Errorf("no ticker configured for batcher")
 	}
 
-	return b, closer
+	return b, nil
 }
 
-// Add adds a stat to the batcher.
-func (b *Batcher) Add(m agentsdk.AgentMetric) {
+// Add adds a stat to the batcher for the given workspace and agent.
+func (b *Batcher) Add(
+	ctx context.Context,
+	agentID uuid.UUID,
+	st agentsdk.Stats,
+) error {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
-	b.buf = append(b.buf, m)
+	now := database.Now()
+	ws, err := b.store.GetWorkspaceByAgentID(ctx, agentID)
+	if err != nil {
+		return err
+	}
+	payload, err := json.Marshal(st.ConnectionsByProto)
+	if err != nil {
+		b.log.Error(ctx, "marshal agent connections by proto",
+			slog.F("workspace_agent_id", agentID),
+			slog.Error(err),
+		)
+		payload = json.RawMessage("{}")
+	}
+	p := database.InsertWorkspaceAgentStatParams{
+		ID:                          uuid.New(),
+		CreatedAt:                   now,
+		WorkspaceID:                 ws.ID,
+		UserID:                      ws.OwnerID,
+		TemplateID:                  ws.TemplateID,
+		ConnectionsByProto:          payload,
+		ConnectionCount:             st.ConnectionCount,
+		RxPackets:                   st.RxPackets,
+		RxBytes:                     st.RxBytes,
+		TxPackets:                   st.TxPackets,
+		TxBytes:                     st.TxBytes,
+		SessionCountVSCode:          st.SessionCountVSCode,
+		SessionCountJetBrains:       st.SessionCountJetBrains,
+		SessionCountReconnectingPTY: st.SessionCountReconnectingPTY,
+		SessionCountSSH:             st.SessionCountSSH,
+		ConnectionMedianLatencyMS:   st.ConnectionMedianLatencyMS,
+	}
+	b.buf = append(b.buf, p)
+	return nil
 }
 
 // Run runs the batcher.
 func (b *Batcher) Run(ctx context.Context) {
 	for {
 		select {
-		case tick, ok := <-b.ticker:
-			if !ok {
-				b.log.Warn(ctx, "ticker closed, flushing batcher")
-				b.flush(ctx, time.Now())
-				return
-			}
+		case tick := <-b.ticker:
 			b.flush(ctx, tick)
+		case <-b.flushLever:
+			// If the flush lever is depressed, flush the buffer immediately.
+			b.log.Warn(ctx, "flushing due to full buffer", slog.F("count", len(b.buf)))
+			b.flush(ctx, database.Now())
 		case <-ctx.Done():
-			b.log.Warn(ctx, "context done, flushing batcher")
-			b.flush(ctx, time.Now())
+			b.log.Warn(ctx, "context done, flushing before exit")
+			b.flush(ctx, database.Now())
 			return
 		}
 	}
@@ -118,12 +153,14 @@ func (b *Batcher) Run(ctx context.Context) {
 func (b *Batcher) flush(ctx context.Context, now time.Time) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
+	// TODO(Cian): After flushing, should we somehow reset the ticker?
 
 	if len(b.buf) == 0 {
+		b.log.Debug(ctx, "nothing to flush")
 		return
 	}
 
-	b.log.Debug(context.Background(), "flushing batcher", slog.F("count", len(b.buf)))
+	b.log.Debug(context.Background(), "flushing buffer", slog.F("count", len(b.buf)))
 	// TODO(cian): update the query to batch-insert multiple stats
 	for range b.buf {
 		if _, err := b.store.InsertWorkspaceAgentStat(ctx, database.InsertWorkspaceAgentStatParams{
--- coderd/batchstats/batcher_test.go
+++ coderd/batchstats/batcher_test.go
@@ -3,6 +3,7 @@ package batchstats_test
 import (
 	"context"
 	"github.com/coder/coder/codersdk/agentsdk"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
@@ -27,15 +28,18 @@ func TestBatchStats(t *testing.T) {
 	ws2 := dbgen.Workspace(t, store, database.Workspace{
 		LastUsedAt: time.Now().Add(-time.Hour),
 	})
+	// TODO: link to ws1 and ws2
+	agt1 := dbgen.WorkspaceAgent(t, store, database.WorkspaceAgent{})
+	agt2 := dbgen.WorkspaceAgent(t, store, database.WorkspaceAgent{})
 	startedAt := time.Now()
 	tick := make(chan time.Time)
 
-	b, closer := batchstats.New(
+	b, err := batchstats.New(
 		batchstats.WithStore(store),
 		batchstats.WithLogger(log),
 		batchstats.WithTicker(tick),
 	)
-	t.Cleanup(closer)
+	require.NoError(t, err)
 
 	// When: it becomes time to report stats
 	done := make(chan struct{})
@@ -62,7 +66,7 @@ func TestBatchStats(t *testing.T) {
 	require.Equal(t, ws2.LastUsedAt, updated2.LastUsedAt)
 
 	// When: a single data point is added for ws1
-	b.Add(agentsdk.AgentMetric{})
+	require.NoError(t, b.Add(ctx, agt1.ID, randAgentSDKStats(t)))
 	// And it becomes time to report stats
 	t2 := time.Now()
 	tick <- t2
@@ -85,7 +89,11 @@ func TestBatchStats(t *testing.T) {
 	// (equal to batch size)
 	t3 := time.Now()
 	for i := 0; i < batchstats.DefaultBatchSize; i++ {
-		b.Add(agentsdk.AgentMetric{})
+		if i%2 == 0 {
+			require.NoError(t, b.Add(ctx, agt1.ID, randAgentSDKStats(t)))
+		} else {
+			require.NoError(t, b.Add(ctx, agt2.ID, randAgentSDKStats(t)))
+		}
 	}
 
 	// Then: it should immediately flush its stats to store.
@@ -101,3 +109,78 @@ func TestBatchStats(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEqual(t, ws2.LastUsedAt, updated2.LastUsedAt)
 }
+
+// randAgentSDKStats returns a random agentsdk.Stats
+func randAgentSDKStats(t *testing.T, opts ...func(*agentsdk.Stats)) agentsdk.Stats {
+	t.Helper()
+	var s agentsdk.Stats
+	for _, opt := range opts {
+		opt(&s)
+	}
+	return s
+}
+
+// randInsertWorkspaceAgentStatParams returns a random InsertWorkspaceAgentStatParams
+func randInsertWorkspaceAgentStatParams(t *testing.T, opts ...func(params *database.InsertWorkspaceAgentStatParams)) database.InsertWorkspaceAgentStatParams {
+	t.Helper()
+	p := database.InsertWorkspaceAgentStatParams{
+		ID:                          uuid.New(),
+		CreatedAt:                   time.Now(),
+		UserID:                      uuid.New(),
+		WorkspaceID:                 uuid.New(),
+		TemplateID:                  uuid.New(),
+		AgentID:                     uuid.New(),
+		ConnectionsByProto:          []byte(`{"tcp": 1}`),
+		ConnectionCount:             1,
+		RxPackets:                   1,
+		RxBytes:                     1,
+		TxPackets:                   1,
+		TxBytes:                     1,
+		SessionCountVSCode:          1,
+		SessionCountJetBrains:       1,
+		SessionCountReconnectingPTY: 0,
+		SessionCountSSH:             0,
+		ConnectionMedianLatencyMS:   0,
+	}
+	for _, opt := range opts {
+		opt(&p)
+	}
+	return p
+}
+
+//type InsertWorkspaceAgentStatParams struct {
+//	ID                          uuid.UUID       `db:"id" json:"id"`
+//	CreatedAt                   time.Time       `db:"created_at" json:"created_at"`
+//	UserID                      uuid.UUID       `db:"user_id" json:"user_id"`
+//	WorkspaceID                 uuid.UUID       `db:"workspace_id" json:"workspace_id"`
+//	TemplateID                  uuid.UUID       `db:"template_id" json:"template_id"`
+//	AgentID                     uuid.UUID       `db:"agent_id" json:"agent_id"`
+//	ConnectionsByProto          json.RawMessage `db:"connections_by_proto" json:"connections_by_proto"`
+//	ConnectionCount             int64           `db:"connection_count" json:"connection_count"`
+//	RxPackets                   int64           `db:"rx_packets" json:"rx_packets"`
+//	RxBytes                     int64           `db:"rx_bytes" json:"rx_bytes"`
+//	TxPackets                   int64           `db:"tx_packets" json:"tx_packets"`
+//	TxBytes                     int64           `db:"tx_bytes" json:"tx_bytes"`
+//	SessionCountVSCode          int64           `db:"session_count_vscode" json:"session_count_vscode"`
+//	SessionCountJetBrains       int64           `db:"session_count_jetbrains" json:"session_count_jetbrains"`
+//	SessionCountReconnectingPTY int64           `db:"session_count_reconnecting_pty" json:"session_count_reconnecting_pty"`
+//	SessionCountSSH             int64           `db:"session_count_ssh" json:"session_count_ssh"`
+//	ConnectionMedianLatencyMS   float64         `db:"connection_median_latency_ms" json:"connection_median_latency_ms"`
+//}
+
+//type GetWorkspaceAgentStatsRow struct {
+//	UserID                       uuid.UUID `db:"user_id" json:"user_id"`
+//	AgentID                      uuid.UUID `db:"agent_id" json:"agent_id"`
+//	WorkspaceID                  uuid.UUID `db:"workspace_id" json:"workspace_id"`
+//	TemplateID                   uuid.UUID `db:"template_id" json:"template_id"`
+//	AggregatedFrom               time.Time `db:"aggregated_from" json:"aggregated_from"`
+//	WorkspaceRxBytes             int64     `db:"workspace_rx_bytes" json:"workspace_rx_bytes"`
+//	WorkspaceTxBytes             int64     `db:"workspace_tx_bytes" json:"workspace_tx_bytes"`
+//	WorkspaceConnectionLatency50 float64   `db:"workspace_connection_latency_50" json:"workspace_connection_latency_50"`
+//	WorkspaceConnectionLatency95 float64   `db:"workspace_connection_latency_95" json:"workspace_connection_latency_95"`
+//	AgentID_2                    uuid.UUID `db:"agent_id_2" json:"agent_id_2"`
+//	SessionCountVSCode           int64     `db:"session_count_vscode" json:"session_count_vscode"`
+//	SessionCountSSH              int64     `db:"session_count_ssh" json:"session_count_ssh"`
+//	SessionCountJetBrains        int64     `db:"session_count_jetbrains" json:"session_count_jetbrains"`
+//	SessionCountReconnectingPTY  int64     `db:"session_count_reconnecting_pty" json:"session_count_reconnecting_pty"`
+//}
