bcd0b996539e994e5b5b7fe88d31e8f0d1168265
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2023-08-01 16:54:19 +0000

    Add storybook

---
+++ site/src/pages/HealthPage/HealthPage.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { HealthPageView } from "./HealthPage"
+import { MockHealth } from "testHelpers/entities"
+
+const meta: Meta<typeof HealthPageView> = {
+  title: "pages/HealthPageView",
+  component: HealthPageView,
+  args: {
+    tab: {
+      value: "derp",
+      set: () => {},
+    },
+    healthStatus: MockHealth,
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof HealthPageView>
+
+export const HealthPage: Story = {}
+
+export const UnhealthPage: Story = {
+  args: {
+    healthStatus: {
+      ...MockHealth,
+      healthy: false,
+      derp: {
+        ...MockHealth.derp,
+        healthy: false,
+      },
+    },
+  },
+}
--- site/src/pages/HealthPage/HealthPage.tsx
+++ site/src/pages/HealthPage/HealthPage.tsx
@@ -31,7 +31,6 @@ export default function HealthPage() {
     queryKey: ["health"],
     queryFn: () => getHealth(),
   })
-  const styles = useStyles()
 
   return (
     <>
@@ -40,165 +39,177 @@ export default function HealthPage() {
       </Helmet>
 
       {healthStatus ? (
-        <Box
-          sx={{
-            height: "calc(100vh - 62px - 36px)",
-            overflow: "hidden",
-            // Remove padding added from dashboard layout (.siteContent)
-            marginBottom: "-48px",
-          }}
-        >
-          <FullWidthPageHeader sticky={false}>
-            <Stack direction="row" spacing={2} alignItems="center">
-              {healthStatus.data.healthy ? (
-                <CheckCircleOutlined
-                  sx={{
-                    width: 32,
-                    height: 32,
-                    color: (theme) => theme.palette.success.light,
-                  }}
-                />
-              ) : (
-                <ErrorOutline
-                  sx={{
-                    width: 32,
-                    height: 32,
-                    color: (theme) => theme.palette.error.main,
-                  }}
-                />
-              )}
-
-              <div>
-                <PageHeaderTitle>
-                  {healthStatus.data.healthy ? "Healthy" : "Unhealthy"}
-                </PageHeaderTitle>
-                <PageHeaderSubtitle>
-                  {healthStatus.data.healthy
-                    ? "Your deploy is working as expected"
-                    : "Check the errors below"}
-                </PageHeaderSubtitle>
-              </div>
-            </Stack>
-
-            <Stats aria-label="Deployment details" className={styles.stats}>
-              <StatsItem
-                className={styles.statsItem}
-                label="Last check"
-                value={new Date(healthStatus.data.time).toLocaleString()}
-              />
-              <StatsItem
-                className={styles.statsItem}
-                label="Coder version"
-                value={healthStatus.data.coder_version}
-              />
-            </Stats>
-          </FullWidthPageHeader>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "start",
-              height: "100%",
-            }}
-          >
-            <Box
-              sx={{
-                width: (theme) => theme.spacing(32),
-                flexShrink: 0,
-                borderRight: (theme) => `1px solid ${theme.palette.divider}`,
-                height: "100%",
-              }}
-            >
-              <Box
-                sx={{
-                  fontSize: 10,
-                  textTransform: "uppercase",
-                  fontWeight: 500,
-                  color: (theme) => theme.palette.text.secondary,
-                  padding: (theme) => theme.spacing(1.5, 3),
-                  letterSpacing: "0.5px",
-                }}
-              >
-                Health checks
-              </Box>
-              <Box component="nav">
-                {Object.entries(sections).map(([key, label]) => {
-                  const isActive = tab.value === key
-                  const isHealthy =
-                    healthStatus.data[key as keyof typeof sections].healthy
-
-                  return (
-                    <Box
-                      component="button"
-                      key={key}
-                      onClick={() => {
-                        tab.set(key)
-                      }}
-                      sx={{
-                        background: isActive ? colors.gray[13] : "none",
-                        border: "none",
-                        fontSize: 14,
-                        width: "100%",
-                        display: "flex",
-                        alignItems: "center",
-                        gap: 1,
-                        textAlign: "left",
-                        height: 36,
-                        padding: (theme) => theme.spacing(0, 3),
-                        cursor: "pointer",
-                        pointerEvents: isActive ? "none" : "auto",
-                        color: (theme) =>
-                          isActive
-                            ? theme.palette.text.primary
-                            : theme.palette.text.secondary,
-                        "&:hover": {
-                          background: (theme) => theme.palette.action.hover,
-                          color: (theme) => theme.palette.text.primary,
-                        },
-                      }}
-                    >
-                      {isHealthy ? (
-                        <CheckCircleOutlined
-                          sx={{
-                            width: 16,
-                            height: 16,
-                            color: (theme) => theme.palette.success.light,
-                          }}
-                        />
-                      ) : (
-                        <ErrorOutline
-                          sx={{
-                            width: 16,
-                            height: 16,
-                            color: (theme) => theme.palette.error.main,
-                          }}
-                        />
-                      )}
-                      {label}
-                    </Box>
-                  )
-                })}
-              </Box>
-            </Box>
-            {/* 62px - navbar and 36px - the bottom bar */}
-            <Box sx={{ height: "100%", overflowY: "auto", width: "100%" }}>
-              <SyntaxHighlighter
-                language="json"
-                editorProps={{ height: "100%" }}
-                value={JSON.stringify(
-                  healthStatus.data[
-                    tab.value as keyof typeof healthStatus.data
-                  ],
-                  null,
-                  2,
-                )}
-              />
-            </Box>
-          </Box>
-        </Box>
+        <HealthPageView healthStatus={healthStatus.data} tab={tab} />
       ) : (
         <Loader />
       )}
     </>
+  )
+}
+
+export function HealthPageView({
+  healthStatus,
+  tab,
+}: {
+  healthStatus: Awaited<ReturnType<typeof getHealth>>["data"]
+  tab: ReturnType<typeof useTab>
+}) {
+  const styles = useStyles()
+
+  return (
+    <Box
+      sx={{
+        height: "calc(100vh - 62px - 36px)",
+        overflow: "hidden",
+        // Remove padding added from dashboard layout (.siteContent)
+        marginBottom: "-48px",
+      }}
+    >
+      <FullWidthPageHeader sticky={false}>
+        <Stack direction="row" spacing={2} alignItems="center">
+          {healthStatus.healthy ? (
+            <CheckCircleOutlined
+              sx={{
+                width: 32,
+                height: 32,
+                color: (theme) => theme.palette.success.light,
+              }}
+            />
+          ) : (
+            <ErrorOutline
+              sx={{
+                width: 32,
+                height: 32,
+                color: (theme) => theme.palette.error.main,
+              }}
+            />
+          )}
+
+          <div>
+            <PageHeaderTitle>
+              {healthStatus.healthy ? "Healthy" : "Unhealthy"}
+            </PageHeaderTitle>
+            <PageHeaderSubtitle>
+              {healthStatus.healthy
+                ? "Your deploy is working as expected"
+                : "Check the errors below"}
+            </PageHeaderSubtitle>
+          </div>
+        </Stack>
+
+        <Stats aria-label="Deployment details" className={styles.stats}>
+          <StatsItem
+            className={styles.statsItem}
+            label="Last check"
+            value={new Date(healthStatus.time).toLocaleString()}
+          />
+          <StatsItem
+            className={styles.statsItem}
+            label="Coder version"
+            value={healthStatus.coder_version}
+          />
+        </Stats>
+      </FullWidthPageHeader>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "start",
+          height: "100%",
+        }}
+      >
+        <Box
+          sx={{
+            width: (theme) => theme.spacing(32),
+            flexShrink: 0,
+            borderRight: (theme) => `1px solid ${theme.palette.divider}`,
+            height: "100%",
+          }}
+        >
+          <Box
+            sx={{
+              fontSize: 10,
+              textTransform: "uppercase",
+              fontWeight: 500,
+              color: (theme) => theme.palette.text.secondary,
+              padding: (theme) => theme.spacing(1.5, 3),
+              letterSpacing: "0.5px",
+            }}
+          >
+            Health checks
+          </Box>
+          <Box component="nav">
+            {Object.entries(sections).map(([key, label]) => {
+              const isActive = tab.value === key
+              const isHealthy =
+                healthStatus[key as keyof typeof sections].healthy
+
+              return (
+                <Box
+                  component="button"
+                  key={key}
+                  onClick={() => {
+                    tab.set(key)
+                  }}
+                  sx={{
+                    background: isActive ? colors.gray[13] : "none",
+                    border: "none",
+                    fontSize: 14,
+                    width: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    gap: 1,
+                    textAlign: "left",
+                    height: 36,
+                    padding: (theme) => theme.spacing(0, 3),
+                    cursor: "pointer",
+                    pointerEvents: isActive ? "none" : "auto",
+                    color: (theme) =>
+                      isActive
+                        ? theme.palette.text.primary
+                        : theme.palette.text.secondary,
+                    "&:hover": {
+                      background: (theme) => theme.palette.action.hover,
+                      color: (theme) => theme.palette.text.primary,
+                    },
+                  }}
+                >
+                  {isHealthy ? (
+                    <CheckCircleOutlined
+                      sx={{
+                        width: 16,
+                        height: 16,
+                        color: (theme) => theme.palette.success.light,
+                      }}
+                    />
+                  ) : (
+                    <ErrorOutline
+                      sx={{
+                        width: 16,
+                        height: 16,
+                        color: (theme) => theme.palette.error.main,
+                      }}
+                    />
+                  )}
+                  {label}
+                </Box>
+              )
+            })}
+          </Box>
+        </Box>
+        {/* 62px - navbar and 36px - the bottom bar */}
+        <Box sx={{ height: "100%", overflowY: "auto", width: "100%" }}>
+          <SyntaxHighlighter
+            language="json"
+            editorProps={{ height: "100%" }}
+            value={JSON.stringify(
+              healthStatus[tab.value as keyof typeof healthStatus],
+              null,
+              2,
+            )}
+          />
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
