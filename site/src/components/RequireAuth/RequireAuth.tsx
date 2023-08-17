9710bada06da6565a158f451f3daaf2e9b271a1c
Kayla Washburn <mckayla@hey.com>
2023-08-17 13:54:20 -0600

    fix: redirect to login upon authentication error (#9134)

--- site/src/components/RequireAuth/RequireAuth.tsx
+++ site/src/components/RequireAuth/RequireAuth.tsx
@@ -1,18 +1,44 @@
+import axios from "axios"
 import { useAuth } from "components/AuthProvider/AuthProvider"
-import { FC } from "react"
+import { FC, useEffect } from "react"
 import { Outlet, Navigate, useLocation } from "react-router-dom"
 import { embedRedirect } from "../../utils/redirect"
 import { FullScreenLoader } from "../Loader/FullScreenLoader"
 import { DashboardProvider } from "components/Dashboard/DashboardProvider"
 import { ProxyProvider } from "contexts/ProxyContext"
+import { getErrorDetail } from "api/errors"
 
 export const RequireAuth: FC = () => {
-  const [authState] = useAuth()
+  const [authState, authSend] = useAuth()
   const location = useLocation()
   const isHomePage = location.pathname === "/"
   const navigateTo = isHomePage
     ? "/login"
     : embedRedirect(`${location.pathname}${location.search}`)
+
+  useEffect(() => {
+    const interceptorHandle = axios.interceptors.response.use(
+      (okResponse) => okResponse,
+      (error) => {
+        // 401 Unauthorized
+        // If we encountered an authentication error, then our token is probably
+        // invalid and we should update the auth state to reflect that.
+        if (
+          error.response.status === 401 &&
+          getErrorDetail(error)?.startsWith("API key expired")
+        ) {
+          authSend("SIGN_OUT")
+        }
+
+        // Otherwise, pass the response through so that it can be displayed in the UI
+        return Promise.reject(error)
+      },
+    )
+
+    return () => {
+      axios.interceptors.response.eject(interceptorHandle)
+    }
+  }, [authSend])
 
   if (authState.matches("signedOut")) {
     return <Navigate to={navigateTo} state={{ isRedirect: !isHomePage }} />
