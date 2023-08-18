a64927caacb00de14e1f0fa3d038db5fde5bda19
Cian Johnston <cian@coder.com>
2023-08-18 11:48:54 +0000

    rename incorrectly named migration file

--- coderd/database/migrations/000151_workspace_agent_auth_tokens_unique_idx.down.sql
+++ coderd/database/migrations/000151_workspace_agent_auth_tokens_unique_idx.down.sql
@@ -1,5 +1,5 @@
 -- Drop the unique index
 DROP INDEX IF EXISTS workspace_agents_auth_token_uniq_idx;
 
--- Recreate the old non-unqiue index.
+-- Recreate the old non-unique index.
 CREATE INDEX workspace_agents_auth_token_idx ON workspace_agents USING btree (auth_token);
