0a5c90dc572dc23e1b895c98492771cec12ba235
Marcin Tojek <marcin@coder.com>
2023-08-23 11:07:14 +0200

    fix: rewrite onlyDataResources

--- provisioner/terraform/executor_internal_test.go
+++ provisioner/terraform/executor_internal_test.go
@@ -3,6 +3,7 @@ package terraform
 import (
 	"testing"
 
+	tfjson "github.com/hashicorp/terraform-json"
 	"github.com/stretchr/testify/require"
 
 	"github.com/coder/coder/v2/provisionersdk/proto"
@@ -40,4 +41,61 @@ From standing in the English rain`))
 		{Level: proto.LogLevel_INFO, Output: "From standing in the English rain"},
 	}
 	require.Equal(t, expected, logr.logs)
+}
+
+func TestOnlyDataResources(t *testing.T) {
+	t.Parallel()
+
+	sm := &tfjson.StateModule{
+		Resources: []*tfjson.StateResource{
+			{
+				Name:    "cat",
+				Type:    "coder_parameter",
+				Mode:    "data",
+				Address: "cat-address",
+			},
+			{
+				Name:    "dog",
+				Type:    "foobar",
+				Mode:    "magic",
+				Address: "dog-address",
+			},
+			{
+				Name:    "cow",
+				Type:    "foobaz",
+				Mode:    "data",
+				Address: "cow-address",
+			},
+		},
+		ChildModules: []*tfjson.StateModule{
+			{
+				Resources: []*tfjson.StateResource{
+					{
+						Name:    "child-cat",
+						Type:    "coder_parameter",
+						Mode:    "data",
+						Address: "child-cat-address",
+					},
+					{
+						Name:    "child-dog",
+						Type:    "foobar",
+						Mode:    "data",
+						Address: "child-dog-address",
+					},
+					{
+						Name:    "child-cow",
+						Mode:    "data",
+						Type:    "magic",
+						Address: "child-cow-address",
+					},
+				},
+				Address: "child-module-1",
+			},
+		},
+		Address: "fake-module",
+	}
+
+	filtered := onlyDataResources(*sm)
+	require.Len(t, filtered.Resources, 2)
+	require.Len(t, filtered.ChildModules, 1)
 }
