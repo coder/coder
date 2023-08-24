c8f436917256d0f11bf91854559dd287487cf03d
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2023-08-24 17:53:27 +0000

    Improve verbiage

--- site/src/pages/WorkspacesPage/WorkspacesPageView.tsx
+++ site/src/pages/WorkspacesPage/WorkspacesPageView.tsx
@@ -129,7 +129,7 @@ export const WorkspacesPageView: FC<
                 startIcon={<DeleteOutlined />}
                 onClick={onDeleteAll}
               >
-                Delete all
+                Delete selected
               </Button>
             </Box>
           </>
