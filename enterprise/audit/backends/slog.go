843fa34eaf5aa503fbc163f3f4e90cef44a1b09d
Colin Adler <colin1adler@gmail.com>
2023-08-28 22:07:59 +0000

    fix(enterprise): ensure audit log json fields are formatted correctly

--- enterprise/audit/backends/slog.go
+++ enterprise/audit/backends/slog.go
@@ -2,8 +2,10 @@ package backends
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/fatih/structs"
+	"github.com/sqlc-dev/pqtype"
 
 	"cdr.dev/slog"
 	"github.com/coder/coder/v2/coderd/database"
@@ -15,14 +17,14 @@ type slogBackend struct {
 }
 
 func NewSlog(logger slog.Logger) audit.Backend {
-	return slogBackend{log: logger}
+	return &slogBackend{log: logger}
 }
 
-func (slogBackend) Decision() audit.FilterDecision {
+func (*slogBackend) Decision() audit.FilterDecision {
 	return audit.FilterDecisionExport
 }
 
-func (b slogBackend) Export(ctx context.Context, alog database.AuditLog) error {
+func (b *slogBackend) Export(ctx context.Context, alog database.AuditLog) error {
 	// We don't use structs.Map because we don't want to recursively convert
 	// fields into maps. When we keep the type information, slog can more
 	// pleasantly format the output. For example, the clean result of
@@ -30,9 +32,22 @@ func (b slogBackend) Export(ctx context.Context, alog database.AuditLog) error {
 	sfs := structs.Fields(alog)
 	var fields []any
 	for _, sf := range sfs {
-		fields = append(fields, slog.F(sf.Name(), sf.Value()))
+		fields = append(fields, b.fieldToSlog(sf))
 	}
 
 	b.log.Info(ctx, "audit_log", fields...)
 	return nil
+}
+
+func (*slogBackend) fieldToSlog(field *structs.Field) slog.Field {
+	val := field.Value()
+
+	switch ty := field.Value().(type) {
+	case pqtype.Inet:
+		val = ty.IPNet.IP.String()
+	case sql.NullString:
+		val = ty.String
+	}
+
+	return slog.F(field.Name(), val)
 }
