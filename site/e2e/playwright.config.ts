f5d813c4fbab9217862ab4869fdfd2cf75c5c55a
Marcin Tojek <mtojek@users.noreply.github.com>
2023-08-29 15:10:31 +0200

    test(site): e2e: update workspace with parameters (#9338)

--- site/e2e/playwright.config.ts
+++ site/e2e/playwright.config.ts
@@ -41,6 +41,7 @@ export default defineConfig({
       `--access-url=http://localhost:${port} ` +
       `--http-address=localhost:${port} ` +
       `--in-memory --telemetry=false ` +
+      `--dangerous-disable-rate-limits ` +
       `--provisioner-daemons 10 ` +
       `--provisioner-daemons-echo ` +
       `--provisioner-daemon-poll-interval 50ms`,
