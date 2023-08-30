b3d0b1cebd967584e8c85a0ccaa526e330ff448d
Marcin Tojek <marcin@coder.com>
2023-08-30 17:09:10 +0200

    fix

--- site/e2e/tests/app.spec.ts
+++ site/e2e/tests/app.spec.ts
@@ -2,6 +2,7 @@ import { test } from "@playwright/test"
 import { randomUUID } from "crypto"
 import * as http from "http"
 import { createTemplate, createWorkspace, startAgent } from "../helpers"
+import { beforeCoderTest } from "../hooks"
 
 test.beforeEach(async ({ page }) => await beforeCoderTest(page))
 
