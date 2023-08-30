f8e4e667d5a8237b280d408230cb6821ec51e4a5
McKayla Washburn <mckayla@hey.com>
2023-08-30 19:28:40 +0000

    fix: hide OIDC and Github auth settings when they are disabled

--- site/src/components/DeploySettingsLayout/OptionsTable.tsx
+++ site/src/components/DeploySettingsLayout/OptionsTable.tsx
@@ -21,7 +21,7 @@ const OptionsTable: FC<{
   const styles = useStyles()
 
   if (options.length === 0) {
-    return <DisabledBadge></DisabledBadge>
+    return <p>No options to configure</p>
   }
 
   return (
--- site/src/pages/DeploySettingsPage/UserAuthSettingsPage/UserAuthSettingsPageView.tsx
+++ site/src/pages/DeploySettingsPage/UserAuthSettingsPage/UserAuthSettingsPageView.tsx
@@ -19,56 +19,59 @@ export type UserAuthSettingsPageViewProps = {
 
 export const UserAuthSettingsPageView = ({
   options,
-}: UserAuthSettingsPageViewProps): JSX.Element => (
-  <>
-    <Stack direction="column" spacing={6}>
-      <div>
-        <Header title="User Authentication" />
+}: UserAuthSettingsPageViewProps): JSX.Element => {
+  const oidcEnabled = Boolean(
+    useDeploymentOptions(options, "OIDC Client ID")[0].value,
+  )
+  const githubEnabled = Boolean(
+    useDeploymentOptions(options, "OAuth2 GitHub Client ID")[0].value,
+  )
 
-        <Header
-          title="Login with OpenID Connect"
-          secondary
-          description="Set up authentication to login with OpenID Connect."
-          docsHref={docs("/admin/auth#openid-connect-with-google")}
-        />
+  return (
+    <>
+      <Stack direction="column" spacing={6}>
+        <div>
+          <Header title="User Authentication" />
 
-        <Badges>
-          {useDeploymentOptions(options, "OIDC Client ID")[0].value ? (
-            <EnabledBadge />
-          ) : (
-            <DisabledBadge />
+          <Header
+            title="Login with OpenID Connect"
+            secondary
+            description="Set up authentication to login with OpenID Connect."
+            docsHref={docs("/admin/auth#openid-connect-with-google")}
+          />
+
+          <Badges>{oidcEnabled ? <EnabledBadge /> : <DisabledBadge />}</Badges>
+
+          {oidcEnabled && (
+            <OptionsTable
+              options={options.filter((o) =>
+                deploymentGroupHasParent(o.group, "OIDC"),
+              )}
+            />
           )}
-        </Badges>
+        </div>
 
-        <OptionsTable
-          options={options.filter((o) =>
-            deploymentGroupHasParent(o.group, "OIDC"),
+        <div>
+          <Header
+            title="Login with GitHub"
+            secondary
+            description="Set up authentication to login with GitHub."
+            docsHref={docs("/admin/auth#github")}
+          />
+
+          <Badges>
+            {githubEnabled ? <EnabledBadge /> : <DisabledBadge />}
+          </Badges>
+
+          {githubEnabled && (
+            <OptionsTable
+              options={options.filter((o) =>
+                deploymentGroupHasParent(o.group, "GitHub"),
+              )}
+            />
           )}
-        />
-      </div>
-
-      <div>
-        <Header
-          title="Login with GitHub"
-          secondary
-          description="Set up authentication to login with GitHub."
-          docsHref={docs("/admin/auth#github")}
-        />
-
-        <Badges>
-          {useDeploymentOptions(options, "OAuth2 GitHub Client ID")[0].value ? (
-            <EnabledBadge />
-          ) : (
-            <DisabledBadge />
-          )}
-        </Badges>
-
-        <OptionsTable
-          options={options.filter((o) =>
-            deploymentGroupHasParent(o.group, "GitHub"),
-          )}
-        />
-      </div>
-    </Stack>
-  </>
-)
+        </div>
+      </Stack>
+    </>
+  )
+}
