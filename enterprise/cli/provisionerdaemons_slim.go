be22f25a9e23b034ddc01a0452749f53ffe0bee2
Mathias Fredriksson <mafredri@gmail.com>
2023-09-01 17:20:34 +0000

    refactor(enterprise/cli): remove provisionerd from slim binary
    This change saves 8 MB in the slim binary.
    
    Ref: #9380

--- enterprise/cli/provisionerdaemons_slim.go
+++ enterprise/cli/provisionerdaemons_slim.go
@@ -6,19 +6,18 @@ import (
 	"github.com/coder/coder/v2/cli/clibase"
 )
 
-func (r *RootCmd) proxyServer() *clibase.Cmd {
-	root := &clibase.Cmd{
-		Use:     "server",
-		Short:   "Start a workspace proxy server",
-		Aliases: []string{},
+func (r *RootCmd) provisionerDaemons() *clibase.Cmd {
+	cmd := &clibase.Cmd{
+		Use:   "provisionerd",
+		Short: "Manage provisioner daemons",
 		// We accept RawArgs so all commands and flags are accepted.
 		RawArgs: true,
 		Hidden:  true,
 		Handler: func(inv *clibase.Invocation) error {
-			slimUnsupported(inv.Stderr, "workspace-proxy server")
+			slimUnsupported(inv.Stderr, "coder provisionerd")
 			return nil
 		},
 	}
 
-	return root
+	return cmd
 }
