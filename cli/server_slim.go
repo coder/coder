d2115941b7c73417e4ab347a2cb8be5cb5eec96d
Mathias Fredriksson <mafredri@gmail.com>
2023-09-04 19:38:53 +0300

    refactor(cli)!: remove reset-password from slim binary (#9520)
    This is an alternative approach to #9519 and removes 2 MB instead of 1
    MB (1.2 MB accounted for by embedded migration SQL files).
    
    Combined with #9481, #9506, #9508, #9517, a total of 5 MB is removed.
    
    Ref: #9380

--- cli/server_slim.go
+++ cli/server_slim.go
@@ -3,12 +3,7 @@
 package cli
 
 import (
-	"fmt"
-	"io"
-	"os"
-
 	"github.com/coder/coder/v2/cli/clibase"
-	"github.com/coder/coder/v2/cli/cliui"
 )
 
 func (r *RootCmd) Server(_ func()) *clibase.Cmd {
@@ -19,18 +14,10 @@ func (r *RootCmd) Server(_ func()) *clibase.Cmd {
 		RawArgs: true,
 		Hidden:  true,
 		Handler: func(inv *clibase.Invocation) error {
-			serverUnsupported(inv.Stderr)
+			SlimUnsupported(inv.Stderr, "server")
 			return nil
 		},
 	}
 
 	return root
-}
-
-func serverUnsupported(w io.Writer) {
-	_, _ = fmt.Fprintf(w, "You are using a 'slim' build of Coder, which does not support the %s subcommand.\n", cliui.DefaultStyles.Code.Render("server"))
-	_, _ = fmt.Fprintln(w, "")
-	_, _ = fmt.Fprintln(w, "Please use a build of Coder from GitHub releases:")
-	_, _ = fmt.Fprintln(w, "  https://github.com/coder/coder/releases")
-	os.Exit(1)
 }
