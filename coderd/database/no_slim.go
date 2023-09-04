94cc9b71cc78ce1138368c3bc82a5cfd82a7731b
Mathias Fredriksson <mafredri@gmail.com>
2023-09-04 18:45:23 +0000

    fix lint

--- coderd/database/no_slim.go
+++ coderd/database/no_slim.go
@@ -3,6 +3,6 @@ package database
 const (
 	// This declaration protects against imports in slim builds, see
 	// no_slim_slim.go.
-	//nolint:revive
+	//nolint:revive,unused
 	_DO_NOT_IMPORT_THIS_PACKAGE_IN_SLIM_BUILDS = "DO_NOT_IMPORT_THIS_PACKAGE_IN_SLIM_BUILDS"
 )
