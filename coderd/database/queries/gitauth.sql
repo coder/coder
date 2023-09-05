55b93e7d60509326208bd48aa0138229be53e89e
Cian Johnston <cian@coder.com>
2023-09-05 10:01:50 +0100

    fix indentation

--- coderd/database/queries/gitauth.sql
+++ coderd/database/queries/gitauth.sql
@@ -11,9 +11,9 @@ INSERT INTO git_auth_links (
     created_at,
     updated_at,
     oauth_access_token,
-	oauth_access_token_key_id,
+    oauth_access_token_key_id,
     oauth_refresh_token,
-	oauth_refresh_token_key_id,
+    oauth_refresh_token_key_id,
     oauth_expiry
 ) VALUES (
     $1,
@@ -23,16 +23,16 @@ INSERT INTO git_auth_links (
     $5,
     $6,
     $7,
-	$8,
-	$9
+    $8,
+    $9
 ) RETURNING *;
 
 -- name: UpdateGitAuthLink :one
 UPDATE git_auth_links SET
     updated_at = $3,
     oauth_access_token = $4,
-	oauth_access_token_key_id = $5,
+    oauth_access_token_key_id = $5,
     oauth_refresh_token = $6,
-	oauth_refresh_token_key_id = $7,
+    oauth_refresh_token_key_id = $7,
     oauth_expiry = $8
 WHERE provider_id = $1 AND user_id = $2 RETURNING *;
