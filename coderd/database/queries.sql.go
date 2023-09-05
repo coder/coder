f340cbab8cec717bbf81d104d82176507adee7b9
Cian Johnston <cian@coder.com>
2023-09-05 10:11:13 +0100

    fixup! fix indentation

--- coderd/database/queries.sql.go
+++ coderd/database/queries.sql.go
@@ -937,9 +937,9 @@ INSERT INTO git_auth_links (
     created_at,
     updated_at,
     oauth_access_token,
-	oauth_access_token_key_id,
+    oauth_access_token_key_id,
     oauth_refresh_token,
-	oauth_refresh_token_key_id,
+    oauth_refresh_token_key_id,
     oauth_expiry
 ) VALUES (
     $1,
@@ -949,8 +949,8 @@ INSERT INTO git_auth_links (
     $5,
     $6,
     $7,
-	$8,
-	$9
+    $8,
+    $9
 ) RETURNING provider_id, user_id, created_at, updated_at, oauth_access_token, oauth_refresh_token, oauth_expiry, oauth_access_token_key_id, oauth_refresh_token_key_id
 `
 
@@ -997,9 +997,9 @@ const updateGitAuthLink = `-- name: UpdateGitAuthLink :one
 UPDATE git_auth_links SET
     updated_at = $3,
     oauth_access_token = $4,
-	oauth_access_token_key_id = $5,
+    oauth_access_token_key_id = $5,
     oauth_refresh_token = $6,
-	oauth_refresh_token_key_id = $7,
+    oauth_refresh_token_key_id = $7,
     oauth_expiry = $8
 WHERE provider_id = $1 AND user_id = $2 RETURNING provider_id, user_id, created_at, updated_at, oauth_access_token, oauth_refresh_token, oauth_expiry, oauth_access_token_key_id, oauth_refresh_token_key_id
 `
