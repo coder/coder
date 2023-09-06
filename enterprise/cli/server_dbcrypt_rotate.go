e69e3ef6be75c1ed2bb2b37b6c7b571d1afb1bae
Cian Johnston <cian@coder.com>
2023-09-06 17:33:55 +0100

    add decrypt/delete commands

--- enterprise/cli/server_dbcrypt_rotate.go
+++ enterprise/cli/server_dbcrypt_rotate.go
@@ -11,6 +11,7 @@ import (
 	"cdr.dev/slog/sloggers/sloghuman"
 	"github.com/coder/coder/v2/cli"
 	"github.com/coder/coder/v2/cli/clibase"
+	"github.com/coder/coder/v2/cli/cliui"
 	"github.com/coder/coder/v2/codersdk"
 	"github.com/coder/coder/v2/enterprise/dbcrypt"
 
@@ -26,6 +27,8 @@ func (r *RootCmd) dbcryptCmd() *clibase.Cmd {
 		},
 	}
 	dbcryptCmd.AddSubcommands(
+		r.dbcryptDecryptCmd(),
+		r.dbcryptDeleteCmd(),
 		r.dbcryptRotateCmd(),
 	)
 	return dbcryptCmd
@@ -95,6 +98,129 @@ func (*RootCmd) dbcryptRotateCmd() *clibase.Cmd {
 			logger.Info(ctx, "connected to postgres")
 			if err := dbcrypt.Rotate(ctx, logger, sqlDB, ciphers); err != nil {
 				return xerrors.Errorf("rotate ciphers: %w", err)
+			}
+			logger.Info(ctx, "operation completed successfully")
+			return nil
+		},
+	}
+	return cmd
+}
+
+func (*RootCmd) dbcryptDecryptCmd() *clibase.Cmd {
+	var (
+		vals = new(codersdk.DeploymentValues)
+		opts = vals.Options()
+	)
+	cmd := &clibase.Cmd{
+		Use:   "decrypt",
+		Short: "Decrypt a previously encrypted database.",
+		Options: clibase.OptionSet{
+			*opts.ByName("Postgres Connection URL"),
+			*opts.ByName("External Token Encryption Keys"),
+		},
+		Middleware: clibase.Chain(
+			clibase.RequireNArgs(0),
+		),
+		Handler: func(inv *clibase.Invocation) error {
+			ctx, cancel := context.WithCancel(inv.Context())
+			defer cancel()
+			logger := slog.Make(sloghuman.Sink(inv.Stdout))
+			if ok, _ := inv.ParsedFlags().GetBool("verbose"); ok {
+				logger = logger.Leveled(slog.LevelDebug)
+			}
+
+			if vals.PostgresURL == "" {
+				return xerrors.Errorf("no database configured")
+			}
+
+			switch len(vals.ExternalTokenEncryptionKeys) {
+			case 0:
+				return xerrors.Errorf("no external token encryption keys provided")
+			case 1:
+				logger.Info(ctx, "only one key provided, data will be re-encrypted with the same key")
+			}
+
+			keys := make([][]byte, 0, len(vals.ExternalTokenEncryptionKeys))
+			var newKey []byte
+			for idx, ek := range vals.ExternalTokenEncryptionKeys {
+				dk, err := base64.StdEncoding.DecodeString(ek)
+				if err != nil {
+					return xerrors.Errorf("key must be base64-encoded")
+				}
+				if idx == 0 {
+					newKey = dk
+				} else if bytes.Equal(dk, newKey) {
+					return xerrors.Errorf("old key at index %d is the same as the new key", idx)
+				}
+				keys = append(keys, dk)
+			}
+
+			ciphers, err := dbcrypt.NewCiphers(keys...)
+			if err != nil {
+				return xerrors.Errorf("create ciphers: %w", err)
+			}
+
+			sqlDB, err := cli.ConnectToPostgres(inv.Context(), logger, "postgres", vals.PostgresURL.Value())
+			if err != nil {
+				return xerrors.Errorf("connect to postgres: %w", err)
+			}
+			defer func() {
+				_ = sqlDB.Close()
+			}()
+			logger.Info(ctx, "connected to postgres")
+			if err := dbcrypt.Decrypt(ctx, logger, sqlDB, ciphers); err != nil {
+				return xerrors.Errorf("rotate ciphers: %w", err)
+			}
+			logger.Info(ctx, "operation completed successfully")
+			return nil
+		},
+	}
+	return cmd
+}
+
+func (*RootCmd) dbcryptDeleteCmd() *clibase.Cmd {
+	var (
+		vals = new(codersdk.DeploymentValues)
+		opts = vals.Options()
+	)
+	cmd := &clibase.Cmd{
+		Use:   "delete",
+		Short: "Delete all encrypted data from the database. THIS IS A DESTRUCTIVE OPERATION.",
+		Options: clibase.OptionSet{
+			*opts.ByName("Postgres Connection URL"),
+		},
+		Middleware: clibase.Chain(
+			clibase.RequireNArgs(0),
+		),
+		Handler: func(inv *clibase.Invocation) error {
+			ctx, cancel := context.WithCancel(inv.Context())
+			defer cancel()
+			logger := slog.Make(sloghuman.Sink(inv.Stdout))
+			if ok, _ := inv.ParsedFlags().GetBool("verbose"); ok {
+				logger = logger.Leveled(slog.LevelDebug)
+			}
+
+			if vals.PostgresURL == "" {
+				return xerrors.Errorf("no database configured")
+			}
+
+			if _, err := cliui.Prompt(inv, cliui.PromptOptions{
+				Text:      "This will delete all encrypted data from the database. Are you sure you want to continue?",
+				IsConfirm: true,
+			}); err != nil {
+				return err
+			}
+
+			sqlDB, err := cli.ConnectToPostgres(inv.Context(), logger, "postgres", vals.PostgresURL.Value())
+			if err != nil {
+				return xerrors.Errorf("connect to postgres: %w", err)
+			}
+			defer func() {
+				_ = sqlDB.Close()
+			}()
+			logger.Info(ctx, "connected to postgres")
+			if err := dbcrypt.Delete(ctx, logger, sqlDB); err != nil {
+				return xerrors.Errorf("delete encrypted data: %w", err)
 			}
 			logger.Info(ctx, "operation completed successfully")
 			return nil
