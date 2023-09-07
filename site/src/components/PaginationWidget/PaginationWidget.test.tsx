899b08e12a6699ea7460c3e868b8206cd3d215da
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2023-09-07 13:23:40 +0000

    Refactor PageButton tests

--- site/src/components/PaginationWidget/PaginationWidget.test.tsx
+++ site/src/components/PaginationWidget/PaginationWidget.test.tsx
@@ -1,55 +1,9 @@
 import { screen } from "@testing-library/react";
-import { render } from "../../testHelpers/renderHelpers";
+import { render } from "testHelpers/renderHelpers";
 import { PaginationWidget } from "./PaginationWidget";
 import { createPaginationRef } from "./utils";
 
 describe("PaginatedList", () => {
-  it("displays an accessible previous and next button", () => {
-    render(
-      <PaginationWidget
-        prevLabel="Previous"
-        nextLabel="Next"
-        paginationRef={createPaginationRef({ page: 2, limit: 12 })}
-        numRecords={200}
-      />,
-    );
-
-    expect(screen.getByRole("button", { name: "Previous page" })).toBeEnabled();
-    expect(screen.getByRole("button", { name: "Next page" })).toBeEnabled();
-  });
-
-  it("displays the expected number of pages with one ellipsis tile", () => {
-    const { container } = render(
-      <PaginationWidget
-        prevLabel="Previous"
-        nextLabel="Next"
-        numRecords={200}
-        paginationRef={createPaginationRef({ page: 1, limit: 12 })}
-      />,
-    );
-
-    // 7 total spaces. 6 are page numbers, one is ellipsis
-    expect(
-      container.querySelectorAll(`button[name="Page button"]`),
-    ).toHaveLength(6);
-  });
-
-  it("displays the expected number of pages with two ellipsis tiles", () => {
-    const { container } = render(
-      <PaginationWidget
-        prevLabel="Previous"
-        nextLabel="Next"
-        numRecords={200}
-        paginationRef={createPaginationRef({ page: 6, limit: 12 })}
-      />,
-    );
-
-    // 7 total spaces. 2 sets of ellipsis on either side of the active page
-    expect(
-      container.querySelectorAll(`button[name="Page button"]`),
-    ).toHaveLength(5);
-  });
-
   it("disables the previous button on the first page", () => {
     render(
       <PaginationWidget
