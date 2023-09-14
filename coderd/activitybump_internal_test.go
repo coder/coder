eecfcdb2701e44a98954fdb221719aa199ae6edb
Cian Johnston <cian@coder.com>
2023-09-14 12:46:15 +0100

    test activity bump in multiple timezones

--- coderd/activitybump_internal_test.go
+++ coderd/activitybump_internal_test.go
@@ -22,6 +22,15 @@ import (
 func Test_ActivityBumpWorkspace(t *testing.T) {
 	t.Parallel()
 
+	// We test the below in multiple timezones
+	timezones := []string{
+		"Asia/Kolkata",
+		"Canada/Newfoundland",
+		"Europe/Paris",
+		"US/Central",
+		"UTC",
+	}
+
 	for _, tt := range []struct {
 		name                string
 		transition          database.WorkspaceTransition
@@ -92,117 +101,120 @@ func Test_ActivityBumpWorkspace(t *testing.T) {
 		},
 	} {
 		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
+		for _, tz := range timezones {
+			tz := tz
+			t.Run(tt.name+"/"+tz, func(t *testing.T) {
+				t.Parallel()
 
-			var (
-				now   = dbtime.Now()
-				ctx   = testutil.Context(t, testutil.WaitShort)
-				log   = slogtest.Make(t, nil)
-				db, _ = dbtestutil.NewDB(t)
-				org   = dbgen.Organization(t, db, database.Organization{})
-				user  = dbgen.User(t, db, database.User{
-					Status: database.UserStatusActive,
-				})
-				_ = dbgen.OrganizationMember(t, db, database.OrganizationMember{
-					UserID:         user.ID,
-					OrganizationID: org.ID,
-				})
-				templateVersion = dbgen.TemplateVersion(t, db, database.TemplateVersion{
-					OrganizationID: org.ID,
-					CreatedBy:      user.ID,
-				})
-				template = dbgen.Template(t, db, database.Template{
-					OrganizationID:  org.ID,
-					ActiveVersionID: templateVersion.ID,
-					CreatedBy:       user.ID,
-				})
-				ws = dbgen.Workspace(t, db, database.Workspace{
-					OwnerID:        user.ID,
-					OrganizationID: org.ID,
-					TemplateID:     template.ID,
-					Ttl:            sql.NullInt64{Valid: true, Int64: int64(tt.workspaceTTL)},
-				})
-				job = dbgen.ProvisionerJob(t, db, database.ProvisionerJob{
-					OrganizationID: org.ID,
-					CompletedAt:    tt.jobCompletedAt,
-				})
-				_ = dbgen.WorkspaceResource(t, db, database.WorkspaceResource{
-					JobID: job.ID,
-				})
-				buildID = uuid.New()
-			)
+				var (
+					now   = dbtime.Now()
+					ctx   = testutil.Context(t, testutil.WaitShort)
+					log   = slogtest.Make(t, nil)
+					db, _ = dbtestutil.NewDB(t, dbtestutil.WithTimezone(tz))
+					org   = dbgen.Organization(t, db, database.Organization{})
+					user  = dbgen.User(t, db, database.User{
+						Status: database.UserStatusActive,
+					})
+					_ = dbgen.OrganizationMember(t, db, database.OrganizationMember{
+						UserID:         user.ID,
+						OrganizationID: org.ID,
+					})
+					templateVersion = dbgen.TemplateVersion(t, db, database.TemplateVersion{
+						OrganizationID: org.ID,
+						CreatedBy:      user.ID,
+					})
+					template = dbgen.Template(t, db, database.Template{
+						OrganizationID:  org.ID,
+						ActiveVersionID: templateVersion.ID,
+						CreatedBy:       user.ID,
+					})
+					ws = dbgen.Workspace(t, db, database.Workspace{
+						OwnerID:        user.ID,
+						OrganizationID: org.ID,
+						TemplateID:     template.ID,
+						Ttl:            sql.NullInt64{Valid: true, Int64: int64(tt.workspaceTTL)},
+					})
+					job = dbgen.ProvisionerJob(t, db, database.ProvisionerJob{
+						OrganizationID: org.ID,
+						CompletedAt:    tt.jobCompletedAt,
+					})
+					_ = dbgen.WorkspaceResource(t, db, database.WorkspaceResource{
+						JobID: job.ID,
+					})
+					buildID = uuid.New()
+				)
 
-			var buildNumber int32 = 1
-			// Insert a number of previous workspace builds.
-			for i := 0; i < 5; i++ {
-				insertPrevWorkspaceBuild(t, db, org.ID, templateVersion.ID, ws.ID, database.WorkspaceTransitionStart, buildNumber)
-				buildNumber++
-				insertPrevWorkspaceBuild(t, db, org.ID, templateVersion.ID, ws.ID, database.WorkspaceTransitionStop, buildNumber)
-				buildNumber++
-			}
+				var buildNumber int32 = 1
+				// Insert a number of previous workspace builds.
+				for i := 0; i < 5; i++ {
+					insertPrevWorkspaceBuild(t, db, org.ID, templateVersion.ID, ws.ID, database.WorkspaceTransitionStart, buildNumber)
+					buildNumber++
+					insertPrevWorkspaceBuild(t, db, org.ID, templateVersion.ID, ws.ID, database.WorkspaceTransitionStop, buildNumber)
+					buildNumber++
+				}
 
-			// dbgen.WorkspaceBuild automatically sets deadline to now+1 hour if not set
-			var buildDeadline time.Time
-			if tt.buildDeadlineOffset != nil {
-				buildDeadline = now.Add(*tt.buildDeadlineOffset)
-			}
-			var maxDeadline time.Time
-			if tt.maxDeadlineOffset != nil {
-				maxDeadline = now.Add(*tt.maxDeadlineOffset)
-			}
-			err := db.InsertWorkspaceBuild(ctx, database.InsertWorkspaceBuildParams{
-				ID:                buildID,
-				CreatedAt:         dbtime.Now(),
-				UpdatedAt:         dbtime.Now(),
-				BuildNumber:       buildNumber,
-				InitiatorID:       user.ID,
-				Reason:            database.BuildReasonInitiator,
-				WorkspaceID:       ws.ID,
-				JobID:             job.ID,
-				TemplateVersionID: templateVersion.ID,
-				Transition:        tt.transition,
-				Deadline:          buildDeadline,
-				MaxDeadline:       maxDeadline,
+				// dbgen.WorkspaceBuild automatically sets deadline to now+1 hour if not set
+				var buildDeadline time.Time
+				if tt.buildDeadlineOffset != nil {
+					buildDeadline = now.Add(*tt.buildDeadlineOffset)
+				}
+				var maxDeadline time.Time
+				if tt.maxDeadlineOffset != nil {
+					maxDeadline = now.Add(*tt.maxDeadlineOffset)
+				}
+				err := db.InsertWorkspaceBuild(ctx, database.InsertWorkspaceBuildParams{
+					ID:                buildID,
+					CreatedAt:         dbtime.Now(),
+					UpdatedAt:         dbtime.Now(),
+					BuildNumber:       buildNumber,
+					InitiatorID:       user.ID,
+					Reason:            database.BuildReasonInitiator,
+					WorkspaceID:       ws.ID,
+					JobID:             job.ID,
+					TemplateVersionID: templateVersion.ID,
+					Transition:        tt.transition,
+					Deadline:          buildDeadline,
+					MaxDeadline:       maxDeadline,
+				})
+				require.NoError(t, err, "unexpected error inserting workspace build")
+				bld, err := db.GetWorkspaceBuildByID(ctx, buildID)
+				require.NoError(t, err, "unexpected error fetching inserted workspace build")
+
+				// Validate our initial state before bump
+				require.Equal(t, tt.transition, bld.Transition, "unexpected transition before bump")
+				require.Equal(t, tt.jobCompletedAt.Time.UTC(), job.CompletedAt.Time.UTC(), "unexpected job completed at before bump")
+				require.Equal(t, buildDeadline.UTC(), bld.Deadline.UTC(), "unexpected build deadline before bump")
+				require.Equal(t, maxDeadline.UTC(), bld.MaxDeadline.UTC(), "unexpected max deadline before bump")
+				require.Equal(t, tt.workspaceTTL, time.Duration(ws.Ttl.Int64), "unexpected workspace TTL before bump")
+
+				workaroundWindowsTimeResolution(t)
+
+				// Bump duration is measured from the time of the bump, so we measure from here.
+				start := dbtime.Now()
+				activityBumpWorkspace(ctx, log, db, bld.WorkspaceID)
+				elapsed := time.Since(start)
+				if elapsed > 15*time.Second {
+					t.Logf("warning: activityBumpWorkspace took longer than 15 seconds: %s", elapsed)
+				}
+				// The actual bump could have happened anywhere in the elapsed time, so we
+				// guess at the approximate time of the bump.
+				approxBumpTime := start.Add(elapsed / 2)
+
+				// Validate our state after bump
+				updatedBuild, err := db.GetLatestWorkspaceBuildByWorkspaceID(ctx, bld.WorkspaceID)
+				require.NoError(t, err, "unexpected error getting latest workspace build")
+				if tt.expectedBump == 0 {
+					require.Equal(t, bld.UpdatedAt.UTC(), updatedBuild.UpdatedAt.UTC(), "should not have bumped updated_at")
+					require.Equal(t, bld.Deadline.UTC(), updatedBuild.Deadline.UTC(), "should not have bumped deadline")
+				} else {
+					require.NotEqual(t, bld.UpdatedAt.UTC(), updatedBuild.UpdatedAt.UTC(), "should have bumped updated_at")
+					expectedDeadline := approxBumpTime.Add(tt.expectedBump).UTC()
+					// Note: if CI is especially slow, this test may fail. There is an internal 15-second
+					// deadline in activityBumpWorkspace, so we allow the same window here.
+					require.WithinDuration(t, expectedDeadline, updatedBuild.Deadline.UTC(), 15*time.Second, "unexpected deadline after bump")
+				}
 			})
-			require.NoError(t, err, "unexpected error inserting workspace build")
-			bld, err := db.GetWorkspaceBuildByID(ctx, buildID)
-			require.NoError(t, err, "unexpected error fetching inserted workspace build")
-
-			// Validate our initial state before bump
-			require.Equal(t, tt.transition, bld.Transition, "unexpected transition before bump")
-			require.Equal(t, tt.jobCompletedAt.Time.UTC(), job.CompletedAt.Time.UTC(), "unexpected job completed at before bump")
-			require.Equal(t, buildDeadline.UTC(), bld.Deadline.UTC(), "unexpected build deadline before bump")
-			require.Equal(t, maxDeadline.UTC(), bld.MaxDeadline.UTC(), "unexpected max deadline before bump")
-			require.Equal(t, tt.workspaceTTL, time.Duration(ws.Ttl.Int64), "unexpected workspace TTL before bump")
-
-			workaroundWindowsTimeResolution(t)
-
-			// Bump duration is measured from the time of the bump, so we measure from here.
-			start := dbtime.Now()
-			activityBumpWorkspace(ctx, log, db, bld.WorkspaceID)
-			elapsed := time.Since(start)
-			if elapsed > 15*time.Second {
-				t.Logf("warning: activityBumpWorkspace took longer than 15 seconds: %s", elapsed)
-			}
-			// The actual bump could have happened anywhere in the elapsed time, so we
-			// guess at the approximate time of the bump.
-			approxBumpTime := start.Add(elapsed / 2)
-
-			// Validate our state after bump
-			updatedBuild, err := db.GetLatestWorkspaceBuildByWorkspaceID(ctx, bld.WorkspaceID)
-			require.NoError(t, err, "unexpected error getting latest workspace build")
-			if tt.expectedBump == 0 {
-				require.Equal(t, bld.UpdatedAt.UTC(), updatedBuild.UpdatedAt.UTC(), "should not have bumped updated_at")
-				require.Equal(t, bld.Deadline.UTC(), updatedBuild.Deadline.UTC(), "should not have bumped deadline")
-			} else {
-				require.NotEqual(t, bld.UpdatedAt.UTC(), updatedBuild.UpdatedAt.UTC(), "should have bumped updated_at")
-				expectedDeadline := approxBumpTime.Add(tt.expectedBump).UTC()
-				// Note: if CI is especially slow, this test may fail. There is an internal 15-second
-				// deadline in activityBumpWorkspace, so we allow the same window here.
-				require.WithinDuration(t, expectedDeadline, updatedBuild.Deadline.UTC(), 15*time.Second, "unexpected deadline after bump")
-			}
-		})
+		}
 	}
 }
 
