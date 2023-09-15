7466c8b7b841be7e984858e6f5d9121ca23cd071
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2023-09-15 13:28:34 +0000

    Remove edit group service

--- site/src/api/queries/groups.ts
+++ site/src/api/queries/groups.ts
@@ -1,11 +1,22 @@
 import { QueryClient } from "@tanstack/react-query";
 import * as API from "api/api";
-import { CreateGroupRequest } from "api/typesGenerated";
+import {
+  CreateGroupRequest,
+  Group,
+  PatchGroupRequest,
+} from "api/typesGenerated";
 
 export const groups = (organizationId: string) => {
   return {
     queryKey: ["groups"],
     queryFn: () => API.getGroups(organizationId),
+  };
+};
+
+export const group = (groupId: string) => {
+  return {
+    queryKey: ["group", groupId],
+    queryFn: () => API.getGroup(groupId),
   };
 };
 
@@ -18,6 +29,22 @@ export const createGroup = (queryClient: QueryClient) => {
       API.createGroup(organizationId, request),
     onSuccess: async () => {
       await queryClient.invalidateQueries(["groups"]);
+    },
+  };
+};
+
+export const patchGroup = (queryClient: QueryClient) => {
+  return {
+    mutationFn: ({
+      groupId,
+      ...request
+    }: PatchGroupRequest & { groupId: string }) =>
+      API.patchGroup(groupId, request),
+    onSuccess: async (updatedGroup: Group) => {
+      await Promise.all([
+        queryClient.invalidateQueries(["groups"]),
+        queryClient.invalidateQueries(["group", updatedGroup.id]),
+      ]);
     },
   };
 };
