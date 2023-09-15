6b93fcc795ea56f01441a47d9c6cad6c444a723a
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2023-09-15 13:14:02 +0000

    Remove create group service

--- site/src/api/queries/groups.ts
+++ site/src/api/queries/groups.ts
@@ -1,8 +1,23 @@
+import { QueryClient } from "@tanstack/react-query";
 import * as API from "api/api";
+import { CreateGroupRequest } from "api/typesGenerated";
 
 export const groups = (organizationId: string) => {
   return {
     queryKey: ["groups"],
     queryFn: () => API.getGroups(organizationId),
+  };
+};
+
+export const createGroup = (queryClient: QueryClient) => {
+  return {
+    mutationFn: ({
+      organizationId,
+      ...request
+    }: CreateGroupRequest & { organizationId: string }) =>
+      API.createGroup(organizationId, request),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries(["groups"]);
+    },
   };
 };
