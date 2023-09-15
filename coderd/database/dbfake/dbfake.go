0c11d6c52767b9c1d579b5988855e553cef8c53f
Cian Johnston <cian@coder.com>
2023-09-15 14:48:07 +0100

    fixup! make AllUserIDs a fully-fledged query citizen

--- coderd/database/dbfake/dbfake.go
+++ coderd/database/dbfake/dbfake.go
@@ -812,7 +812,7 @@ func (q *FakeQuerier) ActivityBumpWorkspace(ctx context.Context, workspaceID uui
 	return sql.ErrNoRows
 }
 
-func (q *FakeQuerier) AllUserIDs(ctx context.Context) ([]uuid.UUID, error) {
+func (q *FakeQuerier) AllUserIDs(_ context.Context) ([]uuid.UUID, error) {
 	userIDs := make([]uuid.UUID, 0, len(q.users))
 	for idx := range q.users {
 		userIDs[idx] = q.users[idx].ID
