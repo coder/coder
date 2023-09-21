801c6c994b785cd659fefaea5f548f9511dac0ad
Bruno Quaresma <bruno@coder.com>
2023-09-21 11:21:49 -0300

    chore(site): replace setup machine by react-query (#9809)

--- site/src/api/queries/users.ts
+++ site/src/api/queries/users.ts
@@ -10,3 +10,9 @@ export const updatePassword = () => {
       API.updateUserPassword(userId, request),
   };
 };
+
+export const createFirstUser = () => {
+  return {
+    mutationFn: API.createFirstUser,
+  };
+};
