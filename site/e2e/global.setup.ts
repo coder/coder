30300c38a03b0733015ec1e10836adc8c2b0d198
Ammar Bandukwala <ammar@ammar.io>
2023-09-23 21:40:24 -0500

    ci: test Helm chart against various Kubernetes clusters

--- site/e2e/global.setup.ts
+++ site/e2e/global.setup.ts
@@ -1,16 +1,27 @@
 import { test, expect } from "@playwright/test";
 import * as constants from "./constants";
 import { STORAGE_STATE } from "./playwright.config";
-import { Language } from "../src/pages/CreateUserPage/CreateUserForm";
+import { Language as CreateLanguage } from "../src/pages/CreateUserPage/CreateUserForm";
+import { Language as LoginLanguage } from "../src/pages/LoginPage/SignInForm";
 
-test("create first user", async ({ page }) => {
+test("setup or sign in", async ({ page }) => {
   await page.goto("/", { waitUntil: "domcontentloaded" });
 
-  await page.getByLabel(Language.usernameLabel).fill(constants.username);
-  await page.getByLabel(Language.emailLabel).fill(constants.email);
-  await page.getByLabel(Language.passwordLabel).fill(constants.password);
-  await page.getByTestId("trial").click();
-  await page.getByTestId("create").click();
+  await page.getByLabel(CreateLanguage.emailLabel).fill(constants.email);
+  await page.getByLabel(CreateLanguage.passwordLabel).fill(constants.password);
+
+  const signInButton = page.getByRole("button", {
+    name: LoginLanguage.passwordSignIn,
+  });
+  if (await signInButton.isVisible()) {
+    await signInButton.click();
+  } else {
+    await page
+      .getByLabel(CreateLanguage.usernameLabel)
+      .fill(constants.username);
+    await page.getByTestId("trial").click();
+    await page.getByTestId("create").click();
+  }
 
   await expect(page).toHaveURL(/\/workspaces.*/);
   await page.context().storageState({ path: STORAGE_STATE });
--- site/e2e/playwright.config.ts
+++ site/e2e/playwright.config.ts
@@ -6,12 +6,20 @@ export const port = process.env.CODER_E2E_PORT
   ? Number(process.env.CODER_E2E_PORT)
   : defaultPort;
 
+export const externalURL = process.env.CODER_E2E_BASE_URL;
+
 const coderMain = path.join(__dirname, "../../enterprise/cmd/coder/main.go");
 
 export const STORAGE_STATE = path.join(__dirname, ".auth.json");
 
 const localURL = (port: number, path: string): string => {
   return `http://localhost:${port}${path}`;
+};
+
+// cleanURL removes the trailing slash from the end of a URL if it exists.
+// It also removes leading and trailing whitespace.
+const cleanURL = (url: string): string => {
+  return url.trim().replace(/\/$/, "");
 };
 
 export default defineConfig({
@@ -32,66 +40,78 @@ export default defineConfig({
   ],
   reporter: [["./reporter.ts"]],
   use: {
-    baseURL: `http://localhost:${port}`,
+    baseURL:
+      externalURL !== undefined
+        ? cleanURL(externalURL)
+        : `http://localhost:${port}`,
     video: "retain-on-failure",
     launchOptions: {
       args: ["--disable-webgl"],
     },
   },
-  webServer: {
-    url: `http://localhost:${port}/api/v2/deployment/config`,
-    command:
-      `go run -tags embed ${coderMain} server ` +
-      `--global-config $(mktemp -d -t e2e-XXXXXXXXXX) ` +
-      `--access-url=http://localhost:${port} ` +
-      `--http-address=localhost:${port} ` +
-      `--in-memory --telemetry=false ` +
-      `--dangerous-disable-rate-limits ` +
-      `--provisioner-daemons 10 ` +
-      `--provisioner-daemons-echo ` +
-      `--pprof-enable`,
-    env: {
-      ...process.env,
+  webServer:
+    externalURL !== undefined
+      ? undefined
+      : {
+          url: `http://localhost:${port}/api/v2/deployment/config`,
+          command:
+            `go run -tags embed ${coderMain} server ` +
+            `--global-config $(mktemp -d -t e2e-XXXXXXXXXX) ` +
+            `--access-url=http://localhost:${port} ` +
+            `--http-address=localhost:${port} ` +
+            `--in-memory --telemetry=false ` +
+            `--dangerous-disable-rate-limits ` +
+            `--provisioner-daemons 10 ` +
+            `--provisioner-daemons-echo ` +
+            `--pprof-enable`,
+          env: {
+            ...process.env,
 
-      // This is the test provider for git auth with devices!
-      CODER_GITAUTH_0_ID: gitAuth.deviceProvider,
-      CODER_GITAUTH_0_TYPE: "github",
-      CODER_GITAUTH_0_CLIENT_ID: "client",
-      CODER_GITAUTH_0_DEVICE_FLOW: "true",
-      CODER_GITAUTH_0_APP_INSTALL_URL:
-        "https://github.com/apps/coder/installations/new",
-      CODER_GITAUTH_0_APP_INSTALLATIONS_URL: localURL(
-        gitAuth.devicePort,
-        gitAuth.installationsPath,
-      ),
-      CODER_GITAUTH_0_TOKEN_URL: localURL(
-        gitAuth.devicePort,
-        gitAuth.tokenPath,
-      ),
-      CODER_GITAUTH_0_DEVICE_CODE_URL: localURL(
-        gitAuth.devicePort,
-        gitAuth.codePath,
-      ),
-      CODER_GITAUTH_0_VALIDATE_URL: localURL(
-        gitAuth.devicePort,
-        gitAuth.validatePath,
-      ),
+            // This is the test provider for git auth with devices!
+            CODER_GITAUTH_0_ID: gitAuth.deviceProvider,
+            CODER_GITAUTH_0_TYPE: "github",
+            CODER_GITAUTH_0_CLIENT_ID: "client",
+            CODER_GITAUTH_0_DEVICE_FLOW: "true",
+            CODER_GITAUTH_0_APP_INSTALL_URL:
+              "https://github.com/apps/coder/installations/new",
+            CODER_GITAUTH_0_APP_INSTALLATIONS_URL: localURL(
+              gitAuth.devicePort,
+              gitAuth.installationsPath,
+            ),
+            CODER_GITAUTH_0_TOKEN_URL: localURL(
+              gitAuth.devicePort,
+              gitAuth.tokenPath,
+            ),
+            CODER_GITAUTH_0_DEVICE_CODE_URL: localURL(
+              gitAuth.devicePort,
+              gitAuth.codePath,
+            ),
+            CODER_GITAUTH_0_VALIDATE_URL: localURL(
+              gitAuth.devicePort,
+              gitAuth.validatePath,
+            ),
 
-      CODER_GITAUTH_1_ID: gitAuth.webProvider,
-      CODER_GITAUTH_1_TYPE: "github",
-      CODER_GITAUTH_1_CLIENT_ID: "client",
-      CODER_GITAUTH_1_CLIENT_SECRET: "secret",
-      CODER_GITAUTH_1_AUTH_URL: localURL(gitAuth.webPort, gitAuth.authPath),
-      CODER_GITAUTH_1_TOKEN_URL: localURL(gitAuth.webPort, gitAuth.tokenPath),
-      CODER_GITAUTH_1_DEVICE_CODE_URL: localURL(
-        gitAuth.webPort,
-        gitAuth.codePath,
-      ),
-      CODER_GITAUTH_1_VALIDATE_URL: localURL(
-        gitAuth.webPort,
-        gitAuth.validatePath,
-      ),
-    },
-    reuseExistingServer: false,
-  },
+            CODER_GITAUTH_1_ID: gitAuth.webProvider,
+            CODER_GITAUTH_1_TYPE: "github",
+            CODER_GITAUTH_1_CLIENT_ID: "client",
+            CODER_GITAUTH_1_CLIENT_SECRET: "secret",
+            CODER_GITAUTH_1_AUTH_URL: localURL(
+              gitAuth.webPort,
+              gitAuth.authPath,
+            ),
+            CODER_GITAUTH_1_TOKEN_URL: localURL(
+              gitAuth.webPort,
+              gitAuth.tokenPath,
+            ),
+            CODER_GITAUTH_1_DEVICE_CODE_URL: localURL(
+              gitAuth.webPort,
+              gitAuth.codePath,
+            ),
+            CODER_GITAUTH_1_VALIDATE_URL: localURL(
+              gitAuth.webPort,
+              gitAuth.validatePath,
+            ),
+          },
+          reuseExistingServer: false,
+        },
 });
