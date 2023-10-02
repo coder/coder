1a07ee0b16b526f7e6560b5a7b6c98f43ac037d9
Muhammad Atif Ali <atif@coder.com>
2023-10-02 13:57:21 +0300

    chore(site): make name bold in delete dialog
    This makes the name bold in delete dialog and makes it readable.
    <img width="439" alt="Screenshot 2023-09-24 at 03 30 55" src="https://github.com/coder/coder/assets/10648092/b97e634b-c7c8-4300-b78c-8091b3f2c9f0">
    
    Edit: Tests are passed. thanks @aslilac

--- site/src/pages/UsersPage/UsersPage.test.tsx
+++ site/src/pages/UsersPage/UsersPage.test.tsx
@@ -66,9 +66,7 @@ const deleteUser = async (setupActionSpies: () => void) => {
 
   // Check if the confirm message is displayed
   const confirmDialog = await screen.findByRole("dialog");
-  expect(confirmDialog).toHaveTextContent(
-    `Type ${MockUser2.username} below to confirm.`,
-  );
+  expect(confirmDialog).toHaveTextContent(`Are you sure you want to proceed?`);
 
   // Confirm with text input
   const textField = screen.getByLabelText("Name of the user to delete");
