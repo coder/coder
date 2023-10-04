1f50431a24cee45705a4891e4461de28146f6724
Kyle Carberry <kyle@carberry.com>
2023-10-04 18:23:51 +0000

    Add subcommands

--- cli/externalauth.go
+++ cli/externalauth.go
@@ -3,5 +3,33 @@ package cli
 import "github.com/coder/coder/v2/cli/clibase"
 
 func (r *RootCmd) externalAuth() *clibase.Cmd {
-	return &clibase.Cmd{}
+	return &clibase.Cmd{
+		Use:   "external-auth",
+		Short: "Manage external authentication",
+		Long:  "Authenticate with external services inside of a workspace.",
+		Handler: func(i *clibase.Invocation) error {
+			return i.Command.HelpHandler(i)
+		},
+	}
+}
+
+func (r *RootCmd) externalAuthList() *clibase.Cmd {
+	return &clibase.Cmd{
+		Use:   "list",
+		Short: "List external authentication providers",
+		Long:  "List external authentication.",
+		Handler: func(i *clibase.Invocation) error {
+			return i.Command.HelpHandler(i)
+		},
+	}
+}
+
+func (r *RootCmd) externalAuthAccessToken() *clibase.Cmd {
+	return &clibase.Cmd{
+		Use:   "access-token",
+		Short: "Print auth for an external provider",
+		Long: "Print an access-token for an external auth provider. " +
+			"If the user has a refresh-token, the access-token automatically refresh the access-token. " +
+			"If the user has not authenticated before, ",
+	}
 }
