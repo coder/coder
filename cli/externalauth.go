df8ac5d74b1a0f5088793194e63945232009bb94
Kyle Carberry <kyle@carberry.com>
2023-10-04 19:42:39 +0000

    Improve descriptions

--- cli/externalauth.go
+++ cli/externalauth.go
@@ -29,7 +29,29 @@ func (r *RootCmd) externalAuthAccessToken() *clibase.Cmd {
 		Use:   "access-token",
 		Short: "Print auth for an external provider",
 		Long: "Print an access-token for an external auth provider. " +
-			"If the user has a refresh-token, the access-token automatically refresh the access-token. " +
-			"If the user has not authenticated before, ",
+			"The access-token will be validated and sent to stdout with exit code 0. " +
+			"If a valid access-token cannot be obtained, the URL to authenticate will be sent to stderr with exit code 1\n" + formatExamples(
+			example{
+				Description: "Ensure that the user is authenticated with GitHub before cloning.",
+				Command: `#!/usr/bin/env sh
+
+if coder external-auth access-token github ; then
+  echo "Authenticated with GitHub"
+else
+  echo "Please authenticate with GitHub:"
+  coder external-auth url github
+fi
+`,
+			},
+		),
+		Options: clibase.OptionSet{{
+			Name:        "Silent",
+			Flag:        "s",
+			Description: "Do not print the URL or access token.",
+		}},
+
+		Handler: func(i *clibase.Invocation) error {
+			return nil
+		},
 	}
 }
