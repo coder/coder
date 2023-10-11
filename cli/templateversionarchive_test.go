10001679102d85b9142e31d271d36a491a2758b9
Steven Masley <stevenmasley@coder.com>
2023-10-11 08:42:31 -0500

    Import order

--- cli/templateversionarchive_test.go
+++ cli/templateversionarchive_test.go
@@ -4,18 +4,14 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
-
-	"github.com/coder/coder/v2/provisioner/echo"
-
-	"github.com/coder/coder/v2/codersdk"
-
-	"github.com/coder/coder/v2/testutil"
-
-	"github.com/coder/coder/v2/coderd/rbac"
+	"github.com/stretchr/testify/require"
 
 	"github.com/coder/coder/v2/cli/clitest"
 	"github.com/coder/coder/v2/coderd/coderdtest"
-	"github.com/stretchr/testify/require"
+	"github.com/coder/coder/v2/coderd/rbac"
+	"github.com/coder/coder/v2/codersdk"
+	"github.com/coder/coder/v2/provisioner/echo"
+	"github.com/coder/coder/v2/testutil"
 )
 
 func TestTemplateVersionsArchive(t *testing.T) {
