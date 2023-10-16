6875faf2383af05d440a536d00206c275a6c83e8
Cian Johnston <cian@coder.com>
2023-10-16 13:50:07 +0100

    fix(coderd/provisionerdserver): pass through api ctx to provisionerdserver (#10259)
    Passes through coderd API ctx to provisionerd server so we can cancel workspace updates when API is shutting down.

--- coderd/coderd.go
+++ coderd/coderd.go
@@ -1108,6 +1108,7 @@ func (api *API) CreateInMemoryProvisionerDaemon(ctx context.Context) (client pro
 	logger := api.Logger.Named(fmt.Sprintf("inmem-provisionerd-%s", name))
 	logger.Info(ctx, "starting in-memory provisioner daemon")
 	srv, err := provisionerdserver.NewServer(
+		api.ctx,
 		api.AccessURL,
 		uuid.New(),
 		logger,
