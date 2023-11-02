f4b9a636ccf82632e471899cbd0e15a8ff668265
Parkreiner <michaelsmith@coder.com>
2023-11-02 19:03:19 +0000

    wip: commit progress on AvatarCard

--- site/src/components/AvatarCard/AvatarCard.tsx
+++ site/src/components/AvatarCard/AvatarCard.tsx
@@ -1,5 +1,13 @@
 import { type ReactNode } from "react";
-import { useTheme } from "@emotion/react";
+import { Avatar, AvatarIcon } from "components/Avatar/Avatar";
+import {
+  type CSSObject,
+  type Interpolation,
+  type Theme,
+  useTheme,
+} from "@emotion/react";
+
+type Width = "sm" | "md" | "lg" | "full";
 
 type AvatarCardProps = {
   header: ReactNode;
@@ -7,8 +15,15 @@ type AvatarCardProps = {
   altText: string;
 
   subtitle?: ReactNode;
-  width?: "sm" | "md" | "lg" | "full";
+  width?: Width;
 };
+
+const renderedWidths = {
+  sm: (theme) => theme.spacing(2),
+  md: (theme) => theme.spacing(3),
+  lg: (theme) => theme.spacing(6),
+  full: { width: "100%" },
+} as const satisfies Record<Width, Interpolation<Theme>>;
 
 export function AvatarCard({
   header,
@@ -20,14 +35,40 @@ export function AvatarCard({
   const theme = useTheme();
 
   return (
-    <div css={{ backgroundColor: "blue" }}>
-      <div>
-        <span>{header}</span>
-        {subtitle && <span>{subtitle}</span>}
+    <div
+      css={[
+        renderedWidths[width],
+        {
+          display: "flex",
+          flexFlow: "row nowrap",
+          alignItems: "center",
+          border: `1px solid ${theme.palette.divider}`,
+          gap: theme.spacing(2),
+          padding: theme.spacing(2),
+          borderRadius: theme.shape.borderRadius,
+          cursor: "default",
+        },
+      ]}
+    >
+      <div css={{ marginRight: "auto" }}>
+        <div css={theme.typography.body1 as CSSObject}>{header}</div>
+
+        {subtitle && (
+          <div
+            css={[
+              theme.typography.body2 as CSSObject,
+              { color: theme.palette.text.secondary },
+            ]}
+          >
+            {subtitle}
+          </div>
+        )}
       </div>
 
       <div>
-        <img src={imgUrl} alt={altText} />
+        <Avatar>
+          <AvatarIcon src={imgUrl} alt={altText} />
+        </Avatar>
       </div>
     </div>
   );
