3f491e1dd9603e357b06ae0f149839af252ceabe
Parkreiner <michaelsmith@coder.com>
2023-11-06 18:24:35 +0000

    docs: Clean up wording on docs

--- site/src/hooks/hookPolyfills.ts
+++ site/src/hooks/hookPolyfills.ts
@@ -12,20 +12,22 @@ import { useCallback, useEffect, useRef } from "react";
  * A DIY version of useEffectEvent.
  *
  * Works like useCallback, except that it doesn't take a dependency array, and
- * always returns out a stable function on every single render. The returned-out
+ * always returns out the same function on every single render. The returned-out
  * function is always able to "see" the most up-to-date version of the callback
  * passed in (including its closure values).
  *
  * This is not a 1:1 replacement for useCallback. 99% of the time,
  * useEffectEvent should be called in the same component/custom hook where you
- * have a useEffect call, and should not ever be a prop.
+ * have a useEffect call. A useEffectEvent function probably shouldn't be a
+ * prop, unless you're trying to wrangle a weird library.
  *
  * Example uses of useEffectEvent:
  * 1. Stabilizing a function that you don't have direct control over (because it
  *    comes from a library) without violating useEffect dependency arrays
  * 2. Moving the burden of memoization from the parent to the custom hook (e.g.,
  *    making it so that you don't need your components to always use useCallback
- *    just to get things wired up properly. See also: React Query's queryFn.)
+ *    just to get things wired up properly. Similar example: the queryFn
+ *    property on React Query's useQuery)
  *
  * @see {@link https://react.dev/reference/react/experimental_useEffectEvent}
  */
