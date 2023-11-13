a7ec151ff6af5d187d851a57cb398043b972c6ff
Marcin Tojek <marcin@coder.com>
2023-11-13 13:59:49 +0100

    Healthcheck fix

--- coderd/healthcheck/healthcheck.go
+++ coderd/healthcheck/healthcheck.go
@@ -30,7 +30,7 @@ type Checker interface {
 	Database(ctx context.Context, opts *DatabaseReportOptions) DatabaseReport
 }
 
-// @typescript-generate DerphealthReport
+// @typescript-generate Report
 type Report struct {
 	// Time is the time the report was generated at.
 	Time time.Time `json:"time"`
