37ea50b42b88fa32ebdbc329fbdacde3b50d4165
Parkreiner <michaelsmith@coder.com>
2023-11-14 22:45:27 +0000

    wip: commit progress on usePaginatedQuery

--- site/src/components/PaginationWidget/usePaginatedQuery.ts
+++ site/src/components/PaginationWidget/usePaginatedQuery.ts
@@ -1,9 +1,14 @@
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useEffectEvent } from "hooks/hookPolyfills";
+
+import { type Pagination } from "api/typesGenerated";
 import { DEFAULT_RECORDS_PER_PAGE } from "./utils";
+import { prepareQuery } from "utils/filters";
+import { clamp } from "lodash";
 
 import {
+  type QueryFunction,
   type QueryKey,
   type UseQueryOptions,
   useQueryClient,
@@ -11,15 +16,30 @@ import {
 } from "react-query";
 
 const PAGE_PARAMS_KEY = "page";
+const PAGE_FILTER_KEY = "filter";
 
-// Any JSON-serializable object with a count property representing the total
-// number of records for a given resource
-type PaginatedData = {
-  count: number;
+// Only omitting after_id for simplifying initial implementation; property
+// should probably be added back in down the line
+type PaginationInput = Omit<Pagination, "after_id"> & {
+  q: string;
+  limit: number;
+  offset: number;
 };
 
+/**
+ * Any JSON-serializable object returned by the API that exposes the total
+ * number of records that match a query
+ */
+interface PaginatedData {
+  count: number;
+}
+
+/**
+ * A more specialized version of UseQueryOptions built specifically for
+ * paginated queries.
+ */
 // All the type parameters just mirror the ones used by React Query
-type PaginatedOptions<
+export type UsePaginatedQueryOptions<
   TQueryFnData extends PaginatedData = PaginatedData,
   TError = unknown,
   TData = TQueryFnData,
@@ -28,15 +48,21 @@ type PaginatedOptions<
   UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
   "keepPreviousData" | "queryKey"
 > & {
-  /**
-   * A function that takes a page number and produces a full query key. Must be
-   * a function so that it can be used for the active query, as well as
-   * prefetching
-   */
-  queryKey: (pageNumber: number) => TQueryKey;
+  prefetch?: boolean;
 
-  searchParamsResult: ReturnType<typeof useSearchParams>;
-  prefetchNextPage?: boolean;
+  /**
+   * A function that takes pagination information and produces a full query key.
+   *
+   * Must be a function so that it can be used for the active query, as well as
+   * any prefetching.
+   */
+  queryKey: (pagination: PaginationInput) => TQueryKey;
+
+  /**
+   * A version of queryFn that is required and that has access to the current
+   * page via the pageParams context property
+   */
+  queryFn: QueryFunction<TQueryFnData, TQueryKey, number>;
 };
 
 export function usePaginatedQuery<
@@ -44,65 +70,81 @@ export function usePaginatedQuery<
   TError = unknown,
   TData extends PaginatedData = TQueryFnData,
   TQueryKey extends QueryKey = QueryKey,
->(options: PaginatedOptions<TQueryFnData, TError, TData, TQueryKey>) {
-  const { searchParamsResult, queryKey, prefetchNextPage = false } = options;
-  const [searchParams, setSearchParams] = searchParamsResult;
-  const currentPage = parsePage(searchParams);
+>(options: UsePaginatedQueryOptions<TQueryFnData, TError, TData, TQueryKey>) {
+  const { queryKey, queryFn, prefetch = false, ...otherOptions } = options;
 
-  // Can't use useInfiniteQuery because that hook is designed to work with data
-  // that streams in and can't be cut up into pages easily
-  const query = useQuery({
-    ...options,
-    queryKey: queryKey(currentPage),
-    keepPreviousData: true,
-  });
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = parsePage(searchParams);
 
   const pageSize = DEFAULT_RECORDS_PER_PAGE;
   const pageOffset = (currentPage - 1) * pageSize;
-  const totalRecords = query.data?.count ?? 0;
-  const totalPages = Math.ceil(totalRecords / pageSize);
-  const hasPreviousPage = currentPage > 1;
-  const hasNextPage = pageSize * pageOffset < totalRecords;
+
+  const query = useQuery({
+    ...otherOptions,
+    queryFn: (queryCxt) => queryFn({ ...queryCxt, pageParam: currentPage }),
+    queryKey: queryKey({
+      q: preparePageQuery(searchParams, currentPage),
+      limit: pageSize,
+      offset: pageOffset,
+    }),
+    keepPreviousData: true,
+  });
 
   const queryClient = useQueryClient();
-  const prefetch = useEffectEvent((newPage: number) => {
-    if (!prefetchNextPage) {
+  const prefetchPage = useEffectEvent((newPage: number) => {
+    if (!prefetch) {
       return;
     }
 
-    const newKey = queryKey(newPage);
-    void queryClient.prefetchQuery(newKey);
+    void queryClient.prefetchQuery({
+      queryFn: (queryCxt) => queryFn({ ...queryCxt, pageParam: newPage }),
+      queryKey: queryKey({
+        q: preparePageQuery(searchParams, newPage),
+        limit: pageSize,
+        offset: pageOffset,
+      }),
+    });
   });
+
+  const totalRecords = query.data?.count ?? 0;
+  const totalPages = Math.ceil(totalRecords / pageSize);
+  const hasNextPage = pageSize * pageOffset < totalRecords;
+  const hasPreviousPage = currentPage > 1;
+
+  // Have to split hairs and sync on both the current page and the hasXPage
+  // variables because hasXPage values are derived from server values and aren't
+  // immediately ready on each render
+  useEffect(() => {
+    if (hasNextPage) {
+      prefetchPage(currentPage + 1);
+    }
+  }, [prefetchPage, currentPage, hasNextPage]);
 
   useEffect(() => {
     if (hasPreviousPage) {
-      prefetch(currentPage - 1);
+      prefetchPage(currentPage - 1);
     }
+  }, [prefetchPage, currentPage, hasPreviousPage]);
 
-    if (hasNextPage) {
-      prefetch(currentPage + 1);
+  // Mainly here to catch user if they navigate to a page directly via URL
+  const updatePageIfInvalid = useEffectEvent(() => {
+    const clamped = clamp(currentPage, 1, totalPages);
+
+    if (currentPage !== clamped) {
+      searchParams.set(PAGE_PARAMS_KEY, String(clamped));
+      setSearchParams(searchParams);
     }
-  }, [prefetch, currentPage, hasNextPage, hasPreviousPage]);
-
-  // Tries to redirect a user if they navigate to a page via invalid URL
-  const navigateIfInvalidPage = useEffectEvent(
-    (currentPage: number, totalPages: number) => {
-      const clamped = Math.max(1, Math.min(currentPage, totalPages));
-
-      if (currentPage !== clamped) {
-        searchParams.set(PAGE_PARAMS_KEY, String(clamped));
-        setSearchParams(searchParams);
-      }
-    },
-  );
+  });
 
   useEffect(() => {
-    navigateIfInvalidPage(currentPage, totalPages);
-  }, [navigateIfInvalidPage, currentPage, totalPages]);
+    if (!query.isFetching) {
+      updatePageIfInvalid();
+    }
+  }, [updatePageIfInvalid, query.isFetching]);
 
   const onPageChange = (newPage: number) => {
     const safePage = Number.isInteger(newPage)
-      ? Math.max(1, Math.min(newPage))
+      ? clamp(newPage, 1, totalPages)
       : 1;
 
     searchParams.set(PAGE_PARAMS_KEY, String(safePage));
@@ -112,10 +154,13 @@ export function usePaginatedQuery<
   return {
     ...query,
     onPageChange,
+    goToPreviousPage: () => onPageChange(currentPage - 1),
+    goToNextPage: () => onPageChange(currentPage + 1),
     currentPage,
+    pageSize,
     totalRecords,
     hasNextPage,
-    pageSize,
+    hasPreviousPage,
     isLoading: query.isLoading || query.isFetching,
   } as const;
 }
@@ -123,4 +168,19 @@ export function usePaginatedQuery<
 function parsePage(params: URLSearchParams): number {
   const parsed = Number(params.get("page"));
   return Number.isInteger(parsed) && parsed > 1 ? parsed : 1;
+}
+
+function preparePageQuery(searchParams: URLSearchParams, page: number) {
+  const paramsPage = Number(searchParams.get(PAGE_FILTER_KEY));
+
+  let queryText: string;
+  if (paramsPage === page) {
+    queryText = searchParams.toString();
+  } else {
+    const newParams = new URLSearchParams(searchParams);
+    newParams.set(PAGE_FILTER_KEY, String(page));
+    queryText = newParams.toString();
+  }
+
+  return prepareQuery(queryText);
 }
