1671f5eed685298222d56578559a395a4f33597a
Marcin Tojek <marcin@coder.com>
2023-11-17 15:04:45 +0100

    Better unit

--- coderd/healthcheck/derphealth/derp_test.go
+++ coderd/healthcheck/derphealth/derp_test.go
@@ -171,10 +171,13 @@ func TestDERP(t *testing.T) {
 		report.Run(ctx, opts)
 
 		assert.True(t, report.Healthy)
+		assert.NotEmpty(t, report.Warnings)
 		for _, region := range report.Regions {
 			assert.True(t, region.Healthy)
+			assert.NotEmpty(t, region.Warnings)
 			for _, node := range region.NodeReports {
 				assert.True(t, node.Healthy)
+				assert.NotEmpty(t, node.Warnings)
 				assert.True(t, node.CanExchangeMessages)
 				assert.NotEmpty(t, node.RoundTripPing)
 				assert.Len(t, node.ClientLogs, 2)
