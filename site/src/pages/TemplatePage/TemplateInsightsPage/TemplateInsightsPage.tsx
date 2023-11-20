e78b2112d1601d0c001180f3cef84d2ba45077c4
Mathias Fredriksson <mafredri@gmail.com>
2023-11-20 19:00:58 +0200

    Update site/src/pages/TemplatePage/TemplateInsightsPage/TemplateInsightsPage.tsx

--- site/src/pages/TemplatePage/TemplateInsightsPage/TemplateInsightsPage.tsx
+++ site/src/pages/TemplatePage/TemplateInsightsPage/TemplateInsightsPage.tsx
@@ -796,7 +796,7 @@ function formatOffset(offset: number): string {
   const absoluteOffset = Math.abs(offset);
   const hours = Math.floor(absoluteOffset / 60);
   const minutes = Math.abs(offset) % 60;
-  const formattedHours = `${isPositiveTimezone ? "-" : "+"}${String(
+  const formattedHours = `${isPositiveTimezone ? "+" : "-"}${String(
     hours,
   ).padStart(2, "0")}`;
   const formattedMinutes = String(minutes).padStart(2, "0");
