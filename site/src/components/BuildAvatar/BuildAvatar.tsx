59a92640c040cfa227fa8cb105003aea73b4536a
McKayla Washburn <mckayla@hey.com>
2023-11-20 21:31:26 +0000

    refactor: remove usage of `styled` and `withStyles`

--- site/src/components/BuildAvatar/BuildAvatar.tsx
+++ site/src/components/BuildAvatar/BuildAvatar.tsx
@@ -1,28 +1,10 @@
 import Badge from "@mui/material/Badge";
-import { withStyles } from "@mui/styles";
+import { useTheme } from "@emotion/react";
 import { type FC } from "react";
-import { WorkspaceBuild } from "api/typesGenerated";
+import type { WorkspaceBuild } from "api/typesGenerated";
 import { getDisplayWorkspaceBuildStatus } from "utils/workspace";
 import { Avatar, AvatarProps } from "components/Avatar/Avatar";
-import type { PaletteIndex } from "theme/mui";
-import { useTheme } from "@emotion/react";
 import { BuildIcon } from "components/BuildIcon/BuildIcon";
-
-interface StylesBadgeProps {
-  type: PaletteIndex;
-}
-
-const StyledBadge = withStyles((theme) => ({
-  badge: {
-    backgroundColor: ({ type }: StylesBadgeProps) => theme.palette[type].light,
-    borderRadius: "100%",
-    width: 8,
-    minWidth: 8,
-    height: 8,
-    display: "block",
-    padding: 0,
-  },
-}))(Badge);
 
 export interface BuildAvatarProps {
   build: WorkspaceBuild;
@@ -34,10 +16,9 @@ export const BuildAvatar: FC<BuildAvatarProps> = ({ build, size }) => {
   const displayBuildStatus = getDisplayWorkspaceBuildStatus(theme, build);
 
   return (
-    <StyledBadge
+    <Badge
       role="status"
-      type={displayBuildStatus.type}
-      arial-label={displayBuildStatus.status}
+      aria-label={displayBuildStatus.status}
       title={displayBuildStatus.status}
       overlap="circular"
       anchorOrigin={{
@@ -45,10 +26,19 @@ export const BuildAvatar: FC<BuildAvatarProps> = ({ build, size }) => {
         horizontal: "right",
       }}
       badgeContent={<div></div>}
+      css={{
+        backgroundColor: theme.palette[displayBuildStatus.type].light,
+        borderRadius: "100%",
+        width: 8,
+        minWidth: 8,
+        height: 8,
+        display: "block",
+        padding: 0,
+      }}
     >
       <Avatar background size={size}>
         <BuildIcon transition={build.transition} />
       </Avatar>
-    </StyledBadge>
+    </Badge>
   );
 };
--- site/src/components/GroupAvatar/GroupAvatar.tsx
+++ site/src/components/GroupAvatar/GroupAvatar.tsx
@@ -1,45 +1,44 @@
-import { Avatar } from "components/Avatar/Avatar";
 import Badge from "@mui/material/Badge";
-import { withStyles } from "@mui/styles";
 import Group from "@mui/icons-material/Group";
-import { FC } from "react";
+import { useTheme } from "@emotion/react";
+import { type FC } from "react";
+import { Avatar } from "components/Avatar/Avatar";
 
-const StyledBadge = withStyles((theme) => ({
-  badge: {
-    backgroundColor: theme.palette.background.paper,
-    border: `1px solid ${theme.palette.divider}`,
-    borderRadius: "100%",
-    width: 24,
-    height: 24,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-
-    "& svg": {
-      width: 14,
-      height: 14,
-    },
-  },
-}))(Badge);
-
-export type GroupAvatarProps = {
+export interface GroupAvatarProps {
   name: string;
   avatarURL?: string;
-};
+}
 
 export const GroupAvatar: FC<GroupAvatarProps> = ({ name, avatarURL }) => {
+  const theme = useTheme();
+
   return (
-    <StyledBadge
+    <Badge
       overlap="circular"
       anchorOrigin={{
         vertical: "bottom",
         horizontal: "right",
       }}
       badgeContent={<Group />}
+      css={{
+        backgroundColor: theme.palette.background.paper,
+        border: `1px solid ${theme.palette.divider}`,
+        borderRadius: "100%",
+        width: 24,
+        height: 24,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+
+        "& svg": {
+          width: 14,
+          height: 14,
+        },
+      }}
     >
       <Avatar src={avatarURL} background>
         {name}
       </Avatar>
-    </StyledBadge>
+    </Badge>
   );
 };
