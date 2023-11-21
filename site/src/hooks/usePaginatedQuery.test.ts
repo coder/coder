289363050d99aec15d59a6974a764558bec9bafe
Parkreiner <michaelsmith@coder.com>
2023-11-21 22:58:16 +0000

    wip: commit more progress on tests

--- site/src/hooks/usePaginatedQuery.test.ts
+++ site/src/hooks/usePaginatedQuery.test.ts
@@ -108,7 +108,6 @@ describe(usePaginatedQuery.name, () => {
   });
 
   describe("Prefetching", () => {
-    const noPrefetchTimeout = 1000;
     const mockQueryKey = jest.fn(({ pageNumber }) => ["query", pageNumber]);
     const mockQueryFn = jest.fn(({ pageNumber, limit }) => {
       return Promise.resolve({
@@ -117,81 +116,79 @@ describe(usePaginatedQuery.name, () => {
       });
     });
 
-    it("Prefetches the previous page if it exists", async () => {
-      const startingPage = 2;
+    const testPrefetch = async (
+      startingPage: number,
+      targetPage: number,
+      shouldMatch: boolean,
+    ) => {
       await render(
         { queryKey: mockQueryKey, queryFn: mockQueryFn },
         `/?page=${startingPage}`,
       );
 
-      const pageMatcher = expect.objectContaining({
-        pageNumber: 1,
-      });
+      const pageMatcher = expect.objectContaining({ pageNumber: targetPage });
+      if (shouldMatch) {
+        await waitFor(() => expect(mockQueryFn).toBeCalledWith(pageMatcher));
+      } else {
+        // Can't use waitFor to test this, because the expect call will
+        // immediately succeed for the not case, even though queryFn needs to be
+        // called async via React Query
+        setTimeout(() => {
+          expect(mockQueryFn).not.toBeCalledWith(pageMatcher);
+        }, 1000);
 
-      await waitFor(() => expect(mockQueryFn).toBeCalledWith(pageMatcher));
+        jest.runAllTimers();
+      }
+    };
+
+    it("Prefetches the previous page if it exists", async () => {
+      await testPrefetch(2, 1, true);
     });
 
     it("Prefetches the next page if it exists", async () => {
-      const startingPage = 1;
-      await render(
-        { queryKey: mockQueryKey, queryFn: mockQueryFn },
-        `/?page=${startingPage}`,
-      );
-
-      const pageMatcher = expect.objectContaining({
-        pageNumber: 2,
-      });
-
-      await waitFor(() => expect(mockQueryFn).toBeCalledWith(pageMatcher));
+      await testPrefetch(1, 2, true);
     });
 
     it("Avoids prefetch for previous page if it doesn't exist", async () => {
-      const startingPage = 1;
-      await render(
-        { queryKey: mockQueryKey, queryFn: mockQueryFn },
-        `/?page=${startingPage}`,
-      );
-
-      const pageMatcher = expect.objectContaining({
-        pageNumber: 0,
-      });
-
-      // Can't use waitFor to test this, because the expect call will
-      // immediately succeed for the not case, even though queryFn needs to be
-      // called async via React Query
-      setTimeout(() => {
-        expect(mockQueryFn).not.toBeCalledWith(pageMatcher);
-      }, noPrefetchTimeout);
-
-      jest.runAllTimers();
+      await testPrefetch(1, 0, false);
     });
 
     it("Avoids prefetch for next page if it doesn't exist", async () => {
-      const startingPage = 2;
-      await render(
-        { queryKey: mockQueryKey, queryFn: mockQueryFn },
-        `/?page=${startingPage}`,
-      );
-
-      const pageMatcher = expect.objectContaining({
-        pageNumber: 3,
-      });
-
-      setTimeout(() => {
-        expect(mockQueryFn).not.toBeCalledWith(pageMatcher);
-      }, noPrefetchTimeout);
-
-      jest.runAllTimers();
+      await testPrefetch(2, 3, false);
     });
 
     it("Reuses the same queryKey and queryFn methods for the current page and all prefetching", async () => {
-      //
+      expect.hasAssertions();
     });
   });
 
-  describe("Invalid page safety nets/redirects", () => {});
+  describe("Invalid page safety nets/redirects", () => {
+    it("Auto-redirects user to page 1 if params are corrupt/invalid", async () => {
+      expect.hasAssertions();
+    });
+
+    it("Auto-redirects user to closest page if request page overshoots", async () => {
+      expect.hasAssertions();
+    });
+
+    it("Auto-redirects user to first page if request page goes below 1", async () => {
+      expect.hasAssertions();
+    });
+
+    it("Calls the custom onInvalidPageChange callback if provided", async () => {
+      expect.hasAssertions();
+    });
+  });
+
+  describe("Passing outside value for URLSearchParams", () => {
+    it("Reads from searchParams property if provided", async () => {
+      expect.hasAssertions();
+    });
+
+    it("Flushes state changes via provided searchParams property", async () => {
+      expect.hasAssertions();
+    });
+  });
 
   describe("Returned properties", () => {});
-
-  describe("Passing outside value for URLSearchParams", () => {});
 });
