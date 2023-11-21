bae0da9cc55e5b51f75014187ab2e346311c3f6e
McKayla Washburn <mckayla@hey.com>
2023-11-21 17:01:31 +0000

    🧹

--- site/src/components/BuildAvatar/BuildAvatar.tsx
+++ site/src/components/BuildAvatar/BuildAvatar.tsx
@@ -1,8 +1,10 @@
 import Badge from "@mui/material/Badge";
+import { css, cx } from "@emotion/css";
 import { useTheme } from "@emotion/react";
 import { type FC } from "react";
 import type { WorkspaceBuild } from "api/typesGenerated";
 import { getDisplayWorkspaceBuildStatus } from "utils/workspace";
+import { useClassName } from "hooks/useClassName";
 import { Avatar, AvatarProps } from "components/Avatar/Avatar";
 import { BuildIcon } from "components/BuildIcon/BuildIcon";
 
@@ -13,32 +15,41 @@ export interface BuildAvatarProps {
 
 export const BuildAvatar: FC<BuildAvatarProps> = ({ build, size }) => {
   const theme = useTheme();
-  const displayBuildStatus = getDisplayWorkspaceBuildStatus(theme, build);
+  const { status, type } = getDisplayWorkspaceBuildStatus(theme, build);
+  const badgeType = useClassName(
+    (css, theme) => css`
+      background-color: ${theme.palette[type].light};
+    `,
+    [type],
+  );
 
   return (
     <Badge
       role="status"
-      aria-label={displayBuildStatus.status}
-      title={displayBuildStatus.status}
+      aria-label={status}
+      title={status}
       overlap="circular"
       anchorOrigin={{
         vertical: "bottom",
         horizontal: "right",
       }}
       badgeContent={<div></div>}
-      css={{
-        backgroundColor: theme.palette[displayBuildStatus.type].light,
-        borderRadius: "100%",
-        width: 8,
-        minWidth: 8,
-        height: 8,
-        display: "block",
-        padding: 0,
-      }}
+      classes={{ badge: cx(classNames.badge, badgeType) }}
     >
       <Avatar background size={size}>
         <BuildIcon transition={build.transition} />
       </Avatar>
     </Badge>
   );
+};
+
+const classNames = {
+  badge: css({
+    borderRadius: "100%",
+    width: 8,
+    minWidth: 8,
+    height: 8,
+    display: "block",
+    padding: 0,
+  }),
 };
