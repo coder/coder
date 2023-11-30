d1ce4cc0e066fcb57bc0aefe331d27e88aec3c6a
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2023-11-30 18:22:03 +0000

    refactor(site): improve health check page sidebar

--- site/src/theme/mui.ts
+++ site/src/theme/mui.ts
@@ -79,13 +79,13 @@ export let dark = createTheme({
     fontFamily: BODY_FONT_FAMILY,
 
     body1: {
-      fontSize: "1rem" /* 16px at default scaling */,
-      lineHeight: "1.5rem" /* 24px at default scaling */,
+      fontSize: 16,
+      lineHeight: "160%",
     },
 
     body2: {
-      fontSize: "0.875rem" /* 14px at default scaling */,
-      lineHeight: "1.25rem" /* 20px at default scaling */,
+      fontSize: 14,
+      lineHeight: "160%",
     },
   },
   shape: {
