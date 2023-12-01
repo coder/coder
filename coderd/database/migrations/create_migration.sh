3ca5ebf4155d4e8d96404f3863fc17e55aeb5abe
Colin Adler <colin1adler@gmail.com>
2023-12-01 21:31:41 +0000

    document the correct way to create a new enum value

--- coderd/database/migrations/create_migration.sh
+++ coderd/database/migrations/create_migration.sh
@@ -16,6 +16,23 @@ enum value needs to be referenced in another migration.
 This also means you should not use "BEGIN;" and "COMMIT;" in your migrations, as
 everything is already in a migration.
 
+An example way of the proper way to add an enum value:
+
+CREATE TYPE new_logintype AS ENUM (
+	'password',
+	'github',
+	'oidc',
+	'token' -- this is our new value
+);
+
+ALTER TABLE users
+	ALTER COLUMN login_type DROP DEFAULT, -- if the column has a default, it must be dropped first
+	ALTER COLUMN login_type TYPE new_logintype USING (login_type::text::new_logintype), -- converts the old enum until the new enum using text as an intermediary
+	ALTER COLUMN login_type SET DEFAULT 'password'::new_logintype; -- re-add the default using the new enum
+
+DROP TYPE login_type;
+ALTER TYPE new_logintype RENAME TO login_type;
+
 EOF
 
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
