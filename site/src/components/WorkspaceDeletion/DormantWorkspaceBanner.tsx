12959344afb94387b813ef8c165d60a97bcc6a4c
Jon Ayers <jon@coder.com>
2023-12-06 01:15:42 +0000

    feat: show dormant workspaces by default

--- site/src/components/WorkspaceDeletion/DormantWorkspaceBanner.tsx
+++ site/src/components/WorkspaceDeletion/DormantWorkspaceBanner.tsx
@@ -1,7 +1,5 @@
 import { formatDistanceToNow } from "date-fns";
-import Link from "@mui/material/Link";
 import { type FC } from "react";
-import { Link as RouterLink } from "react-router-dom";
 import type { Workspace } from "api/typesGenerated";
 import { useIsWorkspaceActionsEnabled } from "components/Dashboard/DashboardProvider";
 import { Alert } from "components/Alert/Alert";
@@ -12,36 +10,22 @@ export enum Count {
 }
 
 interface DormantWorkspaceBannerProps {
-  workspaces?: Workspace[];
+  workspace: Workspace;
   onDismiss: () => void;
   shouldRedisplayBanner: boolean;
-  count?: Count;
 }
 
 export const DormantWorkspaceBanner: FC<DormantWorkspaceBannerProps> = ({
-  workspaces,
+  workspace,
   onDismiss,
   shouldRedisplayBanner,
-  count = Count.Singular,
 }) => {
   const experimentEnabled = useIsWorkspaceActionsEnabled();
-
-  if (!workspaces) {
-    return null;
-  }
-
-  const hasDormantWorkspaces = workspaces.find(
-    (workspace) => workspace.dormant_at,
-  );
-
-  const hasDeletionScheduledWorkspaces = workspaces.find(
-    (workspace) => workspace.deleting_at,
-  );
 
   if (
     // Only show this if the experiment is included.
     !experimentEnabled ||
-    !hasDormantWorkspaces ||
+    !workspace.dormant_at ||
     // Banners should be redisplayed after dismissal when additional workspaces are newly scheduled for deletion
     !shouldRedisplayBanner
   ) {
@@ -58,44 +42,27 @@ export const DormantWorkspaceBanner: FC<DormantWorkspaceBannerProps> = ({
   };
 
   const alertText = (): string => {
-    if (workspaces.length === 1) {
-      if (
-        hasDeletionScheduledWorkspaces &&
-        hasDeletionScheduledWorkspaces.deleting_at &&
-        hasDeletionScheduledWorkspaces.dormant_at
-      ) {
-        return `This workspace has been dormant for ${formatDistanceToNow(
-          Date.parse(hasDeletionScheduledWorkspaces.dormant_at),
-        )} and is scheduled to be deleted on ${formatDate(
-          hasDeletionScheduledWorkspaces.deleting_at,
-        )} . To keep it you must activate the workspace.`;
-      } else if (hasDormantWorkspaces && hasDormantWorkspaces.dormant_at) {
-        return `This workspace has been dormant for ${formatDistanceToNow(
-          Date.parse(hasDormantWorkspaces.dormant_at),
-        )}
+    if (workspace.deleting_at) {
+      return `This workspace has been dormant for ${formatDistanceToNow(
+        Date.parse(workspace.last_used_at),
+      )} and is scheduled to be deleted on ${formatDate(
+        workspace.deleting_at,
+      )} . To keep it you must activate the workspace.`;
+    } else if (workspace.dormant_at) {
+      return `This workspace has been dormant for ${formatDistanceToNow(
+        Date.parse(workspace.dormant_at),
+      )}
         and cannot be interacted
 		with. Dormant workspaces are eligible for
 		permanent deletion. To prevent deletion, activate
 		the workspace.`;
-      }
     }
     return "";
   };
 
   return (
     <Alert severity="warning" onDismiss={onDismiss} dismissible>
-      {count === Count.Singular ? (
-        alertText()
-      ) : (
-        <>
-          <span>There are</span>{" "}
-          <Link component={RouterLink} to="/workspaces?filter=is-dormant:true">
-            workspaces
-          </Link>{" "}
-          that may be deleted soon due to inactivity. Activate the workspaces
-          you wish to retain.
-        </>
-      )}
+      {alertText()}
     </Alert>
   );
 };
