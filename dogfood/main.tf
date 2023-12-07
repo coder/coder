7b1eb089cccf2f8d789abff9a92921df2aebf348
Muhammad Atif Ali <atif@coder.com>
2023-12-08 02:04:16 +0300

    chore(dogfood): remove `agent_name` from jetbrains-ide module

--- dogfood/main.tf
+++ dogfood/main.tf
@@ -103,7 +103,6 @@ module "code-server" {
 module "jetbrains_gateway" {
   source         = "https://registry.coder.com/modules/jetbrains-gateway"
   agent_id       = coder_agent.dev.id
-  agent_name     = "dev"
   folder         = local.repo_dir
   jetbrains_ides = ["GO", "WS"]
   default        = "GO"
