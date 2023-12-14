b135d91805d862a73b22bb97cd9a7c21e2b1b84c
McKayla Washburn <mckayla@hey.com>
2023-12-14 18:20:35 +0000

    fix: don't use darkBlue as light for auto

--- site/src/theme/index.ts
+++ site/src/theme/index.ts
@@ -1,8 +1,8 @@
-import { type Theme as MuiTheme } from "@mui/material/styles";
+import type { Theme as MuiTheme } from "@mui/material/styles";
 import dark from "./dark";
 import darkBlue from "./darkBlue";
-import { type NewTheme } from "./experimental";
-import { Colors } from "./colors";
+import type { NewTheme } from "./experimental";
+import type { Colors } from "./colors";
 
 export interface Theme extends MuiTheme {
   colors: Colors;
@@ -14,7 +14,7 @@ export const DEFAULT_THEME = "auto";
 const theme = {
   dark,
   darkBlue,
-  light: darkBlue,
+  light: dark,
 } satisfies Record<string, Theme>;
 
 export default theme;
