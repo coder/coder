7c4fbe5baeaaa0462d21182d74a6198c13dc1fb8
Bruno Quaresma <bruno@coder.com>
2023-12-19 10:43:23 -0300

    refactor(site): make HelpTooltip easier to reuse and compose (#11242)

--- site/src/components/Resources/AgentLatency.tsx
+++ site/src/components/Resources/AgentLatency.tsx
@@ -1,14 +1,16 @@
-import { type FC, useRef, useState } from "react";
+import { type FC } from "react";
 import { useTheme } from "@emotion/react";
 import { Theme } from "@mui/material/styles";
 import type { WorkspaceAgent, DERPRegion } from "api/typesGenerated";
 import {
   HelpTooltipText,
-  HelpPopover,
+  HelpTooltip,
   HelpTooltipTitle,
+  HelpTooltipContent,
 } from "components/HelpTooltip/HelpTooltip";
 import { Stack } from "components/Stack/Stack";
 import { getLatencyColor } from "utils/latency";
+import { PopoverTrigger } from "components/Popover/Popover";
 
 const getDisplayLatency = (theme: Theme, agent: WorkspaceAgent) => {
   // Find the right latency to display
@@ -31,9 +33,6 @@ const getDisplayLatency = (theme: Theme, agent: WorkspaceAgent) => {
 
 export const AgentLatency: FC<{ agent: WorkspaceAgent }> = ({ agent }) => {
   const theme = useTheme();
-  const anchorRef = useRef<HTMLButtonElement>(null);
-  const [isOpen, setIsOpen] = useState(false);
-  const id = isOpen ? "latency-popover" : undefined;
   const latency = getDisplayLatency(theme, agent);
 
   if (!latency || !agent.latency) {
@@ -41,24 +40,17 @@ export const AgentLatency: FC<{ agent: WorkspaceAgent }> = ({ agent }) => {
   }
 
   return (
-    <>
-      <span
-        role="presentation"
-        aria-label="latency"
-        ref={anchorRef}
-        onMouseEnter={() => setIsOpen(true)}
-        onMouseLeave={() => setIsOpen(false)}
-        css={{ cursor: "pointer", color: latency.color }}
-      >
-        {Math.round(Math.round(latency.latency_ms))}ms
-      </span>
-      <HelpPopover
-        id={id}
-        open={isOpen}
-        anchorEl={anchorRef.current}
-        onOpen={() => setIsOpen(true)}
-        onClose={() => setIsOpen(false)}
-      >
+    <HelpTooltip>
+      <PopoverTrigger>
+        <span
+          role="presentation"
+          aria-label="latency"
+          css={{ cursor: "pointer", color: latency.color }}
+        >
+          {Math.round(latency.latency_ms)}ms
+        </span>
+      </PopoverTrigger>
+      <HelpTooltipContent>
         <HelpTooltipTitle>Latency</HelpTooltipTitle>
         <HelpTooltipText>
           This is the latency overhead on non peer to peer connections. The
@@ -86,7 +78,7 @@ export const AgentLatency: FC<{ agent: WorkspaceAgent }> = ({ agent }) => {
               ))}
           </Stack>
         </HelpTooltipText>
-      </HelpPopover>
-    </>
+      </HelpTooltipContent>
+    </HelpTooltip>
   );
 };
