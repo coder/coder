af7bb76738a72dfdbb731eae418b106cc1e3550b
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2023-12-22 16:29:23 +0000

    refactor(site): refactor pill component API

--- site/src/pages/TemplateVersionEditorPage/TemplateVersionStatusBadge.tsx
+++ site/src/pages/TemplateVersionEditorPage/TemplateVersionStatusBadge.tsx
@@ -10,12 +10,9 @@ export const TemplateVersionStatusBadge: FC<{
 }> = ({ version }) => {
   const { text, icon, type } = getStatus(version);
   return (
-    <Pill
-      icon={icon}
-      text={text}
-      type={type}
-      title={`Build status is ${text}`}
-    />
+    <Pill icon={icon} type={type} title={`Build status is ${text}`}>
+      {text}
+    </Pill>
   );
 };
 
