79c0c025f2d8bbd08a4818f046286476937c5631
Garrett Delfosse <garrett@coder.com>
2024-01-04 15:35:13 +0000

    add removal comment

--- cli/templatecreate.go
+++ cli/templatecreate.go
@@ -8,6 +8,7 @@ import (
 	"github.com/coder/pretty"
 )
 
+// TODO(f0ssel): This should be removed a few versions after coder 2.7.0 has been released.
 func (*RootCmd) templateCreate() *clibase.Cmd {
 	cmd := &clibase.Cmd{
 		Use:    "create [name]",
@@ -17,7 +18,7 @@ func (*RootCmd) templateCreate() *clibase.Cmd {
 			_, _ = fmt.Fprintln(inv.Stdout, "\n"+pretty.Sprint(cliui.DefaultStyles.Wrap,
 				pretty.Sprint(
 					cliui.DefaultStyles.Error,
-					"ERROR: The `coder templates create` command has been removed."+
+					"ERROR: The `coder templates create` command has been removed. "+
 						"Use the `coder templates push` command to create and update templates. "+
 						"Use the `coder templates edit` command to change template settings.")))
 			return nil
