789d3df9f90f16d98209a38aa619273ef632c926
Steven Masley <stevenmasley@gmail.com>
2024-01-11 14:53:49 -0600

    example with wrapped

--- cli/errors.go
+++ cli/errors.go
@@ -70,12 +70,11 @@ func (RootCmd) errorExample() *clibase.Cmd {
 			{
 				Use: "multi-error",
 				Handler: func(inv *clibase.Invocation) error {
-					return errors.Join(
+					return xerrors.Errorf("wrapped %w", errors.Join(
 						xerrors.Errorf("first error: %w", errorWithStackTrace()),
 						xerrors.Errorf("second error: %w", errorWithStackTrace()),
 						apiErrorNoHelper,
-					)
-
+					))
 				},
 			},
 			{
