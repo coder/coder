8c6e3341936504dc39f8e47af04ec7b65130550d
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2024-01-12 12:36:33 +0000

    Apply improvements from PRs

--- site/src/pages/WorkspacePage/useResourcesNav.ts
+++ site/src/pages/WorkspacePage/useResourcesNav.ts
@@ -1,32 +1,19 @@
 import { WorkspaceResource } from "api/typesGenerated";
 import { useTab } from "hooks";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 
 export const resourceOptionId = (resource: WorkspaceResource) => {
   return `${resource.type}_${resource.name}`;
 };
 
 export const useResourcesNav = (resources: WorkspaceResource[]) => {
-  const resourcesNav = useTab("resources", "");
-  const selectedResource = resources.find(
-    (r) => resourceOptionId(r) === resourcesNav.value,
+  const firstResource = useMemo(
+    () => (resources.length > 0 ? resources[0] : undefined),
+    [resources],
   );
-
-  useEffect(() => {
-    const hasResourcesWithAgents =
-      resources.length > 0 &&
-      resources[0].agents &&
-      resources[0].agents.length > 0;
-    if (!selectedResource && hasResourcesWithAgents) {
-      resourcesNav.set(resourceOptionId(resources[0]));
-    }
-  }, [resources, selectedResource, resourcesNav]);
-
-  const select = useCallback(
-    (resource: WorkspaceResource) => {
-      resourcesNav.set(resourceOptionId(resource));
-    },
-    [resourcesNav],
+  const resourcesNav = useTab(
+    "resources",
+    firstResource ? resourceOptionId(firstResource) : "",
   );
 
   const isSelected = useCallback(
@@ -34,6 +21,23 @@ export const useResourcesNav = (resources: WorkspaceResource[]) => {
       return resourceOptionId(resource) === resourcesNav.value;
     },
     [resourcesNav.value],
+  );
+
+  const selectedResource = resources.find(isSelected);
+
+  useEffect(() => {
+    const hasResourcesWithAgents =
+      firstResource && firstResource.agents && firstResource.agents.length > 0;
+    if (!selectedResource && hasResourcesWithAgents) {
+      resourcesNav.set(resourceOptionId(firstResource));
+    }
+  }, [firstResource, resourcesNav, selectedResource]);
+
+  const select = useCallback(
+    (resource: WorkspaceResource) => {
+      resourcesNav.set(resourceOptionId(resource));
+    },
+    [resourcesNav],
   );
 
   return {
