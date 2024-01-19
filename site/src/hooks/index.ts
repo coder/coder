4c4ea2d26196a331f378384c06c7ac9ef4ff673f
McKayla Washburn <mckayla@hey.com>
2024-01-19 21:58:43 +0000

    hoo-boy

--- site/src/hooks/index.ts
+++ site/src/hooks/index.ts
@@ -2,5 +2,6 @@ export * from "./useClickable";
 export * from "./useClickableTableRow";
 export * from "./useClipboard";
 export * from "./useFeatureVisibility";
+export * from "./useLocalStorage";
 export * from "./usePagination";
 export * from "./useTab";
