db7875252b90b11ed77f727cfb3129ddff6b0328
McKayla Washburn <mckayla@hey.com>
2024-01-19 18:19:08 +0000

    `parameters: { queries }`

--- site/src/components/WorkspaceOutdatedTooltip/WorkspaceOutdatedTooltip.stories.tsx
+++ site/src/components/WorkspaceOutdatedTooltip/WorkspaceOutdatedTooltip.stories.tsx
@@ -1,22 +1,19 @@
 import { action } from "@storybook/addon-actions";
 import type { Meta, StoryObj } from "@storybook/react";
-import { useQueryClient } from "react-query";
 import { MockTemplateVersion, MockTemplate } from "testHelpers/entities";
 import { WorkspaceOutdatedTooltip } from "./WorkspaceOutdatedTooltip";
 
 const meta: Meta<typeof WorkspaceOutdatedTooltip> = {
   title: "components/WorkspaceOutdatedTooltip",
   component: WorkspaceOutdatedTooltip,
-  decorators: [
-    (Story) => {
-      const queryClient = useQueryClient();
-      queryClient.setQueryData(
-        ["templateVersion", MockTemplateVersion.id],
-        MockTemplateVersion,
-      );
-      return <Story />;
-    },
-  ],
+  parameters: {
+    queries: [
+      {
+        key: ["templateVersion", MockTemplateVersion.id],
+        data: MockTemplateVersion,
+      },
+    ],
+  },
   args: {
     onUpdateVersion: action("onUpdateVersion"),
     templateName: MockTemplate.display_name,
