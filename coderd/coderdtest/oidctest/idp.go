23ef0e7fa48bdb4dfcf04f9c65b0d1e6d1d7fcc6
Steven Masley <stevenmasley@gmail.com>
2024-01-25 12:46:15 -0600

    Revert "fix: github changing emails should update coder emails on login"
    This reverts commit 67c272a8f1ec2658fc35946cd4ecdfc3b30b5d00.
    This commit was intended for another branch

--- coderd/coderdtest/oidctest/idp.go
+++ coderd/coderdtest/oidctest/idp.go
@@ -16,7 +16,6 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
-	"net/http/httputil"
 	"net/url"
 	"strconv"
 	"strings"
@@ -68,9 +67,6 @@ type FakeIDP struct {
 	handler   http.Handler
 	cfg       *oauth2.Config
 
-	// callbackPath allows changing where the callback path to coderd is expected.
-	// This only affects using the Login helper functions.
-	callbackPath string
 	// clientID to be used by coderd
 	clientID     string
 	clientSecret string
@@ -162,12 +158,6 @@ func WithRefresh(hook func(email string) error) func(*FakeIDP) {
 func WithDefaultExpire(d time.Duration) func(*FakeIDP) {
 	return func(f *FakeIDP) {
 		f.defaultExpire = d
-	}
-}
-
-func WithCallbackPath(path string) func(*FakeIDP) {
-	return func(f *FakeIDP) {
-		f.callbackPath = path
 	}
 }
 
@@ -379,12 +369,6 @@ func (f *FakeIDP) Login(t testing.TB, client *codersdk.Client, idTokenClaims jwt
 	t.Helper()
 
 	client, resp := f.AttemptLogin(t, client, idTokenClaims, opts...)
-	if resp.StatusCode != http.StatusOK {
-		data, err := httputil.DumpResponse(resp, true)
-		if err == nil {
-			t.Log(string(data))
-		}
-	}
 	require.Equal(t, http.StatusOK, resp.StatusCode, "client failed to login")
 	return client, resp
 }
@@ -414,11 +398,7 @@ func (f *FakeIDP) AttemptLogin(t testing.TB, client *codersdk.Client, idTokenCla
 func (f *FakeIDP) LoginWithClient(t testing.TB, client *codersdk.Client, idTokenClaims jwt.MapClaims, opts ...func(r *http.Request)) (*codersdk.Client, *http.Response) {
 	t.Helper()
 
-	path := "/api/v2/users/oidc/callback"
-	if f.callbackPath != "" {
-		path = f.callbackPath
-	}
-	coderOauthURL, err := client.URL.Parse(path)
+	coderOauthURL, err := client.URL.Parse("/api/v2/users/oidc/callback")
 	require.NoError(t, err)
 	f.SetRedirect(t, coderOauthURL.String())
 
