92f94d761e16c8fcbbebad4e4664cfa9f5de3b59
Parkreiner <michaelsmith@coder.com>
2024-01-27 00:48:28 +0000

    wip: commit progress on clipboard tests

---
+++ site/src/hooks/useClipboard.test.tsx
@@ -0,0 +1,92 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import { useClipboard } from "./useClipboard";
+
+/**
+ * userEvent.setup is supposed to stub out a clipboard for you, since it doesn't
+ * normally exist in JSDOM. Spent ages trying to figure out how to make it work,
+ * but couldn't figure it out. So the code is using a home-grown mock.
+ *
+ * The bad news is that not using userEvent.setup means that you have to test
+ * all events through fireEvent instead of userEvent
+ *
+ * @todo Figure out how to swap userEvent in.
+ */
+let mockClipboardValue = "";
+
+beforeAll(() => {
+  const originalNavigator = window.navigator;
+  jest.spyOn(window, "navigator", "get").mockImplementation(() => {
+    return {
+      ...originalNavigator,
+      clipboard: {
+        readText: async () => mockClipboardValue,
+        writeText: async (newText) => {
+          mockClipboardValue = newText;
+        },
+      } as typeof window.navigator.clipboard,
+    };
+  });
+
+  jest.spyOn(document, "hasFocus").mockImplementation(() => true);
+  jest.useFakeTimers();
+});
+
+afterAll(() => {
+  jest.resetAllMocks();
+  jest.useRealTimers();
+});
+
+async function prepareInitialClipboardValue(clipboardText: string) {
+  /* eslint-disable-next-line testing-library/render-result-naming-convention --
+     Need to pass the whole render result back */
+  const rendered = renderHook(({ text }) => useClipboard(text), {
+    initialProps: { text: clipboardText },
+  });
+
+  await rendered.result.current.copyToClipboard();
+  await waitFor(() => expect(rendered.result.current.isCopied).toBe(true));
+
+  return rendered;
+}
+
+describe(useClipboard.name, () => {
+  describe("copyToClipboard", () => {
+    it("Injects a new value into the clipboard when called", async () => {
+      await prepareInitialClipboardValue("blah");
+    });
+
+    it("Injects the most recent value the hook rendered with", async () => {
+      const text1 = "blah";
+      const text2 = "nah";
+      const { result, rerender } = await prepareInitialClipboardValue(text1);
+
+      rerender({ text: text2 });
+      await waitFor(() => expect(result.current.isCopied).toBe(false));
+
+      await result.current.copyToClipboard();
+      await waitFor(() => expect(result.current.isCopied).toBe(true));
+      expect(result.current.isCopied).toBe(true);
+    });
+  });
+
+  describe("isCopied property", () => {
+    it("Does not change its value if the clipboard never changes", async () => {
+      const { result } = await prepareInitialClipboardValue("blah");
+      for (let i = 1; i <= 10; i++) {
+        setTimeout(() => {
+          expect(result.current.isCopied).toBe(true);
+        }, i * 10_000);
+      }
+
+      await jest.advanceTimersByTimeAsync(100_000);
+    });
+
+    it.skip("Listens to the user copying different text while in the same tab", async () => {
+      expect.hasAssertions();
+    });
+
+    it.skip("Re-syncs state when user navigates to a different tab and comes back", async () => {
+      expect.hasAssertions();
+    });
+  });
+});
