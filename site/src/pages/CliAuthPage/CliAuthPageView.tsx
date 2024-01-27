cf2d179cfed517b6de9451dcb622c23d07693c1a
Parkreiner <michaelsmith@coder.com>
2024-01-27 03:44:12 +0000

    fix: make center styling more friendly to screen readers

--- site/src/pages/CliAuthPage/CliAuthPageView.tsx
+++ site/src/pages/CliAuthPage/CliAuthPageView.tsx
@@ -6,10 +6,13 @@ import { CodeExample } from "components/CodeExample/CodeExample";
 import { SignInLayout } from "components/SignInLayout/SignInLayout";
 import { Welcome } from "components/Welcome/Welcome";
 import { FullScreenLoader } from "components/Loader/FullScreenLoader";
+import { visuallyHidden } from "@mui/utils";
 
 export interface CliAuthPageViewProps {
   sessionToken?: string;
 }
+
+const VISUALLY_HIDDEN_SPACE = " ";
 
 export const CliAuthPageView: FC<CliAuthPageViewProps> = ({ sessionToken }) => {
   if (!sessionToken) {
@@ -22,6 +25,12 @@ export const CliAuthPageView: FC<CliAuthPageViewProps> = ({ sessionToken }) => {
 
       <p css={styles.instructions}>
         Copy the session token below and
+        {/*
+         * This looks silly, but it's a case where you want to hide the space
+         * visually because it messes up the centering, but you want the space
+         * to still be available to screen readers
+         */}
+        <span css={{ ...visuallyHidden }}>{VISUALLY_HIDDEN_SPACE}</span>
         <strong css={{ display: "block" }}>paste it in your terminal.</strong>
       </p>
 
