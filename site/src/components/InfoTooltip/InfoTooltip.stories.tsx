443b9fc5ba0690a1bdbf3394fe7e9d7cc8263ff5
McKayla Washburn <mckayla@hey.com>
2024-01-29 21:35:05 +0000

    add an interaction test to InfoTooltip

--- site/src/components/InfoTooltip/InfoTooltip.stories.tsx
+++ site/src/components/InfoTooltip/InfoTooltip.stories.tsx
@@ -1,7 +1,9 @@
-import { InfoTooltip } from "./InfoTooltip";
+import { expect } from "@storybook/jest";
 import type { Meta, StoryObj } from "@storybook/react";
+import { userEvent, waitFor, within } from "@storybook/testing-library";
+import { InfoTooltip } from "./InfoTooltip";
 
-const meta: Meta<typeof InfoTooltip> = {
+const meta = {
   title: "components/InfoTooltip",
   component: InfoTooltip,
   args: {
@@ -9,23 +11,54 @@ const meta: Meta<typeof InfoTooltip> = {
     title: "Hello, friend!",
     message: "Today is a lovely day :^)",
   },
-};
+} satisfies Meta<typeof InfoTooltip>;
 
 export default meta;
 type Story = StoryObj<typeof InfoTooltip>;
 
-export const Example: Story = {};
+export const Example: Story = {
+  play: async ({ canvasElement, step }) => {
+    const screen = within(canvasElement);
 
-export const Notice: Story = {
+    await step("activate hover trigger", async () => {
+      await userEvent.hover(screen.getByRole("button"));
+      await waitFor(() =>
+        expect(screen.getByText(meta.args.message)).toBeInTheDocument(),
+      );
+    });
+  },
+};
+
+export const Notice = {
   args: {
     type: "notice",
     message: "Unfortunately, there's a radio connected to my brain",
   },
-};
+  play: async ({ canvasElement, step }) => {
+    const screen = within(canvasElement);
 
-export const Warning: Story = {
+    await step("activate hover trigger", async () => {
+      await userEvent.hover(screen.getByRole("button"));
+      await waitFor(() =>
+        expect(screen.getByText(Notice.args.message)).toBeInTheDocument(),
+      );
+    });
+  },
+} satisfies Story;
+
+export const Warning = {
   args: {
     type: "warning",
     message: "Unfortunately, there's a radio connected to my brain",
   },
-};
+  play: async ({ canvasElement, step }) => {
+    const screen = within(canvasElement);
+
+    await step("activate hover trigger", async () => {
+      await userEvent.hover(screen.getByRole("button"));
+      await waitFor(() =>
+        expect(screen.getByText(Warning.args.message)).toBeInTheDocument(),
+      );
+    });
+  },
+} satisfies Story;
