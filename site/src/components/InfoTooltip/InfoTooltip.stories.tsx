04bde63232115de6a3c44365eee6f58eeb4af957
McKayla Washburn <mckayla@hey.com>
2024-01-30 18:12:47 +0000

    chore: update storybook

--- site/src/components/InfoTooltip/InfoTooltip.stories.tsx
+++ site/src/components/InfoTooltip/InfoTooltip.stories.tsx
@@ -1,6 +1,5 @@
-import { expect } from "@storybook/jest";
 import type { Meta, StoryObj } from "@storybook/react";
-import { userEvent, waitFor, within } from "@storybook/testing-library";
+import { expect, userEvent, waitFor, within } from "@storybook/test";
 import { InfoTooltip } from "./InfoTooltip";
 
 const meta = {
