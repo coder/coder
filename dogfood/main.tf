bd270068b9385c971844759ec44910d007bed5de
Muhammad Atif Ali <atif@coder.com>
2024-01-31 16:28:12 +0300

    fixup!

--- dogfood/main.tf
+++ dogfood/main.tf
@@ -302,7 +302,7 @@ data "docker_registry_image" "dogfood" {
 }
 
 resource "docker_image" "dogfood" {
-  name = "${local.registry_name}@${data.docker_registry_image.dogfood.sha256_digest}"
+  name = "${data.coder_parameter.image_type.value}@${data.docker_registry_image.dogfood.sha256_digest}"
   pull_triggers = [
     data.docker_registry_image.dogfood.sha256_digest,
     sha1(join("", [for f in fileset(path.module, "files/*") : filesha1(f)])),
