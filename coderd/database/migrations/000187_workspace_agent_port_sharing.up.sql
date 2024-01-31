51ab8b24c8beed57b7337157a4b0f715e1cb5a18
Garrett Delfosse <garrett@coder.com>
2024-01-31 16:55:10 +0000

    template queries

--- coderd/database/migrations/000187_workspace_agent_port_sharing.up.sql
+++ coderd/database/migrations/000187_workspace_agent_port_sharing.up.sql
@@ -4,3 +4,22 @@ CREATE TABLE workspace_agent_port_sharing (
 	port integer NOT NULL,
 	share_level integer NOT NULL
 );
+
+ALTER TABLE templates ADD COLUMN max_port_sharing integer NOT NULL DEFAULT 0;
+
+-- Update the template_with_users view by recreating it.
+DROP VIEW template_with_users;
+CREATE VIEW
+    template_with_users
+AS
+    SELECT
+        templates.*,
+		coalesce(visible_users.avatar_url, '') AS created_by_avatar_url,
+		coalesce(visible_users.username, '') AS created_by_username
+    FROM
+        templates
+    LEFT JOIN
+		visible_users
+	ON
+	    templates.created_by = visible_users.id;
+COMMENT ON VIEW template_with_users IS 'Joins in the username + avatar url of the created by user.';
