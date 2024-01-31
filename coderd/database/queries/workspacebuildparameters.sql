e8d82f0127bb1bad5fcbd09765d7cb6f8ba07c78
Mathias Fredriksson <mafredri@gmail.com>
2024-01-31 13:40:17 +0200

    fix(coderd/database): fix limit in `GetUserWorkspaceBuildParameters`

--- coderd/database/queries/workspacebuildparameters.sql
+++ coderd/database/queries/workspacebuildparameters.sql
@@ -16,25 +16,28 @@ WHERE
     workspace_build_id = $1;
 
 -- name: GetUserWorkspaceBuildParameters :many
--- name: GetUserWorkspaceBuildParameters :many
-SELECT DISTINCT ON (tvp.name)
-    tvp.name,
-    wbp.value
-FROM
-    workspace_build_parameters wbp
-JOIN
-    workspace_builds wb ON wb.id = wbp.workspace_build_id
-JOIN
-    workspaces w ON w.id = wb.workspace_id
-JOIN
-    template_version_parameters tvp ON tvp.template_version_id = wb.template_version_id
-WHERE
-    w.owner_id = $1
-    AND wb.transition = 'start'
-    AND w.template_id = $2
-    AND tvp.ephemeral = false
-    AND tvp.name = wbp.name
-ORDER BY
-    tvp.name, wb.created_at DESC
+SELECT name, value
+FROM (
+    SELECT DISTINCT ON (tvp.name)
+        tvp.name,
+        wbp.value,
+        wb.created_at
+    FROM
+        workspace_build_parameters wbp
+    JOIN
+        workspace_builds wb ON wb.id = wbp.workspace_build_id
+    JOIN
+        workspaces w ON w.id = wb.workspace_id
+    JOIN
+        template_version_parameters tvp ON tvp.template_version_id = wb.template_version_id
+    WHERE
+		w.owner_id = $1
+		AND wb.transition = 'start'
+		AND w.template_id = $2
+		AND tvp.ephemeral = false
+		AND tvp.name = wbp.name
+    ORDER BY
+        tvp.name, wb.created_at DESC
+) q1
+ORDER BY created_at DESC, name
 LIMIT 100;
-
