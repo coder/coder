8435d59ff89de16cad733ce9886140522f7ae6e7
Steven Masley <stevenmasley@gmail.com>
2024-02-12 15:30:01 -0600

    add extra case

--- coderd/database/migrations/testdata/fixtures/000189_duplicate_user_links.up.sql
+++ coderd/database/migrations/testdata/fixtures/000189_duplicate_user_links.up.sql
@@ -12,3 +12,8 @@ INSERT INTO public.users(id, email, username, hashed_password, created_at, updat
 INSERT INTO public.organization_members VALUES ('fc1511ef-4fcf-4a3b-98a1-8df64160e35a', 'bb640d07-ca8a-4869-b6bc-ae61ebb2fda1', '2022-11-02 13:05:21.447595+02', '2022-11-02 13:05:21.447595+02', '{}') ON CONFLICT DO NOTHING;
 INSERT INTO public.user_links(user_id, login_type, linked_id, oauth_access_token)
 	VALUES('fc1511ef-4fcf-4a3b-98a1-8df64160e35a', 'github', '100', '');
+
+-- Additionally, there is no unique constraint on user_id. So also add another user_link for the same user.
+-- This has happened on a production database.
+INSERT INTO public.user_links(user_id, login_type, linked_id, oauth_access_token)
+VALUES('fc1511ef-4fcf-4a3b-98a1-8df64160e35a', 'oidc', 'foo', '');
