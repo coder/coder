5bb0ffcbc543fc71e48909446f015a691c5faa7c
Steven Masley <stevenmasley@gmail.com>
2024-02-15 10:26:51 -0600

    Slightly more optimized query

--- coderd/database/queries.sql.go
+++ coderd/database/queries.sql.go
@@ -3278,7 +3278,7 @@ INSERT INTO
 	organizations (id, "name", description, created_at, updated_at, is_default)
 VALUES
 	-- If no organizations exist, and this is the first, make it the default.
-	($1, $2, $3, $4, $5, (SELECT count(*) FROM organizations) = 0) RETURNING id, name, description, created_at, updated_at, is_default
+	($1, $2, $3, $4, $5, (SELECT TRUE FROM organizations LIMIT 1) IS NULL) RETURNING id, name, description, created_at, updated_at, is_default
 `
 
 type InsertOrganizationParams struct {
--- coderd/database/queries/organizations.sql
+++ coderd/database/queries/organizations.sql
@@ -42,4 +42,4 @@ INSERT INTO
 	organizations (id, "name", description, created_at, updated_at, is_default)
 VALUES
 	-- If no organizations exist, and this is the first, make it the default.
-	($1, $2, $3, $4, $5, (SELECT count(*) FROM organizations) = 0) RETURNING *;
+	($1, $2, $3, $4, $5, (SELECT TRUE FROM organizations LIMIT 1) IS NULL) RETURNING *;
