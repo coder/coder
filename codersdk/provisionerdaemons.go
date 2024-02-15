3bacc70e80ca1ef8b148b17190d4e7a8adef1e63
McKayla Washburn <mckayla@hey.com>
2024-02-15 00:08:19 +0000

    it could just all go away

--- codersdk/provisionerdaemons.go
+++ codersdk/provisionerdaemons.go
@@ -199,7 +199,6 @@ func (c *Client) ServeProvisionerDaemon(ctx context.Context, req ServeProvisione
 		return nil, xerrors.Errorf("parse url: %w", err)
 	}
 	query := serverURL.Query()
-	query.Add("version", proto.CurrentVersion.String())
 	query.Add("id", req.ID.String())
 	query.Add("name", req.Name)
 	for _, provisioner := range req.Provisioners {
