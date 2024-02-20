e517d8e1a6d0afe6b32c0c1f2ad0f20f13bd2c14
Asher <ash@coder.com>
2024-02-20 14:20:33 -0900

    Allow null on prefix after downgrade
    Without this I think the tests would fail since they are not adding a
    prefix and there is no default.

--- coderd/database/migrations/000195_oauth2_provider_codes.down.sql
+++ coderd/database/migrations/000195_oauth2_provider_codes.down.sql
@@ -7,11 +7,12 @@ DROP TABLE oauth2_provider_app_codes;
 -- It is not possible to drop enum values from enum types, so the UP on
 -- login_type has "IF NOT EXISTS".
 
--- The constraint on the secret prefix (which is used as an id embedded in the
--- secret) is dropped, but avoid completely reverting back to the previous
+-- The constraints on the secret prefix (which is used as an id embedded in the
+-- secret) are dropped, but avoid completely reverting back to the previous
 -- behavior since that will render existing secrets unusable once upgraded
 -- again.  OAuth2 is blocked outside of development mode in previous versions,
 -- so users will not be able to create broken secrets.  This is really just to
 -- make sure tests keep working (say for a bisect).
 ALTER TABLE ONLY oauth2_provider_app_secrets
-    DROP CONSTRAINT oauth2_provider_app_secrets_secret_prefix_key;
+    DROP CONSTRAINT oauth2_provider_app_secrets_secret_prefix_key,
+    ALTER COLUMN secret_prefix DROP NOT NULL;
