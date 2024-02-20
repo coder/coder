e57505d043b85d5149b17700d70dc69c18b74f97
Marcin Tojek <marcin@coder.com>
2024-02-20 13:32:42 +0100

    ts

--- site/src/utils/richParameters.ts
+++ site/src/utils/richParameters.ts
@@ -79,7 +79,7 @@ export const useValidationSchemaForRichParameters = (
                   if (Number(val) < templateParameter.validation_min) {
                     return ctx.createError({
                       path: ctx.path,
-                      message: `Value must be greater than ${templateParameter.validation_min}.`,
+                      message: takeFirstError(errorRendered(templateParameter, val), `Value must be greater than ${templateParameter.validation_min}.`),
                     });
                   }
                 } else if (
@@ -89,7 +89,7 @@ export const useValidationSchemaForRichParameters = (
                   if (templateParameter.validation_max < Number(val)) {
                     return ctx.createError({
                       path: ctx.path,
-                      message: `Value must be less than ${templateParameter.validation_max}.`,
+                      message: takeFirstError(errorRendered(templateParameter, val), `Value must be less than ${templateParameter.validation_max}.`),
                     });
                   }
                 } else if (
@@ -102,7 +102,7 @@ export const useValidationSchemaForRichParameters = (
                   ) {
                     return ctx.createError({
                       path: ctx.path,
-                      message: `Value must be between ${templateParameter.validation_min} and ${templateParameter.validation_max}.`,
+                      message: takeFirstError(errorRendered(templateParameter, val), `Value must be between ${templateParameter.validation_min} and ${templateParameter.validation_max}.`),
                     });
                   }
                 }
@@ -149,7 +149,7 @@ export const useValidationSchemaForRichParameters = (
                   if (val && !regex.test(val)) {
                     return ctx.createError({
                       path: ctx.path,
-                      message: `${templateParameter.validation_error} (value does not match the pattern ${templateParameter.validation_regex})`,
+                      message: errorRendered(templateParameter, val),
                     });
                   }
                 }
@@ -162,3 +162,25 @@ export const useValidationSchemaForRichParameters = (
     )
     .required();
 };
+
+const takeFirstError = (...errorMessages: string[]): string => {
+  for (const errorMessage of errorMessages) {
+      if (errorMessage) {
+          return errorMessage;
+      }
+  }
+  return "developer error: error message is not provided";
+};
+
+const errorRendered = (parameter: TemplateVersionParameter, value?: string): string => {
+    if (!parameter.validation_error || !value) {
+        return "";
+    }
+
+    const r = new Map<string, string>([
+        ["{min}", parameter.validation_min !== undefined ? parameter.validation_min.toString() : ""],
+        ["{max}", parameter.validation_max !== undefined ? parameter.validation_max.toString() : ""],
+        ["{value}", value]
+    ]);
+    return parameter.validation_error.replace(/{min}|{max}|{value}/g, match => r.get(match) || "");
+}
