9ad5f7cea2c2e639419ff69e09e50454f2d54ff8
McKayla Washburn <mckayla@hey.com>
2024-02-29 21:39:45 +0000

    chore: add stories for `TableEmpty` and `TableLoader`

---
+++ site/src/components/TableLoader/TableLoader.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableContainer from "@mui/material/TableContainer";
+import { TableLoader } from "./TableLoader";
+
+const meta: Meta<typeof TableLoader> = {
+  title: "components/TableLoader",
+  component: TableLoader,
+  decorators: [
+    (Story) => (
+      <TableContainer>
+        <Table>
+          <TableBody>
+            <Story />
+          </TableBody>
+        </Table>
+      </TableContainer>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof TableLoader>;
+
+export const Example: Story = {};
+export { Example as TableLoader };
