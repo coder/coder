d7110c51fd9e988bd326f656ea219405efb1a518
Parkreiner <throwawayclover@gmail.com>
2024-03-03 16:02:50 +0000

    fix: update import

--- site/src/pages/WorkspaceBuildPage/WorkspaceBuildPageView.tsx
+++ site/src/pages/WorkspaceBuildPage/WorkspaceBuildPageView.tsx
@@ -25,7 +25,7 @@ import {
 } from "modules/workspaces/WorkspaceBuild/WorkspaceBuildData";
 import { Sidebar, SidebarCaption, SidebarItem } from "./Sidebar";
 import { TAB_PADDING_X, TabLink, Tabs, TabsList } from "components/Tabs/Tabs";
-import { useTab } from "hooks";
+import { useSearchParamsKey } from "hooks/useSearchParamsKey";
 import { AgentLogs, useAgentLogs } from "modules/resources/AgentLogs";
 
 export const LOGS_TAB_KEY = "logs";
@@ -51,14 +51,17 @@ export const WorkspaceBuildPageView: FC<WorkspaceBuildPageViewProps> = ({
   activeBuildNumber,
 }) => {
   const theme = useTheme();
-  const tab = useTab(LOGS_TAB_KEY, "build");
+  const tabState = useSearchParamsKey({
+    key: LOGS_TAB_KEY,
+    defaultValue: "build",
+  });
 
   if (!build) {
     return <Loader />;
   }
 
   const agents = build.resources.flatMap((r) => r.agents ?? []);
-  const selectedAgent = agents.find((a) => a.id === tab.value);
+  const selectedAgent = agents.find((a) => a.id === tabState.value);
 
   return (
     <DashboardFullPage>
@@ -141,7 +144,7 @@ export const WorkspaceBuildPageView: FC<WorkspaceBuildPageViewProps> = ({
         </Sidebar>
 
         <div css={{ height: "100%", overflowY: "auto", width: "100%" }}>
-          <Tabs active={tab.value}>
+          <Tabs active={tabState.value}>
             <TabsList>
               <TabLink to={`?${LOGS_TAB_KEY}=build`} value="build">
                 Build
@@ -187,7 +190,7 @@ export const WorkspaceBuildPageView: FC<WorkspaceBuildPageViewProps> = ({
             </Alert>
           )}
 
-          {tab.value === "build" ? (
+          {tabState.value === "build" ? (
             <BuildLogsContent logs={logs} />
           ) : (
             <AgentLogsContent agent={selectedAgent!} />
