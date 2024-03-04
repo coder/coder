b0fabde62ccf90afea1ffe89755db7b1f578d6e5
Parkreiner <throwawayclover@gmail.com>
2024-03-04 00:54:58 +0000

    refactor: more updates for clarity

--- site/src/testHelpers/hooks.tsx
+++ site/src/testHelpers/hooks.tsx
@@ -1,11 +1,18 @@
 import {
+  type FC,
+  type PropsWithChildren,
+  type ReactNode,
+  useReducer,
+} from "react";
+
+import {
   type RenderHookOptions,
   type RenderHookResult,
   waitFor,
   renderHook,
   act,
 } from "@testing-library/react";
-import { type ReactNode, useReducer, FC, PropsWithChildren } from "react";
+
 import { QueryClient } from "react-query";
 import { AppProviders } from "App";
 import { RequireAuth } from "contexts/auth/RequireAuth";
@@ -66,14 +73,6 @@ export async function renderHookWithAuth<Result, Props>(
   render: (initialProps: Props) => Result,
   config: RenderHookWithAuthConfig<Props>,
 ): Promise<RenderHookWithAuthResult<Result, Props>> {
-  const { routingOptions = {}, renderOptions = {} } = config;
-  const {
-    path = "/",
-    route = "/",
-    extraRoutes = [],
-    nonAuthenticatedRoutes = [],
-  } = routingOptions;
-
   /**
    * Our setup here is evil, gross, and cursed because of how React Router
    * itself is set up. We need to go through RouterProvider, so that we have a
@@ -89,57 +88,35 @@ export async function renderHookWithAuth<Result, Props>(
    *    and prevent any children from re-rendering (even if they would have new
    *    values).
    *
-   * The current solution side-steps that with a Rube Goldberg approach:
-   * 1. When renderHook's wrapper renders/re-renders, store its children value
-   *    (the JSX for the shell component it uses to expose hook values) in a
-   *    separate variable
-   * 2. Create a RenderHookEscapeHatch component that has two jobs:
-   *    1. Read from the most recent child value escaped from the wrapper
-   *    2. Expose a function for manually triggering re-renders for
-   *       RenderHookEscapeHatch only (parent components will be unaffected)
-   * 3. When we call renderHook for the mounting render, RouterProvider will
-   *    be getting initialized, so it will let the render go through
-   *    1. We eject the children value outside the wrapper and catch it with the
-   *       variable.
-   *    2. RenderHookEscapeHatch renders and uses the variable for its output,
-   *       and exposes the forced re-render helper
-   *    3. We eventually get our full JSX output, and React DOM turns that into
-   *       some UI values
-   * 4. If we have no need for manual re-renders, we're done at this point. Any
-   *    re-renders done via the functions on the custom hook you're trying to
-   *    test will have no problems, and will not have re-render issues
-   *
-   * But if we re-render manually via renderHook's rerender function:
-   * 1. We grab a copy of the reference to the result.current property
-   * 2. We trigger a re-render via renderHook to make sure that we're exposing
-   *    the new re-render props to the variable
-   * 3. RouterProvider will block the re-render, so RenderHookEscapeHatch does
-   *    not produce a new value
-   * 4. We call the force re-render helper to make RenderHookEscapeHatch
-   *    re-render as a child node.
-   * 5. It reads from the variable, so it will inject the most up to date
-   *    version of the renderHook shell component JSX into its output
-   * 6. Just to be on the safe side, we wait for result.current not to equal
-   *    the snapshot we grabbed (even if the inner values are the same, the
-   *    reference values will be different), resolving that via a promise
+   * Have to do a lot of work to side-step those issues, and make sure that
+   * we're not relying on internal React Router implementation details that
+   * could break at a moment's notice
    */
-  // Easy to miss - definite assignments via !
-
-  let escapedLocation!: Location;
+  let escapedLocation!: Location; // Definite assignment via !
   const LocationLeaker: FC<PropsWithChildren> = ({ children }) => {
-    const location = useLocation();
-    escapedLocation = location;
+    escapedLocation = useLocation();
     return <>{children}</>;
   };
 
-  let forceRenderHookChildrenUpdate!: () => void;
+  let forceUpdateRenderHookChildren!: () => void; // Definite assignment via !
   let currentRenderHookChildren: ReactNode = undefined;
 
   const InitialRoute: FC = () => {
-    const [, forceInternalRerender] = useReducer((b: boolean) => !b, false);
-    forceRenderHookChildrenUpdate = () => act(() => forceInternalRerender());
+    const [, forceRerender] = useReducer((b: boolean) => !b, false);
+    forceUpdateRenderHookChildren = () => act(() => forceRerender());
     return <LocationLeaker>{currentRenderHookChildren}</LocationLeaker>;
   };
+
+  /**
+   * Start of main setup
+   */
+  const { routingOptions = {}, renderOptions = {} } = config;
+  const {
+    path = "/",
+    route = "/",
+    extraRoutes = [],
+    nonAuthenticatedRoutes = [],
+  } = routingOptions;
 
   const wrappedExtraRoutes = extraRoutes.map((route) => ({
     ...route,
@@ -180,8 +157,13 @@ export async function renderHookWithAuth<Result, Props>(
    * conditional rendering and not always rendering their children immediately.
    *
    * renderHook's result won't actually exist until the children defined via its
-   * wrapper render in full. Ignore result.current's type signature; it lies to
-   * you, which is normally a good thing (no needless null checks), but not here
+   * wrapper render in full.
+   *
+   * Ignore result.current's type signature; it lies to you. This is normally a
+   * good thing, because the renderHook result will usually evaluate
+   * synchronously, so by the time you get the result back, you won't have to
+   * worry about null checks. But because we're setting things up async,
+   * result.current will be null for at least some period of time
    */
   await waitFor(() => expect(result.current).not.toBe(null));
 
@@ -190,10 +172,10 @@ export async function renderHookWithAuth<Result, Props>(
     queryClient,
     unmount,
     rerender: (newProps) => {
-      const currentValue = result.current;
+      const resultSnapshot = result.current;
       rerender(newProps);
-      forceRenderHookChildrenUpdate();
-      return waitFor(() => expect(result.current).not.toBe(currentValue));
+      forceUpdateRenderHookChildren();
+      return waitFor(() => expect(result.current).not.toBe(resultSnapshot));
     },
     getLocationSnapshot: () => {
       return {
