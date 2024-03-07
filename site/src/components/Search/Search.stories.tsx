2975598914921b8bcfa5a83c5d3a096ee2c346d1
McKayla Washburn <mckayla@hey.com>
2024-03-07 18:57:44 +0000

    basic story

---
+++ site/src/components/Search/Search.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Search, SearchInput } from "./Search";
+
+const meta: Meta<typeof SearchInput> = {
+  title: "components/Search",
+  component: SearchInput,
+  decorators: [
+    (Story) => (
+      <Search>
+        <Story />
+      </Search>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof SearchInput>;
+
+export const Example: Story = {};
