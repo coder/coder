83af8674e83a9c6a971817a2bc3f2610bfa179b8
Bruno Quaresma <bruno@coder.com>
2024-03-11 14:10:19 -0300

    chore(site): add CreateTokenPage story (#12472)

---
+++ site/src/pages/CreateTokenPage/CreateTokenPage.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { CreateTokenPage } from "./CreateTokenPage";
+
+const meta: Meta<typeof CreateTokenPage> = {
+  title: "components/CreateTokenPage",
+  component: CreateTokenPage,
+  parameters: {
+    queries: [
+      {
+        key: ["tokenconfig"],
+        data: { max_token_lifetime: 1_000 },
+      },
+    ],
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof CreateTokenPage>;
+
+export const Default: Story = {};
