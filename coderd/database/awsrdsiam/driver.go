1ff0291c9b4ee51c2defa19b58c256d42df03cba
Garrett Delfosse <garrett@coder.com>
2024-03-14 16:06:34 +0000

    add aws v2

--- coderd/database/awsrdsiam/driver.go
+++ coderd/database/awsrdsiam/driver.go
@@ -1,28 +1,30 @@
 package awsrdsiam
 
 import (
+	"context"
 	"database/sql"
 	"database/sql/driver"
 	"fmt"
 	"net/url"
 
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/rds/rdsutils"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/rds/auth"
 	"golang.org/x/xerrors"
 )
 
 type awsRdsIamDriver struct {
 	parent driver.Driver
-	sess   *session.Session
+	cfg    aws.Config
 }
 
 var _ driver.Driver = &awsRdsIamDriver{}
 
 // Register initializes and registers our aws rds iam wrapped database driver.
-func Register(parentName string) (string, error) {
-	sess, err := session.NewSession()
+func Register(ctx context.Context, parentName string) (string, error) {
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
-		return "", xerrors.Errorf("creating aws session: %w", err)
+		return "", err
 	}
 
 	db, err := sql.Open(parentName, "")
@@ -31,7 +33,7 @@ func Register(parentName string) (string, error) {
 	}
 
 	// create a new aws rds iam driver
-	d := newDriver(db.Driver(), sess)
+	d := newDriver(db.Driver(), cfg)
 	name := fmt.Sprintf("%s-awsrdsiam", parentName)
 	sql.Register(fmt.Sprintf("%s-awsrdsiam", parentName), d)
 
@@ -39,17 +41,17 @@ func Register(parentName string) (string, error) {
 }
 
 // newDriver will create a new *AwsRdsIamDriver using the environment aws session.
-func newDriver(parentDriver driver.Driver, sess *session.Session) *awsRdsIamDriver {
+func newDriver(parentDriver driver.Driver, cfg aws.Config) *awsRdsIamDriver {
 	return &awsRdsIamDriver{
 		parent: parentDriver,
-		sess:   sess,
+		cfg:    cfg,
 	}
 }
 
 // Open creates a new connection to the database using the provided name.
 func (d *awsRdsIamDriver) Open(name string) (driver.Conn, error) {
 	// set password with signed aws authentication token for the rds instance
-	nURL, err := getAuthenticatedURL(d.sess, name)
+	nURL, err := getAuthenticatedURL(d.cfg, name)
 	if err != nil {
 		return nil, xerrors.Errorf("assigning authentication token to url: %w", err)
 	}
@@ -63,7 +65,7 @@ func (d *awsRdsIamDriver) Open(name string) (driver.Conn, error) {
 	return conn, nil
 }
 
-func getAuthenticatedURL(sess *session.Session, dbURL string) (string, error) {
+func getAuthenticatedURL(cfg aws.Config, dbURL string) (string, error) {
 	nURL, err := url.Parse(dbURL)
 	if err != nil {
 		return "", xerrors.Errorf("parsing dbURL: %w", err)
@@ -71,7 +73,7 @@ func getAuthenticatedURL(sess *session.Session, dbURL string) (string, error) {
 
 	// generate a new rds session auth tokenized URL
 	rdsEndpoint := fmt.Sprintf("%s:%s", nURL.Hostname(), nURL.Port())
-	token, err := rdsutils.BuildAuthToken(rdsEndpoint, *sess.Config.Region, nURL.User.Username(), sess.Config.Credentials)
+	token, err := auth.BuildAuthToken(context.Background(), rdsEndpoint, cfg.Region, nURL.User.Username(), cfg.Credentials)
 	if err != nil {
 		return "", xerrors.Errorf("building rds auth token: %w", err)
 	}
--- go.mod
+++ go.mod
@@ -82,7 +82,7 @@ require (
 	github.com/andybalholm/brotli v1.1.0
 	github.com/armon/circbuf v0.0.0-20190214190532-5111143e8da2
 	github.com/awalterschulze/gographviz v2.0.3+incompatible
-	github.com/aws/smithy-go v1.20.0
+	github.com/aws/smithy-go v1.20.1
 	github.com/bgentry/speakeasy v0.1.1-0.20220910012023-760eaf8b6816
 	github.com/bramvdbogaerde/go-scp v1.3.0
 	github.com/briandowns/spinner v1.18.1
@@ -248,10 +248,11 @@ require (
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/armon/go-radix v1.0.1-0.20221118154546-54df44f2176c // indirect
 	github.com/aws/aws-sdk-go v1.50.36
-	github.com/aws/aws-sdk-go-v2 v1.24.1 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.25.3 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.26.1 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.16.12 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.10 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/rds/auth v1.4.3
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.10 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.10 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.2 // indirect
