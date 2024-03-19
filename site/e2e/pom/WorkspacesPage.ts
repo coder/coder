22cb4798a922eae5f090122480307b13b96802b7
Kyle Carberry <kyle@carberry.com>
2024-03-19 17:34:33 +0000

    chore: add docs for adding e2e tests

--- site/e2e/pom/WorkspacesPage.ts
+++
@@ -1,8 +0,0 @@
-import type { Page } from "@playwright/test";
-import { BasePom } from "./BasePom";
-
-export class WorkspacesPage extends BasePom {
-  constructor(baseURL: string | undefined, page: Page, params?: string) {
-    super(baseURL, `/workspaces${params && params}`, page);
-  }
-}
