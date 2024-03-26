2cb8ccc3b83bceb8727165d6ee5d0ea0036253a4
Danny Kopping <danny@coder.com>
2024-03-26 15:30:10 +0200

    Fix duration on windows builds
    Signed-off-by: Danny Kopping <danny@coder.com>

--- testutil/duration_windows.go
+++ testutil/duration_windows.go
@@ -7,10 +7,11 @@ import "time"
 //
 // Windows durations are adjusted for slow CI workers.
 const (
-	WaitShort     = 15 * time.Second
-	WaitMedium    = 20 * time.Second
-	WaitLong      = 35 * time.Second
-	WaitSuperLong = 120 * time.Second
+	WaitSuperShort = time.Second
+	WaitShort      = 15 * time.Second
+	WaitMedium     = 20 * time.Second
+	WaitLong       = 35 * time.Second
+	WaitSuperLong  = 120 * time.Second
 )
 
 // Constants for delaying repeated operations, e.g. in
