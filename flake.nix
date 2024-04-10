7fd9a75ad9c7cba3533c6eb7010684013faacded
Steven Masley <Emyrk@users.noreply.github.com>
2024-04-10 14:08:25 -0500

    chore: nix shell to support playwright e2e tests (#12917)
    * chore: nix shell to support playwright e2e tests
    
    nix is running an older version of chromium, so had to reduce the
    playwright version.
    
    * Add to e2e readme
    
    * add enterprise test comment
    
    * add note about install to readme
    
    * make fmt
    
    * remove shellhook message
    
    Co-authored-by: Kayla Washburn-Love <mckayla@hey.com>
    
    * add link to nixos playwright package to get version
    
    * formatting
    
    ---------
    
    Co-authored-by: Kayla Washburn-Love <mckayla@hey.com>

--- flake.nix
+++ flake.nix
@@ -58,6 +58,7 @@
           pango
           pixman
           pkg-config
+          playwright-driver.browsers
           postgresql_13
           protobuf
           protoc-gen-go
@@ -86,7 +87,13 @@
       in
       {
         defaultPackage = formatter; # or replace it with your desired default package.
-        devShell = pkgs.mkShell { buildInputs = devShellPackages; };
+        devShell = pkgs.mkShell {
+        buildInputs = devShellPackages;
+        shellHook = ''
+          export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+          export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+        '';
+        };
         packages.all = allPackages;
       }
     );
