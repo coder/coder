cfc78f22dc415ea550cc66888f7d6a779e941f99
Steven Masley <stevenmasley@gmail.com>
2024-04-10 10:25:01 -0500

    remove excess error return

--- coderd/coderdtest/oidctest/idp_test.go
+++ coderd/coderdtest/oidctest/idp_test.go
@@ -54,12 +54,11 @@ func TestFakeIDPBasicFlow(t *testing.T) {
 		token = oauthToken
 	})
 
-	resp, err := fake.OIDCCallback(t, expectedState, jwt.MapClaims{})
-	require.NoError(t, err)
+	resp := fake.OIDCCallback(t, expectedState, jwt.MapClaims{})
 	require.Equal(t, http.StatusOK, resp.StatusCode)
 
 	// Test the user info
-	_, err = cfg.Provider.UserInfo(ctx, oauth2.StaticTokenSource(token))
+	_, err := cfg.Provider.UserInfo(ctx, oauth2.StaticTokenSource(token))
 	require.NoError(t, err)
 
 	// Now test it can refresh
--- coderd/externalauth/externalauth_test.go
+++ coderd/externalauth/externalauth_test.go
@@ -486,9 +486,9 @@ func TestConstantQueryParams(t *testing.T) {
 
 	// Actually run an auth request. Although it says OIDC, the flow is the same
 	// for oauth2.
-	cli, resp := fake.OIDCCallback(t, state, jwt.MapClaims{})
+	resp := fake.OIDCCallback(t, state, jwt.MapClaims{})
 	require.True(t, callbackCalled)
-
+	require.Equal(t, http.StatusOK, resp.StatusCode)
 }
 
 type testConfig struct {
