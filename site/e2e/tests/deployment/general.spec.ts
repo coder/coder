d5a8a9183c130435bd55bc60a5a3607f8bd4ec86
Marcin Tojek <marcin@coder.com>
2024-04-11 18:18:52 +0200

    fmt

--- site/e2e/tests/deployment/general.spec.ts
+++ site/e2e/tests/deployment/general.spec.ts
@@ -18,8 +18,16 @@ test("experiments", async ({ page }) => {
   await expect(experimentsLocator).toBeVisible();
 
   // Firstly, check if all enabled experiments are listed
-  expect(experimentsLocator.locator(`li.option-array-item-${e2eFakeExperiment1}.option-enabled`)).toBeVisible;
-  expect(experimentsLocator.locator(`li.option-array-item-${e2eFakeExperiment2}.option-enabled`)).toBeVisible;
+  expect(
+    experimentsLocator.locator(
+      `li.option-array-item-${e2eFakeExperiment1}.option-enabled`,
+    ),
+  ).toBeVisible;
+  expect(
+    experimentsLocator.locator(
+      `li.option-array-item-${e2eFakeExperiment2}.option-enabled`,
+    ),
+  ).toBeVisible;
 
   // Secondly, check if available experiments are listed
   for (const experiment of availableExperiments.safe) {
