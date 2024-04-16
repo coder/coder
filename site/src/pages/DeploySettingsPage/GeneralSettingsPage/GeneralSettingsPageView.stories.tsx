acb0d039b20ab821fc7bda72b6d7a20ffeb509bf
Danny Kopping <danny@coder.com>
2024-04-16 12:06:29 +0200

    Add storybook test
    Signed-off-by: Danny Kopping <danny@coder.com>

--- site/src/pages/DeploySettingsPage/GeneralSettingsPage/GeneralSettingsPageView.stories.tsx
+++ site/src/pages/DeploySettingsPage/GeneralSettingsPage/GeneralSettingsPageView.stories.tsx
@@ -5,6 +5,7 @@ import {
   MockEntitlementsWithUserLimit,
 } from "testHelpers/entities";
 import { GeneralSettingsPageView } from "./GeneralSettingsPageView";
+import {Experiments} from "../../../api/typesGenerated";
 
 const meta: Meta<typeof GeneralSettingsPageView> = {
   title: "pages/DeploySettingsPage/GeneralSettingsPageView",
@@ -103,6 +104,43 @@ export const allExperimentsEnabled: Story = {
         hidden: false,
       },
     ],
-    safeExperiments: [],
+    safeExperiments: ["shared-ports"],
+    enabledExperiments: ["shared-ports"],
+  },
+};
+
+export const invalidExperimentsEnabled: Story = {
+  args: {
+    deploymentOptions: [
+      {
+        name: "Access URL",
+        description:
+          "The URL that users will use to access the Coder deployment.",
+        flag: "access-url",
+        flag_shorthand: "",
+        value: "https://dev.coder.com",
+        hidden: false,
+      },
+      {
+        name: "Wildcard Access URL",
+        description:
+          'Specifies the wildcard hostname to use for workspace applications in the form "*.example.com".',
+        flag: "wildcard-access-url",
+        flag_shorthand: "",
+        value: "*--apps.dev.coder.com",
+        hidden: false,
+      },
+      {
+        name: "Experiments",
+        description:
+          "Enable one or more experiments. These are not ready for production. Separate multiple experiments with commas, or enter '*' to opt-in to all available experiments.",
+        flag: "experiments",
+        value: ["invalid", "*"],
+        flag_shorthand: "",
+        hidden: false,
+      },
+    ],
+    safeExperiments: ["shared-ports"],
+    enabledExperiments: ["invalid"],
   },
 };
