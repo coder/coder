b85d5d84919bc9552d30d264c232f8cc401dfca6
Danny Kopping <danny@coder.com>
2024-04-17 16:59:31 +0200

    feat: add warning about use of old/removed/invalid experiments (#12962)

--- site/src/pages/DeploySettingsPage/GeneralSettingsPage/GeneralSettingsPage.tsx
+++ site/src/pages/DeploySettingsPage/GeneralSettingsPage/GeneralSettingsPage.tsx
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet-async";
 import { useQuery } from "react-query";
 import { deploymentDAUs } from "api/queries/deployment";
 import { entitlements } from "api/queries/entitlements";
-import { availableExperiments } from "api/queries/experiments";
+import { availableExperiments, experiments } from "api/queries/experiments";
 import { pageTitle } from "utils/page";
 import { useDeploySettings } from "../DeploySettingsLayout";
 import { GeneralSettingsPageView } from "./GeneralSettingsPageView";
@@ -12,7 +12,14 @@ const GeneralSettingsPage: FC = () => {
   const { deploymentValues } = useDeploySettings();
   const deploymentDAUsQuery = useQuery(deploymentDAUs());
   const entitlementsQuery = useQuery(entitlements());
-  const experimentsQuery = useQuery(availableExperiments());
+  const enabledExperimentsQuery = useQuery(experiments());
+  const safeExperimentsQuery = useQuery(availableExperiments());
+
+  const safeExperiments = safeExperimentsQuery.data?.safe ?? [];
+  const invalidExperiments =
+    enabledExperimentsQuery.data?.filter((exp) => {
+      return !safeExperiments.includes(exp);
+    }) ?? [];
 
   return (
     <>
@@ -24,7 +31,8 @@ const GeneralSettingsPage: FC = () => {
         deploymentDAUs={deploymentDAUsQuery.data}
         deploymentDAUsError={deploymentDAUsQuery.error}
         entitlements={entitlementsQuery.data}
-        safeExperiments={experimentsQuery.data?.safe ?? []}
+        invalidExperiments={invalidExperiments}
+        safeExperiments={safeExperiments}
       />
     </>
   );
