b9285cb078e0a21fdbf00f1b71d2016caa184ddc
Marcin Tojek <marcin@coder.com>
2024-04-19 11:50:52 +0200

    e2e workspace proxy

--- site/e2e/tests/deployment/workspaceProxies.spec.ts
+++ site/e2e/tests/deployment/workspaceProxies.spec.ts
@@ -1,14 +1,26 @@
-import { test } from "@playwright/test";
-import {
-  setupApiCalls,
-} from "../../api";
+import { test, expect } from "@playwright/test";
+import { setupApiCalls } from "../../api";
+import { coderPort } from "../../constants";
+import { requiresEnterpriseLicense } from "../../helpers";
 
-test("workspace proxies configuration", async ({ page }) => {
+test("default proxy is online", async ({ page }) => {
+  requiresEnterpriseLicense();
+
   await setupApiCalls(page);
 
   await page.goto("/deployment/workspace-proxies", {
     waitUntil: "domcontentloaded",
   });
 
-  await page.pause();
+  const workspaceProxyPrimary = page.locator(
+    `table.MuiTable-root tr[data-testid="primary"]`,
+  );
+
+  const workspaceProxyName = workspaceProxyPrimary.locator("td.name span");
+  const workspaceProxyURL = workspaceProxyPrimary.locator("td.url");
+  const workspaceProxyStatus = workspaceProxyPrimary.locator("td.status span");
+
+  await expect(workspaceProxyName).toHaveText("Default");
+  await expect(workspaceProxyURL).toHaveText("http://localhost:" + coderPort);
+  await expect(workspaceProxyStatus).toHaveText("Healthy");
 });
