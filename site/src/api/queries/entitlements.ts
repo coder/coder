43cb86bd4375a7456b845c8d983081ff7bf106ff
Kyle Carberry <kyle@carberry.com>
2024-04-22 18:40:21 +0000

    Add cached query for entitlements and experiments

--- site/src/api/queries/entitlements.ts
+++ site/src/api/queries/entitlements.ts
@@ -2,12 +2,14 @@ import type { QueryClient, UseQueryOptions } from "react-query";
 import * as API from "api/api";
 import type { Entitlements } from "api/typesGenerated";
 import { getMetadataAsJSON } from "utils/metadata";
+import { cachedQuery } from "./util"
 
 const initialEntitlementsData = getMetadataAsJSON<Entitlements>("entitlements");
 const ENTITLEMENTS_QUERY_KEY = ["entitlements"] as const;
 
 export const entitlements = (): UseQueryOptions<Entitlements> => {
   return {
+    ...cachedQuery,
     queryKey: ENTITLEMENTS_QUERY_KEY,
     queryFn: () => API.getEntitlements(),
     initialData: initialEntitlementsData,
--- site/src/api/queries/experiments.ts
+++ site/src/api/queries/experiments.ts
@@ -2,12 +2,14 @@ import type { UseQueryOptions } from "react-query";
 import * as API from "api/api";
 import type { Experiments } from "api/typesGenerated";
 import { getMetadataAsJSON } from "utils/metadata";
+import { cachedQuery } from "./util"
 
 const initialExperimentsData = getMetadataAsJSON<Experiments>("experiments");
 const experimentsKey = ["experiments"] as const;
 
 export const experiments = (): UseQueryOptions<Experiments> => {
   return {
+    ...cachedQuery,
     queryKey: experimentsKey,
     initialData: initialExperimentsData,
     queryFn: () => API.getExperiments(),
