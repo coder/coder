25ad857a348377d26c89778e36536c32b3d0598a
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2024-04-26 12:55:58 +0000

    fix(site): fix template schedule options

---
+++ site/src/components/StackLabel/StackLabel.tsx
@@ -0,0 +1,31 @@
+import FormHelperText, {
+  type FormHelperTextProps,
+} from "@mui/material/FormHelperText";
+import type { ComponentProps, FC } from "react";
+import { Stack } from "components/Stack/Stack";
+
+/**
+ * Use these components as the label in FormControlLabel when implementing radio
+ * buttons, checkboxes, or switches to ensure proper styling.
+ */
+
+export const StackLabel: FC<ComponentProps<typeof Stack>> = (props) => {
+  return (
+    <Stack
+      spacing={0.5}
+      css={{ paddingLeft: 12, fontWeight: 500 }}
+      {...props}
+    />
+  );
+};
+
+export const StackLabelHelperText: FC<FormHelperTextProps> = (props) => {
+  return (
+    <FormHelperText
+      css={{
+        marginTop: 0,
+      }}
+      {...props}
+    />
+  );
+};
