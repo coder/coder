614dcf1d994dfb48d298e0c1f09e6a8687814d27
Garrett Delfosse <garrett@coder.com>
2024-05-02 20:57:38 +0000

    fix: remove mention of protocol lag

--- site/src/modules/resources/PortForwardButton.tsx
+++ site/src/modules/resources/PortForwardButton.tsx
@@ -402,7 +402,7 @@ export const PortForwardPopoverView: FC<PortForwardPopoverViewProps> = ({
         <HelpTooltipTitle>Shared Ports</HelpTooltipTitle>
         <HelpTooltipText css={{ color: theme.palette.text.secondary }}>
           {canSharePorts
-            ? "Ports can be shared with other Coder users or with the public. Changing the protocol may take up to 1 minute to take effect."
+            ? "Ports can be shared with other Coder users or with the public."
             : "This workspace template does not allow sharing ports. Contact a template administrator to enable port sharing."}
         </HelpTooltipText>
         {canSharePorts && (
