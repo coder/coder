6545327f981b5c12c2b03a5d2c18b805e5ad62d3
Parkreiner <throwawayclover@gmail.com>
2024-05-03 19:00:06 +0000

    refactor: rename test file

--- site/e2e/tests/signOut.spec.ts
+++ site/e2e/tests/signOut.spec.ts
@@ -5,19 +5,24 @@ import { setupApiCalls } from "../api";
 import { assertNoUncaughtRuntimeError } from "../helpers";
 import { beforeCoderTest } from "../hooks";
 
-const applicationName = getApplicationName();
 test.beforeEach(async ({ page }) => await beforeCoderTest(page));
 
 // Test assumes that global setup will automatically handle the sign in process
 test("Signing out", async ({ page, baseURL }) => {
   await setupApiCalls(page);
 
-  const dropdown = page.getByRole("button", { name: accessibleDropdownLabel });
+  const name = new RegExp(accessibleDropdownLabel);
+  const dropdown = page.getByRole("button", { name });
+  await expect(dropdown).toBeAttached({
+    timeout: 10_000,
+  });
+
   await dropdown.click();
 
   const signOutButton = page.getByRole("button", { name: /Sign Out/ });
   await signOutButton.click();
 
+  const applicationName = getApplicationName();
   await expect(page).toHaveTitle(`Sign in to ${applicationName}`);
   await expect(page).toHaveURL(`${baseURL}/login`);
 
