b20c63c1852d1abd597c06d8a0e1d6a87f29ca88
recanman <29310982+recanman@users.noreply.github.com>
2024-05-03 14:09:23 -0700

    fix: install openrc service on alpine (#12294) (#12870)
    * fix: install openrc service on alpine (#12294)
    
    * fmt
    
    ---------
    
    Co-authored-by: Kyle Carberry <kyle@coder.com>

--- scripts/package.sh
+++ scripts/package.sh
@@ -89,9 +89,16 @@ ln "$(realpath scripts/linux-pkg/coder.service)" "$temp_dir/"
 ln "$(realpath scripts/linux-pkg/nfpm.yaml)" "$temp_dir/"
 ln "$(realpath scripts/linux-pkg/preinstall.sh)" "$temp_dir/"
 
+nfpm_config_file="nfpm.yaml"
+
+# Use nfpm-alpine.yaml when building for Alpine (OpenRC).
+if [[ "$format" == "apk" ]]; then
+	nfpm_config_file="nfpm-alpine.yaml"
+fi
+
 pushd "$temp_dir"
 GOARCH="$arch" CODER_VERSION="$version" nfpm package \
-	-f nfpm.yaml \
+	-f "$nfpm_config_file" \
 	-p "$format" \
 	-t "$output_path" \
 	1>&2
