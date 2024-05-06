83c3691a843db284b75f53b5e3a3eb444b109162
Parkreiner <throwawayclover@gmail.com>
2024-05-06 12:33:01 +0000

    wip: commit current changes

--- site/e2e/tests/signOut.spec.ts
+++ site/e2e/tests/signOut.spec.ts
@@ -11,18 +11,13 @@ test.beforeEach(async ({ page }) => await beforeCoderTest(page));
 // Test assumes that global setup will automatically handle the sign in process
 test("Signing out", async ({ page, baseURL }) => {
   await setupApiCalls(page);
-  await page.goto("/workspaces");
+  await page.goto("/");
 
   const dropdownName = new RegExp(accessibleDropdownLabel);
   const dropdown = page.getByRole("button", { name: dropdownName });
   await dropdown.click();
 
-  const signOutOption = page.getByRole("menuitem", {
-    name: Language.signOutLabel,
-  });
-
-  await expect(signOutOption).toBeAttached({ timeout: 2_000 });
-
+  const signOutOption = page.getByText(Language.signOutLabel);
   await signOutOption.click();
 
   const applicationName = getApplicationName();
