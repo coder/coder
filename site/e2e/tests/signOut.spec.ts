5323d4a600a6997d5e4c0f899c2fd9cee0d8c075
Parkreiner <throwawayclover@gmail.com>
2024-05-06 16:20:48 +0000

    chore: finish test

--- site/e2e/tests/signOut.spec.ts
+++ site/e2e/tests/signOut.spec.ts
@@ -1,14 +1,13 @@
 import { test, expect } from "@playwright/test";
 import { accessibleDropdownLabel } from "modules/dashboard/Navbar/UserDropdown/UserDropdown";
 import { Language } from "modules/dashboard/Navbar/UserDropdown/UserDropdownContent";
-import { setupApiCalls } from "../api";
 import * as constants from "../constants";
 import { assertNoUncaughtRuntimeError } from "../helpers";
 import { beforeCoderTest } from "../hooks";
 
 test.beforeEach(async ({ page }) => await beforeCoderTest(page));
 
-test("Signing out", async ({ page, baseURL }) => {
+test("Signing in and out", async ({ page, baseURL }) => {
   const handleSignIn = async () => {
     await page.goto(`${baseURL}/login`, { waitUntil: "domcontentloaded" });
 
@@ -32,7 +31,15 @@ test("Signing out", async ({ page, baseURL }) => {
     const dropdown = page.getByRole("button", { name: dropdownName });
     await dropdown.click();
 
-    const signOutOption = page.getByText(Language.signOutLabel);
+    const signOutOption = page.getByRole("menuitem", {
+      name: Language.signOutLabel,
+
+      // Need to set to true because MUI automatically hides non-focused menu
+      // items from screen readers, and trying to tab through everything felt
+      // like it could get even more flaky
+      includeHidden: true,
+    });
+
     await signOutOption.click();
 
     await expect(page).toHaveTitle(/^Sign in to /);
@@ -40,14 +47,13 @@ test("Signing out", async ({ page, baseURL }) => {
     expect(atLoginPage).toBe(true);
 
     /**
-     * 2024-05-02 - Adding this to assert that we can't have regressions around
+     * 2024-05-06 - Adding this to assert that we can't have regressions around
      * the log out flow after it was fixed.
      * @see {@link https://github.com/coder/coder/issues/13130}
      */
     await assertNoUncaughtRuntimeError(page);
   };
 
-  await setupApiCalls(page);
   await handleSignIn();
   await handleSignOut();
 });
