038bcfb69e543d742a53b06046241d7496f31c67
Parkreiner <throwawayclover@gmail.com>
2024-05-06 17:47:51 +0000

    revert: remove potentially-unauthenticated API call

--- site/e2e/tests/signIn.spec.ts
+++ site/e2e/tests/signIn.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from "@playwright/test";
-import { logout } from "api/api";
 import { accessibleDropdownLabel } from "modules/dashboard/Navbar/UserDropdown/UserDropdown";
 import { Language } from "modules/dashboard/Navbar/UserDropdown/UserDropdownContent";
 import { setupApiCalls } from "../api";
@@ -11,10 +10,11 @@ test.beforeEach(async ({ page }) => await beforeCoderTest(page));
 
 test("Signing in and out", async ({ page, baseURL }) => {
   const handleSignIn = async () => {
-    // Make sure that if there is any user data that wasn't cleaned up from
-    // previous tests, it is immediately removed
-    await logout();
     await page.goto(`${baseURL}/login`, { waitUntil: "domcontentloaded" });
+
+    if (!page.url().startsWith(`${baseURL}/login`)) {
+      return;
+    }
 
     const emailField = page.getByRole("textbox", { name: /Email/ });
     const passwordField = page.getByRole("textbox", { name: /Password/ });
