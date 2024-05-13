c7b1889537d6f3ae226ab9617dc29dde887114bd
McKayla Washburn <mckayla@hey.com>
2024-05-13 18:49:02 +0000

    chore: add setting to enabled multi-organization ui

--- site/src/pages/UserSettingsPage/AccountPage/AccountPage.tsx
+++ site/src/pages/UserSettingsPage/AccountPage/AccountPage.tsx
@@ -1,4 +1,5 @@
-import type { FC } from "react";
+import Button from "@mui/material/Button";
+import { type FC, useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { groupsForUser } from "api/queries/groups";
 import { Stack } from "components/Stack/Stack";
@@ -8,18 +9,34 @@ import { useDashboard } from "modules/dashboard/useDashboard";
 import { Section } from "../Section";
 import { AccountForm } from "./AccountForm";
 import { AccountUserGroups } from "./AccountUserGroups";
+import { DisabledBadge, EnabledBadge } from "components/Badges/Badges";
 
 export const AccountPage: FC = () => {
   const { user: me, permissions, organizationId } = useAuthenticated();
   const { updateProfile, updateProfileError, isUpdatingProfile } =
     useAuthContext();
-  const { entitlements } = useDashboard();
+  const { entitlements, experiments } = useDashboard();
 
   const hasGroupsFeature = entitlements.features.user_role_management.enabled;
   const groupsQuery = useQuery({
     ...groupsForUser(organizationId, me.id),
     enabled: hasGroupsFeature,
   });
+
+  const multiOrgExperimentEnabled = experiments.includes("multi-organization");
+  const [multiOrgUiEnabled, setMultiOrgUiEnabled] = useState(
+    () =>
+      multiOrgExperimentEnabled &&
+      Boolean(localStorage.getItem("enableMultiOrganizationUi")),
+  );
+
+  useEffect(() => {
+    if (multiOrgUiEnabled) {
+      localStorage.setItem("enableMultiOrganizationUi", "true");
+    } else {
+      localStorage.removeItem("enableMultiOrganizationUi");
+    }
+  }, [multiOrgUiEnabled]);
 
   return (
     <Stack spacing={6}>
@@ -40,6 +57,23 @@ export const AccountPage: FC = () => {
           loading={groupsQuery.isLoading}
           error={groupsQuery.error}
         />
+      )}
+
+      {multiOrgExperimentEnabled && (
+        <Section
+          title="Organizations"
+          description={
+            <span>Danger: enabling will break things in the UI.</span>
+          }
+        >
+          <Stack>
+            {multiOrgUiEnabled ? <EnabledBadge /> : <DisabledBadge />}
+            <Button onClick={() => setMultiOrgUiEnabled((enabled) => !enabled)}>
+              {multiOrgUiEnabled ? "Disable" : "Enable"} frontend
+              multi-organization support
+            </Button>
+          </Stack>
+        </Section>
       )}
     </Stack>
   );
