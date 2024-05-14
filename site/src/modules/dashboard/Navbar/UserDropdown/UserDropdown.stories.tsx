af0c1263298e2cc9860539c7827488b0abcad7bb
McKayla Washburn <mckayla@hey.com>
2024-05-14 23:37:49 +0000

    🧹

--- site/src/modules/dashboard/Navbar/UserDropdown/UserDropdown.stories.tsx
+++ site/src/modules/dashboard/Navbar/UserDropdown/UserDropdown.stories.tsx
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { expect, screen, userEvent, within, waitFor } from "@storybook/test";
 import { MockBuildInfo, MockUser } from "testHelpers/entities";
-import { UserDropdown } from "./UserDropdown";
 import { withDashboardProvider } from "testHelpers/storybook";
+import { UserDropdown } from "./UserDropdown";
 
 const meta: Meta<typeof UserDropdown> = {
   title: "modules/dashboard/UserDropdown",
