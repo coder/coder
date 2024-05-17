c3221cec8121167949de9565ba9e98c3adc85be1
Mathias Fredriksson <mafredri@gmail.com>
2024-05-17 11:25:10 +0300

    chore(scripts): fix stable release promote script (#13204)
    (cherry picked from commit f66d0445da659cda503135faa27f7b6b82186c5f)

--- scripts/release_promote_stable.sh
+++ scripts/release_promote_stable.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 # shellcheck source=scripts/lib.sh
 source "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
 
+# Make sure GITHUB_TOKEN is set for the release command.
+gh_auth
+
 # This script is a convenience wrapper around the release promote command.
 #
 # Sed hack to make help text look like this script.
