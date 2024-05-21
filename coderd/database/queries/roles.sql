95a8931fc8e763bc39e38088d57c167311874f1e
Steven Masley <stevenmasley@gmail.com>
2024-05-21 11:33:57 -1000

    cleanup

--- coderd/database/queries/roles.sql
+++ coderd/database/queries/roles.sql
@@ -16,7 +16,6 @@ WHERE
 	organization_id IS null
 	ELSE true
   END
-  -- Org scoping filter, to only fetch site wide roles
   AND CASE WHEN @organization_id :: uuid != '00000000-0000-0000-0000-000000000000'::uuid  THEN
       organization_id = @organization_id
     ELSE true
