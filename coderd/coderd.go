4fd057e0e5f1a755cf867c956c057dca52f1e15d
Marcin Tojek <marcin@coder.com>
2024-06-04 12:41:12 +0200

    fix lint

--- coderd/coderd.go
+++ coderd/coderd.go
@@ -1399,13 +1399,12 @@ func (api *API) CreateInMemoryTaggedProvisionerDaemon(dialCtx context.Context, n
 	}
 
 	//nolint:gocritic // in-memory provisioners are owned by system
-	tags := provisionersdk.MutateTags(uuid.Nil, provisionerTags)
 	daemon, err := api.Database.UpsertProvisionerDaemon(dbauthz.AsSystemRestricted(dialCtx), database.UpsertProvisionerDaemonParams{
 		Name:           name,
 		OrganizationID: defaultOrg.ID,
 		CreatedAt:      dbtime.Now(),
 		Provisioners:   dbTypes,
-		Tags:           tags,
+		Tags:           provisionersdk.MutateTags(uuid.Nil, provisionerTags),
 		LastSeenAt:     sql.NullTime{Time: dbtime.Now(), Valid: true},
 		Version:        buildinfo.Version(),
 		APIVersion:     proto.CurrentVersion.String(),
