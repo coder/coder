cf03f6b99e0da02f052e43dd9a841395102295c4
Danny Kopping <danny@coder.com>
2024-06-11 06:24:19 +0200

    feat: database tables & queries

---
+++ coderd/database/migrations/000219_notifications.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS notification_messages;
+DROP TABLE IF EXISTS notification_templates;
+DROP TYPE IF EXISTS notification_receiver;
+DROP TYPE IF EXISTS notification_method;
+DROP TYPE IF EXISTS notification_message_status;
