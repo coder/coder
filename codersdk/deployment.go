36698c52f825600bea548019b95dd7cd14c416d9
Danny Kopping <danny@coder.com>
2024-07-01 12:24:23 +0200

    Enable experiment as safe
    Signed-off-by: Danny Kopping <danny@coder.com>

--- codersdk/deployment.go
+++ codersdk/deployment.go
@@ -2473,7 +2473,7 @@ const (
 // users to opt-in to via --experimental='*'.
 // Experiments that are not ready for consumption by all users should
 // not be included here and will be essentially hidden.
-var ExperimentsAll = Experiments{}
+var ExperimentsAll = Experiments{ExperimentNotifications}
 
 // Experiments is a list of experiments.
 // Multiple experiments may be enabled at the same time.
