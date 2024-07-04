853e5b8f0fe4bd01d95047f9ffc872f935303e98
Jon Ayers <jon@coder.com>
2024-07-04 00:18:22 +0000

    feat: add meticulous recorder

--- site/src/index.tsx
+++ site/src/index.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { tryLoadAndStartRecorder } from "@alwaysmeticulous/recorder-loader";
 import { App } from "./App";
 
 console.info(`    ▄█▀    ▀█▄
@@ -12,5 +13,28 @@ const element = document.getElementById("root");
 if (element === null) {
   throw new Error("root element is null");
 }
+
 const root = createRoot(element);
-root.render(<App />);
+async function startApp() {
+  // Record all sessions on localhost, staging stacks and preview URLs
+  if (isInternal()) {
+    // Start the Meticulous recorder before you initialise your app.
+    // Note: all errors are caught and logged, so no need to surround with try/catch
+    await tryLoadAndStartRecorder({
+      projectId: "Y4uHy1qs0B660xxUdrkLPkazUMPr6OuTqYEnShaR",
+      isProduction: false,
+    });
+  }
+
+  root.render(<App />);
+}
+
+function isInternal() {
+  return (
+    window.location.hostname.indexOf("dev.coder.com") > -1 ||
+    window.location.hostname.indexOf("localhost") > -1 ||
+    window.location.hostname.indexOf("127.0.0.1") > -1
+  );
+}
+
+startApp();
