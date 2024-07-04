6a04efe26980dca8e50c9677c23a770bab31db0a
Cian Johnston <cian@coder.com>
2024-07-04 10:16:43 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -97,7 +97,7 @@
             name = "coder-${osArch}";
             # Updated with ./scripts/update-flake.sh`.
             # This should be updated whenever go.mod changes!
-            vendorHash = "sha256-e0L6osJwG0EF0M3TefxaAjDvN4jvQHxTGEUEECNO1Vw=";
+            vendorHash = "sha256-WGKzJYRRvviG8R0zdpv7V4Ae0J3MwuBJKLpi6iYOiZo=";
             proxyVendor = true;
             src = ./.;
             nativeBuildInputs = with pkgs; [ getopt openssl zstd ];
