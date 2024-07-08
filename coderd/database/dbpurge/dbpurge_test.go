42f4bffc6fbdb051c74969c99f8be8f169a78bbf
Marcin Tojek <marcin@coder.com>
2024-07-08 12:12:06 +0200

    chore: refactor AgentHasNotConnectedSinceWeek_LogsExpired

--- coderd/database/dbpurge/dbpurge_test.go
+++ coderd/database/dbpurge/dbpurge_test.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 	"golang.org/x/exp/slices"
@@ -183,20 +184,28 @@ func TestDeleteOldWorkspaceAgentLogs(t *testing.T) {
 		// given
 		agent := mustCreateAgentWithLogs(ctx, t, db, user, org, tmpl, tv, now.Add(-8*24*time.Hour), t.Name())
 
+		// Make sure that agent logs have been collected.
+		agentLogs, err := db.GetWorkspaceAgentLogsAfter(ctx, database.GetWorkspaceAgentLogsAfterParams{
+			AgentID: agent,
+		})
+		require.NoError(t, err)
+		require.NotZero(t, agentLogs, "agent logs must be present")
+
 		// when
 		closer := dbpurge.New(ctx, logger, db)
 		defer closer.Close()
 
 		// then
-		require.Eventually(t, func() bool {
-			agentLogs, err := db.GetWorkspaceAgentLogsAfter(ctx, database.GetWorkspaceAgentLogsAfterParams{
+		assert.Eventually(t, func() bool {
+			agentLogs, err = db.GetWorkspaceAgentLogsAfter(ctx, database.GetWorkspaceAgentLogsAfterParams{
 				AgentID: agent,
 			})
 			if err != nil {
-				return false
+				require.FailNow(t, "unable to get agent logs: %w", err)
 			}
 			return !containsAgentLog(agentLogs, t.Name())
 		}, testutil.WaitShort, testutil.IntervalFast)
+		require.NotContains(t, agentLogs, t.Name())
 	})
 
 	t.Run("AgentConnectedSixDaysAgo_LogsValid", func(t *testing.T) {
