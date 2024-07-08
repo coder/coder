6b6b32c5861c8d358caa01128b5712ca90fd4cb4
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2024-07-08 20:23:32 +0000

    Add e2e tests

---
+++ site/e2e/tests/workspaces/autoCreateWorkspace.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect } from "@playwright/test";
+import { username } from "../../constants";
+import {
+  createTemplate,
+  createWorkspace,
+  echoResponsesWithParameters,
+} from "../../helpers";
+import { emptyParameter } from "../../parameters";
+import type { RichParameter } from "../../provisionerGenerated";
+
+test("create workspace in auto mode", async ({ page }) => {
+  const richParameters: RichParameter[] = [
+    { ...emptyParameter, name: "repo", type: "string" },
+  ];
+  const template = await createTemplate(
+    page,
+    echoResponsesWithParameters(richParameters),
+  );
+  const name = "test-workspace";
+  await page.goto(
+    `/templates/${template}/workspace?mode=auto&param.repo=example&name=${name}`,
+    {
+      waitUntil: "domcontentloaded",
+    },
+  );
+  await expect(page).toHaveTitle(`${username}/${name} - Coder`);
+});
+
+test("use an existing workspace that matches the `match` parameter instead of creating a new one", async ({
+  page,
+}) => {
+  const richParameters: RichParameter[] = [
+    { ...emptyParameter, name: "repo", type: "string" },
+  ];
+  const template = await createTemplate(
+    page,
+    echoResponsesWithParameters(richParameters),
+  );
+  const prevWorkspace = await createWorkspace(page, template);
+  await page.goto(
+    `/templates/${template}/workspace?mode=auto&param.repo=example&name=new-name&match=name:${prevWorkspace}`,
+    {
+      waitUntil: "domcontentloaded",
+    },
+  );
+  await expect(page).toHaveTitle(`${username}/${prevWorkspace} - Coder`);
+});
