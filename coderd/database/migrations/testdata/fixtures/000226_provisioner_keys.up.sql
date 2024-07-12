efcf83221a713e010a02c629939879e961d1234f
Garrett Delfosse <garrett@coder.com>
2024-07-12 20:31:36 +0000

    add fixture

---
+++ coderd/database/migrations/testdata/fixtures/000226_provisioner_keys.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO provisioner_keys
+    (id, created_at, organization_id, name, hashed_secret)
+VALUES
+    ('b90547be-8870-4d68-8184-e8b2242b7c01', '2021-06-01 00:00:00', 'b90547be-8870-4d68-8184-e8b2242b7c01', 'qua', '\xDEADBEEF'::bytea);
