4c7b85f800df7d7453d33ad20c548e4558400cec
Jon Ayers <jon@coder.com>
2024-07-16 17:00:16 +0000

    lint

--- site/src/index.tsx
+++ site/src/index.tsx
@@ -1,5 +1,5 @@
-import { createRoot } from "react-dom/client";
 import { tryLoadAndStartRecorder } from "@alwaysmeticulous/recorder-loader";
+import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
 console.info(`    ▄█▀    ▀█▄
@@ -33,8 +33,11 @@ function isInternal() {
   return (
     window.location.hostname.indexOf("dev.coder.com") > -1 ||
     window.location.hostname.indexOf("localhost") > -1 ||
-    window.location.hostname.indexOf("127.0.0.1") > -1
+    window.location.hostname.indexOf("127.0.0.1") > -1 ||
+    window.location.hostname.indexOf("qpmefhb7mbvgs.pit-1.try.coder.app") > -1
   );
 }
 
-startApp();
+startApp().catch((error) => {
+  console.error(error);
+});
