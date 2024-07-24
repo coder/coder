6499543e670806b3c9654315d71160000f4e151e
Jaayden Halko <jaayden.halko@gmail.com>
2024-07-24 23:35:05 +0000

    fix

--- site/e2e/helpers.ts
+++ site/e2e/helpers.ts
@@ -198,7 +198,7 @@ export const createTemplate = async (
   const name = randomName();
   await page.getByLabel("Name *").fill(name);
   await page.getByTestId("form-submit").click();
-  await expectUrl(page).toHavePathName(`/templates/${name}/files?orgId=`, {
+  await expectUrl(page).toHavePathName(`/templates/${name}/files`, {
     timeout: 30000,
   });
   return name;
