a145af056c78f273e7ff9a9b8f1a392f8f14e9ea
Danny Kopping <danny@coder.com>
2024-07-29 08:56:09 +0200

    Template method update tests
    Signed-off-by: Danny Kopping <danny@coder.com>

--- codersdk/notifications.go
+++ codersdk/notifications.go
@@ -3,11 +3,27 @@ package codersdk
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+
+	"github.com/google/uuid"
+	"golang.org/x/xerrors"
 )
 
 type NotificationsSettings struct {
 	NotifierPaused bool `json:"notifier_paused"`
+}
+
+type NotificationTemplate struct {
+	ID            uuid.UUID `json:"id" format:"uuid"`
+	Name          string    `json:"name"`
+	TitleTemplate string    `json:"title_template"`
+	BodyTemplate  string    `json:"body_template"`
+	Actions       []byte    `json:"actions"`
+	Group         string    `json:"group"`
+	Method        string    `json:"method"`
+	IsSystem      bool      `json:"is_system"`
 }
 
 func (c *Client) GetNotificationsSettings(ctx context.Context) (NotificationsSettings, error) {
@@ -37,6 +53,49 @@ func (c *Client) PutNotificationsSettings(ctx context.Context, settings Notifica
 		return ReadBodyAsError(res)
 	}
 	return nil
+}
+
+func (c *Client) UpdateNotificationTemplateMethod(ctx context.Context, notificationTemplateId uuid.UUID, method string) error {
+	res, err := c.Request(ctx, http.MethodPost,
+		fmt.Sprintf("/api/v2/notifications/templates/%s/method", notificationTemplateId),
+		UpdateNotificationTemplateMethod{Method: method},
+	)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusNotModified {
+		return nil
+	}
+	if res.StatusCode != http.StatusOK {
+		return ReadBodyAsError(res)
+	}
+	return nil
+}
+
+func (c *Client) GetSystemNotificationTemplates(ctx context.Context) ([]NotificationTemplate, error) {
+	res, err := c.Request(ctx, http.MethodGet, "/api/v2/notifications/templates/system", nil)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, ReadBodyAsError(res)
+	}
+
+	var templates []NotificationTemplate
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, xerrors.Errorf("read response body: %w", err)
+	}
+
+	if err := json.Unmarshal(body, &templates); err != nil {
+		return nil, xerrors.Errorf("unmarshal response body: %w", err)
+	}
+
+	return templates, nil
 }
 
 type UpdateNotificationTemplateMethod struct {
