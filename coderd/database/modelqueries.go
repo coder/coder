86c3f282a311a491bc1d49e04c1736f83bad7c54
Kyle Carberry <kyle@carberry.com>
2024-07-29 18:21:46 +0000

    Fix model query

--- coderd/database/modelqueries.go
+++ coderd/database/modelqueries.go
@@ -361,6 +361,7 @@ func (q *sqlQuerier) GetAuthorizedUsers(ctx context.Context, arg GetUsersParams,
 			&i.QuietHoursSchedule,
 			&i.ThemePreference,
 			&i.Name,
+			&i.GithubComUserID,
 			&i.Count,
 		); err != nil {
 			return nil, err
