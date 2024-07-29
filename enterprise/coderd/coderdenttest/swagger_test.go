c555a476d4e58180b03d0a94dbadcb7ad19ea74f
McKayla Washburn <mckayla@hey.com>
2024-07-29 21:56:27 +0000

    u

--- enterprise/coderd/coderdenttest/swagger_test.go
+++ enterprise/coderd/coderdenttest/swagger_test.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"fmt"
-
 	"github.com/coder/coder/v2/coderd/coderdtest"
 	"github.com/coder/coder/v2/enterprise/coderd/coderdenttest"
 )
@@ -14,15 +12,11 @@ import (
 func TestEnterpriseEndpointsDocumented(t *testing.T) {
 	t.Parallel()
 
-	fmt.Println("wtf")
 	swaggerComments, err := coderdtest.ParseSwaggerComments("..", "../../../coderd")
 	require.NoError(t, err, "can't parse swagger comments")
 	require.NotEmpty(t, swaggerComments, "swagger comments must be present")
-	fmt.Println("wtf2")
 
 	//nolint: dogsled
 	_, _, api, _ := coderdenttest.NewWithAPI(t, nil)
-	fmt.Println("wtf3")
 	coderdtest.VerifySwaggerDefinitions(t, api.AGPL.APIHandler, swaggerComments)
-	fmt.Println("wtf4")
 }
