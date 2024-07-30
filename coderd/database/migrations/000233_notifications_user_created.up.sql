b843232b7b3244bf27cd1b559781e5f3375320c4
Marcin Tojek <marcin@coder.com>
2024-07-30 13:11:13 +0200

    fix: created_account_name

--- coderd/database/migrations/000233_notifications_user_created.up.sql
+++ coderd/database/migrations/000233_notifications_user_created.up.sql
@@ -1,6 +1,6 @@
 INSERT INTO notification_templates (id, name, title_template, body_template, "group", actions)
-VALUES ('4e19c0ac-94e1-4532-9515-d1801aa283b2', 'User account created', E'User account "{{.Labels.user_account_name}}" created',
-        E'Hi {{.UserName}},\n\New user account **{{.Labels.user_account_name}}** has been created.',
+VALUES ('4e19c0ac-94e1-4532-9515-d1801aa283b2', 'User account created', E'User account "{{.Labels.created_account_name}}" created',
+        E'Hi {{.UserName}},\n\New user account **{{.Labels.created_account_name}}** has been created.',
         'Workspace Events', '[
         {
             "label": "View accounts",
