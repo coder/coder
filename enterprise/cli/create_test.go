893169c83baa64a0541f0a929cadde459a9d3152
Kyle Carberry <kyle@coder.com>
2024-07-30 03:37:13 -0400

    fix: duplicate tags map in mutation to resolve race (#14047)
    * fix: duplicate tags map in mutation to resolve race
    
    See: https://github.com/coder/coder/actions/runs/10149619748/job/28064952716?pr=14046
    
    * Fix deployment values race

--- enterprise/cli/create_test.go
+++ enterprise/cli/create_test.go
@@ -34,11 +34,11 @@ func TestEnterpriseCreate(t *testing.T) {
 		secondTemplates []string
 	}
 
-	dv := coderdtest.DeploymentValues(t)
-	dv.Experiments = []string{string(codersdk.ExperimentMultiOrganization)}
 	// setupMultipleOrganizations creates an extra organization, assigns a member
 	// both organizations, and optionally creates templates in each organization.
 	setupMultipleOrganizations := func(t *testing.T, args setupArgs) setupData {
+		dv := coderdtest.DeploymentValues(t)
+		dv.Experiments = []string{string(codersdk.ExperimentMultiOrganization)}
 		ownerClient, first := coderdenttest.New(t, &coderdenttest.Options{
 			Options: &coderdtest.Options{
 				DeploymentValues: dv,
