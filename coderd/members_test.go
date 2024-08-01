15e7ca24825fb58a9be02e4fad9deb4995a380b1
Steven Masley <stevenmasley@gmail.com>
2024-08-01 14:59:25 -0500

    chore: fixup test that it now requires enterprise

--- coderd/members_test.go
+++ coderd/members_test.go
@@ -50,37 +50,6 @@ func TestListMembers(t *testing.T) {
 	})
 }
 
-func TestRemoveMember(t *testing.T) {
-	t.Parallel()
-
-	t.Run("OK", func(t *testing.T) {
-		t.Parallel()
-		owner := coderdtest.New(t, nil)
-		first := coderdtest.CreateFirstUser(t, owner)
-		orgAdminClient, orgAdmin := coderdtest.CreateAnotherUser(t, owner, first.OrganizationID, rbac.ScopedRoleOrgAdmin(first.OrganizationID))
-		_, user := coderdtest.CreateAnotherUser(t, owner, first.OrganizationID)
-
-		ctx := testutil.Context(t, testutil.WaitMedium)
-		// Verify the org of 3 members
-		members, err := orgAdminClient.OrganizationMembers(ctx, first.OrganizationID)
-		require.NoError(t, err)
-		require.Len(t, members, 3)
-		require.ElementsMatch(t,
-			[]uuid.UUID{first.UserID, user.ID, orgAdmin.ID},
-			db2sdk.List(members, onlyIDs))
-
-		// Delete a member
-		err = orgAdminClient.DeleteOrganizationMember(ctx, first.OrganizationID, user.Username)
-		require.NoError(t, err)
-
-		members, err = orgAdminClient.OrganizationMembers(ctx, first.OrganizationID)
-		require.NoError(t, err)
-		require.Len(t, members, 2)
-		require.ElementsMatch(t,
-			[]uuid.UUID{first.UserID, orgAdmin.ID},
-			db2sdk.List(members, onlyIDs))
-	})
-}
 
 func onlyIDs(u codersdk.OrganizationMemberWithUserData) uuid.UUID {
 	return u.UserID
