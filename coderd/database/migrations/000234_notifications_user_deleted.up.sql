4e787ec3a7cd26e327fba773c5c214273be97293
Marcin Tojek <marcin@coder.com>
2024-08-02 11:51:06 +0200

    fix

--- coderd/database/migrations/000234_notifications_user_deleted.up.sql
+++ coderd/database/migrations/000234_notifications_user_deleted.up.sql
@@ -1,7 +1,7 @@
 INSERT INTO notification_templates (id, name, title_template, body_template, "group", actions)
 VALUES ('f44d9314-ad03-4bc8-95d0-5cad491da6b6', 'User account deleted', E'User account "{{.Labels.deleted_account_name}}" deleted',
         E'Hi {{.UserName}},\n\nUser account **{{.Labels.created_account_name}}** has been deleted.',
-        'Workspace Events', '[
+        'User Events', '[
         {
             "label": "View accounts",
             "url": "{{ base_url }}/deployment/users?filter=status%3Aactive"
