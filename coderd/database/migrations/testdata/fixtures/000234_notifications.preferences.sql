c30f5625585ce52e9cc74705eed301af566a6b45
Danny Kopping <danny@coder.com>
2024-08-02 09:15:27 +0000

    CI
    Signed-off-by: Danny Kopping <danny@coder.com>

---
+++ coderd/database/migrations/testdata/fixtures/000234_notifications.preferences.sql
@@ -0,0 +1,6 @@
+INSERT INTO notification_preferences (user_id, notification_template_id, disabled, created_at, updated_at) VALUES
+	('a3d5b8f6-1f29-4a0f-a1c1-33d5a8e4e8f1', 'c9f5a2b4-3d18-4b8b-a2c4-45f1a9e2d3b7', FALSE, '2024-07-15 10:30:00+00', '2024-07-15 10:30:00+00'),
+	('b7c3f9e5-6d8b-4b3a-b2f5-56e1c8d4f2a9', 'd2e7f3a8-4b7c-4d9b-b7e1-67a2b9d5c4e8', TRUE,  '2024-07-20 12:45:00+00', '2024-07-20 12:45:00+00'),
+	('c2f6d4e7-5b8a-4c9e-b5a7-78f4a9d3b2c1', 'e9d8a7b2-5c8d-4e1f-a7b5-78d3c9f6e4b7', FALSE, '2024-07-25 14:00:00+00', '2024-07-25 14:00:00+00'),
+	('d4a8b5f6-8d7b-4a9e-b4c5-89f2e7c1d3a8', 'f7b6c3a4-6e9a-4b7c-b3f2-90e1d2c5a7b6', TRUE,  '2024-07-30 16:15:00+00', '2024-07-30 16:15:00+00'),
+	('e7c6b9f2-9d8a-4c7b-b8a4-91f3e8a2d5b9', 'a2d7f4b8-7e1f-4a3c-a8b5-12b4d9c6e3f7', FALSE, '2024-08-01 18:30:00+00', '2024-08-01 18:30:00+00');
