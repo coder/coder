476ed7dda70f7342e504a9f86a9cdd0262379612
Steven Masley <stevenmasley@gmail.com>
2024-08-05 13:38:59 -0500

    add unit test to verify not deleting org member from default org

--- coderd/members_test.go
+++ coderd/members_test.go
@@ -30,6 +30,25 @@ func TestAddMember(t *testing.T) {
 	})
 }
 
+func TestDeleteMember(t *testing.T) {
+	t.Parallel()
+
+	t.Run("NotAllowed", func(t *testing.T) {
+		t.Parallel()
+		owner := coderdtest.New(t, nil)
+		first := coderdtest.CreateFirstUser(t, owner)
+		_, user := coderdtest.CreateAnotherUser(t, owner, first.OrganizationID)
+
+		ctx := testutil.Context(t, testutil.WaitMedium)
+		// Deleting members from the default org is not allowed.
+		// If this behavior changes, and we allow deleting members from the default org,
+		// this test should be updated to check there is no error.
+		// nolint:gocritic // must be an owner to see the user
+		err := owner.DeleteOrganizationMember(ctx, first.OrganizationID, user.Username)
+		require.ErrorContains(t, err, "Multi-organizations is currently an experiment")
+	})
+}
+
 func TestListMembers(t *testing.T) {
 	t.Parallel()
 
