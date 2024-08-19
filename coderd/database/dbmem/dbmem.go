bdf6dd927b74d80c942d4199db40225199cb7a00
Steven Masley <stevenmasley@gmail.com>
2024-08-19 11:20:08 -0500

    chore: scope workspace quotas to organizations
    Quotas are now a function of (user_id, organization_id). They are
    still sourced from groups. Deprecate the old api endpoint.

--- coderd/database/dbmem/dbmem.go
+++ coderd/database/dbmem/dbmem.go
@@ -3314,13 +3314,13 @@ func (q *FakeQuerier) GetProvisionerLogsAfterID(_ context.Context, arg database.
 	return logs, nil
 }
 
-func (q *FakeQuerier) GetQuotaAllowanceForUser(_ context.Context, userID uuid.UUID) (int64, error) {
+func (q *FakeQuerier) GetQuotaAllowanceForUser(_ context.Context, params database.GetQuotaAllowanceForUserParams) (int64, error) {
 	q.mutex.RLock()
 	defer q.mutex.RUnlock()
 
 	var sum int64
 	for _, member := range q.groupMembers {
-		if member.UserID != userID {
+		if member.UserID != params.UserID {
 			continue
 		}
 		if _, err := q.getOrganizationByIDNoLock(member.GroupID); err == nil {
@@ -3340,7 +3340,7 @@ func (q *FakeQuerier) GetQuotaAllowanceForUser(_ context.Context, userID uuid.UU
 	// Grab the quota for the Everyone group iff the user is a member of
 	// said organization.
 	for _, mem := range q.organizationMembers {
-		if mem.UserID != userID {
+		if mem.UserID != params.UserID {
 			continue
 		}
 
@@ -3348,19 +3348,25 @@ func (q *FakeQuerier) GetQuotaAllowanceForUser(_ context.Context, userID uuid.UU
 		if err != nil {
 			return -1, xerrors.Errorf("failed to get everyone group for org %q", mem.OrganizationID.String())
 		}
+		if group.OrganizationID != params.OrganizationID {
+			continue
+		}
 		sum += int64(group.QuotaAllowance)
 	}
 
 	return sum, nil
 }
 
-func (q *FakeQuerier) GetQuotaConsumedForUser(_ context.Context, userID uuid.UUID) (int64, error) {
+func (q *FakeQuerier) GetQuotaConsumedForUser(_ context.Context, params database.GetQuotaConsumedForUserParams) (int64, error) {
 	q.mutex.RLock()
 	defer q.mutex.RUnlock()
 
 	var sum int64
 	for _, workspace := range q.workspaces {
-		if workspace.OwnerID != userID {
+		if workspace.OwnerID != params.OwnerID {
+			continue
+		}
+		if workspace.OrganizationID != params.OrganizationID {
 			continue
 		}
 		if workspace.Deleted {
--- enterprise/coderd/workspacequota.go
+++ enterprise/coderd/workspacequota.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 
 	"cdr.dev/slog"
@@ -13,7 +14,6 @@ import (
 	"github.com/coder/coder/v2/coderd/database"
 	"github.com/coder/coder/v2/coderd/httpapi"
 	"github.com/coder/coder/v2/coderd/httpmw"
-	"github.com/coder/coder/v2/coderd/rbac/policy"
 	"github.com/coder/coder/v2/codersdk"
 	"github.com/coder/coder/v2/provisionerd/proto"
 )
@@ -48,12 +48,18 @@ func (c *committer) CommitQuota(
 	)
 	err = c.Database.InTx(func(s database.Store) error {
 		var err error
-		consumed, err = s.GetQuotaConsumedForUser(ctx, workspace.OwnerID)
+		consumed, err = s.GetQuotaConsumedForUser(ctx, database.GetQuotaConsumedForUserParams{
+			OwnerID:        workspace.OwnerID,
+			OrganizationID: workspace.OrganizationID,
+		})
 		if err != nil {
 			return err
 		}
 
-		budget, err = s.GetQuotaAllowanceForUser(ctx, workspace.OwnerID)
+		budget, err = s.GetQuotaAllowanceForUser(ctx, database.GetQuotaAllowanceForUserParams{
+			UserID:         workspace.OwnerID,
+			OrganizationID: workspace.OrganizationID,
+		})
 		if err != nil {
 			return err
 		}
@@ -120,13 +126,35 @@ func (c *committer) CommitQuota(
 // @Param user path string true "User ID, name, or me"
 // @Success 200 {object} codersdk.WorkspaceQuota
 // @Router /workspace-quota/{user} [get]
-func (api *API) workspaceQuota(rw http.ResponseWriter, r *http.Request) {
-	user := httpmw.UserParam(r)
-
-	if !api.AGPL.Authorize(r, policy.ActionRead, user) {
-		httpapi.ResourceNotFound(rw)
+// @Deprecated this endpoint will be removed,
+// use /organizations/{organization}/members/{user}/workspace-quota instead
+func (api *API) workspaceQuotaByUser(rw http.ResponseWriter, r *http.Request) {
+	defaultOrg, err := api.Database.GetDefaultOrganization(r.Context())
+	if err != nil {
+		httpapi.InternalServerError(rw, err)
 		return
 	}
+	chi.RouteContext(r.Context()).URLParams.Add("organization", defaultOrg.ID.String())
+
+	// defer to the new endpoint
+	api.workspaceQuota(rw, r)
+}
+
+// @Summary Get workspace quota by user
+// @ID get-workspace-quota-by-user
+// @Security CoderSessionToken
+// @Produce json
+// @Tags Enterprise
+// @Param user path string true "User ID, name, or me"
+// @Success 200 {object} codersdk.WorkspaceQuota
+// @Router /organizations/{organization}/members/{user}/workspace-quota [get]
+func (api *API) workspaceQuota(rw http.ResponseWriter, r *http.Request) {
+	var (
+		organization = httpmw.OrganizationParam(r)
+		member       = httpmw.OrganizationMemberParam(r)
+	)
+
+	//defaultOrg, _ := api.Database.GetDefaultOrganization(r.Context())
 
 	api.entitlementsMu.RLock()
 	licensed := api.entitlements.Features[codersdk.FeatureTemplateRBAC].Enabled
@@ -136,7 +164,10 @@ func (api *API) workspaceQuota(rw http.ResponseWriter, r *http.Request) {
 	var quotaAllowance int64 = -1
 	if licensed {
 		var err error
-		quotaAllowance, err = api.Database.GetQuotaAllowanceForUser(r.Context(), user.ID)
+		quotaAllowance, err = api.Database.GetQuotaAllowanceForUser(r.Context(), database.GetQuotaAllowanceForUserParams{
+			UserID:         member.UserID,
+			OrganizationID: organization.ID,
+		})
 		if err != nil {
 			httpapi.Write(r.Context(), rw, http.StatusInternalServerError, codersdk.Response{
 				Message: "Failed to get allowance",
@@ -146,7 +177,10 @@ func (api *API) workspaceQuota(rw http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	quotaConsumed, err := api.Database.GetQuotaConsumedForUser(r.Context(), user.ID)
+	quotaConsumed, err := api.Database.GetQuotaConsumedForUser(r.Context(), database.GetQuotaConsumedForUserParams{
+		OwnerID:        member.UserID,
+		OrganizationID: organization.ID,
+	})
 	if err != nil {
 		httpapi.Write(r.Context(), rw, http.StatusInternalServerError, codersdk.Response{
 			Message: "Failed to get consumed",
