b4d980ebff476ad16886aa7e5300f5790bcf6735
Ethan Dickson <ethan@coder.com>
2024-08-21 11:51:07 +0000

    chore: skip completion install prompts in non-interactive shells

--- flake.nix
+++ flake.nix
@@ -117,7 +117,7 @@
             name = "coder-${osArch}";
             # Updated with ./scripts/update-flake.sh`.
             # This should be updated whenever go.mod changes!
-            vendorHash = "sha256-I2YMiYXq8XrJd6jw8JZnOL5wugPTLsip9JGELEy5Uao=";
+            vendorHash = "sha256-lkjb9a40kzi7c3JbVb+5wT1JYTaobLl4lIq11FIvxuQ=";
             proxyVendor = true;
             src = ./.;
             nativeBuildInputs = with pkgs; [ getopt openssl zstd ];
