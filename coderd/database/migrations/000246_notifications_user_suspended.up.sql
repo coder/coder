55e4e13c510885c3aeb38bcee008293b0e587ced
Marcin Tojek <marcin@coder.com>
2024-08-21 14:03:06 +0200

    fix escape

--- coderd/database/migrations/000246_notifications_user_suspended.up.sql
+++ coderd/database/migrations/000246_notifications_user_suspended.up.sql
@@ -1,6 +1,6 @@
 INSERT INTO notification_templates (id, name, title_template, body_template, "group", actions)
 VALUES ('b02ddd82-4733-4d02-a2d7-c36f3598997d', 'User account suspended', E'User account "{{.Labels.suspended_account_name}}" suspended',
-        E'Hi {{.UserName}},\n\User account **{{.Labels.suspended_account_name}}** has been suspended.',
+        E'Hi {{.UserName}},\nUser account **{{.Labels.suspended_account_name}}** has been suspended.',
         'Workspace Events', '[
         {
             "label": "View accounts",
@@ -9,7 +9,7 @@ VALUES ('b02ddd82-4733-4d02-a2d7-c36f3598997d', 'User account suspended', E'User
     ]'::jsonb);
 INSERT INTO notification_templates (id, name, title_template, body_template, "group", actions)
 VALUES ('9f5af851-8408-4e73-a7a1-c6502ba46689', 'User account activated', E'User account "{{.Labels.activated_account_name}}" activated',
-        E'Hi {{.UserName}},\n\User account **{{.Labels.activated_account_name}}** has been activated.',
+        E'Hi {{.UserName}},\nUser account **{{.Labels.activated_account_name}}** has been activated.',
         'Workspace Events', '[
         {
             "label": "View accounts",
