0782614a6e93ba05aed150e16f3731e2e3dad20e
Marcin Tojek <marcin@coder.com>
2024-08-23 15:58:16 +0200

    fix

--- coderd/database/migrations/000248_notifications_manual_build_failed.up.sql
+++ coderd/database/migrations/000248_notifications_manual_build_failed.up.sql
@@ -1,6 +1,6 @@
 INSERT INTO notification_templates (id, name, title_template, body_template, "group", actions)
 VALUES ('2faeee0f-26cb-4e96-821c-85ccb9f71513', 'Workspace Manual Build Failed', E'Workspace "{{.Labels.name}}" manual build failed',
-        E'Hi {{.UserName}},\nA manual build of the workspace **{{.Labels.name}}** using the template **{{.Labels.template_name}}** failed.\nThe workspace build was initiated by "**{{.Labels.initiator}}**".',
+        E'Hi {{.UserName}},\nA manual build of the workspace **{{.Labels.name}}** using the template **{{.Labels.template_name}}** failed.\nThe workspace build was initiated by **{{.Labels.initiator}}**.',
         'Workspace Events', '[
         {
             "label": "View workspace",
