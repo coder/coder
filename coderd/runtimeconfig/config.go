5c92688a121ee2b6782b328e4b617b8a0c76dd4e
Danny Kopping <danny@coder.com>
2024-08-28 17:04:53 +0200

    Implementation
    Signed-off-by: Danny Kopping <danny@coder.com>

---
+++ coderd/runtimeconfig/config.go
@@ -0,0 +1,97 @@
+package runtimeconfig
+
+import (
+	"context"
+	"errors"
+	"reflect"
+
+	"github.com/spf13/pflag"
+	"golang.org/x/xerrors"
+)
+
+// TODO: comment
+type Value pflag.Value
+
+type Entry[T Value] struct {
+	val T
+	key string
+}
+
+func New[T Value](key, val string) (out Entry[T], err error) {
+	out.Init(key)
+
+	if err = out.Set(val); err != nil {
+		return out, err
+	}
+
+	return out, nil
+}
+
+func MustNew[T Value](key, val string) Entry[T] {
+	out, err := New[T](key, val)
+	if err != nil {
+		panic(err)
+	}
+	return out
+}
+
+func (o *Entry[T]) Init(key string) {
+	o.val = create[T]()
+	o.key = key
+}
+
+func (o *Entry[T]) Set(s string) error {
+	if reflect.ValueOf(o.val).IsNil() {
+		return xerrors.Errorf("instance of %T is uninitialized", o.val)
+	}
+	return o.val.Set(s)
+}
+
+func (o *Entry[T]) Type() string {
+	return o.val.Type()
+}
+
+func (o *Entry[T]) String() string {
+	return o.val.String()
+}
+
+func (o *Entry[T]) StartupValue() T {
+	return o.val
+}
+
+func (o *Entry[T]) Resolve(ctx context.Context, r Resolver) (T, error) {
+	return o.resolve(ctx, r)
+}
+
+func (o *Entry[T]) resolve(ctx context.Context, r Resolver) (T, error) {
+	var zero T
+
+	val, err := r.ResolveByKey(ctx, o.key)
+	if err != nil {
+		return zero, err
+	}
+
+	inst := create[T]()
+	if err = inst.Set(val); err != nil {
+		return zero, xerrors.Errorf("instantiate new %T: %w", inst, err)
+	}
+	return inst, nil
+}
+
+func (o *Entry[T]) Save(ctx context.Context, m Mutator, val T) error {
+	return m.MutateByKey(ctx, o.key, val.String())
+}
+
+func (o *Entry[T]) Coalesce(ctx context.Context, r Resolver) (T, error) {
+	var zero T
+
+	resolved, err := o.resolve(ctx, r)
+	if err != nil {
+		if errors.Is(err, EntryNotFound) {
+			return o.StartupValue(), nil
+		}
+		return zero, err
+	}
+
+	return resolved, nil
+}
---
+++ coderd/runtimeconfig/resolver.go
@@ -0,0 +1,63 @@
+package runtimeconfig
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/google/uuid"
+	"golang.org/x/xerrors"
+)
+
+type StoreResolver struct {
+	store Store
+}
+
+func NewStoreResolver(store Store) *StoreResolver {
+	return &StoreResolver{store}
+}
+
+func (s StoreResolver) ResolveByKey(ctx context.Context, key string) (string, error) {
+	if s.store == nil {
+		panic("developer error: store must be set")
+	}
+
+	val, err := s.store.GetRuntimeSetting(ctx, key)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", xerrors.Errorf("%q: %w", key, EntryNotFound)
+		}
+		return "", xerrors.Errorf("fetch %q: %w", key, err)
+	}
+
+	return val, nil
+}
+
+type OrgResolver struct {
+	inner Resolver
+	orgID uuid.UUID
+}
+
+func NewOrgResolver(orgID uuid.UUID, inner Resolver) *OrgResolver {
+	if inner == nil {
+		panic("developer error: resolver is nil")
+	}
+
+	return &OrgResolver{inner: inner, orgID: orgID}
+}
+
+func (r OrgResolver) ResolveByKey(ctx context.Context, key string) (string, error) {
+	return r.inner.ResolveByKey(ctx, orgKey(r.orgID, key))
+}
+
+// NoopResolver will always fail to resolve the given key.
+// Useful in tests where you just want to look up the startup value of configs, and are not concerned with runtime config.
+type NoopResolver struct {}
+
+func NewNoopResolver() *NoopResolver {
+	return &NoopResolver{}
+}
+
+func (n NoopResolver) ResolveByKey(context.Context, string) (string, error) {
+	return "", EntryNotFound
+}
