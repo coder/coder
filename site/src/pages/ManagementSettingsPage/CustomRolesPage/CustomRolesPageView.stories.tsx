9514f3000772b3ed4e21776cdaf9c1466eab5d5c
Jaayden Halko <jaayden.halko@gmail.com>
2024-08-31 19:47:03 +0000

    feat: extract permissions pull list component and add tests

--- site/src/pages/ManagementSettingsPage/CustomRolesPage/CustomRolesPageView.stories.tsx
+++ site/src/pages/ManagementSettingsPage/CustomRolesPage/CustomRolesPageView.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import {
-	MockRoleWithOrgPermissions,
 	MockOrganizationAuditorRole,
+	MockRoleWithOrgPermissions,
 } from "testHelpers/entities";
 import { CustomRolesPageView } from "./CustomRolesPageView";
 
