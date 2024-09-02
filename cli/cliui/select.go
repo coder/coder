33196cd7b804543398ea8c1a131bc3242f0e31c6
Danielle Maywood <danielle@coder.com>
2024-09-02 08:13:52 +0000

    chore: reimplement testing workaround

--- cli/cliui/select.go
+++ cli/cliui/select.go
@@ -1,6 +1,7 @@
 package cliui
 
 import (
+	"flag"
 	"fmt"
 	"strings"
 
@@ -65,6 +66,15 @@ func RichSelect(inv *serpent.Invocation, richOptions RichSelectOptions) (*coders
 
 // Select displays a list of user options.
 func Select(inv *serpent.Invocation, opts SelectOptions) (string, error) {
+	// The survey library used *always* fails when testing on Windows,
+	// as it requires a live TTY (can't be a conpty). We should fork
+	// this library to add a dummy fallback, that simply reads/writes
+	// to the IO provided. See:
+	// https://github.com/AlecAivazis/survey/blob/master/terminal/runereader_windows.go#L94
+	if flag.Lookup("test.v") != nil {
+		return opts.Options[0], nil
+	}
+
 	initialModel := selectModel{
 		search:     textinput.New(),
 		hideSearch: opts.HideSearch,
@@ -238,6 +248,11 @@ type MultiSelectOptions struct {
 }
 
 func MultiSelect(inv *serpent.Invocation, opts MultiSelectOptions) ([]string, error) {
+	// Similar hack is applied to Select()
+	if flag.Lookup("test.v") != nil {
+		return opts.Defaults, nil
+	}
+
 	options := make([]*multiSelectOption, len(opts.Options))
 	for i, option := range opts.Options {
 		chosen := false
