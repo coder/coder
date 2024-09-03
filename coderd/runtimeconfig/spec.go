e67da12bb14e9f601cc5bda5d34e6c6e183a7d3d
Danny Kopping <danny@coder.com>
2024-09-03 15:22:59 +0200

    Touchups
    Signed-off-by: Danny Kopping <danny@coder.com>

--- coderd/runtimeconfig/spec.go
+++ coderd/runtimeconfig/spec.go
@@ -2,15 +2,16 @@ package runtimeconfig
 
 import "context"
 
-type Initializer interface {
-	Init(key string)
-}
-
+// Resolver is an interface for resolving runtime settings.
 type Resolver interface {
+	// GetRuntimeSetting gets a runtime setting by key.
 	GetRuntimeSetting(ctx context.Context, key string) (string, error)
 }
 
+// Mutator is an interface for mutating runtime settings.
 type Mutator interface {
+	// UpsertRuntimeSetting upserts a runtime setting by key.
 	UpsertRuntimeSetting(ctx context.Context, key, val string) error
+	// DeleteRuntimeSetting deletes a runtime setting by key.
 	DeleteRuntimeSetting(ctx context.Context, key string) error
 }
