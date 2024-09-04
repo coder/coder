7c7e0f95a95ff6797565ad45d9e7e1dfc27de6fa
Marcin Tojek <tojek.m@gmail.com>
2024-09-04 12:24:55 +0000

    WIP

--- coderd/database/migrations/000249_email_reports.up.sql
+++ coderd/database/migrations/000249_email_reports.up.sql
@@ -2,17 +2,15 @@ INSERT INTO notification_templates (id, name, title_template, body_template, "gr
 VALUES ('bc0d9b9c-6dca-40a7-a209-fb2681e3341a', 'Report: Workspace Builds Failed For Template', E'Workspace builds failed for template "{{.Labels.template_display_name}}"',
         E'Hi {{.UserName}},
 
-Template {{.Labels.template_display_name}} has failed to build {{.Data.failed_builds}}/{{.Data.total_builds}} times over the last {{.Data.report_frequency}} and may be unstable.
+Template **{{.Labels.template_display_name}}** has failed to build {{.Data.failed_builds}}/{{.Data.total_builds}} times over the last {{.Data.report_frequency}} and may be unstable.
 
 **Report:**
-
 {{range $version := .Data.template_versions}}
   **{{$version.template_version_name}}** failed {{$version.failed_count}} time{{if gt $version.failed_count 1}}s{{end}}:
   {{range $build := $version.failed_builds}}
     * [{{$build.workspace_owner_username}} / {{$build.workspace_name}} / #{{$build.build_number}}]({{base_url}}/@{{$build.workspace_owner_username}}/{{$build.workspace_name}}/builds/{{$build.build_number}})
   {{- end}}
 {{end}}
-
 We recommend reviewing these issues to ensure future builds are successful.',
         'Template Events', '[
         {
