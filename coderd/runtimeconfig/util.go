549e4fb7b19e5f3eaaa79932c08a832af9ee6f23
Steven Masley <stevenmasley@gmail.com>
2024-09-04 09:18:54 -0500

    linting errors

--- coderd/runtimeconfig/util.go
+++ coderd/runtimeconfig/util.go
@@ -1,17 +1,11 @@
 package runtimeconfig
 
 import (
-	"fmt"
 	"reflect"
-
-	"github.com/google/uuid"
 )
 
 func create[T any]() T {
 	var zero T
+	//nolint:forcetypeassert
 	return reflect.New(reflect.TypeOf(zero).Elem()).Interface().(T)
-}
-
-func orgKey(orgID uuid.UUID, key string) string {
-	return fmt.Sprintf("%s:%s", orgID.String(), key)
 }
