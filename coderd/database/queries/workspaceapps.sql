75c7816e52856e80a7b26db4cb7473c5e178e758
Danielle Maywood <danielle@coder.com>
2024-09-06 08:19:06 +0000

    fix: use spaces instead of tabs

--- coderd/database/queries/workspaceapps.sql
+++ coderd/database/queries/workspaceapps.sql
@@ -29,7 +29,7 @@ INSERT INTO
         healthcheck_threshold,
         health,
         display_order,
-		hidden
+        hidden
     )
 VALUES
     ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING *;
