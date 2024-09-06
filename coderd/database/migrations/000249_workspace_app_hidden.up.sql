d561cbcd9b8efab01011b40006eac33a0d5181bb
Danielle Maywood <danielle@themaywoods.com>
2024-09-06 09:16:40 +0100

    fix: 'not shown' instead of 'displayed'
    The existing description is the opposite of what the field actually says.
    
    Co-authored-by: Cian Johnston <cian@coder.com>

--- coderd/database/migrations/000249_workspace_app_hidden.up.sql
+++ coderd/database/migrations/000249_workspace_app_hidden.up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE workspace_apps ADD COLUMN hidden boolean NOT NULL DEFAULT false;
 
 COMMENT ON COLUMN workspace_apps.hidden
-IS 'Determines if the app is displayed in user interfaces.'
+IS 'Determines if the app is not shown in user interfaces.'
