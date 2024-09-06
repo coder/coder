cd2bbaeca240bd920f6a4c3ae34bd12696c32866
Danny Kopping <danny@coder.com>
2024-09-06 10:42:52 +0200

    Fixtures
    Signed-off-by: Danny Kopping <danny@coder.com>

---
+++ coderd/database/migrations/testdata/fixtures/000249_frobulations.up.sql
@@ -0,0 +1,7 @@
+INSERT INTO frobulators (id, user_id, org_id, model_number)
+VALUES
+    (gen_random_uuid(), '30095c71-380b-457a-8995-97b8ee6e5307', 'bb640d07-ca8a-4869-b6bc-ae61ebb2fda1', 'FRB-1001'),
+    (gen_random_uuid(), '30095c71-380b-457a-8995-97b8ee6e5307', 'bb640d07-ca8a-4869-b6bc-ae61ebb2fda1', 'FRB-1002'),
+    (gen_random_uuid(), '30095c71-380b-457a-8995-97b8ee6e5307', 'bb640d07-ca8a-4869-b6bc-ae61ebb2fda1', 'FRB-1003'),
+    (gen_random_uuid(), '30095c71-380b-457a-8995-97b8ee6e5307', 'bb640d07-ca8a-4869-b6bc-ae61ebb2fda1', 'FRB-1004'),
+    (gen_random_uuid(), '30095c71-380b-457a-8995-97b8ee6e5307', 'bb640d07-ca8a-4869-b6bc-ae61ebb2fda1', 'FRB-1005');
