8eb46cbb523764fa8265859a6d4bdb2943fff0b4
Steven Masley <stevenmasley@gmail.com>
2024-09-09 11:49:39 -0500

    chore: remove DeploymentEntry and Coalesce

--- coderd/runtimeconfig/entry_test.go
+++ coderd/runtimeconfig/entry_test.go
@@ -1,19 +1,16 @@
 package runtimeconfig_test
 
 import (
-	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/coder/coder/v2/coderd/database/dbmem"
 	"github.com/coder/coder/v2/coderd/runtimeconfig"
-	"github.com/coder/coder/v2/coderd/util/ptr"
 	"github.com/coder/coder/v2/testutil"
 	"github.com/coder/serpent"
 )
 
-// TestEntry demonstrates creating org-level overrides for deployment-level settings.
 func TestEntry(t *testing.T) {
 	t.Parallel()
 
@@ -28,40 +25,6 @@ func TestEntry(t *testing.T) {
 		require.NotPanics(t, func() {
 			runtimeconfig.MustNew[*serpent.Float64]("my-field")
 		})
-
-		{
-			var field runtimeconfig.DeploymentEntry[*serpent.Float64]
-			field.Initialize("my-field")
-			// "hello" cannot be set on a *serpent.Float64 field.
-			require.Error(t, field.Set("hello"))
-		}
-	})
-
-	t.Run("zero", func(t *testing.T) {
-		t.Parallel()
-
-		rlv := runtimeconfig.NewNoopResolver()
-
-		// A zero-value declaration of a runtimeconfig.Entry should behave as a zero value of the generic type.
-		// NB! A name has not been set for this entry; it is "uninitialized".
-		var field runtimeconfig.DeploymentEntry[*serpent.Bool]
-		var zero serpent.Bool
-		require.Equal(t, field.StartupValue().Value(), zero.Value())
-
-		// Setting a value will not produce an error.
-		require.NoError(t, field.SetStartupValue("true"))
-
-		// Attempting to resolve will produce an error.
-		_, err := field.Resolve(context.Background(), rlv)
-		require.ErrorIs(t, err, runtimeconfig.ErrNameNotSet)
-
-		// Attempting to unset
-		err = field.UnsetRuntimeValue(context.Background(), rlv)
-		require.ErrorIs(t, err, runtimeconfig.ErrNameNotSet)
-
-		// Attempting to set
-		val := serpent.BoolOf(ptr.Ref(true))
-		require.ErrorIs(t, field.SetRuntimeValue(context.Background(), rlv, val), runtimeconfig.ErrNameNotSet)
 	})
 
 	t.Run("simple", func(t *testing.T) {
@@ -71,29 +34,17 @@ func TestEntry(t *testing.T) {
 		mgr := runtimeconfig.NewManager()
 		db := dbmem.New()
 
-		var (
-			base     = serpent.String("system@dev.coder.com")
-			override = serpent.String("dogfood@dev.coder.com")
-		)
+		var override = serpent.String("dogfood@dev.coder.com")
 
-		var field runtimeconfig.DeploymentEntry[*serpent.String]
-		field.Initialize("my-field")
-		field.SetStartupValue(base.String())
-		// Check that default has been set.
-		require.Equal(t, base.String(), field.StartupValue().String())
-		// Validate that it returns that value.
-		require.Equal(t, base.String(), field.String())
-		// Validate that there is no org-level override right now.
+		field := runtimeconfig.MustNew[*serpent.String]("string-field")
+
+		// No value set yet.
 		_, err := field.Resolve(ctx, mgr.Resolver(db))
 		require.ErrorIs(t, err, runtimeconfig.ErrEntryNotFound)
-		// Coalesce returns the deployment-wide value.
-		val, err := field.Coalesce(ctx, mgr.Resolver(db))
-		require.NoError(t, err)
-		require.Equal(t, base.String(), val.String())
 		// Set an org-level override.
 		require.NoError(t, field.SetRuntimeValue(ctx, mgr.Resolver(db), &override))
-		// Coalesce now returns the org-level value.
-		val, err = field.Coalesce(ctx, mgr.Resolver(db))
+		// Value was updated
+		val, err := field.Resolve(ctx, mgr.Resolver(db))
 		require.NoError(t, err)
 		require.Equal(t, override.String(), val.String())
 	})
@@ -105,32 +56,18 @@ func TestEntry(t *testing.T) {
 		mgr := runtimeconfig.NewManager()
 		db := dbmem.New()
 
-		var (
-			base = serpent.Struct[map[string]string]{
-				Value: map[string]string{"access_type": "offline"},
-			}
-			override = serpent.Struct[map[string]string]{
-				Value: map[string]string{
-					"a": "b",
-					"c": "d",
-				},
-			}
-		)
+		var override = serpent.Struct[map[string]string]{
+			Value: map[string]string{
+				"a": "b",
+				"c": "d",
+			},
+		}
 
-		var field runtimeconfig.DeploymentEntry[*serpent.Struct[map[string]string]]
-		field.Initialize("my-field")
-		field.SetStartupValue(base.String())
-
-		// Check that default has been set.
-		require.Equal(t, base.String(), field.StartupValue().String())
-		// Validate that there is no org-level override right now.
+		field := runtimeconfig.MustNew[*serpent.Struct[map[string]string]]("string-field")
+		// Validate that there is no runtime override right now.
 		_, err := field.Resolve(ctx, mgr.Resolver(db))
 		require.ErrorIs(t, err, runtimeconfig.ErrEntryNotFound)
-		// Coalesce returns the deployment-wide value.
-		val, err := field.Coalesce(ctx, mgr.Resolver(db))
-		require.NoError(t, err)
-		require.Equal(t, base.Value, val.Value)
-		// Set an org-level override.
+		// Set a runtime value
 		require.NoError(t, field.SetRuntimeValue(ctx, mgr.Resolver(db), &override))
 		// Coalesce now returns the org-level value.
 		structVal, err := field.Resolve(ctx, mgr.Resolver(db))
