9332d153ffcce435d799501f96c4fbe33bd0126c
Jon Ayers <jon@coder.com>
2024-09-12 19:00:42 +0100

    fix: avoid logging no such process errors for process priority (#14655)

--- agent/agent.go
+++ agent/agent.go
@@ -1670,11 +1670,14 @@ func (a *agent) manageProcessPriority(ctx context.Context, debouncer *logDebounc
 
 		score, niceErr := proc.Niceness(a.syscaller)
 		if niceErr != nil && !xerrors.Is(niceErr, os.ErrPermission) {
-			debouncer.Warn(ctx, "unable to get proc niceness",
-				slog.F("cmd", proc.Cmd()),
-				slog.F("pid", proc.PID),
-				slog.Error(niceErr),
-			)
+			if !isNoSuchProcessErr(niceErr) {
+				debouncer.Warn(ctx, "unable to get proc niceness",
+					slog.F("cmd", proc.Cmd()),
+					slog.F("pid", proc.PID),
+					slog.Error(niceErr),
+				)
+			}
+
 			continue
 		}
 
@@ -1690,12 +1693,14 @@ func (a *agent) manageProcessPriority(ctx context.Context, debouncer *logDebounc
 		if niceErr == nil {
 			err := proc.SetNiceness(a.syscaller, niceness)
 			if err != nil && !xerrors.Is(err, os.ErrPermission) {
-				debouncer.Warn(ctx, "unable to set proc niceness",
-					slog.F("cmd", proc.Cmd()),
-					slog.F("pid", proc.PID),
-					slog.F("niceness", niceness),
-					slog.Error(err),
-				)
+				if !isNoSuchProcessErr(err) {
+					debouncer.Warn(ctx, "unable to set proc niceness",
+						slog.F("cmd", proc.Cmd()),
+						slog.F("pid", proc.PID),
+						slog.F("niceness", niceness),
+						slog.Error(err),
+					)
+				}
 			}
 		}
 
@@ -1704,12 +1709,14 @@ func (a *agent) manageProcessPriority(ctx context.Context, debouncer *logDebounc
 			oomScoreStr := strconv.Itoa(oomScore)
 			err := afero.WriteFile(a.filesystem, fmt.Sprintf("/proc/%d/oom_score_adj", proc.PID), []byte(oomScoreStr), 0o644)
 			if err != nil && !xerrors.Is(err, os.ErrPermission) {
-				debouncer.Warn(ctx, "unable to set oom_score_adj",
-					slog.F("cmd", proc.Cmd()),
-					slog.F("pid", proc.PID),
-					slog.F("score", oomScoreStr),
-					slog.Error(err),
-				)
+				if !isNoSuchProcessErr(err) {
+					debouncer.Warn(ctx, "unable to set oom_score_adj",
+						slog.F("cmd", proc.Cmd()),
+						slog.F("pid", proc.PID),
+						slog.F("score", oomScoreStr),
+						slog.Error(err),
+					)
+				}
 			}
 		}
 		modProcs = append(modProcs, proc)
@@ -2138,4 +2145,8 @@ func (l *logDebouncer) log(ctx context.Context, level slog.Level, msg string, fi
 		l.logger.Error(ctx, msg, fields...)
 	}
 	l.messages[msg] = time.Now()
+}
+
+func isNoSuchProcessErr(err error) bool {
+	return err != nil && strings.Contains(err.Error(), "no such process")
 }
