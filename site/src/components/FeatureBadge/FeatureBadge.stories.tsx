a98186479dec68ecd4e1a1dc0d25d3d6e955610d
Michael Smith <throwawayclover@gmail.com>
2024-09-13 22:10:25 +0000

    wip: commit progress on adding story for controlled variant

--- site/src/components/FeatureBadge/FeatureBadge.stories.tsx
+++ site/src/components/FeatureBadge/FeatureBadge.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { FeatureBadge } from "./FeatureBadge";
+import { useState } from "react";
+import { useTheme } from "@emotion/react";
 
 const meta: Meta<typeof FeatureBadge> = {
 	title: "components/FeatureBadge",
@@ -41,5 +43,46 @@ export const LargeStaticBeta: Story = {
 		type: "beta",
 		size: "lg",
 		variant: "static",
+	},
+};
+
+export const HoverControlledByParent: Story = {
+	args: {
+		type: "experimental",
+		size: "sm",
+	},
+
+	decorators: (Story, context) => {
+		const theme = useTheme();
+		const [isHovering, setIsHovering] = useState(false);
+
+		return (
+			<button
+				type="button"
+				onPointerEnter={() => setIsHovering(true)}
+				onPointerLeave={() => setIsHovering(false)}
+				css={[
+					{
+						backgroundColor: theme.palette.background.default,
+						color: theme.palette.text.primary,
+						display: "flex",
+						flexFlow: "row nowrap",
+						alignItems: "center",
+						columnGap: "16px",
+					},
+					isHovering && {
+						backgroundColor: "green",
+					},
+				]}
+			>
+				<span>Blah</span>
+				{Story({
+					args: {
+						...context.initialArgs,
+						variant: isHovering ? "staticHover" : "static",
+					},
+				})}
+			</button>
+		);
 	},
 };
