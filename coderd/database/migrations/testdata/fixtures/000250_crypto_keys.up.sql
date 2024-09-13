c91309177c8e687777bdbc88c58341b7327a908a
Jon Ayers <jon@coder.com>
2024-09-13 02:03:56 +0000

    fixture

---
+++ coderd/database/migrations/testdata/fixtures/000250_crypto_keys.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO crypto_keys (feature, sequence, starts_at, secret) VALUES 
+('workspace_apps', 1, now(), 'abc'),
+('oidc_convert', 1, now(), 'def'),
+('peer_reconnect', 1, now(), 'ghi');
