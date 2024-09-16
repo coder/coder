b7ad71451529dcbb424ad3bf4d85c71fcab5799c
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2024-09-16 18:52:47 +0000

    Fix authz test

--- coderd/database/dbauthz/dbauthz_test.go
+++ coderd/database/dbauthz/dbauthz_test.go
@@ -554,7 +554,7 @@ func (s *MethodTestSuite) TestProvisionerJob() {
 	s.Run("GetProvisionerJobTimingsByJobID", s.Subtest(func(db database.Store, check *expects) {
 		j := dbgen.ProvisionerJob(s.T(), db, nil, database.ProvisionerJob{})
 		t := dbgen.ProvisionerJobTimings(s.T(), db, database.InsertProvisionerJobTimingsParams{JobID: j.ID})
-		check.Args(j.ID).Asserts().Returns(slice.New(t))
+		check.Args(j.ID).Asserts().Returns(t)
 	}))
 	s.Run("GetProvisionerJobsByIDs", s.Subtest(func(db database.Store, check *expects) {
 		a := dbgen.ProvisionerJob(s.T(), db, nil, database.ProvisionerJob{})
