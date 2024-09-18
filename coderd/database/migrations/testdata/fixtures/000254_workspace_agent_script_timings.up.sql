669b837a34a224ff1edf171151cfc1b27c108d42
Danielle Maywood <danielle@coder.com>
2024-09-18 10:52:44 +0000

    fix: typo

--- coderd/database/migrations/testdata/fixtures/000254_workspace_agent_script_timings.up.sql
+++ coderd/database/migrations/testdata/fixtures/000254_workspace_agent_script_timings.up.sql
@@ -1,3 +1,3 @@
-INSERT INTO workspace_agent_sscript_timings (job_id, display_name, started_at, ended_at, exit_code)
+INSERT INTO workspace_agent_script_timings (job_id, display_name, started_at, ended_at, exit_code)
 VALUES
     ('f41f5fd1-2a4e-4e52-adad-aee36d2da506', 'Startup Script', NOW() - INTERVAL '1 hour 55 minutes', NOW() - INTERVAL '1 hour 50 minutes', 0);
