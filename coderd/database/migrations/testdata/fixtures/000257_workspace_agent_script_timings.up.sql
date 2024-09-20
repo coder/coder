58462162b37b85b0b00430a34321e41ef505a177
Danielle Maywood <danielle@coder.com>
2024-09-20 15:44:08 +0000

    fix: testdata fixture

--- coderd/database/migrations/testdata/fixtures/000257_workspace_agent_script_timings.up.sql
+++ coderd/database/migrations/testdata/fixtures/000257_workspace_agent_script_timings.up.sql
@@ -1,3 +1,3 @@
-INSERT INTO workspace_agent_script_timings (script_id, display_name, started_at, ended_at, exit_code, stage, timed_out)
+INSERT INTO workspace_agent_script_timings (script_id, started_at, ended_at, exit_code, stage, timed_out)
 VALUES
-    ((SELECT id FROM workspace_agent_scripts LIMIT 1), 'Startup Script', NOW() - INTERVAL '1 hour 55 minutes', NOW() - INTERVAL '1 hour 50 minutes', 0, 'start', false);
+    ((SELECT id FROM workspace_agent_scripts LIMIT 1), NOW() - INTERVAL '1 hour 55 minutes', NOW() - INTERVAL '1 hour 50 minutes', 0, 'start', false);
