0f6453395031fbda93a2da2211ea9c909d396819
Michael Smith <throwawayclover@gmail.com>
2024-09-20 02:33:16 +0000

    chore: update stories

--- site/src/components/FeatureStageBadge/FeatureStageBadge.stories.tsx
+++ site/src/components/FeatureStageBadge/FeatureStageBadge.stories.tsx
@@ -5,52 +5,34 @@ const meta: Meta<typeof FeatureStageBadge> = {
 	title: "components/FeatureStageBadge",
 	component: FeatureStageBadge,
 	args: {
-		type: "beta",
+		contentType: "beta",
 	},
 };
 
 export default meta;
 type Story = StoryObj<typeof FeatureStageBadge>;
 
-export const SmallInteractiveBeta: Story = {
+export const MediumBeta: Story = {
 	args: {
-		type: "beta",
+		size: "md",
+	},
+};
+
+export const SmallBeta: Story = {
+	args: {
 		size: "sm",
-		variant: "interactive",
 	},
 };
 
-export const SmallInteractiveExperimental: Story = {
+export const LargeBeta: Story = {
 	args: {
-		type: "experimental",
-		size: "sm",
-		variant: "interactive",
-	},
-};
-
-export const LargeInteractiveBeta: Story = {
-	args: {
-		type: "beta",
 		size: "lg",
-		variant: "interactive",
 	},
 };
 
-export const LargeStaticBeta: Story = {
+export const MediumExperimental: Story = {
 	args: {
-		type: "beta",
-		size: "lg",
-		variant: "static",
-	},
-};
-
-// While it's possible, we shouldn't use the xs size with the interactive
-// variant, because then the badge might be too small for users with motor
-// control issues to get their hand to hover over the target reliably
-export const ExtraSmallStaticBeta: Story = {
-	args: {
-		type: "beta",
-		size: "xs",
-		variant: "static",
+		size: "md",
+		contentType: "experimental",
 	},
 };
