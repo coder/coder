3db9f487f0d38207c5bc48102833f10e942d4bf7
Jaayden Halko <jaayden.halko@gmail.com>
2024-09-24 00:39:20 +0000

    chore: document uuid regex

--- site/src/pages/ManagementSettingsPage/IdpSyncPage/IdpPillList.tsx
+++ site/src/pages/ManagementSettingsPage/IdpSyncPage/IdpPillList.tsx
@@ -12,8 +12,9 @@ interface PillListProps {
 	roles: readonly string[];
 }
 
+// used to check if the role is a UUID
 const UUID =
-	/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+	/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 export const IdpPillList: FC<PillListProps> = ({ roles }) => {
 	return (
