97b25d9680b2a352c1b7b26ac1f649329eed0d2f
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2024-09-25 22:12:18 +0000

    Fix key

--- site/src/modules/workspaces/WorkspaceTiming/Chart/BarBlocks.tsx
+++ site/src/modules/workspaces/WorkspaceTiming/Chart/BarBlocks.tsx
@@ -37,8 +37,9 @@ export const BarBlocks: FC<BarBlocksProps> = ({ count }) => {
 
 	return (
 		<div ref={blocksRef} css={styles.blocks}>
-			{Array.from({ length: nOfBlocks }, (_, i) => i + 1).map((n) => (
-				<div key={n} css={styles.block} style={{ minWidth: blockSize }} />
+			{Array.from({ length: nOfBlocks }).map((_, i) => (
+				// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+				<div key={i} css={styles.block} style={{ minWidth: blockSize }} />
 			))}
 			{!hasSpacing && (
 				<div css={styles.extraBlock}>
