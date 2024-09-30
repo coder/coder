e70ad2b4b3e4e5690a0f6555af398e4642f3995b
Kayla Washburn-Love <mckayla@hey.com>
2024-09-30 12:38:08 -0600

    fix: always show upload and scratch in create template gallery (#14327)

--- site/src/pages/CreateTemplateGalleryPage/CreateTemplateGalleryPage.tsx
+++ site/src/pages/CreateTemplateGalleryPage/CreateTemplateGalleryPage.tsx
@@ -1,16 +1,13 @@
 import { templateExamples } from "api/queries/templates";
 import type { TemplateExample } from "api/typesGenerated";
-import { useDashboard } from "modules/dashboard/useDashboard";
 import type { FC } from "react";
 import { Helmet } from "react-helmet-async";
 import { useQuery } from "react-query";
 import { pageTitle } from "utils/page";
 import { getTemplatesByTag } from "utils/starterTemplates";
-import { CreateTemplatesPageView } from "./CreateTemplatesPageView";
-import { StarterTemplatesPageView } from "./StarterTemplatesPageView";
+import { CreateTemplateGalleryPageView } from "./CreateTemplateGalleryPageView";
 
 const CreateTemplatesGalleryPage: FC = () => {
-	const { showOrganizations } = useDashboard();
 	const templateExamplesQuery = useQuery(templateExamples());
 	const starterTemplatesByTag = templateExamplesQuery.data
 		? // Currently, the scratch template should not be displayed on the starter templates page.
@@ -22,17 +19,10 @@ const CreateTemplatesGalleryPage: FC = () => {
 			<Helmet>
 				<title>{pageTitle("Create a Template")}</title>
 			</Helmet>
-			{showOrganizations ? (
-				<CreateTemplatesPageView
-					error={templateExamplesQuery.error}
-					starterTemplatesByTag={starterTemplatesByTag}
-				/>
-			) : (
-				<StarterTemplatesPageView
-					error={templateExamplesQuery.error}
-					starterTemplatesByTag={starterTemplatesByTag}
-				/>
-			)}
+			<CreateTemplateGalleryPageView
+				error={templateExamplesQuery.error}
+				starterTemplatesByTag={starterTemplatesByTag}
+			/>
 		</>
 	);
 };
