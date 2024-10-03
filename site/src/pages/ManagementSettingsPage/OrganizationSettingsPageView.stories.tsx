6fea1beeb1196d54db3d6ae76172b2b36efe4ef8
McKayla Washburn <mckayla@hey.com>
2024-10-03 21:44:59 +0000

    wow

--- site/src/pages/ManagementSettingsPage/OrganizationSettingsPageView.stories.tsx
+++ site/src/pages/ManagementSettingsPage/OrganizationSettingsPageView.stories.tsx
@@ -11,7 +11,6 @@ const meta: Meta<typeof OrganizationSettingsPageView> = {
 	title: "pages/OrganizationSettingsPageView",
 	component: OrganizationSettingsPageView,
 	parameters: { chromatic },
-	decorators: [withManagementSettingsProvider],
 	args: {
 		organization: MockOrganization,
 	},
