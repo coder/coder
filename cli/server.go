b552267520e3b7dd0472cd5ee05dabc8615a4c82
defelmnq <vincent@coder.com>
2024-10-04 09:04:04 +0000

    chore: lint

--- cli/server.go
+++ cli/server.go
@@ -1310,7 +1310,7 @@ func templateHelpers(options *coderd.Options) map[string]any {
 		"base_url":     func() string { return options.AccessURL.String() },
 		"current_year": func() string { return strconv.Itoa(time.Now().Year()) },
 		"logo_url": func() string {
-			ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 			defer cancel()
 
 			logoURL, err := options.Database.GetLogoURL(ctx)
@@ -1325,7 +1325,7 @@ func templateHelpers(options *coderd.Options) map[string]any {
 			return logoURL
 		},
 		"app_name": func() string {
-			ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 			defer cancel()
 
 			appName, err := options.Database.GetApplicationName(ctx)
