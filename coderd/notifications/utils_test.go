2f620c9663becfd9fd09524fbf6d1cbce4d59468
defelmnq <defelmnq+github@gmail.com>
2024-10-16 14:48:55 +0200

    feat(notifications): improve logo_url and app_name fetching moving it to notifications package

--- coderd/notifications/utils_test.go
+++ coderd/notifications/utils_test.go
@@ -39,8 +39,8 @@ func defaultHelpers() map[string]any {
 	return map[string]any{
 		"base_url":     func() string { return "http://test.com" },
 		"current_year": func() string { return "2024" },
-		"logo_url":     func() string { return "https://logo.company" },
-		"app_name":     func() string { return "TestCompany" },
+		"logo_url":     func() string { return "https://coder.com/coder-logo-horizontal.png" },
+		"app_name":     func() string { return "Coder" },
 	}
 }
 
