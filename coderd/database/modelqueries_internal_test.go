6bee486c49426a86b621c3e6ae0ae744751b8ce5
Steven Masley <stevenmasley@gmail.com>
2024-10-16 17:22:20 -0500

    remove unused

--- coderd/database/modelqueries_internal_test.go
+++ coderd/database/modelqueries_internal_test.go
@@ -1,12 +1,9 @@
 package database
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/coder/coder/v2/testutil"
 )
 
 func TestIsAuthorizedQuery(t *testing.T) {
@@ -15,20 +12,4 @@ func TestIsAuthorizedQuery(t *testing.T) {
 	query := `SELECT true;`
 	_, err := insertAuthorizedFilter(query, "")
 	require.ErrorContains(t, err, "does not contain authorized replace string", "ensure replace string")
-}
-
-// TestWorkspaceTableConvert verifies all workspace fields are converted
-// when reducing a `Workspace` to a `WorkspaceTable`.
-func TestWorkspaceTableConvert(t *testing.T) {
-	t.Parallel()
-
-	var workspace Workspace
-	err := testutil.PopulateStruct(&workspace, nil)
-	require.NoError(t, err)
-
-	workspace.WorkspaceTable()
-	require.JSONEq(t)
-
-	fmt.Println(workspace)
-
 }
