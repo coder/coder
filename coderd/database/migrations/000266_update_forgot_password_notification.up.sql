c2797043b4f4392d670151fe546fefa5ff29a405
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2024-10-18 12:38:17 +0000

    Update message with @stirby copy

--- coderd/database/migrations/000266_update_forgot_password_notification.up.sql
+++ coderd/database/migrations/000266_update_forgot_password_notification.up.sql
@@ -1,7 +1,7 @@
 UPDATE notification_templates
 SET
     title_template = E'Reset your password for Coder',
-    body_template = E'Hi {{.UserName}},\n\nA request to reset the password for your Coder account has been made.\n\nIf you did not request to reset your password, you can ignore this message.',
+    body_template = E'Hi {{.UserName}},\n\nUse the link below to reset your password.\n\nIf you did not make this request, you can ignore this message.',
     actions = '[{
 		"label": "Reset password",
 		"url": "{{ base_url }}/reset-password/change?otp={{.Labels.one_time_passcode}}&email={{ .UserEmail }}"
