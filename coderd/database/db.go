81a02204bc712bda902fe7d96ec14478b80cac54
Steven Masley <stevenmasley@gmail.com>
2024-10-23 14:16:09 -0500

    wip

--- coderd/database/db.go
+++ coderd/database/db.go
@@ -80,6 +80,14 @@ func (q *sqlQuerier) Ping(ctx context.Context) (time.Duration, error) {
 	return time.Since(start), err
 }
 
+// retryAmount is an arbitrary number.
+// Unit tests should disable retries.
+var retryAmount = 3
+
+func init() {
+	retryAmount = 1
+}
+
 func (q *sqlQuerier) InTx(function func(Store) error, txOpts *sql.TxOptions) error {
 	_, inTx := q.db.(*sqlx.Tx)
 	isolation := sql.LevelDefault
@@ -93,8 +101,6 @@ func (q *sqlQuerier) InTx(function func(Store) error, txOpts *sql.TxOptions) err
 	// If we are in a transaction already, the parent InTx call will handle the retry.
 	// We do not want to duplicate those retries.
 	if !inTx && isolation == sql.LevelSerializable {
-		// This is an arbitrarily chosen number.
-		const retryAmount = 3
 		var err error
 		attempts := 0
 		for attempts = 0; attempts < retryAmount; attempts++ {
