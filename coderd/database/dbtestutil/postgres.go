0b25f51e8976455d6d2a6371556491f5fe4487ae
Hugo Dutka <hugo@coder.com>
2024-10-27 13:13:09 +0100

    createDatabaseFromTemplate

--- coderd/database/dbtestutil/postgres.go
+++ coderd/database/dbtestutil/postgres.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/cenkalti/backoff/v4"
@@ -19,47 +21,136 @@ import (
 // Open creates a new PostgreSQL database instance.  With DB_FROM environment variable set, it clones a database
 // from the provided template.  With the environment variable unset, it creates a new Docker container running postgres.
 func Open() (string, func(), error) {
-	if os.Getenv("DB_FROM") != "" {
-		// In CI, creating a Docker container for each test is slow.
-		// This expects a PostgreSQL instance with the hardcoded credentials
-		// available.
-		dbURL := "postgres://postgres:postgres@127.0.0.1:5432/postgres?sslmode=disable"
-		db, err := sql.Open("postgres", dbURL)
-		if err != nil {
-			return "", nil, xerrors.Errorf("connect to ci postgres: %w", err)
-		}
+	var (
+		username = "postgres"
+		password = "postgres"
+		host     = "127.0.0.1"
+		port     = "5432"
+	)
 
-		defer db.Close()
-
-		dbName, err := cryptorand.StringCharset(cryptorand.Lower, 10)
-		if err != nil {
-			return "", nil, xerrors.Errorf("generate db name: %w", err)
-		}
-
-		dbName = "ci" + dbName
-		_, err = db.Exec("CREATE DATABASE " + dbName + " WITH TEMPLATE " + os.Getenv("DB_FROM"))
-		if err != nil {
-			return "", nil, xerrors.Errorf("create db with template: %w", err)
-		}
-
-		dsn := "postgres://postgres:postgres@127.0.0.1:5432/" + dbName + "?sslmode=disable"
-		// Normally this would get cleaned up by removing the container but if we
-		// reuse the same container for multiple tests we run the risk of filling
-		// up our disk. Avoid this!
-		cleanup := func() {
-			cleanupConn, err := sql.Open("postgres", dbURL)
-			if err != nil {
-				_, _ = fmt.Fprintf(os.Stderr, "cleanup database %q: failed to connect to postgres: %s\n", dbName, err.Error())
-			}
-			defer cleanupConn.Close()
-			_, err = cleanupConn.Exec("DROP DATABASE " + dbName + ";")
-			if err != nil {
-				_, _ = fmt.Fprintf(os.Stderr, "failed to clean up database %q: %s\n", dbName, err.Error())
-			}
-		}
-		return dsn, cleanup, nil
+	dbName, err := cryptorand.StringCharset(cryptorand.Lower, 10)
+	if err != nil {
+		return "", nil, xerrors.Errorf("generate db name: %w", err)
 	}
-	return OpenContainerized(0)
+	dbName = "test_" + dbName
+
+	if err := createDatabaseFromTemplate(CreateDatabaseArgs{
+		Username: username,
+		Password: password,
+		Host:     host,
+		Port:     port,
+		DBName:   dbName,
+	}); err != nil {
+		return "", nil, xerrors.Errorf("create database: %w", err)
+	}
+
+	cleanup := func() {
+		cleanupDbURL := fmt.Sprintf("postgres://%s:%s@%s:%s?sslmode=disable", username, password, host, port)
+		cleanupConn, err := sql.Open("postgres", cleanupDbURL)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "cleanup database %q: failed to connect to postgres: %s\n", dbName, err.Error())
+		}
+		defer cleanupConn.Close()
+		_, err = cleanupConn.Exec("DROP DATABASE " + dbName + ";")
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "failed to clean up database %q: %s\n", dbName, err.Error())
+		}
+	}
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", username, password, host, port, dbName)
+	return dsn, cleanup, nil
+}
+
+var templateInitLock = sync.Mutex{}
+
+type CreateDatabaseArgs struct {
+	Username string
+	Password string
+	Host     string
+	Port     string
+	DBName   string
+}
+
+// createDatabaseFromTemplate creates a new database from a template database.
+// The template database is created if it doesn't exist.
+func createDatabaseFromTemplate(args CreateDatabaseArgs) error {
+	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s?sslmode=disable", args.Username, args.Password, args.Host, args.Port)
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		return xerrors.Errorf("connect to postgres: %w", err)
+	}
+	defer db.Close()
+
+	templateDBName := fmt.Sprintf("tpl_%s", migrations.GetMigrationsHash()[:32])
+	_, err = db.Exec("CREATE DATABASE " + args.DBName + " WITH TEMPLATE " + templateDBName)
+	if err == nil {
+		// Template database already exists and we successfully created the new database.
+		return nil
+	}
+	if !(strings.Contains(err.Error(), "template database") && strings.Contains(err.Error(), "does not exist")) {
+		return xerrors.Errorf("create db with template: %w", err)
+	}
+
+	// We need to create the template database.
+	templateInitLock.Lock()
+	defer templateInitLock.Unlock()
+
+	// Someone else might have created the template db while we were waiting.
+	tplDbExistsRes, err := db.Query("SELECT 1 FROM pg_database WHERE datname = $1", templateDBName)
+	if err != nil {
+		return xerrors.Errorf("check if db exists: %w", err)
+	}
+	tplDbAlreadyExists := tplDbExistsRes.Next()
+	if !tplDbAlreadyExists {
+		// We will use a temporary template database to avoid race conditions. We will
+		// rename it to the real template database name after we're sure it was fully
+		// initialized.
+		// It's dropped here to ensure that if a previous run of this function failed
+		// midway, we don't encounter issues with the temporary database still existing.
+		tmpTemplateDBName := "tmp_" + templateDBName
+		_, err = db.Exec("DROP DATABASE IF EXISTS " + tmpTemplateDBName)
+		if err != nil {
+			return xerrors.Errorf("drop tmp template db: %w", err)
+		}
+
+		_, err = db.Exec("CREATE DATABASE " + tmpTemplateDBName)
+		if err != nil {
+			return xerrors.Errorf("create tmp template db: %w", err)
+		}
+		tplDbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", args.Username, args.Password, args.Host, args.Port, tmpTemplateDBName)
+		tplDb, err := sql.Open("postgres", tplDbURL)
+		if err != nil {
+			return xerrors.Errorf("connect to template db: %w", err)
+		}
+		defer tplDb.Close()
+		if err := migrations.Up(tplDb); err != nil {
+			return xerrors.Errorf("migrate template db: %w", err)
+		}
+		if err := tplDb.Close(); err != nil {
+			return xerrors.Errorf("close template db: %w", err)
+		}
+		_, err = db.Exec("ALTER DATABASE " + tmpTemplateDBName + " RENAME TO " + templateDBName)
+		if err != nil {
+			return xerrors.Errorf("rename tmp template db: %w", err)
+		}
+	}
+
+	_, err = db.Exec("CREATE DATABASE " + args.DBName + " WITH TEMPLATE " + templateDBName)
+	if err != nil {
+		return xerrors.Errorf("create db with template after migrations: %w", err)
+	}
+
+	return nil
+}
+
+func CreateDatabaseTest() error {
+	return createDatabaseFromTemplate(CreateDatabaseArgs{
+		Username: "postgres",
+		Password: "postgres",
+		Host:     "127.0.0.1",
+		Port:     "5432",
+		DBName:   "my_beautiful_db",
+	})
 }
 
 // OpenContainerized creates a new PostgreSQL server using a Docker container.  If port is nonzero, forward host traffic
@@ -76,7 +167,7 @@ func OpenContainerized(port int) (string, func(), error) {
 	}
 
 	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Repository: "gcr.io/coder-dev-1/postgres",
+		Repository: "postgres",
 		Tag:        "13",
 		Env: []string{
 			"POSTGRES_PASSWORD=postgres",
