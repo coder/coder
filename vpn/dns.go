704f29f667160bceda4f91cb5db860c4dccd6ef5
Ethan Dickson <ethan@coder.com>
2024-11-05 07:49:44 +0000

    review

--- vpn/dns.go
+++ vpn/dns.go
@@ -2,15 +2,40 @@ package vpn
 
 import "tailscale.com/net/dns"
 
-func NewVPNManager(t *Tunnel) dns.OSConfigurator {
-	return &vpnDNSManager{tunnel: t}
+func NewDNSConfigurator(t *Tunnel) dns.OSConfigurator {
+	return &dnsManager{tunnel: t}
 }
 
-type vpnDNSManager struct {
+type dnsManager struct {
 	tunnel *Tunnel
 }
 
-func (v *vpnDNSManager) SetDNS(cfg dns.OSConfig) error {
+func (v *dnsManager) SetDNS(cfg dns.OSConfig) error {
+	settings := convertDNSConfig(cfg)
+	return v.tunnel.ApplyNetworkSettings(v.tunnel.ctx, &NetworkSettingsRequest{
+		DnsSettings: settings,
+	})
+}
+
+func (*dnsManager) GetBaseConfig() (dns.OSConfig, error) {
+	// Tailscale calls this function to blend the OS's DNS configuration with
+	// it's own, so this is only called if `SupportsSplitDNS` returns false.
+	return dns.OSConfig{}, dns.ErrGetBaseConfigNotSupported
+}
+
+func (*dnsManager) SupportsSplitDNS() bool {
+	// macOS & Windows 10+ support split DNS, so we'll assume all CoderVPN
+	// clients do too.
+	return true
+}
+
+// Close implements dns.OSConfigurator.
+func (*dnsManager) Close() error {
+	// There's no cleanup that we need to initiate from within the dylib.
+	return nil
+}
+
+func convertDNSConfig(cfg dns.OSConfig) *NetworkSettingsRequest_DNSSettings {
 	servers := make([]string, 0, len(cfg.Nameservers))
 	for _, ns := range cfg.Nameservers {
 		servers = append(servers, ns.String())
@@ -23,33 +48,11 @@ func (v *vpnDNSManager) SetDNS(cfg dns.OSConfig) error {
 	for _, domain := range cfg.MatchDomains {
 		matchDomains = append(matchDomains, domain.WithoutTrailingDot())
 	}
-	return v.tunnel.ApplyNetworkSettings(v.tunnel.ctx, &NetworkSettingsRequest{
-		DnsSettings: &NetworkSettingsRequest_DNSSettings{
-			Servers:              servers,
-			SearchDomains:        searchDomains,
-			DomainName:           "coder",
-			MatchDomains:         matchDomains,
-			MatchDomainsNoSearch: false,
-		},
-	})
+	return &NetworkSettingsRequest_DNSSettings{
+		Servers:              servers,
+		SearchDomains:        searchDomains,
+		DomainName:           "coder",
+		MatchDomains:         matchDomains,
+		MatchDomainsNoSearch: false,
+	}
 }
-
-func (vpnDNSManager) GetBaseConfig() (dns.OSConfig, error) {
-	// Tailscale calls this function to blend the OS's DNS configuration with
-	// it's own, so this is only called if `SupportsSplitDNS` returns false.
-	return dns.OSConfig{}, dns.ErrGetBaseConfigNotSupported
-}
-
-func (*vpnDNSManager) SupportsSplitDNS() bool {
-	// macOS & Windows 10+ support split DNS, so we'll assume all CoderVPN
-	// clients do too.
-	return true
-}
-
-// Close implements dns.OSConfigurator.
-func (*vpnDNSManager) Close() error {
-	// There's no cleanup that we need to initiate from within the dylib.
-	return nil
-}
-
-var _ dns.OSConfigurator = (*vpnDNSManager)(nil)
