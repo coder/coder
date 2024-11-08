3377d22e7d19b0e075989034f6415f0be10d6203
Hugo Dutka <hugo@coder.com>
2024-11-08 13:16:28 +0000

    add a workspace_modules fixture

---
+++ coderd/database/migrations/testdata/fixtures/000274_workspace_modules.up.sql
@@ -0,0 +1,20 @@
+INSERT INTO
+    public.workspace_modules (
+        id,
+        job_id,
+        transition,
+        source,
+        version,
+        key,
+        created_at
+    )
+VALUES
+    (
+        '5b1a722c-b8a0-40b0-a3a0-d8078fff9f6c',
+        '424a58cb-61d6-4627-9907-613c396c4a38',
+        'start',
+        'test-source',
+        'v1.0.0',
+        'test-key',
+        '2024-11-08 10:00:00+00'
+    );
