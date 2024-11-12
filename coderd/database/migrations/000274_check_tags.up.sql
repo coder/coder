5941dc3203c9b12907412343f809717d989406ee
Sas Swart <sas.swart.cdk@gmail.com>
2024-11-12 10:51:44 +0000

    fix job acquisition for untagged provisioners

--- coderd/database/migrations/000274_check_tags.up.sql
+++ coderd/database/migrations/000274_check_tags.up.sql
@@ -7,7 +7,8 @@ CREATE OR REPLACE FUNCTION tags_compatible(subset_tags tags, superset_tags tags)
 RETURNS boolean as $$
 BEGIN
 	RETURN CASE
-		WHEN superset_tags :: jsonb = '{"scope": "organization", "owner": ""}' :: jsonb
+		-- Special case for untagged provisioners
+		WHEN subset_tags :: jsonb = '{"scope": "organization", "owner": ""}' :: jsonb
 		THEN subset_tags = superset_tags
 		ELSE subset_tags :: jsonb <@ superset_tags :: jsonb
 	END;
