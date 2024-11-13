f10561e91df7fb82f2980ec142480ac5de0897e4
Sas Swart <sas.swart.cdk@gmail.com>
2024-11-13 13:21:38 +0000

    attempt to fix the special case for provisioner_tagset_contains

--- coderd/database/migrations/000274_check_tags.up.sql
+++ coderd/database/migrations/000274_check_tags.up.sql
@@ -2,15 +2,15 @@ CREATE DOMAIN tagset AS jsonb;
 
 COMMENT ON DOMAIN tagset IS 'A set of tags that match provisioner daemons to provisioner jobs, which can originate from workspaces or templates. tagset is a narrowed type over jsonb. It is expected to be the JSON representation of map[string]string. That is, {"key1": "value1", "key2": "value2"}. We need the narrowed type instead of just using jsonb so that we can give sqlc a type hint, otherwise it defaults to json.RawMessage. json.RawMessage is a suboptimal type to use in the context that we need tagset for.';
 
-CREATE OR REPLACE FUNCTION provisioner_tagset_contains(superset tagset, subset tagset)
+CREATE OR REPLACE FUNCTION provisioner_tagset_contains(provisioner_tags tagset, job_tags tagset)
 RETURNS boolean as $$
 BEGIN
 	RETURN
 		-- Special case for untagged provisioners, where only an exact match should count
-		(subset = '{"scope": "organization", "owner": ""}' :: jsonb AND subset :: jsonb = superset :: jsonb)
+		(job_tags = '{"scope": "organization", "owner": ""}' :: jsonb AND job_tags :: jsonb = provisioner_tags :: jsonb)
 		-- General case
-		OR subset :: jsonb <@ superset :: jsonb;
+		OR job_tags :: jsonb <@ provisioner_tags :: jsonb;
 END;
 $$ LANGUAGE plpgsql;
 
-COMMENT ON FUNCTION provisioner_tagset_contains(tagset, tagset) IS 'Returns true if the superset contains the subset, or if the subset represents an untagged provisioner and the superset is exactly equal to the subset.';
+COMMENT ON FUNCTION provisioner_tagset_contains(tagset, tagset) IS 'Returns true if the provisioner_tags contains the job_tags, or if the job_tags represents an untagged provisioner and the superset is exactly equal to the subset.';
