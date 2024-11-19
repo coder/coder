8a93635019cf636e1b558492e01d4d016544326e
Garrett Delfosse <garrett@coder.com>
2024-11-19 15:58:36 +0000

    coderd

--- scaletest/terraform/new/vars.tf
+++ scaletest/terraform/new/vars.tf
@@ -1,9 +1,10 @@
-variable "project_id" {
-  description = "The project in which to provision resources"
-}
-
 variable "name" {
   description = "The name all resources will be prefixed with"
+}
+
+// GCP
+variable "project_id" {
+  description = "The project in which to provision resources"
 }
 
 variable "deployments" {
@@ -33,20 +34,16 @@ variable "node_image_type" {
   default     = "cos_containerd"
 }
 
-// Preemptible nodes are way cheaper, but can be pulled out
-// from under you at any time. Caveat emptor.
 variable "node_preemptible" {
   description = "Use preemptible nodes."
   default     = false
 }
 
-// These variables control the node pool dedicated to Coder.
 variable "nodepool_machine_type_coder" {
   description = "Machine type to use for Coder control plane nodepool."
   default     = "t2d-standard-4"
 }
 
-// These variables control the size of the database to be used by Coder.
 variable "cloudsql_version" {
   description = "CloudSQL version to provision"
   default     = "POSTGRES_14"
@@ -60,4 +57,132 @@ variable "cloudsql_tier" {
 variable "cloudsql_max_connections" {
   description = "CloudSQL database max_connections"
   default     = 500
+}
+
+// Cloudflare
+variable "cloudflare_api_token" {
+  description = "Cloudflare API token."
+  sensitive   = true
+}
+
+variable "cloudflare_email" {
+  description = "Cloudflare email address."
+  sensitive   = true
+}
+
+variable "cloudflare_domain" {
+  description = "Cloudflare coder domain."
+}
+
+// Coder
+variable "coder_chart_version" {
+  description = "Version of the Coder Helm chart to install. Defaults to latest."
+  default     = null
+}
+
+variable "coder_image_tag" {
+  description = "Tag to use for Coder image."
+  default     = "latest"
+}
+
+variable "coder_image_repo" {
+  description = "Repository to use for Coder image."
+  default     = "ghcr.io/coder/coder"
+}
+
+variable "coder_replicas" {
+  description = "Number of Coder replicas to provision."
+  default     = 1
+}
+
+variable "coder_cpu_request" {
+  description = "CPU request to allocate to Coder."
+  default     = "500m"
+}
+
+variable "coder_mem_request" {
+  description = "Memory request to allocate to Coder."
+  default     = "512Mi"
+}
+
+variable "coder_cpu_limit" {
+  description = "CPU limit to allocate to Coder."
+  default     = "1000m"
+}
+
+variable "coder_mem_limit" {
+  description = "Memory limit to allocate to Coder."
+  default     = "1024Mi"
+}
+
+variable "coder_experiments" {
+  description = "Coder Experiments to enable."
+  default     = ""
+}
+
+// Workspaces
+variable "workspace_image" {
+  description = "Image and tag to use for workspaces."
+  default     = "docker.io/codercom/enterprise-minimal:ubuntu"
+}
+
+variable "workspace_cpu_request" {
+  description = "CPU request to allocate to workspaces."
+  default     = "100m"
+}
+
+variable "workspace_cpu_limit" {
+  description = "CPU limit to allocate to workspaces."
+  default     = "100m"
+}
+
+variable "workspace_mem_request" {
+  description = "Memory request to allocate to workspaces."
+  default     = "128Mi"
+}
+
+variable "workspace_mem_limit" {
+  description = "Memory limit to allocate to workspaces."
+  default     = "128Mi"
+}
+
+// Provisioners
+variable "provisionerd_cpu_request" {
+  description = "CPU request to allocate to provisionerd."
+  default     = "100m"
+}
+
+variable "provisionerd_mem_request" {
+  description = "Memory request to allocate to provisionerd."
+  default     = "1Gi"
+}
+
+variable "provisionerd_cpu_limit" {
+  description = "CPU limit to allocate to provisionerd."
+  default     = "1000m"
+}
+
+variable "provisionerd_mem_limit" {
+  description = "Memory limit to allocate to provisionerd."
+  default     = "1Gi"
+}
+
+variable "provisionerd_replicas" {
+  description = "Number of Provisionerd replicas."
+  default     = 1
+}
+
+variable "provisionerd_chart_version" {
+  description = "Version of the Provisionerd Helm chart to install. Defaults to latest."
+  default     = null
+}
+
+variable "provisionerd_image_repo" {
+  description = "Repository to use for Provisionerd image."
+  default     = "ghcr.io/coder/coder"
+}
+
+variable "provisionerd_image_tag" {
+  description = "Tag to use for Provisionerd image."
+  default     = "latest"
 }
