ddbfc3eee034c156461aabc6fffd8e8e968e268c
Garrett Delfosse <garrett@coder.com>
2024-11-19 15:58:36 +0000

    coderd

--- scaletest/terraform/new/main.tf
+++ scaletest/terraform/new/main.tf
@@ -9,10 +9,55 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.5"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+
+    // We use the kubectl provider to apply Custom Resources.
+    // The kubernetes provider requires the CRD is already present 
+    // and would require a separate apply step beforehand.
+    // https://github.com/hashicorp/terraform-provider-kubernetes/issues/1367
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.0.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.9"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 
   required_version = "~> 1.9.0"
 }
 
 provider "google" {
+}
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.cluster[0].endpoint}"
+  cluster_ca_certificate = base64decode(google_container_cluster.cluster[0].master_auth.0.cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
+}
+
+provider "kubectl" {
+  host                   = "https://${google_container_cluster.cluster[0].endpoint}"
+  cluster_ca_certificate = base64decode(google_container_cluster.cluster[0].master_auth.0.cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
+  load_config_file       = false
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.cluster[0].endpoint}"
+    cluster_ca_certificate = base64decode(google_container_cluster.cluster[0].master_auth.0.cluster_ca_certificate)
+    token                  = data.google_client_config.default.access_token
+  }
 }
