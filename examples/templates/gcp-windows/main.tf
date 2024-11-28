1ba51ed8ffdfbd2de8159c667cb2c4566e466a2e
Phorcys <57866459+phorcys420@users.noreply.github.com>
2024-11-28 03:57:02 +0000

    fix: remove jetbrains gateway and code-server modules

--- examples/templates/gcp-windows/main.tf
+++ examples/templates/gcp-windows/main.tf
@@ -52,38 +52,6 @@ resource "coder_agent" "main" {
   os   = "windows"
 }
 
-# See https://registry.coder.com/modules/code-server
-module "code-server" {
-  count  = data.coder_workspace.me.start_count
-  source = "registry.coder.com/modules/code-server/coder"
-
-  # This ensures that the latest version of the module gets downloaded, you can also pin the module version to prevent breaking changes in production.
-  version = ">= 1.0.0"
-
-  agent_id = coder_agent.main.id
-  order    = 1
-}
-
-# See https://registry.coder.com/modules/jetbrains-gateway
-module "jetbrains_gateway" {
-  count  = data.coder_workspace.me.start_count
-  source = "registry.coder.com/modules/jetbrains-gateway/coder"
-
-  # JetBrains IDEs to make available for the user to select
-  jetbrains_ides = ["IU", "PY", "WS", "PS", "RD", "CL", "GO", "RM"]
-  default        = "IU"
-
-  # Default folder to open when starting a JetBrains IDE
-  folder = "/home/coder"
-
-  # This ensures that the latest version of the module gets downloaded, you can also pin the module version to prevent breaking changes in production.
-  version = ">= 1.0.0"
-
-  agent_id   = coder_agent.main.id
-  agent_name = "main"
-  order      = 2
-}
-
 resource "google_compute_instance" "dev" {
   zone         = module.gcp_region.value
   count        = data.coder_workspace.me.start_count
