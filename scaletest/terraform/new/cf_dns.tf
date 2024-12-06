1241c05b34035e8cf35f907455f3d4c7724bb4fc
Garrett Delfosse <garrett@coder.com>
2024-12-06 18:27:44 +0000

    working eu

--- scaletest/terraform/new/cf_dns.tf
+++ scaletest/terraform/new/cf_dns.tf
@@ -5,3 +5,11 @@ resource "cloudflare_record" "coder" {
   type    = "A"
   ttl     = 3600
 }
+
+resource "cloudflare_record" "coder_europe" {
+  zone_id = var.cloudflare_zone_id
+  name    = local.coder_europe_subdomain
+  content = google_compute_address.coder["europe"].address
+  type    = "A"
+  ttl     = 3600
+}
