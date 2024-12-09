98935253589f544436f0cc3e72fdb730ec184f47
Garrett Delfosse <garrett@coder.com>
2024-12-09 18:24:21 +0000

    fmt:

--- scaletest/terraform/action/deployments.tf
+++ scaletest/terraform/action/deployments.tf
@@ -22,41 +22,4 @@ locals {
       cidr      = "10.202.0.0/24"
     }
   }
-
-  scenarios = {
-    small = {
-      coder = {
-        nodepool_size = 1
-        machine_type  = "t2d-standard-4"
-        replicas      = 1
-        cpu_request   = "1000m"
-        mem_request   = "6Gi"
-        cpu_limit     = "2000m"
-        mem_limit     = "12Gi"
-      }
-      provisionerd = {
-        replicas    = 1
-        cpu_request = "100m"
-        mem_request = "1Gi"
-        cpu_limit   = "1000m"
-        mem_limit   = "1Gi"
-      }
-      workspaces = {
-        nodepool_size = 1
-        machine_type  = "t2d-standard-4"
-        cpu_request   = "100m"
-        mem_request   = "128Mi"
-        cpu_limit     = "100m"
-        mem_limit     = "128Mi"
-      }
-      misc = {
-        nodepool_size = 1
-        machine_type  = "t2d-standard-4"
-      }
-      cloudsql = {
-        tier            = "db-f1-micro"
-        max_connections = 500
-      }
-    }
-  }
 }
