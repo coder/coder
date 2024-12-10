d193bb1a064dbabea56633c89ffe8ef06afce5b5
McKayla Washburn <mckayla@hey.com>
2024-12-10 19:47:49 +0000

    >:(

--- site/e2e/tests/organizationMembers.spec.ts
+++ site/e2e/tests/organizationMembers.spec.ts
@@ -12,11 +12,8 @@ test.beforeEach(async ({ page }) => {
 test("create and delete organization", async ({ page }) => {
 	requiresLicense();
 
-	// Create an organization
-	await page.goto("/organizations/new", {
-		waitUntil: "domcontentloaded",
-	});
-
+	// Create a new organization to test
+	await page.goto("/organizations/new", { waitUntil: "domcontentloaded" });
 	const name = randomName();
 	await page.getByLabel("Slug").fill(name);
 	await page.getByLabel("Display name").fill(`Org ${name}`);
@@ -24,20 +21,30 @@ test("create and delete organization", async ({ page }) => {
 	await page.getByLabel("Icon", { exact: true }).fill("/emojis/1f957.png");
 	await page.getByRole("button", { name: "Submit" }).click();
 
-	// Expect to be redirected to the new organization
+	// Navigate to members page
 	await expectUrl(page).toHavePathName(`/organizations/${name}`);
 	await expect(page.getByText("Organization created.")).toBeVisible();
-
 	await page.getByText("Members").click();
 
+	// Add a user to the org
 	const personToAdd = await createUser(page);
 	await page.getByPlaceholder("User email or username").fill(personToAdd.email);
 	await page.getByRole("option", { name: personToAdd.email }).click();
 	await page.getByRole("button", { name: "Add user" }).click();
-	await page.pause();
-	await expect(
-		page.locator("tr", { hasText: personToAdd.email }),
-	).toBeVisible();
+	const addedRow = page.locator("tr", { hasText: personToAdd.email });
+	await expect(addedRow).toBeVisible();
+
+	// Give them a role
+	await addedRow.getByLabel("Edit user roles").click();
+	await page.getByRole("checkbox", { name: "Organization admin" }).click();
+	// await addedRow.getByLabel("Edit user roles").click();
+	await expect(addedRow.getByText("Organization admin")).toBeVisible();
+
+	// Remove them from the org
+	await addedRow.getByLabel("More options").click();
+	await page.getByText("Remove").click(); // Click the "Remove" option
+	await page.getByRole("button", { name: "Remove" }).click(); // Click "Remove" in the confirmation dialog
+	await expect(addedRow).not.toBeVisible();
 
 	// await page.getByRole("button", { name: "Delete this organization" }).click();
 	// const dialog = page.getByTestId("dialog");
