3fc95da268793fc6eb1e7ff06851206f067ee28e
Mathias Fredriksson <mafredri@gmail.com>
2024-12-11 20:43:15 +0200

    fix failf

--- coderd/insights_test.go
+++ coderd/insights_test.go
@@ -121,7 +121,7 @@ func TestDeploymentInsights(t *testing.T) {
 
 	select {
 	case <-ctx.Done():
-		require.Failf(t, "timed out waiting for initial rollup event: %v", ctx.Err())
+		require.Fail(t, "timed out waiting for initial rollup event", ctx.Err())
 	case ev := <-rollupEvents:
 		require.True(t, ev.Init, "want init event")
 	}
