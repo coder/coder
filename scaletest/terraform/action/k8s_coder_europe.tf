769b5d311e7ee1a86983f8879605c3643357ec09
Garrett Delfosse <garrett@coder.com>
2024-12-11 21:26:33 +0000

    tpl

--- scaletest/terraform/action/k8s_coder_europe.tf
+++ scaletest/terraform/action/k8s_coder_europe.tf
@@ -51,94 +51,26 @@ resource "helm_release" "coder_europe" {
   name       = local.coder_release_name
   version    = var.coder_chart_version
   namespace  = kubernetes_namespace.coder_europe.metadata.0.name
-  values = [<<EOF
-coder:
-  workspaceProxy: true
-  affinity:
-    nodeAffinity:
-      requiredDuringSchedulingIgnoredDuringExecution:
-        nodeSelectorTerms:
-        - matchExpressions:
-          - key: "cloud.google.com/gke-nodepool"
-            operator: "In"
-            values: ["${google_container_node_pool.node_pool["europe_coder"].name}"]
-    podAntiAffinity:
-      preferredDuringSchedulingIgnoredDuringExecution:
-      - weight: 1
-        podAffinityTerm:
-          topologyKey: "kubernetes.io/hostname"
-          labelSelector:
-            matchExpressions:
-            - key:      "app.kubernetes.io/instance"
-              operator: "In"
-              values:   ["${local.coder_release_name}"]
-  env:
-    - name: CODER_PRIMARY_ACCESS_URL
-      value: "${local.deployments.primary.url}"
-    - name: CODER_PROXY_SESSION_TOKEN
-      valueFrom:
-        secretKeyRef:
-          key: token
-          name: "${kubernetes_secret.proxy_token_europe.metadata.0.name}"
-    - name: "CODER_ACCESS_URL"
-      value: "${local.deployments.europe.url}"
-    - name: "CODER_CACHE_DIRECTORY"
-      value: "/tmp/coder"
-    - name: "CODER_TELEMETRY_ENABLE"
-      value: "false"
-    - name: "CODER_LOGGING_HUMAN"
-      value: "/dev/null"
-    - name: "CODER_LOGGING_STACKDRIVER"
-      value: "/dev/stderr"
-    - name: "CODER_PPROF_ENABLE"
-      value: "true"
-    - name: "CODER_PROMETHEUS_ENABLE"
-      value: "true"
-    - name: "CODER_PROMETHEUS_COLLECT_AGENT_STATS"
-      value: "true"
-    - name: "CODER_PROMETHEUS_COLLECT_DB_METRICS"
-      value: "true"
-    - name: "CODER_VERBOSE"
-      value: "true"
-    - name: "CODER_EXPERIMENTS"
-      value: "${var.coder_experiments}"
-    - name: "CODER_DANGEROUS_DISABLE_RATE_LIMITS"
-      value: "true"
-    # Disabling built-in provisioner daemons
-    - name: "CODER_PROVISIONER_DAEMONS"
-      value: "0"
-    - name: CODER_PROVISIONER_DAEMON_PSK
-      valueFrom:
-        secretKeyRef:
-          key: psk
-          name: "${kubernetes_secret.provisionerd_psk_europe.metadata.0.name}"
-  image:
-    repo: ${var.coder_image_repo}
-    tag: ${var.coder_image_tag}
-  replicaCount: "${local.scenarios[var.scenario].coder.replicas}"
-  resources:
-    requests:
-      cpu: "${local.scenarios[var.scenario].coder.cpu_request}"
-      memory: "${local.scenarios[var.scenario].coder.mem_request}"
-    limits:
-      cpu: "${local.scenarios[var.scenario].coder.cpu_limit}"
-      memory: "${local.scenarios[var.scenario].coder.mem_limit}"
-  securityContext:
-    readOnlyRootFilesystem: true
-  service:
-    enable: true
-    sessionAffinity: None
-    loadBalancerIP: "${google_compute_address.coder["europe"].address}"
-  volumeMounts:
-  - mountPath: "/tmp"
-    name: cache
-    readOnly: false
-  volumes:
-  - emptyDir:
-      sizeLimit: 1024Mi
-    name: cache
-EOF
-  ]
+  values = [templatefile("${path.module}/coder_helm_values.tftpl", {
+    workspace_proxy = true,
+    provisionerd = false,
+    primary_url = local.deployments.primary.url,
+    proxy_token = kubernetes_secret.proxy_token_europe.metadata.0.name,
+    db_secret = null,
+    ip_address = google_compute_address.coder["europe"].address,
+    provisionerd_psk = null,
+    access_url = local.deployments.europe.url,
+    node_pool = google_container_node_pool.node_pool["europe_coder"].name,
+    release_name = local.coder_release_name,
+    experiments = var.coder_experiments,
+    image_repo = var.coder_image_repo,
+    image_tag = var.coder_image_tag,
+    replicas = local.scenarios[var.scenario].coder.replicas,
+    cpu_request = local.scenarios[var.scenario].coder.cpu_request,
+    mem_request = local.scenarios[var.scenario].coder.mem_request,
+    cpu_limit = local.scenarios[var.scenario].coder.cpu_limit,
+    mem_limit = local.scenarios[var.scenario].coder.mem_limit,
+  })]
 }
 
 resource "helm_release" "provisionerd_europe" {
@@ -149,66 +81,24 @@ resource "helm_release" "provisionerd_europe" {
   name       = local.provisionerd_release_name
   version    = var.provisionerd_chart_version
   namespace  = kubernetes_namespace.coder_europe.metadata.0.name
-  values = [<<EOF
-coder:
-  affinity:
-    nodeAffinity:
-      requiredDuringSchedulingIgnoredDuringExecution:
-        nodeSelectorTerms:
-        - matchExpressions:
-          - key: "cloud.google.com/gke-nodepool"
-            operator: "In"
-            values: ["${google_container_node_pool.node_pool["europe_coder"].name}"]
-    podAntiAffinity:
-      preferredDuringSchedulingIgnoredDuringExecution:
-      - weight: 1
-        podAffinityTerm:
-          topologyKey: "kubernetes.io/hostname"
-          labelSelector:
-            matchExpressions:
-            - key:      "app.kubernetes.io/instance"
-              operator: "In"
-              values:   ["${local.coder_release_name}"]
-  env:
-    - name: "CODER_URL"
-      value: "${local.deployments.primary.url}"
-    - name: "CODER_VERBOSE"
-      value: "true"
-    - name: "CODER_CONFIG_DIR"
-      value: "/tmp/config"
-    - name: "CODER_CACHE_DIRECTORY"
-      value: "/tmp/coder"
-    - name: "CODER_TELEMETRY_ENABLE"
-      value: "false"
-    - name: "CODER_LOGGING_HUMAN"
-      value: "/dev/null"
-    - name: "CODER_LOGGING_STACKDRIVER"
-      value: "/dev/stderr"
-    - name: "CODER_PROMETHEUS_ENABLE"
-      value: "true"
-    - name: "CODER_PROVISIONERD_TAGS"
-      value: "scope=organization"
-  image:
-    repo: ${var.provisionerd_image_repo}
-    tag: ${var.provisionerd_image_tag}
-  replicaCount: "${local.scenarios[var.scenario].provisionerd.replicas}"
-  resources:
-    requests:
-      cpu: "${local.scenarios[var.scenario].provisionerd.cpu_request}"
-      memory: "${local.scenarios[var.scenario].provisionerd.mem_request}"
-    limits:
-      cpu: "${local.scenarios[var.scenario].provisionerd.cpu_limit}"
-      memory: "${local.scenarios[var.scenario].provisionerd.mem_limit}"
-  securityContext:
-    readOnlyRootFilesystem: true
-  volumeMounts:
-  - mountPath: "/tmp"
-    name: cache
-    readOnly: false
-  volumes:
-  - emptyDir:
-      sizeLimit: 1024Mi
-    name: cache
-EOF
-  ]
+  values = [templatefile("${path.module}/coder_helm_values.tftpl", {
+    workspace_proxy = false,
+    provisionerd = true,
+    primary_url = null,
+    proxy_token = null,
+    db_secret = null,
+    ip_address = null,
+    provisionerd_psk = kubernetes_secret.provisionerd_psk_europe.metadata.0.name,
+    access_url = local.deployments.primary.url,
+    node_pool = google_container_node_pool.node_pool["europe_coder"].name,
+    release_name = local.coder_release_name,
+    experiments = var.coder_experiments,
+    image_repo = var.coder_image_repo,
+    image_tag = var.coder_image_tag,
+    replicas = local.scenarios[var.scenario].provisionerd.replicas,
+    cpu_request = local.scenarios[var.scenario].provisionerd.cpu_request,
+    mem_request = local.scenarios[var.scenario].provisionerd.mem_request,
+    cpu_limit = local.scenarios[var.scenario].provisionerd.cpu_limit,
+    mem_limit = local.scenarios[var.scenario].provisionerd.mem_limit,
+  })]
 }
--- scaletest/terraform/action/k8s_coder_primary.tf
+++ scaletest/terraform/action/k8s_coder_primary.tf
@@ -71,91 +71,26 @@ resource "helm_release" "coder_primary" {
   name       = local.coder_release_name
   version    = var.coder_chart_version
   namespace  = kubernetes_namespace.coder_primary.metadata.0.name
-  values = [<<EOF
-coder:
-  affinity:
-    nodeAffinity:
-      requiredDuringSchedulingIgnoredDuringExecution:
-        nodeSelectorTerms:
-        - matchExpressions:
-          - key: "cloud.google.com/gke-nodepool"
-            operator: "In"
-            values: ["${google_container_node_pool.node_pool["primary_coder"].name}"]
-    podAntiAffinity:
-      preferredDuringSchedulingIgnoredDuringExecution:
-      - weight: 1
-        podAffinityTerm:
-          topologyKey: "kubernetes.io/hostname"
-          labelSelector:
-            matchExpressions:
-            - key:      "app.kubernetes.io/instance"
-              operator: "In"
-              values:   ["${local.coder_release_name}"]
-  env:
-    - name: "CODER_ACCESS_URL"
-      value: "${local.deployments.primary.url}"
-    - name: "CODER_CACHE_DIRECTORY"
-      value: "/tmp/coder"
-    - name: "CODER_TELEMETRY_ENABLE"
-      value: "false"
-    - name: "CODER_LOGGING_HUMAN"
-      value: "/dev/null"
-    - name: "CODER_LOGGING_STACKDRIVER"
-      value: "/dev/stderr"
-    - name: "CODER_PG_CONNECTION_URL"
-      valueFrom:
-        secretKeyRef:
-          name: "${kubernetes_secret.coder_db.metadata.0.name}"
-          key: url
-    - name: "CODER_PPROF_ENABLE"
-      value: "true"
-    - name: "CODER_PROMETHEUS_ENABLE"
-      value: "true"
-    - name: "CODER_PROMETHEUS_COLLECT_AGENT_STATS"
-      value: "true"
-    - name: "CODER_PROMETHEUS_COLLECT_DB_METRICS"
-      value: "true"
-    - name: "CODER_VERBOSE"
-      value: "true"
-    - name: "CODER_EXPERIMENTS"
-      value: "${var.coder_experiments}"
-    - name: "CODER_DANGEROUS_DISABLE_RATE_LIMITS"
-      value: "true"
-    # Disabling built-in provisioner daemons
-    - name: "CODER_PROVISIONER_DAEMONS"
-      value: "0"
-    - name: CODER_PROVISIONER_DAEMON_PSK
-      valueFrom:
-        secretKeyRef:
-          key: psk
-          name: "${kubernetes_secret.provisionerd_psk_primary.metadata.0.name}"
-  image:
-    repo: ${var.coder_image_repo}
-    tag: ${var.coder_image_tag}
-  replicaCount: "${local.scenarios[var.scenario].coder.replicas}"
-  resources:
-    requests:
-      cpu: "${local.scenarios[var.scenario].coder.cpu_request}"
-      memory: "${local.scenarios[var.scenario].coder.mem_request}"
-    limits:
-      cpu: "${local.scenarios[var.scenario].coder.cpu_limit}"
-      memory: "${local.scenarios[var.scenario].coder.mem_limit}"
-  securityContext:
-    readOnlyRootFilesystem: true
-  service:
-    enable: true
-    sessionAffinity: None
-    loadBalancerIP: "${google_compute_address.coder["primary"].address}"
-  volumeMounts:
-  - mountPath: "/tmp"
-    name: cache
-    readOnly: false
-  volumes:
-  - emptyDir:
-      sizeLimit: 1024Mi
-    name: cache
-EOF
-  ]
+  values = [templatefile("${path.module}/coder_helm_values.tftpl", {
+    workspace_proxy = false,
+    provisionerd = false,
+    primary_url = null,
+    proxy_token = null,
+    db_secret = kubernetes_secret.coder_db.metadata.0.name,
+    ip_address = google_compute_address.coder["primary"].address,
+    provisionerd_psk = kubernetes_secret.provisionerd_psk_primary.metadata.0.name,
+    access_url = local.deployments.primary.url,
+    node_pool = google_container_node_pool.node_pool["primary_coder"].name,
+    release_name = local.coder_release_name,
+    experiments = var.coder_experiments,
+    image_repo = var.coder_image_repo,
+    image_tag = var.coder_image_tag,
+    replicas = local.scenarios[var.scenario].coder.replicas,
+    cpu_request = local.scenarios[var.scenario].coder.cpu_request,
+    mem_request = local.scenarios[var.scenario].coder.mem_request,
+    cpu_limit = local.scenarios[var.scenario].coder.cpu_limit,
+    mem_limit = local.scenarios[var.scenario].coder.mem_limit,
+  })]
 }
 
 resource "helm_release" "provisionerd_chart" {
@@ -166,66 +101,24 @@ resource "helm_release" "provisionerd_chart" {
   name       = local.provisionerd_release_name
   version    = var.provisionerd_chart_version
   namespace  = kubernetes_namespace.coder_primary.metadata.0.name
-  values = [<<EOF
-coder:
-  affinity:
-    nodeAffinity:
-      requiredDuringSchedulingIgnoredDuringExecution:
-        nodeSelectorTerms:
-        - matchExpressions:
-          - key: "cloud.google.com/gke-nodepool"
-            operator: "In"
-            values: ["${google_container_node_pool.node_pool["primary_coder"].name}"]
-    podAntiAffinity:
-      preferredDuringSchedulingIgnoredDuringExecution:
-      - weight: 1
-        podAffinityTerm:
-          topologyKey: "kubernetes.io/hostname"
-          labelSelector:
-            matchExpressions:
-            - key:      "app.kubernetes.io/instance"
-              operator: "In"
-              values:   ["${local.coder_release_name}"]
-  env:
-    - name: "CODER_URL"
-      value: "${local.deployments.primary.url}"
-    - name: "CODER_VERBOSE"
-      value: "true"
-    - name: "CODER_CONFIG_DIR"
-      value: "/tmp/config"
-    - name: "CODER_CACHE_DIRECTORY"
-      value: "/tmp/coder"
-    - name: "CODER_TELEMETRY_ENABLE"
-      value: "false"
-    - name: "CODER_LOGGING_HUMAN"
-      value: "/dev/null"
-    - name: "CODER_LOGGING_STACKDRIVER"
-      value: "/dev/stderr"
-    - name: "CODER_PROMETHEUS_ENABLE"
-      value: "true"
-    - name: "CODER_PROVISIONERD_TAGS"
-      value: "scope=organization"
-  image:
-    repo: ${var.provisionerd_image_repo}
-    tag: ${var.provisionerd_image_tag}
-  replicaCount: "${local.scenarios[var.scenario].provisionerd.replicas}"
-  resources:
-    requests:
-      cpu: "${local.scenarios[var.scenario].provisionerd.cpu_request}"
-      memory: "${local.scenarios[var.scenario].provisionerd.mem_request}"
-    limits:
-      cpu: "${local.scenarios[var.scenario].provisionerd.cpu_limit}"
-      memory: "${local.scenarios[var.scenario].provisionerd.mem_limit}"
-  securityContext:
-    readOnlyRootFilesystem: true
-  volumeMounts:
-  - mountPath: "/tmp"
-    name: cache
-    readOnly: false
-  volumes:
-  - emptyDir:
-      sizeLimit: 1024Mi
-    name: cache
-EOF
-  ]
+  values = [templatefile("${path.module}/coder_helm_values.tftpl", {
+    workspace_proxy = false,
+    provisionerd = true,
+    primary_url = null,
+    proxy_token = null,
+    db_secret = null,
+    ip_address = null,
+    provisionerd_psk = kubernetes_secret.provisionerd_psk_primary.metadata.0.name,
+    access_url = local.deployments.primary.url,
+    node_pool = google_container_node_pool.node_pool["primary_coder"].name,
+    release_name = local.coder_release_name,
+    experiments = var.coder_experiments,
+    image_repo = var.coder_image_repo,
+    image_tag = var.coder_image_tag,
+    replicas = local.scenarios[var.scenario].provisionerd.replicas,
+    cpu_request = local.scenarios[var.scenario].provisionerd.cpu_request,
+    mem_request = local.scenarios[var.scenario].provisionerd.mem_request,
+    cpu_limit = local.scenarios[var.scenario].provisionerd.cpu_limit,
+    mem_limit = local.scenarios[var.scenario].provisionerd.mem_limit,
+  })]
 }
