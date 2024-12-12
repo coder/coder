d15745a67f39a8267d824e78f00dd64ac1c893dc
Jaayden Halko <jaayden.halko@gmail.com>
2024-12-12 15:53:18 +0000

    feat: display warning dialog if user switches off assign default org

--- site/src/components/Dialog/Dialog.tsx
+++ site/src/components/Dialog/Dialog.tsx
@@ -3,7 +3,6 @@
  * @see {@link https://ui.shadcn.com/docs/components/dialog}
  */
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { X } from "lucide-react";
 import {
 	type ComponentPropsWithoutRef,
 	type ElementRef,
@@ -46,29 +45,19 @@ export const DialogContent = forwardRef<
 		<DialogPrimitive.Content
 			ref={ref}
 			className={cn(
-				`fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg
-				gap-4 border bg-background p-6 shadow-lg duration-200 sm:rounded-lg
-				translate-x-[-50%] translate-y-[-50%]
-				data-[state=open]:animate-in data-[state=closed]:animate-out
-				data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0
-				data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95
-				data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]
-				data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]`,
+				`fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg gap-4
+				 border border-solid border-border bg-surface-primary p-8 shadow-lg duration-200 sm:rounded-lg
+				 translate-x-[-50%] translate-y-[-50%]
+				 data-[state=open]:animate-in data-[state=closed]:animate-out
+				 data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0
+				 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95
+				 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]
+				 data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]`,
 				className,
 			)}
 			{...props}
 		>
 			{children}
-			<DialogPrimitive.Close
-				className={`absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity
-			hover:opacity-100
-			focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2
-			disabled:pointer-events-none
-			data-[state=open]:bg-surface-secondary data-[state=open]:text-content-disabled`}
-			>
-				<X className="h-4 w-4" />
-				<span className="sr-only">Close</span>
-			</DialogPrimitive.Close>
 		</DialogPrimitive.Content>
 	</DialogPortal>
 ));
@@ -106,7 +95,7 @@ export const DialogTitle = forwardRef<
 	<DialogPrimitive.Title
 		ref={ref}
 		className={cn(
-			"text-lg font-semibold leading-none tracking-tight",
+			"text-xl m-0 text-content-primary font-semibold leading-none tracking-tight",
 			className,
 		)}
 		{...props}
@@ -119,7 +108,7 @@ export const DialogDescription = forwardRef<
 >(({ className, ...props }, ref) => (
 	<DialogPrimitive.Description
 		ref={ref}
-		className={cn("text-sm text-content-disabled", className)}
+		className={cn("text-sm text-content-secondary", className)}
 		{...props}
 	/>
 ));
