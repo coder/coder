c5e87690be8b769d11503b95a8e67cfc70f16f95
Stephen Kirby <58410745+stirby@users.noreply.github.com>
2024-12-16 12:01:04 -0600

    chore: acquire lock for individual workspace transition (#15883)
    When Coder is ran in High Availability mode, each Coder instance has a
    lifecycle executor. These lifecycle executors are all trying to do the
    same work, and whilst transactions saves us from this causing an issue,
    we are still doing extra work that could be prevented.
    
    This PR adds a `TryAcquireLock` call for each attempted workspace
    transition, meaning two Coder instances shouldn't duplicate effort.
    
    (cherry picked from commit 50ff06cc3c1845f5aecd260d743be5cb0a0f4431)
    
    Co-authored-by: Danielle Maywood <danielle@themaywoods.com>

--- coderd/autobuild/lifecycle_executor.go
+++ coderd/autobuild/lifecycle_executor.go
@@ -3,6 +3,7 @@ package autobuild
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"net/http"
 	"sync"
 	"sync/atomic"
@@ -176,6 +177,15 @@ func (e *Executor) runOnce(t time.Time) Stats {
 				)
 				err := e.db.InTx(func(tx database.Store) error {
 					var err error
+
+					ok, err := tx.TryAcquireLock(e.ctx, database.GenLockID(fmt.Sprintf("lifecycle-executor:%s", wsID)))
+					if err != nil {
+						return xerrors.Errorf("try acquire lifecycle executor lock: %w", err)
+					}
+					if !ok {
+						log.Debug(e.ctx, "unable to acquire lock for workspace, skipping")
+						return nil
+					}
 
 					// Re-check eligibility since the first check was outside the
 					// transaction and the workspace settings may have changed.
@@ -372,7 +382,7 @@ func (e *Executor) runOnce(t time.Time) Stats {
 				}
 				return nil
 			}()
-			if err != nil {
+			if err != nil && !xerrors.Is(err, context.Canceled) {
 				log.Error(e.ctx, "failed to transition workspace", slog.Error(err))
 				statsMu.Lock()
 				stats.Errors[wsID] = err
