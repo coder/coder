7b2c25902b51ed0266f6d71f32d5e04433637e5e
Sas Swart <sas.swart.cdk@gmail.com>
2024-12-17 08:14:35 +0000

    make gen

--- coderd/database/migrations/000279_user_status_changes.down.sql
+++ coderd/database/migrations/000279_user_status_changes.down.sql
@@ -6,7 +6,6 @@ DROP FUNCTION IF EXISTS record_user_status_change();
 
 -- Drop the indexes
 DROP INDEX IF EXISTS idx_user_status_changes_changed_at;
-DROP INDEX IF EXISTS idx_user_status_changes_user_id;
 
 -- Drop the table
 DROP TABLE IF EXISTS user_status_changes;
