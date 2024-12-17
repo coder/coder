dbf1eb963e244a6d6d26cd364786b0f9b9ba07ee
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2024-12-17 09:23:20 +0000

    Add avatarLetters utility

--- site/src/components/Avatar/Avatar.tsx
+++ site/src/components/Avatar/Avatar.tsx
@@ -91,4 +91,12 @@ const AvatarFallback = React.forwardRef<
 ));
 AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
 
-export { Avatar, AvatarImage, AvatarFallback };
+function avatarLetters(str: string): string {
+	const words = str.split(" ");
+	if (words.length === 1) {
+		return words[0].charAt(0).toUpperCase();
+	}
+	return (words[0].charAt(0) + words[1].charAt(0)).toUpperCase();
+}
+
+export { Avatar, AvatarImage, AvatarFallback, avatarLetters };
