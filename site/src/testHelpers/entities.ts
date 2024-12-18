6b8858ee0cc79bd6352606d9fd94639e4dea2cee
defelmnq <vincent@coder.com>
2024-12-18 00:17:23 +0000

    work on ts tests

--- site/src/testHelpers/entities.ts
+++ site/src/testHelpers/entities.ts
@@ -865,7 +865,7 @@ export const MockWorkspaceApp: TypesGen.WorkspaceApp = {
 		threshold: 0,
 	},
 	hidden: false,
-	open_in: "",
+	open_in: "slim-window",
 };
 
 export const MockWorkspaceAgentLogSource: TypesGen.WorkspaceAgentLogSource = {
