904b857742b21168c86bbfea970e0d284459b77d
Cian Johnston <cian@coder.com>
2024-12-19 11:25:24 +0000

    typo fix

--- enterprise/coderd/coderdenttest/coderdenttest.go
+++ enterprise/coderd/coderdenttest/coderdenttest.go
@@ -121,7 +121,7 @@ func NewWithAPI(t *testing.T, options *Options) (
 	var provisionerCloser io.Closer = nopcloser{}
 	if options.IncludeProvisionerDaemon {
 		provisionerCloser = coderdtest.NewProvisionerDaemon(t, coderAPI.AGPL)
-		// Wait for provisioner daemons to be connected befroe continuing
+		// Wait for provisioner daemons to be connected before continuing
 		coderdtest.AwaitProvisionerDaemonsConnected(t, coderAPI.AGPL)
 	}
 
