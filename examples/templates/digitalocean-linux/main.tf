03edd604a79817f35d2de6b37523728ccf772c49
Phorcys <57866459+phorcys420@users.noreply.github.com>
2024-12-24 20:52:23 +0100

    chore(examples/templates/digitalocean-linux): make template more friendly (#15938)

--- examples/templates/digitalocean-linux/main.tf
+++ examples/templates/digitalocean-linux/main.tf
@@ -9,30 +9,29 @@ terraform {
   }
 }
 
-provider "coder" {
-}
+provider "coder" {}
 
-variable "step1_do_project_id" {
+variable "project_uuid" {
   type        = string
   description = <<-EOF
-    Enter project ID
+    DigitalOcean project ID
 
       $ doctl projects list
   EOF
   sensitive   = true
 
   validation {
-    # make sure length of alphanumeric string is 36
-    condition     = length(var.step1_do_project_id) == 36
+    # make sure length of alphanumeric string is 36 (UUIDv4 size)
+    condition     = length(var.project_uuid) == 36
     error_message = "Invalid Digital Ocean Project ID."
   }
 
 }
 
-variable "step2_do_admin_ssh_key" {
+variable "ssh_key_id" {
   type        = number
   description = <<-EOF
-    Enter admin SSH key ID (some Droplet images require an SSH key to be set):
+    DigitalOcean SSH key ID (some Droplet images require an SSH key to be set):
 
     Can be set to "0" for no key.
 
@@ -41,9 +40,10 @@ variable "step2_do_admin_ssh_key" {
       $ doctl compute ssh-key list
   EOF
   sensitive   = true
+  default     = 0
 
   validation {
-    condition     = var.step2_do_admin_ssh_key >= 0
+    condition     = var.ssh_key_id >= 0
     error_message = "Invalid Digital Ocean SSH key ID, a number is required."
   }
 }
@@ -291,11 +291,11 @@ resource "digitalocean_droplet" "workspace" {
     coder_agent_token = coder_agent.main.token
   })
   # Required to provision Fedora.
-  ssh_keys = var.step2_do_admin_ssh_key > 0 ? [var.step2_do_admin_ssh_key] : []
+  ssh_keys = var.ssh_key_id > 0 ? [var.ssh_key_id] : []
 }
 
 resource "digitalocean_project_resources" "project" {
-  project = var.step1_do_project_id
+  project = var.project_uuid
   # Workaround for terraform plan when using count.
   resources = length(digitalocean_droplet.workspace) > 0 ? [
     digitalocean_volume.home_volume.urn,
