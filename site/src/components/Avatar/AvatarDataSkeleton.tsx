b2d3190ca9ac04ebe54a2fa42d43f420a3108923
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2025-01-07 12:31:26 +0000

    fix: adjust template row alignment when missing description

--- site/src/components/Avatar/AvatarDataSkeleton.tsx
+++ site/src/components/Avatar/AvatarDataSkeleton.tsx
@@ -4,8 +4,8 @@ import type { FC } from "react";
 
 export const AvatarDataSkeleton: FC = () => {
 	return (
-		<Stack spacing={1.5} direction="row" alignItems="center">
-			<Skeleton variant="circular" width={36} height={36} />
+		<Stack spacing={1} direction="row" className="w-full">
+			<Skeleton variant="rectangular" className="size-6 rounded-sm" />
 
 			<Stack spacing={0}>
 				<Skeleton variant="text" width={100} />
