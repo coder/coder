2c262fd09dba11b0ea47477304f276967aef9254
Jaayden Halko <jaayden.halko@gmail.com>
2025-01-08 21:24:05 +0000

    chore: cleanup

--- site/src/components/Sidebar/Sidebar.tsx
+++ site/src/components/Sidebar/Sidebar.tsx
@@ -67,7 +67,7 @@ export const SettingsSidebarNavItem: FC<SettingsSidebarNavItemProps> = ({
 		<NavLink
 			end={end}
 			to={href}
-			className={({ isActive }) =>
+			className={() =>
 				cn(
 					"relative text-sm text-content-secondary no-underline font-medium py-2 px-3 hover:bg-surface-secondary rounded-md transition ease-in-out duration-150",
 					{
