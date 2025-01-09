0499bdaa043734fd036074b72a8f7552734fafca
Aaron Lehmann <alehmann@netflix.com>
2025-01-09 10:54:54 -0800

    feat: --ssh-host-prefix flag for "coder ssh"
    This adds a flag matching `--ssh-host-prefix` from `coder config-ssh` to
    `coder ssh`. By trimming a custom prefix from the argument, we can set
    up wildcard-based `Host` entries in SSH config for the IDE plugins (and
    eventually `coder config-ssh`).
    
    We also replace `--` in the argument with `/`, so ownership can be
    specified in wildcard-based SSH hosts like `<owner>--<workspace>`.

--- cli/ssh_test.go
+++ cli/ssh_test.go
@@ -1568,6 +1568,69 @@ func TestSSH(t *testing.T) {
 			})
 		}
 	})
+
+	t.Run("SSHHostPrefix", func(t *testing.T) {
+		t.Parallel()
+		client, workspace, agentToken := setupWorkspaceForAgent(t)
+		_, _ = tGoContext(t, func(ctx context.Context) {
+			// Run this async so the SSH command has to wait for
+			// the build and agent to connect!
+			_ = agenttest.New(t, client.URL, agentToken)
+			<-ctx.Done()
+		})
+
+		clientOutput, clientInput := io.Pipe()
+		serverOutput, serverInput := io.Pipe()
+		defer func() {
+			for _, c := range []io.Closer{clientOutput, clientInput, serverOutput, serverInput} {
+				_ = c.Close()
+			}
+		}()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testutil.WaitLong)
+		defer cancel()
+
+		user, err := client.User(ctx, codersdk.Me)
+		require.NoError(t, err)
+
+		inv, root := clitest.New(t, "ssh", "--stdio", "--ssh-host-prefix", "coder.dummy.com--", fmt.Sprintf("coder.dummy.com--%s--%s", user.Username, workspace.Name))
+		clitest.SetupConfig(t, client, root)
+		inv.Stdin = clientOutput
+		inv.Stdout = serverInput
+		inv.Stderr = io.Discard
+
+		cmdDone := tGo(t, func() {
+			err := inv.WithContext(ctx).Run()
+			assert.NoError(t, err)
+		})
+
+		conn, channels, requests, err := ssh.NewClientConn(&stdioConn{
+			Reader: serverOutput,
+			Writer: clientInput,
+		}, "", &ssh.ClientConfig{
+			// #nosec
+			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		})
+		require.NoError(t, err)
+		defer conn.Close()
+
+		sshClient := ssh.NewClient(conn, channels, requests)
+		session, err := sshClient.NewSession()
+		require.NoError(t, err)
+		defer session.Close()
+
+		command := "sh -c exit"
+		if runtime.GOOS == "windows" {
+			command = "cmd.exe /c exit"
+		}
+		err = session.Run(command)
+		require.NoError(t, err)
+		err = sshClient.Close()
+		require.NoError(t, err)
+		_ = clientOutput.Close()
+
+		<-cmdDone
+	})
 }
 
 //nolint:paralleltest // This test uses t.Setenv, parent test MUST NOT be parallel.
--- docs/reference/cli/ssh.md
+++ docs/reference/cli/ssh.md
@@ -20,6 +20,15 @@ coder ssh [flags] <workspace>
 
 Specifies whether to emit SSH output over stdin/stdout.
 
+### --ssh-host-prefix
+
+|             |                                         |
+|-------------|-----------------------------------------|
+| Type        | <code>string</code>                     |
+| Environment | <code>$CODER_SSH_SSH_HOST_PREFIX</code> |
+
+Strip this prefix from the provided hostname to determine the workspace name. This is useful when used as part of an OpenSSH proxy command.
+
 ### -A, --forward-agent
 
 |             |                                       |
