5b23899f633d44d8066a751f5a1135f3ab93b598
Jaayden Halko <jaayden.halko@gmail.com>
2025-01-14 15:08:45 +0000

    fix: update input component to 40px height

--- site/src/components/Input/Input.tsx
+++ site/src/components/Input/Input.tsx
@@ -13,7 +13,7 @@ export const Input = forwardRef<
 		<input
 			type={type}
 			className={cn(
-				`flex h-9 w-full rounded-md border border-border border-solid bg-transparent px-3 py-1
+				`flex h-10 w-full rounded-md border border-border border-solid bg-transparent px-3
 				text-base shadow-sm transition-colors
 				file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-content-primary
 				placeholder:text-content-secondary
