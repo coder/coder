034c35919abb76dff557ad5d20fbae87321bf8d1
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2025-01-14 14:52:45 +0000

    Implement User Engagement component

---
+++ site/src/pages/DeploymentSettingsPage/GeneralSettingsPage/UserEngagementChart.tsx
@@ -0,0 +1,158 @@
+import { Button } from "components/Button/Button";
+import {
+	ChartContainer,
+	ChartTooltip,
+	ChartTooltipContent,
+	type ChartConfig,
+} from "components/Chart/Chart";
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from "components/Collapsible/Collapsible";
+import { ChevronDownIcon, ExternalLinkIcon } from "lucide-react";
+import type { FC } from "react";
+import { Link } from "react-router-dom";
+import { CartesianGrid, XAxis, Area, AreaChart, YAxis } from "recharts";
+
+const chartConfig = {
+	users: {
+		label: "Users",
+		color: "hsl(var(--chart-1))",
+	},
+} satisfies ChartConfig;
+
+export type UserEngagementChartProps = {
+	data: {
+		date: string;
+		users: number;
+	}[];
+};
+
+export const UserEngagementChart: FC<UserEngagementChartProps> = ({ data }) => {
+	return (
+		<section className="border border-solid rounded">
+			<div className="p-4">
+				<Collapsible>
+					<header className="flex flex-col gap-2 items-start">
+						<h3 className="text-md m-0 font-medium">User Engagement</h3>
+
+						<CollapsibleTrigger asChild>
+							<Button className="h-auto p-0 border-0 bg-transparent font-medium text-content-secondary hover:bg-transparent hover:text-content-primary">
+								<ChevronDownIcon />
+								How we calculate engaged users
+							</Button>
+						</CollapsibleTrigger>
+					</header>
+
+					<CollapsibleContent className="pt-2 pl-7 pr-5 space-y-4 [&_p]:m-0 [&_p]:text-sm [&_p]:text-content-secondary font-medium">
+						<p>
+							We consider a user “engaged” if they initiate a connection to
+							their workspace. The connection can be made through apps, web
+							terminal or SSH.
+						</p>
+						<p>
+							The graph shows the number of unique users who were engaged at
+							least once during the day.
+						</p>
+						<div>
+							<p>You might also check:</p>
+							<ul className="list-none p-0 m-0 [&_a]:text-sm [&_a]:font-medium [&_a]:text-content-link [&_a]:no-underline [&_a]:flex [&_a]:items-center [&_a]:gap-0.5 [&_svg]:p-[2px]">
+								<li>
+									<Link to="/audit">
+										Activity Audit
+										<ExternalLinkIcon className="size-icon-sm" />
+									</Link>
+								</li>
+								<li>
+									<Link to="/deployment/licenses">
+										License Consumption
+										<ExternalLinkIcon className="size-icon-sm" />
+									</Link>
+								</li>
+							</ul>
+						</div>
+					</CollapsibleContent>
+				</Collapsible>
+			</div>
+
+			<div className="p-6 border-0 border-t border-solid">
+				<ChartContainer config={chartConfig} className="aspect-auto h-64">
+					<AreaChart
+						accessibilityLayer
+						data={data}
+						margin={{
+							left: 0,
+							right: 0,
+						}}
+					>
+						<CartesianGrid vertical={false} />
+						<XAxis
+							dataKey="date"
+							tickLine={false}
+							tickMargin={12}
+							tickFormatter={(value: string) =>
+								new Date(value).toLocaleDateString(undefined, {
+									month: "short",
+									day: "numeric",
+								})
+							}
+						/>
+						<YAxis
+							dataKey="users"
+							tickLine={false}
+							axisLine={false}
+							tickMargin={12}
+							tickFormatter={(value: number) => {
+								return value === 0 ? "" : value.toLocaleString();
+							}}
+						/>
+						<ChartTooltip
+							cursor={false}
+							content={
+								<ChartTooltipContent
+									className="font-medium text-content-secondary"
+									labelClassName="text-content-primary"
+									labelFormatter={(_, p) => {
+										const item = p[0];
+										return `${item.value} users`;
+									}}
+									formatter={(v, n, item) => {
+										const date = new Date(item.payload.date);
+										return date.toLocaleString(undefined, {
+											month: "long",
+											day: "2-digit",
+										});
+									}}
+								/>
+							}
+						/>
+						<defs>
+							<linearGradient id="fillUsers" x1="0" y1="0" x2="0" y2="1">
+								<stop
+									offset="5%"
+									stopColor="var(--color-users)"
+									stopOpacity={0.8}
+								/>
+								<stop
+									offset="95%"
+									stopColor="var(--color-users)"
+									stopOpacity={0.1}
+								/>
+							</linearGradient>
+						</defs>
+
+						<Area
+							dataKey="users"
+							type="natural"
+							fill="url(#fillUsers)"
+							fillOpacity={0.4}
+							stroke="var(--color-users)"
+							stackId="a"
+						/>
+					</AreaChart>
+				</ChartContainer>
+			</div>
+		</section>
+	);
+};
