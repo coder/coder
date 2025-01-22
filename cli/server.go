d807c7eb0de18ac55716a16b1196b41f273f12ba
Danny Kopping <danny@coder.com>
2025-01-22 12:33:32 +0000

    WIP on control loop
    Signed-off-by: Danny Kopping <danny@coder.com>

--- cli/server.go
+++ cli/server.go
@@ -31,6 +31,8 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/coder/coder/v2/coderd/prebuilds"
+
 	"github.com/charmbracelet/lipgloss"
 	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/coreos/go-systemd/daemon"
@@ -940,6 +942,11 @@ func (r *RootCmd) Server(newAPI func(context.Context, *coderd.Options) (*coderd.
 			} else {
 				cliui.Info(inv.Stdout, "Notifications are currently disabled as there are no configured delivery methods. See https://coder.com/docs/admin/monitoring/notifications#delivery-methods for more details.")
 			}
+
+			// TODO: implement experiment and configs
+			prebuildsCtrl := prebuilds.NewController(logger.Named("prebuilds.controller"), options.Database)
+			go prebuildsCtrl.Loop(ctx)
+			defer prebuildsCtrl.Stop()
 
 			// Since errCh only has one buffered slot, all routines
 			// sending on it must be wrapped in a select/default to
