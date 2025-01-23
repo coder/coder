795b6502bbcd51264b3a7f7f63469dd41e301bb8
Danny Kopping <danny@coder.com>
2025-01-23 14:24:29 +0000

    Incorporate in-progress jobs into state calculation
    Signed-off-by: Danny Kopping <danny@coder.com>

--- coderd/database/migrations/000290_prebuilds.up.sql
+++ coderd/database/migrations/000290_prebuilds.up.sql
@@ -9,7 +9,7 @@ FROM workspaces
 WHERE owner_id = 'c42fdf75-3097-471c-8c33-fb52454d81c0';
 
 CREATE VIEW workspace_prebuild_builds AS
-SELECT workspace_id
+SELECT *
 FROM workspace_builds
 WHERE initiator_id = 'c42fdf75-3097-471c-8c33-fb52454d81c0';
 
