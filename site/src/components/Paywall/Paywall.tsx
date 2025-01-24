f1b981328753a16287ef31caa060120db2c19b52
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2025-01-24 12:40:19 +0000

    Use paywall button as contained

--- site/src/components/Paywall/Paywall.tsx
+++ site/src/components/Paywall/Paywall.tsx
@@ -56,7 +56,7 @@ export const Paywall: FC<PaywallProps> = ({
 					</li>
 				</ul>
 				<div css={styles.learnButton}>
-					<Button variant="outline" asChild>
+					<Button asChild>
 						<a
 							href="https://coder.com/pricing#compare-plans"
 							target="_blank"
