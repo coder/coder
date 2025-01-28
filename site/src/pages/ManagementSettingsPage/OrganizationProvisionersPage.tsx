d32a5e1a01f37296a5cca31c48e1d4af7e9da3f7
Jaayden Halko <jaayden.halko@gmail.com>
2025-01-28 20:47:05 +0000

    fix: fix UI bugs for orgs (#16302)
    resolves coder/internal#288
    resolves coder/internal#287
    
    Fixes alignment issue on create organizations page when license banner
    is visible.
    
    before
    <img width="1178" alt="Screenshot 2025-01-27 at 18 36 35"
    src="https://github.com/user-attachments/assets/b845f1df-adb3-4876-aba5-8fa9d56eb22b"
    />
    
    after
    <img width="961" alt="Screenshot 2025-01-27 at 18 47 06"
    src="https://github.com/user-attachments/assets/4a6374f9-9571-453c-9258-9cc4631afb77"
    />

--- site/src/pages/ManagementSettingsPage/OrganizationProvisionersPage.tsx
+++ site/src/pages/ManagementSettingsPage/OrganizationProvisionersPage.tsx
@@ -29,7 +29,10 @@ const OrganizationProvisionersPage: FC = () => {
 		<>
 			<Helmet>
 				<title>
-					{pageTitle("Members", organization.display_name || organization.name)}
+					{pageTitle(
+						"Provisioners",
+						organization.display_name || organization.name,
+					)}
 				</title>
 			</Helmet>
 			<OrganizationProvisionersPageView
