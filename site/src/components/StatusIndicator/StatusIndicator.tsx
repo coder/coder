1d785339b6d613ddd464b53a2ddf9e01d6ac875e
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2025-02-05 18:56:14 +0000

    Apply fmt

--- site/src/components/StatusIndicator/StatusIndicator.tsx
+++ site/src/components/StatusIndicator/StatusIndicator.tsx
@@ -1,5 +1,5 @@
-import { cva, type VariantProps } from "class-variance-authority";
-import { createContext, useContext, type FC } from "react";
+import { type VariantProps, cva } from "class-variance-authority";
+import { type FC, createContext, useContext } from "react";
 import { cn } from "utils/cn";
 
 const statusIndicatorVariants = cva(
