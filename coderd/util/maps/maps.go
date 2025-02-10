795ffda6669fcf78e9f3aaf3a0a69566d4f4fd46
Cian Johnston <cian@coder.com>
2025-02-10 14:21:59 +0000

    fix(coderd/util/maps): ignore zero values in Subset

--- coderd/util/maps/maps.go
+++ coderd/util/maps/maps.go
@@ -8,9 +8,14 @@ import (
 
 // Subset returns true if all the keys of a are present
 // in b and have the same values.
+// If the corresponding value of a[k] is the zero value in
+// b, Subset will skip comparing that value.
+// This allows checking for the presence of map keys.
 func Subset[T, U comparable](a, b map[T]U) bool {
+	var uz U
 	for ka, va := range a {
-		if vb, ok := b[ka]; !ok || va != vb {
+		ignoreZeroValue := va == uz
+		if vb, ok := b[ka]; !ok || (!ignoreZeroValue && va != vb) {
 			return false
 		}
 	}
