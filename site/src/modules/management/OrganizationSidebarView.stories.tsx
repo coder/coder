54c1ba4dab9a26902cbbec73028261f3bbd54a3e
Kira Pilot <kira@coder.com>
2025-02-14 23:26:11 +0000

    sort imports

--- site/src/modules/management/OrganizationSidebarView.stories.tsx
+++ site/src/modules/management/OrganizationSidebarView.stories.tsx
@@ -1,12 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { expect, userEvent, waitFor, within } from "@storybook/test";
+import type { AuthorizationResponse } from "api/typesGenerated";
 import {
 	MockNoPermissions,
 	MockOrganization,
 	MockOrganization2,
 	MockPermissions,
 } from "testHelpers/entities";
-import type { AuthorizationResponse } from "api/typesGenerated";
 import { withDashboardProvider } from "testHelpers/storybook";
 import {
 	OrganizationSidebarView,
