7800e9e34db0d8a2fc24c36af95dd1d79de7d2a0
Marcin Tojek <marcin@coder.com>
2025-02-14 13:59:26 +0100

    fix

--- site/src/modules/dashboard/Navbar/MobileMenu.test.tsx
+++ site/src/modules/dashboard/Navbar/MobileMenu.test.tsx
@@ -8,14 +8,14 @@ describe("support link", () => {
 			origin: mockOrigin,
 		}; // Mock the location origin
 
-		expect(includeOrigin("/test")).toBe(mockOrigin + "/test");
+		expect(includeOrigin("/test")).toBe(`${mockOrigin}/test`);
 		expect(includeOrigin("/path/to/resource")).toBe(
-			mockOrigin + "/path/to/resource",
+			`${mockOrigin}/path/to/resource`,
 		);
 	});
 
 	it("should return the target unchanged if it does not start with '/'", () => {
-		expect(includeOrigin(mockOrigin + "/page")).toBe(mockOrigin + "/page");
+		expect(includeOrigin(`${mockOrigin}/page`)).toBe(`${mockOrigin}/page`);
 		expect(includeOrigin("../relative/path")).toBe("../relative/path");
 		expect(includeOrigin("relative/path")).toBe("relative/path");
 	});
