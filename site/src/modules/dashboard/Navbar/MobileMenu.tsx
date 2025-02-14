4d1274237b1e0780d80ada366f011560bfacb7c3
Marcin Tojek <marcin@coder.com>
2025-02-14 12:36:37 +0100

    fmt/biome

--- site/src/modules/dashboard/Navbar/MobileMenu.tsx
+++ site/src/modules/dashboard/Navbar/MobileMenu.tsx
@@ -307,7 +307,11 @@ const UserSettingsSub: FC<UserSettingsSubProps> = ({
 								asChild
 								className={cn(itemStyles.default, itemStyles.sub)}
 							>
-								<a href={includeOrigin(l.target)} target="_blank" rel="noreferrer">
+								<a
+									href={includeOrigin(l.target)}
+									target="_blank"
+									rel="noreferrer"
+								>
 									{l.name}
 								</a>
 							</DropdownMenuItem>
