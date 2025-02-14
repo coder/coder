7b3cdce034f96d45e3ca7586e37bb951f4e44c87
Marcin Tojek <marcin@coder.com>
2025-02-14 13:47:46 +0100

    fix

--- site/src/modules/dashboard/Navbar/MobileMenu.test.tsx
+++ site/src/modules/dashboard/Navbar/MobileMenu.test.tsx
@@ -1,21 +1,19 @@
 import { includeOrigin } from "./MobileMenu";
 
+const mockOrigin = "https://example.com";
+
 describe("support link", () => {
 	it("should include origin if target starts with '/'", () => {
-		const mockOrigin = "https://example.com";
+		(window as unknown as { location: Partial<Location> }).location = { origin: mockOrigin }; // Mock the location origin
 
-		window.location = { origin: mockOrigin }; // Mock the location origin
-
-		expect(includeOrigin("/test")).toBe("https://example.com/test");
+		expect(includeOrigin("/test")).toBe(mockOrigin + "/test");
 		expect(includeOrigin("/path/to/resource")).toBe(
-			"https://example.com/path/to/resource",
+			mockOrigin + "/path/to/resource",
 		);
 	});
 
 	it("should return the target unchanged if it does not start with '/'", () => {
-		expect(includeOrigin("https://example.com/page")).toBe(
-			"https://example.com/page",
-		);
+		expect(includeOrigin(mockOrigin + "/page")).toBe(mockOrigin + "/page");
 		expect(includeOrigin("../relative/path")).toBe("../relative/path");
 		expect(includeOrigin("relative/path")).toBe("relative/path");
 	});
