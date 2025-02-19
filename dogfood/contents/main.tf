4f671b30004c92d09ec0d30bc6250826a3a5e7fa
defelmnq <vincent@coder.com>
2025-02-19 06:44:01 +0000

    fix version for provider

--- dogfood/contents/main.tf
+++ dogfood/contents/main.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     coder = {
-      source = "coder/coder"
+      source  = "coder/coder"
+      version = "2.2.0-pre0"
     }
     docker = {
       source  = "kreuzwerker/docker"
