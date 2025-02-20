a656cb84b4c47ed91da389e42c03494e39145bbc
Kira Pilot <kira@coder.com>
2025-02-20 02:05:03 +0000

    fix: default org selection in template form if only one

---
+++ site/src/components/OrganizationAutocomplete/OrganizationAutocomplete.stories.tsx
@@ -0,0 +1,61 @@
+import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from "@storybook/react";
+import { userEvent, within } from "@storybook/test";
+import {
+	MockOrganization,
+	MockOrganization2,
+	MockUser,
+} from "testHelpers/entities";
+import { OrganizationAutocomplete } from "./OrganizationAutocomplete";
+
+const meta: Meta<typeof OrganizationAutocomplete> = {
+	title: "components/OrganizationAutocomplete",
+	component: OrganizationAutocomplete,
+	args: {
+		value: null,
+		onChange: action("Selected organization"),
+	},
+};
+
+export default meta;
+type Story = StoryObj<typeof OrganizationAutocomplete>;
+
+export const ManyOrgs: Story = {
+	parameters: {
+		showOrganizations: true,
+		user: MockUser,
+		features: ["multiple_organizations"],
+		permissions: { viewDeploymentValues: true },
+		queries: [
+			{
+				key: ["organizations"],
+				data: [MockOrganization, MockOrganization2],
+			},
+		],
+	},
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		const button = canvas.getByRole("button");
+		await userEvent.click(button);
+	},
+};
+
+export const OneOrg: Story = {
+	parameters: {
+		showOrganizations: true,
+		user: MockUser,
+		features: ["multiple_organizations"],
+		permissions: { viewDeploymentValues: true },
+		queries: [
+			{
+				key: ["organizations"],
+				data: [MockOrganization],
+			},
+		],
+	},
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		const button = canvas.getByRole("button");
+		await userEvent.click(button);
+	},
+};
