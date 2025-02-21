56260b095f8efd68bc2328bacda5838f6505e435
Jaayden Halko <jaayden.halko@gmail.com>
2025-02-21 12:41:00 +0000

    fix: fix frontend display of error messages

--- site/src/pages/OrganizationSettingsPage/OrganizationSettingsPage.tsx
+++ site/src/pages/OrganizationSettingsPage/OrganizationSettingsPage.tsx
@@ -1,9 +1,11 @@
+import { getErrorMessage } from "api/errors";
 import {
 	deleteOrganization,
 	updateOrganization,
 } from "api/queries/organizations";
 import { EmptyState } from "components/EmptyState/EmptyState";
 import { displaySuccess } from "components/GlobalSnackbar/utils";
+import { displayError } from "components/GlobalSnackbar/utils";
 import { useOrganizationSettings } from "modules/management/OrganizationSettingsLayout";
 import type { FC } from "react";
 import { useMutation, useQueryClient } from "react-query";
@@ -42,10 +44,19 @@ const OrganizationSettingsPage: FC = () => {
 				navigate(`/organizations/${updatedOrganization.name}/settings`);
 				displaySuccess("Organization settings updated.");
 			}}
-			onDeleteOrganization={() => {
-				deleteOrganizationMutation.mutate(organization.id);
-				displaySuccess("Organization deleted.");
-				navigate("/organizations");
+			onDeleteOrganization={async () => {
+				try {
+					await deleteOrganizationMutation.mutateAsync(organization.id);
+					displaySuccess(`Organization ${organization.name} deleted`);
+					navigate("/organizations");
+				} catch (error) {
+					displayError(
+						getErrorMessage(
+							error,
+							`Failed to delete organization: ${organization.name}`,
+						),
+					);
+				}
 			}}
 		/>
 	);
