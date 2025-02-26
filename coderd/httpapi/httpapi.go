50654558720b38900d811c26676e30a5ff7b9cbf
Michael Smith <throwawayclover@gmail.com>
2025-02-26 23:20:50 +0000

    chore: delete ServerSentEventSender

--- coderd/httpapi/httpapi.go
+++ coderd/httpapi/httpapi.go
@@ -1,7 +1,6 @@
 package httpapi
 
 import (
-	"bytes"
 	"context"
 	"database/sql"
 	"encoding/json"
@@ -11,7 +10,6 @@ import (
 	"net/http"
 	"reflect"
 	"strings"
-	"time"
 
 	"github.com/go-playground/validator/v10"
 	"golang.org/x/xerrors"
@@ -354,112 +352,6 @@ func OneWayWebSocket[T any](rw http.ResponseWriter, r *http.Request) (
 			return ctx.Err()
 		}
 		return nil
-	}
-
-	return sendEvent, closed, nil
-}
-
-func ServerSentEventSender(rw http.ResponseWriter, r *http.Request) (sendEvent func(ctx context.Context, sse codersdk.ServerSentEvent) error, closed chan struct{}, err error) {
-	h := rw.Header()
-	h.Set("Content-Type", "text/event-stream")
-	h.Set("Cache-Control", "no-cache")
-	h.Set("Connection", "keep-alive")
-	h.Set("X-Accel-Buffering", "no")
-
-	f, ok := rw.(http.Flusher)
-	if !ok {
-		panic("http.ResponseWriter is not http.Flusher")
-	}
-
-	closed = make(chan struct{})
-	type sseEvent struct {
-		payload []byte
-		errC    chan error
-	}
-	eventC := make(chan sseEvent)
-
-	// Synchronized handling of events (no guarantee of order).
-	go func() {
-		defer close(closed)
-
-		// Send a heartbeat every 15 seconds to avoid the connection being killed.
-		ticker := time.NewTicker(time.Second * 15)
-		defer ticker.Stop()
-
-		for {
-			var event sseEvent
-
-			select {
-			case <-r.Context().Done():
-				return
-			case event = <-eventC:
-			case <-ticker.C:
-				event = sseEvent{
-					payload: []byte(fmt.Sprintf("event: %s\n\n", codersdk.ServerSentEventTypePing)),
-				}
-			}
-
-			_, err := rw.Write(event.payload)
-			if event.errC != nil {
-				event.errC <- err
-			}
-			if err != nil {
-				return
-			}
-			f.Flush()
-		}
-	}()
-
-	sendEvent = func(ctx context.Context, sse codersdk.ServerSentEvent) error {
-		buf := &bytes.Buffer{}
-		enc := json.NewEncoder(buf)
-
-		_, err := buf.WriteString(fmt.Sprintf("event: %s\n", sse.Type))
-		if err != nil {
-			return err
-		}
-
-		if sse.Data != nil {
-			_, err = buf.WriteString("data: ")
-			if err != nil {
-				return err
-			}
-			err = enc.Encode(sse.Data)
-			if err != nil {
-				return err
-			}
-		}
-
-		err = buf.WriteByte('\n')
-		if err != nil {
-			return err
-		}
-
-		event := sseEvent{
-			payload: buf.Bytes(),
-			errC:    make(chan error, 1), // Buffered to prevent deadlock.
-		}
-
-		select {
-		case <-r.Context().Done():
-			return r.Context().Err()
-		case <-ctx.Done():
-			return ctx.Err()
-		case <-closed:
-			return xerrors.New("server sent event sender closed")
-		case eventC <- event:
-			// Re-check closure signals after sending the event to allow
-			// for early exit. We don't check closed here because it
-			// can't happen while processing the event.
-			select {
-			case <-r.Context().Done():
-				return r.Context().Err()
-			case <-ctx.Done():
-				return ctx.Err()
-			case err := <-event.errC:
-				return err
-			}
-		}
 	}
 
 	return sendEvent, closed, nil
