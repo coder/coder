63a33f9ee6f20dcb961fda3a5894880b63402854
Marcin Tojek <marcin@coder.com>
2025-02-27 14:04:23 +0100

    fix: handle undefined job while updating build progress

--- site/src/pages/WorkspacePage/WorkspaceBuildProgress.tsx
+++ site/src/pages/WorkspacePage/WorkspaceBuildProgress.tsx
@@ -81,6 +81,7 @@ export const WorkspaceBuildProgress: FC<WorkspaceBuildProgressProps> = ({
 	useEffect(() => {
 		const updateProgress = () => {
 			if (
+				job == undefined ||
 				job.status !== "running" ||
 				transitionStats.P50 === undefined ||
 				transitionStats.P95 === undefined ||
