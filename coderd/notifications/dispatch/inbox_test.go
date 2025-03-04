a11f505f4aea903bd37e5f8ae0de4c5a70563664
defelmnq <vincent@coder.com>
2025-03-04 11:24:22 +0000

    add missing inbox tests

---
+++ coderd/notifications/dispatch/inbox_test.go
@@ -0,0 +1,104 @@
+package dispatch_test
+
+import (
+	"context"
+	"testing"
+
+	"cdr.dev/slog"
+	"cdr.dev/slog/sloggers/slogtest"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+
+	"github.com/coder/coder/v2/coderd/database"
+	"github.com/coder/coder/v2/coderd/database/dbtestutil"
+	"github.com/coder/coder/v2/coderd/notifications"
+	"github.com/coder/coder/v2/coderd/notifications/dispatch"
+	"github.com/coder/coder/v2/coderd/notifications/types"
+)
+
+func TestInbox(t *testing.T) {
+	t.Parallel()
+
+	logger := slogtest.Make(t, &slogtest.Options{IgnoreErrors: true, IgnoredErrorIs: []error{}}).Leveled(slog.LevelDebug)
+	tests := []struct {
+		name          string
+		msgID         uuid.UUID
+		payload       types.MessagePayload
+		expectedErr   string
+		expectedRetry bool
+	}{
+		{
+			name:  "OK",
+			msgID: uuid.New(),
+			payload: types.MessagePayload{
+				NotificationName:       "test",
+				NotificationTemplateID: notifications.TemplateWorkspaceDeleted.String(),
+				UserID:                 "1e965b51-9465-43d8-ac20-c5f689f9c788",
+				Actions: []types.TemplateAction{
+					{
+						Label: "View my workspace",
+						URL:   "https://coder.com/workspaces/1",
+					},
+				},
+			},
+		},
+		{
+			name: "InvalidUserID",
+			payload: types.MessagePayload{
+				NotificationName:       "test",
+				NotificationTemplateID: notifications.TemplateWorkspaceDeleted.String(),
+				UserID:                 "invalid",
+				Actions:                []types.TemplateAction{},
+			},
+			expectedErr:   "parse user ID",
+			expectedRetry: false,
+		},
+		{
+			name: "InvalidTemplateID",
+			payload: types.MessagePayload{
+				NotificationName:       "test",
+				NotificationTemplateID: "invalid",
+				UserID:                 "1e965b51-9465-43d8-ac20-c5f689f9c788",
+				Actions:                []types.TemplateAction{},
+			},
+			expectedErr:   "parse template ID",
+			expectedRetry: false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			db, _ := dbtestutil.NewDB(t)
+			ctx := context.Background()
+
+			handler := dispatch.NewInboxHandler(logger.Named("smtp"), db)
+			dispatcherFunc, err := handler.Dispatcher(tc.payload, "", "", nil)
+			require.NoError(t, err)
+
+			retryable, err := dispatcherFunc(ctx, tc.msgID)
+
+			if tc.expectedErr != "" {
+				require.ErrorContains(t, err, tc.expectedErr)
+				if tc.expectedRetry {
+					require.True(t, retryable)
+				} else {
+					require.False(t, retryable)
+				}
+			} else {
+				require.NoError(t, err)
+				require.False(t, retryable)
+				uid := uuid.MustParse(tc.payload.UserID)
+				notifs, err := db.GetInboxNotificationsByUserID(ctx, database.GetInboxNotificationsByUserIDParams{
+					UserID: uid,
+				})
+
+				require.NoError(t, err)
+				require.Len(t, notifs, 1)
+				require.Equal(t, tc.msgID, notifs[0].ID)
+			}
+		})
+	}
+}
