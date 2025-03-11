4d29e93940603fbfe638396f774c8e43667ced0d
Mathias Fredriksson <mafredri@gmail.com>
2025-03-11 12:04:06 +0200

    Update agent/agentssh/agentssh.go

--- agent/agentssh/agentssh.go
+++ agent/agentssh/agentssh.go
@@ -900,7 +900,7 @@ func (s *Server) CreateCommand(ctx context.Context, script string, env []string,
 		cmd.Dir = homedir
 	}
 	cmd.Env = append(ei.Environ(), env...)
-	// Set login variables.
+	// Set login variables (see `man login`).
 	cmd.Env = append(cmd.Env, fmt.Sprintf("USER=%s", username))
 	cmd.Env = append(cmd.Env, fmt.Sprintf("LOGNAME=%s", username))
 	cmd.Env = append(cmd.Env, fmt.Sprintf("SHELL=%s", shell))
