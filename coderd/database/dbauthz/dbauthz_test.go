b40d37b7be7e0578c86e7eb0802ce3683347ff87
Danny Kopping <dannykopping@gmail.com>
2025-03-12 11:12:45 +0000

    Implement missing dbauthz tests
    Signed-off-by: Danny Kopping <dannykopping@gmail.com>

--- coderd/database/dbauthz/dbauthz_test.go
+++ coderd/database/dbauthz/dbauthz_test.go
@@ -4642,6 +4642,72 @@ func (s *MethodTestSuite) TestNotifications() {
 	}))
 }
 
+func (s *MethodTestSuite) TestPrebuilds() {
+	s.Run("ClaimPrebuild", s.Subtest(func(db database.Store, check *expects) {
+		check.Args(database.ClaimPrebuildParams{}).
+			Asserts(rbac.ResourceWorkspace, policy.ActionUpdate).
+			ErrorsWithInMemDB(dbmem.ErrUnimplemented).
+			ErrorsWithPG(sql.ErrNoRows)
+
+	}))
+	s.Run("GetPrebuildMetrics", s.Subtest(func(_ database.Store, check *expects) {
+		check.Args().
+			Asserts(rbac.ResourceTemplate, policy.ActionRead).
+			ErrorsWithInMemDB(dbmem.ErrUnimplemented)
+
+	}))
+	s.Run("GetPrebuildsInProgress", s.Subtest(func(_ database.Store, check *expects) {
+		check.Args().
+			Asserts(rbac.ResourceTemplate, policy.ActionRead).
+			ErrorsWithInMemDB(dbmem.ErrUnimplemented)
+
+	}))
+	s.Run("GetPresetsBackoff", s.Subtest(func(_ database.Store, check *expects) {
+		check.Args(time.Time{}).
+			Asserts(rbac.ResourceTemplate, policy.ActionRead).
+			ErrorsWithInMemDB(dbmem.ErrUnimplemented)
+
+	}))
+	s.Run("GetRunningPrebuilds", s.Subtest(func(_ database.Store, check *expects) {
+		check.Args().
+			Asserts(rbac.ResourceTemplate, policy.ActionRead).
+			ErrorsWithInMemDB(dbmem.ErrUnimplemented)
+
+	}))
+	s.Run("GetTemplatePresetsWithPrebuilds", s.Subtest(func(db database.Store, check *expects) {
+		user := dbgen.User(s.T(), db, database.User{})
+		check.Args(uuid.NullUUID{UUID: user.ID, Valid: true}).
+			Asserts(rbac.ResourceTemplate, policy.ActionRead).
+			ErrorsWithInMemDB(dbmem.ErrUnimplemented)
+
+	}))
+	s.Run("InsertPresetPrebuild", s.Subtest(func(db database.Store, check *expects) {
+		org := dbgen.Organization(s.T(), db, database.Organization{})
+		user := dbgen.User(s.T(), db, database.User{})
+		template := dbgen.Template(s.T(), db, database.Template{
+			CreatedBy:      user.ID,
+			OrganizationID: org.ID,
+		})
+		templateVersion := dbgen.TemplateVersion(s.T(), db, database.TemplateVersion{
+			TemplateID:     uuid.NullUUID{UUID: template.ID, Valid: true},
+			OrganizationID: org.ID,
+			CreatedBy:      user.ID,
+		})
+		preset := dbgen.Preset(s.T(), db, database.InsertPresetParams{
+			Name: coderdtest.RandomName(s.T()),
+			TemplateVersionID: templateVersion.ID,
+		})
+		check.Args(database.InsertPresetPrebuildParams{
+			ID:                  uuid.New(),
+			PresetID:            preset.ID,
+			DesiredInstances:    1,
+		}).
+			Asserts(rbac.ResourceSystem, policy.ActionCreate).
+			ErrorsWithInMemDB(dbmem.ErrUnimplemented)
+
+	}))
+}
+
 func (s *MethodTestSuite) TestOAuth2ProviderApps() {
 	s.Run("GetOAuth2ProviderApps", s.Subtest(func(db database.Store, check *expects) {
 		apps := []database.OAuth2ProviderApp{
