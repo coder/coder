769ae1d24c708e4e4ade40195939a4b9acd9f573
Danny Kopping <dannykopping@gmail.com>
2025-03-13 20:07:29 +0000

    removing unnecessary changes
    Signed-off-by: Danny Kopping <dannykopping@gmail.com>

--- enterprise/coderd/roles_test.go
+++ enterprise/coderd/roles_test.go
@@ -7,8 +7,6 @@ import (
 	"slices"
 	"testing"
 
-	"github.com/coder/coder/v2/coderd/database/dbtestutil"
-
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 
@@ -335,12 +333,6 @@ func TestCustomOrganizationRole(t *testing.T) {
 	// Verify deleting a custom role cascades to all members
 	t.Run("DeleteRoleCascadeMembers", func(t *testing.T) {
 		t.Parallel()
-
-		// TODO: we should not be returning the prebuilds user in OrganizationMembers, and this is not returned in dbmem.
-		if !dbtestutil.WillUsePostgres() {
-			t.Skip("This test requires postgres")
-		}
-
 		owner, first := coderdenttest.New(t, &coderdenttest.Options{
 			LicenseOptions: &coderdenttest.LicenseOptions{
 				Features: license.Features{
