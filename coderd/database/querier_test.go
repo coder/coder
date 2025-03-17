324fde215478eeac76ea5e97ab32d47b9f542d50
Danny Kopping <dannykopping@gmail.com>
2025-03-17 12:53:48 +0000

    Fixes
    Signed-off-by: Danny Kopping <dannykopping@gmail.com>

--- coderd/database/querier_test.go
+++ coderd/database/querier_test.go
@@ -15,7 +15,6 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"cdr.dev/slog/sloggers/slogtest"
-
 	"github.com/coder/coder/v2/coderd/coderdtest"
 	"github.com/coder/coder/v2/coderd/database"
 	"github.com/coder/coder/v2/coderd/database/db2sdk"
