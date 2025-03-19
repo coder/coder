fb9005a57f45670258746cbf64e708da19542eba
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2025-03-19 18:37:57 +0000

    Last try

--- site/src/modules/notifications/NotificationsInbox/InboxPopover.stories.tsx
+++ site/src/modules/notifications/NotificationsInbox/InboxPopover.stories.tsx
@@ -11,7 +11,7 @@ const meta: Meta<typeof InboxPopover> = {
 	},
 	render: (args) => {
 		return (
-			<div className="w-full max-w-screen-xl p-6 h-[720px]">
+			<div className="w-full max-w-screen-xl py-2 px-6">
 				<header className="flex justify-end">
 					<InboxPopover {...args} />
 				</header>
