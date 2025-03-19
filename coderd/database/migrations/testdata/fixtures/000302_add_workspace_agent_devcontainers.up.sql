013bb4557550bf694789627d57d8186bb472aa01
Mathias Fredriksson <mafredri@gmail.com>
2025-03-19 14:34:18 +0000

    add fixcture

---
+++ coderd/database/migrations/testdata/fixtures/000302_add_workspace_agent_devcontainers.up.sql
@@ -0,0 +1,15 @@
+INSERT INTO
+	workspace_agent_devcontainers (
+		agent_id,
+		created_at,
+		id,
+		workspace_folder,
+		config_path
+	)
+VALUES (
+	'45e89705-e09d-4850-bcec-f9a937f5d78d',
+	'2021-09-01 00:00:00',
+	'489c0a1d-387d-41f0-be55-63aa7c5d7b14',
+	'/workspace',
+	'/workspace/.devcontainer/devcontainer.json'
+)
