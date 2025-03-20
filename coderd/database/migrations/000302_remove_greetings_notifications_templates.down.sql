a844284d3be1f58642daeabd18aefb7d73f92be2
defelmnq <vincent@coder.com>
2025-03-20 13:24:45 +0000

    work on greetings

--- coderd/database/migrations/000302_remove_greetings_notifications_templates.down.sql
+++ coderd/database/migrations/000302_remove_greetings_notifications_templates.down.sql
@@ -60,3 +60,10 @@ UPDATE notification_templates SET body_template = E'Hi {{.UserName}},\n\n'||
 		E'This is a test notification.' WHERE id = 'c425f63e-716a-4bf4-ae24-78348f706c3f';
 UPDATE notification_templates SET body_template = E'Hi {{.UserName}},\n\n' ||
 					E'The template **{{.Labels.name}}** was deleted by **{{ .Labels.initiator }}**.\n\n' WHERE id = '29a09665-2a4c-403f-9648-54301670e7be';
+UPDATE notification_templates SET body_template = E'Hi {{.UserName}},\n\n'||
+		E'Your workspace **{{.Labels.name}}** has been marked as [**dormant**](https://coder.com/docs/templates/schedule#dormancy-threshold-enterprise) because of {{.Labels.reason}}.\n' ||
+		E'Dormant workspaces are [automatically deleted](https://coder.com/docs/templates/schedule#dormancy-auto-deletion-enterprise) after {{.Labels.timeTilDormant}} of inactivity.\n' ||
+		E'To prevent deletion, use your workspace with the link below.' WHERE id = '0ea69165-ec14-4314-91f1-69566ac3c5a0';
+UPDATE notification_templates SET body_template = E'Hi {{.UserName}},\n\n'||
+		E'Your workspace **{{.Labels.name}}** has been marked for **deletion** after {{.Labels.timeTilDormant}} of [dormancy](https://coder.com/docs/templates/schedule#dormancy-auto-deletion-enterprise) because of {{.Labels.reason}}.\n' ||
+		E'To prevent deletion, use your workspace with the link below.' WHERE id = '51ce2fdf-c9ca-4be1-8d70-628674f9bc42';
