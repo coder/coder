9f43a34234671877dc8636fa5d37bb81b7f38583
shpark <esifea1908@gmail.com>
2025-03-20 14:40:35 +0000

    feat(coderd): add OIDCLogoutResponse

--- codersdk/users.go
+++ codersdk/users.go
@@ -311,6 +311,11 @@ type UserParameter struct {
 	Value string `json:"value"`
 }
 
+// OIDCLogoutResponse represents the response for an OIDC logout request
+type OIDCLogoutResponse struct {
+	URL string `json:"oidc_logout_url"`
+}
+
 // UserAutofillParameters returns all recently used parameters for the given user.
 func (c *Client) UserAutofillParameters(ctx context.Context, user string, templateID uuid.UUID) ([]UserParameter, error) {
 	res, err := c.Request(ctx, http.MethodGet, fmt.Sprintf("/api/v2/users/%s/autofill-parameters?template_id=%s", user, templateID), nil)
