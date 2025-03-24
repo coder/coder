8fd074043a40a88810f45d61b65b84ff91daa7ec
Brett Kolodny <brett.kolodny@coder.com>
2025-03-24 15:49:57 +0000

    fix: consistent spacing in sql migrations

--- coderd/database/migrations/000306_update_protect_deleting_organization_function.down.sql
+++ coderd/database/migrations/000306_update_protect_deleting_organization_function.down.sql
@@ -3,14 +3,14 @@ DROP TRIGGER IF EXISTS protect_deleting_organizations ON organizations;
 
 -- Revert the function to its original implementation
 CREATE OR REPLACE FUNCTION protect_deleting_organizations()
-	RETURNS TRIGGER AS
+    RETURNS TRIGGER AS
 $$
 DECLARE
     workspace_count int;
-	template_count int;
-	group_count int;
-	member_count int;
-	provisioner_keys_count int;
+    template_count int;
+    group_count int;
+    member_count int;
+    provisioner_keys_count int;
 BEGIN
     workspace_count := (
         SELECT count(*) as count FROM workspaces
@@ -19,50 +19,50 @@ BEGIN
             AND workspaces.deleted = false
     );
 
-	template_count := (
+    template_count := (
         SELECT count(*) as count FROM templates
         WHERE
             templates.organization_id = OLD.id
             AND templates.deleted = false
     );
 
-	group_count := (
+    group_count := (
         SELECT count(*) as count FROM groups
         WHERE
             groups.organization_id = OLD.id
     );
 
-	member_count := (
+    member_count := (
         SELECT count(*) as count FROM organization_members
         WHERE
             organization_members.organization_id = OLD.id
     );
 
-	provisioner_keys_count := (
-		Select count(*) as count FROM provisioner_keys
-		WHERE
-			provisioner_keys.organization_id = OLD.id
-	);
+    provisioner_keys_count := (
+        Select count(*) as count FROM provisioner_keys
+        WHERE
+            provisioner_keys.organization_id = OLD.id
+    );
 
     -- Fail the deletion if one of the following:
     -- * the organization has 1 or more workspaces
-	-- * the organization has 1 or more templates
-	-- * the organization has 1 or more groups other than "Everyone" group
-	-- * the organization has 1 or more members other than the organization owner
-	-- * the organization has 1 or more provisioner keys
+    -- * the organization has 1 or more templates
+    -- * the organization has 1 or more groups other than "Everyone" group
+    -- * the organization has 1 or more members other than the organization owner
+    -- * the organization has 1 or more provisioner keys
 
     IF (workspace_count + template_count + provisioner_keys_count) > 0 THEN
             RAISE EXCEPTION 'cannot delete organization: organization has % workspaces, % templates, and % provisioner keys that must be deleted first', workspace_count, template_count, provisioner_keys_count;
     END IF;
 
-	IF (group_count) > 1 THEN
+    IF (group_count) > 1 THEN
             RAISE EXCEPTION 'cannot delete organization: organization has % groups that must be deleted first', group_count - 1;
     END IF;
 
     -- Allow 1 member to exist, because you cannot remove yourself. You can
     -- remove everyone else. Ideally, we only omit the member that matches
     -- the user_id of the caller, however in a trigger, the caller is unknown.
-	IF (member_count) > 1 THEN
+    IF (member_count) > 1 THEN
             RAISE EXCEPTION 'cannot delete organization: organization has % members that must be deleted first', member_count - 1;
     END IF;
 
--- coderd/database/migrations/000306_update_protect_deleting_organization_function.up.sql
+++ coderd/database/migrations/000306_update_protect_deleting_organization_function.up.sql
@@ -2,14 +2,14 @@ DROP TRIGGER IF EXISTS protect_deleting_organizations ON organizations;
 
 -- Replace the function with the new implementation
 CREATE OR REPLACE FUNCTION protect_deleting_organizations()
-	RETURNS TRIGGER AS
+    RETURNS TRIGGER AS
 $$
 DECLARE
     workspace_count int;
-	template_count int;
-	group_count int;
-	member_count int;
-	provisioner_keys_count int;
+    template_count int;
+    group_count int;
+    member_count int;
+    provisioner_keys_count int;
 BEGIN
     workspace_count := (
         SELECT count(*) as count FROM workspaces
@@ -18,37 +18,37 @@ BEGIN
             AND workspaces.deleted = false
     );
 
-	template_count := (
+    template_count := (
         SELECT count(*) as count FROM templates
         WHERE
             templates.organization_id = OLD.id
             AND templates.deleted = false
     );
 
-	group_count := (
+    group_count := (
         SELECT count(*) as count FROM groups
         WHERE
             groups.organization_id = OLD.id
     );
 
-	member_count := (
+    member_count := (
         SELECT count(*) as count FROM organization_members
         WHERE
             organization_members.organization_id = OLD.id
     );
 
-	provisioner_keys_count := (
-		Select count(*) as count FROM provisioner_keys
-		WHERE
-			provisioner_keys.organization_id = OLD.id
-	);
+    provisioner_keys_count := (
+        Select count(*) as count FROM provisioner_keys
+        WHERE
+            provisioner_keys.organization_id = OLD.id
+    );
 
     -- Fail the deletion if one of the following:
     -- * the organization has 1 or more workspaces
-	-- * the organization has 1 or more templates
-	-- * the organization has 1 or more groups other than "Everyone" group
-	-- * the organization has 1 or more members other than the organization owner
-	-- * the organization has 1 or more provisioner keys
+    -- * the organization has 1 or more templates
+    -- * the organization has 1 or more groups other than "Everyone" group
+    -- * the organization has 1 or more members other than the organization owner
+    -- * the organization has 1 or more provisioner keys
 
     -- Only create error message for resources that actually exist
     IF (workspace_count + template_count + provisioner_keys_count) > 0 THEN
@@ -73,14 +73,14 @@ BEGIN
         END;
     END IF;
 
-	IF (group_count) > 1 THEN
+    IF (group_count) > 1 THEN
             RAISE EXCEPTION 'cannot delete organization: organization has % groups that must be deleted first', group_count - 1;
     END IF;
 
     -- Allow 1 member to exist, because you cannot remove yourself. You can
     -- remove everyone else. Ideally, we only omit the member that matches
     -- the user_id of the caller, however in a trigger, the caller is unknown.
-	IF (member_count) > 1 THEN
+    IF (member_count) > 1 THEN
             RAISE EXCEPTION 'cannot delete organization: organization has % members that must be deleted first', member_count - 1;
     END IF;
 
@@ -92,5 +92,5 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER protect_deleting_organizations
     BEFORE UPDATE ON organizations
     FOR EACH ROW
-	WHEN (NEW.deleted = true AND OLD.deleted = false)
+    WHEN (NEW.deleted = true AND OLD.deleted = false)
     EXECUTE FUNCTION protect_deleting_organizations();
