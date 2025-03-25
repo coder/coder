521a13b65c3e5fa4bb9dff414ec5cb7aa4bcdc34
defelmnq <vincent@coder.com>
2025-03-25 23:38:23 +0000

    improve dormant workspace notification wording

---
+++ coderd/database/migrations/000311_improve_dormant_workspace_notification.up.sql
@@ -0,0 +1,3 @@
+UPDATE notification_templates SET body_template = E'Your workspace **{{.Labels.name}}** has been marked as [**dormant**](https://coder.com/docs/templates/schedule#dormancy-threshold-enterprise) due to inactivity exceeding the dormancy threshold.' ||
+	E'This workspace will be automatically deleted in {{.Labels.timeTilDormant}} if it remains inactive.' ||
+	E'To prevent deletion, activate your workspace using the link below.' WHERE id = '0ea69165-ec14-4314-91f1-69566ac3c5a0';
