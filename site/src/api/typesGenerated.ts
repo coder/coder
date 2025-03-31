489641d0beb2b5e47fbe232463384ac446d6fde3
Bruno Quaresma <bruno@coder.com>
2025-03-31 09:40:24 -0300

    feat: set icons for each type of notification (#17115)
    Each notification type will have an icon to represent the context:
    
    <img width="503" alt="Screenshot 2025-03-26 at 13 44 35"
    src="https://github.com/user-attachments/assets/1187c1c0-1043-4a32-b105-a7f91b52f8ca"
    />
    
    This depends on https://github.com/coder/coder/pull/17013

--- site/src/api/typesGenerated.ts
+++ site/src/api/typesGenerated.ts
@@ -839,18 +839,6 @@ export interface ExternalAuthUser {
 	readonly name: string;
 }
 
-// From codersdk/inboxnotification.go
-export const FallbackIconAccount = "DEFAULT_ICON_ACCOUNT";
-
-// From codersdk/inboxnotification.go
-export const FallbackIconOther = "DEFAULT_ICON_OTHER";
-
-// From codersdk/inboxnotification.go
-export const FallbackIconTemplate = "DEFAULT_ICON_TEMPLATE";
-
-// From codersdk/inboxnotification.go
-export const FallbackIconWorkspace = "DEFAULT_ICON_WORKSPACE";
-
 // From codersdk/deployment.go
 export interface Feature {
 	readonly entitlement: Entitlement;
@@ -1123,6 +1111,18 @@ export interface InboxNotificationAction {
 	readonly label: string;
 	readonly url: string;
 }
+
+// From codersdk/inboxnotification.go
+export const InboxNotificationFallbackIconAccount = "DEFAULT_ICON_ACCOUNT";
+
+// From codersdk/inboxnotification.go
+export const InboxNotificationFallbackIconOther = "DEFAULT_ICON_OTHER";
+
+// From codersdk/inboxnotification.go
+export const InboxNotificationFallbackIconTemplate = "DEFAULT_ICON_TEMPLATE";
+
+// From codersdk/inboxnotification.go
+export const InboxNotificationFallbackIconWorkspace = "DEFAULT_ICON_WORKSPACE";
 
 // From codersdk/insights.go
 export type InsightsReportInterval = "day" | "week";
