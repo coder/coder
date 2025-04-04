40ee7976eb2e1fa13e6cda648f395038654bb1bd
Marcin Tojek <marcin@coder.com>
2025-04-04 13:55:32 +0200

    Terminal uses font

--- site/src/theme/constants.ts
+++ site/src/theme/constants.ts
@@ -1,11 +1,15 @@
+import type { TerminalFontName } from "api/typesGenerated";
+
 export const borderRadius = 8;
 export const MONOSPACE_FONT_FAMILY =
 	"'IBM Plex Mono', 'Lucida Console', 'Lucida Sans Typewriter', 'Liberation Mono', 'Monaco', 'Courier New', Courier, monospace";
-export const TERMINAL_FONT_FAMILY_1 = MONOSPACE_FONT_FAMILY;
-export const TERMINAL_FONT_FAMILY_2 = MONOSPACE_FONT_FAMILY.replace(
-	"IBM Plex Mono",
-	"Fira Code",
-);
+export const terminalFonts: Record<TerminalFontName, string> = {
+	"fira-code": MONOSPACE_FONT_FAMILY.replace("IBM Plex Mono", "Fira Code"),
+	"ibm-plex-mono": MONOSPACE_FONT_FAMILY,
+
+	"": MONOSPACE_FONT_FAMILY,
+};
+
 export const BODY_FONT_FAMILY = `"Inter Variable", system-ui, sans-serif`;
 export const navHeight = 62;
 export const containerWidth = 1380;
