074ec2887d45d5c63bb03355087c9f18a09ac40c
Mathias Fredriksson <mafredri@gmail.com>
2025-04-07 11:32:37 +0300

    test(agent/agentssh): fix test race and improve Windows compat (#17271)
    Fixes coder/internal#558

--- agent/agentssh/agentssh_test.go
+++ agent/agentssh/agentssh_test.go
@@ -153,7 +153,9 @@ func TestNewServer_CloseActiveConnections(t *testing.T) {
 		logger := slogtest.Make(t, &slogtest.Options{IgnoreErrors: true}).Leveled(slog.LevelDebug)
 		s, err := agentssh.NewServer(ctx, logger, prometheus.NewRegistry(), afero.NewMemMapFs(), agentexec.DefaultExecer, nil)
 		require.NoError(t, err)
-		defer s.Close()
+		t.Cleanup(func() {
+			_ = s.Close()
+		})
 		err = s.UpdateHostSigner(42)
 		assert.NoError(t, err)
 
@@ -190,10 +192,17 @@ func TestNewServer_CloseActiveConnections(t *testing.T) {
 				}
 				// The 60 seconds here is intended to be longer than the
 				// test. The shutdown should propagate.
-				err = sess.Start("/bin/bash -c 'trap \"sleep 60\" SIGTERM; sleep 60'")
+				if runtime.GOOS == "windows" {
+					// Best effort to at least partially test this in Windows.
+					err = sess.Start("echo start\"ed\" && sleep 60")
+				} else {
+					err = sess.Start("/bin/bash -c 'trap \"sleep 60\" SIGTERM; echo start\"ed\"; sleep 60'")
+				}
 				assert.NoError(t, err)
 
+				pty.ExpectMatchContext(ctx, "started")
 				close(ch)
+
 				err = sess.Wait()
 				assert.Error(t, err)
 			}(waitConns[i])
