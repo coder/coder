0fc42895e6d82c6d64a7a4da1843337d9db5fccd
Jaayden Halko <jaayden.halko@gmail.com>
2025-04-11 17:48:17 +0000

    chore: cleanup, update validation

--- site/src/pages/CreateWorkspacePage/CreateWorkspacePageViewExperimental.tsx
+++ site/src/pages/CreateWorkspacePage/CreateWorkspacePageViewExperimental.tsx
@@ -72,7 +72,7 @@ export interface CreateWorkspacePageViewExperimentalProps {
 	startPollingExternalAuth: () => void;
 }
 
-// const getInitialParameterValues = (
+// const getInitialParameterValues1 = (
 // 	params: Parameter[],
 // 	autofillParams?: AutofillBuildParameter[],
 // ): WorkspaceBuildParameter[] => {
@@ -94,7 +94,7 @@ export interface CreateWorkspacePageViewExperimentalProps {
 // 			name: parameter.name,
 // 			value:
 // 				autofillParam &&
-// 				// isValidValue(parameter, autofillParam) &&
+// 				isValidValue(parameter, autofillParam) &&
 // 				autofillParam.source !== "user_history"
 // 					? autofillParam.value
 // 					: parameter.default_value,
