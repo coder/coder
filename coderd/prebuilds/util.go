f3e24b1716ed135c3bb8a17b24a67e687679b7da
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2025-04-15 15:31:56 +0000

    refactor: minor fixes in util.go

--- coderd/prebuilds/util.go
+++ coderd/prebuilds/util.go
@@ -10,8 +10,8 @@ import (
 // GenerateName generates a 20-byte prebuild name which should safe to use without truncation in most situations.
 // UUIDs may be too long for a resource name in cloud providers (since this ID will be used in the prebuild's name).
 //
-// We're generating a 9-byte suffix (72 bits of entry):
-// 1 - e^(-1e9^2 / (2 * 2^72)) = ~0.0001% likelihood of collision in 1 billion IDs.
+// We're generating a 9-byte suffix (72 bits of entropy):
+// 1 - e^(-1e9^2 / (2 * 2^72)) = ~0.01% likelihood of collision in 1 billion IDs.
 // See https://en.wikipedia.org/wiki/Birthday_attack.
 func GenerateName() (string, error) {
 	b := make([]byte, 9)
