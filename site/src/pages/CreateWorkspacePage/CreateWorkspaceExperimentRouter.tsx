9491b23b9d05000e532ca19a113b2a05376ab5b0
McKayla Washburn <mckayla@hey.com>
2025-04-16 21:31:54 +0000

    pls hold

--- site/src/pages/CreateWorkspacePage/CreateWorkspaceExperimentRouter.tsx
+++ site/src/pages/CreateWorkspacePage/CreateWorkspaceExperimentRouter.tsx
@@ -1,12 +1,24 @@
 import { useDashboard } from "modules/dashboard/useDashboard";
-import type { FC } from "react";
+import { createContext, type FC } from "react";
 import CreateWorkspacePage from "./CreateWorkspacePage";
 import CreateWorkspacePageExperimental from "./CreateWorkspacePageExperimental";
+import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import { templateByName } from "api/queries/templates";
 
 const CreateWorkspaceExperimentRouter: FC = () => {
 	const { experiments } = useDashboard();
-
 	const dynamicParametersEnabled = experiments.includes("dynamic-parameters");
+
+	const { organization: organizationName = "default", template: templateName } =
+		useParams() as { organization?: string; template: string };
+	const templateQuery = useQuery(
+		templateByName(organizationName, templateName),
+	);
+
+	const something = JSON.parse(
+		localStorage.getItem(`parameters.${templateQuery.data?.id}.optOut`) ?? "",
+	);
 
 	if (dynamicParametersEnabled) {
 		return <CreateWorkspacePageExperimental />;
@@ -16,3 +28,5 @@ const CreateWorkspaceExperimentRouter: FC = () => {
 };
 
 export default CreateWorkspaceExperimentRouter;
+
+const CreateWorkspaceProvider = createContext(undefined);
