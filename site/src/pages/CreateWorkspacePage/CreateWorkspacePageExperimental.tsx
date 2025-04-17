473db06c79e9363cc8184f6c1d82a50e6aa80cc5
Jaayden Halko <jaayden.halko@gmail.com>
2025-04-17 19:53:15 +0000

    fix: state issues with dynamic parameters

--- site/src/pages/CreateWorkspacePage/CreateWorkspacePageExperimental.tsx
+++ site/src/pages/CreateWorkspacePage/CreateWorkspacePageExperimental.tsx
@@ -86,7 +86,7 @@ const CreateWorkspacePageExperimental: FC = () => {
 			if (prev?.id === response.id) {
 				return prev;
 			}
-			return response;
+			return structuredClone(response);
 		});
 	}, []);
 
