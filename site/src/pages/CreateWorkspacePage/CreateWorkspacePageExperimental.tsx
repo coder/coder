ab6c88ec321d3d731330cd619b217b3f45f922ed
Jaayden Halko <jaayden.halko@gmail.com>
2025-04-17 20:08:35 +0000

    fix: remove structuredClone

--- site/src/pages/CreateWorkspacePage/CreateWorkspacePageExperimental.tsx
+++ site/src/pages/CreateWorkspacePage/CreateWorkspacePageExperimental.tsx
@@ -86,7 +86,7 @@ const CreateWorkspacePageExperimental: FC = () => {
 			if (prev?.id === response.id) {
 				return prev;
 			}
-			return structuredClone(response);
+			return response;
 		});
 	}, []);
 
