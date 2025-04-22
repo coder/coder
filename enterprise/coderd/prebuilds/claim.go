50d23e4780a863f90a2c36192285febb2dd97302
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2025-04-22 17:47:35 +0000

    CR's fixes

--- enterprise/coderd/prebuilds/claim.go
+++ enterprise/coderd/prebuilds/claim.go
@@ -5,12 +5,11 @@ import (
 	"database/sql"
 	"errors"
 
-	"github.com/coder/coder/v2/coderd/prebuilds"
-
 	"github.com/google/uuid"
 	"golang.org/x/xerrors"
 
 	"github.com/coder/coder/v2/coderd/database"
+	"github.com/coder/coder/v2/coderd/prebuilds"
 )
 
 type EnterpriseClaimer struct{}
@@ -22,39 +21,23 @@ func (_ EnterpriseClaimer) Claim(
 	name string,
 	presetID uuid.UUID,
 ) (*uuid.UUID, error) {
-	var prebuildID *uuid.UUID
-	err := store.InTx(func(db database.Store) error {
-		// TODO: do we need this?
-		//// Ensure no other replica can claim a prebuild for this user simultaneously.
-		// err := store.AcquireLock(ctx, database.GenLockID(fmt.Sprintf("prebuild-user-claim-%s", userID.String())))
-		// if err != nil {
-		//	return xerrors.Errorf("acquire claim lock for user %q: %w", userID.String(), err)
-		//}
-
-		result, err := db.ClaimPrebuiltWorkspace(ctx, database.ClaimPrebuiltWorkspaceParams{
-			NewUserID: userID,
-			NewName:   name,
-			PresetID:  presetID,
-		})
-		if err != nil {
-			switch {
-			// No eligible prebuilds found
-			case errors.Is(err, sql.ErrNoRows):
-				// Exit, this will result in a nil prebuildID being returned, which is fine
-				return nil
-			default:
-				return xerrors.Errorf("claim prebuild for user %q: %w", userID.String(), err)
-			}
-		}
-
-		prebuildID = &result.ID
-
-		return nil
-	}, &database.TxOptions{
-		TxIdentifier: "prebuild-claim",
+	result, err := store.ClaimPrebuiltWorkspace(ctx, database.ClaimPrebuiltWorkspaceParams{
+		NewUserID: userID,
+		NewName:   name,
+		PresetID:  presetID,
 	})
+	if err != nil {
+		switch {
+		// No eligible prebuilds found
+		case errors.Is(err, sql.ErrNoRows):
+			// Exit, this will result in a nil prebuildID being returned, which is fine
+			return nil, nil
+		default:
+			return nil, xerrors.Errorf("claim prebuild for user %q: %w", userID.String(), err)
+		}
+	}
 
-	return prebuildID, err
+	return &result.ID, nil
 }
 
 func (_ EnterpriseClaimer) Initiator() uuid.UUID {
