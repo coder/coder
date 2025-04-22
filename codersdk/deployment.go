afb175d9ee78111b44da8349b7d2a88643755c85
Jaayden Halko <jaayden.halko@gmail.com>
2025-04-22 10:09:16 +0100

    chore: make dynamic params a valid experiment (#17492)

--- codersdk/deployment.go
+++ codersdk/deployment.go
@@ -3262,7 +3262,9 @@ const (
 // users to opt-in to via --experimental='*'.
 // Experiments that are not ready for consumption by all users should
 // not be included here and will be essentially hidden.
-var ExperimentsAll = Experiments{}
+var ExperimentsAll = Experiments{
+	ExperimentDynamicParameters,
+}
 
 // Experiments is a list of experiments.
 // Multiple experiments may be enabled at the same time.
