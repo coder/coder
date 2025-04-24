58d8eaca3ad1d6048c8884dcb8862ec499b4d814
M Atif Ali <me@matifali.dev>
2025-04-24 12:11:31 +0500

    bump version constraint

--- examples/templates/docker-devcontainer/main.tf
+++ examples/templates/docker-devcontainer/main.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     coder = {
-      source = "coder/coder"
+      source  = "coder/coder"
+      version = "~> 2.0"
     }
     docker = {
       source = "kreuzwerker/docker"
