8d8fc9938958c2dce4a4ee6da6b26f8de496af35
M Atif Ali <atif@coder.com>
2025-04-24 12:21:31 +0500

    chore(dogfood): allow provider minor version updates (#17554)

--- dogfood/coder/main.tf
+++ dogfood/coder/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = "2.3.0"
+      version = "~> 2.0"
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.0"
+      version = "~> 3.0"
     }
   }
 }
