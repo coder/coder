e06717806338316b892fa110edf32442e0ce4cac
Brett Kolodny <brett.kolodny@coder.com>
2025-04-28 16:51:40 +0000

    chore: alphabetize exports

--- site/src/hooks/index.ts
+++ site/src/hooks/index.ts
@@ -1,5 +1,5 @@
+export * from "./useAuthenticated";
 export * from "./useClickable";
 export * from "./useClickableTableRow";
 export * from "./useClipboard";
 export * from "./usePagination";
-export * from "./useAuthenticated";
