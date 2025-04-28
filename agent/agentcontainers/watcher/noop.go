45a978c0d151b3f64a2ba93008faf17043bcad46
Mathias Fredriksson <mafredri@gmail.com>
2025-04-28 11:56:28 +0000

    fix close of closed channel

--- agent/agentcontainers/watcher/noop.go
+++ agent/agentcontainers/watcher/noop.go
@@ -12,7 +12,9 @@ func NewNoop() Watcher {
 }
 
 type noopWatcher struct {
-	closed chan struct{}
+	mu     synx.Mutex
+	closed bool
+	done   chan struct{}
 }
 
 func (*noopWatcher) Add(string) error {
@@ -27,12 +29,18 @@ func (n *noopWatcher) Next(ctx context.Context) (*fsnotify.Event, error) {
 	select {
 	case <-ctx.Done():
 		return nil, ctx.Err()
-	case <-n.closed:
+	case <-n.done:
 		return nil, xerrors.New("watcher closed")
 	}
 }
 
 func (n *noopWatcher) Close() error {
-	close(n.closed)
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	if n.closed {
+		return nil
+	}
+	n.closed = true
+	close(n.done)
 	return nil
 }
