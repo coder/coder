fc0694c5a3bf8727069ccbf98b5a37bf561e6c7b
Michael Smith <throwawayclover@gmail.com>
2025-04-29 21:52:04 -0400

    fix: add back logic that was removed during merge

--- site/src/pages/WorkspacesPage/LastUsed.tsx
+++ site/src/pages/WorkspacesPage/LastUsed.tsx
@@ -11,16 +11,33 @@ interface LastUsedProps {
 }
 
 export const LastUsed: FC<LastUsedProps> = ({ lastUsedAt }) => {
-	/**
-	 * @todo Verify that this is equivalent
-	 */
 	const [circle, message] = useTimeSync({
 		idealRefreshIntervalMs: IDEAL_REFRESH_ONE_MINUTE,
 		select: (date) => {
+			/**
+			 * @todo Verify that t.from(now) is equivalent to t.fromNow()
+			 */
 			const t = dayjs(lastUsedAt);
-			const deltaMsg = t.from(dayjs(date));
-			const circle = <StatusIndicatorDot variant="inactive" />;
-			return [circle, deltaMsg] as const;
+			const now = dayjs(date);
+			let message = t.from(now);
+			let circle = <StatusIndicatorDot variant="inactive" />;
+
+			if (t.isAfter(now.subtract(1, "hour"))) {
+				circle = <StatusIndicatorDot variant="success" />;
+				// Since the agent reports on a 10m interval,
+				// the last_used_at can be inaccurate when recent.
+				message = "Now";
+			} else if (t.isAfter(now.subtract(3, "day"))) {
+				circle = <StatusIndicatorDot variant="pending" />;
+			} else if (t.isAfter(now.subtract(1, "month"))) {
+				circle = <StatusIndicatorDot variant="warning" />;
+			} else if (t.isAfter(now.subtract(100, "year"))) {
+				circle = <StatusIndicatorDot variant="failed" />;
+			} else {
+				message = "Never";
+			}
+
+			return [circle, message] as const;
 		},
 	});
 
