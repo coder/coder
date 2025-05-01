c57d54aa299e09d25b669c5a1f8ce6fa4fb7c4fa
Jaayden Halko <jaayden@coder.com>
2025-05-01 15:17:11 +0000

    fix: fix merge

--- site/src/pages/UserSettingsPage/ExternalAuthPage/ExternalAuthPageView.tsx
+++ site/src/pages/UserSettingsPage/ExternalAuthPage/ExternalAuthPageView.tsx
@@ -1,7 +1,6 @@
 import { useTheme } from "@emotion/react";
 import AutorenewIcon from "@mui/icons-material/Autorenew";
 import LoadingButton from "@mui/lab/LoadingButton";
-import Divider from "@mui/material/Divider";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
