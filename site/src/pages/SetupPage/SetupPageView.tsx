e9061ae28f83807a9abcbf4653de04437e54eee9
M Atif Ali <atif@coder.com>
2025-05-02 20:00:25 +0500

    fix(site): update licensing info URL

--- site/src/pages/SetupPage/SetupPageView.tsx
+++ site/src/pages/SetupPage/SetupPageView.tsx
@@ -247,7 +247,7 @@ export const SetupPageView: FC<SetupPageViewProps> = ({
 								quotas, and more.
 							</span>
 							<Link
-								href={docs("/licensing")}
+								href="https://coder.com/pricing"
 								target="_blank"
 								css={{ marginTop: 4, display: "inline-block", fontSize: 13 }}
 							>
