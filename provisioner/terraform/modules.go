5c77cdfa61b08875423fd76806398501eb51d481
ケイラ <mckayla@hey.com>
2025-05-08 16:13:46 -0600

    fix: persist terraform modules during template import (#17665)

--- provisioner/terraform/modules.go
+++ provisioner/terraform/modules.go
@@ -1,9 +1,13 @@
 package terraform
 
 import (
+	"archive/tar"
+	"bytes"
 	"encoding/json"
+	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"golang.org/x/xerrors"
 
@@ -20,8 +24,12 @@ type modulesFile struct {
 	Modules []*module `json:"Modules"`
 }
 
+func getModulesDirectory(workdir string) string {
+	return filepath.Join(workdir, ".terraform", "modules")
+}
+
 func getModulesFilePath(workdir string) string {
-	return filepath.Join(workdir, ".terraform", "modules", "modules.json")
+	return filepath.Join(getModulesDirectory(workdir), "modules.json")
 }
 
 func parseModulesFile(filePath string) ([]*proto.Module, error) {
@@ -61,4 +69,62 @@ func getModules(workdir string) ([]*proto.Module, error) {
 		filteredModules = append(filteredModules, m)
 	}
 	return filteredModules, nil
+}
+
+func getModulesArchive(workdir string) ([]byte, error) {
+	modulesDir := getModulesDirectory(workdir)
+	if _, err := os.ReadDir(modulesDir); err != nil {
+		if os.IsNotExist(err) {
+			return []byte{}, nil
+		}
+
+		return nil, err
+	}
+	empty := true
+	var b bytes.Buffer
+	w := tar.NewWriter(&b)
+	err := filepath.WalkDir(modulesDir, func(filePath string, info fs.DirEntry, err error) error {
+		if err != nil {
+			return xerrors.Errorf("failed to create modules archive: %w", err)
+		}
+		if info.IsDir() {
+			return nil
+		}
+		archivePath, found := strings.CutPrefix(filePath, workdir+string(os.PathSeparator))
+		if !found {
+			return xerrors.Errorf("walked invalid file path: %q", filePath)
+		}
+
+		content, err := os.ReadFile(filePath)
+		if err != nil {
+			return xerrors.Errorf("failed to read module file while archiving: %w", err)
+		}
+		empty = false
+		err = w.WriteHeader(&tar.Header{
+			Name: archivePath,
+			Size: int64(len(content)),
+			Mode: 0o644,
+			Uid:  1000,
+			Gid:  1000,
+		})
+		if err != nil {
+			return xerrors.Errorf("failed to add module file to archive: %w", err)
+		}
+		if _, err = w.Write(content); err != nil {
+			return xerrors.Errorf("failed to write module file to archive: %w", err)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	err = w.Close()
+	if err != nil {
+		return nil, xerrors.Errorf("failed to close module files archive: %w", err)
+	}
+	// Don't persist empty tar files in the database
+	if empty {
+		return []byte{}, nil
+	}
+	return b.Bytes(), nil
 }
