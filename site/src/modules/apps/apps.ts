77abfeafc769aa428c45aa9072c6b31276ffc4e8
Bruno Quaresma <bruno@coder.com>
2025-05-09 10:41:52 -0300

    chore: extract app access logic for reuse (#17724)
    We are starting to add app links in many places in the UI, and to make
    it consistent, this PR extracts the most core logic into the
    modules/apps for reuse.
    
    Related to https://github.com/coder/coder/issues/17311

--- site/src/modules/apps/apps.ts
+++ site/src/modules/apps/apps.ts
@@ -1,3 +1,15 @@
+import type {
+	Workspace,
+	WorkspaceAgent,
+	WorkspaceApp,
+} from "api/typesGenerated";
+
+// This is a magic undocumented string that is replaced
+// with a brand-new session token from the backend.
+// This only exists for external URLs, and should only
+// be used internally, and is highly subject to break.
+export const SESSION_TOKEN_PLACEHOLDER = "$SESSION_TOKEN";
+
 type GetVSCodeHrefParams = {
 	owner: string;
 	workspace: string;
@@ -49,6 +61,73 @@ export const getTerminalHref = ({
 	}/terminal?${params}`;
 };
 
-export const openAppInNewWindow = (name: string, href: string) => {
+export const openAppInNewWindow = (href: string) => {
 	window.open(href, "_blank", "width=900,height=600");
+};
+
+export type GetAppHrefParams = {
+	path: string;
+	host: string;
+	workspace: Workspace;
+	agent: WorkspaceAgent;
+	token?: string;
+};
+
+export const getAppHref = (
+	app: WorkspaceApp,
+	{ path, token, workspace, agent, host }: GetAppHrefParams,
+): string => {
+	if (isExternalApp(app)) {
+		return needsSessionToken(app)
+			? app.url.replaceAll(SESSION_TOKEN_PLACEHOLDER, token ?? "")
+			: app.url;
+	}
+
+	// The backend redirects if the trailing slash isn't included, so we add it
+	// here to avoid extra roundtrips.
+	let href = `${path}/@${workspace.owner_name}/${workspace.name}.${
+		agent.name
+	}/apps/${encodeURIComponent(app.slug)}/`;
+
+	if (app.command) {
+		// Terminal links are relative. The terminal page knows how
+		// to select the correct workspace proxy for the websocket
+		// connection.
+		href = `/@${workspace.owner_name}/${workspace.name}.${
+			agent.name
+		}/terminal?command=${encodeURIComponent(app.command)}`;
+	}
+
+	if (host && app.subdomain && app.subdomain_name) {
+		const baseUrl = `${window.location.protocol}//${host.replace(/\*/g, app.subdomain_name)}`;
+		const url = new URL(baseUrl);
+		url.pathname = "/";
+		href = url.toString();
+	}
+
+	return href;
+};
+
+export const needsSessionToken = (app: WorkspaceApp) => {
+	if (!isExternalApp(app)) {
+		return false;
+	}
+
+	// HTTP links should never need the session token, since Cookies
+	// handle sharing it when you access the Coder Dashboard. We should
+	// never be forwarding the bare session token to other domains!
+	const isHttp = app.url.startsWith("http");
+	const requiresSessionToken = app.url.includes(SESSION_TOKEN_PLACEHOLDER);
+	return requiresSessionToken && !isHttp;
+};
+
+type ExternalWorkspaceApp = WorkspaceApp & {
+	external: true;
+	url: string;
+};
+
+export const isExternalApp = (
+	app: WorkspaceApp,
+): app is ExternalWorkspaceApp => {
+	return app.external && app.url !== undefined;
 };
