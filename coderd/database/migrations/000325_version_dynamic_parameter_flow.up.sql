31486b6b3c96d45d425d6b1a6fad78fdef4ee663
Steven Masley <stevenmasley@gmail.com>
2025-05-14 09:56:50 -0500

    bump migration

--- coderd/database/migrations/000325_version_dynamic_parameter_flow.up.sql
+++ coderd/database/migrations/000325_version_dynamic_parameter_flow.up.sql
@@ -1,9 +1,10 @@
+-- Default to `true`. Users will have to opt out of the new flow if they have issues.
 ALTER TABLE templates ADD COLUMN dynamic_parameter_flow BOOL NOT NULL DEFAULT true;
 
 COMMENT ON COLUMN templates.dynamic_parameter_flow IS
 	'Determines whether to default to the dynamic parameter creation flow for this template. '
 	'As a template wide setting, the template admin can opt out if there are any issues. '
-	'Opt out is allowed as workspace creation is a core workflow. This column will be removed '
-	'when the dynamic parameter creation flow is fully rolled out and stable.';
+	'An escape hatch is required, as workspace creation is a core workflow and cannot break. '
+	'This column will be removed when the dynamic parameter creation flow is stable.';
 
 
