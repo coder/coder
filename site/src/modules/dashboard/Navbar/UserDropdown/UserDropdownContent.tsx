2c49fd9e9618efd2b55fb749fec208abd6760299
M Atif Ali <atif@coder.com>
2025-05-15 10:41:01 -0700

    feat: add copy button for workspace name in breadcrumb (#17822)
    Co-authored-by: BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>

--- site/src/modules/dashboard/Navbar/UserDropdown/UserDropdownContent.tsx
+++ site/src/modules/dashboard/Navbar/UserDropdown/UserDropdownContent.tsx
@@ -151,15 +151,7 @@ export const UserDropdownContent: FC<UserDropdownContentProps> = ({
 						</Tooltip>
 						<CopyButton
 							text={buildInfo.deployment_id}
-							buttonStyles={css`
-                width: 16px;
-                height: 16px;
-
-                svg {
-                  width: 16px;
-                  height: 16px;
-                }
-              `}
+							label="Copy deployment ID"
 						/>
 					</div>
 				)}
@@ -181,7 +173,7 @@ const GithubStar: FC<SvgIconProps> = (props) => (
 		fill="currentColor"
 		{...props}
 	>
-		<path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"></path>
+		<path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z" />
 	</svg>
 );
 
