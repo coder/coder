5db1da3fbfd229b1788c96bdf39a74294702a873
Danny Kopping <dannykopping@gmail.com>
2025-05-19 16:28:19 +0200

    Revert "Update scripts/rules.go"
    This reverts commit b5fb079eee248ce65f726fe8dfc4e681d20fe2fd.

--- scripts/rules.go
+++ scripts/rules.go
@@ -143,7 +143,7 @@ func publishInTransaction(m dsl.Matcher) {
 
 	// Match direct calls to the Publish method of a pubsub instance inside InTx
 	m.Match(`
-		$_.InTx(func($_) error {
+		$x.InTx(func($y) error {
 			$*_
 			$_ = $ps.Publish($evt, $msg)
 			$*_
