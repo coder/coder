45df24d0fc19571f5ff17a6d1d5ba3189578bbbf
Spike Curtis <spike@coder.com>
2025-05-19 07:51:25 +0000

    appease fmt

--- coderd/httpmw/apikey_test.go
+++ coderd/httpmw/apikey_test.go
@@ -531,7 +531,7 @@ func TestAPIKey(t *testing.T) {
 		r.Header.Set(codersdk.SessionTokenHeader, token)
 
 		// Include a valid oauth token for refreshing. If this token is invalid,
-		// it is difficult to tell an auth failure from a expired api key, or 
+		// it is difficult to tell an auth failure from an expired api key, or
 		// an expired oauth key.
 		oauthToken := &oauth2.Token{
 			AccessToken:  "wow",
