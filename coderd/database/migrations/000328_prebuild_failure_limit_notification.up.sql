37173b0a86927f640ece1f5fd133c01aef3d1709
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2025-05-19 19:05:16 +0000

    fix: minor improvement to template

--- coderd/database/migrations/000328_prebuild_failure_limit_notification.up.sql
+++ coderd/database/migrations/000328_prebuild_failure_limit_notification.up.sql
@@ -5,6 +5,8 @@ VALUES ('414d9331-c1fc-4761-b40c-d1f4702279eb',
 		E'There is a problem creating prebuilt workspaces for the preset',
 		$$
 The number of failed prebuilds has reached the hard limit for template **{{ .Labels.template }}** and preset **{{ .Labels.preset }}**.
+
+To resume prebuilds, fix the underlying issue and upload a new template version.
 $$,
 		'Template Events',
 		'[
