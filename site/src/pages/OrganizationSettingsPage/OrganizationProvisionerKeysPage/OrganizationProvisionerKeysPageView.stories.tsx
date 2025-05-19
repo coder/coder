2118a078bb6f83165eb879f67bd40d6847bbc430
Cian Johnston <cian@coder.com>
2025-05-19 11:57:03 +0100

    update storybook

--- site/src/pages/OrganizationSettingsPage/OrganizationProvisionerKeysPage/OrganizationProvisionerKeysPageView.stories.tsx
+++ site/src/pages/OrganizationSettingsPage/OrganizationProvisionerKeysPage/OrganizationProvisionerKeysPageView.stories.tsx
@@ -1,5 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { MockProvisioner, MockProvisionerKey } from "testHelpers/entities";
+import {
+	ProvisionerKeyIDBuiltIn,
+	ProvisionerKeyIDPSK,
+	ProvisionerKeyIDUserAuth,
+} from "api/typesGenerated";
+import {
+	MockBuildInfo,
+	MockProvisioner,
+	MockProvisionerKey,
+} from "testHelpers/entities";
 import { OrganizationProvisionerKeysPageView } from "./OrganizationProvisionerKeysPageView";
 
 const mockProvisionerKeyDaemons = [
@@ -10,8 +19,46 @@ const mockProvisionerKeyDaemons = [
 		daemons: [
 			{
 				...MockProvisioner,
+				name: "Test Provisioner 1",
+				id: "daemon-1",
+			},
+			{
+				...MockProvisioner,
+				name: "Test Provisioner 2",
+				id: "daemon-2",
 			},
 		],
+	},
+	{
+		key: {
+			...MockProvisionerKey,
+			name: "no-daemons",
+		},
+		daemons: [],
+	},
+	// Built-in provisioners, user-auth, and PSK keys are not shown here.
+	{
+		key: {
+			...MockProvisionerKey,
+			id: ProvisionerKeyIDBuiltIn,
+			name: "built-in",
+		},
+		daemons: [],
+	},
+	{
+		key: {
+			...MockProvisionerKey,
+			id: ProvisionerKeyIDUserAuth,
+			name: "user-auth",
+		},
+		daemons: [],
+	},
+	{
+		key: {
+			...MockProvisionerKey,
+			id: ProvisionerKeyIDPSK,
+			name: "PSK",
+		},
 	},
 ];
 
@@ -28,22 +75,34 @@ const meta: Meta<typeof OrganizationProvisionerKeysPageView> = {
 export default meta;
 type Story = StoryObj<typeof OrganizationProvisionerKeysPageView>;
 
-export const Example: Story = {};
+export const Default: Story = {
+	args: {
+		buildVersion: MockBuildInfo.version,
+		error: undefined,
+		provisionerKeyDaemons: mockProvisionerKeyDaemons,
+		onRetry: () => {},
+		showPaywall: false,
+	},
+};
 
 export const Paywalled: Story = {
+	...Default,
 	args: {
 		showPaywall: true,
 	},
 };
 
-export const NoProvisionerKeys: Story = {
+export const Empty: Story = {
+	...Default,
 	args: {
 		provisionerKeyDaemons: [],
 	},
 };
 
 export const ErrorLoadingProvisionerKeys: Story = {
+	...Default,
 	args: {
+		provisionerKeyDaemons: [],
 		error: "Failed to load provisioner keys",
 	},
 };
