adb120ee06b84a50994342a5502b739b4324669c
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2025-05-20 21:12:30 +0000

    refactor: support unhealthy on workspace status indicator

--- site/src/pages/WorkspacesPage/WorkspacesTable.tsx
+++ site/src/pages/WorkspacesPage/WorkspacesTable.tsx
@@ -290,6 +290,7 @@ export const WorkspacesTable: FC<WorkspacesTableProps> = ({
 							</TableCell>
 
 							<WorkspaceStatusCell workspace={workspace} />
+
 							<WorkspaceActionsCell
 								workspace={workspace}
 								onActionSuccess={onActionSuccess}
@@ -395,14 +396,6 @@ const WorkspaceStatusCell: FC<WorkspaceStatusCellProps> = ({ workspace }) => {
 		<TableCell>
 			<div className="flex flex-col">
 				<WorkspaceStatusIndicator workspace={workspace}>
-					{workspace.latest_build.status === "running" &&
-						!workspace.health.healthy && (
-							<InfoTooltip
-								type="warning"
-								title="Workspace is unhealthy"
-								message="Your workspace is running but some agents are unhealthy."
-							/>
-						)}
 					{workspace.dormant_at && (
 						<WorkspaceDormantBadge workspace={workspace} />
 					)}
