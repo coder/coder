5164413a1e114119965420c0469ad2dfc03beccd
Danielle Maywood <danielle@themaywoods.com>
2025-05-28 10:25:07 +0000

    chore: some changes
    - replace spaces with tabs.
    - remove _var suffixes.
    - check if provisioner job as a workspace build

--- coderd/database/migrations/000332_workspace_agent_name_unique_trigger.up.sql
+++ coderd/database/migrations/000332_workspace_agent_name_unique_trigger.up.sql
@@ -1,50 +1,48 @@
 CREATE OR REPLACE FUNCTION check_workspace_agent_name_unique()
 RETURNS TRIGGER AS $$
 DECLARE
-    provisioner_job_id_var uuid;
-    workspace_id_var uuid;
-    agents_with_name_var int;
+	provisioner_job_id uuid;
+	workspace_build_count int;
+	agents_with_name int;
 BEGIN
-	-- Find the provisioner job and workspace the agent is
-	-- being inserted into.
-	SELECT INTO provisioner_job_id_var, workspace_id_var
-		provisioner_jobs.id, workspaces.id
+	-- Find the provisioner job the workspace agent is being inserted into.
+	SELECT provisioner_jobs.id INTO provisioner_job_id
 	FROM workspace_resources
 	JOIN provisioner_jobs ON provisioner_jobs.id = workspace_resources.job_id
-	JOIN workspace_builds ON workspace_builds.job_id = provisioner_jobs.id
-	JOIN workspaces ON workspaces.id = workspace_builds.workspace_id
 	WHERE workspace_resources.id = NEW.resource_id;
 
-	-- If there is no workspace or provisioner job attached to the agent,
-	-- we will allow the insert to happen as there is no need to guarantee
-	-- uniqueness.
-	IF workspace_id_var IS NULL OR provisioner_job_id_var IS NULL THEN
+	-- Get whether the provisioner job has an associated workspace build
+	SELECT COUNT(*) INTO workspace_build_count
+	FROM workspace_builds
+	WHERE workspace_builds.job_id = provisioner_job_id;
+
+	-- If the provisioner job doesn't have a workspace build, we'll just
+	-- allow this.
+	IF workspace_build_count = 0 THEN
 		RETURN NEW;
 	END IF;
 
 	-- Count how many agents in this provisioner job already have
 	-- the given agent name.
-	SELECT COUNT(*) INTO agents_with_name_var
+	SELECT COUNT(*) INTO agents_with_name
 	FROM workspace_agents
 	JOIN workspace_resources ON workspace_resources.id = workspace_agents.resource_id
 	JOIN provisioner_jobs ON provisioner_jobs.id = workspace_resources.job_id
-	JOIN workspace_builds ON workspace_builds.job_id = provisioner_jobs.id
-	WHERE provisioner_jobs.id = provisioner_job_id_var
-	  AND workspace_builds.workspace_id = workspace_id_var
+	WHERE provisioner_jobs.id = provisioner_job_id
 	  AND workspace_agents.name = NEW.name
 	  AND workspace_agents.id != NEW.id;
 
 	-- If there's already an agent with this name, raise an error
-    IF agents_with_name_var > 0 THEN
-        RAISE EXCEPTION 'workspace agent name "%" already exists in this provisioner job', NEW.name
-            USING ERRCODE = 'unique_violation';
-    END IF;
+	IF agents_with_name > 0 THEN
+		RAISE EXCEPTION 'workspace agent name "%" already exists in this provisioner job', NEW.name
+			USING ERRCODE = 'unique_violation';
+	END IF;
 
-    RETURN NEW;
+	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER workspace_agent_name_unique_trigger
-    BEFORE INSERT OR UPDATE OF name, resource_id ON workspace_agents
-    FOR EACH ROW
-    EXECUTE FUNCTION check_workspace_agent_name_unique();
+	BEFORE INSERT OR UPDATE OF name, resource_id ON workspace_agents
+	FOR EACH ROW
+	EXECUTE FUNCTION check_workspace_agent_name_unique();
