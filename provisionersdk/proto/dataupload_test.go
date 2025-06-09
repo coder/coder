1a704291168dcb3309f18df14111824b1eff9924
Steven Masley <stevenmasley@gmail.com>
2025-06-09 10:01:02 -0500

    add unit tests and fuzz test

---
+++ provisionersdk/proto/dataupload_test.go
@@ -0,0 +1,93 @@
+package proto_test
+
+import (
+	crand "crypto/rand"
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/coder/coder/v2/provisionersdk/proto"
+)
+
+// Fuzz must be run manually with the `-fuzz` flag to generate random test cases.
+// By default, it only runs the added seed corpus cases.
+// go test -fuzz=FuzzBytesToDataUpload
+func FuzzBytesToDataUpload(f *testing.F) {
+	// Cases to always run in standard `go test` runs.
+	f.Add([]byte{})
+	f.Add()
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		first, chunks := proto.BytesToDataUpload(proto.DataUploadType_UPLOAD_TYPE_MODULE_FILES, data)
+
+		fmt.Println(len(data))
+		builder, err := proto.NewDataBuilder(first)
+		require.NoError(t, err)
+
+		var done bool
+		for _, chunk := range chunks {
+			require.False(t, done)
+			done, err = builder.Add(chunk)
+			require.NoError(t, err)
+		}
+
+		if len(chunks) > 0 {
+			require.True(t, done)
+		}
+
+		finalData, err := builder.Complete()
+		require.NoError(t, err)
+		require.Equal(t, data, finalData)
+	})
+}
+
+// TestBytesToDataUpload tests the BytesToDataUpload function and the DataBuilder
+// with large random data uploads.
+func TestBytesToDataUpload(t *testing.T) {
+	t.Parallel()
+
+	for i := 0; i < 20; i++ {
+		// Generate random data
+		//nolint:gosec // Just a unit test
+		chunkCount := 1 + rand.Intn(3)
+		size := (chunkCount * proto.ChunkSize) + (rand.Int() % proto.ChunkSize)
+		data := make([]byte, size)
+		_, err := crand.Read(data)
+		require.NoError(t, err)
+
+		first, chunks := proto.BytesToDataUpload(proto.DataUploadType_UPLOAD_TYPE_MODULE_FILES, data)
+		builder, err := proto.NewDataBuilder(first)
+		require.NoError(t, err)
+
+		// Try to add some bad chunks
+		_, err = builder.Add(&proto.ChunkPiece{Data: []byte{}, FullDataHash: make([]byte, 32)})
+		require.ErrorContains(t, err, "data hash does not match")
+
+		// Verify 'Complete' fails before adding any chunks
+		_, err = builder.Complete()
+		require.ErrorContains(t, err, "data upload is not complete")
+
+		// Add the chunks
+		var done bool
+		for _, chunk := range chunks {
+			require.False(t, done, "data upload should not be complete before adding all chunks")
+
+			done, err = builder.Add(chunk)
+			require.NoError(t, err, "chunk %d should be added successfully", chunk.PieceIndex)
+		}
+		require.True(t, done, "data upload should be complete after adding all chunks")
+
+		// Try to add another chunk after completion
+		done, err = builder.Add(chunks[0])
+		require.ErrorContains(t, err, "data upload is already complete")
+		require.True(t, done, "still complete")
+
+		// Verify the final data matches the original
+		got, err := builder.Complete()
+		require.NoError(t, err)
+
+		require.Equal(t, data, got, "final data should match the original data")
+	}
+}
