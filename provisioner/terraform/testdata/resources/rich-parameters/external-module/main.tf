57da39f8b8ff541758023fd5c54e234fc1f15028
Danny Kopping <dannykopping@gmail.com>
2025-06-20 16:56:11 +0200

    chore: unify tf testdata versions & fix tests
    Signed-off-by: Danny Kopping <dannykopping@gmail.com>

--- provisioner/terraform/testdata/resources/rich-parameters/external-module/main.tf
+++ provisioner/terraform/testdata/resources/rich-parameters/external-module/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = ">=2.0.0"
+      version = ">= 2.0.0"
     }
     docker = {
       source  = "kreuzwerker/docker"
