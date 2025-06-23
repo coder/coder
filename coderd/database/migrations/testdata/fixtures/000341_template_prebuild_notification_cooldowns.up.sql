c90d36f9487c51b5748f44253b7029aba5038d92
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2025-06-23 20:52:33 +0000

    fix: add sql fixture to pass tests

---
+++ coderd/database/migrations/testdata/fixtures/000341_template_prebuild_notification_cooldowns.up.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+	template_prebuild_notification_cooldowns (
+		template_id,
+		notification_type,
+		last_notification_sent
+	)
+	VALUES (
+		'6b298946-7a4f-47ac-9158-b03b08740a41',
+		'admin',
+		'2025-01-01 00:00:00+00'
+	);
