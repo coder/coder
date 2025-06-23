6877142a0e2e76f0286a52680f6c0a9d6ccb2870
Jaayden Halko <jaayden.halko@gmail.com>
2025-06-23 20:47:30 +0100

    feat: create experimental template embed page for dynamic params (#17999)
    resolves coder/preview#58

---
+++ site/src/components/Separator/Separator.tsx
@@ -0,0 +1,30 @@
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
+/**
+ * Copied from shadc/ui on 06/20/2025
+ * @see {@link https://ui.shadcn.com/docs/components/separator}
+ */
+import type * as React from "react";
+
+import { cn } from "utils/cn";
+
+function Separator({
+	className,
+	orientation = "horizontal",
+	decorative = true,
+	...props
+}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+	return (
+		<SeparatorPrimitive.Root
+			data-slot="separator"
+			decorative={decorative}
+			orientation={orientation}
+			className={cn(
+				"bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+				className,
+			)}
+			{...props}
+		/>
+	);
+}
+
+export { Separator };
