2f55e2946695762025638959923a824fea0f3f8e
Cian Johnston <cian@coder.com>
2025-06-23 14:07:42 +0100

    fix: complete job and mark workspace as deleted when no provisioners are available (#18465)
    Alternate fix for https://github.com/coder/coder/issues/18080
    
    Modifies wsbuilder to complete the provisioner job and mark the
    workspace as deleted if it is clear that no provisioner will be able to
    pick up the delete build.
    
    This has a significant advantage of not deviating too much from the
    current semantics of `POST /api/v2/workspacebuilds`.
    https://github.com/coder/coder/pull/18460 ends up returning a 204 on
    orphan delete due to no build being created.
    
    Downside is that we have to duplicate some responsibilities of
    provisionerdserver in wsbuilder.
    
    There is a slight gotcha to this approach though: if you stop a
    provisioner and then immediately try to orphan-delete, the job will
    still be created because of the provisioner heartbeat interval. However
    you can cancel it and try again.

--- coderd/wsbuilder/wsbuilder.go
+++ coderd/wsbuilder/wsbuilder.go
@@ -464,6 +464,50 @@ func (b *Builder) buildTx(authFunc func(action policy.Action, object rbac.Object
 			return BuildError{http.StatusInternalServerError, "get workspace build", err}
 		}
 
+		// If the requestor is trying to orphan-delete a workspace and there are no
+		// provisioners available, we should complete the build and mark the
+		// workspace as deleted ourselves.
+		// There are cases where tagged provisioner daemons have been decommissioned
+		// without deleting the relevant workspaces, and without any provisioners
+		// available these workspaces cannot be deleted.
+		// Orphan-deleting a workspace sends an empty state to Terraform, which means
+		// it won't actually delete anything. So we actually don't need to execute a
+		// provisioner job at all for an orphan delete, but deleting without a workspace
+		// build or provisioner job would result in no audit log entry, which is a deal-breaker.
+		hasActiveEligibleProvisioner := false
+		for _, pd := range provisionerDaemons {
+			age := now.Sub(pd.ProvisionerDaemon.LastSeenAt.Time)
+			if age <= provisionerdserver.StaleInterval {
+				hasActiveEligibleProvisioner = true
+				break
+			}
+		}
+		if b.state.orphan && !hasActiveEligibleProvisioner {
+			// nolint: gocritic // At this moment, we are pretending to be provisionerd.
+			if err := store.UpdateProvisionerJobWithCompleteWithStartedAtByID(dbauthz.AsProvisionerd(b.ctx), database.UpdateProvisionerJobWithCompleteWithStartedAtByIDParams{
+				CompletedAt: sql.NullTime{Valid: true, Time: now},
+				Error:       sql.NullString{Valid: false},
+				ErrorCode:   sql.NullString{Valid: false},
+				ID:          provisionerJob.ID,
+				StartedAt:   sql.NullTime{Valid: true, Time: now},
+				UpdatedAt:   now,
+			}); err != nil {
+				return BuildError{http.StatusInternalServerError, "mark orphan-delete provisioner job as completed", err}
+			}
+
+			// Re-fetch the completed provisioner job.
+			if pj, err := store.GetProvisionerJobByID(b.ctx, provisionerJob.ID); err == nil {
+				provisionerJob = pj
+			}
+
+			if err := store.UpdateWorkspaceDeletedByID(b.ctx, database.UpdateWorkspaceDeletedByIDParams{
+				ID:      b.workspace.ID,
+				Deleted: true,
+			}); err != nil {
+				return BuildError{http.StatusInternalServerError, "mark workspace as deleted", err}
+			}
+		}
+
 		return nil
 	}, nil)
 	if err != nil {
