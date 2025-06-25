c22233fcfe48259819664bf13faa88ea4add29a8
Danny Kopping <dannykopping@gmail.com>
2025-06-25 15:58:47 +0200

    chore: use coder/aisdk-go instead of kylecarbs/aisdk-go
    Signed-off-by: Danny Kopping <dannykopping@gmail.com>

--- codersdk/toolsdk/toolsdk_test.go
+++ codersdk/toolsdk/toolsdk_test.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/kylecarbs/aisdk-go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
