a4a5892c506bf5769b4982a899dc423e0d05f471
Sas Swart <sas.swart.cdk@gmail.com>
2025-06-26 15:54:52 +0200

    feat: graduate prebuilds to general availability (#18607)
    This PR removes the prebuilds experiment and allows the use of prebuilds
    without opting into an experiment.
    
    (cherry picked from commit c6e0ba12d3face3d5112bacc17452ffccb235d0d)

--- coderd/apidoc/docs.go
+++ coderd/apidoc/docs.go
@@ -12194,15 +12194,13 @@ const docTemplate = `{
                 "auto-fill-parameters",
                 "notifications",
                 "workspace-usage",
-                "web-push",
-                "workspace-prebuilds"
+                "web-push"
             ],
             "x-enum-comments": {
                 "ExperimentAutoFillParameters": "This should not be taken out of experiments until we have redesigned the feature.",
                 "ExperimentExample": "This isn't used for anything.",
                 "ExperimentNotifications": "Sends notifications via SMTP and webhooks following certain events.",
                 "ExperimentWebPush": "Enables web push notifications through the browser.",
-                "ExperimentWorkspacePrebuilds": "Enables the new workspace prebuilds feature.",
                 "ExperimentWorkspaceUsage": "Enables the new workspace usage tracking."
             },
             "x-enum-varnames": [
@@ -12210,8 +12208,7 @@ const docTemplate = `{
                 "ExperimentAutoFillParameters",
                 "ExperimentNotifications",
                 "ExperimentWorkspaceUsage",
-                "ExperimentWebPush",
-                "ExperimentWorkspacePrebuilds"
+                "ExperimentWebPush"
             ]
         },
         "codersdk.ExternalAuth": {
