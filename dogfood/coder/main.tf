be093004883a407ad70a00f00d8cb84a29b7a960
Mathias Fredriksson <mafredri@gmail.com>
2025-06-26 13:49:09 +0000

    not supported

--- dogfood/coder/main.tf
+++ dogfood/coder/main.tf
@@ -328,21 +328,23 @@ module "coder-login" {
 }
 
 module "cursor" {
-  count      = data.coder_workspace.me.start_count
-  source     = "dev.registry.coder.com/coder/cursor/coder"
-  version    = "1.1.0"
-  agent_id   = coder_agent.dev.id
-  agent_name = "dev"
-  folder     = local.repo_dir
+  count    = data.coder_workspace.me.start_count
+  source   = "dev.registry.coder.com/coder/cursor/coder"
+  version  = "1.1.0"
+  agent_id = coder_agent.dev.id
+  # Not supported yet.
+  # agent_name = "dev"
+  folder = local.repo_dir
 }
 
 module "windsurf" {
-  count      = data.coder_workspace.me.start_count
-  source     = "registry.coder.com/coder/windsurf/coder"
-  version    = "1.0.0"
-  agent_id   = coder_agent.dev.id
-  agent_name = "dev"
-  folder     = local.repo_dir
+  count    = data.coder_workspace.me.start_count
+  source   = "registry.coder.com/coder/windsurf/coder"
+  version  = "1.0.0"
+  agent_id = coder_agent.dev.id
+  # Not supported yet.
+  # agent_name = "dev"
+  folder = local.repo_dir
 }
 
 module "zed" {
