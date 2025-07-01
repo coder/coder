617f43e783301f1d75748e207d5f4badf933f0e9
Danny Kopping <dannykopping@gmail.com>
2025-07-01 09:54:19 +0200

    chore: review comments
    Signed-off-by: Danny Kopping <dannykopping@gmail.com>

--- scripts/which-release.sh
+++ scripts/which-release.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+# shellcheck source=scripts/lib.sh
+source "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
+
 COMMIT=$1
 if [[ -z "${COMMIT}" ]]; then
-	echo "Usage: $0 <commit-ref>"
-	echo ""
-	echo -n "Example: $0 "
-	echo $'$(gh pr view <pr-num> --json mergeCommit | jq \'.mergeCommit.oid\' -r)'
+	log "Usage: $0 <commit-ref>"
+	log ""
+	log -n "Example: $0 "
+	log $'$(gh pr view <pr-num> --json mergeCommit | jq \'.mergeCommit.oid\' -r)'
 	exit 2
 fi
 
 REMOTE=$(git remote -v | grep coder/coder | awk '{print $1}' | head -n1)
 if [[ -z "${REMOTE}" ]]; then
-	echo "Could not find remote for coder/coder"
-	exit 1
+	error "Could not find remote for coder/coder"
 fi
 
-echo "It is recommended that you run \`git fetch -ap ${REMOTE}\` to ensure you get a correct result."
+log "It is recommended that you run \`git fetch -ap ${REMOTE}\` to ensure you get a correct result."
 
-RELEASES=$(git branch -r --contains "${COMMIT}" | grep "${REMOTE}" | grep "/release/" | sed "s|${REMOTE}/||")
+RELEASES=$(git branch -r --contains "${COMMIT}" | grep "${REMOTE}" | grep "/release/" | sed "s|${REMOTE}/||" || true)
 if [[ -z "${RELEASES}" ]]; then
-	echo "Commit was not found in any release branch"
+	log "Commit was not found in any release branch"
 else
-	echo "Commit was found in the following release branches:"
-	echo "${RELEASES}"
+	log "Commit was found in the following release branches:"
+	log "${RELEASES}"
 fi
