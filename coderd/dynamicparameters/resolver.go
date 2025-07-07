22489900a2bf5e51e8125b58cb84e34a2df365e4
Steven Masley <stevenmasley@gmail.com>
2025-07-07 11:23:48 -0600

    fix immutable check against

--- coderd/dynamicparameters/resolver.go
+++ coderd/dynamicparameters/resolver.go
@@ -55,6 +55,16 @@ func ResolveParameters(
 		values[preset.Name] = parameterValue{Source: sourcePreset, Value: preset.Value}
 	}
 
+	// originalInputValues is going to be used to detect if a user tried to change
+	// an immutable parameter after the first build.
+	// The actual input values are mutated based on attributes like mutability
+	// and ephemerality.
+	originalInputValues := make(map[string]parameterValue, len(values))
+	for name, value := range values {
+		// Store the original values for later use.
+		originalInputValues[name] = value
+	}
+
 	// Render the parameters using the values that were supplied to the previous build.
 	//
 	// This is how the form should look to the user on their workspace settings page.
@@ -116,15 +126,15 @@ func ResolveParameters(
 		parameterNames[parameter.Name] = struct{}{}
 
 		if !firstBuild && !parameter.Mutable {
-			originalValue, ok := previousValuesMap[parameter.Name]
+			originalValue, ok := originalInputValues[parameter.Name]
 			// Immutable parameters should not be changed after the first build.
-			// If the value matches the original value, that is fine.
+			// If the value matches the original input value, that is fine.
 			//
 			// If the original value is not set, that means this is a new parameter. New
 			// immutable parameters are allowed. This is an opinionated choice to prevent
 			// workspaces failing to update or delete. Ideally we would block this, as
 			// immutable parameters should only be able to be set at creation time.
-			if ok && parameter.Value.AsString() != originalValue {
+			if ok && parameter.Value.AsString() != originalValue.Value {
 				var src *hcl.Range
 				if parameter.Source != nil {
 					src = &parameter.Source.HCLBlock().TypeRange
