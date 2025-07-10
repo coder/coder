b9a8746a754f3c6071f4df054248f08016aa7cad
Jeonghwan Lee <jhlee@cryptolab.co.kr>
2025-07-11 08:25:08 +0900

    hotfix(site): delay login page redering to prevent early redirection

--- site/src/contexts/auth/AuthProvider.tsx
+++ site/src/contexts/auth/AuthProvider.tsx
@@ -83,7 +83,7 @@ export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
 	const isSigningIn = loginMutation.isLoading;
 	const isUpdatingProfile = updateProfileMutation.isLoading;
 
-	const signOut = useCallback(() => {
+	const signOut = useCallback(async () => {
 		logoutMutation.mutate();
 	}, [logoutMutation]);
 
