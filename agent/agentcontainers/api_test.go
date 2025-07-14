cd45e06a5a74fe1efb0dfffd7fe39008bb2ea7f5
Danielle Maywood <danielle@themaywoods.com>
2025-07-14 22:13:45 +0000

    chore: we have eventually

--- agent/agentcontainers/api_test.go
+++ agent/agentcontainers/api_test.go
@@ -2883,21 +2883,12 @@ func TestAPI(t *testing.T) {
 			Op:   fsnotify.Write,
 		})
 
-		// Retry RefreshContainers until subagent injection succeeds
-		// This handles the race condition where the watcher loop may not have
-		// finished processing the config change yet
-		var subAgentCreated bool
-		for i := 0; i < 10; i++ {
+		require.Eventuallyf(t, func() bool {
 			err = api.RefreshContainers(ctx)
 			require.NoError(t, err)
 
-			if len(fakeSAC.created) > 0 {
-				subAgentCreated = true
-				break
-			}
-			time.Sleep(10 * time.Millisecond)
-		}
-		require.True(t, subAgentCreated, "subagent should be created after config change")
+			return len(fakeSAC.created) > 0
+		}, testutil.WaitShort, testutil.IntervalFast, "subagent should be created after config change")
 
 		t.Log("Phase 2: Cont, waiting for sub agent to exit")
 		exitSubAgentOnce.Do(func() {
@@ -2932,21 +2923,12 @@ func TestAPI(t *testing.T) {
 			Op:   fsnotify.Write,
 		})
 
-		// Retry RefreshContainers until subagent injection succeeds
-		// This handles the race condition where the watcher loop may not have
-		// finished processing the config change yet
-		subAgentCreated = false
-		for i := 0; i < 10; i++ {
+		require.Eventuallyf(t, func() bool {
 			err = api.RefreshContainers(ctx)
 			require.NoError(t, err)
 
-			if len(fakeSAC.created) > 0 {
-				subAgentCreated = true
-				break
-			}
-			time.Sleep(10 * time.Millisecond)
-		}
-		require.True(t, subAgentCreated, "subagent should be created after config change")
+			return len(fakeSAC.created) > 0
+		}, testutil.WaitShort, testutil.IntervalFast, "subagent should be created after config change")
 
 		req = httptest.NewRequest(http.MethodGet, "/", nil).WithContext(ctx)
 		rec = httptest.NewRecorder()
