618121cba39833db7c1ca4653d7ce7745d5aff57
Dean Sheather <dean@deansheather.com>
2025-07-24 14:18:29 +1000

    chore: add support for tailscale soft isolation in VPN (#19023)

--- vpn/tunnel.go
+++ vpn/tunnel.go
@@ -271,13 +271,14 @@ func (t *Tunnel) start(req *StartRequest) error {
 		svrURL,
 		apiToken,
 		&Options{
-			Headers:          header,
-			Logger:           t.clientLogger,
-			DNSConfigurator:  networkingStack.DNSConfigurator,
-			Router:           networkingStack.Router,
-			TUNDevice:        networkingStack.TUNDevice,
-			WireguardMonitor: networkingStack.WireguardMonitor,
-			UpdateHandler:    t,
+			Headers:             header,
+			Logger:              t.clientLogger,
+			UseSoftNetIsolation: req.GetTunnelUseSoftNetIsolation(),
+			DNSConfigurator:     networkingStack.DNSConfigurator,
+			Router:              networkingStack.Router,
+			TUNDevice:           networkingStack.TUNDevice,
+			WireguardMonitor:    networkingStack.WireguardMonitor,
+			UpdateHandler:       t,
 		},
 	)
 	if err != nil {
