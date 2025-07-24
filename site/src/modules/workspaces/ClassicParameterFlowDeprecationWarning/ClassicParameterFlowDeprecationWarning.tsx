7144f4f0ba0f2755392a3bfb4c8a6154eb81e46a
Jaayden Halko <jaayden.halko@gmail.com>
2025-07-24 20:08:45 +0000

    chore: update warning content

--- site/src/modules/workspaces/ClassicParameterFlowDeprecationWarning/ClassicParameterFlowDeprecationWarning.tsx
+++ site/src/modules/workspaces/ClassicParameterFlowDeprecationWarning/ClassicParameterFlowDeprecationWarning.tsx
@@ -2,6 +2,7 @@ import { Alert } from "components/Alert/Alert";
 import { Link } from "components/Link/Link";
 import { linkToTemplate, useLinks } from "modules/navigation";
 import type { FC } from "react";
+import { docs } from "utils/docs";
 
 interface ClassicParameterFlowDeprecationWarningProps {
 	organizationName: string;
@@ -26,9 +27,17 @@ export const ClassicParameterFlowDeprecationWarning: FC<
 		<Alert severity="warning" className="mb-2">
 			<div>
 				This template is using the classic parameter flow, which will be{" "}
-				<strong>deprecated</strong> in a future release. Please migrate to
-				dynamic improved functionality.
+				<strong>deprecated</strong> and removed in a future release. Please
+				migrate to{" "}
+				<a
+					href={docs("/admin/templates/extending-templates/dynamic-parameters")}
+					className="text-content-link"
+				>
+					dynamic parameters
+				</a>{" "}
+				on template settings for improved functionality.
 			</div>
+
 			<Link className="text-xs" href={templateSettingsLink}>
 				Go to Template Settings
 			</Link>
