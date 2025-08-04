e80f91e900b72715b73651ed94189f1d64f8a9fb
Spike Curtis <spike@coder.com>
2025-08-04 10:04:44 +0400

    chore: add small scenario to scaletest (#19110)
    Relaxes the `terraform` version constraint to be at least 1.9, since
    1.12 is installed in our Dogfood image
    
    Adds a `small` scenario to keep costs down while we continue to develop
    capabilities.

--- scaletest/terraform/action/scenarios.tf
+++ scaletest/terraform/action/scenarios.tf
@@ -35,5 +35,40 @@ locals {
         max_connections = 500
       }
     }
+    small = {
+      coder = {
+        nodepool_size = 3
+        machine_type  = "c2d-standard-8"
+        replicas      = 3
+        cpu_request   = "4000m"
+        mem_request   = "12Gi"
+        cpu_limit     = "4000m"
+        mem_limit     = "12Gi"
+      }
+      provisionerd = {
+        replicas    = 5
+        cpu_request = "100m"
+        mem_request = "256Mi"
+        cpu_limit   = "1000m"
+        mem_limit   = "1Gi"
+      }
+      workspaces = {
+        count_per_deployment = 10
+        nodepool_size        = 3
+        machine_type         = "c2d-standard-8"
+        cpu_request          = "100m"
+        mem_request          = "128Mi"
+        cpu_limit            = "100m"
+        mem_limit            = "128Mi"
+      }
+      misc = {
+        nodepool_size = 1
+        machine_type  = "c2d-standard-8"
+      }
+      cloudsql = {
+        tier            = "db-custom-2-7680"
+        max_connections = 100
+      }
+    }
   }
 }
