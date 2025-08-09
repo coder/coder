565753655fa6924fe4d878433f4fe4b54f213220
Michael Smith <throwawayclover@gmail.com>
2025-08-09 06:50:53 +0000

    chore: add basic overflow message

--- site/src/modules/resources/AgentLogs/AgentLogs.tsx
+++ site/src/modules/resources/AgentLogs/AgentLogs.tsx
@@ -2,7 +2,7 @@ import type { Interpolation, Theme } from "@emotion/react";
 import Tooltip from "@mui/material/Tooltip";
 import type { WorkspaceAgentLogSource } from "api/typesGenerated";
 import type { Line } from "components/Logs/LogLine";
-import { type ComponentProps, type ReactNode, forwardRef } from "react";
+import { type ComponentProps, forwardRef } from "react";
 import { FixedSizeList as List } from "react-window";
 import { AGENT_LOG_LINE_HEIGHT, AgentLogLine } from "./AgentLogLine";
 
@@ -24,16 +24,6 @@ function groupLogSourcesById(
 	return sourcesById;
 }
 
-const message: ReactNode = (
-	<p>
-		Startup logs exceeded the max size of{" "}
-		<span className="tracking-wide font-mono">1MB</span>, and will not continue
-		to be written to the database! Logs will continue to be written to the
-		<span className="font-mono">/tmp/coder-startup-script.log</span> file in the
-		workspace.
-	</p>
-);
-
 type AgentLogsProps = Omit<
 	ComponentProps<typeof List>,
 	"children" | "itemSize" | "itemCount" | "itemKey"
@@ -52,132 +42,145 @@ export const AgentLogs = forwardRef<List, AgentLogsProps>(
 		const getLogSource = (id: string) => logSourceByID[id] || fallbackLog;
 
 		return (
-			<List
-				{...listProps}
-				ref={ref}
-				css={styles.logs}
-				itemCount={logs.length}
-				itemSize={AGENT_LOG_LINE_HEIGHT}
-				itemKey={(index) => logs[index].id || 0}
-			>
-				{({ index, style }) => {
-					const log = logs[index];
-					const logSource = getLogSource(log.sourceId);
+			<div className="flex flex-col gap-2">
+				<List
+					{...listProps}
+					ref={ref}
+					css={styles.logs}
+					itemCount={logs.length}
+					itemSize={AGENT_LOG_LINE_HEIGHT}
+					itemKey={(index) => logs[index].id || 0}
+				>
+					{({ index, style }) => {
+						const log = logs[index];
+						const logSource = getLogSource(log.sourceId);
 
-					let assignedIcon = false;
-					let icon: JSX.Element;
-					// If no icon is specified, we show a deterministic
-					// colored circle to identify unique scripts.
-					if (logSource.icon) {
-						icon = (
-							<img
-								src={logSource.icon}
-								alt=""
-								width={14}
-								height={14}
-								css={{
-									marginRight: 8,
-									flexShrink: 0,
-								}}
-							/>
-						);
-					} else {
-						icon = (
-							<div
-								css={{
-									width: 14,
-									height: 14,
-									marginRight: 8,
-									flexShrink: 0,
-									background: determineScriptDisplayColor(
-										logSource.display_name,
-									),
-									borderRadius: "100%",
-								}}
-							/>
-						);
-						assignedIcon = true;
-					}
-
-					const doesNextLineHaveDifferentSource =
-						index < logs.length - 1 &&
-						getLogSource(logs[index + 1].sourceId).id !== log.sourceId;
-
-					// We don't want every line to repeat the icon, because
-					// that is ugly and repetitive. This removes the icon
-					// for subsequent lines of the same source and shows a
-					// line instead, visually indicating they are from the
-					// same source.
-					const shouldHideSource =
-						index > 0 &&
-						getLogSource(logs[index - 1].sourceId).id === log.sourceId;
-					if (shouldHideSource) {
-						icon = (
-							<div
-								css={{
-									width: 14,
-									height: 14,
-									marginRight: 8,
-									display: "flex",
-									justifyContent: "center",
-									position: "relative",
-									flexShrink: 0,
-								}}
-							>
-								<div
-									className="dashed-line"
-									css={(theme) => ({
-										height: doesNextLineHaveDifferentSource ? "50%" : "100%",
-										width: 2,
-										background: theme.experimental.l1.outline,
-										borderRadius: 2,
-									})}
+						let assignedIcon = false;
+						let icon: JSX.Element;
+						// If no icon is specified, we show a deterministic
+						// colored circle to identify unique scripts.
+						if (logSource.icon) {
+							icon = (
+								<img
+									src={logSource.icon}
+									alt=""
+									width={14}
+									height={14}
+									css={{
+										marginRight: 8,
+										flexShrink: 0,
+									}}
 								/>
-								{doesNextLineHaveDifferentSource && (
+							);
+						} else {
+							icon = (
+								<div
+									css={{
+										width: 14,
+										height: 14,
+										marginRight: 8,
+										flexShrink: 0,
+										background: determineScriptDisplayColor(
+											logSource.display_name,
+										),
+										borderRadius: "100%",
+									}}
+								/>
+							);
+							assignedIcon = true;
+						}
+
+						const doesNextLineHaveDifferentSource =
+							index < logs.length - 1 &&
+							getLogSource(logs[index + 1].sourceId).id !== log.sourceId;
+
+						// We don't want every line to repeat the icon, because
+						// that is ugly and repetitive. This removes the icon
+						// for subsequent lines of the same source and shows a
+						// line instead, visually indicating they are from the
+						// same source.
+						const shouldHideSource =
+							index > 0 &&
+							getLogSource(logs[index - 1].sourceId).id === log.sourceId;
+						if (shouldHideSource) {
+							icon = (
+								<div
+									css={{
+										width: 14,
+										height: 14,
+										marginRight: 8,
+										display: "flex",
+										justifyContent: "center",
+										position: "relative",
+										flexShrink: 0,
+									}}
+								>
 									<div
 										className="dashed-line"
 										css={(theme) => ({
-											height: 2,
-											width: "50%",
-											top: "calc(50% - 2px)",
-											left: "calc(50% - 1px)",
+											height: doesNextLineHaveDifferentSource ? "50%" : "100%",
+											width: 2,
 											background: theme.experimental.l1.outline,
 											borderRadius: 2,
-											position: "absolute",
 										})}
 									/>
-								)}
-							</div>
-						);
-					}
+									{doesNextLineHaveDifferentSource && (
+										<div
+											className="dashed-line"
+											css={(theme) => ({
+												height: 2,
+												width: "50%",
+												top: "calc(50% - 2px)",
+												left: "calc(50% - 1px)",
+												background: theme.experimental.l1.outline,
+												borderRadius: 2,
+												position: "absolute",
+											})}
+										/>
+									)}
+								</div>
+							);
+						}
 
-					return (
-						<AgentLogLine
-							line={log}
-							number={index + 1}
-							maxLineNumber={logs.length}
-							style={style}
-							sourceIcon={
-								<Tooltip
-									title={
-										<>
-											{logSource.display_name}
-											{assignedIcon && (
-												<i>
-													<br />
-													No icon specified!
-												</i>
-											)}
-										</>
-									}
-								>
-									{icon}
-								</Tooltip>
-							}
-						/>
-					);
-				}}
-			</List>
+						return (
+							<AgentLogLine
+								line={log}
+								number={index + 1}
+								maxLineNumber={logs.length}
+								style={style}
+								sourceIcon={
+									<Tooltip
+										title={
+											<>
+												{logSource.display_name}
+												{assignedIcon && (
+													<i>
+														<br />
+														No icon specified!
+													</i>
+												)}
+											</>
+										}
+									>
+										{icon}
+									</Tooltip>
+								}
+							/>
+						);
+					}}
+				</List>
+
+				{overflowed && (
+					<p>
+						Startup logs exceeded the max size of{" "}
+						<span className="tracking-wide font-mono">1MB</span>, and will not
+						continue to be written to the database! Logs will continue to be
+						written to the
+						<span className="font-mono">/tmp/coder-startup-script.log</span>{" "}
+						file in the workspace.
+					</p>
+				)}
+			</div>
 		);
 	},
 );
