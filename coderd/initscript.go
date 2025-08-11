23072bd90f60f44a4b7bcd8b65ac76c387571e27
Kacper Sawicki <kacper@coder.com>
2025-08-11 14:11:11 +0000

    add Content-Digest header to init script response

--- coderd/initscript.go
+++ coderd/initscript.go
@@ -1,6 +1,8 @@
 package coderd
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 	"net/http"
 	"strings"
@@ -34,7 +36,10 @@ func (api *API) initScript(rw http.ResponseWriter, r *http.Request) {
 	script = strings.ReplaceAll(script, "${ACCESS_URL}", api.AccessURL.String()+"/")
 	script = strings.ReplaceAll(script, "${AUTH_TYPE}", "token")
 
+	scriptBytes := []byte(script)
+	hash := sha256.Sum256(scriptBytes)
+	rw.Header().Set("Content-Digest", fmt.Sprintf("sha256:%x", base64.StdEncoding.EncodeToString(hash[:])))
 	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	rw.WriteHeader(http.StatusOK)
-	_, _ = rw.Write([]byte(script))
+	_, _ = rw.Write(scriptBytes)
 }
