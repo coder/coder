fca38059ce68606e13e3364acfff1841eb1ef139
Brett Kolodny <brett.kolodny@coder.com>
2025-08-13 17:01:42 +0000

    chore: use consistent whitespace

--- site/src/pages/WorkspacePage/Workspace.tsx
+++ site/src/pages/WorkspacePage/Workspace.tsx
@@ -156,10 +156,10 @@ export const Workspace: FC<WorkspaceProps> = ({
 				<div
 					style={{
 						background: `radial-gradient(
-      circle at 1px 1px,
-      hsl(var(--surface-invert-secondary)) 0,
-      transparent 1px
-    ) -2px -2px / 16px 16px;`,
+			circle at 1px 1px,
+			hsl(var(--surface-invert-secondary)) 0,
+			transparent 1px
+		) -2px -2px / 16px 16px;`,
 					}}
 					className="p-8 overflow-y-auto relative w-full"
 				>
