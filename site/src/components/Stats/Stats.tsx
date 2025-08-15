58423d282d6fa6bbe3d0465b8bb6fc72f85c2c31
Michael Smith <throwawayclover@gmail.com>
2025-08-15 06:35:02 +0000

    chore: convert most of Stats

--- site/src/components/Stats/Stats.tsx
+++ site/src/components/Stats/Stats.tsx
@@ -6,7 +6,11 @@ export const Stats: FC<HTMLAttributes<HTMLDivElement>> = ({
 	...attrs
 }) => {
 	return (
-		<div css={styles.stats} {...attrs}>
+		<div
+			className="block p-4 rounded-md border-border m-0 flex-wrap text-content-secondary md:flex md:py-0"
+			css={styles.stats}
+			{...attrs}
+		>
 			{children}
 		</div>
 	);
@@ -19,65 +23,19 @@ interface StatsItemProps extends HTMLAttributes<HTMLDivElement> {
 
 export const StatsItem: FC<StatsItemProps> = ({ label, value, ...attrs }) => {
 	return (
-		<div css={styles.statItem} {...attrs}>
-			<span css={styles.statsLabel}>{label}:</span>
-			<span css={styles.statsValue}>{value}</span>
+		<div className="py-3.5 px-4 flex items-baseline gap-2 md:p-2" {...attrs}>
+			<span className="block break-words">{label}:</span>
+			<span className="mt-0.5 flex break-words text-content-primary items-center [&_a]:text-content-primary no-underline font-semibold [&_a]:hover:underline">
+				{value}
+			</span>
 		</div>
 	);
 };
 
 const styles = {
-	stats: (theme) => ({
-		...(theme.typography.body2 as CSSObject),
-		paddingLeft: 16,
-		paddingRight: 16,
-		borderRadius: 8,
-		border: `1px solid ${theme.palette.divider}`,
-		display: "flex",
-		alignItems: "center",
-		color: theme.palette.text.secondary,
-		margin: "0px",
-		flexWrap: "wrap",
-
-		[theme.breakpoints.down("md")]: {
-			display: "block",
-			padding: 16,
-		},
-	}),
-
-	statItem: (theme) => ({
-		padding: 14,
-		paddingLeft: 16,
-		paddingRight: 16,
-		display: "flex",
-		alignItems: "baseline",
-		gap: 8,
-
-		[theme.breakpoints.down("md")]: {
-			padding: 8,
-		},
-	}),
-
-	statsLabel: {
-		display: "block",
-		wordWrap: "break-word",
+	stats: (theme) => {
+		const value = theme.typography.body2;
+		console.log(value);
+		return value as CSSObject;
 	},
-
-	statsValue: (theme) => ({
-		marginTop: 2,
-		display: "flex",
-		wordWrap: "break-word",
-		color: theme.palette.text.primary,
-		alignItems: "center",
-
-		"& a": {
-			color: theme.palette.text.primary,
-			textDecoration: "none",
-			fontWeight: 600,
-
-			"&:hover": {
-				textDecoration: "underline",
-			},
-		},
-	}),
 } satisfies Record<string, Interpolation<Theme>>;
