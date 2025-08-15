0d406fab02c3434646dd6db448f39a3e3dfe4432
Michael Smith <throwawayclover@gmail.com>
2025-08-15 07:01:34 +0000

    refactor: SearchField

--- site/src/components/SearchField/SearchField.tsx
+++ site/src/components/SearchField/SearchField.tsx
@@ -1,11 +1,9 @@
-import { useTheme } from "@emotion/react";
 import IconButton from "@mui/material/IconButton";
 import InputAdornment from "@mui/material/InputAdornment";
 import TextField, { type TextFieldProps } from "@mui/material/TextField";
 import Tooltip from "@mui/material/Tooltip";
-import visuallyHidden from "@mui/utils/visuallyHidden";
 import { SearchIcon, XIcon } from "lucide-react";
-import { type FC, useEffect, useRef } from "react";
+import type { FC } from "react";
 
 export type SearchFieldProps = Omit<TextFieldProps, "onChange"> & {
 	onChange: (query: string) => void;
@@ -19,46 +17,27 @@ export const SearchField: FC<SearchFieldProps> = ({
 	InputProps,
 	...textFieldProps
 }) => {
-	const theme = useTheme();
-	const inputRef = useRef<HTMLInputElement>(null);
-
-	useEffect(() => {
-		if (autoFocus) {
-			inputRef.current?.focus();
-		}
-	});
-
 	return (
 		<TextField
+			autoFocus={autoFocus}
 			// Specifying `minWidth` so that the text box can't shrink so much
 			// that it becomes un-clickable as we add more filter controls
-			css={{ minWidth: "280px" }}
+			className="min-w-[280px]"
 			size="small"
 			value={value}
 			onChange={(e) => onChange(e.target.value)}
-			inputRef={inputRef}
 			InputProps={{
 				startAdornment: (
 					<InputAdornment position="start">
-						<SearchIcon
-							className="size-icon-xs"
-							css={{
-								color: theme.palette.text.secondary,
-							}}
-						/>
+						<SearchIcon className="size-icon-xs text-content-secondary" />
 					</InputAdornment>
 				),
 				endAdornment: value !== "" && (
 					<InputAdornment position="end">
 						<Tooltip title="Clear search">
-							<IconButton
-								size="small"
-								onClick={() => {
-									onChange("");
-								}}
-							>
+							<IconButton size="small" onClick={() => onChange("")}>
 								<XIcon className="size-icon-xs" />
-								<span css={{ ...visuallyHidden }}>Clear search</span>
+								<span className="sr-only">Clear search</span>
 							</IconButton>
 						</Tooltip>
 					</InputAdornment>
