f2ee89c36ab53cc9b952ab8586695c671a8c9744
Callum Styan <callumstyan@gmail.com>
2025-08-19 10:24:40 -0700

    fix: fix more `TestWorkspaceAutobuild` flakes (#19417)
    made these commits yesterday but apparently I forgot to push so they got
    missed in https://github.com/coder/coder/pull/19398
    
    ---------
    
    Signed-off-by: Callum Styan <callumstyan@gmail.com>

--- enterprise/coderd/workspaces_test.go
+++ enterprise/coderd/workspaces_test.go
@@ -1122,7 +1122,7 @@ func TestWorkspaceAutobuild(t *testing.T) {
 
 		// Simulate the workspace being dormant beyond the threshold.
 		tickTime2 := ws.DormantAt.Add(2 * transitionTTL)
-		coderdtest.UpdateProvisionerLastSeenAt(t, db, p.ID, tickTime)
+		coderdtest.UpdateProvisionerLastSeenAt(t, db, p.ID, tickTime2)
 		ticker <- tickTime2
 		stats = <-statCh
 		require.Len(t, stats.Transitions, 1)
@@ -1481,7 +1481,7 @@ func TestWorkspaceAutobuild(t *testing.T) {
 
 		// Force an autostart transition again.
 		tickTime2 := sched.Next(firstBuild.CreatedAt)
-		coderdtest.UpdateProvisionerLastSeenAt(t, db, p.ID, tickTime)
+		coderdtest.UpdateProvisionerLastSeenAt(t, db, p.ID, tickTime2)
 		tickCh <- tickTime2
 		stats = <-statsCh
 		require.Len(t, stats.Errors, 0)
