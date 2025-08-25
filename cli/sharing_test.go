fb5b947c2e9c4d88bd40dde865faf9123137eca6
Brett Kolodny <brett.kolodny@coder.com>
2025-08-25 15:53:11 +0000

    wip

--- cli/sharing_test.go
+++ cli/sharing_test.go
@@ -7,8 +7,10 @@ import (
 	"github.com/coder/coder/v2/coderd/coderdtest"
 	"github.com/coder/coder/v2/coderd/database"
 	"github.com/coder/coder/v2/coderd/database/dbfake"
+	"github.com/coder/coder/v2/codersdk"
 	"github.com/coder/coder/v2/testutil"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSharingShare(t *testing.T) {
@@ -21,63 +23,79 @@ func TestSharingShare(t *testing.T) {
 			client, db                           = coderdtest.NewWithDatabase(t, nil)
 			orgOwner                             = coderdtest.CreateFirstUser(t, client)
 			workspaceOwnerClient, workspaceOwner = coderdtest.CreateAnotherUser(t, client, orgOwner.OrganizationID)
-			_, toShareWithUser                   = coderdtest.CreateAnotherUser(t, client, orgOwner.OrganizationID)
 			workspace                            = dbfake.WorkspaceBuild(t, db, database.WorkspaceTable{
 				OwnerID:        workspaceOwner.ID,
 				OrganizationID: orgOwner.OrganizationID,
 			}).Do().Workspace
+			_, toShareWithUser = coderdtest.CreateAnotherUser(t, client, orgOwner.OrganizationID)
 		)
 
-		var _ = testutil.Context(t, testutil.WaitMedium)
-		var _, root = clitest.New(t, "sharing", "share", workspace.Name, "--user", toShareWithUser.Username)
+		ctx := testutil.Context(t, testutil.WaitMedium)
+		var inv, root = clitest.New(t, "sharing", "share", workspace.Name, "--user", toShareWithUser.Username)
 		clitest.SetupConfig(t, workspaceOwnerClient, root)
+
+		err := inv.WithContext(ctx).Run()
+		require.NoError(t, err)
 
 		// TODO: Test output of the command
 
-		// TODO: Test updated ACL
+		acl, err := workspaceOwnerClient.WorkspaceACL(inv.Context(), workspace.ID)
+		assert.NoError(t, err)
+		assert.Equal(t,
+			codersdk.WorkspaceACL{
+				Users: []codersdk.WorkspaceUser{{
+					MinimalUser: codersdk.MinimalUser{
+						ID:        toShareWithUser.ID,
+						Username:  toShareWithUser.Username,
+						AvatarURL: toShareWithUser.AvatarURL,
+					},
+					Role: codersdk.WorkspaceRole("use"),
+				}},
+			},
+			acl)
 
 		assert.True(t, false)
 	})
 
-	t.Run("ShareWithUsers+Multiple", func(t *testing.T) {
-		t.Parallel()
+	// t.Run("ShareWithUsers+Multiple", func(t *testing.T) {
+	// 	t.Parallel()
 
-		assert.True(t, false)
-	})
+	// 	assert.True(t, false)
+	// })
 
-	t.Run("ShareWithUsers+UseRole", func(t *testing.T) {
-		t.Parallel()
+	// t.Run("ShareWithUsers+UseRole", func(t *testing.T) {
+	// 	t.Parallel()
 
-		assert.True(t, false)
-	})
+	// 	assert.True(t, false)
+	// })
 
-	t.Run("ShareWithUsers+AdminRole", func(t *testing.T) {
-		t.Parallel()
+	// t.Run("ShareWithUsers+AdminRole", func(t *testing.T) {
+	// 	t.Parallel()
 
-		assert.True(t, false)
-	})
+	// 	assert.True(t, false)
+	// })
 
-	t.Run("ShareWithGroups+Simple", func(t *testing.T) {
-		t.Parallel()
+	// t.Run("ShareWithGroups+Simple", func(t *testing.T) {
+	// 	t.Parallel()
 
-		assert.True(t, false)
-	})
+	// 	assert.True(t, false)
+	// })
 
-	t.Run("ShareWithGroups+Mutliple", func(t *testing.T) {
-		t.Parallel()
+	// t.Run("ShareWithGroups+Mutliple", func(t *testing.T) {
+	// 	t.Parallel()
 
-		assert.True(t, false)
-	})
+	// 	assert.True(t, false)
+	// })
 
-	t.Run("ShareWithGroups+UseRole", func(t *testing.T) {
-		t.Parallel()
+	// t.Run("ShareWithGroups+UseRole", func(t *testing.T) {
+	// 	t.Parallel()
 
-		assert.True(t, false)
-	})
+	// 	assert.True(t, false)
+	// })
 
-	t.Run("ShareWithGroups+AdminRole", func(t *testing.T) {
-		t.Parallel()
+	// t.Run("ShareWithGroups+AdminRole", func(t *testing.T) {
+	// 	t.Parallel()
 
-		assert.True(t, false)
-	})
+	// 	assert.True(t, false)
+	// })
 }
