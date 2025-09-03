1242cbeb599c5b1430e8d6a386d6838554f5d747
Andrew Aquino <dawneraq@gmail.com>
2025-09-03 01:29:55 +0000

    refactor: replace deprecated Popover in WorkspacesButton

--- site/src/pages/WorkspacesPage/WorkspacesButton.tsx
+++ site/src/pages/WorkspacesPage/WorkspacesButton.tsx
@@ -6,7 +6,7 @@ import {
 	Popover,
 	PopoverContent,
 	PopoverTrigger,
-} from "components/deprecated/Popover/Popover";
+} from "components/Popover/Popover";
 import { Loader } from "components/Loader/Loader";
 import { MenuSearch } from "components/Menu/MenuSearch";
 import { OverflowY } from "components/OverflowY/OverflowY";
@@ -54,14 +54,15 @@ export const WorkspacesButton: FC<WorkspacesButtonProps> = ({
 
 	return (
 		<Popover>
-			<PopoverTrigger>
+			<PopoverTrigger asChild>
 				<Button size="lg">
 					{children}
 					<ChevronDownIcon />
 				</Button>
 			</PopoverTrigger>
 			<PopoverContent
-				horizontal="right"
+				align="end"
+				className="bg-surface-secondary border-surface-quaternary w-[320px]"
 				css={{
 					".MuiPaper-root": searchStyles.content,
 				}}
