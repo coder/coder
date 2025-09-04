07fd1e2c1a4cdd4da41385111702dcca716bd1e2
Andrew Aquino <dawneraq@gmail.com>
2025-09-04 01:08:47 +0000

    test: use set past dateRange

--- site/src/pages/TemplatePage/TemplateInsightsPage/TemplateInsightsControls.stories.tsx
+++ site/src/pages/TemplatePage/TemplateInsightsPage/TemplateInsightsControls.stories.tsx
@@ -11,7 +11,10 @@ export default meta;
 type Story = StoryObj<typeof TemplateInsightsControls>;
 
 const defaultArgs: Partial<ComponentProps<typeof TemplateInsightsControls>> = {
-	dateRange: { startDate: new Date(), endDate: new Date() },
+	dateRange: {
+		startDate: new Date("2025-08-05"),
+		endDate: new Date("2025-08-07"),
+	},
 	setDateRange: () => {},
 	searchParams: new URLSearchParams(),
 	setSearchParams: () => {},
