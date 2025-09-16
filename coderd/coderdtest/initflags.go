f004c6865c623c965b98b2e45a6d9da0983592da
Steven Masley <stevenmasley@gmail.com>
2025-09-16 11:37:42 -0500

    chore: change parallism to 8, add helpful panic message

--- coderd/coderdtest/initflags.go
+++ coderd/coderdtest/initflags.go
@@ -3,14 +3,18 @@ package coderdtest
 import (
 	"flag"
 	"fmt"
+	"runtime"
 	"strconv"
 	"testing"
 )
 
 const (
-	MaxTestParallelism = 1
+	// MaxTestParallelism is set to match our MakeFile's `make test` target.
+	MaxTestParallelism = 8
 )
 
+// init defines the default parallelism for tests, capping it to MaxTestParallelism.
+// Any user-provided value for -test.parallel will override this.
 func init() {
 	// Setup the test flags.
 	testing.Init()
@@ -21,8 +25,19 @@ func init() {
 	par := flag.CommandLine.Lookup("test.parallel")
 	parValue, err := strconv.ParseInt(par.Value.String(), 0, 64)
 	if err != nil {
-		// This should never happen.
-		panic(fmt.Sprintf("failed to parse test.parallel: %v", err))
+		// This should never happen, but if it does, panic with a useful message. If you
+		// are reading this message because of a panic, that means the default value for
+		// -test.parallel is not an integer. A safe fix is to comment out the panic. This
+		// will assume the default value of '0', and replace it with MaxTestParallelism.
+		// Which is not ideal, but at least tests will run.
+		msg := fmt.Sprintf("failed to parse test.parallel: %v", err)
+		info := "Resolve the issue in the file initflags.go"
+		_, file, line, ok := runtime.Caller(0)
+		if ok {
+			info = fmt.Sprintf("Resolve the issue in the file %s:%d", file, line)
+		}
+
+		panic(msg + "\n" + info)
 	}
 
 	if parValue > MaxTestParallelism {
