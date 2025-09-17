42cc91bcdad20562c758ba86bec480d4b539e8eb
Michael Smith <michaelsmith@coder.com>
2025-09-17 23:49:48 +0000

    fix: update styling for open proxy menu

--- site/src/modules/dashboard/Navbar/ProxyMenu.tsx
+++ site/src/modules/dashboard/Navbar/ProxyMenu.tsx
@@ -190,6 +190,7 @@ export const ProxyMenu: FC<ProxyMenuProps> = ({ proxyContextValue }) => {
 									{proxy.display_name}
 
 									<Latency
+										className="ml-auto"
 										latency={latencies?.[proxy.id]?.latencyMS}
 										isLoading={proxyLatencyLoading(proxy)}
 									/>
