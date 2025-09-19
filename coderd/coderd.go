c113d9183013cd61ee0f07d2b0059e4d7708c3c6
Danny Kopping <danny@coder.com>
2025-09-19 13:42:45 +0200

    chore: integration test

--- coderd/coderd.go
+++ coderd/coderd.go
@@ -2013,7 +2013,7 @@ func (api *API) CreateInMemoryAIBridgeServer(dialCtx context.Context) (client ai
 
 	mux := drpcmux.New()
 	srv, err := aibridgedserver.NewServer(api.ctx, api.Database, api.Logger.Named("aibridgedserver"),
-		api.DeploymentValues.AccessURL.String(), api.ExternalAuthConfigs)
+		api.AccessURL.String(), api.ExternalAuthConfigs)
 	if err != nil {
 		return nil, err
 	}
