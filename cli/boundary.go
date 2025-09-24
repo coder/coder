7db3f5d8b5791c7a77fc86dd3ece0bd9f201bdc5
Benjamin Peinhardt <benjaminpeinhardt@gmail.com>
2025-09-24 15:50:32 +0000

    update comments from jail to boundary

--- cli/boundary.go
+++ cli/boundary.go
@@ -6,8 +6,8 @@ import (
 )
 
 func (*RootCmd) boundary() *serpent.Command {
-	cmd := boundarycli.BaseCommand() // Package coder/jail/cli exports a "base command" designed to be integrated as a subcommand.
-	cmd.Hidden = true                // We want jail to be a hidden command in coder for now.
-	cmd.Use += " [args...]"          // The base command looks like `jail -- command`. Serpent adds the flags piece, but we need to add the args.
+	cmd := boundarycli.BaseCommand() // Package coder/boundary/cli exports a "base command" designed to be integrated as a subcommand.
+	cmd.Hidden = true                // We want boundary to be a hidden command in coder for now.
+	cmd.Use += " [args...]"          // The base command looks like `boundary -- command`. Serpent adds the flags piece, but we need to add the args.
 	return cmd
 }
