47fff3af9ad950728a83097c18c5b95389cea894
Benjamin Peinhardt <benjaminpeinhardt@gmail.com>
2025-09-24 15:41:42 +0000

    clean up test

--- cli/boundary_test.go
+++ cli/boundary_test.go
@@ -1,7 +1,6 @@
 package cli_test
 
 import (
-	"context"
 	"testing"
 
 	boundarycli "github.com/coder/boundary/cli"
@@ -14,22 +13,20 @@ import (
 // Actually testing the functionality of coder/boundary takes place in the
 // coder/boundary repo, since it's a dependency of coder.
 // Here we want to test basically that integrating it as a subcommand doesn't break anything.
-func TestJailSubcommand(t *testing.T) {
+func TestBoundarySubcommand(t *testing.T) {
+	t.Parallel()
+	ctx := testutil.Context(t, testutil.WaitShort)
+
 	inv, _ := clitest.New(t, "boundary", "--help")
 	pty := ptytest.New(t).Attach(inv)
-	ctx, cancelFunc := context.WithTimeout(context.Background(), testutil.WaitShort)
-	defer cancelFunc()
-	done := make(chan any)
+
 	go func() {
 		err := inv.WithContext(ctx).Run()
 		assert.NoError(t, err)
-		close(done)
 	}()
 
 	// Expect the --help output to include the short description.
 	// We're simply confirming that `coder boundary --help` ran without a runtime error as
 	// a good chunk of serpents self validation logic happens at runtime.
 	pty.ExpectMatch(boundarycli.BaseCommand().Short)
-	cancelFunc()
-	<-done
 }
