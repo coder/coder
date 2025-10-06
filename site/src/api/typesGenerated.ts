1f8273215a639779b2750aef418207e81a148fbb
Sas Swart <sas.swart.cdk@gmail.com>
2025-10-06 09:38:47 +0000

    make -B gen

--- site/src/api/typesGenerated.ts
+++ site/src/api/typesGenerated.ts
@@ -2060,7 +2060,7 @@ export interface OrganizationProvisionerJobsOptions {
 	readonly IDs: readonly string[];
 	readonly Status: readonly ProvisionerJobStatus[];
 	readonly Tags: Record<string, string>;
-	readonly InitiatorID: string | null;
+	readonly Initiator: string;
 }
 
 // From codersdk/idpsync.go
