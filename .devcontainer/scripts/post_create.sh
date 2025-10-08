cc0b8b55b5c205ef3f61605071b3a36a6ceb678c
Mathias Fredriksson <mafredri@gmail.com>
2025-10-08 10:13:14 +0300

    Update post_create.sh

--- .devcontainer/scripts/post_create.sh
+++ .devcontainer/scripts/post_create.sh
@@ -10,7 +10,7 @@ install_devcontainer_cli() {
 
 install_ssh_config() {
 	echo "🔑 Installing SSH configuration..."
-	if [[ -d /mnt/home/coder/.ssh ]]; then
+	if [ -d /mnt/home/coder/.ssh ]; then
 		rsync -a /mnt/home/coder/.ssh/ ~/.ssh/
 		chmod 0700 ~/.ssh
 	else
