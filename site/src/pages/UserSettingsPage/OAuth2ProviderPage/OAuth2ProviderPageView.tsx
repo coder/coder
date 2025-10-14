3e96c09c09e3872c8bd1b05dc09c96be0e83ff39
Jake Howell <jacob@coder.com>
2025-10-14 02:01:26 +0000

    fix: resolve oauth2 table empty state not rendering

--- site/src/pages/UserSettingsPage/OAuth2ProviderPage/OAuth2ProviderPageView.tsx
+++ site/src/pages/UserSettingsPage/OAuth2ProviderPage/OAuth2ProviderPageView.tsx
@@ -8,7 +8,9 @@ import type * as TypesGen from "api/typesGenerated";
 import { ErrorAlert } from "components/Alert/ErrorAlert";
 import { Avatar } from "components/Avatar/Avatar";
 import { Button } from "components/Button/Button";
+import { ChooseOne, Cond } from "components/Conditionals/ChooseOne";
 import { Stack } from "components/Stack/Stack";
+import { TableEmpty } from "components/TableEmpty/TableEmpty";
 import { TableLoader } from "components/TableLoader/TableLoader";
 import type { FC } from "react";
 
@@ -38,19 +40,19 @@ const OAuth2ProviderPageView: FC<OAuth2ProviderPageViewProps> = ({
 						</TableRow>
 					</TableHead>
 					<TableBody>
-						{isLoading && <TableLoader />}
-						{apps?.map((app) => (
-							<OAuth2AppRow key={app.id} app={app} revoke={revoke} />
-						))}
-						{apps?.length === 0 && (
-							<TableRow>
-								<TableCell colSpan={999}>
-									<div css={{ textAlign: "center" }}>
-										No OAuth2 applications have been authorized.
-									</div>
-								</TableCell>
-							</TableRow>
-						)}
+						<ChooseOne>
+							<Cond condition={isLoading}>
+								<TableLoader />
+							</Cond>
+							<Cond condition={apps === null || apps?.length === 0}>
+								<TableEmpty message="No OAuth2 applications have been authorized" />
+							</Cond>
+							<Cond>
+								{apps?.map((app) => (
+									<OAuth2AppRow key={app.id} app={app} revoke={revoke} />
+								))}
+							</Cond>
+						</ChooseOne>
 					</TableBody>
 				</Table>
 			</TableContainer>
