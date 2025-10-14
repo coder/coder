134495adab5d026f8d1382db0cde00e00285c831
Jake Howell <jacob@coder.com>
2025-10-14 10:36:02 +0000

    feat: add `/aigovernance` routes and layout

---
+++ site/src/pages/AIGovernancePage/RequestLogsPage/RequestLogsPageView.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+
+// biome-ignore lint/suspicious/noEmptyInterface: TODO
+interface RequestLogsPageViewProps {
+
+}
+
+export const RequestLogsPageView: FC<RequestLogsPageViewProps> = () => {
+	return (
+		<div>TODO</div>
+	);
+};
