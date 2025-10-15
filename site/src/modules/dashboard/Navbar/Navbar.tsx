b7280f53377d6d5ecbc06ddba2702309184c3997
Jake Howell <jake@hwll.me>
2025-10-15 08:55:04 +0000

    chore: add missing todo

--- site/src/modules/dashboard/Navbar/Navbar.tsx
+++ site/src/modules/dashboard/Navbar/Navbar.tsx
@@ -26,6 +26,7 @@ export const Navbar: FC = () => {
 	const canViewConnectionLog =
 		featureVisibility.connection_log && permissions.viewAnyConnectionLog;
 	// TODO: We should have a separate permission for this
+	// TODO: We should have a check for the experiment too.
 	const canViewAIGovernance = featureVisibility.aibridge;
 
 	const uniqueLinks = new Map<string, LinkConfig>();
