0d4bd15cbe982efb98010d2c934fd9164ab2b9b3
Jake Howell <jake@hwll.me>
2025-10-22 02:43:53 +0000

    fix: improve rendered content from json blob

--- site/src/pages/AIGovernancePage/RequestLogsPage/RequestLogsRow/RequestLogsRow.tsx
+++ site/src/pages/AIGovernancePage/RequestLogsPage/RequestLogsRow/RequestLogsRow.tsx
@@ -66,16 +66,34 @@ export const RequestLogsRow: FC<RequestLogsRowProps> = ({ interception }) => {
 			</TableRow>
 			{isOpen && (
 				<TableRow>
-					<TableCell colSpan={999}>
-						<pre>
-							{JSON.stringify(
-								{
-									interception,
-								},
-								null,
-								2,
-							)}
-						</pre>
+					<TableCell colSpan={999} className="p-4 border-t-0">
+						<dl
+							className={cn([
+								"text-xs text-content-secondary",
+								"m-0 grid grid-cols-[auto_1fr] gap-x-4 items-center",
+								"[&_dd]:text-content-primary [&_dd]:font-mono [&_dd]:leading-[22px] [&_dt]:font-medium",
+							])}
+						>
+							<dt>Request ID:</dt>
+							<dd data-chromatic="ignore">{interception.id}</dd>
+
+							<dt>Timestamp:</dt>
+							<dd data-chromatic="ignore">
+								{new Date(interception.started_at).toLocaleString()}
+							</dd>
+
+							<dt>Initiator:</dt>
+							<dd data-chromatic="ignore">{interception.initiator.username}</dd>
+
+							<dt>Model:</dt>
+							<dd data-chromatic="ignore">{interception.model}</dd>
+
+							<dt>Total Tokens:</dt>
+							<dd data-chromatic="ignore">{tokens}</dd>
+
+							<dt>Tool Calls:</dt>
+							<dd data-chromatic="ignore">{interception.tool_usages.length}</dd>
+						</dl>
 					</TableCell>
 				</TableRow>
 			)}
