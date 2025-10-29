f91e13b135ccad65dc7d3228b076653be9b4fea5
Steven Masley <stevenmasley@gmail.com>
2025-10-29 18:27:06 +0000

    make gen

--- site/e2e/provisionerGenerated.ts
+++ site/e2e/provisionerGenerated.ts
@@ -405,6 +405,7 @@ export interface Metadata {
   runningAgentAuthTokens: RunningAgentAuthToken[];
   taskId: string;
   taskPrompt: string;
+  templateVersionId: string;
 }
 
 /** Config represents execution configuration shared by all subsequent requests in the Session */
@@ -414,6 +415,8 @@ export interface Config {
   /** state is the provisioner state (if any) */
   state: Uint8Array;
   provisionerLogLevel: string;
+  templateId: string;
+  templateVersionId: string;
 }
 
 /** ParseRequest consumes source-code to produce inputs. */
@@ -1298,6 +1301,9 @@ export const Metadata = {
     if (message.taskPrompt !== "") {
       writer.uint32(186).string(message.taskPrompt);
     }
+    if (message.templateVersionId !== "") {
+      writer.uint32(194).string(message.templateVersionId);
+    }
     return writer;
   },
 };
@@ -1312,6 +1318,12 @@ export const Config = {
     }
     if (message.provisionerLogLevel !== "") {
       writer.uint32(26).string(message.provisionerLogLevel);
+    }
+    if (message.templateId !== "") {
+      writer.uint32(34).string(message.templateId);
+    }
+    if (message.templateVersionId !== "") {
+      writer.uint32(42).string(message.templateVersionId);
     }
     return writer;
   },
