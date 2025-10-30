dc96f84d6551dd1faec759ef214c3cb48956c877
Zach Kipp <zach.kipp@coder.com>
2025-10-30 14:21:41 -0600

    feat(cli): add macOS support for session token keyring storage
    Add support for storing the CLI session token in the OS keyring on macOS
    when the --use-keyring flag is provided.

---
+++ cli/sessionstore/sessionstore_darwin_test.go
@@ -0,0 +1,34 @@
+//go:build darwin
+
+package sessionstore_test
+
+import (
+	"encoding/base64"
+	"os/exec"
+	"testing"
+)
+
+const (
+	execPathKeychain = "/usr/bin/security"
+	fixedUsername    = "coder-session-token"
+)
+
+func readRawKeychainCredential(t *testing.T, service string) []byte {
+	t.Helper()
+
+	out, err := exec.Command(
+		execPathKeychain,
+		"find-generic-password",
+		"-s", service,
+		"-wa", fixedUsername).CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dst := make([]byte, base64.StdEncoding.DecodedLen(len(out)))
+	n, err := base64.StdEncoding.Decode(dst, out)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return dst[:n]
+}
