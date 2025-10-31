650069a99ce9fe91678607e21e81e6e98d5aaeb5
DevelopmentCats <christofer@coder.com>
2025-10-31 11:35:05 -0500

    feat: replace filebrowser with copyparty in coder dogfood template

--- dogfood/coder/main.tf
+++ dogfood/coder/main.tf
@@ -393,12 +393,20 @@ module "jetbrains" {
   tooltip       = "You need to [Install Coder Desktop](https://coder.com/docs/user-guides/desktop#install-coder-desktop) to use this button."
 }
 
-module "filebrowser" {
-  count      = data.coder_workspace.me.start_count
-  source     = "dev.registry.coder.com/coder/filebrowser/coder"
-  version    = "1.1.2"
-  agent_id   = coder_agent.dev.id
-  agent_name = "dev"
+module "copyparty" {
+  count     = data.coder_workspace.me.start_count
+  source    = "dev.registry.coder.com/djarbz/copyparty/coder"
+  version   = "1.0.1"
+  agent_id  = coder_agent.dev.id
+  subdomain = true
+  arguments = [
+    "-v", "/tmp:/tmp:r",
+    "-v", "/home/coder:/home:rw",
+    "-v", "${local.repo_dir}:/coder:A:c,dotsrch",
+    "-e2dsa",
+    "--re-maxage", "900",
+    "--see-dots",
+  ]
 }
 
 module "coder-login" {
