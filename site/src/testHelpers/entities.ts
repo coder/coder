aabdce780f936261c452934f17f97033fe364dd2
ケイラ <kayla@tree.camp>
2025-11-12 21:23:23 +0000

    oops

--- site/src/testHelpers/entities.ts
+++ site/src/testHelpers/entities.ts
@@ -1791,31 +1791,6 @@ export const MockTemplateVersionVariable5: TypesGen.TemplateVersionVariable = {
 	sensitive: false,
 };
 
-export const MockWorkspaceRequest: TypesGen.CreateWorkspaceRequest = {
-	name: "test",
-	template_version_id: "test-template-version",
-	rich_parameter_values: [],
-};
-
-export const MockWorkspaceRichParametersRequest: TypesGen.CreateWorkspaceRequest =
-	{
-		name: "test",
-		template_version_id: "test-template-version",
-		rich_parameter_values: [
-			{
-				name: MockTemplateVersionParameter1.name,
-				value: MockTemplateVersionParameter1.default_value,
-			},
-		],
-	};
-
-const _MockUserAgent = {
-	browser: "Chrome 99.0.4844",
-	device: "Other",
-	ip_address: "11.22.33.44",
-	os: "Windows 10",
-};
-
 export const MockAuthMethodsPasswordOnly: TypesGen.AuthMethods = {
 	password: { enabled: true },
 	github: { enabled: false, default_provider_configured: true },
