b8b8ec314947175171f0f3407fc746cda7eab96b
Paweł Banaszewski <pawel@coder.com>
2025-11-13 10:53:10 +0000

    lint fix

--- provisioner/terraform/testdata/fake-terraform-installer/setup_fakes.sh
+++ provisioner/terraform/testdata/fake-terraform-installer/setup_fakes.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ $# -lt 2 ]; then
-    echo "at least 2 arguments required"
+	echo "at least 2 arguments required"
 fi
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -13,15 +13,14 @@ shift
 
 versions=("$@")
 versions_without_last=("${versions[@]:0:${#versions[@]}-1}")
-last_version="${@: -1}"
+last_version="${versions[-1]}"
 
-
-cd $tmpDir
-cp ${SCRIPT_DIR}/fake-terraform.go .
+cd "$tmpDir"
+cp "${SCRIPT_DIR}"/fake-terraform.go .
 
 ## Prepare main index.json
 # Prefix
-cat > ./index.json << EOF
+cat >./index.json <<EOF
 {
   "name": "terraform",
   "versions": {
@@ -29,7 +28,7 @@ EOF
 
 # versions with comma in the end
 for ver in "${versions_without_last[@]}"; do
-  cat >> ./index.json << EOF
+	cat >>./index.json <<EOF
     "${ver}": {
       "builds": [
         {
@@ -48,7 +47,7 @@ EOF
 done
 
 # last without comma
-cat >> ./index.json << EOF
+cat >>./index.json <<EOF
     "${last_version}": {
       "builds": [
         {
@@ -70,8 +69,8 @@ EOF
 ## Prepare per version index.json
 # Prepare index.json
 for ver in "${versions[@]}"; do
-  mkdir ./${ver}
-  cat > ./${ver}/index.json << EOF
+	mkdir ./"${ver}"
+	cat >./"${ver}"/index.json <<EOF
 {
   "builds": [
     {
@@ -92,11 +91,11 @@ done
 ## Prepare zips
 go mod init fake-terraform
 
-echo "LICENSE" > ./LICENSE.txt
+echo "LICENSE" >./LICENSE.txt
 for ver in "${versions[@]}"; do
-  go build -ldflags "-X main.version=${ver}" -o terraform
-  zip terraform_${ver}_linux_amd64.zip terraform LICENSE.txt
-  mv terraform_${ver}_linux_amd64.zip ./${ver}
+	go build -ldflags "-X main.version=${ver}" -o terraform
+	zip terraform_"${ver}"_linux_amd64.zip terraform LICENSE.txt
+	mv terraform_"${ver}"_linux_amd64.zip ./"${ver}"
 done
 
 rm -f LICENSE.txt
