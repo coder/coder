1e7dab933be8dbb15dc7b623d059569e39f09d10
Marcin Tojek <marcin@coder.com>
2025-11-18 13:53:11 +0100

    integration test

--- enterprise/coderd/templates_test.go
+++ enterprise/coderd/templates_test.go
@@ -2111,3 +2111,70 @@ func TestMultipleOrganizationTemplates(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestInvalidateTemplatePrebuilds(t *testing.T) {
+	t.Parallel()
+
+	// Given
+	templateVersionParameters := []*proto.RichParameter{
+		{Name: "param1", Type: "string", Required: false, DefaultValue: "default1"},
+		{Name: "param2", Type: "string", Required: false, DefaultValue: "default2"},
+		{Name: "param3", Type: "string", Required: false, DefaultValue: "default3"},
+	}
+	presetWithParameters1 := &proto.Preset{
+		Name: "Preset With Parameters 1",
+		Parameters: []*proto.PresetParameter{
+			{Name: "param1", Value: "value1"},
+			{Name: "param2", Value: "value2"},
+			{Name: "param3", Value: "value3"},
+		},
+	}
+	presetWithParameters2 := &proto.Preset{
+		Name: "Preset With Parameters 2",
+		Parameters: []*proto.PresetParameter{
+			{Name: "param1", Value: "value4"},
+			{Name: "param2", Value: "value5"},
+			{Name: "param3", Value: "value6"},
+		},
+	}
+
+	//
+
+	ownerClient, owner := coderdenttest.New(t, &coderdenttest.Options{
+		Options: &coderdtest.Options{
+			IncludeProvisionerDaemon: true,
+		},
+		LicenseOptions: &coderdenttest.LicenseOptions{
+			Features: license.Features{
+				codersdk.FeatureTemplateRBAC: 1,
+			},
+		}})
+	templateAdminClient, _ := coderdtest.CreateAnotherUser(t, ownerClient, owner.OrganizationID, rbac.RoleTemplateAdmin())
+
+	planResponse := &proto.Response{
+		Type: &proto.Response_Plan{
+			Plan: &proto.PlanComplete{
+				Presets: []*proto.Preset{
+					presetWithParameters1, presetWithParameters2,
+				},
+				Parameters: templateVersionParameters,
+			},
+		},
+	}
+
+	version1 := coderdtest.CreateTemplateVersion(t, templateAdminClient, owner.OrganizationID, &echo.Responses{
+		Parse:          echo.ParseComplete,
+		ProvisionPlan:  []*proto.Response{planResponse},
+		ProvisionApply: echo.ApplyComplete,
+	})
+	coderdtest.AwaitTemplateVersionJobCompleted(t, templateAdminClient, version1.ID)
+	template := coderdtest.CreateTemplate(t, templateAdminClient, owner.OrganizationID, version1.ID)
+
+	// When
+	ctx := testutil.Context(t, testutil.WaitShort)
+	invalidated, err := templateAdminClient.InvalidateTemplatePrebuilds(ctx, template.ID)
+	require.NoError(t, err)
+
+	// Then
+	require.Equal(t, []string{presetWithParameters1.Name, presetWithParameters2.Name}, invalidated.InvalidatedPresets)
+}
