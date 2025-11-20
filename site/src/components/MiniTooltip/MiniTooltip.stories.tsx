e39841fe219d851dc3aeb96ce800c77503ec46d9
Andrew Aquino <dawneraq@gmail.com>
2025-11-20 20:25:31 +0000

    test: add MiniTooltip stories

---
+++ site/src/components/MiniTooltip/MiniTooltip.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Button } from "components/Button/Button";
+import MiniTooltip from "./MiniTooltip";
+import { expect, screen, userEvent, waitFor } from "storybook/test";
+
+const title = "Add to library";
+const triggerText = "Hover";
+
+const meta: Meta<typeof MiniTooltip> = {
+	title: "components/MiniTooltip",
+	component: MiniTooltip,
+	args: {
+		title,
+		children: <Button variant="outline">{triggerText}</Button>,
+	},
+	play: async ({ step }) => {
+		await step("activate hover trigger", async () => {
+			await userEvent.hover(screen.getByText(triggerText));
+			await waitFor(() =>
+				expect(screen.getByRole("tooltip")).toHaveTextContent(title),
+			);
+		});
+	},
+};
+
+export default meta;
+type Story = StoryObj<typeof MiniTooltip>;
+
+export const Default: Story = {};
