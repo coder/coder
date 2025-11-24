5fb52dca0cef273d0cacd4d5d4c4fc4013d51209
Jake Howell <jake@hwll.me>
2025-11-25 10:23:39 +1100

    fix: remove inflight interceptions from aibridge returned values (#20852)
    Addresses [`aibridge#54`](https://github.com/coder/aibridge/issues/54)
    
    When querying against the values in the database for
    `/api/experimental/aibridge/interceptions` we found strange behaviour
    wherein there was interceptions that lacked prompting and other various
    fields we want. Generally this was as a result of the data not actually
    existing for these values (as they were inflight).
    
    The simple solution to this was to hide them if they didn't exist. This
    PR addresses that.
    
    ---------
    
    Co-authored-by: Danny Kopping <danny@coder.com>

--- coderd/database/queries/aibridge.sql
+++ coderd/database/queries/aibridge.sql
@@ -89,8 +89,10 @@ SELECT
 FROM
 	aibridge_interceptions
 WHERE
+	-- Remove inflight interceptions (ones which lack an ended_at value).
+	aibridge_interceptions.ended_at IS NOT NULL
 	-- Filter by time frame
-	CASE
+	AND CASE
 		WHEN @started_after::timestamptz != '0001-01-01 00:00:00+00'::timestamptz THEN aibridge_interceptions.started_at >= @started_after::timestamptz
 		ELSE true
 	END
@@ -126,8 +128,10 @@ FROM
 JOIN
 	visible_users ON visible_users.id = aibridge_interceptions.initiator_id
 WHERE
+	-- Remove inflight interceptions (ones which lack an ended_at value).
+	aibridge_interceptions.ended_at IS NOT NULL
 	-- Filter by time frame
-	CASE
+	AND CASE
 		WHEN @started_after::timestamptz != '0001-01-01 00:00:00+00'::timestamptz THEN aibridge_interceptions.started_at >= @started_after::timestamptz
 		ELSE true
 	END
