fd85a250409f899c2b0aa1d622ccfbd6913d07fe
Spike Curtis <spike@coder.com>
2025-11-25 14:25:24 +0400

    fix: mock Agent querying OS for listening ports in tests (#20842)
    fixes https://github.com/coder/internal/issues/1123
    
    We want to tests that ports are not included after they are no longer used, but this isn't safe on the real OS networking stack because there is no way to guarantee a port _won't_ be used. Instead, we introduce an interface and fake implementation for testing.
    
    On order to leave the filtering logic in the test path, this PR also does some refactoring.
    
    Caching logic is left in the real OS querying implementation and a new test case is added for it in this PR.

--- agent/ports_unsupported.go
+++ agent/ports_unsupported.go
@@ -2,9 +2,17 @@
 
 package agent
 
-import "github.com/coder/coder/v2/codersdk"
+import (
+	"time"
 
-func (*listeningPortsHandler) getListeningPorts() ([]codersdk.WorkspaceAgentListeningPort, error) {
+	"github.com/coder/coder/v2/codersdk"
+)
+
+type osListeningPortsGetter struct {
+	cacheDuration time.Duration
+}
+
+func (*osListeningPortsGetter) GetListeningPorts() ([]codersdk.WorkspaceAgentListeningPort, error) {
 	// Can't scan for ports on non-linux or non-windows_amd64 systems at the
 	// moment. The UI will not show any "no ports found" message to the user, so
 	// the user won't suspect a thing.
