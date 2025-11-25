e6da205a8025d04f4fd6092ddaf15300d6d17c19
Rowan Smith <rowan@coder.com>
2025-11-25 20:38:23 +1100

    fix: add YAML name for Postgres Connection URL File option

--- codersdk/deployment.go
+++ codersdk/deployment.go
@@ -2559,6 +2559,7 @@ func (c *DeploymentValues) Options() serpent.OptionSet {
 			Flag:        "postgres-url-file",
 			Env:         "CODER_PG_CONNECTION_URL_FILE",
 			Value:       &c.PostgresURLFile,
+			YAML:        "pgConnectionURLFile",
 		},
 		{
 			Name:        "Postgres Auth",
