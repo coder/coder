71f8ac9f7e0ea8c106f3b52447303dfa6188cefe
Zach Kipp <zach.kipp@coder.com>
2025-12-05 16:30:53 -0700

    fix: remove debug logging from boundary audit logs endpoint

--- enterprise/coderd/boundaryauditlog.go
+++ enterprise/coderd/boundaryauditlog.go
@@ -1,7 +1,6 @@
 package coderd
 
 import (
-	"fmt"
 	"net/http"
 
 	agpl "github.com/coder/coder/v2/coderd"
@@ -46,16 +45,7 @@ func (api *API) boundaryAuditLogs(rw http.ResponseWriter, r *http.Request) {
 	// #nosec G115 - Safe conversion as pagination limit is expected to be within int32 range
 	filter.LimitOpt = int32(page.Limit)
 
-	api.Logger.Info(ctx, "boundary audit logs query", "countFilter", countFilter, "db_type", fmt.Sprintf("%T", api.Database))
-	//nolint:gocritic // Debug - bypass dbauthz
-	ctx = dbauthz.AsSystemRestricted(ctx)
-	
-	// Debug: check if we can see other tables
-	users, usersErr := api.Database.GetUsers(ctx, database.GetUsersParams{})
-	api.Logger.Info(ctx, "boundary audit logs DEBUG users count", "count", len(users), "err", usersErr)
-	
 	count, err := api.Database.CountBoundaryAuditLogs(ctx, countFilter)
-	api.Logger.Info(ctx, "boundary audit logs count result", "count", count, "err", err)
 	if dbauthz.IsNotAuthorizedError(err) {
 		httpapi.Forbidden(rw)
 		return
