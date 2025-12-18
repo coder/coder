bb4beb6b8eed236c3e3e313a61b78d7f85def833
Jaayden Halko <jaayden@coder.com>
2025-12-18 20:47:12 +0000

    fix: fix storybook tests

--- site/src/pages/CreateTemplatePage/CreateTemplateForm.stories.tsx
+++ site/src/pages/CreateTemplatePage/CreateTemplateForm.stories.tsx
@@ -106,7 +106,7 @@ export const StarterTemplateWithProvisionerWarning: Story = {
 		showOrganizationPicker: true,
 	},
 	play: async () => {
-		const organizationPicker = screen.getByPlaceholderText("Organization name");
+		const organizationPicker = screen.getByTestId("organization-autocomplete");
 		await userEvent.click(organizationPicker);
 		const org2 = await screen.findByText(MockOrganization2.display_name);
 		await userEvent.click(org2);
@@ -146,7 +146,7 @@ export const StarterTemplatePermissionsCheck: Story = {
 		showOrganizationPicker: true,
 	},
 	play: async () => {
-		const organizationPicker = screen.getByPlaceholderText("Organization name");
+		const organizationPicker = screen.getByTestId("organization-autocomplete");
 		await userEvent.click(organizationPicker);
 	},
 };
