73253df6bfb0055c8363a75546530700b44e385f
Spike Curtis <spike@coder.com>
2025-12-19 12:22:49 +0400

    fix: use separate HTTP clients in scale test load generators (#21288)
    While scale testing, I noticed that our load generators send basically
    all requests to a single Coderd instance.
    
    e.g. 
    
    
    ![image.png](https://app.graphite.com/user-attachments/assets/e259862a-adf1-47e7-a37b-fd14e420058e.png)
    
    
    This is because our scale test commands create all `Runner`s using the
    same codersdk Client, which means they share an underlying HTTP client.
    With HTTP/2 a single TCP session can multiplex many different HTTP
    requests (including websockets). So, it creates a single TCP connection
    to a single coderd, and then sends all the requests down the one TCP
    connections.
    
    This PR modifies the `exp scaletest` load generator commands to create
    an independent HTTP client per `Runner`. This means that each runner
    will create its own TCP connection. This should help spread the load and
    make a more realistic test, because in a real deployment, scaled out
    load will be coming over different TCP connections.

---
+++ scaletest/loadtestutil/client_test.go
@@ -0,0 +1,50 @@
+package loadtestutil_test
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/coder/coder/v2/codersdk"
+	"github.com/coder/coder/v2/scaletest/loadtestutil"
+)
+
+func TestDupClientCopyingHeaders(t *testing.T) {
+	t.Parallel()
+	httpClient := &http.Client{
+		Transport: &codersdk.HeaderTransport{
+			Transport: &codersdk.HeaderTransport{
+				Transport: http.DefaultTransport,
+				Header: map[string][]string{
+					"X-Coder-Test":  {"foo"},
+					"X-Coder-Test3": {"socks"},
+				},
+			},
+			Header: map[string][]string{
+				"X-Coder-Test":  {"bar"},
+				"X-Coder-Test2": {"baz"},
+			},
+		},
+	}
+	serverURL, err := url.Parse("http://coder.example.com")
+	require.NoError(t, err)
+	sdkClient := codersdk.New(serverURL,
+		codersdk.WithSessionToken("test-token"), codersdk.WithHTTPClient(httpClient))
+
+	dup, err := loadtestutil.DupClientCopyingHeaders(sdkClient, map[string][]string{
+		"X-Coder-Test3": {"clocks"},
+		"X-Coder-Test4": {"bears"},
+	})
+	require.NoError(t, err)
+	require.Equal(t, "http://coder.example.com", dup.URL.String())
+	require.Equal(t, "test-token", dup.SessionToken())
+	ht, ok := dup.HTTPClient.Transport.(*codersdk.HeaderTransport)
+	require.True(t, ok)
+	require.Equal(t, "bar", ht.Header.Get("X-Coder-Test"))
+	require.Equal(t, "baz", ht.Header.Get("X-Coder-Test2"))
+	require.Equal(t, "clocks", ht.Header.Get("X-Coder-Test3"))
+	require.Equal(t, "bears", ht.Header.Get("X-Coder-Test4"))
+	require.NotEqual(t, http.DefaultTransport, ht.Transport)
+}
