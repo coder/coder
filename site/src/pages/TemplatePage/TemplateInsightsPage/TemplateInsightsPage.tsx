790fc25fff4a3209af397a70bbf0d61c3c7172b2
Marcin Tojek <tojek.m@gmail.com>
2025-12-23 09:49:01 +0000

    fix(site): defensive access to entitlement features
    When entitlement features like user_limit might not exist in the
    response, using optional chaining prevents runtime errors.
    
    This fixes the License page crash when user_limit feature is not
    present in the entitlements response.
    
    Fixes #14784

--- site/src/pages/TemplatePage/TemplateInsightsPage/TemplateInsightsPage.tsx
+++ site/src/pages/TemplatePage/TemplateInsightsPage/TemplateInsightsPage.tsx
@@ -252,8 +252,8 @@ export const TemplateInsightsPageView: FC<TemplateInsightsPageViewProps> = ({
 					css={{ gridColumn: "span 2" }}
 					interval={interval}
 					userLimit={
-						entitlements?.features.user_limit.enabled
-							? entitlements?.features.user_limit.limit
+						entitlements?.features.user_limit?.enabled
+							? entitlements?.features.user_limit?.limit
 							: undefined
 					}
 					data={templateInsights.data?.interval_reports}
