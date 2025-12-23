c0a686db487a23624b23ff5c8c430970ca97f3da
George Katsitadze <george.katsitadze@gmail.com>
2025-12-23 12:20:50 -0800

    cleaner comment

--- coderd/database/migrations/testdata/fixtures/000403_pre_allow_same_role_name_in_different_orgs.up.sql
+++ coderd/database/migrations/testdata/fixtures/000403_pre_allow_same_role_name_in_different_orgs.up.sql
@@ -1,6 +1,6 @@
 -- Fixture for migration 000404_allow_same_role_name_in_different_orgs.
 -- Inserts a custom role with an all-zero organization_id to ensure the
--- migration doesn't choke on such rows.
+-- migration correctly normalizes such values.
 INSERT INTO custom_roles (name, display_name, organization_id)
 VALUES (
 	'custom-role-zero-org-id',
