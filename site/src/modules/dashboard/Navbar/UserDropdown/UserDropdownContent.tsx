71bab13d7756d87bc5196fb73f81e1dc3453b799
Jake Howell <jake@hwll.me>
2025-12-23 03:11:38 +0000

    fix: restore \uu00a9 in `<UserDropdownContent />`

--- site/src/modules/dashboard/Navbar/UserDropdown/UserDropdownContent.tsx
+++ site/src/modules/dashboard/Navbar/UserDropdown/UserDropdownContent.tsx
@@ -28,7 +28,7 @@ import { SupportIcon } from "../SupportIcon";
 export const Language = {
 	accountLabel: "Account",
 	signOutLabel: "Sign Out",
-	copyrightText: `&copy; ${new Date().getFullYear()} Coder Technologies, Inc.`,
+	copyrightText: `\u00a9 ${new Date().getFullYear()} Coder Technologies, Inc.`,
 };
 
 interface UserDropdownContentProps {
