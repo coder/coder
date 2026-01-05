e10fceb23c25c3b43f92191bfb4ddbd9cc5a85c0
George K <george.katsitadze@gmail.com>
2026-01-05 07:43:08 -0800

    fix(coderd/database): allow same custom role name for different orgs (#21312)
    Previously the `idx_custom_roles_name_lower` index prevented that.
    
    A check constraint was also added to ensure the `organization_id` column cannot be set to the all-zero UUID.

--- coderd/database/check_constraint.go
+++ coderd/database/check_constraint.go
@@ -7,6 +7,7 @@ type CheckConstraint string
 // CheckConstraint enums.
 const (
 	CheckAPIKeysAllowListNotEmpty                CheckConstraint = "api_keys_allow_list_not_empty"                // api_keys
+	CheckOrganizationIDNotZero                   CheckConstraint = "organization_id_not_zero"                     // custom_roles
 	CheckOneTimePasscodeSet                      CheckConstraint = "one_time_passcode_set"                        // users
 	CheckUsersUsernameMinLength                  CheckConstraint = "users_username_min_length"                    // users
 	CheckMaxProvisionerLogsLength                CheckConstraint = "max_provisioner_logs_length"                  // provisioner_jobs
