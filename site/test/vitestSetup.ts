269d5c62673f6857aafbef889deb4a189eed8c6e
ケイラ <kayla@tree.camp>
2026-01-05 20:10:52 +0000

    typo

--- site/test/vitestSetup.ts
+++ site/test/vitestSetup.ts
@@ -4,7 +4,7 @@ import { Blob as NativeBlob } from "node:buffer";
 // years. MDN categorizes this API as baseline[2].
 // [1]: https://github.com/jsdom/jsdom/issues/2555
 // [2]: https://developer.mozilla.org/en-US/docs/Web/API/Blob/arrayBuffer
-// @ts-expect-error - Minor type incompatibilites due to TypeScript's
+// @ts-expect-error - Minor type incompatibilities due to TypeScript's
 // introduction of the `ArrayBufferLife` type and the related generic parameters
 // changes.
 globalThis.Blob = NativeBlob;
