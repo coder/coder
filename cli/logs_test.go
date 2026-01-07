e8ec69422c3ba345e1cf1f2f4a61cc6fe03841f8
Cian Johnston <cian@coder.com>
2026-01-07 12:20:50 +0000

    add tests

--- cli/logs_test.go
+++ cli/logs_test.go
@@ -1,17 +1,16 @@
 package cli_test
 
 import (
+	"fmt"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/coder/coder/v2/cli/clitest"
 	"github.com/coder/coder/v2/coderd/coderdtest"
 	"github.com/coder/coder/v2/coderd/database"
 	"github.com/coder/coder/v2/coderd/database/dbfake"
-	"github.com/coder/coder/v2/provisionersdk/proto"
+	"github.com/coder/coder/v2/coderd/database/dbgen"
 	"github.com/coder/coder/v2/testutil"
-	"github.com/google/uuid"
 
 	"github.com/stretchr/testify/require"
 )
@@ -23,32 +22,22 @@ func TestLogsCmd(t *testing.T) {
 	owner := coderdtest.CreateFirstUser(t, client)
 	memberClient, memberUser := coderdtest.CreateAnotherUser(t, client, owner.OrganizationID)
 
-	agentID := uuid.New()
-	ws := dbfake.WorkspaceBuild(t, db, database.WorkspaceTable{
+	wb := dbfake.WorkspaceBuild(t, db, database.WorkspaceTable{
 		OwnerID:        memberUser.ID,
 		OrganizationID: owner.OrganizationID,
-	}).WithAgent(func(a []*proto.Agent) []*proto.Agent {
-		a[0].Id = agentID.String()
-		return a
-	}).Do()
-	setupCtx := testutil.Context(t, testutil.WaitShort)
-	_, err := db.InsertProvisionerJobLogs(setupCtx, database.InsertProvisionerJobLogsParams{
-		JobID:     ws.Build.JobID,
-		CreatedAt: []time.Time{ws.Build.UpdatedAt},
-		Stage:     []string{"Test"},
-		Level:     []database.LogLevel{database.LogLevelInfo},
-		Source:    []database.LogSource{database.LogSourceProvisioner},
-		Output:    []string{"test provisioner log"},
+	}).WithAgent().Do()
+	jobLog := dbgen.ProvisionerJobLog(t, db, database.ProvisionerJobLog{
+		JobID:  wb.Build.JobID,
+		Output: "test provisioner log",
 	})
-	require.NoError(t, err, "insert provisioner job logs")
-	_, err = db.InsertWorkspaceAgentLogs(setupCtx, database.InsertWorkspaceAgentLogsParams{
-		AgentID:     agentID,
-		CreatedAt:   ws.Build.UpdatedAt,
-		Level:       []database.LogLevel{database.LogLevelInfo},
-		LogSourceID: uuid.New(),
-		Output:      []string{"test agent log"},
-	})
-	require.NoError(t, err, "insert workspace agent logs")
+	var agentlogs []database.WorkspaceAgentLog
+	for _, agt := range wb.Agents {
+		agentlog := dbgen.WorkspaceAgentLog(t, db, database.WorkspaceAgentLog{
+			AgentID: agt.ID,
+			Output:  "test agent log for " + agt.ID.String(),
+		})
+		agentlogs = append(agentlogs, agentlog)
+	}
 
 	t.Run("workspace not found", func(t *testing.T) {
 		t.Parallel()
@@ -59,21 +48,51 @@ func TestLogsCmd(t *testing.T) {
 		var stdout strings.Builder
 		inv.Stdout = &stdout
 		err := inv.WithContext(ctx).Run()
-		require.NoError(t, err)
-		require.Contains(t, stdout.String(), "failed to get workspace")
+		require.Error(t, err)
 	})
 
-	t.Run("latest build logs", func(t *testing.T) {
+	// Note: not testing with --follow as it is inherently racy.
+	t.Run("current build", func(t *testing.T) {
 		t.Parallel()
 
-		inv, root := clitest.New(t, "logs", ws.Workspace.Name)
+		inv, root := clitest.New(t, "logs", wb.Workspace.Name)
 		clitest.SetupConfig(t, memberClient, root)
 		ctx := testutil.Context(t, testutil.WaitShort)
 		var stdout strings.Builder
 		inv.Stdout = &stdout
 		err := inv.WithContext(ctx).Run()
 		require.NoError(t, err)
-		require.Contains(t, stdout.String(), "test provisioner log")
-		require.Contains(t, stdout.String(), "test agent log")
+		require.Contains(t, stdout.String(), jobLog.Output)
+		for _, log := range agentlogs {
+			require.Contains(t, stdout.String(), log.Output)
+		}
+	})
+
+	t.Run("specific build", func(t *testing.T) {
+		t.Parallel()
+
+		inv, root := clitest.New(t, "logs", wb.Workspace.Name, "-n", fmt.Sprintf("%d", wb.Build.BuildNumber))
+		clitest.SetupConfig(t, memberClient, root)
+		ctx := testutil.Context(t, testutil.WaitShort)
+		var stdout strings.Builder
+		inv.Stdout = &stdout
+		err := inv.WithContext(ctx).Run()
+		require.NoError(t, err)
+		require.Contains(t, stdout.String(), jobLog.Output)
+		for _, log := range agentlogs {
+			require.Contains(t, stdout.String(), log.Output)
+		}
+	})
+
+	t.Run("build out of range", func(t *testing.T) {
+		t.Parallel()
+
+		inv, root := clitest.New(t, "logs", wb.Workspace.Name, "-n", "-9999")
+		clitest.SetupConfig(t, memberClient, root)
+		ctx := testutil.Context(t, testutil.WaitShort)
+		var stdout strings.Builder
+		inv.Stdout = &stdout
+		err := inv.WithContext(ctx).Run()
+		require.ErrorContains(t, err, "invalid build number offset")
 	})
 }
