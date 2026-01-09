16bbcfe838823eaa67b8c17160e7c2112d612340
George Katsitadze <george.katsitadze@gmail.com>
2026-01-09 11:08:11 -0800

    bump migration numbers to keep up with main

--- coderd/database/migrations/000405_add_system_role_support.down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE custom_roles DROP COLUMN IF EXISTS member_permissions;
-
-ALTER TABLE custom_roles DROP COLUMN IF EXISTS is_system;
--- coderd/database/migrations/000406_add_workspace_sharing_disabled.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE organizations
-    ADD COLUMN workspace_sharing_disabled boolean NOT NULL DEFAULT false;
