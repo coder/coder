6043d55eea7fa17453dd6849c6f70d2a43dddbb6
Kacper Sawicki <kacper@ksawicki.pl>
2026-01-30 14:53:42 +0000

    fix: add aibridge roles to SDK and test expectations

--- codersdk/rbacroles.go
+++ codersdk/rbacroles.go
@@ -2,11 +2,13 @@ package codersdk
 
 // Ideally this roles would be generated from the rbac/roles.go package.
 const (
-	RoleOwner         string = "owner"
-	RoleMember        string = "member"
-	RoleTemplateAdmin string = "template-admin"
-	RoleUserAdmin     string = "user-admin"
-	RoleAuditor       string = "auditor"
+	RoleOwner           string = "owner"
+	RoleMember          string = "member"
+	RoleTemplateAdmin   string = "template-admin"
+	RoleUserAdmin       string = "user-admin"
+	RoleAuditor         string = "auditor"
+	RoleAibridgeUser    string = "aibridge-user"
+	RoleAibridgeAuditor string = "aibridge-auditor"
 
 	RoleOrganizationAdmin                string = "organization-admin"
 	RoleOrganizationMember               string = "organization-member"
