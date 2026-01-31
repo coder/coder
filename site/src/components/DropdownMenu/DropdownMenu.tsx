83998e9599c81720b7be3abd33c96efad07c7453
Jake Howell <jake@hwll.me>
2026-01-31 03:16:35 +0000

    chore: remove unused import

--- site/src/components/DropdownMenu/DropdownMenu.tsx
+++ site/src/components/DropdownMenu/DropdownMenu.tsx
@@ -7,7 +7,7 @@
  */
 
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
-import { Check, ChevronRight, Circle } from "lucide-react";
+import { Check, ChevronRight } from "lucide-react";
 import {
 	type ComponentPropsWithoutRef,
 	type ElementRef,
