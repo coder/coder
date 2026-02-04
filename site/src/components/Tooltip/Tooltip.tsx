2aafc7d35bd6ad2f3844b7988e87ceda1335f8ad
Andrew Aquino <dawneraq@gmail.com>
2026-02-04 00:37:00 +0000

    feat: add medium shadow to all tooltips

--- site/src/components/Tooltip/Tooltip.tsx
+++ site/src/components/Tooltip/Tooltip.tsx
@@ -31,7 +31,7 @@ export const TooltipContent = React.forwardRef<
 			ref={ref}
 			sideOffset={sideOffset}
 			className={cn(
-				"z-50 overflow-hidden rounded-md bg-surface-primary px-3 py-2 text-xs font-medium text-content-secondary",
+				"z-50 overflow-hidden rounded-md bg-surface-primary px-3 py-2 text-xs font-medium text-content-secondary shadow-md",
 				"border border-solid border-border animate-in fade-in-0 zoom-in-95",
 				"data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
 				"data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2",
