4d8ea43e11145568dc8ce4585ba9eaa4e4d10725
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2026-02-09 14:07:12 -0500

    docs(cli): fix help text for --oidc-icon-url
    Issue: Grammar - redundant phrasing
    
    Changed 'URL pointing to the icon' to 'URL of the icon'. The phrase 'pointing to' is redundant since a URL inherently points to a resource.

--- codersdk/deployment.go
+++ codersdk/deployment.go
@@ -2339,7 +2339,7 @@ func (c *DeploymentValues) Options() serpent.OptionSet {
 		},
 		{
 			Name:        "OpenID connect icon URL",
-			Description: "URL pointing to the icon to use on the OpenID Connect login button.",
+			Description: "URL of the icon to use on the OpenID Connect login button.",
 			Flag:        "oidc-icon-url",
 			Env:         "CODER_OIDC_ICON_URL",
 			Value:       &c.OIDC.IconURL,
