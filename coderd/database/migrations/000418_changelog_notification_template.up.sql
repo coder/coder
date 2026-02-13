c4cee8d0022721feb3a7aa642e51c53f32f8f9ae
Jaayden Halko <jaayden@coder.com>
2026-02-13 07:16:33 +0000

    feat(notifications): add changelog notification template and icon

---
+++ coderd/database/migrations/000418_changelog_notification_template.up.sql
@@ -0,0 +1,17 @@
+INSERT INTO notification_templates (
+    id,
+    name,
+    title_template,
+    body_template,
+    "group",
+    actions,
+    method
+) VALUES (
+    'b02d53fd-477d-4a65-8d42-1b7e4b38f8c3',
+    'Changelog',
+    E'{{.Labels.version}}',
+    E'{{.Labels.summary}}',
+    'Changelog',
+    '[{"label":"View changelog","url":"/changelog/{{.Labels.version}}"}]'::jsonb,
+    'inbox'::notification_method
+);
