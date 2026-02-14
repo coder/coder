0142b44b922b8c5a0793e4b7faa04a8690e0ad7c
Jake Howell <jake@hwll.me>
2026-02-14 11:26:39 +0000

    feat: de-mui `<Layout />`

--- site/src/pages/UserSettingsPage/Layout.tsx
+++ site/src/pages/UserSettingsPage/Layout.tsx
@@ -1,6 +1,5 @@
 import { Loader } from "components/Loader/Loader";
 import { Margins } from "components/Margins/Margins";
-import { Stack } from "components/Stack/Stack";
 import { useAuthenticated } from "hooks";
 import { type FC, Suspense } from "react";
 import { Outlet } from "react-router";
@@ -15,14 +14,14 @@ const Layout: FC = () => {
 			<title>{pageTitle("Settings")}</title>
 
 			<Margins>
-				<Stack css={{ padding: "48px 0" }} direction="row" spacing={6}>
+				<div className="flex flex-row gap-12 py-12">
 					<Sidebar user={me} />
 					<Suspense fallback={<Loader />}>
-						<main css={{ maxWidth: 800, width: "100%" }}>
+						<main className="w-full max-w-full">
 							<Outlet />
 						</main>
 					</Suspense>
-				</Stack>
+				</div>
 			</Margins>
 		</>
 	);
