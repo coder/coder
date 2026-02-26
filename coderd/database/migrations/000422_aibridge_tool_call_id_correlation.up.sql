571c84a47088729d19586ba621ab47d0eddf9cd0
Danny Kopping <danny@coder.com>
2026-02-26 16:20:30 +0200

    chore: merge migrations
    Signed-off-by: Danny Kopping <danny@coder.com>

--- coderd/database/migrations/000422_aibridge_tool_call_id_correlation.up.sql
+++ coderd/database/migrations/000422_aibridge_tool_call_id_correlation.up.sql
@@ -4,8 +4,11 @@ ADD COLUMN provider_tool_call_id text NULL; -- nullable to allow existing data t
 CREATE INDEX idx_aibridge_tool_usages_provider_tool_call_id ON aibridge_tool_usages (provider_tool_call_id);
 
 ALTER TABLE aibridge_interceptions
-ADD COLUMN thread_parent_id UUID NULL;
+ADD COLUMN thread_parent_id UUID NULL,
+ADD COLUMN thread_root_id UUID NULL;
 
 COMMENT ON COLUMN aibridge_interceptions.thread_parent_id IS 'The interception which directly caused this interception to occur, usually through an agentic loop or threaded conversation.';
+COMMENT ON COLUMN aibridge_interceptions.thread_root_id IS 'The root interception of the thread that this interception belongs to.';
 
 CREATE INDEX idx_aibridge_interceptions_thread_parent_id ON aibridge_interceptions (thread_parent_id);
+CREATE INDEX idx_aibridge_interceptions_thread_root_id ON aibridge_interceptions (thread_root_id);
--- coderd/database/migrations/000423_aibridge_thread_root_id.up.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE aibridge_interceptions
-ADD COLUMN thread_root_id UUID NULL;
-
-COMMENT ON COLUMN aibridge_interceptions.thread_root_id IS 'The root interception of the thread that this interception belongs to.';
-
-CREATE INDEX idx_aibridge_interceptions_thread_root_id ON aibridge_interceptions (thread_root_id);
