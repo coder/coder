933480e196bbacda433d0c225424169653667a4e
Jake Howell <jake@hwll.me>
2026-03-03 06:12:07 +0000

    fix: resolve checkbox classes

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -32,6 +32,7 @@ module.exports = {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
 				sm: "calc(var(--radius) - 4px)",
+				xs: "calc(var(--radius) - 6px)",
 			},
 			colors: {
 				content: {
