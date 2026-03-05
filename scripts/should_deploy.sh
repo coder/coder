9c91f472b9e9348c671a974d3503fd508f9af39d
Cian Johnston <cian@coder.com>
2026-03-05 09:47:15 +0000

    ci: remove temporary deploy override (#22389)
    Merge after Tuesday March 3rd.
    
    Co-authored-by: Atif Ali <atif@coder.com>

--- scripts/should_deploy.sh
+++ scripts/should_deploy.sh
@@ -17,20 +17,6 @@ deploy_branch=main
 # branch names.
 branch_name=$(git branch --show-current)
 
-# --- BEGIN TEMPORARY SHORT-CIRCUIT ---
-# Forces deployment of main. Remove after 2026-03-04T12:00Z.
-if [[ "$branch_name" == "main" ]]; then
-	log "TEMPORARY SHORT-CIRCUIT: deploying main"
-	log "VERDICT: DEPLOY"
-	echo "DEPLOY"
-	exit 0
-else
-	log "VERDICT: DO NOT DEPLOY"
-	echo "NOOP"
-	exit 0
-fi
-# --- END TEMPORARY SHORT-CIRCUIT ---
-
 if [[ "$branch_name" != "main" && ! "$branch_name" =~ ^release/[0-9]+\.[0-9]+$ ]]; then
 	error "Current branch '$branch_name' is not a supported branch name for dogfood, must be 'main' or 'release/x.y'"
 fi
