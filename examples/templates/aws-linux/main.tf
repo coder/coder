5bb43474026b6d76f7561d207d9ffef1c9fb1d8a
Jan Zenkner <neunkasulle@users.noreply.github.com>
2026-03-10 09:35:14 -0700

    fix: update aws linux to ubuntu 24.04
    The currently configured ubuntu 20.04 has an older version of curl , which does not contain the `--retry-on-errors` that is used in the  AgentAPI installation in the claude-code module.
    
    Tested template build, VM startup and JetBrains Gateway connection via self hosted coder instance

--- examples/templates/aws-linux/main.tf
+++ examples/templates/aws-linux/main.tf
@@ -150,7 +150,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
