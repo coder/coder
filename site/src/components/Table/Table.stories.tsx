147ba693166a06c33ebc57200cb6709d56374c8e
Jaayden Halko <jaayden@coder.com>
2026-03-12 17:54:10 +0000

    chore: cleanup

--- site/src/components/Table/Table.stories.tsx
+++ site/src/components/Table/Table.stories.tsx
@@ -54,34 +54,34 @@ const invoices = [
 	},
 ];
 
-const defaultTableChildren = (
-	<>
-		<TableHeader>
-			<TableRow>
-				<TableHead className="w-[100px]">Invoice</TableHead>
-				<TableHead>Status</TableHead>
-				<TableHead>Method</TableHead>
-				<TableHead className="text-right">Amount</TableHead>
-			</TableRow>
-		</TableHeader>
-		<TableBody>
-			{invoices.map((invoice) => (
-				<TableRow key={invoice.invoice}>
-					<TableCell className="font-medium">{invoice.invoice}</TableCell>
-					<TableCell>{invoice.paymentStatus}</TableCell>
-					<TableCell>{invoice.paymentMethod}</TableCell>
-					<TableCell className="text-right">{invoice.totalAmount}</TableCell>
-				</TableRow>
-			))}
-		</TableBody>
-	</>
-);
-
 const meta: Meta<typeof Table> = {
 	title: "components/Table",
 	component: Table,
 	args: {
-		children: defaultTableChildren,
+		children: (
+			<>
+				<TableHeader>
+					<TableRow>
+						<TableHead className="w-[100px]">Invoice</TableHead>
+						<TableHead>Status</TableHead>
+						<TableHead>Method</TableHead>
+						<TableHead className="text-right">Amount</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{invoices.map((invoice) => (
+						<TableRow key={invoice.invoice}>
+							<TableCell className="font-medium">{invoice.invoice}</TableCell>
+							<TableCell>{invoice.paymentStatus}</TableCell>
+							<TableCell>{invoice.paymentMethod}</TableCell>
+							<TableCell className="text-right">
+								{invoice.totalAmount}
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</>
+		),
 	},
 };
 
