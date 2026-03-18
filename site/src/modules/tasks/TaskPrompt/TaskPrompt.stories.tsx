4f566f92b524a8eb5a339b062cf13e26eca07365
Danielle Maywood <danielle@themaywoods.com>
2026-03-18 11:16:30 +0000

    fix(site): use ExternalImage for preset icons in task prompt (#23206)

--- site/src/modules/tasks/TaskPrompt/TaskPrompt.stories.tsx
+++ site/src/modules/tasks/TaskPrompt/TaskPrompt.stories.tsx
@@ -13,6 +13,7 @@ import {
 import { withAuthProvider, withToaster } from "testHelpers/storybook";
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { API } from "api/api";
+import { templateVersionPresetsKey } from "api/queries/templates";
 import type { Task } from "api/typesGenerated";
 import { expect, spyOn, userEvent, waitFor, within } from "storybook/test";
 import type TasksPage from "../../../pages/TasksPage/TasksPage";
@@ -495,6 +496,50 @@ export const PresetSelectorFocused: Story = {
 		const canvas = within(canvasElement);
 		const presetSelect = await canvas.findByLabelText(/preset/i);
 		presetSelect.focus();
+	},
+};
+
+// Regression test for https://github.com/coder/coder/issues/22245
+// Dark monochrome icons (like GitHub or Tasks) were invisible on dark
+// backgrounds because icons used a plain <img> instead of
+// ExternalImage, which applies theme-aware CSS filters.
+export const IconContrast: Story = {
+	args: {
+		templates: [
+			{
+				...MockTemplate,
+				id: "github-template",
+				name: "github-template",
+				display_name: "GitHub",
+				icon: "/icon/github.svg",
+				active_version_id: MockTemplateVersion.id,
+			},
+			{
+				...MockTemplate,
+				id: "tasks-template",
+				name: "tasks-template",
+				display_name: "Tasks",
+				icon: "/icon/tasks.svg",
+				active_version_id: MockTemplateVersion.id,
+			},
+		],
+	},
+	parameters: {
+		queries: [
+			{
+				key: templateVersionPresetsKey(MockTemplateVersion.id),
+				data: [
+					{
+						...MockPresets[0],
+						Icon: "/icon/github.svg",
+					},
+					{
+						...MockPresets[1],
+						Icon: "/icon/tasks.svg",
+					},
+				],
+			},
+		],
 	},
 };
 
