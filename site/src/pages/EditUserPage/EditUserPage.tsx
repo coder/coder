47d0107a2e46b5f6bc3fee10a1b76b6d99cec5ea
Kayla はな <kayla@tree.camp>
2026-03-18 22:48:17 +0000

    clean starting point

---
+++ site/src/pages/EditUserPage/EditUserPage.tsx
@@ -0,0 +1,66 @@
+import { getErrorDetail, getErrorMessage } from "api/errors";
+import { authMethods, createUser } from "api/queries/users";
+import { Margins } from "components/Margins/Margins";
+import { useDashboard } from "modules/dashboard/useDashboard";
+import type { FC } from "react";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
+import { pageTitle } from "utils/page";
+import { EditUserForm } from "./EditUserForm";
+
+const EditUserPage: FC = () => {
+	const navigate = useNavigate();
+	const queryClient = useQueryClient();
+	const createUserMutation = useMutation(createUser(queryClient));
+	const authMethodsQuery = useQuery(authMethods());
+	const { showOrganizations } = useDashboard();
+
+	return (
+		<Margins>
+			<title>{pageTitle("Create User")}</title>
+
+			<EditUserForm
+				error={createUserMutation.error}
+				isLoading={createUserMutation.isPending}
+				onSubmit={async (user) => {
+					const mutation = createUserMutation.mutateAsync(
+						{
+							username: user.username,
+							name: user.name,
+							email: user.email,
+							organization_ids: [user.organization],
+							login_type: user.login_type,
+							password: user.password,
+							user_status: null,
+							service_account: user.service_account,
+						},
+						{
+							onSuccess: () => {
+								navigate("..", { relative: "path" });
+							},
+						},
+					);
+					toast.promise(mutation, {
+						loading: `Creating user "${user.username}"...`,
+						success: `User "${user.username}" created successfully.`,
+						error: (e) => ({
+							message: getErrorMessage(
+								e,
+								`Failed to create user "${user.username}".`,
+							),
+							description: getErrorDetail(e),
+						}),
+					});
+				}}
+				onCancel={() => {
+					navigate("..", { relative: "path" });
+				}}
+				authMethods={authMethodsQuery.data}
+				showOrganizations={showOrganizations}
+			/>
+		</Margins>
+	);
+};
+
+export default EditUserPage;
