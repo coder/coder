aa0a662e1071de719727291dbb642d2642f09864
Asher <ash@coder.com>
2026-03-24 13:44:45 -0800

    chore: simplify permissions check
    Looks like the hook already returns permissions.
    
    But the main reason I want this is because I want to add stories and it
    seems easier to mock the workspace permission endpoint (this is what the
    other workspace settings pages are doing so I can just copy that; I
    tried mocking /authcheck but despite using the right key it kept
    404ing).

--- site/src/pages/WorkspaceSettingsPage/WorkspaceParametersPage/WorkspaceParametersPageExperimental.tsx
+++ site/src/pages/WorkspaceSettingsPage/WorkspaceParametersPage/WorkspaceParametersPageExperimental.tsx
@@ -1,6 +1,5 @@
 import { API } from "api/api";
 import { DetailedError } from "api/errors";
-import { checkAuthorization } from "api/queries/authCheck";
 import type {
 	DynamicParametersRequest,
 	DynamicParametersResponse,
@@ -26,15 +25,11 @@ import {
 	TooltipProvider,
 	TooltipTrigger,
 } from "#/components/Tooltip/Tooltip";
-import {
-	type WorkspacePermissions,
-	workspaceChecks,
-} from "../../../modules/workspaces/permissions";
 import { useWorkspaceSettings } from "../useWorkspaceSettings";
 import { WorkspaceParametersPageViewExperimental } from "./WorkspaceParametersPageViewExperimental";
 
 const WorkspaceParametersPageExperimental: FC = () => {
-	const { workspace } = useWorkspaceSettings();
+	const { permissions, workspace } = useWorkspaceSettings();
 	const navigate = useNavigate();
 	const [searchParams] = useSearchParams();
 	const templateVersionId = searchParams.get("templateVersionId") ?? undefined;
@@ -190,12 +185,6 @@ const WorkspaceParametersPageExperimental: FC = () => {
 		},
 	});
 
-	const checks = workspace ? workspaceChecks(workspace) : {};
-	const permissionsQuery = useQuery({
-		...checkAuthorization({ checks }),
-		enabled: workspace !== undefined,
-	});
-	const permissions = permissionsQuery.data as WorkspacePermissions | undefined;
 	const canChangeVersions = Boolean(permissions?.updateWorkspaceVersion);
 
 	const handleSubmit = (values: {
