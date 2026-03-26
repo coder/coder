81fe7543b457750562ab567c0dc79d7acfcb86db
Cian Johnston <cian@coder.com>
2026-03-26 14:53:47 +0000

    chore: set tls.VersionTLS12 MinVersion in cli/server.go to address gosec warning (#23646)
    I was investigating `//nolint` comments and this one popped up.
    It raised my eyebrows enough to warrant its own PR.

--- cli/server.go
+++ cli/server.go
@@ -2327,7 +2327,8 @@ func ConfigureHTTPClient(ctx context.Context, clientCertFile, clientKeyFile stri
 			return ctx, nil, err
 		}
 
-		tlsClientConfig := &tls.Config{ //nolint:gosec
+		tlsClientConfig := &tls.Config{
+			MinVersion:   tls.VersionTLS12,
 			Certificates: certificates,
 			NextProtos:   []string{"h2", "http/1.1"},
 		}
