30f95a11ed18d936b4d4eec9b8c15db8949a7458
Kayla はな <kayla@tree.camp>
2026-04-09 00:09:02 +0000

    Update DividerWithText.tsx

--- site/src/pages/DeploymentSettingsPage/LicensesSettingsPage/DividerWithText.tsx
+++ site/src/pages/DeploymentSettingsPage/LicensesSettingsPage/DividerWithText.tsx
@@ -3,11 +3,11 @@ import type { FC, PropsWithChildren } from "react";
 export const DividerWithText: FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<div className="flex items-center">
-			<div className="border-b-2 border-border w-full" />
+			<div className="border-b-2 border-solid border-border w-full" />
 			<span className="py-1 px-4 text-xl text-content-secondary">
 				{children}
 			</span>
-			<div className="border-b-2 border-border w-full" />
+			<div className="border-b-2 border-solid border-border w-full" />
 		</div>
 	);
 };
