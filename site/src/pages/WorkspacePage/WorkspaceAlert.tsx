9948a82ede66f6c0e70ee260e118f5e6a96a46d0
Jake Howell <jake@hwll.me>
2026-04-10 07:42:16 +0000

    feat: update `<WorkspaceAlert />` to use a button

--- site/src/pages/WorkspacePage/WorkspaceAlert.tsx
+++ site/src/pages/WorkspacePage/WorkspaceAlert.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react";
 import { Alert, AlertDescription, AlertTitle } from "#/components/Alert/Alert";
-import { Link } from "#/components/Link/Link";
+import { Button } from "#/components/Button/Button";
 
 interface WorkspaceAlertProps {
 	title: string;
@@ -21,14 +21,14 @@ export const WorkspaceAlert: FC<WorkspaceAlertProps> = ({
 		<Alert severity={severity} prominent={prominent}>
 			<AlertTitle>{title}</AlertTitle>
 			<AlertDescription>
-				<p>{detail}</p>
-				<p>
-					{troubleshootingURL && (
-						<Link href={troubleshootingURL} target="_blank">
+				<div className="mb-2">{detail}</div>
+				{troubleshootingURL && (
+					<Button asChild>
+						<a href={troubleshootingURL} target="_blank" rel="noopener">
 							View docs to troubleshoot
-						</Link>
-					)}
-				</p>
+						</a>
+					</Button>
+				)}
 			</AlertDescription>
 		</Alert>
 	);
