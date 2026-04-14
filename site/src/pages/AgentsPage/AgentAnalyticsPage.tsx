ae846c6b51590ebf39b1281ebf656172a8aad5b7
Bartek Gatz <bartek@coder.com>
2026-04-14 12:23:19 +0000

    fix: add DPM banner to Agent Analytics page

--- site/src/pages/AgentsPage/AgentAnalyticsPage.tsx
+++ site/src/pages/AgentsPage/AgentAnalyticsPage.tsx
@@ -2,6 +2,8 @@ import dayjs, { type Dayjs } from "dayjs";
 import { type FC, useState } from "react";
 import { useQuery } from "react-query";
 import { chatCostSummary } from "#/api/queries/chats";
+import { deploymentConfig } from "#/api/queries/deployment";
+import { DataProtectionBanner } from "#/components/DataProtectionBanner";
 import { ScrollArea } from "#/components/ScrollArea/ScrollArea";
 import { useAuthContext } from "#/contexts/auth/AuthProvider";
 import { AgentAnalyticsPageView } from "./AgentAnalyticsPageView";
@@ -27,6 +29,10 @@ const AgentAnalyticsPage: FC<AgentAnalyticsPageProps> = ({ now }) => {
 	const [anchor] = useState<Dayjs>(() => dayjs());
 	const dateRange = createDateRange(now ?? anchor);
 
+	const configQuery = useQuery(deploymentConfig());
+	const dataProtectionEnabled =
+		configQuery.data?.config?.data_protection?.enabled;
+
 	const summaryQuery = useQuery({
 		...chatCostSummary(user?.id ?? "me", {
 			start_date: dateRange.startDate,
@@ -38,6 +44,7 @@ const AgentAnalyticsPage: FC<AgentAnalyticsPageProps> = ({ now }) => {
 	return (
 		<ScrollArea className="min-h-0 flex-1" viewportClassName="[&>div]:!block">
 			<AgentPageHeader mobileBack={{ to: "/agents", label: "Agents" }} />
+			<DataProtectionBanner dataProtectionEnabled={dataProtectionEnabled} />
 			<AgentAnalyticsPageView
 				summary={summaryQuery.data}
 				isLoading={summaryQuery.isLoading}
