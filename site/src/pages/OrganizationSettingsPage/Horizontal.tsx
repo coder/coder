fa51cfb54413ae12d1942925b56b5b40ebcd0632
Jake Howell <jake@hwll.me>
2026-04-30 07:04:45 +0000

    fix: resolve gaps

--- site/src/pages/OrganizationSettingsPage/Horizontal.tsx
+++ site/src/pages/OrganizationSettingsPage/Horizontal.tsx
@@ -19,7 +19,7 @@ export const HorizontalSection: FC<HorizontalSectionProps> = ({
 	...attrs
 }) => {
 	return (
-		<section className="flex flex-col gap-4 lg:flex-row lg:gap-30" {...attrs}>
+		<section className="flex flex-col gap-4 lg:flex-row lg:gap-32" {...attrs}>
 			<div className="w-full shrink-0 lg:sticky lg:top-6 lg:max-w-[312px]">
 				<h2 className="m-0 mb-2 flex flex-row items-center gap-3 text-xl font-normal text-content-primary">
 					{title}
