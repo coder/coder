e0aa553b0af9e760033fe265cd3dfd14154f9f58
Cian Johnston <cian@coder.com>
2026-05-06 20:47:07 +0100

    fix(coderd/httpapi): CloseRead on test conns to ensure pings pong

--- coderd/httpapi/websocket_internal_test.go
+++ coderd/httpapi/websocket_internal_test.go
@@ -38,12 +38,14 @@ func websocketPair(ctx context.Context, t *testing.T) *websocket.Conn {
 	//nolint:bodyclose
 	clientConn, _, err := websocket.Dial(ctx, srv.URL, nil)
 	require.NoError(t, err)
+	_ = clientConn.CloseRead(ctx) // needed for pings to pong
 	t.Cleanup(func() {
 		_ = clientConn.Close(websocket.StatusNormalClosure, "test cleanup")
 	})
 
 	select {
 	case sc := <-serverConnCh:
+		_ = sc.CloseRead(ctx) // needed for pongs to ping
 		return sc
 	case <-ctx.Done():
 		t.Fatal("timed out waiting for server websocket accept")
