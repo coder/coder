11496accd999a0f2789e1555f5589744adc825f0
Cian Johnston <cian@coder.com>
2026-05-13 17:24:24 +0000

    remove dead mockEventSenderWrite indirection

--- coderd/httpapi/httpapi_test.go
+++ coderd/httpapi/httpapi_test.go
@@ -193,12 +193,6 @@ func (m mockOneWaySocketWriter) WriteHeader(code int) {
 	m.serverRecorder.WriteHeader(code)
 }
 
-type mockEventSenderWrite func(b []byte) (int, error)
-
-func (w mockEventSenderWrite) Write(b []byte) (int, error) {
-	return w(b)
-}
-
 func TestOneWayWebSocketEventSender(t *testing.T) {
 	t.Parallel()
 
@@ -220,10 +214,6 @@ func TestOneWayWebSocketEventSender(t *testing.T) {
 		mockServer, mockClient := net.Pipe()
 		recorder := httptest.NewRecorder()
 
-		var write mockEventSenderWrite = func(b []byte) (int, error) {
-			return mockServer.Write(b)
-		}
-
 		return mockOneWaySocketWriter{
 			testContext:    t,
 			serverConn:     mockServer,
@@ -231,7 +221,7 @@ func TestOneWayWebSocketEventSender(t *testing.T) {
 			serverRecorder: recorder,
 			serverReadWriter: bufio.NewReadWriter(
 				bufio.NewReader(mockServer),
-				bufio.NewWriter(write),
+				bufio.NewWriter(mockServer),
 			),
 		}
 	}
