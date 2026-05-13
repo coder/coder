c922bf149cd8fae582eab570ea867ccce99d1b3c
Danielle Maywood <danielle@themaywoods.com>
2026-05-13 22:30:21 +0000

    test(site): trigger visual report artifact demo

--- site/src/components/PasswordField/PasswordField.tsx
+++ site/src/components/PasswordField/PasswordField.tsx
@@ -37,7 +37,7 @@ export const PasswordField: FC<PasswordFieldProps> = ({
 		: field.helperText;
 
 	return (
-		<div className="flex flex-col items-start gap-4">
+		<div className="flex flex-col items-start gap-6">
 			<Label htmlFor={field.id}>{label}</Label>
 			<Input
 				id={field.id}
