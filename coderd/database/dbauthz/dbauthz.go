b884f90ab5c1a36842a36c1a6c5c8fce534950bb
Steven Masley <Emyrk@users.noreply.github.com>
2026-05-20 17:40:50 -0500

    fix: drop N+1 db query on template ACL available (#25465)
    Fixes
    [PLAT-149](https://linear.app/codercom/issue/PLAT-149/template-permissions-search-is-extremely-slow-with-many-groups).
    
    `/acl/available` ran a db query per group. A deployment with >5,000
    groups made this route extremely slow.
    
    (cherry picked from commit 9b6eadab770bcdfc65e1762c56766a57c0cfe6de)

--- coderd/database/dbauthz/dbauthz.go
+++ coderd/database/dbauthz/dbauthz.go
@@ -3262,6 +3262,15 @@ func (q *querier) GetGroupMembersCountByGroupID(ctx context.Context, arg databas
 	return memberCount, nil
 }
 
+func (q *querier) GetGroupMembersCountByGroupIDs(ctx context.Context, arg database.GetGroupMembersCountByGroupIDsParams) ([]database.GetGroupMembersCountByGroupIDsRow, error) {
+	if err := q.authorizeContext(ctx, policy.ActionRead, rbac.ResourceGroup); err != nil {
+		// Ideally we would check read access on each group ID, but that would be N queries.
+		// So this function is really only usable by admins.
+		return nil, err
+	}
+	return q.db.GetGroupMembersCountByGroupIDs(ctx, arg)
+}
+
 func (q *querier) GetGroups(ctx context.Context, arg database.GetGroupsParams) ([]database.GetGroupsRow, error) {
 	if err := q.authorizeContext(ctx, policy.ActionRead, rbac.ResourceSystem); err == nil {
 		// Optimize this query for system users as it is used in telemetry.
