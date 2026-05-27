e7461f588a1b592604e0d91fe81cc79c5a6e68c2
Jon Ayers <jon@coder.com>
2026-05-27 19:00:59 +0000

    use parallel

--- coderd/x/nats/cluster_internal_test.go
+++ coderd/x/nats/cluster_internal_test.go
@@ -76,9 +76,11 @@ func Test_parsePeerAddresses(t *testing.T) {
 	})
 }
 
-//nolint:paralleltest // Cluster tests bind free ports and reload shared route state.
+// Cluster tests bind free ports and reload shared route state.
 func TestPubsub_SetPeerAddresses(t *testing.T) {
+	t.Parallel()
 	t.Run("OK", func(t *testing.T) {
+		t.Parallel()
 		a := newTestPubsub(t, newClusterTestOptions(t))
 		b := newTestPubsub(t, newClusterTestOptions(t))
 		c := newTestPubsub(t, newClusterTestOptions(t))
@@ -99,12 +101,14 @@ func TestPubsub_SetPeerAddresses(t *testing.T) {
 	})
 
 	t.Run("StandaloneConfigError", func(t *testing.T) {
+		t.Parallel()
 		ps := newTestPubsub(t, Options{})
 		err := ps.SetPeerAddresses(nil)
 		require.ErrorContains(t, err, "not started with clustering enabled")
 	})
 
 	t.Run("Closed", func(t *testing.T) {
+		t.Parallel()
 		ps := newTestPubsub(t, newClusterTestOptions(t))
 		require.NoError(t, ps.Close())
 		err := ps.SetPeerAddresses(nil)
@@ -112,6 +116,7 @@ func TestPubsub_SetPeerAddresses(t *testing.T) {
 	})
 
 	t.Run("DropsSelfRoute", func(t *testing.T) {
+		t.Parallel()
 		ps := newTestPubsub(t, newClusterTestOptions(t))
 		require.NoError(t, ps.SetPeerAddresses([]string{clusterRouteAddress(t, ps)}))
 		require.Empty(t, ps.currentRoutes)
