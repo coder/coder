8cfd8487310377649008678761fa7c5d2a4a458a
Andrew Aquino <dawneraq@gmail.com>
2026-06-01 22:32:16 +0000

    test: add story to delete template used by a workspace

--- site/src/pages/TemplatePage/TemplatePageHeader.stories.tsx
+++ site/src/pages/TemplatePage/TemplatePageHeader.stories.tsx
@@ -1,12 +1,30 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { MockTemplate, MockTemplateVersion } from "#/testHelpers/entities";
+import {
+	MockTemplate,
+	MockTemplateVersion,
+	MockWorkspace,
+} from "#/testHelpers/entities";
 import { withDashboardProvider } from "#/testHelpers/storybook";
 import { TemplatePageHeader } from "./TemplatePageHeader";
+import { workspacesKey } from "#/api/queries/workspaces";
 
 const meta: Meta<typeof TemplatePageHeader> = {
 	title: "pages/TemplatePage/TemplatePageHeader",
 	component: TemplatePageHeader,
 	decorators: [withDashboardProvider],
+	parameters: {
+		queries: [
+			{
+				key: workspacesKey({
+					q: `organization:${MockTemplate.organization_name} template:${MockTemplate.name}`,
+				}),
+				data: {
+					workspaces: [],
+					count: 0,
+				},
+			},
+		],
+	},
 	args: {
 		template: MockTemplate,
 		activeVersion: MockTemplateVersion,
@@ -29,6 +47,22 @@ export const CanNotUpdate: Story = {
 		permissions: {
 			canUpdateTemplate: false,
 		},
+	},
+};
+
+export const HasWorkspaces: Story = {
+	parameters: {
+		queries: [
+			{
+				key: workspacesKey({
+					q: `organization:${MockTemplate.organization_name} template:${MockTemplate.name}`,
+				}),
+				data: {
+					workspaces: [MockWorkspace],
+					count: 1,
+				},
+			},
+		],
 	},
 };
 
