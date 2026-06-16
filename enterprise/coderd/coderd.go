5326e93c4c23afdde4379d485ca5f5e2b2f63b6d
Susana Cardoso Ferreira <susana@coder.com>
2026-06-16 09:16:30 +0000

    chore: link TODO to AIGOV-443

--- enterprise/coderd/coderd.go
+++ enterprise/coderd/coderd.go
@@ -612,7 +612,7 @@ func New(ctx context.Context, options *Options) (_ *API, err error) {
 		r.Route("/users/{user}/ai/budget", func(r chi.Router) {
 			// AI cost controls are a paid feature (AI Governance add-on).
 			r.Use(
-				// TODO: remove once AI Gateway cost control functionality is stable.
+				// TODO(AIGOV-443): remove once AI Gateway cost control functionality is stable.
 				httpmw.RequireExperiment(api.AGPL.Experiments, codersdk.ExperimentAIGatewayCostControl),
 				api.RequireFeatureMW(codersdk.FeatureAIBridge),
 				apiKeyMiddleware,
