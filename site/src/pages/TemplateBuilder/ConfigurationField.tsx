0a2a38c625be23a12048522c754a82df306a6ef4
Andrew Aquino <dawneraq@gmail.com>
2026-06-25 17:33:32 +0000

    fix: override dropdown's grid-column-end with !important

--- site/src/pages/TemplateBuilder/ConfigurationField.tsx
+++ site/src/pages/TemplateBuilder/ConfigurationField.tsx
@@ -118,8 +118,8 @@ const SelectField: FC<SelectFieldDefinition> = ({
 }) => {
 	const descriptionId = `${id}-description`;
 	return (
-		// All fields span 2 columns, except for dropdowns which can only be 1 column
-		<div className="col-end-1 flex flex-col gap-2">
+		// All fields span 2 columns, except for dropdowns which can only be 1 column (50% width)
+		<div className="!col-end-1 flex flex-col gap-2">
 			<Label htmlFor={id}>
 				{label}
 				{required && (
