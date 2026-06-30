64e52e1020bb0270b961e092137a436eca1643d1
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2026-06-30 15:51:26 +0000

    fix(site/pages/TemplateBuilder): remove duplicate router decorator from story

--- site/src/pages/TemplateBuilder/TemplateAlternatives.stories.tsx
+++ site/src/pages/TemplateBuilder/TemplateAlternatives.stories.tsx
@@ -1,19 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import {
-	reactRouterParameters,
-	withRouter,
-} from "storybook-addon-remix-react-router";
 import { TemplateAlternatives } from "./TemplateAlternatives";
 
 const meta: Meta<typeof TemplateAlternatives> = {
 	title: "pages/TemplateBuilder/TemplateAlternatives",
 	component: TemplateAlternatives,
-	decorators: [withRouter],
-	parameters: {
-		reactRouter: reactRouterParameters({
-			routing: { path: "/templates/new/builder" },
-		}),
-	},
 };
 
 export default meta;
