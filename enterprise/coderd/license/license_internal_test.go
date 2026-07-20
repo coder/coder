585421df6e708a637dd0344df816bb2a380e047d
Scott Miller <scott@coder.com>
2026-07-20 15:42:22 +0000

    test(enterprise/coderd/license): restore TestNextLicenseValidityPeriod
    The previous commit unintentionally replaced license_internal_test.go
    when adding TestBetterUserLimit; restore it and move the new test to
    its own file.

--- enterprise/coderd/license/license_internal_test.go
+++ enterprise/coderd/license/license_internal_test.go
@@ -2,79 +2,139 @@ package license
 
 import (
 	"testing"
+	"time"
 
-	"github.com/stretchr/testify/require"
-
-	"github.com/coder/coder/v2/codersdk"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestBetterUserLimit(t *testing.T) {
+func TestNextLicenseValidityPeriod(t *testing.T) {
 	t.Parallel()
 
-	cand := func(limit int64, entitlement codersdk.Entitlement, addon bool) userLimitCandidate {
-		return userLimitCandidate{limit: limit, entitlement: entitlement, aiGovernanceAddon: addon}
-	}
-	entitled := codersdk.EntitlementEntitled
-	grace := codersdk.EntitlementGracePeriod
+	t.Run("Apply", func(t *testing.T) {
+		t.Parallel()
 
-	cases := []struct {
-		name           string
-		a, b           userLimitCandidate
-		countA, countB int64
-		want           bool
-	}{
-		{
-			name: "ComplianceBeatsEntitlement",
-			a:    cand(200, grace, false), countA: 150,
-			b: cand(100, entitled, false), countB: 150,
-			want: true,
-		},
-		{
-			name: "ComplianceBeatsHigherLimit",
-			a:    cand(100, entitled, true), countA: 90,
-			b: cand(200, entitled, false), countB: 250,
-			want: true,
-		},
-		{
-			name: "EntitlementBeatsLimitWhenBothCompliant",
-			a:    cand(100, entitled, false), countA: 50,
-			b: cand(200, grace, false), countB: 50,
-			want: true,
-		},
-		{
-			name: "HigherLimitWinsWhenBothCompliantAndEqualEntitlement",
-			a:    cand(200, entitled, false), countA: 50,
-			b: cand(100, entitled, false), countB: 50,
-			want: true,
-		},
-		{
-			name: "HigherLimitWinsWhenBothOver",
-			a:    cand(200, entitled, false), countA: 250,
-			b: cand(100, entitled, true), countB: 150,
-			want: true,
-		},
-		{
-			name: "AddonBreaksExactTies",
-			a:    cand(100, entitled, true), countA: 50,
-			b: cand(100, entitled, false), countB: 80,
-			want: true,
-		},
-		{
-			name: "EqualCandidatesAreNotBetter",
-			a:    cand(100, entitled, false), countA: 50,
-			b: cand(100, entitled, false), countB: 50,
-			want: false,
-		},
-	}
+		testCases := []struct {
+			name string
 
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-			require.Equal(t, tc.want, betterUserLimit(tc.a, tc.b, tc.countA, tc.countB))
-			if tc.want {
-				require.False(t, betterUserLimit(tc.b, tc.a, tc.countB, tc.countA),
-					"strict ordering must not hold both ways")
-			}
-		})
+			licensePeriods  [][2]time.Time
+			expectedPeriods [][2]time.Time
+		}{
+			{
+				name:            "None",
+				licensePeriods:  [][2]time.Time{},
+				expectedPeriods: [][2]time.Time{},
+			},
+			{
+				name: "One",
+				licensePeriods: [][2]time.Time{
+					{time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)},
+				},
+				expectedPeriods: [][2]time.Time{
+					{time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)},
+				},
+			},
+			{
+				name: "TwoOverlapping",
+				licensePeriods: [][2]time.Time{
+					{time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC)},
+					{time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC)},
+				},
+				expectedPeriods: [][2]time.Time{
+					{time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC)},
+				},
+			},
+			{
+				name: "TwoNonOverlapping",
+				licensePeriods: [][2]time.Time{
+					{time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)},
+					{time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC)},
+				},
+				expectedPeriods: [][2]time.Time{
+					{time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)},
+					{time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC)},
+				},
+			},
+			{
+				name: "ThreeOverlapping",
+				licensePeriods: [][2]time.Time{
+					{time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC)},
+					{time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 5, 0, 0, 0, 0, time.UTC)},
+					{time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 6, 0, 0, 0, 0, time.UTC)},
+				},
+				expectedPeriods: [][2]time.Time{
+					{time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 6, 0, 0, 0, 0, time.UTC)},
+				},
+			},
+			{
+				name: "ThreeNonOverlapping",
+				licensePeriods: [][2]time.Time{
+					{time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)},
+					{time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC)},
+					{time.Date(2025, 1, 5, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 6, 0, 0, 0, 0, time.UTC)},
+				},
+				expectedPeriods: [][2]time.Time{
+					{time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)},
+					{time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC)},
+					{time.Date(2025, 1, 5, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 6, 0, 0, 0, 0, time.UTC)},
+				},
+			},
+			{
+				name: "PeriodContainsAnotherPeriod",
+				licensePeriods: [][2]time.Time{
+					{time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 8, 0, 0, 0, 0, time.UTC)},
+					{time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 6, 0, 0, 0, 0, time.UTC)},
+				},
+				expectedPeriods: [][2]time.Time{
+					{time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 8, 0, 0, 0, 0, time.UTC)},
+				},
+			},
+			{
+				name: "EndBeforeStart",
+				licensePeriods: [][2]time.Time{
+					{time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC), time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC)},
+				},
+				expectedPeriods: nil,
+			},
+		}
+
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				t.Parallel()
+
+				// Test with all possible permutations of the periods to ensure
+				// consistency regardless of the order.
+				ps := permutations(tc.licensePeriods)
+				for _, p := range ps {
+					t.Logf("permutation: %v", p)
+					period := &licenseValidityPeriod{}
+					for _, times := range p {
+						t.Logf("applying %v", times)
+						period.Apply(times[0], times[1])
+					}
+					assert.Equal(t, tc.expectedPeriods, period.merged(), "merged")
+				}
+			})
+		}
+	})
+}
+
+func permutations[T any](arr []T) [][]T {
+	var res [][]T
+	var helper func([]T, int)
+	helper = func(a []T, i int) {
+		if i == len(a)-1 {
+			// make a copy before appending
+			tmp := make([]T, len(a))
+			copy(tmp, a)
+			res = append(res, tmp)
+			return
+		}
+		for j := i; j < len(a); j++ {
+			a[i], a[j] = a[j], a[i]
+			helper(a, i+1)
+			a[i], a[j] = a[j], a[i] // backtrack
+		}
 	}
+	helper(arr, 0)
+	return res
 }
