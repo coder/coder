f5121823c9a4cd0276098dbcadc3ab08140911a5
McKayla Washburn-Love <kayla@tree.camp>
2026-07-24 00:14:41 +0000

    fix workspace settings stuff

--- site/src/pages/WorkspaceSettingsPage/Sidebar.tsx
+++ site/src/pages/WorkspaceSettingsPage/Sidebar.tsx
@@ -1,28 +1,14 @@
-import { Avatar } from "#/components/Avatar/Avatar";
 import {
 	Sidebar as BaseSidebar,
 	SettingsSidebarNavItem,
-	SidebarHeader,
 } from "#/components/Sidebar/Sidebar";
 import { useWorkspaceSettings } from "./useWorkspaceSettings";
 
 export const Sidebar: React.FC = () => {
-	const { owner, workspace, permissions } = useWorkspaceSettings();
+	const { permissions } = useWorkspaceSettings();
 
 	return (
 		<BaseSidebar>
-			<SidebarHeader
-				avatar={
-					<Avatar
-						variant="icon"
-						src={workspace.template_icon}
-						fallback={workspace.name}
-					/>
-				}
-				title={workspace.name}
-				linkTo={`/@${owner}/${workspace.name}`}
-				subtitle={workspace.template_display_name ?? workspace.template_name}
-			/>
 			<div className="flex flex-col gap-1">
 				<SettingsSidebarNavItem end href="">
 					General
--- site/src/pages/WorkspaceSettingsPage/WorkspaceSettingsLayout.tsx
+++ site/src/pages/WorkspaceSettingsPage/WorkspaceSettingsLayout.tsx
@@ -6,6 +6,15 @@ import {
 	workspacePermissions,
 } from "#/api/queries/workspaces";
 import { ErrorAlert } from "#/components/Alert/ErrorAlert";
+import { Avatar } from "#/components/Avatar/Avatar";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "#/components/Breadcrumb/Breadcrumb";
 import { Loader } from "#/components/Loader/Loader";
 import { pageTitle } from "#/utils/page";
 import { Sidebar } from "./Sidebar";
@@ -29,35 +38,79 @@ export const WorkspaceSettingsLayout: FC = () => {
 	}
 
 	const error = workspaceQuery.error || permissionsQuery.error;
+	const workspace = workspaceQuery.data;
 
 	return (
 		<>
 			<title>{pageTitle(workspaceName, "Settings")}</title>
 
-			<section className="px-4 sm:px-6 lg:px-10 max-w-screen-2xl mx-auto">
-				<div className="flex flex-col gap-8 py-6 lg:flex-row lg:gap-28 lg:py-10">
-					{error ? (
-						<ErrorAlert error={error} />
-					) : (
-						workspaceQuery.data && (
-							<WorkspaceSettings.Provider
-								value={{
-									owner: username,
-									workspace: workspaceQuery.data,
-									permissions: permissionsQuery.data,
-								}}
-							>
-								<Sidebar />
-								<div className="grow min-w-0">
-									<Suspense fallback={<Loader />}>
-										<Outlet />
-									</Suspense>
-								</div>
-							</WorkspaceSettings.Provider>
-						)
-					)}
-				</div>
-			</section>
+			<div>
+				<Breadcrumb>
+					<BreadcrumbList>
+						<BreadcrumbItem>
+							<BreadcrumbPage>Workspace Settings</BreadcrumbPage>
+						</BreadcrumbItem>
+						{workspace && (
+							<>
+								<BreadcrumbSeparator />
+								<BreadcrumbItem>
+									<BreadcrumbPage className="flex items-center gap-2">
+										<Avatar
+											size="sm"
+											fallback={workspace.owner_name}
+											src={workspace.owner_avatar_url}
+										/>
+										{workspace.owner_name}
+									</BreadcrumbPage>
+								</BreadcrumbItem>
+								<BreadcrumbSeparator />
+								<BreadcrumbItem>
+									<BreadcrumbLink to="..">
+										<BreadcrumbPage className="flex items-center gap-2">
+											<Avatar
+												variant="icon"
+												size="sm"
+												fallback={
+													workspace.template_display_name ??
+													workspace.template_name
+												}
+												src={workspace.template_icon}
+											/>
+											{workspace.name}
+										</BreadcrumbPage>
+									</BreadcrumbLink>
+								</BreadcrumbItem>
+							</>
+						)}
+					</BreadcrumbList>
+				</Breadcrumb>
+				<div className="h-px border-none bg-border" />
+
+				<section className="px-4 sm:px-6 lg:px-10 max-w-screen-2xl mx-auto">
+					<div className="flex flex-col gap-8 py-6 lg:flex-row lg:gap-28 lg:py-10">
+						{error ? (
+							<ErrorAlert error={error} />
+						) : (
+							workspaceQuery.data && (
+								<WorkspaceSettings.Provider
+									value={{
+										owner: username,
+										workspace: workspaceQuery.data,
+										permissions: permissionsQuery.data,
+									}}
+								>
+									<Sidebar />
+									<div className="grow min-w-0">
+										<Suspense fallback={<Loader />}>
+											<Outlet />
+										</Suspense>
+									</div>
+								</WorkspaceSettings.Provider>
+							)
+						)}
+					</div>
+				</section>
+			</div>
 		</>
 	);
 };
