1d25da7c06db6efd53a2a6a4403a495fc3e22fbe
Jake Howell <jake@hwll.me>
2026-07-28 04:02:43 +0000

    fix: demui `<Form />` emotion usage

--- site/src/components/Form/Form.tsx
+++ site/src/components/Form/Form.tsx
@@ -1,4 +1,3 @@
-import { useTheme } from "@emotion/react";
 import {
 	type ComponentProps,
 	createContext,
@@ -21,21 +20,14 @@ type FormProps = HTMLProps<HTMLFormElement> & {
 };
 
 export const Form: FC<FormProps> = ({ direction, ...formProps }) => {
-	const theme = useTheme();
-
 	return (
 		<FormContext.Provider value={{ direction }}>
 			<form
 				{...formProps}
-				css={{
-					display: "flex",
-					flexDirection: "column",
-					gap: direction === "horizontal" ? 80 : 40,
-
-					[theme.breakpoints.down("md")]: {
-						gap: 64,
-					},
-				}}
+				className={cn(
+					"flex flex-col gap-16",
+					direction === "horizontal" ? "md:gap-20" : "md:gap-10",
+				)}
 			/>
 		</FormContext.Provider>
 	);
