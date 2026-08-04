37de7e55b985b37b048d9e7aa5ddb018efe80859
TJ <tracy@coder.com>
2026-08-04 09:16:49 -0700

    fix(site): reorder Switch thumb classes to restore translate offset
    After #27071 replaced `h-4 w-4` with `size-4` in the Switch thumb
    variants, the unchecked thumb rendered flush to the left edge (most
    visible in the disabled state). Moving the translate utilities before
    `size-*` restores the intended thumb offset.

--- site/src/components/Switch/Switch.tsx
+++ site/src/components/Switch/Switch.tsx
@@ -34,8 +34,8 @@ const thumbVariants = cva(
 		variants: {
 			size: {
 				default:
-					"size-4 data-[state=checked]:translate-x-2.5 data-[state=unchecked]:-translate-x-1.5",
-				sm: "size-3 data-[state=checked]:translate-x-1.5 data-[state=unchecked]:-translate-x-1.5",
+					"data-[state=checked]:translate-x-2.5 data-[state=unchecked]:-translate-x-1.5 size-4",
+				sm: "data-[state=checked]:translate-x-1.5 data-[state=unchecked]:-translate-x-1.5 size-3",
 			},
 		},
 		defaultVariants: {
