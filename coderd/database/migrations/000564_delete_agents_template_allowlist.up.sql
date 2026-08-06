a23ce3a1aac8470d8fc20f9b540b576b50f44ffa
Ethan Dickson <ethan@coder.com>
2026-08-06 03:48:24 +0000

    migration numbers

--- coderd/database/migrations/000564_delete_agents_template_allowlist.up.sql
+++ coderd/database/migrations/000564_delete_agents_template_allowlist.up.sql
@@ -1,4 +1,4 @@
--- 000562 copied this value into templates.agents_allowed. Deleting the row
+-- 000563 copied this value into templates.agents_allowed. Deleting the row
 -- makes the original list unrecoverable.
 DELETE FROM site_configs
 WHERE key = 'agents_template_allowlist';
