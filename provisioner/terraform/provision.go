3073a8db5b0963b73407b88550b8272cdb33a595
Jon Ayers <jon@coder.com>
2026-08-10 20:52:40 +0000

    feat(provisioner/terraform): export AI agent session token to templates

--- provisioner/terraform/provision.go
+++ provisioner/terraform/provision.go
@@ -375,6 +375,7 @@ func provisionEnv(
 		"CODER_WORKSPACE_ID="+metadata.GetWorkspaceId(),
 		"CODER_WORKSPACE_OWNER_ID="+metadata.GetWorkspaceOwnerId(),
 		"CODER_WORKSPACE_OWNER_SESSION_TOKEN="+metadata.GetWorkspaceOwnerSessionToken(),
+		"CODER_WORKSPACE_AI_AGENT_SESSION_TOKEN="+metadata.GetWorkspaceAiAgentSessionToken(),
 		"CODER_WORKSPACE_TEMPLATE_ID="+metadata.GetTemplateId(),
 		"CODER_WORKSPACE_TEMPLATE_NAME="+metadata.GetTemplateName(),
 		"CODER_WORKSPACE_TEMPLATE_VERSION="+metadata.GetTemplateVersion(),
