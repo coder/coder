821d91fabd23cab94bd10af3fc072b49cfa5f117
Jon Ayers <jon@coder.com>
2026-08-18 18:21:50 -0500

    fix: log tailnet tunnel authorization decisions (#27819)

--- coderd/apidoc/docs.go
+++ coderd/apidoc/docs.go
@@ -18995,11 +18995,11 @@ const docTemplate = `{
                     "type": "string"
                 },
                 "status_code": {
-                    "description": "StatusCode is the HTTP status code of the request.",
+                    "description": "StatusCode is the HTTP status code or tunnel authorization outcome.",
                     "type": "integer"
                 },
                 "user": {
-                    "description": "User is omitted if the connection event was from an unauthenticated user.",
+                    "description": "User is omitted if the connection event was unauthenticated.",
                     "allOf": [
                         {
                             "$ref": "#/definitions/codersdk.User"
