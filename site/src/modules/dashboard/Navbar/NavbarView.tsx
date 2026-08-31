ea65bd950a46d491c5c8f4cfb34cbe79321ce694
Ethan <39577870+ethanndickson@users.noreply.github.com>
2026-08-31 17:49:57 +0200

    fix(site): remove member-facing Models nav item (#28772)
    (cherry picked from commit 3d3b36ff893ba7bb84b46cc96b3a0f738c64eb8f)

--- site/src/modules/dashboard/Navbar/NavbarView.tsx
+++ site/src/modules/dashboard/Navbar/NavbarView.tsx
@@ -33,7 +33,6 @@ interface NavbarViewProps {
 	onSignOut: () => void;
 	adminPermissions: AdminSettingsPermissions;
 	canCreateChat: boolean;
-	canViewModels: boolean;
 	proxyContextValue?: ProxyContextValue;
 }
 
@@ -50,7 +49,6 @@ export const NavbarView: FC<NavbarViewProps> = ({
 	onSignOut,
 	adminPermissions,
 	canCreateChat,
-	canViewModels,
 	proxyContextValue,
 }) => {
 	const prerelease = getPrereleaseFlag(buildInfo);
@@ -82,7 +80,6 @@ export const NavbarView: FC<NavbarViewProps> = ({
 			<NavItems
 				className="ml-4 hidden md:flex"
 				user={user}
-				canViewModels={canViewModels}
 				canCreateChat={canCreateChat}
 			/>
 
@@ -148,7 +145,6 @@ export const NavbarView: FC<NavbarViewProps> = ({
 				<div className="md:hidden">
 					<MobileMenu
 						proxyContextValue={proxyContextValue}
-						canViewModels={canViewModels}
 						adminPermissions={adminPermissions}
 						user={user}
 						supportLinks={supportLinks}
@@ -163,16 +159,10 @@ export const NavbarView: FC<NavbarViewProps> = ({
 interface NavItemsProps {
 	className?: string;
 	user: TypesGen.User;
-	canViewModels: boolean;
 	canCreateChat: boolean;
 }
 
-const NavItems: FC<NavItemsProps> = ({
-	className,
-	user,
-	canCreateChat,
-	canViewModels,
-}) => {
+const NavItems: FC<NavItemsProps> = ({ className, user, canCreateChat }) => {
 	const location = useLocation();
 
 	return (
@@ -197,16 +187,6 @@ const NavItems: FC<NavItemsProps> = ({
 				Templates
 			</NavLink>
 			<TasksNavItem user={user} />
-			{canViewModels && (
-				<NavLink
-					className={({ isActive }) =>
-						cn(linkStyles.default, { [linkStyles.active]: isActive })
-					}
-					to="/ai/settings/models"
-				>
-					Models
-				</NavLink>
-			)}
 			{canCreateChat && (
 				<NavLink
 					className={({ isActive }) => {
