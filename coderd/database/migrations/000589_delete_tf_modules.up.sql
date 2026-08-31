397f8adb678217dd07d025e31d999c63f0aa6dc4
Steven Masley <stevenmasley@gmail.com>
2026-08-31 17:15:47 -0500

    add second query

--- coderd/database/migrations/000589_delete_tf_modules.up.sql
+++ coderd/database/migrations/000589_delete_tf_modules.up.sql
@@ -60,3 +60,26 @@ WHERE files.id = c.id;
 -- 		  AND f.created_at < '2026-08-31 22:00:00+00'
 -- 	)
 -- 	ORDER BY w.name;
+--
+-- A simple SELECT query to show all template versions that were built
+-- from the identified modules
+--
+-- 	SELECT
+-- 		t.name  AS template,
+-- 		tv.name AS template_version,
+-- 		tv.id   AS template_version_id,
+-- 		f.id    AS module_file_id,
+-- 		f.created_at AS module_cached_at,
+-- 		tv.created_at AS version_created_at
+-- 	FROM files f
+-- 			 JOIN template_version_terraform_values tvtv
+-- 				  ON tvtv.cached_module_files = f.id
+-- 			 JOIN template_versions tv
+-- 				  ON tv.id = tvtv.template_version_id
+-- 			 JOIN templates t
+-- 				  ON t.id = tv.template_id
+-- 	WHERE f.created_by = '00000000-0000-0000-0000-000000000000'
+-- 	  AND f.mimetype = 'application/x-tar'
+-- 	  AND f.created_at >= '2026-08-31 08:00:00+00'
+-- 	  AND f.created_at < '2026-08-31 22:00:00+00'
+-- 	ORDER BY t.name, tv.created_at;
