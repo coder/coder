6c499fd31666d53c56b0933ba60f193503fef154
Christin Tostrup <christin@coder.com>
2026-09-03 10:11:23 +0000

    feat(site/src/pages/TemplateBuilder): reduce page header bottom spacing to 24px

--- site/src/pages/TemplateBuilder/TemplateBuilderPageView.tsx
+++ site/src/pages/TemplateBuilder/TemplateBuilderPageView.tsx
@@ -281,7 +281,7 @@ export const TemplateBuilderPageView: FC<TemplateBuilderPageViewProps> = ({
 
 	return (
 		<Margins className="pb-12">
-			<PageHeader>
+			<PageHeader className="pb-6">
 				<PageHeaderTitle>Create new template</PageHeaderTitle>
 				<PageHeaderSubtitle>
 					A Terraform blueprint for reproducible workspaces.
