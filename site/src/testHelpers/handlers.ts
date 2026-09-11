5a0a6a1779fd51553f2116f24fe88ae5c28a9479
Michael Suchacz <203725896+ibetitsmike@users.noreply.github.com>
2026-09-11 00:20:29 +0000

    test(site/src/testHelpers): mock notifications inbox in page tests

--- site/src/testHelpers/handlers.ts
+++ site/src/testHelpers/handlers.ts
@@ -4,6 +4,7 @@ import { HttpResponse, http } from "msw";
 import type {
 	CreateUserSecretRequest,
 	CreateWorkspaceBuildRequest,
+	ListInboxNotificationsResponse,
 	UpdateUserSecretRequest,
 	UserSecret,
 } from "#/api/typesGenerated";
@@ -12,6 +13,13 @@ import * as M from "./entities";
 import { MockGroup, MockWorkspaceQuota } from "./entities";
 
 export const handlers = [
+	http.get("/api/v2/notifications/inbox", () => {
+		return HttpResponse.json<ListInboxNotificationsResponse>({
+			notifications: [],
+			unread_count: 0,
+		});
+	}),
+
 	http.get("/api/v2/templates/:templateId/daus", () => {
 		return HttpResponse.json(M.MockTemplateDAUResponse);
 	}),
