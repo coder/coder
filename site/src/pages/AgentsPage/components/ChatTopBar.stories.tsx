9a339fa935845c33af99984a477519fbe863cdbe
Garrett Delfosse <delfossegarrett@gmail.com>
2026-09-22 17:07:10 +0000

    fix(site/src/pages/AgentsPage/components): provide the dashboard context to ChatTopBar stories

--- site/src/pages/AgentsPage/components/ChatTopBar.stories.tsx
+++ site/src/pages/AgentsPage/components/ChatTopBar.stories.tsx
@@ -72,6 +72,7 @@ const defaultProps = {
 const meta: Meta<typeof ChatTopBar> = {
 	title: "pages/AgentsPage/ChatTopBar",
 	component: ChatTopBar,
+	decorators: [withDashboardProvider],
 	beforeEach: () => {
 		requestArchiveAgent.mockClear();
 		requestArchiveAndDeleteWorkspace.mockClear();
@@ -570,7 +571,7 @@ export const PreservesArchivedFilterOnMobileBack: Story = {
 };
 
 export const ShareChatButton: Story = {
-	decorators: [withAuthProvider, withDashboardProvider],
+	decorators: [withAuthProvider],
 	args: {
 		chat: {
 			...MockChat,
